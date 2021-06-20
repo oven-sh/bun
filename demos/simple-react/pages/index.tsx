020ce327794089995f6b4511cfca1e949a7eab03
Jarred Sumner <jarred@jarredsumner.com>
2021-06-19 18:30:32 -0700

    color looper i'll probably get rid of
    Former-commit-id: 5015c6a96964c23154509bc26633c18ce36aa9ff

---
+++ demos/simple-react/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Main } from "../src/main";
+
+export function getInitialProps() {
+  return {};
+}
+
+export default function IndexRoute() {
+  return <Main productName={"Next.js (Webpack 5)"} />;
+}

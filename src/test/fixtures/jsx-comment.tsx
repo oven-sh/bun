9a2ce8e1fa8ef8ddf1bd9a735b9e9b9d87586d91
Jarred Sumner <jarred@jarredsumner.com>
2021-08-27 02:33:31 -0700

    jsx comment fixture
    Former-commit-id: e3d9bb34c8deff27b72a45f182522a0057eeb42d

---
+++ src/test/fixtures/jsx-comment.tsx
@@ -0,0 +1,19 @@
+const foo = (
+  <HelloComponent
+    // this is valid JSX!
+    wow="hello"
+  />
+);
+
+const bar = (
+  <HelloComponent
+    /* this 
+    is 
+    also 
+    valid 
+    JSX! 
+  */
+    wow="hello"
+    /* extremely valid JSX!  */
+  />
+);

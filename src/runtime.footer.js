01601fc8a7e609f1e88188f862e7f0c165624413
Jarred Sumner <jarred@jarredsumner.com>
2021-09-23 17:36:49 -0700

    Globally define require() so that UMD modules which check for the existence of require() to determine if it's CommonJS use the CommonJS code path instead of the globalThis code path

--- src/runtime.footer.js
+++ src/runtime.footer.js
@@ -16,3 +16,4 @@ export var __export = BUN_RUNTIME.__export;
 export var __reExport = BUN_RUNTIME.__reExport;
 export var __cJS2eSM = BUN_RUNTIME.__cJS2eSM;
 export var regeneratorRuntime = BUN_RUNTIME.regeneratorRuntime;
+globalThis.require ||= BUN_RUNTIME.__require;

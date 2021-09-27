86109dcfd008baf8778cc221cc25f90dd77121e9
Jarred Sumner <jarred@jarredsumner.com>
2021-09-27 01:33:15 -0700

    Add a few macros examples

---
+++ examples/macros/components/index.tsx
@@ -0,0 +1,11 @@
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+import { IPAddresses } from "./example";
+
+const Start = function () {
+  const root = document.createElement("div");
+  document.body.appendChild(root);
+  ReactDOM.render(<IPAddresses />, root);
+};
+
+Start();

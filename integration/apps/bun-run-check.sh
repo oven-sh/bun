95d2860930f306bfc7c2eb68d13ede9a56f949e0
Jarred Sumner <jarred@jarredsumner.com>
2021-10-29 19:17:44 -0700

    Add integration test that verifies bash exported functions work in `bun run`

--- integration/apps/bun-run-check.sh
+++ integration/apps/bun-run-check.sh
@@ -2,13 +2,29 @@
 
 killall -9 $(basename $BUN_BIN) || echo "";
 
+# https://github.com/Jarred-Sumner/bun/issues/40
+# Define a function (details aren't important)
+fn() { :; }
+# The important bit: export the function
+export -f fn
+
+
 rm -rf /tmp/bun-run-check
 mkdir -p /tmp/bun-run-check
 
 
 cp ./bun-run-check-package.json /tmp/bun-run-check/package.json
 cd /tmp/bun-run-check
-bun run --silent this-should-work
+
+$BUN_BIN run bash -- -c ""
+
+if (( $? )); then
+    echo "Bash exported functions are broken"
+    exit 1
+fi
+
+$BUN_BIN run --silent this-should-work
+
 exit $?
 
 

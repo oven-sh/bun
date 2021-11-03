35c05f61e540efbe8717aaf461f1aad4541e878d
Jarred Sumner <jarred@jarredsumner.com>
2021-11-03 00:22:37 -0700

    Bump next version

--- examples/next/pages/index.tsx
+++ examples/next/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home({}) {
 
         <p className={styles.description}>
           Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>pages/index.tsx</code>
         </p>
       </main>
 

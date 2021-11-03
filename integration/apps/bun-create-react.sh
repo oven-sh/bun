ac9bd094f38f63ed4311164d9f9897cf19d04d88
Jarred Sumner <jarred@jarredsumner.com>
2021-11-02 23:25:31 -0700

    [test] Make sure we curl a jsx file in react integration test

--- integration/apps/bun-create-react.sh
+++ integration/apps/bun-create-react.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-killall -9 $(basename $BUN_BIN) || echo "";
+killall -9 $(basename $BUN_BIN) || echo ""
 
-rm -rf /tmp/react-app;
-mkdir -p /tmp/react-app;
-$BUN_BIN create react /tmp/react-app;
-cd /tmp/react-app;
+rm -rf /tmp/react-app
+mkdir -p /tmp/react-app
+$BUN_BIN create react /tmp/react-app
+cd /tmp/react-app
 BUN_CRASH_WITHOUT_JIT=1 $BUN_BIN --port 8087 &
 sleep 0.005
 
-curl --fail http://localhost:8087/ && killall -9 $(basename $BUN_BIN) && echo "✅ bun create react passed."
+curl --fail http://localhost:8087/ && curl --fail http://localhost:8087/src/index.jsx && killall -9 $(basename $BUN_BIN) && echo "✅ bun create react passed."
 exit $?
-

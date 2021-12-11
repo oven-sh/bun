50f8e0daa84e498bb236a2e9697f1fbd835c777e
Jarred Sumner <jarred@jarredsumner.com>
2021-12-10 20:25:25 -0800

    [bun install] Improve test coverage for tilda version ranges

--- src/install/semver.zig
+++ src/install/semver.zig
@@ -1746,6 +1746,29 @@ test "Version parsing" {
 
 test "Range parsing" {
     defer expect.done(@src());
+
+    expect.range("~1.2.3", "1.2.3", @src());
+    expect.range("~1.2", "1.2.0", @src());
+    expect.range("~1", "1.0.0", @src());
+    expect.range("~1", "1.2.0", @src());
+    expect.range("~1", "1.2.999", @src());
+    expect.range("~0.2.3", "0.2.3", @src());
+    expect.range("~0.2", "0.2.0", @src());
+    expect.range("~0.2", "0.2.1", @src());
+
+    expect.range("~0 ", "0.0.0", @src());
+
+    expect.notRange("~1.2.3", "1.3.0", @src());
+    expect.notRange("~1.2", "1.3.0", @src());
+    expect.notRange("~1", "2.0.0", @src());
+    expect.notRange("~0.2.3", "0.3.0", @src());
+    expect.notRange("~0.2.3", "1.0.0", @src());
+    expect.notRange("~0 ", "1.0.0", @src());
+    expect.notRange("~0.2", "0.1.0", @src());
+    expect.notRange("~0.2", "0.3.0", @src());
+
+    expect.notRange("~3.0.5", "3.3.0", @src());
+
     expect.range("^1.1.4", "1.1.4", @src());
 
     expect.range(">2", "3", @src());

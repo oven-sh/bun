a1aadd5d32dbb0285c2b0f353933cb1bfb2584bd
Jarred Sumner <jarred@jarredsumner.com>
2021-12-25 15:55:47 -0800

    run-test file

---
+++ run-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+cd $GITHUB_WORKSPACE/bun
+
+bun install
+bun install --cwd $GITHUB_WORKSPACE/bun/integration/snippets
+bun install --cwd $GITHUB_WORKSPACE/bun/integration/scripts
+
+make $BUN_TEST_FILE

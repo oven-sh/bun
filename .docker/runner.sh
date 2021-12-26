74bf8ca5ddb99fb65e4690db0fd4ca76fe10bfd7
Jarred Sumner <jarred@jarredsumner.com>
2021-12-26 00:18:01 -0800

    :runner:

--- .docker/runner.sh
+++ .docker/runner.sh
@@ -2,5 +2,4 @@
 
 set -euxo pipefail
 
-id=$(cat ${RUNNER_TEMP}/container-id)
-docker container run --security-opt seccomp=.docker/chrome.json --env GITHUB_WORKSPACE=$GITHUB_WORKSPACE --env BUN_TEST_NAME=$BUN_TEST_NAME --ulimit memlock=-1:-1 --init --rm ${id}
+docker container run --security-opt seccomp=.docker/chrome.json --env GITHUB_WORKSPACE=$GITHUB_WORKSPACE --env BUN_TEST_NAME=$BUN_TEST_NAME --ulimit memlock=-1:-1 --init --rm bun-test:latest

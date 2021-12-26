b1b1e5d1a58b243055369c8f8932072d58aab396
Jarred Sumner <jarred@jarredsumner.com>
2021-12-25 23:04:31 -0800

    explicit runner

---
+++ .docker/runner.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+id=$(cat ${RUNNER_TEMP}/container-id)
+docker container run --security-opt seccomp=.docker/chrome.json --env GITHUB_WORKSPACE=$GITHUB_WORKSPACE --env BUN_TEST_NAME=$BUN_TEST_NAME --ulimit memlock=-1:-1 --init --rm ${id}

efdf7fb93e21cf37a6f4195b6bc047c85af4ac99
Jarred Sumner <jarred@jarredsumner.com>
2021-12-27 20:32:00 -0800

    Update build-base.sh

--- .docker/build-base.sh
+++ .docker/build-base.sh
@@ -4,9 +4,18 @@ set -euxo pipefail
 
 export DOCKER_BUILDKIT=1
 
-docker buildx build -t bunbunbunbun/bun-test-base:latest -f Dockerfile.base --target bun-test-base . --platform=linux/amd64 --build-arg BUILDARCH=amd64
-docker buildx build -t bunbunbunbun/bun-base:latest --platform=linux/amd64 --build-arg BUILDARCH=amd64 .
-docker buildx build -t bunbunbunbun/bun-base-with-zig-and-webkit:latest --platform=linux/amd64 --build-arg BUILDARCH=amd64 .
+docker buildx build \
+    -t bunbunbunbun/bun-test-base:latest -f Dockerfile.base \
+    --target bun-test-base \
+    --platform=linux/$BUILDARCH --build-arg BUILDARCH=$BUILDARCH .
+docker buildx build \
+    --target bun-base \
+    -t bunbunbunbun/bun-base:latest --platform=linux/$UILDARCH \
+    --build-arg BUILDARCH=$BUILDARCH .
+docker buildx build \
+    -t bunbunbunbun/bun-base-with-zig-and-webkit:latest \
+    --target bun-base-with-zig-and-webkit \
+    --platform=linux/$BUILDARCH --build-arg BUILDARCH=$BUILDARCH .
 
 docker push bunbunbunbun/bun-test-base:latest
 docker push bunbunbunbun/bun-base:latest

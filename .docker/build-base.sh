77778aeadf7b142d8633d09c7b4330d5bcdcb7f1
Jarred Sumner <jarred@jarredsumner.com>
2021-12-27 20:32:50 -0800

    Update build-base.sh

--- .docker/build-base.sh
+++ .docker/build-base.sh
@@ -10,10 +10,12 @@ docker buildx build \
     --platform=linux/$BUILDARCH --build-arg BUILDARCH=$BUILDARCH .
 docker buildx build \
     --target bun-base \
+    -f Dockerfile.base \
     -t bunbunbunbun/bun-base:latest --platform=linux/$BUILDARCH \
     --build-arg BUILDARCH=$BUILDARCH .
 docker buildx build \
     -t bunbunbunbun/bun-base-with-zig-and-webkit:latest \
+    -f Dockerfile.base \
     --target bun-base-with-zig-and-webkit \
     --platform=linux/$BUILDARCH --build-arg BUILDARCH=$BUILDARCH .
 

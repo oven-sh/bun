6ef71a800f16fb55bc2071d98082260c2dda230e
Jarred Sumner <jarred@jarredsumner.com>
2021-12-27 20:33:49 -0800

    Update pull.sh

--- .docker/pull.sh
+++ .docker/pull.sh
@@ -2,10 +2,10 @@
 
 set -euxo pipefail
 
-docker pull bunbunbunbun/bun-test-base:amd64 --platform=linux/amd64
-docker pull bunbunbunbun/bun-base:amd64 --platform=linux/amd64
-docker pull bunbunbunbun/bun-base-with-zig-and-webkit:amd64 --platform=linux/amd64
+docker pull bunbunbunbun/bun-test-base:latest --platform=linux/amd64
+docker pull bunbunbunbun/bun-base:latest --platform=linux/amd64
+docker pull bunbunbunbun/bun-base-with-zig-and-webkit:latest --platform=linux/amd64
 
-docker tag bun-base:latest bunbunbunbun/bun-base:amd64
-docker tag bun-base-with-zig-and-webkit:latest bunbunbunbun/bun-base-with-zig-and-webkit:amd64
-docker tag bun-test-base:latest bunbunbunbun/bun-test-base:amd64
+docker tag bun-base:latest bunbunbunbun/bun-base:latest
+docker tag bun-base-with-zig-and-webkit:latest bunbunbunbun/bun-base-with-zig-and-webkit:latest
+docker tag bun-test-base:latest bunbunbunbun/bun-test-base:latest

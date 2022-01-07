aefbc2e67faae25d25b9fbf0fd11da191edeae39
Jarred Sumner <jarred@jarredsumner.com>
2022-01-06 19:32:10 -0800

    Update identity_context.zig

--- src/identity_context.zig
+++ src/identity_context.zig
@@ -12,7 +12,7 @@ pub fn IdentityContext(comptime Key: type) type {
 
 /// When storing hashes as keys in a hash table, we don't want to hash the hashes or else we increase the chance of collisions. This is also marginally faster since it means hashing less stuff.
 /// `ArrayIdentityContext` and `IdentityContext` are distinct because ArrayHashMap expects u32 hashes but HashMap expects u64 hashes.
-const ArrayIdentityContext = struct {
+pub const ArrayIdentityContext = struct {
     pub fn hash(_: @This(), key: u32) u32 {
         return key;
     }

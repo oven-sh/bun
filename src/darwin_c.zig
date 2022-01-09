2e998692457c21f278b8366da361f8671ec325df
Jarred Sumner <jarred@jarredsumner.com>
2022-01-08 23:12:36 -0800

    Update darwin_c.zig

--- src/darwin_c.zig
+++ src/darwin_c.zig
@@ -9,6 +9,61 @@ const off_t = std.c.off_t;
 const errno = os.errno;
 const zeroes = mem.zeroes;
 
+pub const COPYFILE_STATE_SRC_FD = @as(c_int, 1);
+pub const COPYFILE_STATE_SRC_FILENAME = @as(c_int, 2);
+pub const COPYFILE_STATE_DST_FD = @as(c_int, 3);
+pub const COPYFILE_STATE_DST_FILENAME = @as(c_int, 4);
+pub const COPYFILE_STATE_QUARANTINE = @as(c_int, 5);
+pub const COPYFILE_STATE_STATUS_CB = @as(c_int, 6);
+pub const COPYFILE_STATE_STATUS_CTX = @as(c_int, 7);
+pub const COPYFILE_STATE_COPIED = @as(c_int, 8);
+pub const COPYFILE_STATE_XATTRNAME = @as(c_int, 9);
+pub const COPYFILE_STATE_WAS_CLONED = @as(c_int, 10);
+pub const COPYFILE_DISABLE_VAR = "COPYFILE_DISABLE";
+pub const COPYFILE_ACL = @as(c_int, 1) << @as(c_int, 0);
+pub const COPYFILE_STAT = @as(c_int, 1) << @as(c_int, 1);
+pub const COPYFILE_XATTR = @as(c_int, 1) << @as(c_int, 2);
+pub const COPYFILE_DATA = @as(c_int, 1) << @as(c_int, 3);
+pub const COPYFILE_SECURITY = COPYFILE_STAT | COPYFILE_ACL;
+pub const COPYFILE_METADATA = COPYFILE_SECURITY | COPYFILE_XATTR;
+pub const COPYFILE_ALL = COPYFILE_METADATA | COPYFILE_DATA;
+/// Descend into hierarchies 
+pub const COPYFILE_RECURSIVE = @as(c_int, 1) << @as(c_int, 15);
+/// return flags for xattr or acls if set 
+pub const COPYFILE_CHECK = @as(c_int, 1) << @as(c_int, 16);
+/// fail if destination exists 
+pub const COPYFILE_EXCL = @as(c_int, 1) << @as(c_int, 17);
+/// don't follow if source is a symlink 
+pub const COPYFILE_NOFOLLOW_SRC = @as(c_int, 1) << @as(c_int, 18);
+/// don't follow if dst is a symlink 
+pub const COPYFILE_NOFOLLOW_DST = @as(c_int, 1) << @as(c_int, 19);
+/// unlink src after copy 
+pub const COPYFILE_MOVE = @as(c_int, 1) << @as(c_int, 20);
+/// unlink dst before copy 
+pub const COPYFILE_UNLINK = @as(c_int, 1) << @as(c_int, 21);
+pub const COPYFILE_NOFOLLOW = COPYFILE_NOFOLLOW_SRC | COPYFILE_NOFOLLOW_DST;
+pub const COPYFILE_PACK = @as(c_int, 1) << @as(c_int, 22);
+pub const COPYFILE_UNPACK = @as(c_int, 1) << @as(c_int, 23);
+pub const COPYFILE_CLONE = @as(c_int, 1) << @as(c_int, 24);
+pub const COPYFILE_CLONE_FORCE = @as(c_int, 1) << @as(c_int, 25);
+pub const COPYFILE_RUN_IN_PLACE = @as(c_int, 1) << @as(c_int, 26);
+pub const COPYFILE_DATA_SPARSE = @as(c_int, 1) << @as(c_int, 27);
+pub const COPYFILE_PRESERVE_DST_TRACKED = @as(c_int, 1) << @as(c_int, 28);
+pub const COPYFILE_VERBOSE = @as(c_int, 1) << @as(c_int, 30);
+pub const COPYFILE_RECURSE_ERROR = @as(c_int, 0);
+pub const COPYFILE_RECURSE_FILE = @as(c_int, 1);
+pub const COPYFILE_RECURSE_DIR = @as(c_int, 2);
+pub const COPYFILE_RECURSE_DIR_CLEANUP = @as(c_int, 3);
+pub const COPYFILE_COPY_DATA = @as(c_int, 4);
+pub const COPYFILE_COPY_XATTR = @as(c_int, 5);
+pub const COPYFILE_START = @as(c_int, 1);
+pub const COPYFILE_FINISH = @as(c_int, 2);
+pub const COPYFILE_ERR = @as(c_int, 3);
+pub const COPYFILE_PROGRESS = @as(c_int, 4);
+pub const COPYFILE_CONTINUE = @as(c_int, 0);
+pub const COPYFILE_SKIP = @as(c_int, 1);
+pub const COPYFILE_QUIT = @as(c_int, 2);
+
 // int clonefileat(int src_dirfd, const char * src, int dst_dirfd, const char * dst, int flags);
 pub extern "c" fn clonefileat(c_int, [*c]const u8, c_int, [*c]const u8, uint32_t: c_int) c_int;
 // int fclonefileat(int srcfd, int dst_dirfd, const char * dst, int flags);

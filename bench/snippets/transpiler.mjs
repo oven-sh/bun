8f7cf6bf476016280c453da53e45c040761be1d6
Jarred Sumner <jarred@jarredsumner.com>
2022-01-21 05:09:58 -0800

    little

--- bench/snippets/transpiler.mjs
+++ bench/snippets/transpiler.mjs
@@ -1,23 +1,27 @@
 import { readFileSync } from "fs";
 
 var transformSync;
+var transform;
 var opts;
 if (process.isBun) {
   const transpiler = new Bun.Transpiler({ loader: "jsx" });
   transformSync = transpiler.transformSync.bind(transpiler);
+  transform = transpiler.transform.bind(transpiler);
   opts = "jsx";
-} else if (process.env["USE_ESBUILD"]) {
+} else if (process.env["esbuild"]) {
   try {
     const esbuild = await import("esbuild");
     transformSync = esbuild.transformSync;
+    transform = esbuild.transform;
     opts = { loader: "jsx" };
   } catch (exception) {
     throw exception;
   }
-} else if (process.env["USE_SWC"]) {
+} else if (process.env["swc"]) {
   try {
     const swc = await import("@swc/core");
     transformSync = swc.transformSync;
+    transform = swc.transform;
     opts = {
       sourceMaps: false,
       inlineSourcesContent: false,
@@ -31,8 +35,24 @@ if (process.isBun) {
   } catch (exception) {
     throw exception;
   }
+} else if (process.env["babel"]) {
+  try {
+    const swc = await import("@babel/core");
+    transformSync = swc.transformSync;
+    transform = swc.transform;
+    opts = {
+      sourceMaps: false,
+      presets: [(await import("@babel/preset-react")).default],
+    };
+  } catch (exception) {
+    throw exception;
+  }
 }
 
 const code = readFileSync("src/test/fixtures/simple.jsx", "utf8");
 
-transformSync(code, opts);
+if (process.env.SYNC) {
+  console.log(transformSync(code, opts));
+} else {
+  console.log(await transform(code, opts));
+}

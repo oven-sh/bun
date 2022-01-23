9c627e884de0e3c3b0f6d1819aabd9a484ff2d1e
Jarred Sumner <jarred@jarredsumner.com>
2022-01-22 21:40:57 -0800

    Create cat.mjs

---
+++ bench/snippets/cat.mjs
@@ -0,0 +1,5 @@
+import { readFileSync } from "node:fs";
+const count = parseInt(process.env.ITERATIONS || "1", 10) || 1;
+const arg = process.argv.slice(1);
+for (let i = 0; i < count; i++)
+  console.log(arg.map((file) => readFileSync(file, "utf8")).join(""));

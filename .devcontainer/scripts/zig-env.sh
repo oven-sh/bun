7dbfb3ef41d634c33f48239aa9a767c7ef8af19d
Jarred Sumner <jarred@jarredsumner.com>
2022-01-25 19:57:12 -0800

    Fix up dev container

--- .devcontainer/scripts/zig-env.sh
+++ .devcontainer/scripts/zig-env.sh
@@ -3,6 +3,5 @@
 curl -L https://github.com/Jarred-Sumner/vscode-zig/releases/download/fork-v1/zig-0.2.5.vsix >/home/ubuntu/vscode-zig.vsix
 git clone https://github.com/zigtools/zls /home/ubuntu/zls
 cd /home/ubuntu/zls
-git checkout e472fca3be6335f16032b48e40ca0d5ffda6ab0a
 git submodule update --init --recursive --progress --depth=1
 zig build -Drelease-fast

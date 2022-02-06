9715c9223abfabb3cde1a7adfaf9e2a553ee901e
Jarred Sumner <jarred@jarredsumner.com>
2022-02-05 21:26:55 -0800

    Add unit test for toml imports

---
+++ integration/bunjs-only-snippets/toml.test.js
@@ -0,0 +1,17 @@
+import { describe, it, expect } from "bun:test";
+
+it("syntax", async () => {
+  const toml = (await import("./toml-fixture.toml")).default;
+  expect(toml.framework).toBe("next");
+  expect(toml.bundle.packages["@emotion/react"]).toBe(true);
+  expect(toml.array[0].entry_one).toBe("one");
+  expect(toml.array[0].entry_two).toBe("two");
+  expect(toml.array[1].entry_one).toBe("three");
+  expect(toml.array[1].entry_two).toBe(undefined);
+  expect(toml.array[1].nested[0].entry_one).toBe("four");
+  expect(toml.dev.one.two.three).toBe(4);
+  expect(toml.dev.foo).toBe(123);
+  expect(toml.inline.array[0]).toBe(1234);
+  expect(toml.inline.array[1]).toBe(4);
+  expect(toml.dev["foo.bar"]).toBe("baz");
+});

12dbc5dfa4f3252f45ce3528b6ebe17216b346c0
Jarred Sumner <jarred@jarredsumner.com>
2022-02-24 21:00:42 -0800

    Update generated versions

--- src/generated_versions_list.zig
+++ src/generated_versions_list.zig
@@ -1,7 +1,7 @@
 // AUTO-GENERATED FILE. Created via .scripts/write-versions.sh
 
-pub const webkit = "96e77eccfde8dc9c207520d8ced856d8bdb8d386";
-pub const mimalloc = "f412df7a2b64421e1f1d61fde6055a6ea288e8f5";
+pub const webkit = "0796400b47734b1d446eeb0197221d8dc64bdc53";
+pub const mimalloc = "817569dfad79732233fb86649c89e04387ce02e9";
 pub const libarchive = "dc321febde83dd0f31158e1be61a7aedda65e7a2";
 pub const picohttpparser = "066d2b1e9ab820703db0837a7255d92d30f0c9f5";
 pub const boringssl = "b3ed071ecc4efb77afd0a025ea1078da19578bfd";

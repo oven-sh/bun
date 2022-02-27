a6901fdac1684492c340f4eb949f815ad0537f9a
Jarred Sumner <jarred@jarredsumner.com>
2022-02-27 01:53:01 -0800

    Update jsc.zig

--- src/jsc.zig
+++ src/jsc.zig
@@ -6,6 +6,9 @@ pub usingnamespace @import("./javascript/jsc/javascript.zig");
 pub const C = @import("./javascript/jsc/javascript_core_c_api.zig");
 pub const WebCore = @import("./javascript/jsc/webcore.zig");
 pub const Jest = @import("./javascript/jsc/test/jest.zig");
+pub const API = struct {
+    pub const Transpiler = @import("./javascript/jsc/api/transpiler.zig");
+};
 pub const Node = struct {
     pub usingnamespace @import("./javascript/jsc/node/types.zig");
     pub usingnamespace @import("./javascript/jsc/node/node_fs.zig");

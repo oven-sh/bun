64466e3259b53d4676ce8eb9982f6ebf00c6f9fa
Jarred Sumner <jarred@jarredsumner.com>
2022-03-15 06:03:29 -0700

    bump webkit

--- src/generated_versions_list.zig
+++ src/generated_versions_list.zig
@@ -1,6 +1,6 @@
 // AUTO-GENERATED FILE. Created via .scripts/write-versions.sh
 
-pub const webkit = "0796400b47734b1d446eeb0197221d8dc64bdc53";
+pub const webkit = "9a039f4901c48680700174c28dd4693873f7593a";
 pub const mimalloc = "817569dfad79732233fb86649c89e04387ce02e9";
 pub const libarchive = "dc321febde83dd0f31158e1be61a7aedda65e7a2";
 pub const picohttpparser = "066d2b1e9ab820703db0837a7255d92d30f0c9f5";

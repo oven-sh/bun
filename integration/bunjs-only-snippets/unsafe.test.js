b633573ad0904c1caaea4d867174695e7bd5fd19
Jarred Sumner <jarred@jarredsumner.com>
2022-03-16 05:05:37 -0700

    [bun.js] Bun.unsafe test should check the gc

--- integration/bunjs-only-snippets/unsafe.test.js
+++ integration/bunjs-only-snippets/unsafe.test.js
@@ -1,17 +1,26 @@
 import { test, expect, it, describe } from "bun:test";
 
-it("arrayBufferToString u8", () => {
+it("arrayBufferToString u8", async () => {
   var encoder = new TextEncoder();
   const bytes = encoder.encode("hello world");
   Bun.gc(true);
   expect(Bun.unsafe.arrayBufferToString(bytes)).toBe("hello world");
+  Bun.gc(true);
+  await new Promise((resolve) => setTimeout(resolve, 0));
+  Bun.gc(true);
 });
 
-it("arrayBufferToString ArrayBuffer", () => {
+it("arrayBufferToString ArrayBuffer", async () => {
   var encoder = new TextEncoder();
-  const bytes = encoder.encode("hello world");
+  var bytes = encoder.encode("hello world");
   Bun.gc(true);
-  expect(Bun.unsafe.arrayBufferToString(bytes.buffer)).toBe("hello world");
+  const out = Bun.unsafe.arrayBufferToString(bytes.buffer);
+  expect(out).toBe("hello world");
+  Bun.gc(true);
+  await new Promise((resolve) => setTimeout(resolve, 0));
+  globalThis.bytes = bytes;
+  Bun.gc(true);
+  expect(out).toBe("hello world");
 });
 
 it("arrayBufferToString u16", () => {
@@ -29,4 +38,5 @@ it("arrayBufferToString u16", () => {
   }
   Bun.gc(true);
   expect(charCodes.length).toBe("hello world".length);
+  Bun.gc(true);
 });

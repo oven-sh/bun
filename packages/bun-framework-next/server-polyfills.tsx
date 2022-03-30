c5538cfe102d5865eec03789f620b75e6c1fb3f5
Jarred Sumner <jarred@jarredsumner.com>
2022-03-30 15:46:12 -0700

    Don't need to import react here

--- packages/bun-framework-next/server-polyfills.tsx
+++ packages/bun-framework-next/server-polyfills.tsx
@@ -2,7 +2,6 @@ globalThis.global = globalThis;
 
 import { Buffer } from "buffer";
 import { URL } from "./url-polyfill";
-import * as React from "react";
 
 globalThis.Buffer ||= Buffer;
 globalThis.URL = URL;

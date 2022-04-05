2c0e8e0a99932fbf10872cce4ad417d24cdf262e
Jarred Sumner <jarred@jarredsumner.com>
2022-04-05 03:51:24 -0700

    Add update message

--- packages/bun-framework-next/server-polyfills.tsx
+++ packages/bun-framework-next/server-polyfills.tsx
@@ -1,3 +1,16 @@
 import { Buffer } from "buffer";
 
 globalThis.Buffer ||= Buffer;
+
+if (!("URL" in globalThis)) {
+  class Outdated extends Error {
+    constructor(message) {
+      super(message);
+      this.name = "Outdated";
+    }
+  }
+
+  throw new Outdated(
+    "Missing \"URL\", please run 'bun upgrade' to update to the latest version of Bun (added in v0.0.74)"
+  );
+}

ddaab5a836a0ec669ceebeb75ff8543178e89dc3
Jarred Sumner <jarred@jarredsumner.com>
2022-04-05 19:15:58 -0700

    fix example

--- examples/bun/http.ts
+++ examples/bun/http.ts
@@ -23,13 +23,13 @@ Bun.serve({
 });
 
 // Start a fast HTTP server from the main file's export
-export default {
-  fetch(req) {
-    return new Response(
-      `This is another way to start a server! 
-       if the main file export default's an object 
-       with 'fetch'. Bun automatically calls Bun.serve`
-    );
-  },
-  // so autocomplete & type checking works
-} as Bun.Serve;
+// export default {
+//   fetch(req) {
+//     return new Response(
+//       `This is another way to start a server!
+//        if the main file export default's an object
+//        with 'fetch'. Bun automatically calls Bun.serve`
+//     );
+//   },
+//   // so autocomplete & type checking works
+// } as Bun.Serve;

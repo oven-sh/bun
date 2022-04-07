5ac69205913bb949d015d8162cefe14d488209f5
Jarred Sumner <jarred@jarredsumner.com>
2022-04-06 23:38:37 -0700

    7 hrs

--- src/runtime/index-with-refresh.ts
+++ src/runtime/index-with-refresh.ts
@@ -1,12 +1,7 @@
-import { __injectFastRefresh } from "./hmr";
 export * from "./hmr";
 export * from "./errors";
 export * from "./index-without-hmr";
-import * as __FastRefreshRuntime from "../react-refresh";
-if (typeof window !== "undefined") {
-  __injectFastRefresh(__FastRefreshRuntime);
-}
-export { __FastRefreshRuntime };
+export * as __FastRefreshRuntime from "../react-refresh";
 
 globalThis.process ||= {
   env: {},

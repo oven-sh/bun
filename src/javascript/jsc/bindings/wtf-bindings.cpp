3b6c3d8196aa9f32b7180723a95512337f49ed8c
Jarred Sumner <jarred@jarredsumner.com>
2022-05-08 01:12:20 -0700

    Update wtf-bindings.cpp

--- src/javascript/jsc/bindings/wtf-bindings.cpp
+++ src/javascript/jsc/bindings/wtf-bindings.cpp
@@ -10,5 +10,5 @@ extern "C" JSC::EncodedJSValue WTF__toBase64URLStringValue(const uint8_t* bytes,
 {
     WTF::String string = WTF::base64URLEncodeToString(reinterpret_cast<const LChar*>(bytes), static_cast<unsigned int>(length));
     string.impl()->ref();
-    return JSC::JSValue::encode(JSC::jsString(globalObject->vm(), string.impl()));
+    return JSC::JSValue::encode(JSC::jsString(globalObject->vm(), string));
 }

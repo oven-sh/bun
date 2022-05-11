47b6dc9eea9ccf93e217bf61492af4d52ac7ef3f
Jarred Sumner <jarred@jarredsumner.com>
2022-05-11 03:19:29 -0700

    Add test for import.meta.require

--- integration/bunjs-only-snippets/import-meta.test.js
+++ integration/bunjs-only-snippets/import-meta.test.js
@@ -1,9 +1,16 @@
 import { it, expect } from "bun:test";
+import sync from "./require-json.json";
 
 const { path, dir } = import.meta;
 
 it("import.meta.resolveSync", () => {
-  expect(import.meta.resolveSync(import.meta.file, import.meta.dir)).toBe(path);
+  expect(
+    import.meta.resolveSync("./" + import.meta.file, import.meta.path)
+  ).toBe(path);
+});
+
+it("import.meta.require", () => {
+  expect(import.meta.require("./require-json.json").hello).toBe(sync.hello);
 });
 
 it("import.meta.dir", () => {

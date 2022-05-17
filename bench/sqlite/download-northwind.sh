7ca297a5cc3d19d9a7af9f3965cc11fda7ffb78f
Jarred Sumner <jarred@jarredsumner.com>
2022-05-16 19:51:30 -0700

    Update download-northwind.sh

--- bench/sqlite/download-northwind.sh
+++ bench/sqlite/download-northwind.sh
@@ -2,7 +2,9 @@
 set -euo pipefail
 
 rm -rf Northwind_large.sqlite.zip
-wget https://github.com/jpwhite3/northwind-SQLite3/blob/master/Northwind_large.sqlite.zip
+wget https://github.com/jpwhite3/northwind-SQLite3/blob/master/Northwind_large.sqlite.zip?raw=true
 unzip Northwind_large.sqlite.zip
 rm Northwind_large.sqlite.zip
 mv Northwind_large.sqlite /tmp/northwind.sqlite
+rm -rf Northwind* || echo ""
+rm -rf __MACOSX

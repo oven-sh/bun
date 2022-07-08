ae8c80ba899b535533df17c9a48f5cb128909aec
Victory Osikwemhe <zombieleetnca@gmail.com>
2022-07-08 02:15:16 +0100

    extend bash completion to support options with specific requirements

--- completions/bun.bash
+++ completions/bun.bash
@@ -1,42 +1,24 @@
 #/usr/bin/env bash
 
-LONG_OPTIONS=(
-    "--use"
-    "--bunfile"
-    "--server-bunfile"
-    "--cwd"
-    "--config"
-    "--disable-react-fast-refresh"
-    "--disable-hmr"
-    "--extension-order"
-    "--jsx-factory"
-    "--jsx-fragment"
-    "--extension-order"
-    "--jsx-factory"
-    "--jsx-fragment"
-    "--jsx-import-source"
-    "--jsx-production"
-    "--jsx-runtime"
-    "--main-fields"
-    "--no-summary"
-    "--version"
-    "--platform"
-    "--public-dir"
-    "--tsconfig-override"
-    "--define"
-    "--external"
-    "--help"
-    "--inject"
-    "--loader"
-    "--origin"
-    "--port"
-    "--silent"
-    "--dump-environment-variables"
-    "--dump-limits"
-    "--disable-bun.js"
-);
+declare -A GLOBAL_OPTIONS;
+declare -A PACKAGE_OPTIONS;
 
-SHORT_OPTIONS=( "-c"  "-v" "-d" "-e" "-h" "-i" "-l" "-u" "-p" );
+
+SUBCOMMANDS="dev bun create run install add remove upgrade completions discord help";
+
+GLOBAL_OPTIONS[LONG_OPTIONS]="--use --cwd --bunfile --server-bunfile --config --disable-react-fast-refresh --disable-hmr --extension-order --jsx-factory --jsx-fragment --extension-order --jsx-factory --jsx-fragment --jsx-import-source --jsx-production --jsx-runtime --main-fields --no-summary --version --platform --public-dir --tsconfig-override --define --external --help --inject --loader --origin --port --dump-environment-variables --dump-limits --disable-bun-js";
+GLOBAL_OPTIONS[SHORT_OPTIONS]="-c -v -d -e -h -i -l -u -p";
+
+
+PACKAGE_OPTIONS[ADD_OPTIONS_LONG]="--development --optional";
+PACKAGE_OPTIONS[ADD_OPTIONS_SHORT]="-d";
+
+PACKAGE_OPTIONS[REMOVE_OPTIONS_LONG]="";
+PACKAGE_OPTIONS[REMOVE_OPTIONS_SHORT]="";
+
+PACKAGE_OPTIONS[SHARED_OPTIONS_LONG]="--config --yarn --production --no-save --dry-run --lockfile --force --cache-dir --no-cache --silent --verbose --global --cwd --backend --link-native-bins --help";
+PACKAGE_OPTIONS[SHARED_OPTIONS_SHORT]="-c -y -p -f -g";
+
 
 file_arguments() {
 
@@ -44,40 +26,134 @@ file_arguments() {
     local extension="${2}";
 
     [[ -z "${cur_word}" ]] && {
-        COMPREPLY=( $(compgen -G "*.${extension}" -- "${cur_word}"));
-        return 0;
+        COMPREPLY=( $(compgen -G "*.${extension}" -- "${cur_word}") );
     }
 
-    COMPREPLY=( $(compgen -fG "${cur_word}*.${extension}" -- "${cur_word}"));
+    COMPREPLY=( $(compgen -fG "${cur_word}*.${extension}" -- "${cur_word}") );
 }
 
-_bun_completions() {
+long_short_completion() {
 
-    local cur_word="${COMP_WORDS[${COMP_CWORD}]}"
-    local prev="${COMP_WORDS[$(( COMP_CWORD - 1 ))]}"
+    local cur_word="${1}";
+    local wordlist="${2}";
+    local short_options="${3}"
 
-    [[ "${cur_word}" =~ ^- ]] && {
-        COMPREPLY=( $(compgen -W "${SHORT_OPTIONS[*]} ${LONG_OPTIONS[*]}" -- "${cur_word}"));
+    [[ -z "${cur_word}" || "${cur_word}" =~ ^- ]] && {
+        COMPREPLY=( $(compgen -W "${wordlist}" -- "${cur_word}"));
         return 0;
     }
 
     [[ "${cur_word}" =~ ^-[A-Za_z]+ ]] && {
-        COMPREPLY=( $(compgen -W "${SHORT_OPTIONS[*]}" -- "${cur_word}"));
+        COMPREPLY=( $(compgen -W "${short_options}" -- "${cur_word}"));
         return 0;
     }
+}
+
+read_scripts_in_package_json() {
+
+    local package_json;
+    local line=0;
+    local working_dir="${PWD}";
+
+    for ((; line < ${#COMP_WORDS[@]}; line+=1)); do
+        [[ "${COMP_WORDS[${line}]}" == "--cwd" ]] && working_dir="${COMP_WORDS[$((line + 1))]}";
+    done
+
+    [[ -f "${working_dir}/package.json" ]] && package_json=$(<${working_dir}/package.json);
+
+    [[ "${package_json}" =~ "\"scripts\""[[:space:]]*":"[[:space:]]*\{(.*)\} ]] && {
+
+        local package_json_compreply;
+        local matched="${BASH_REMATCH[@]:1}";
+        local scripts="${matched%%\}*}";
+
+        shopt -s extglob;
+        scripts="${scripts//@(\"|\')/}";
+        shopt -u extglob;
+
+        readarray -td, scripts <<<"${scripts}";
+
+        for completion in "${scripts[@]}"; do
+            package_json_compreply+=( "${completion%:*}" );
+        done
+
+        COMPREPLY+=( $(compgen -W "${package_json_compreply[*]}" -- "${cur_word}") );
+    }
+}
+
+_bun_completions() {
+
+    local cur_word="${COMP_WORDS[${COMP_CWORD}]}";
+    local prev="${COMP_WORDS[$(( COMP_CWORD - 1 ))]}";
 
     case "${prev}" in
-        -c|--config)        file_arguments "${cur_word}" "toml";;
-        --bunfile)          file_arguments "${cur_word}" "bun";;
-        --server-bunfile)   file_arguments "${cur_word}" "server.bun";;
-        --cwd|--public-dir) COMPREPLY=( $(compgen -d -- "${cur_word}" ) );;
-        --jsx-runtime)      COMPREPLY=( $(compgen -W "automatic classic" -- "${cur_word}") );;
-        --platform)         COMPREPLY=( $(compgen -W "browser node" -- "${cur_word}") );;
+        help|--help|-h|-v|--version) return -1;;
+        -c|--config)      file_arguments "${cur_word}" "toml"       && return 0;;
+        --bunfile)        file_arguments "${cur_word}" "bun"        && return 0;;
+        --server-bunfile) file_arguments "${cur_word}" "server.bun" && return 0;;
+        --backend)
+            COMPREPLY=( $(compgen -W "clonefile copyfile hardlink clonefile_each_dir" -- "${cur_file}") );
+            return 0;;
+        --cwd|--public-dir)
+            COMPREPLY=( $(compgen -d -- "${cur_word}" ));
+            return 0;;
+        --jsx-runtime)
+            COMPREPLY=( $(compgen -W "automatic classic" -- "${cur_word}") );
+            return 0;;
+        --platform)
+            COMPREPLY=( $(compgen -W "browser node" -- "${cur_word}") );
+            return 0;;
         -l|--loader)
             [[ "${cur_word:$(( ${#cur_word} - 1 )):1}" == ":" ]] && {
-                COMPREPLY=( $(compgen -W "jsx js json tsx ts css") );
+                COMPREPLY=( $(compgen -W "jsx js json tsx ts css" -- "${cur_word}") );
             }
-            ;;
+            return 0;;
+    esac
+
+    case "${COMP_WORDS[1]}" in
+        help|--help|-h|-v|--version) return -1;;
+        add)
+            long_short_completion \
+                "${cur_word}" \
+                "${PACKAGE_OPTIONS[ADD_OPTIONS_LONG]} ${PACKAGE_OPTIONS[ADD_OPTIONS_SHORT]} ${PACKAGE_OPTIONS[SHARED_OPTIONS_LONG]} ${PACKAGE_OPTIONS[SHARED_OPTIONS_SHORT]}" \
+                "${PACKAGE_OPTIONS[ADD_OPTIONS_SHORT]} ${PACKAGE_OPTIONS[SHARED_OPTIONS_SHORT]}"
+            return 0;;
+        remove|install)
+            long_short_completion \
+                "${cur_word}" \
+                "${PACKAGE_OPTIONS[REMOVE_OPTIONS_LONG]} ${PACKAGE_OPTIONS[REMOVE_OPTIONS_SHORT]} ${PACKAGE_OPTIONS[SHARED_OPTIONS_LONG]} ${PACKAGE_OPTIONS[SHARED_OPTIONS_SHORT]}" \
+                "${PACKAGE_OPTIONS[REMOVE_OPTIONS_SHORT]} ${PACKAGE_OPTIONS[SHARED_OPTIONS_SHORT]}";
+            return 0;;
+        create)
+            COMPREPLY=( $(compgen -W "--force --no-install --no-git --verbose --no-package-json --open next react" -- "${cur_word}") );
+            return 0;;
+        upgrade)
+            COMPREPLY=( $(compgen -W "--version --cwd --help -v -h") );
+            return 0;;
+        run)
+
+            COMPREPLY=( $(compgen -W "--version --cwd --help --silent -v -h" -- "${cur_word}" ));
+            read_scripts_in_package_json;
+            return 0;;
+
+        *)
+            long_short_completion \
+                "${cur_word}" \
+                "${GLOBAL_OPTIONS[*]}" \
+                "${GLOBAL_OPTIONS[SHORT_OPTIONS]}"
+
+            read_scripts_in_package_json;
+
+            local regexp_subcommand="^[dbcriauh]";
+
+            [[ "${prev}" =~ ${regexp_subcommand} ]] && {
+                COMPREPLY+=( $(compgen -W "${SUBCOMMANDS}" -- "${cur_word}") );
+            }
+            [[ -z "${cur_word}" ]] && {
+                declare -A comp_reply_associative="( $(echo ${COMPREPLY[@]} | sed 's/[^ ]*/[&]=&/g') )";
+                [[ -z "${comp_reply_associative[${prev}]}" ]] && unset COMPREPLY;
+            }
+            return 0;;
     esac
 
 }

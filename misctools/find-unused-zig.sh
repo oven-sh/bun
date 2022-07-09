9849dd019e50a328bbafb3679222757f23f39ba2
lucasew <lucas59356@gmail.com>
2022-07-09 13:23:20 -0300

    update bash references to work in non-fhs compliant distros
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- misctools/find-unused-zig.sh
+++ misctools/find-unused-zig.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 rg "@import\(\"(.*\.zig)\"\)" src -r "\$1" --only-matching  -I  | xargs basename | sort | uniq > /tmp/imported-names.txt
 find src -iname "*.zig" | xargs basename | sort | uniq > /tmp/all-names.txt

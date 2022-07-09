5d7aabb38aa8821200ab5ff584282d968f9c7958
FinnRG <finngaertner@protonmail.com>
2022-07-09 20:19:35 +0200

    fix: Append n when printing a BigInt

--- src/bun.js/bindings/exports.zig
+++ src/bun.js/bindings/exports.zig
@@ -1614,7 +1614,7 @@ pub const ZigConsoleClient = struct {
                 },
                 .BigInt => {
                     var out_str = value.getZigString(this.globalThis).slice();
-                    writer.print(comptime Output.prettyFmt("<r><yellow>{s}<r>", enable_ansi_colors), .{out_str});
+                    writer.print(comptime Output.prettyFmt("<r><yellow>{s}n<r>", enable_ansi_colors), .{out_str});
                 },
                 .Double => {
                     writer.print(comptime Output.prettyFmt("<r><yellow>{d}<r>", enable_ansi_colors), .{value.asNumber()});

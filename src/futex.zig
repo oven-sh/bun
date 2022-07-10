2db039a06250edd672957b2bc6fa114745d92a10
thislooksfun <tlf@thislooks.fun>
2022-07-09 22:52:00 -0500

    style: remove some trailing whitespace

--- src/futex.zig
+++ src/futex.zig
@@ -1,7 +1,7 @@
 //! Futex is a mechanism used to block (`wait`) and unblock (`wake`) threads using a 32bit memory address as hints.
 //! Blocking a thread is acknowledged only if the 32bit memory address is equal to a given value.
 //! This check helps avoid block/unblock deadlocks which occur if a `wake()` happens before a `wait()`.
-//! Using Futex, other Thread synchronization primitives can be built which efficiently wait for cross-thread events or signals.  
+//! Using Futex, other Thread synchronization primitives can be built which efficiently wait for cross-thread events or signals.
 
 // This is copy-pasted from Zig's source code to fix an issue with linking on macOS Catalina and earlier.
 
@@ -22,7 +22,7 @@ const spinLoopHint = std.atomic.spinLoopHint;
 /// - The value at `ptr` is no longer equal to `expect`.
 /// - The caller is unblocked by a matching `wake()`.
 /// - The caller is unblocked spuriously by an arbitrary internal signal.
-/// 
+///
 /// If `timeout` is provided, and the caller is blocked for longer than `timeout` nanoseconds`, `error.TimedOut` is returned.
 ///
 /// The checking of `ptr` and `expect`, along with blocking the caller, is done atomically

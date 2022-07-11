441e51106fe7d05fea070b8b25969f11881a7a00
Jesse Lucas <jessejlucas@hotmail.com>
2022-07-11 21:19:33 +0800

    hono example with typescript

---
+++ examples/hono/src/index.ts
@@ -0,0 +1,14 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+const port = process.env.PORT || 3000;
+const home = app.get("/", (c) => {
+  return c.json({ message: "Hello World!" });
+});
+
+console.log(`Running at http://localhost:${port}`);
+
+export default {
+  port: 3000,
+  fetch: home.fetch,
+};

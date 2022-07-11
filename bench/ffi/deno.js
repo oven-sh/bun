5575259156711dfe11394c9dd756ae647a47885a
evanwashere <github@evan.lol>
2022-07-11 16:01:07 -0400

    cleanup benchmarks

---
+++ bench/ffi/deno.js
@@ -0,0 +1,26 @@
+import { run, bench, group } from '../node_modules/mitata/src/cli.mjs';
+
+const extension = 'darwin' !== Deno.build.os ? 'so' : 'dylib';
+const path = new URL('src/target/release/libffi_napi_bench.' + extension, import.meta.url).pathname;
+
+const {
+  symbols: {
+    ffi_noop,
+    ffi_hash,
+    ffi_string,
+  },
+} = Deno.dlopen(path, {
+  ffi_noop: { parameters: [], result: 'void' },
+  ffi_string: { parameters: [], result: 'pointer' },
+  ffi_hash: { parameters: ['pointer', 'usize'], result: 'u32' },
+});
+
+const bytes = new Uint8Array(64);
+
+group('deno:ffi', () => {
+  bench('noop', () => ffi_noop());
+  bench('hash', () => ffi_hash(bytes, bytes.byteLength));
+  bench('c string', () => new Deno.UnsafePointerView(ffi_string()).getCString());
+});
+
+await run();
---
+++ bench/sqlite/bun.js
@@ -0,0 +1,27 @@
+import { run, bench } from 'mitata';
+import { Database } from 'bun:sqlite';
+
+const db = Database.open('./src/northwind.sqlite');
+
+{
+  const sql = db.prepare(`SELECT * FROM "Order"`);
+  bench('SELECT * FROM "Order"', () => {
+    sql.all();
+  });
+}
+
+{
+  const sql = db.prepare(`SELECT * FROM "Product"`);
+  bench('SELECT * FROM "Product"', () => {
+    sql.all();
+  });
+}
+
+{
+  const sql = db.prepare(`SELECT * FROM "OrderDetail"`);
+  bench('SELECT * FROM "OrderDetail"', () => {
+    sql.all();
+  });
+}
+
+await run();

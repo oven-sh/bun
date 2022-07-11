7904f49b5dc48dc057729eb158067baf54f94cfb
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2022-07-11 03:41:03 -0700

    Fixes https://github.com/oven-sh/bun/issues/229

--- test/apps/bun-run-check.sh
+++ test/apps/bun-run-check.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# TODO: move this test to bun once we have a child_process equivalent.
+
 set -euo pipefail
 
 (killall -9 "$(basename "$BUN_BIN")" || echo "") >/dev/null 2>&1
@@ -10,9 +12,13 @@ fn() { :; }
 # The important bit: export the function
 export -f fn
 
-DIR=$(mktemp -d -t bun-run-check)
+rm -rf /tmp/bun-run-check
+mkdir -p /tmp/bun-run-check
+DIR=/tmp/bun-run-check
 
 cp ./bun-run-check-package.json "$DIR/package.json"
+cp ./bun-run-check-nameless-package.json "$DIR/package.json"
+
 cd "$DIR"
 
 $BUN_BIN run bash -- -c ""
@@ -25,8 +31,43 @@ fi
 # https://github.com/oven-sh/bun/issues/53
 rm -f "$DIR/bun-run-out.expected.txt" "$DIR/bun-run-out.txt" >/dev/null 2>&1
 
-$BUN_BIN run --silent argv -- foo bar baz > "$DIR/bun-run-out.txt"
-npm run --silent argv -- foo bar baz > "$DIR/bun-run-out.expected.txt"
+$BUN_BIN run --silent argv -- foo bar baz >"$DIR/bun-run-out.txt"
+npm run --silent argv -- foo bar baz >"$DIR/bun-run-out.expected.txt"
+
+cmp -s "$DIR/bun-run-out.expected.txt" "$DIR/bun-run-out.txt"
+if (($?)); then
+    echo "argv failed"
+    exit 1
+fi
+
+$BUN_BIN run --silent this-should-work
+
+if (($?)); then
+    echo "this-should work failed"
+    exit 1
+fi
+
+# Run it a second time with our other script which has no name
+
+rm -rf /tmp/bun-run-check
+mkdir -p /tmp/bun-run-check
+DIR=/tmp/bun-run-check
+
+cd "../"
+cd "$DIR"
+
+$BUN_BIN run bash -- -c ""
+
+if (($?)); then
+    echo "Bash exported functions are broken"
+    exit 1
+fi
+
+# https://github.com/oven-sh/bun/issues/53
+rm -f "$DIR/bun-run-out.expected.txt" "$DIR/bun-run-out.txt" >/dev/null 2>&1
+
+$BUN_BIN run --silent argv -- foo bar baz >"$DIR/bun-run-out.txt"
+npm run --silent argv -- foo bar baz >"$DIR/bun-run-out.expected.txt"
 
 cmp -s "$DIR/bun-run-out.expected.txt" "$DIR/bun-run-out.txt"
 if (($?)); then

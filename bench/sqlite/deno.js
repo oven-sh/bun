a9e4ff2029a769737982fcf7e0b7d76d35cf35e4
evan <github@evan.lol>
2022-07-12 04:40:12 -0400

    cleanup benchmarks folder (#587)
    * cleanup benchmarks
    
    * run prettier

---
+++ bench/sqlite/deno.js
@@ -0,0 +1,27 @@
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
+import { run, bench } from "../node_modules/mitata/src/cli.mjs";
+
+const db = new DB("./src/northwind.sqlite");
+
+{
+  const sql = db.prepareQuery(`SELECT * FROM "Order"`);
+  bench('SELECT * FROM "Order"', () => {
+    sql.allEntries();
+  });
+}
+
+{
+  const sql = db.prepareQuery(`SELECT * FROM "Product"`);
+  bench('SELECT * FROM "Product"', () => {
+    sql.allEntries();
+  });
+}
+
+{
+  const sql = db.prepareQuery(`SELECT * FROM "OrderDetail"`);
+  bench('SELECT * FROM "OrderDetail"', () => {
+    sql.allEntries();
+  });
+}
+
+await run();

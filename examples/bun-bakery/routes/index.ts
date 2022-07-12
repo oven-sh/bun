27375bd4f306e160f280f72821f7265f8d3fb9c4
Dennis Dudek <admin@kapsonfire.de>
2022-07-12 23:48:47 +0200

    updated to use latest version

--- examples/bun-bakery/routes/index.ts
+++ examples/bun-bakery/routes/index.ts
@@ -1,9 +1,5 @@
 import {Context} from "@kapsonfire/bun-bakery"
 
 export async function GET(ctx: Context) {
-    ctx.sendResponse(new Response('<img src="/assets/bunbakery.png"><h1>Hello World!</h1>', {
-        headers: {
-            'content-type': 'text/html'
-        }
-    }));
+    ctx.sendHTML('<img src="/assets/bunbakery.png"><h1>Hello World!</h1>');
 }

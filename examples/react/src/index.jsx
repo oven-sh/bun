3014a3c9dbce376c0d1946077674dabf3c082f9b
jonathan-rubin <jonathan.rubin@mailchimp.com>
2022-07-12 10:08:56 -0400

    Updated react example template

--- examples/react/src/index.jsx
+++ examples/react/src/index.jsx
@@ -1,11 +1,11 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );

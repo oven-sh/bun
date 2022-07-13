f8312e4653c491f3c4244de7d77f4de44b29945b
evanwashere <github@evan.lol>
2022-07-13 04:50:25 -0400

    cleanup

--- src/bun.js/http.exports.js
+++ src/bun.js/http.exports.js
@@ -1,5 +1,5 @@
-import { EventEmitter } from 'node:events';
-import { Readable, Writable } from 'node:stream';
+import { EventEmitter } from "node:events";
+import { Readable, Writable } from "node:stream";
 
 export function createServer(options, callback) {
   return new Server(options, callback);
@@ -12,22 +12,22 @@ export class Server extends EventEmitter {
   constructor(options, callback) {
     super();
 
-    if (typeof options === 'function') {
+    if (typeof options === "function") {
       callback = options;
       options = {};
-    } else if (options == null || typeof options === 'object') {
+    } else if (options == null || typeof options === "object") {
       options = { ...options };
     } else {
-      throw new Error('bun-http-polyfill: invalid arguments');
+      throw new Error("bun-http-polyfill: invalid arguments");
     }
 
     this.#options = options;
-    if (callback) this.on('request', callback);
+    if (callback) this.on("request", callback);
   }
 
   close() {
     if (this.#server) {
-      this.emit('close');
+      this.emit("close");
       this.#server.stop();
       this.#server = undefined;
     }
@@ -49,30 +49,23 @@ export class Server extends EventEmitter {
             const http_req = new req_class(req);
             const http_res = new res_class({ reply, req: http_req });
 
-            http_req.once('error', (err) => reject(err));
-            http_res.once('error', (err) => reject(err));
+            http_req.once("error", (err) => reject(err));
+            http_res.once("error", (err) => reject(err));
 
-            server.emit('request', http_req, http_res);
+            server.emit("request", http_req, http_res);
           });
         },
       });
 
       if (listening_cb) listening_cb();
     } catch (err) {
-      this.emit('error', new Error(`bun-http-polyfill: Bun.serve failed: ${err.message}`));
+      this.emit(
+        "error",
+        new Error(`bun-http-polyfill: Bun.serve failed: ${err.message}`)
+      );
     }
   }
 }
-
-async function req_read_body(req, size) {
-  // TODO: streaming
-  try {
-    req._body = Buffer.from(await req.arrayBuffer());
-    req.push(req._body.subarray(req._body_offset, req._body_offset += size));
-  } catch (err) {
-    req.destroy(err);
-  }
-};
 
 export class IncomingMessage extends Readable {
   constructor(req) {
@@ -102,44 +95,58 @@ export class IncomingMessage extends Readable {
     this._socket = undefined;
     this.rawHeaders = rawHeaders;
     this.url = url.pathname + url.search;
-    this._no_body = 'GET' === method || 'HEAD' === method || 'TRACE' === method || 'CONNECT' === method || 'OPTIONS' === method;
+    this._no_body =
+      "GET" === method ||
+      "HEAD" === method ||
+      "TRACE" === method ||
+      "CONNECT" === method ||
+      "OPTIONS" === method;
+  }
+
+  _construct(callback) {
+    // TODO: streaming
+    (async () => {
+      if (!this._no_body)
+        try {
+          this._body = Buffer.from(await this._req.arrayBuffer());
+
+          callback();
+        } catch (err) {
+          callback(err);
+        }
+    })();
   }
 
   _read(size) {
     if (this._no_body) {
       this.push(null);
       this.complete = true;
-    }
-
-    else {
-      if (!this._body) req_read_body(this, size);
-
-      else {
-        if (this._body_offset >= this._body.length) {
-          this.push(null);
-          this.complete = true;
-        } else {
-          this.push(this._body.subarray(this._body_offset, this._body_offset += size));
-        }
+    } else {
+      if (this._body_offset >= this._body.length) {
+        this.push(null);
+        this.complete = true;
+      } else {
+        this.push(
+          this._body.subarray(this._body_offset, (this._body_offset += size))
+        );
       }
     }
   }
 
-
   get aborted() {
-    throw new Error('not implemented');
+    throw new Error("not implemented");
   }
 
   get connection() {
-    throw new Error('not implemented');
+    throw new Error("not implemented");
   }
 
   get statusCode() {
-    throw new Error('not implemented');
+    throw new Error("not implemented");
   }
 
   get statusMessage() {
-    throw new Error('not implemented');
+    throw new Error("not implemented");
   }
 
   get httpVersion() {
@@ -166,20 +173,20 @@ export class IncomingMessage extends Readable {
     if (this._socket) return this._socket;
 
     this._socket = new EventEmitter();
-    this.on('end', () => duplex.emit('end'));
-    this.on('close', () => duplex.emit('close'));
+    this.on("end", () => duplex.emit("end"));
+    this.on("close", () => duplex.emit("close"));
 
     return this._socket;
   }
 
   setTimeout(msecs, callback) {
-    throw new Error('not implemented');
+    throw new Error("not implemented");
   }
 }
 
 export class ServerResponse extends Writable {
   constructor({ req, reply }) {
-    const headers = new Headers;
+    const headers = new Headers();
     const sink = new Bun.ArrayBufferSink();
     sink.start({ stream: false, asUint8Array: true });
 
@@ -215,48 +222,49 @@ export class ServerResponse extends Writable {
     callback();
     this.headersSent = true;
 
-    if (this.sendDate && !this._headers.has('date')) {
-      this._headers.set('date', new Date().toUTCString());
+    if (this.sendDate && !this._headers.has("date")) {
+      this._headers.set("date", new Date().toUTCString());
     }
 
-    this._reply(new Response(this._sink.end(), {
-      headers: this._headers,
-      status: this.statusCode,
-      statusText: this.statusMessage ?? STATUS_CODES[this.statusCode],
-    }));
+    this._reply(
+      new Response(this._sink.end(), {
+        headers: this._headers,
+        status: this.statusCode,
+        statusText: this.statusMessage ?? STATUS_CODES[this.statusCode],
+      })
+    );
   }
 
-
   get socket() {
-    throw new Error('not implemented');
+    throw new Error("not implemented");
   }
 
   get connection() {
-    throw new Error('not implemented');
+    throw new Error("not implemented");
   }
 
   writeProcessing() {
-    throw new Error('not implemented');
+    throw new Error("not implemented");
   }
 
   addTrailers(headers) {
-    throw new Error('not implemented');
+    throw new Error("not implemented");
   }
 
   assignSocket(socket) {
-    throw new Error('not implemented');
+    throw new Error("not implemented");
   }
 
   detachSocket(socket) {
-    throw new Error('not implemented');
+    throw new Error("not implemented");
   }
 
   writeContinue(callback) {
-    throw new Error('not implemented');
+    throw new Error("not implemented");
   }
 
   setTimeout(msecs, callback) {
-    throw new Error('not implemented');
+    throw new Error("not implemented");
   }
 
   get shouldKeepAlive() {
@@ -346,84 +354,106 @@ export class ServerResponse extends Writable {
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 export const METHODS = [
-  'ACL',         'BIND',       'CHECKOUT',
-  'CONNECT',     'COPY',       'DELETE',
-  'GET',         'HEAD',       'LINK',
-  'LOCK',        'M-SEARCH',   'MERGE',
-  'MKACTIVITY',  'MKCALENDAR', 'MKCOL',
-  'MOVE',        'NOTIFY',     'OPTIONS',
-  'PATCH',       'POST',       'PROPFIND',
-  'PROPPATCH',   'PURGE',      'PUT',
-  'REBIND',      'REPORT',     'SEARCH',
-  'SOURCE',      'SUBSCRIBE',  'TRACE',
-  'UNBIND',      'UNLINK',     'UNLOCK',
-  'UNSUBSCRIBE'
+  "ACL",
+  "BIND",
+  "CHECKOUT",
+  "CONNECT",
+  "COPY",
+  "DELETE",
+  "GET",
+  "HEAD",
+  "LINK",
+  "LOCK",
+  "M-SEARCH",
+  "MERGE",
+  "MKACTIVITY",
+  "MKCALENDAR",
+  "MKCOL",
+  "MOVE",
+  "NOTIFY",
+  "OPTIONS",
+  "PATCH",
+  "POST",
+  "PROPFIND",
+  "PROPPATCH",
+  "PURGE",
+  "PUT",
+  "REBIND",
+  "REPORT",
+  "SEARCH",
+  "SOURCE",
+  "SUBSCRIBE",
+  "TRACE",
+  "UNBIND",
+  "UNLINK",
+  "UNLOCK",
+  "UNSUBSCRIBE",
 ];
 
 export const STATUS_CODES = {
-  '100': 'Continue',
-  '101': 'Switching Protocols',
-  '102': 'Processing',
-  '103': 'Early Hints',
-  '200': 'OK',
-  '201': 'Created',
-  '202': 'Accepted',
-  '203': 'Non-Authoritative Information',
-  '204': 'No Content',
-  '205': 'Reset Content',
-  '206': 'Partial Content',
-  '207': 'Multi-Status',
-  '208': 'Already Reported',
-  '226': 'IM Used',
-  '300': 'Multiple Choices',
-  '301': 'Moved Permanently',
-  '302': 'Found',
-  '303': 'See Other',
-  '304': 'Not Modified',
-  '305': 'Use Proxy',
-  '307': 'Temporary Redirect',
-  '308': 'Permanent Redirect',
-  '400': 'Bad Request',
-  '401': 'Unauthorized',
-  '402': 'Payment Required',
-  '403': 'Forbidden',
-  '404': 'Not Found',
-  '405': 'Method Not Allowed',
-  '406': 'Not Acceptable',
-  '407': 'Proxy Authentication Required',
-  '408': 'Request Timeout',
-  '409': 'Conflict',
-  '410': 'Gone',
-  '411': 'Length Required',
-  '412': 'Precondition Failed',
-  '413': 'Payload Too Large',
-  '414': 'URI Too Long',
-  '415': 'Unsupported Media Type',
-  '416': 'Range Not Satisfiable',
-  '417': 'Expectation Failed',
-  '418': "I'm a Teapot",
-  '421': 'Misdirected Request',
-  '422': 'Unprocessable Entity',
-  '423': 'Locked',
-  '424': 'Failed Dependency',
-  '425': 'Too Early',
-  '426': 'Upgrade Required',
-  '428': 'Precondition Required',
-  '429': 'Too Many Requests',
-  '431': 'Request Header Fields Too Large',
-  '451': 'Unavailable For Legal Reasons',
-  '500': 'Internal Server Error',
-  '501': 'Not Implemented',
-  '502': 'Bad Gateway',
-  '503': 'Service Unavailable',
-  '504': 'Gateway Timeout',
-  '505': 'HTTP Version Not Supported',
-  '506': 'Variant Also Negotiates',
-  '507': 'Insufficient Storage',
-  '508': 'Loop Detected',
-  '509': 'Bandwidth Limit Exceeded',
-  '510': 'Not Extended',
-  '511': 'Network Authentication Required'
+  100: "Continue",
+  101: "Switching Protocols",
+  102: "Processing",
+  103: "Early Hints",
+  200: "OK",
+  201: "Created",
+  202: "Accepted",
+  203: "Non-Authoritative Information",
+  204: "No Content",
+  205: "Reset Content",
+  206: "Partial Content",
+  207: "Multi-Status",
+  208: "Already Reported",
+  226: "IM Used",
+  300: "Multiple Choices",
+  301: "Moved Permanently",
+  302: "Found",
+  303: "See Other",
+  304: "Not Modified",
+  305: "Use Proxy",
+  307: "Temporary Redirect",
+  308: "Permanent Redirect",
+  400: "Bad Request",
+  401: "Unauthorized",
+  402: "Payment Required",
+  403: "Forbidden",
+  404: "Not Found",
+  405: "Method Not Allowed",
+  406: "Not Acceptable",
+  407: "Proxy Authentication Required",
+  408: "Request Timeout",
+  409: "Conflict",
+  410: "Gone",
+  411: "Length Required",
+  412: "Precondition Failed",
+  413: "Payload Too Large",
+  414: "URI Too Long",
+  415: "Unsupported Media Type",
+  416: "Range Not Satisfiable",
+  417: "Expectation Failed",
+  418: "I'm a Teapot",
+  421: "Misdirected Request",
+  422: "Unprocessable Entity",
+  423: "Locked",
+  424: "Failed Dependency",
+  425: "Too Early",
+  426: "Upgrade Required",
+  428: "Precondition Required",
+  429: "Too Many Requests",
+  431: "Request Header Fields Too Large",
+  451: "Unavailable For Legal Reasons",
+  500: "Internal Server Error",
+  501: "Not Implemented",
+  502: "Bad Gateway",
+  503: "Service Unavailable",
+  504: "Gateway Timeout",
+  505: "HTTP Version Not Supported",
+  506: "Variant Also Negotiates",
+  507: "Insufficient Storage",
+  508: "Loop Detected",
+  509: "Bandwidth Limit Exceeded",
+  510: "Not Extended",
+  511: "Network Authentication Required",
 };
 
 function _normalizeArgs(args) {
@@ -437,7 +467,7 @@ function _normalizeArgs(args) {
 
   const arg0 = args[0];
   let options = {};
-  if (typeof arg0 === 'object' && arg0 !== null) {
+  if (typeof arg0 === "object" && arg0 !== null) {
     // (options[...][, cb])
     options = arg0;
     // } else if (isPipeName(arg0)) {
@@ -446,16 +476,14 @@ function _normalizeArgs(args) {
   } else {
     // ([port][, host][...][, cb])
     options.port = arg0;
-    if (args.length > 1 && typeof args[1] === 'string') {
+    if (args.length > 1 && typeof args[1] === "string") {
       options.host = args[1];
     }
   }
 
   const cb = args[args.length - 1];
-  if (typeof cb !== 'function')
-    arr = [options, null];
-  else
-    arr = [options, cb];
+  if (typeof cb !== "function") arr = [options, null];
+  else arr = [options, cb];
 
   // arr[normalizedArgsSymbol] = true;
   return arr;
@@ -464,17 +492,16 @@ function _normalizeArgs(args) {
 function _writeHead(statusCode, reason, obj, response) {
   statusCode |= 0;
   if (statusCode < 100 || statusCode > 999) {
-    throw new Error('status code must be between 100 and 999');
+    throw new Error("status code must be between 100 and 999");
   }
 
-
-  if (typeof reason === 'string') {
+  if (typeof reason === "string") {
     // writeHead(statusCode, reasonPhrase[, headers])
     response.statusMessage = reason;
   } else {
     // writeHead(statusCode[, headers])
     if (!response.statusMessage)
-      response.statusMessage = STATUS_CODES[statusCode] || 'unknown';
+      response.statusMessage = STATUS_CODES[statusCode] || "unknown";
     obj = reason;
   }
   response.statusCode = statusCode;
@@ -484,7 +511,7 @@ function _writeHead(statusCode, reason, obj, response) {
     let k;
     if (Array.isArray(obj)) {
       if (obj.length % 2 !== 0) {
-        throw new Error('raw headers must have an even number of elements');
+        throw new Error("raw headers must have an even number of elements");
       }
 
       for (let n = 0; n < obj.length; n += 2) {
@@ -510,4 +537,4 @@ export default {
   createServer,
   ServerResponse,
   IncomingMessage,
-}
+};

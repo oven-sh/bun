0bf1d8c0ac864ccc9ca27af9d1a99a15ff46aa4e
evanwashere <github@evan.lol>
2022-07-18 19:26:30 -0400

    only show benchmark results

--- bench/log/deno.mjs
+++ bench/log/deno.mjs
@@ -1,4 +1,5 @@
 import { bench, run } from "../node_modules/mitata/src/cli.mjs";
 
 bench("console.log", () => console.log("hello"));
+bench("console.log({ hello: 'object' })", () => console.log({ hello: "object" }));
 await run();

dfcdca40d79a6021ee7e9d7ab7accc520190991d
Yusuke Wada <yusuke@kamawada.com>
2022-07-18 21:48:02 +0900

    add favicon

--- examples/hono/src/index.ts
+++ examples/hono/src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono";
+import { serveStatic } from 'hono/serve-static.bun';
 
 const port = parseInt(process.env.PORT) || 3000;
 
 const app = new Hono();
+
+app.use('/favicon.ico', serveStatic({ path: './public/favicon.ico' }));
 
 app.get("/", (c) => {
   return c.json({ message: "Hello World!" });

80f797b2e561904cdfd6c71c2843c5f29ea2364e
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2022-07-22 01:37:42 -0700

    Mark as executable

---
+++ test/bun.js/snapshots.debug.js
@@ -0,0 +1,10 @@
+
+// test/bun.js/module-require.snapshot.js
+import {
+__require as require
+} from "bun:wrap";
+console.log(import.meta.require("/Users/jarred/Code/bun/node_modules/react/index.js"));
+
+// test/bun.js/require-referenceerror.snapshot.js
+
+// test/bun.js/tiny-typed-emitter.snapshot.js

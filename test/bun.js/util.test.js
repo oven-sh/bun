e6686628b4c1c2b03216ec9d0429d221875b7972
Omer Shahar <38870042+Omer-Shahar@users.noreply.github.com>
2022-07-24 03:03:43 +0300

    Change `toBe` to `toEqual` (deep equality)

--- test/bun.js/util.test.js
+++ test/bun.js/util.test.js
@@ -7,7 +7,7 @@ describe("util", () => {
     // test/bun.js/text-encoder.test.js covers test cases for TextEncoder
     // here we test only if we use the same via util.TextEncoder
     it("is same as global TextEncoder", () => {
-      expect(TextEncoder === globalThis.TextEncoder).toBe(true);
+      expect(TextEncoder).toEqual(globalThis.TextEncoder);
     });
   });
 });

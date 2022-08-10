d07cbee9107e00bf8f3a6c27c4f956a3a8b03771
DanielVain <80036840+DanielVain@users.noreply.github.com>
2022-08-10 22:10:33 +0300

    Update deno.mjs

--- bench/log/deno.mjs
+++ bench/log/deno.mjs
@@ -1,4 +1,4 @@
-import { bench, run } from "../node_modules/mitata/src/cli.mjs.js";
+import { bench, run } from "../node_modules/mitata/src/cli.mjs";
 
 bench("console.log", () => console.log("hello"));
 bench("console.log({ hello: 'object' })", () => console.log({ hello: "object" }));

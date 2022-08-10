3776500acc908f5f4f5e738252355207b01792f0
Jordan Lees <jordanlees@mailbox.org>
2022-08-10 13:40:53 -0400

    #941 fix

--- src/node-fallbacks/assert.js
+++ src/node-fallbacks/assert.js
@@ -1,1 +1,3 @@
-export * from 'assert';
+// #941 - esbuild does not play nicely with the ESM-style import-export 
+//  with assert's function-type export. CommonJS-style fixes it.
+module.exports = require('assert');

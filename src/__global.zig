1bc5036d2e94f25cdf1044f583c48091adbf72ad
Zilin Zhu <zhuzilinallen@gmail.com>
2022-08-11 14:11:24 +0800

    fix MD5 length and compile error (#1050)

--- src/__global.zig
+++ src/__global.zig
@@ -14,9 +14,9 @@ else
 pub const package_json_version_with_sha = if (Environment.git_sha.len == 0)
     package_json_version
 else if (Environment.isDebug)
-    std.fmt.comptimePrint("0.1.{d}_debug ({s})", .{ build_id, Environment.git_sha[0..8] })
+    std.fmt.comptimePrint("0.1.{d}_debug ({s})", .{ build_id, Environment.git_sha[0..@minimum(Environment.git_sha.len, 8)] })
 else
-    std.fmt.comptimePrint("0.1.{d} ({s})", .{ build_id, Environment.git_sha[0..8] });
+    std.fmt.comptimePrint("0.1.{d} ({s})", .{ build_id, Environment.git_sha[0..@minimum(Environment.git_sha.len, 8)] });
 
 pub const os_name = if (Environment.isWindows)
     "win32"

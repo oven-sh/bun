80a8c721a24f4c64b4175fadb9326b422efec461
lau1944 <a954723219@gmail.com>
2022-08-15 12:45:35 +0800

    feat: rm express dir

--- examples/express/middleware.ts
+++
@@ -1,9 +0,0 @@
-
-export const defaultMiddleware = (req, res, next) => {
-    console.log('You are in the middleware');
-    next()
-}
-
-export const testMiddleware = (req, res, next) => {
-    res.status(200).send('You are in test middleware');
-}

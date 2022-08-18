036e5800454c1d8a85bba5da29682fd53dcc3f17
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2022-08-18 01:13:11 -0700

    Fixes https://github.com/oven-sh/bun/issues/1069

---
+++ test/bun.js/fileUrl.test.js
@@ -0,0 +1,20 @@
+import { expect, it, describe } from "bun:test";
+import { pathToFileURL, fileURLToPath } from "bun";
+describe("pathToFileURL", () => {
+  it("should convert a path to a file url", () => {
+    expect(pathToFileURL("/path/to/file.js").href).toBe(
+      "file:///path/to/file.js"
+    );
+  });
+});
+
+describe("fileURLToPath", () => {
+  it("should convert a file url to a path", () => {
+    expect(fileURLToPath("file:///path/to/file.js")).toBe("/path/to/file.js");
+  });
+  it("should convert a URL to a path", () => {
+    expect(fileURLToPath(new URL("file:///path/to/file.js"))).toBe(
+      "/path/to/file.js"
+    );
+  });
+});

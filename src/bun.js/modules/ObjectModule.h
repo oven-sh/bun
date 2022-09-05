d2397b60e79e4386c6a7b7a9783a6f8e379a5ae0
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2022-09-05 05:16:21 -0700

    Move `generateObjectModuleSourceCode` to separate file to fix build error

--- src/bun.js/modules/ObjectModule.h
+++ src/bun.js/modules/ObjectModule.h
@@ -4,30 +4,8 @@
 #include "JavaScriptCore/JSGlobalObject.h"
 
 namespace Zig {
-inline JSC::SyntheticSourceProvider::SyntheticSourceGenerator
+JSC::SyntheticSourceProvider::SyntheticSourceGenerator
 generateObjectModuleSourceCode(JSC::JSGlobalObject *globalObject,
-                               JSC::JSObject *object) {
-  JSC::VM &vm = globalObject->vm();
-
-  return [object](JSC::JSGlobalObject *lexicalGlobalObject,
-                  JSC::Identifier moduleKey,
-                  Vector<JSC::Identifier, 4> &exportNames,
-                  JSC::MarkedArgumentBuffer &exportValues) -> void {
-    JSC::VM &vm = lexicalGlobalObject->vm();
-    GlobalObject *globalObject =
-        reinterpret_cast<GlobalObject *>(lexicalGlobalObject);
-    JSC::EnsureStillAliveScope stillAlive(object);
-
-    PropertyNameArray properties(vm, PropertyNameMode::Strings,
-                                 PrivateSymbolMode::Exclude);
-    object->getPropertyNames(globalObject, properties,
-                             DontEnumPropertiesMode::Exclude);
-
-    for (auto &entry : properties) {
-      exportNames.append(entry);
-      exportValues.append(object->get(globalObject, entry));
-    }
-  };
-}
+                               JSC::JSObject *object);
 
 } // namespace Zig

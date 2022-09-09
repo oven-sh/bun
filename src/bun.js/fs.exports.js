781940d6c4ab9691882e13cbe60453a51aad99d6
Youness Masaoudi <marvelbark2@gmail.com>
2022-09-10 01:26:32 +0200

    Implement createReadStream using bun apis

--- src/bun.js/fs.exports.js
+++ src/bun.js/fs.exports.js
@@ -152,6 +152,11 @@ function promisify(fsFunction) {
   };
 }
 
+
+
+
+
+
 export var accessSync = fs.accessSync.bind(fs);
 export var appendFileSync = fs.appendFileSync.bind(fs);
 export var closeSync = fs.closeSync.bind(fs);
@@ -187,7 +192,334 @@ export var unlinkSync = fs.unlinkSync.bind(fs);
 export var utimesSync = fs.utimesSync.bind(fs);
 export var lutimesSync = fs.lutimesSync.bind(fs);
 
-export var createReadStream = fs.createReadStream.bind(fs);
+
+var __getOwnPropNames = Object.getOwnPropertyNames;
+
+var __commonJS = (cb, mod) =>
+  function __require2() {
+    return (
+      mod ||
+      (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod),
+      mod.exports
+    );
+  };
+
+
+var reader_stream_utils = __commonJS({
+  "utils for ReaderStream"(exports, module) {
+    "use strict";
+
+    let Readable = null;
+    if (!Readable) {
+      Readable = import.meta.require("node:stream").Readable;
+      console.log("Readable Initialized")
+    }
+    module.exports = {
+      Readable,
+      finished: import.meta.require("node:stream"),
+      validateInteger(value,
+        name,
+        min = Number.MAX_SAFE_INTEGER,
+        max = Number.MAX_SAFE_INTEGER) {
+        // Todo
+      },
+
+      toPathIfFileURL(path) {
+        if (path != null && path.href && path.origin) {
+          return path;
+        } else {
+          return Bun.pathToFileURL(path);
+        }
+      },
+      PromisePrototypeThen(self, thenFn, catchFn) {
+        return self.then(thenFn, catchFn);
+      },
+
+      FileHandleOperations(handle) {
+        return {
+          open: (path, flags, mode, cb) => {
+            throw new Error('open()');
+          },
+          close: (fd, cb) => {
+            handle[kUnref]();
+            PromisePrototypeThen(handle.close(),
+              () => cb(), cb);
+          },
+          read: (fd, buf, offset, length, pos, cb) => {
+            PromisePrototypeThen(handle.read(buf, offset, length, pos),
+              (r) => cb(null, r.bytesRead, r.buffer),
+              (err) => cb(err, 0, buf));
+          },
+          write: (fd, buf, offset, length, pos, cb) => {
+            PromisePrototypeThen(handle.write(buf, offset, length, pos),
+              (r) => cb(null, r.bytesWritten, r.buffer),
+              (err) => cb(err, 0, buf));
+          },
+          writev: (fd, buffers, pos, cb) => {
+            PromisePrototypeThen(handle.writev(buffers, pos),
+              (r) => cb(null, r.bytesWritten, r.buffers),
+              (err) => cb(err, 0, buffers));
+          }
+        };
+      },
+      close(stream, err, cb) {
+        if (!stream.fd) {
+          cb(err);
+        } else {
+          stream["kFs"].close(stream.fd, (er) => {
+            cb(er || err);
+          });
+          stream.fd = null;
+        }
+      },
+      errorOrDestroy(stream, err, sync = null) {
+        const r = stream._readableState;
+        const w = stream._writableState;
+        if ((w && w.destroyed) || (r && r.destroyed)) {
+          return this;
+        }
+        if ((r && r.autoDestroy) || (w && w.autoDestroy))
+          stream.destroy(err);
+        else if (err) {
+          err.stack;
+          if (w && !w.errored) {
+            w.errored = err;
+          }
+          if (r && !r.errored) {
+            r.errored = err;
+          }
+          if (sync) {
+            process.nextTick(emitErrorNT, stream, err);
+          } else {
+            this.emitErrorNT(stream, err);
+          }
+        }
+      },
+
+      emitErrorNT(self, err) {
+        const r = self._readableState;
+        const w = self._writableState;
+        if ((w && w.errorEmitted) || (r && r.errorEmitted)) {
+          return;
+        }
+        if (w) {
+          w.errorEmitted = true;
+        }
+        if (r) {
+          r.errorEmitted = true;
+        }
+        self.emit("error", err);
+      },
+
+      importFd(stream, options) {
+        if (typeof options.fd === 'number') {
+          // When fd is a raw descriptor, we must keep our fingers crossed
+          // that the descriptor won't get closed, or worse, replaced with
+          // another one
+          // https://github.com/nodejs/node/issues/35862
+          stream[kFs] = options.fs || fs;
+          return options.fd;
+        } else if (typeof options.fd === 'object' &&
+          options.fd instanceof FileHandle) {
+          // When fd is a FileHandle we can listen for 'close' events
+          if (options.fs) {
+            // FileHandle is not supported with custom fs operations
+            throw new Error('FileHandle with fs');
+          }
+          stream[kHandle] = options.fd;
+          stream[kFs] = FileHandleOperations(stream[kHandle]);
+          stream[kHandle][kRef]();
+          // options.fd.on('close', FunctionPrototypeBind(stream.close, stream));
+          return options.fd.fd;
+        }
+
+        throw new Error("Error to find filesystem api");
+      }
+    };
+  },
+});
+
+
+
+
+
+var utils = reader_stream_utils();
+
+export class ReadStream extends utils.Readable {
+
+  static kIoDone = "end";
+  static kIsPerformingIO = Symbol('kIsPerformingIO');
+  static kFs = "kFs";
+
+
+  pending = {
+    __proto__: null,
+    get() { return this.fd === null; },
+    configurable: true
+  };
+
+  autoClose = {
+    __proto__: null,
+    get() {
+      return this._readableState.autoDestroy;
+    },
+    set(val) {
+      this._readableState.autoDestroy = val;
+    }
+  }
+
+  constructor(path, options) {
+    if (Object.keys(options).length > 0)
+      super(options);
+    else
+      super();
+
+    if (options.highWaterMark === undefined)
+      options.highWaterMark = 64 * 1024;
+
+    if (options.autoDestroy === undefined) {
+      options.autoDestroy = false;
+    }
+    if (options.autoDestroy === undefined) {
+      options.autoDestroy = false;
+    }
+    if (options.fd == null) {
+      this.fd = null;
+      if (options.fs == null || options.fs == undefined)
+        this[ReadStream.kFs] = import.meta.require("node:fs");
+
+      else
+        this[ReadStream.kFs] = options.fs;
+
+
+      // Path will be ignored when fd is specified, so it can be falsy
+      this.path = utils.toPathIfFileURL(path);
+      this.flags = options.flags === undefined ? 'r' : options.flags;
+      this.mode = options.mode === undefined ? 0o666 : options.mode;
+
+      // validatePath(this.path);
+    } else {
+      this.fd = utils.getValidatedFd(utils.importFd(this, options));
+    }
+
+    this.start = options.start;
+    this.end = options.end;
+    this.pos = undefined;
+    this.bytesRead = 0;
+
+    //this.closed = false;
+    if (this.start !== undefined) {
+      utils.validateInteger(this.start, 'start', 0);
+      this.pos = this.start;
+    }
+
+    if (this.end === undefined) {
+      this.end = Infinity;
+    } else if (this.end !== Infinity) {
+      utils.validateInteger(this.end, 'end', 0);
+
+      if (this.start !== undefined && this.start > this.end) {
+        throw new Error(
+          'start' +
+          `<= "end" (here: ${this.end})` +
+          this.start
+        );
+      }
+    }
+  }
+
+  _open(callback) {
+    this[ReadStream.kFs].open(this.path, this.flags, this.mode, (er, fd) => {
+
+      if (er) {
+        callback(er);
+      } else {
+        this.fd = fd;
+        callback();
+        this.emit('open', this.fd);
+        this.emit('ready');
+      }
+    });
+  }
+
+  _construct(callback) {
+    const stream = this;
+
+    if (typeof stream.fd === 'number') {
+      callback();
+      return;
+    }
+
+    this._open(callback);
+  }
+
+
+  _read(n) {
+    n = this.pos !== undefined ?
+      Math.min(this.end - this.pos + 1,
+        n) :
+      Math.min(this.end - this.bytesRead + 1, n);
+
+    if (n <= 0) {
+      this.push(null);
+      return;
+    }
+    let buf = Buffer.allocUnsafeSlow(n);
+    this[ReadStream.kIsPerformingIO] = true;
+    this[ReadStream.kFs]
+      .read(this.fd, buf, 0, n, this.pos, (er, bytesRead) => {
+        this[ReadStream.kIsPerformingIO] = false;
+
+        // Tell ._destroy() that it's safe to close the fd now.
+        if (this.destroyed) {
+          this.emit("#kDestroy", er);
+          return;
+        }
+
+        if (er) {
+          utils.errorOrDestroy(this, er);
+        } else if (bytesRead > 0) {
+          if (this.pos !== undefined) {
+            this.pos += bytesRead;
+          }
+
+          this.bytesRead += bytesRead;
+
+          if (bytesRead !== buf.length) {
+            // Slow path. Shrink to fit.
+            // Copy instead of slice so that we don't retain
+            // large backing buffer for small reads.
+            const dst = Buffer.allocUnsafeSlow(bytesRead);
+            buf.copy(dst, 0, 0, bytesRead);
+            buf = dst;
+          }
+
+
+          this.push(buf);
+        } else {
+          this.push(null);
+        }
+      });
+  }
+
+  _destroy(err, cb) {
+    if (this[ReadStream.kIsPerformingIO]) {
+      this.once(ReadStream.kIoDone, (er) => close(this, err || er, cb));
+    } else {
+      close(this, err, cb);
+    }
+  }
+
+  close(cb) {
+    //TODO:
+    if (typeof cb === 'function') utils.finished(this, cb);
+    this.destroy();
+  };
+}
+
+export var createReadStream = function (path, options = {}) {
+  return new ReadStream(path, options);
+}
 export var createWriteStream = fs.createWriteStream.bind(fs);
 
 export var promises = {

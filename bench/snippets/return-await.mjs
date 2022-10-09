cc6077fbf508806fa078aff53e4b2f1e58f1d38b
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2022-10-08 18:19:23 -0700

    Snippet for return await

---
+++ bench/snippets/return-await.mjs
@@ -0,0 +1,19 @@
+import { bench, run } from "../node_modules/mitata/src/cli.mjs";
+
+bench("return await Promise.resolve(1)", async function () {
+  return await Promise.resolve(1);
+});
+
+bench("return Promise.resolve(1) (async fn)", async function () {
+  return Promise.resolve(1);
+});
+
+bench("return await 1", async function () {
+  return await 1;
+});
+
+bench("return 1 (async fn)", async function () {
+  return 1;
+});
+
+await run();

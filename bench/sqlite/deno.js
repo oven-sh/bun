4b5c9acc72908ba22e3f708c4df59c6316e4846b
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2022-10-09 00:49:08 -0700

    Update bench

--- bench/sqlite/deno.js
+++ bench/sqlite/deno.js
@@ -1,26 +1,26 @@
-import { DB } from "https://deno.land/x/sqlite/mod.ts";
+import { Database } from "https://deno.land/x/sqlite3@0.6.1/mod.ts";
 import { run, bench } from "../node_modules/mitata/src/cli.mjs";
 
-const db = new DB("./src/northwind.sqlite");
+const db = new Database("./src/northwind.sqlite");
 
 {
-  const sql = db.prepareQuery(`SELECT * FROM "Order"`);
+  const sql = db.prepare(`SELECT * FROM "Order"`);
   bench('SELECT * FROM "Order"', () => {
-    sql.allEntries();
+    sql.all();
   });
 }
 
 {
-  const sql = db.prepareQuery(`SELECT * FROM "Product"`);
+  const sql = db.prepare(`SELECT * FROM "Product"`);
   bench('SELECT * FROM "Product"', () => {
-    sql.allEntries();
+    sql.all();
   });
 }
 
 {
-  const sql = db.prepareQuery(`SELECT * FROM "OrderDetail"`);
+  const sql = db.prepare(`SELECT * FROM "OrderDetail"`);
   bench('SELECT * FROM "OrderDetail"', () => {
-    sql.allEntries();
+    sql.all();
   });
 }
 

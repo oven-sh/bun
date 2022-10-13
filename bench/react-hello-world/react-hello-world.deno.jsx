af033c02c5fbaade201abfe332f376879d9e6885
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2022-10-13 15:32:25 -0700

    Fix bench

--- bench/react-hello-world/react-hello-world.deno.jsx
+++ bench/react-hello-world/react-hello-world.deno.jsx
@@ -1,11 +1,11 @@
 import { renderToReadableStream } from "https://esm.run/react-dom/server";
-import { serve } from "https://deno.land/std@0.146.0/http/server.ts";
 import * as React from "https://esm.run/react";
 
 const App = () => (
   <html>
     <body>
       <h1>Hello World</h1>
+      <p>This is an example.</p>
     </body>
   </html>
 );
@@ -17,7 +17,7 @@ const headers = {
   },
 };
 
-await serve(
+Deno.serve(
   async (req) => {
     return new Response(await renderToReadableStream(<App />), headers);
   },

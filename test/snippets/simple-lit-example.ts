c555a05a43cf35f3c2d76f4ddd43ee3673f3afa6
Dylan Conway <dylan.conway567@gmail.com>
2022-11-01 16:13:59 -0700

    await on DOMContentLoaded for elements instead of setup function

--- test/snippets/simple-lit-example.ts
+++ test/snippets/simple-lit-example.ts
@@ -1,6 +1,23 @@
 import { LitElement, html, css } from "lit";
 import { customElement, property, eventOptions } from "lit/decorators.js";
 
+var loadedResolve;
+var loadedPromise = new Promise((resolve) => {
+  loadedResolve = resolve;
+});
+
+if (document?.readyState === "loading") {
+  document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+      loadedResolve();
+    },
+    { once: true }
+  );
+} else {
+  loadedResolve();
+}
+
 @customElement("my-element")
 export class MyElement extends LitElement {
   static styles = css`
@@ -30,13 +47,16 @@ export class MyElement extends LitElement {
   }
 }
 
-export function setup() {
+function setup() {
   let element = document.createElement("my-element");
   element.id = "my-element-id";
   document.body.appendChild(element);
 }
 
-export function test() {
+export async function test() {
+  setup();
+  await loadedPromise;
+
   let element = document.getElementById("my-element-id");
   let shadowRoot = element.shadowRoot;
   let planet = shadowRoot.getElementById("planet-id");

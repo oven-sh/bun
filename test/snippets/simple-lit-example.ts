8309d795e277de6c6ddf6ae740112e1dc6d03ced
Dylan Conway <dylan.conway567@gmail.com>
2022-11-01 14:19:18 -0700

    optional setup function for loading elements, simulate clicks in lit test

--- test/snippets/simple-lit-example.ts
+++ test/snippets/simple-lit-example.ts
@@ -1,24 +1,55 @@
-import { LitElement, css, html } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { LitElement, html, css } from "lit";
+import { customElement, property, eventOptions } from "lit/decorators.js";
+
+@customElement("my-element")
+export class MyElement extends LitElement {
+  static styles = css`
+    :host {
+      display: inline-block;
+      padding: 10px;
+      background: lightgray;
+    }
+    .planet {
+      color: var(--planet-color, blue);
+    }
+  `;
+
+  @property() planet = "Earth";
+
+  render() {
+    return html`
+      <span @click=${this.togglePlanet} class="planet" id="planet-id"
+        >${this.planet}</span
+      >
+    `;
+  }
+
+  @eventOptions({ once: true })
+  togglePlanet() {
+    this.planet = this.planet === "Earth" ? "Mars" : "Earth";
+  }
+}
+
+export function setup() {
+  let element = document.createElement("my-element");
+  element.id = "my-element-id";
+  document.body.appendChild(element);
+}
 
 export function test() {
-  @customElement("simple-greeting")
-  class SimpleGreeting extends LitElement {
-    // Define scoped styles right with your component, in plain CSS
-    static styles = css`
-      :host {
-        color: blue;
-      }
-    `;
-
-    // Declare reactive properties
-    @property()
-    name?: string = "World";
-
-    // Render the UI as a function of component state
-    render() {
-      return html`<p>Hello, ${this.name}!</p>`;
-    }
+  let element = document.getElementById("my-element-id");
+  let shadowRoot = element.shadowRoot;
+  let planet = shadowRoot.getElementById("planet-id");
+  if (element.__planet !== "Earth") {
+    throw new Error("Unexpected planet name: " + element.__planet);
+  }
+  planet.click();
+  if (element.__planet !== "Mars") {
+    throw new Error("Unexpected planet name: " + element.__planet);
+  }
+  planet.click();
+  if (element.__planet !== "Mars") {
+    throw new Error("Unexpected planet name: " + element.__planet);
   }
 
   return testDone(import.meta.url);

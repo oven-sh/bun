136798d9ab9a50255bb7373ad0cf0d2775efd0da
Dylan Conway <dylan.conway567@gmail.com>
2022-11-09 14:05:42 -0800

    captureStackTrace types

--- packages/bun-types/bun.d.ts
+++ packages/bun-types/bun.d.ts
@@ -2729,6 +2729,97 @@ declare module "bun" {
   export const revision: string;
 }
 
+// Declare "static" methods in Error
+interface ErrorConstructor {
+  /** Create .stack property on a target object */
+  captureStackTrace(targetObject: object, constructorOpt?: Function): void;
+
+  /**
+   * Optional override for formatting stack traces
+   *
+   * @see https://v8.dev/docs/stack-trace-api#customizing-stack-traces
+   */
+  prepareStackTrace?:
+    | ((err: Error, stackTraces: CallSite[]) => any)
+    | undefined;
+
+  stackTraceLimit: number;
+}
+
+interface CallSite {
+  /**
+   * Value of "this"
+   */
+  getThis(): unknown;
+
+  /**
+   * Type of "this" as a string.
+   * This is the name of the function stored in the constructor field of
+   * "this", if available.  Otherwise the object's [[Class]] internal
+   * property.
+   */
+  getTypeName(): string | null;
+
+  /**
+   * Current function
+   */
+  getFunction(): Function | undefined;
+
+  /**
+   * Name of the current function, typically its name property.
+   * If a name property is not available an attempt will be made to try
+   * to infer a name from the function's context.
+   */
+  getFunctionName(): string | null;
+
+  /**
+   * Name of the property [of "this" or one of its prototypes] that holds
+   * the current function
+   */
+  getMethodName(): string | null;
+
+  /**
+   * Name of the script [if this function was defined in a script]
+   */
+  getFileName(): string | null;
+
+  /**
+   * Current line number [if this function was defined in a script]
+   */
+  getLineNumber(): number | null;
+
+  /**
+   * Current column number [if this function was defined in a script]
+   */
+  getColumnNumber(): number | null;
+
+  /**
+   * A call site object representing the location where eval was called
+   * [if this function was created using a call to eval]
+   */
+  getEvalOrigin(): string | undefined;
+
+  /**
+   * Is this a toplevel invocation, that is, is "this" the global object?
+   */
+  isToplevel(): boolean;
+
+  /**
+   * Does this call take place in code defined by a call to eval?
+   */
+  isEval(): boolean;
+
+  /**
+   * Is this call in native V8 code?
+   */
+  isNative(): boolean;
+
+  /**
+   * Is this a constructor call?
+   */
+  isConstructor(): boolean;
+}
+
 type TypedArray =
   | Uint8Array
   | Int8Array

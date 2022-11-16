5de98f23bb4e69e54ea173039ce9ba5a0839e968
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2022-11-15 23:23:36 -0800

    Fix another assertion failure

--- src/bun.js/api/server.zig
+++ src/bun.js/api/server.zig
@@ -2182,7 +2182,7 @@ fn NewRequestContext(comptime ssl_enabled: bool, comptime debug_mode: bool, comp
         }
 
         pub fn onStartStreamingRequestBody(this: *RequestContext) JSC.WebCore.DrainResult {
-            if (this.aborted) {
+            if (this.aborted or this.finalized) {
                 return JSC.WebCore.DrainResult{
                     .aborted = void{},
                 };

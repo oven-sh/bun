27c013cdfef58693c62d194d08f1e465505ff398
Dylan Conway <dylan.conway567@gmail.com>
2022-11-18 17:23:13 -0800

    fix call signature

--- src/bun.js/scripts/generate-classes.ts
+++ src/bun.js/scripts/generate-classes.ts
@@ -852,7 +852,7 @@ JSC_DEFINE_CUSTOM_SETTER(${symbolName(
       typeName,
       proto[name].setter || proto[name].accessor.setter,
     )}(thisObject->wrapped(),${
-          !!proto[name].this ? " thisValue: " : ""
+          !!proto[name].this ? " thisValue, " : ""
         } lexicalGlobalObject, encodedValue);
 
     RELEASE_AND_RETURN(throwScope, result);

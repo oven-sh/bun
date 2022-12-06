d3e3998ab6f752c8eadc57db74232473e2d73b11
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2022-12-06 13:23:13 -0800

    Update glibc-versions-hack.cpp

--- src/bun.js/bindings/glibc-versions-hack.cpp
+++ src/bun.js/bindings/glibc-versions-hack.cpp
@@ -16,6 +16,69 @@
 #endif
 #endif
 
+// this ones from musl libc
+long __syscall_ret(unsigned long r)
+{
+    if (r > -4096UL) {
+        errno = -r;
+        return -1;
+    }
+    return r;
+}
+
+extern "C" int fcntl(int fd, int cmd, ...)
+{
+    unsigned long arg;
+    va_list ap;
+    va_start(ap, cmd);
+    arg = va_arg(ap, unsigned long);
+    va_end(ap);
+    if (cmd == F_SETFL)
+        arg |= O_LARGEFILE;
+    if (cmd == F_SETLKW)
+        return syscall_cp(SYS_fcntl, fd, cmd, (void*)arg);
+    if (cmd == F_GETOWN) {
+        struct f_owner_ex ex;
+        int ret = __syscall(SYS_fcntl, fd, F_GETOWN_EX, &ex);
+        if (ret == -EINVAL)
+            return __syscall(SYS_fcntl, fd, cmd, (void*)arg);
+        if (ret)
+            return __syscall_ret(ret);
+        return ex.type == F_OWNER_PGRP ? -ex.pid : ex.pid;
+    }
+    if (cmd == F_DUPFD_CLOEXEC) {
+        int ret = __syscall(SYS_fcntl, fd, F_DUPFD_CLOEXEC, arg);
+        if (ret != -EINVAL) {
+            if (ret >= 0)
+                __syscall(SYS_fcntl, ret, F_SETFD, FD_CLOEXEC);
+            return __syscall_ret(ret);
+        }
+        ret = __syscall(SYS_fcntl, fd, F_DUPFD_CLOEXEC, 0);
+        if (ret != -EINVAL) {
+            if (ret >= 0)
+                __syscall(SYS_close, ret);
+            return __syscall_ret(-EINVAL);
+        }
+        ret = __syscall(SYS_fcntl, fd, F_DUPFD, arg);
+        if (ret >= 0)
+            __syscall(SYS_fcntl, ret, F_SETFD, FD_CLOEXEC);
+        return __syscall_ret(ret);
+    }
+    switch (cmd) {
+    case F_SETLK:
+    case F_GETLK:
+    case F_GETOWN_EX:
+    case F_SETOWN_EX:
+        return syscall(SYS_fcntl, fd, cmd, (void*)arg);
+    default:
+        return syscall(SYS_fcntl, fd, cmd, arg);
+    }
+}
+
+extern "C" double __real_pow(double x, double y);
+extern "C" double __real_exp(double x);
+extern "C" double __real_log(double x);
+
 extern "C" int __wrap_fcntl(int fd, int cmd, ...)
 {
     // fcntl has 2 or 3 args, and I don't know whether it's safe to
@@ -23,7 +86,7 @@ extern "C" int __wrap_fcntl(int fd, int cmd, ...)
     // as a pointer using va_arg, although the man page says it can be an int!
     va_list va;
     va_start(va, cmd);
-    return __real_fcntl(fd, cmd, va_arg(va, void*));
+    return fcntl(fd, cmd, va_arg(va, void*));
     va_end(va);
 }
 
@@ -31,7 +94,7 @@ extern "C" int __wrap_fcntl64(int fd, int cmd, ...)
 {
     va_list va;
     va_start(va, cmd);
-    return __real_fcntl(fd, cmd, va_arg(va, void*));
+    return fcntl(fd, cmd, va_arg(va, void*));
     va_end(va);
 }
 

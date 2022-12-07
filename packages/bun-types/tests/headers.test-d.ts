5133e5dc23614d24610de44b651842cc3a8cfeaa
Colin McDonnell <colinmcd@alum.mit.edu>
2022-12-06 16:33:21 -0800

    Update type tests

--- packages/bun-types/tests/headers.test-d.ts
+++ packages/bun-types/tests/headers.test-d.ts
@@ -1,1 +1,6 @@
-headers.test - d.ts;
+const headers = new Headers();
+headers.append("Set-Cookie", "a=1");
+headers.append("Set-Cookie", "b=1; Secure");
+
+console.log(headers.getAll("Set-Cookie")); // ["a=1", "b=1; Secure"]
+console.log(headers.toJSON()); // { "set-cookie": "a=1, b=1; Secure" }

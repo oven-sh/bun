88538b7c2c68fe506d0a20a8f5b78e47c6c60299
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2022-12-09 16:41:38 -0800

    Deprecate very incomplete Solid.js JSX transform
    We don't have time to do a good job of this and Bun.plugin makes it possible to use Solid with Bun

--- test/bun.js/solid-dom-fixtures/simpleElements/code.js
+++
@@ -1,9 +0,0 @@
-const template = (
-  <div id="main">
-    <style>{"div { color: red; }"}</style>
-    <h1>Welcome</h1>
-    <label for={"entry"}>Edit:</label>
-    <input id="entry" type="text" />
-    {/* Comment Node */}
-  </div>
-);

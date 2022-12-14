bf604871c5001a58ca507153ad2343a403984f39
Derrick Farris <mr.dcfarris@gmail.com>
2022-12-13 22:44:15 -0600

    fix(stream): put Readable._readableState.constructed default in spec (true, not false)

--- src/bun.js/bindings/JSReadableState.cpp
+++ src/bun.js/bindings/JSReadableState.cpp
@@ -108,6 +108,8 @@ void JSReadableState::finishCreation(JSC::VM& vm, JSC::JSGlobalObject* globalObj
             m_encoding.set(vm, this, JSC::jsNull());
         }
     }
+
+    setBool(JSReadableState::Mask::constructed, true);
 }
 
 const JSC::ClassInfo JSReadableState::s_info = { "ReadableState"_s, &Base::s_info, nullptr, nullptr, CREATE_METHOD_TABLE(JSReadableState) };
@@ -305,9 +307,12 @@ JSReadableState_NULLABLE_BOOLEAN_GETTER_SETTER(paused)
 
 #undef JSReadableState_JSVALUE_GETTER_SETTER
 
-#define JSReadableState_GETTER_SETTER_HASH_TABLE_VALUE(NAME)                                                                                                                                                                                                          \
-    {                                                                                                                                                                                                                                                                 \
-#NAME ""_s, static_cast < unsigned>(JSC::PropertyAttribute::DontDelete | JSC::PropertyAttribute::CustomAccessor | JSC::PropertyAttribute::DOMAttribute), NoIntrinsic, { HashTableValue::GetterSetterType, jsReadableState_##NAME, setJSReadableState_##NAME } \
+#define JSReadableState_GETTER_SETTER_HASH_TABLE_VALUE(NAME)                                                                                                                  \
+    {                                                                                                                                                                         \
+#NAME ""_s, static_cast < unsigned>(JSC::PropertyAttribute::DontDelete | JSC::PropertyAttribute::CustomAccessor | JSC::PropertyAttribute::DOMAttribute), NoIntrinsic, \
+        {                                                                                                                                                                     \
+            HashTableValue::GetterSetterType, jsReadableState_##NAME, setJSReadableState_##NAME                                                                               \
+        }                                                                                                                                                                     \
     }
 
     /* Hash table for prototype */

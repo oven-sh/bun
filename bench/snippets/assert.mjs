42fc49d413da3cfd87b0e1554e95f86166eb1516
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2022-12-17 12:17:21 -0800

    Add node:assert bench

---
+++ bench/snippets/assert.mjs
@@ -0,0 +1,15 @@
+import { bench, group, run } from "mitata";
+import * as assert from "assert";
+
+bench("deepEqual", () => {
+  assert.deepEqual({ foo: "123", bar: "baz" }, { foo: "123", bar: "baz" });
+});
+
+bench("deepStrictEqual", () => {
+  assert.deepStrictEqual(
+    { foo: "123", beep: "boop" },
+    { foo: "123", beep: "boop" },
+  );
+});
+
+await run();

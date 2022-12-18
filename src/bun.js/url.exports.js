84963c6f57ca72fbc671711649021422208550f1
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2022-12-18 12:06:10 +0530

    add urlToHttpOptions

--- src/bun.js/url.exports.js
+++ src/bun.js/url.exports.js
@@ -426,6 +426,30 @@ function X(s, r) {
   });
 var Y, k;
 S && ((Y = S("pathToFileURL")), (k = S("fileURLToPath")));
+
+// https://github.com/stevenvachon/url-to-options/blob/main/index.js
+function urlToHttpOptions(url) {
+  var options = {
+    protocol: url.protocol,
+    hostname:
+      typeof url.hostname === "string" && url.hostname.startsWith("[")
+        ? url.hostname.slice(1, -1)
+        : url.hostname,
+    hash: url.hash,
+    search: url.search,
+    pathname: url.pathname,
+    path: `${url.pathname || ""}${url.search || ""}`,
+    href: url.href,
+  };
+  if (url.port !== "") {
+    options.port = Number(url.port);
+  }
+  if (url.username || url.password) {
+    options.auth = `${url.username}:${url.password}`;
+  }
+  return options;
+}
+
 var ut = {
   parse: A,
   resolve: W,
@@ -436,6 +460,7 @@ var ut = {
   fileURLToPath: k,
   URL: F,
   URLSearchParams: M,
+  urlToHttpOptions,
 };
 ("use strict");
 export {
@@ -449,4 +474,5 @@ export {
   Y as pathToFileURL,
   W as resolve,
   X as resolveObject,
+  urlToHttpOptions,
 };

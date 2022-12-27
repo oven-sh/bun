40e3e675028de91315aa902c1261e3d3e94de0c2
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2022-12-26 18:37:01 -0800

    Update node-timers.test.ts

--- test/bun.js/node-timers.test.ts
+++ test/bun.js/node-timers.test.ts
@@ -1,10 +1,11 @@
 import { test } from "bun:test";
 import { setTimeout } from "node:timers";
 
-test("unref is possible", () => {
-  const timer = setTimeout(() => {
-    throw new Error("should not be called");
-  }, 1000);
-  timer.unref();
-  clearTimeout(timer);
-});
+// not implemented yet
+// test("unref is possible", () => {
+//   const timer = setTimeout(() => {
+//     throw new Error("should not be called");
+//   }, 1000);
+//   timer.unref();
+//   clearTimeout(timer);
+// });

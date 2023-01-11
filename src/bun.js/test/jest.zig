9f66d2f419066bb99fa4b1f5bfd8cb5d994f953f
Ethan Burrell <ethanburrell@berkeley.edu>
2023-01-11 07:34:41 +0000

    respond to reviews

--- src/bun.js/test/jest.zig
+++ src/bun.js/test/jest.zig
@@ -1549,7 +1549,7 @@ pub const DescribeScope = struct {
         var scope = allocator.create(DescribeScope) catch unreachable;
         scope.* = .{
             .label = (label.toSlice(allocator).cloneIfNeeded(allocator) catch unreachable).slice(),
-            .parent = active,
+            .parent = active orelse this,
             .file_id = this.file_id,
         };
         var new_this = DescribeScope.Class.make(ctx, scope);

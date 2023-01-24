be79f6e893a3a94e13c1fa85d71f900546123d17
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-01-24 04:08:24 -0800

    Add failing test

--- test/bun.js/hot.test.ts
+++ test/bun.js/hot.test.ts
@@ -20,25 +20,63 @@ it("should hot reload when file is overwritten", async () => {
     writeFileSync(root, readFileSync(root, "utf-8"));
   }
 
-  await (async function () {
-    for await (const line of runner.stdout!) {
-      var str = new TextDecoder().decode(line);
+  for await (const line of runner.stdout!) {
+    var str = new TextDecoder().decode(line);
 
-      if (str.includes("[#!root]")) {
-        reloadCounter++;
+    if (str.includes("[#!root]")) {
+      reloadCounter++;
 
-        if (reloadCounter === 3) {
-          runner.unref();
-          runner.kill();
-          return;
-        }
-
-        expect(str).toContain(`[#!root] Reloaded: ${reloadCounter}`);
-
-        await onReload();
+      if (reloadCounter === 3) {
+        runner.unref();
+        runner.kill();
+        break;
       }
+
+      expect(str).toContain(`[#!root] Reloaded: ${reloadCounter}`);
+
+      await onReload();
     }
-  })();
+  }
+
+  expect(reloadCounter).toBe(3);
+});
+
+// This test fails
+it.skip("should hot reload when a file is deleted and rewritten", async () => {
+  const root = import.meta.dir + "/hot-runner.js";
+  const runner = spawn({
+    cmd: [bunExe(), "--hot", "run", root],
+    env: bunEnv,
+    stdout: "pipe",
+    stderr: "inherit",
+    stdin: "ignore",
+  });
+
+  var reloadCounter = 0;
+
+  async function onReload() {
+    const contents = readFileSync(root, "utf-8");
+    unlinkSync(root);
+    writeFileSync(root, contents);
+  }
+
+  for await (const line of runner.stdout!) {
+    var str = new TextDecoder().decode(line);
+
+    if (str.includes("[#!root]")) {
+      reloadCounter++;
+
+      if (reloadCounter === 3) {
+        runner.unref();
+        runner.kill();
+        break;
+      }
+
+      expect(str).toContain(`[#!root] Reloaded: ${reloadCounter}`);
+
+      await onReload();
+    }
+  }
 
   expect(reloadCounter).toBe(3);
 });

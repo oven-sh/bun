ae2c4b4a8f7a5aec55b775358f610ddcb24e8d2b
Sean Scally <scally@users.noreply.github.com>
2023-01-24 12:58:41 -0800

    Add FileSystemRouter + React example

---
+++ examples/react-file-system-router/pages/index.tsx
@@ -0,0 +1,17 @@
+// reachable from http://localhost:3000/
+
+export default () => (
+  <html>
+    <head>
+      <title>index</title>
+    </head>
+    <body>
+      <h1>
+        <a href="/one" >one</a>
+      </h1>
+      <h1>
+        <a href="/two" >two</a>
+      </h1>
+    </body>
+  </html>
+)

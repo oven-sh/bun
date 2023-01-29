f9c44debd750a473077cf01b055acf63e40c2577
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-01-28 21:32:46 -0800

    another micro bench

---
+++ bench/snippets/native-overhead.mjs
@@ -0,0 +1,25 @@
+import { bench, run } from "mitata";
+
+// These are no-op C++ functions that are exported to JS.
+const lazy = globalThis[Symbol.for("Bun.lazy")];
+const noop = lazy("noop");
+const fn = noop.function;
+const regular = noop.functionRegular;
+
+bench("C++ fn regular", () => {
+  regular();
+});
+
+bench("C++ fn", () => {
+  fn();
+});
+
+bench("C++ getter", () => {
+  return noop.getterSetter;
+});
+
+bench("C++ setter", () => {
+  noop.getterSetter = 1;
+});
+
+run();

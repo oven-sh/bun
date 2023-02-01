7a6b9508f671cc8818777881b6a152b28d1f1961
Ethan Burrell <ethanburrell@berkeley.edu>
2023-02-01 07:05:31 +0000

    add another case

--- test/bun.js/bun-test/snapshot-tests/hint.test.ts
+++ test/bun.js/bun-test/snapshot-tests/hint.test.ts
@@ -8,5 +8,6 @@ describe("test hint", () => {
     it("hint should be well formatted", () => {
         expect("do not use a hint").toMatchSnapshot()
         expect("some string").toMatchSnapshot("using a hint")
+        expect("another string").toMatchSnapshot("using a hint")
     })
 })

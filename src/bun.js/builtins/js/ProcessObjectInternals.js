40f5cfc875bc7af662ee68a9fe8aaf70c819dba9
Derrick Farris <mr.dcfarris@gmail.com>
2023-02-10 23:52:39 -0600

    feat(tty): get `setRawMode` and `isRaw` working

--- src/bun.js/builtins/js/ProcessObjectInternals.js
+++ src/bun.js/builtins/js/ProcessObjectInternals.js
@@ -465,12 +465,26 @@ function getStdinStream(fd_, rawRequire, Bun) {
     #onClose;
     #onDrain;
 
+    #setRawMode;
+    #internalIsRaw;
+
     get isTTY() {
       return require("tty").isatty(fd_);
     }
 
     get fd() {
       return fd_;
+    }
+
+    get isRaw() {
+      return (this.#internalIsRaw ??=
+        require("node:tty")[Symbol.for("__BUN_INTERNAL_DO_NOT_USE_ELSE_RISK_TERMINATION__isRaw")])(fd_);
+    }
+
+    #createSetRawMode() {
+      var internalSetRawMode =
+        require("node:tty")[Symbol.for("__BUN_INTERNAL_DO_NOT_USE_ELSE_RISK_TERMINATION__setRawMode")];
+      return mode => internalSetRawMode(fd_, mode);
     }
 
     constructor() {
@@ -513,7 +527,12 @@ function getStdinStream(fd_, rawRequire, Bun) {
       }
     }
 
-    setRawMode(mode) {}
+    setRawMode(mode) {
+      // TODO: Validate mode
+      (this.#setRawMode ??= this.#createSetRawMode())(mode);
+      return this;
+    }
+
     on(name, callback) {
       // Streams don't generally required to present any data when only
       // `readable` events are present, i.e. `readableFlowing === false`

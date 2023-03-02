c2a5eb91c11388bf0cf12e40125c2033cb866b60
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-03-02 15:11:50 -0800

    Ensure prettier knows where to look

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: "avoid",
+  printWidth: 120,
+  trailingComma: "all",
+  useTabs: false,
+  overrides: [
+    {
+      files: "README.md",
+      options: {
+        printWidth: 80,
+      },
+    },
+  ],
+};

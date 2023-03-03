bde98c951ee33202a506c75851b8ee53e83413c0
Zhiyuan Guo <zhiyuan.guo@shopee.com>
2023-03-03 11:50:11 +0800

    fix: rm outdated vscode extension

--- .devcontainer/scripts/zig-env.sh
+++ .devcontainer/scripts/zig-env.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-curl -L https://github.com/zigtools/zls-vscode/releases/download/1.1.6/zls-vscode-1.1.6.vsix >/home/ubuntu/vscode-zig.vsix
 git clone https://github.com/zigtools/zls /home/ubuntu/zls
 cd /home/ubuntu/zls
 git checkout 30869d7d8741656448e46fbf14f14da9ca7e5a21

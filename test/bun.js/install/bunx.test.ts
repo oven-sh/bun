feae9e507e968a607eca69cc58f5b61e8930ef60
Alex Lam S.L <alexlamsl@gmail.com>
2023-03-05 18:54:00 +0200

    consider current working directory when resolving relative paths (#2313)
    * consider current working directory when resolving relative paths
    
    fixes #2298
    
    * comment test
    
    ---------
    
    Co-authored-by: Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>

--- test/bun.js/install/bunx.test.ts
+++ test/bun.js/install/bunx.test.ts
@@ -11,7 +11,7 @@ import { readdirSorted } from "./dummy.registry";
 let x_dir;
 
 beforeEach(async () => {
-  x_dir = realpathSync(await mkdtemp(join(tmpdir(), "bun-install.test")));
+  x_dir = realpathSync(await mkdtemp(join(tmpdir(), "bun-x.test")));
 });
 afterEach(async () => {
   await rm(x_dir, { force: true, recursive: true });
@@ -178,4 +178,32 @@ for (const entry of await decompress(Buffer.from(buffer))) {
   ]);
   expect(await exited).toBe(0);
   expect(await readdirSorted(x_dir)).toEqual([".cache", "test.js"]);
+});
+
+it("should execute from current working directory", async () => {
+  await writeFile(
+    join(x_dir, "test.js"),
+    `
+console.log(
+6
+*
+7
+)`,
+  );
+  const { stdout, stderr, exited } = spawn({
+    cmd: [bunExe(), "--bun", "x", "uglify-js", "test.js", "--compress"],
+    cwd: x_dir,
+    stdout: null,
+    stdin: "pipe",
+    stderr: "pipe",
+    env,
+  });
+  expect(stderr).toBeDefined();
+  const err = await new Response(stderr).text();
+  expect(err).not.toContain("error");
+  expect(stdout).toBeDefined();
+  const out = await new Response(stdout).text();
+  expect(out.split(/\r?\n/)).toEqual(["console.log(42);", ""]);
+  expect(await exited).toBe(0);
+  expect(await readdirSorted(x_dir)).toEqual(["test.js"]);
 });

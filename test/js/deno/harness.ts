eb9e3e9aecc8696e5117eb82eaa4d543f957a134
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-03-14 16:42:16 -0700

    Update harness.ts

--- test/js/deno/harness.ts
+++ test/js/deno/harness.ts
@@ -4,13 +4,39 @@ export * from "./harness/assert.js";
 export * from "./harness/fixture.js";
 
 import { readTextFile } from "./harness/fixture.js";
+import { callerSourceOrigin } from "bun:jsc";
 import { test } from "./harness/test.js";
+import { hideFromStackTrace } from "harness";
+
+const internalSymbol = Symbol("Deno[internal]");
+class BrokenTest extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "BrokenTest";
+  }
+}
+
+hideFromStackTrace(BrokenTest.prototype.constructor);
+
+const handler = {
+  get(target: any, prop: string) {
+    throw new BrokenTest(
+      "Deno[Deno.internal]." +
+        String(prop) +
+        " accessed in " +
+        callerSourceOrigin() +
+        ".\n\nThis test should not be included in the test harness. Please skip or remove it from the test runner.",
+    );
+  },
+};
+
+hideFromStackTrace(handler.get);
 
 export const Deno = {
   test,
   readTextFile,
-  internal: "[internal]",
-  ["[internal]"]: {},
+  internal: internalSymbol,
+  [internalSymbol]: new Proxy({}, handler),
 };
 
 // @ts-expect-error

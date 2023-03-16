27b483385026311f68c2cf028b9a2c56584ffd1e
Dylan Conway <dylan.conway567@gmail.com>
2023-03-15 21:46:31 -0700

    unref poll_ref

--- src/bun.js/api/bun/socket.zig
+++ src/bun.js/api/bun/socket.zig
@@ -1037,6 +1037,7 @@ fn NewSocket(comptime ssl: bool) type {
                 const err_ = err.toErrorInstance(globalObject);
                 promise.rejectOnNextTickAsHandled(globalObject, err_);
                 this.has_pending_activity.store(false, .Release);
+                this.poll_ref.unref(handlers.vm);
             }
         }
 

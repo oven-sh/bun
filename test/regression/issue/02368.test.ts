1f798723c118cced4498385d9fbecfa6f0dccc1a
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-03-18 23:23:32 -0700

    fix incorrect test

--- test/regression/issue/02368.test.ts
+++ test/regression/issue/02368.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "bun:test";
 
-test("can clone a response", () => {
+test("can clone a response", async () => {
   const response = new Response("bun", {
     status: 201,
     headers: {
@@ -8,14 +8,14 @@ test("can clone a response", () => {
     },
   });
   // @ts-ignore
-  const clone = new Response(response);
+  const clone = response.clone();
   expect(clone.status).toBe(201);
   expect(clone.headers.get("content-type")).toBe("text/bun;charset=utf-8");
-  expect(async () => await response.text()).toBe("bun");
-  expect(async () => await clone.text()).toBe("bun");
+  expect(await response.text()).toBe("bun");
+  expect(await clone.text()).toBe("bun");
 });
 
-test("can clone a request", () => {
+test("can clone a request", async () => {
   const request = new Request("http://example.com/", {
     method: "PUT",
     headers: {
@@ -23,10 +23,11 @@ test("can clone a request", () => {
     },
     body: "bun",
   });
+  expect(request.method).toBe("PUT");
   // @ts-ignore
   const clone = new Request(request);
   expect(clone.method).toBe("PUT");
   expect(clone.headers.get("content-type")).toBe("text/bun;charset=utf-8");
-  expect(async () => await request.text()).toBe("bun");
-  expect(async () => await clone.text()).toBe("bun");
+  expect(await request.text()).toBe("bun");
+  expect(await clone.text()).toBe("bun");
 });

c54d10f4fd942c61aa4c1e73e1b8c45fb6b53d29
Dylan Conway <dylan.conway567@gmail.com>
2023-03-21 02:39:11 -0700

    replace with `getDirectIndex`

--- src/bun.js/bindings/bindings.cpp
+++ src/bun.js/bindings/bindings.cpp
@@ -167,15 +167,6 @@ static bool canPerformFastPropertyEnumerationForIterationBun(Structure* s)
     return true;
 }
 
-// second half of JSObject::getOwnPropertySlotByIndex()
-JSValue tryGetOwnPropertySlotByIndex(JSGlobalObject* globalObject, JSObject* obj, unsigned i)
-{
-    PropertySlot slot(obj, PropertySlot::InternalMethodType::Get);
-    if (obj->methodTable()->getOwnPropertySlotByIndex(obj, globalObject, i, slot))
-        return slot.getValue(globalObject, i);
-    return JSValue();
-}
-
 template<bool isStrict>
 bool Bun__deepEquals(JSC__JSGlobalObject* globalObject, JSValue v1, JSValue v2, Vector<std::pair<JSC::JSValue, JSC::JSValue>, 16>& stack, ThrowScope* scope, bool addToStack)
 {
@@ -487,14 +478,9 @@ bool Bun__deepEquals(JSC__JSGlobalObject* globalObject, JSValue v1, JSValue v2, 
         }
 
         for (uint64_t i = 0; i < length; i++) {
-            JSValue left = o1->canGetIndexQuickly(i)
-                ? o1->getIndexQuickly(i)
-                : tryGetOwnPropertySlotByIndex(globalObject, o1, i);
+            JSValue left = o1->getDirectIndex(globalObject, i);
             RETURN_IF_EXCEPTION(*scope, false);
-
-            JSValue right = o2->canGetIndexQuickly(i)
-                ? o2->getIndexQuickly(i)
-                : tryGetOwnPropertySlotByIndex(globalObject, o2, i);
+            JSValue right = o2->getDirectIndex(globalObject, i);
             RETURN_IF_EXCEPTION(*scope, false);
 
             if constexpr (isStrict) {

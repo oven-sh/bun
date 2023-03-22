5915ab19feca54166c11f6f0dcbcbcf481eadea4
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-03-21 21:52:11 -0700

    Don't use function from bundler branch

--- src/js_parser.zig
+++ src/js_parser.zig
@@ -16406,17 +16406,25 @@ fn NewParser_(
                     // Handle exporting this class from a namespace
                     if (was_export_inside_namespace) {
                         stmts.append(
-                            Stmt.assign(
-                                p.newExpr(
-                                    E.Dot{
-                                        .target = p.newExpr(E.Identifier{ .ref = p.enclosing_namespace_arg_ref.? }, stmt.loc),
-                                        .name = p.symbols.items[data.class.class_name.?.ref.?.innerIndex()].original_name,
-                                        .name_loc = data.class.class_name.?.loc,
-                                    },
-                                    stmt.loc,
-                                ),
-                                p.newExpr(E.Identifier{ .ref = data.class.class_name.?.ref.? }, data.class.class_name.?.loc),
-                                p.allocator,
+                            Stmt.alloc(
+                                S.SExpr,
+                                S.SExpr{
+                                    .value = p.newExpr(
+                                        E.Identifier{ .ref = data.class.class_name.?.ref.? },
+                                        data.class.class_name.?.loc,
+                                    ).assign(
+                                        p.newExpr(
+                                            E.Dot{
+                                                .target = p.newExpr(E.Identifier{ .ref = p.enclosing_namespace_arg_ref.? }, stmt.loc),
+                                                .name = p.symbols.items[data.class.class_name.?.ref.?.innerIndex()].original_name,
+                                                .name_loc = data.class.class_name.?.loc,
+                                            },
+                                            stmt.loc,
+                                        ),
+                                        p.allocator,
+                                    ),
+                                },
+                                stmt.loc,
                             ),
                         ) catch unreachable;
                     }

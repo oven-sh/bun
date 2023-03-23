18721ffa722428a3bbeb2979ed25eeda6485ecfd
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-03-22 22:28:35 -0700

    clamp it

--- src/bundler/bundle_v2.zig
+++ src/bundler/bundle_v2.zig
@@ -109,6 +109,8 @@ pub const ThreadPool = struct {
             } else |_| {}
         }
 
+        this.cpu_count = @min(this.cpu_count, this.workers.len - 1);
+
         this.pool = ThreadPoolLib.init(.{
             .max_threads = this.cpu_count,
         });

07ce6de0b753265c586fd94abfcc7a6112ddb07b
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-04-01 17:14:23 -0700

    Implement `node:stream/promise`

---
+++ src/bun.js/node_streams_promises.exports.js
@@ -0,0 +1,9 @@
+var { promises } = import.meta.require("node:stream");
+
+export var { pipeline, finished } = promises;
+
+export default {
+  pipeline,
+  finished,
+  [Symbol.for("CommonJS")]: 0,
+};

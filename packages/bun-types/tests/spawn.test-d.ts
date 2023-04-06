569d4940bb68cb47fa546b5505f5a326073d6c5c
dave caruso <me@paperdave.net>
2023-04-05 22:15:06 -0400

    rebase (#1501)

--- packages/bun-types/tests/spawn.test-d.ts
+++ packages/bun-types/tests/spawn.test-d.ts
@@ -1,3 +1,6 @@
+import { FileSink } from "bun";
+import * as tsd from "tsd";
+
 Bun.spawn(["echo", "hello"]);
 {
   const proc = Bun.spawn(["echo", "hello"], {
@@ -9,6 +12,10 @@ Bun.spawn(["echo", "hello"]);
   });
 
   proc.pid; // process ID of subprocess
+
+  tsd.expectType<ReadableStream<Uint8Array>>(proc.stdout);
+  tsd.expectType<undefined>(proc.stderr);
+  tsd.expectType<undefined>(proc.stdin);
 }
 
 {
@@ -28,17 +35,17 @@ Bun.spawn(["echo", "hello"]);
   });
 
   // enqueue string data
-  proc.stdin!.write("hello");
+  proc.stdin.write("hello");
 
   // enqueue binary data
   const enc = new TextEncoder();
-  proc.stdin!.write(enc.encode(" world!"));
+  proc.stdin.write(enc.encode(" world!"));
 
   // send buffered data
-  proc.stdin!.flush();
+  proc.stdin.flush();
 
   // close the input stream
-  proc.stdin!.end();
+  proc.stdin.end();
 }
 
 {
@@ -66,5 +73,52 @@ Bun.spawn(["echo", "hello"]);
 }
 
 {
+  const proc = Bun.spawn(["echo", "hello"], {
+    stdio: ["pipe", "pipe", "pipe"],
+  });
+  tsd.expectType<FileSink>(proc.stdin);
+  tsd.expectType<ReadableStream<Uint8Array>>(proc.stdout);
+  tsd.expectType<ReadableStream<Uint8Array>>(proc.stderr);
 }
-export {};
+{
+  const proc = Bun.spawn(["echo", "hello"], {
+    stdio: ["inherit", "inherit", "inherit"],
+  });
+  tsd.expectType<undefined>(proc.stdin);
+  tsd.expectType<undefined>(proc.stdout);
+  tsd.expectType<undefined>(proc.stderr);
+}
+{
+  const proc = Bun.spawn(["echo", "hello"], {
+    stdio: ["ignore", "ignore", "ignore"],
+  });
+  tsd.expectType<undefined>(proc.stdin);
+  tsd.expectType<undefined>(proc.stdout);
+  tsd.expectType<undefined>(proc.stderr);
+}
+{
+  const proc = Bun.spawn(["echo", "hello"], {
+    stdio: [null, null, null],
+  });
+  tsd.expectType<undefined>(proc.stdin);
+  tsd.expectType<undefined>(proc.stdout);
+  tsd.expectType<undefined>(proc.stderr);
+}
+{
+  const proc = Bun.spawn(["echo", "hello"], {
+    stdio: [new Request("1"), null, null],
+  });
+  tsd.expectType<number>(proc.stdin);
+}
+{
+  const proc = Bun.spawn(["echo", "hello"], {
+    stdio: [new Response("1"), null, null],
+  });
+  tsd.expectType<number>(proc.stdin);
+}
+{
+  const proc = Bun.spawn(["echo", "hello"], {
+    stdio: [new Uint8Array([]), null, null],
+  });
+  tsd.expectType<number>(proc.stdin);
+}

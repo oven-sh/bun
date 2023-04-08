a35657e572678973b935b2834ad1fdaacd73716e
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-04-08 01:53:20 -0700

    update syntax

--- src/bundler.zig
+++ src/bundler.zig
@@ -1380,7 +1380,7 @@ pub const Bundler = struct {
                 opts.transform_require_to_import = bundler.options.allow_runtime and !bundler.options.platform.isBun();
                 opts.features.allow_runtime = bundler.options.allow_runtime;
                 opts.features.trim_unused_imports = bundler.options.trim_unused_imports orelse loader.isTypeScript();
-                opts.features.should_fold_numeric_constants = platform.isBun();
+                opts.features.should_fold_typescript_constant_expressions = loader.isTypeScript();
                 opts.features.dynamic_require = platform.isBun();
 
                 opts.can_import_from_bundle = bundler.options.node_modules_bundle != null;

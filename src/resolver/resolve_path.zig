a03ee1826252517b1c802f7afb845378368b4e0b
Dylan Conway <35280289+dylan-conway@users.noreply.github.com>
2023-04-12 19:25:39 -0700

    fix bundling many entry points (#2640)
    * fix going out of bounds when length is over 8
    
    * remove

--- src/resolver/resolve_path.zig
+++ src/resolver/resolve_path.zig
@@ -128,12 +128,13 @@ pub fn longestCommonPathGeneric(input: []const []const u8, comptime separator: u
         },
         else => {
             var string_index: usize = 1;
-            while (index < min_length) : (index += 1) {
-                while (string_index < input.len) : (string_index += 1) {
-                    if (input[0][string_index] != input[index][string_index]) {
+            while (string_index < input.len) : (string_index += 1) {
+                while (index < min_length) : (index += 1) {
+                    if (input[0][index] != input[string_index][index]) {
                         break;
                     }
                 }
+                if (index == min_length) index -= 1;
                 if (@call(.always_inline, isPathSeparator, .{input[0][index]})) {
                     last_common_separator = index;
                 }

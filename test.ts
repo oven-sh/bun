7cda761a9a8fc6f888ea6a78fbe79262247850d1
Dave Caruso <me@paperdave.net>
2023-04-28 17:54:58 -0400

    remove this file

--- test.ts
+++
@@ -1,6 +0,0 @@
-const bookmarks = {
-  group: ["https://www.google.com", "https://www.google.com"],
-};
-Object.entries(bookmarks).map(async ([group, links]) => {
-  const a = await Promise.all(links.map(fetch));
-});

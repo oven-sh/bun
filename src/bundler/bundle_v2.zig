1dce9af0b5c50b44651abf3f9e0fbb6fd65ac053
Dylan Conway <dylan.conway567@gmail.com>
2023-05-02 21:42:23 -0700

    comment

--- src/bundler/bundle_v2.zig
+++ src/bundler/bundle_v2.zig
@@ -9893,6 +9893,9 @@ const LinkerContext = struct {
             for (this.export_star_records[source_index]) |id| {
                 const record = records[id];
                 if (record.source_index.isValid()) {
+                    // This file has dynamic exports if the exported imports are from a file
+                    // that either has dynamic exports directly or transitively by itself
+                    // having an export star from a file with dynamic exports.
                     const kind = this.entry_point_kinds[record.source_index.get()];
                     if ((record.source_index.isInvalid() and (!kind.isEntryPoint() or !this.output_format.keepES6ImportExportSyntax())) or
                         (record.source_index.isValid() and record.source_index.get() != source_index and this.hasDynamicExportsDueToExportStar(record.source_index.get())))

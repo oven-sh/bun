1f8c60123d504585d8777df107473695918e47d0
Dylan Conway <35280289+dylan-conway@users.noreply.github.com>
2023-05-09 23:52:40 -0700

    fix catch scope var declarations (#2839)
    * use `catch_binding` kind and report errors
    
    * make hash optional

--- src/js_ast.zig
+++ src/js_ast.zig
@@ -6564,6 +6564,7 @@ pub const Scope = struct {
         function_args,
         function_body,
         class_static_init,
+        catch_binding,
 
         pub fn jsonStringify(self: @This(), opts: anytype, o: anytype) !void {
             return try std.json.stringify(@tagName(self), opts, o);

aa34529b5b713ba856db47f253f66f651903c438
Dylan Conway <35280289+dylan-conway@users.noreply.github.com>
2023-05-19 16:04:38 -0700

    resolve rope string for multipart template (#2963)
    * resolve rope for multipart template
    
    * fix tests
    
    * clone template part if it is `e_string` and folded
    
    * clone string only
    
    * don't clone data again

--- src/js_ast.zig
+++ src/js_ast.zig
@@ -2428,11 +2428,10 @@ pub const E = struct {
                 if (part.value.data == .e_string and part.tail.cooked.isUTF8() and part.value.data.e_string.isUTF8()) {
                     if (parts.items.len == 0) {
                         if (part.value.data.e_string.len() > 0) {
-                            head.data.e_string.push(part.value.data.e_string);
+                            head.data.e_string.push(Expr.init(E.String, part.value.data.e_string.*, logger.Loc.Empty).data.e_string);
                         }
 
                         if (part.tail.cooked.len() > 0) {
-                            head.data.e_string.resolveRopeIfNeeded(allocator);
                             head.data.e_string.push(Expr.init(E.String, part.tail.cooked, part.tail_loc).data.e_string);
                         }
 
@@ -2443,7 +2442,7 @@ pub const E = struct {
 
                         if (prev_part.tail.cooked.isUTF8()) {
                             if (part.value.data.e_string.len() > 0) {
-                                prev_part.tail.cooked.push(part.value.data.e_string);
+                                prev_part.tail.cooked.push(Expr.init(E.String, part.value.data.e_string.*, logger.Loc.Empty).data.e_string);
                             }
 
                             if (part.tail.cooked.len() > 0) {

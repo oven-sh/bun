8e91569d8af233e4ec10bb5b65e90baad4974da9
cirospaciari <ciro.spaciari@gmail.com>
2023-05-19 23:18:33 -0300

    add back socket.io tests

--- test/js/third_party/socket.io/socket.io-close.test.ts
+++ test/js/third_party/socket.io/socket.io-close.test.ts
@@ -1,214 +1,214 @@
-// import { describe, it, expect } from "bun:test";
-// import { io as ioc } from "socket.io-client";
-// import { join } from "path";
-// import { createServer } from "http";
-// import { createClient, getPort, success, fail, eioHandshake, eioPoll, eioPush } from "./support/util.ts";
-// import { Server } from "socket.io";
-// import { exec, ChildProcess } from "child_process";
+import { describe, it, expect } from "bun:test";
+import { io as ioc } from "socket.io-client";
+import { join } from "path";
+import { createServer } from "http";
+import { createClient, getPort, success, fail, eioHandshake, eioPoll, eioPush } from "./support/util.ts";
+import { Server } from "socket.io";
+import { exec, ChildProcess } from "child_process";
 
-// describe("close", () => {
-//   it.skip("should be able to close sio sending a srv", done => {
-//     const httpServer = createServer().listen(0);
-//     const io = new Server(httpServer);
-//     const port = getPort(io);
-//     const net = require("net");
-//     const server = net.createServer();
+describe("close", () => {
+  it.skip("should be able to close sio sending a srv", done => {
+    const httpServer = createServer().listen(0);
+    const io = new Server(httpServer);
+    const port = getPort(io);
+    const net = require("net");
+    const server = net.createServer();
 
-//     const clientSocket = createClient(io, "/", { reconnection: false });
-//     let timeout = setTimeout(() => {
-//       fail(done, io, new Error("timeout"), clientSocket);
-//     }, 200);
+    const clientSocket = createClient(io, "/", { reconnection: false });
+    let timeout = setTimeout(() => {
+      fail(done, io, new Error("timeout"), clientSocket);
+    }, 200);
 
-//     clientSocket.on("disconnect", () => {
-//       try {
-//         expect(io.sockets.sockets.size).toBe(0);
-//       } catch (err) {
-//         fail(done, io, err, clientSocket);
-//       }
-//       server.listen(port);
-//     });
+    clientSocket.on("disconnect", () => {
+      try {
+        expect(io.sockets.sockets.size).toBe(0);
+      } catch (err) {
+        fail(done, io, err, clientSocket);
+      }
+      server.listen(port);
+    });
 
-//     clientSocket.on("connect", () => {
-//       try {
-//         expect(io.sockets.sockets.size).toBe(1);
-//         io.close();
-//       } catch (err) {
-//         fail(done, io, err, clientSocket);
-//       }
-//     });
+    clientSocket.on("connect", () => {
+      try {
+        expect(io.sockets.sockets.size).toBe(1);
+        io.close();
+      } catch (err) {
+        fail(done, io, err, clientSocket);
+      }
+    });
 
-//     server.once("listening", () => {
-//       // PORT should be free
-//       server.close((error: any) => {
-//         clearTimeout(timeout);
-//         try {
-//           expect(error).toBe(undefined);
-//           success(done, io, clientSocket);
-//         } catch (err) {
-//           fail(done, io, err, clientSocket);
-//         }
-//       });
-//     });
-//   });
+    server.once("listening", () => {
+      // PORT should be free
+      server.close((error: any) => {
+        clearTimeout(timeout);
+        try {
+          expect(error).toBe(undefined);
+          success(done, io, clientSocket);
+        } catch (err) {
+          fail(done, io, err, clientSocket);
+        }
+      });
+    });
+  });
 
-//   it.skip("should be able to close sio sending a srv", done => {
-//     const io = new Server(0);
-//     const port = getPort(io);
-//     const net = require("net");
-//     const server = net.createServer();
+  it.skip("should be able to close sio sending a srv", done => {
+    const io = new Server(0);
+    const port = getPort(io);
+    const net = require("net");
+    const server = net.createServer();
 
-//     const clientSocket = ioc("ws://0.0.0.0:" + port, {
-//       reconnection: false,
-//     });
-//     let timeout = setTimeout(() => {
-//       fail(done, io, new Error("timeout"), clientSocket);
-//     }, 200);
+    const clientSocket = ioc("ws://0.0.0.0:" + port, {
+      reconnection: false,
+    });
+    let timeout = setTimeout(() => {
+      fail(done, io, new Error("timeout"), clientSocket);
+    }, 200);
 
-//     clientSocket.on("disconnect", () => {
-//       try {
-//         expect(io.sockets.sockets.size).toBe(0);
-//       } catch (err) {
-//         fail(done, io, err, clientSocket);
-//       }
-//       server.listen(port);
-//     });
+    clientSocket.on("disconnect", () => {
+      try {
+        expect(io.sockets.sockets.size).toBe(0);
+      } catch (err) {
+        fail(done, io, err, clientSocket);
+      }
+      server.listen(port);
+    });
 
-//     clientSocket.on("connect", () => {
-//       try {
-//         expect(io.sockets.sockets.size).toBe(1);
-//         io.close();
-//       } catch (err) {
-//         fail(done, io, err, clientSocket);
-//       }
-//     });
+    clientSocket.on("connect", () => {
+      try {
+        expect(io.sockets.sockets.size).toBe(1);
+        io.close();
+      } catch (err) {
+        fail(done, io, err, clientSocket);
+      }
+    });
 
-//     server.once("listening", () => {
-//       // PORT should be free
-//       server.close((error: any) => {
-//         clearTimeout(timeout);
-//         try {
-//           expect(error).toBe(undefined);
-//           success(done, io, clientSocket);
-//         } catch (err) {
-//           fail(done, io, err, clientSocket);
-//         }
-//       });
-//     });
-//   });
+    server.once("listening", () => {
+      // PORT should be free
+      server.close((error: any) => {
+        clearTimeout(timeout);
+        try {
+          expect(error).toBe(undefined);
+          success(done, io, clientSocket);
+        } catch (err) {
+          fail(done, io, err, clientSocket);
+        }
+      });
+    });
+  });
 
-//   describe("graceful close", () => {
-//     function fixture(filename: string) {
-//       return '"' + process.execPath + '" "' + join(__dirname, "fixtures", filename) + '"';
-//     }
-//     // TODO failing on macOS
-//     it.skip("should stop socket and timers", done => {
-//       let process: ChildProcess;
-//       const timeout = setTimeout(() => {
-//         process?.kill();
-//         done(new Error("timeout"));
-//       }, 3000);
+  describe("graceful close", () => {
+    function fixture(filename: string) {
+      return '"' + process.execPath + '" "' + join(__dirname, "fixtures", filename) + '"';
+    }
+    // TODO failing on macOS
+    it.skip("should stop socket and timers", done => {
+      let process: ChildProcess;
+      const timeout = setTimeout(() => {
+        process?.kill();
+        done(new Error("timeout"));
+      }, 3000);
 
-//       process = exec(fixture("server-close.ts"), err => {
-//         clearTimeout(timeout);
-//         done(err);
-//       });
-//     });
-//   });
+      process = exec(fixture("server-close.ts"), err => {
+        clearTimeout(timeout);
+        done(err);
+      });
+    });
+  });
 
-//   describe("protocol violations", () => {
-//     it("should close the connection when receiving several CONNECT packets", done => {
-//       const httpServer = createServer();
-//       const io = new Server(httpServer);
+  describe("protocol violations", () => {
+    it("should close the connection when receiving several CONNECT packets", done => {
+      const httpServer = createServer();
+      const io = new Server(httpServer);
 
-//       httpServer.listen(0);
+      httpServer.listen(0);
 
-//       let timeout = setTimeout(() => {
-//         fail(done, io, new Error("timeout"));
-//       }, 1500);
+      let timeout = setTimeout(() => {
+        fail(done, io, new Error("timeout"));
+      }, 1500);
 
-//       (async () => {
-//         const sid = await eioHandshake(httpServer);
-//         // send a first CONNECT packet
-//         await eioPush(httpServer, sid, "40");
-//         // send another CONNECT packet
-//         await eioPush(httpServer, sid, "40");
-//         // session is cleanly closed (not discarded, see 'client.close()')
-//         // first, we receive the Socket.IO handshake response
-//         await eioPoll(httpServer, sid);
-//         // then a close packet
-//         return await eioPoll(httpServer, sid);
-//       })().then(body => {
-//         clearTimeout(timeout);
-//         try {
-//           expect(body).toBe("6\u001e1");
+      (async () => {
+        const sid = await eioHandshake(httpServer);
+        // send a first CONNECT packet
+        await eioPush(httpServer, sid, "40");
+        // send another CONNECT packet
+        await eioPush(httpServer, sid, "40");
+        // session is cleanly closed (not discarded, see 'client.close()')
+        // first, we receive the Socket.IO handshake response
+        await eioPoll(httpServer, sid);
+        // then a close packet
+        return await eioPoll(httpServer, sid);
+      })().then(body => {
+        clearTimeout(timeout);
+        try {
+          expect(body).toBe("6\u001e1");
 
-//           io.close();
-//           success(done, io);
-//         } catch (err) {
-//           fail(done, io, err);
-//         }
-//       });
-//     });
+          io.close();
+          success(done, io);
+        } catch (err) {
+          fail(done, io, err);
+        }
+      });
+    });
 
-//     it("should close the connection when receiving an EVENT packet while not connected", done => {
-//       const httpServer = createServer();
-//       const io = new Server(httpServer);
+    it("should close the connection when receiving an EVENT packet while not connected", done => {
+      const httpServer = createServer();
+      const io = new Server(httpServer);
 
-//       httpServer.listen(0);
-//       let timeout = setTimeout(() => {
-//         fail(done, io, new Error("timeout"));
-//       }, 1500);
+      httpServer.listen(0);
+      let timeout = setTimeout(() => {
+        fail(done, io, new Error("timeout"));
+      }, 1500);
 
-//       (async () => {
-//         const sid = await eioHandshake(httpServer);
-//         // send an EVENT packet
-//         await eioPush(httpServer, sid, '42["some event"]');
-//         // session is cleanly closed, we receive a close packet
-//         return await eioPoll(httpServer, sid);
-//       })().then(body => {
-//         clearTimeout(timeout);
-//         try {
-//           expect(body).toBe("6\u001e1");
+      (async () => {
+        const sid = await eioHandshake(httpServer);
+        // send an EVENT packet
+        await eioPush(httpServer, sid, '42["some event"]');
+        // session is cleanly closed, we receive a close packet
+        return await eioPoll(httpServer, sid);
+      })().then(body => {
+        clearTimeout(timeout);
+        try {
+          expect(body).toBe("6\u001e1");
 
-//           io.close();
-//           success(done, io);
-//         } catch (err) {
-//           fail(done, io, err);
-//         }
-//       });
-//     });
+          io.close();
+          success(done, io);
+        } catch (err) {
+          fail(done, io, err);
+        }
+      });
+    });
 
-//     it("should close the connection when receiving an invalid packet", done => {
-//       const httpServer = createServer();
-//       const io = new Server(httpServer);
+    it.skip("should close the connection when receiving an invalid packet", done => {
+      const httpServer = createServer();
+      const io = new Server(httpServer);
 
-//       httpServer.listen(0);
-//       let timeout = setTimeout(() => {
-//         fail(done, io, new Error("timeout"));
-//       }, 1500);
+      httpServer.listen(0);
+      let timeout = setTimeout(() => {
+        fail(done, io, new Error("timeout"));
+      }, 1500);
 
-//       (async () => {
-//         const sid = await eioHandshake(httpServer);
-//         // send a CONNECT packet
-//         await eioPush(httpServer, sid, "40");
-//         // send an invalid packet
-//         await eioPush(httpServer, sid, "4abc");
-//         // session is cleanly closed (not discarded, see 'client.close()')
-//         // first, we receive the Socket.IO handshake response
-//         await eioPoll(httpServer, sid);
-//         // then a close packet
-//         return await eioPoll(httpServer, sid);
-//       })().then(body => {
-//         clearTimeout(timeout);
-//         try {
-//           expect(body).toBe("6\u001e1");
+      (async () => {
+        const sid = await eioHandshake(httpServer);
+        // send a CONNECT packet
+        await eioPush(httpServer, sid, "40");
+        // send an invalid packet
+        await eioPush(httpServer, sid, "4abc");
+        // session is cleanly closed (not discarded, see 'client.close()')
+        // first, we receive the Socket.IO handshake response
+        await eioPoll(httpServer, sid);
+        // then a close packet
+        return await eioPoll(httpServer, sid);
+      })().then(body => {
+        clearTimeout(timeout);
+        try {
+          expect(body).toBe("6\u001e1");
 
-//           io.close();
-//           success(done, io);
-//         } catch (err) {
-//           fail(done, io, err);
-//         }
-//       });
-//     });
-//   });
-// });
+          io.close();
+          success(done, io);
+        } catch (err) {
+          fail(done, io, err);
+        }
+      });
+    });
+  });
+});
--- test/js/third_party/socket.io/socket.io-utility-methods.test.ts
+++ test/js/third_party/socket.io/socket.io-utility-methods.test.ts
@@ -1,181 +1,181 @@
-// import { createServer } from "http";
-// import { describe, it, expect, beforeEach, afterEach } from "bun:test";
-// import { io as ioc, Socket as ClientSocket } from "socket.io-client";
-// import { Adapter, BroadcastOptions } from "socket.io-adapter";
-// import type { AddressInfo } from "net";
-// import { Server } from "socket.io";
+import { createServer } from "http";
+import { describe, it, expect, beforeEach, afterEach } from "bun:test";
+import { io as ioc, Socket as ClientSocket } from "socket.io-client";
+import { Adapter, BroadcastOptions } from "socket.io-adapter";
+import type { AddressInfo } from "net";
+import { Server } from "socket.io";
 
-// import { createPartialDone } from "./support/util.ts";
+import { createPartialDone } from "./support/util.ts";
 
-// const SOCKETS_COUNT = 3;
+const SOCKETS_COUNT = 3;
 
-// class DummyAdapter extends Adapter {
-//   fetchSockets(opts: BroadcastOptions): Promise<any[]> {
-//     return Promise.resolve([
-//       {
-//         id: "42",
-//         handshake: {
-//           headers: {
-//             accept: "*/*",
-//           },
-//           query: {
-//             transport: "polling",
-//             EIO: "4",
-//           },
-//         },
-//         rooms: ["42", "room1"],
-//         data: {
-//           username: "john",
-//         },
-//       },
-//     ]);
-//   }
-// }
+class DummyAdapter extends Adapter {
+  fetchSockets(opts: BroadcastOptions): Promise<any[]> {
+    return Promise.resolve([
+      {
+        id: "42",
+        handshake: {
+          headers: {
+            accept: "*/*",
+          },
+          query: {
+            transport: "polling",
+            EIO: "4",
+          },
+        },
+        rooms: ["42", "room1"],
+        data: {
+          username: "john",
+        },
+      },
+    ]);
+  }
+}
 
-// describe("utility methods", () => {
-//   let io: Server, clientSockets: ClientSocket[], serverSockets: Socket[];
-//   beforeEach(done => {
-//     const srv = createServer();
-//     io = new Server(srv);
-//     const timeout = setTimeout(() => {
-//       serverSockets = [];
-//       // done(new Error("timeout"));
-//       done();
-//     }, 300);
+describe("utility methods", () => {
+  let io: Server, clientSockets: ClientSocket[], serverSockets: Socket[];
+  beforeEach(done => {
+    const srv = createServer();
+    io = new Server(srv);
+    const timeout = setTimeout(() => {
+      serverSockets = [];
+      // done(new Error("timeout"));
+      done();
+    }, 300);
 
-//     srv.listen(() => {
-//       const port = (srv.address() as AddressInfo).port;
+    srv.listen(() => {
+      const port = (srv.address() as AddressInfo).port;
 
-//       clientSockets = [];
-//       for (let i = 0; i < SOCKETS_COUNT; i++) {
-//         clientSockets.push(
-//           ioc(`http://localhost:${port}`, {
-//             // FIXME needed so that clients are properly closed
-//             transports: ["websocket"],
-//           }),
-//         );
-//       }
+      clientSockets = [];
+      for (let i = 0; i < SOCKETS_COUNT; i++) {
+        clientSockets.push(
+          ioc(`http://localhost:${port}`, {
+            // FIXME needed so that clients are properly closed
+            transports: ["websocket"],
+          }),
+        );
+      }
 
-//       serverSockets = [];
-//       io.on("connection", (socket: Socket) => {
-//         serverSockets.push(socket);
-//         if (serverSockets.length === SOCKETS_COUNT) {
-//           clearTimeout(timeout);
-//           done();
-//         }
-//       });
-//     });
-//   });
+      serverSockets = [];
+      io.on("connection", (socket: Socket) => {
+        serverSockets.push(socket);
+        if (serverSockets.length === SOCKETS_COUNT) {
+          clearTimeout(timeout);
+          done();
+        }
+      });
+    });
+  });
 
-//   afterEach(() => {
-//     io.close();
-//     clientSockets.forEach(socket => socket.disconnect());
-//   });
+  afterEach(() => {
+    io.close();
+    clientSockets.forEach(socket => socket.disconnect());
+  });
 
-//   describe("fetchSockets", () => {
-//     it.skip("returns all socket instances", async () => {
-//       const sockets = await io.fetchSockets();
-//       expect(sockets.length).toBe(3);
-//     });
+  describe("fetchSockets", () => {
+    it.skip("returns all socket instances", async () => {
+      const sockets = await io.fetchSockets();
+      expect(sockets.length).toBe(3);
+    });
 
-//     it.skip("returns all socket instances in the given room", async () => {
-//       serverSockets[0]?.join(["room1", "room2"]);
-//       serverSockets[1]?.join("room1");
-//       serverSockets[2]?.join("room2");
-//       const sockets = await io.in("room1").fetchSockets();
-//       expect(sockets.length).toBe(2);
-//     });
+    it.skip("returns all socket instances in the given room", async () => {
+      serverSockets[0]?.join(["room1", "room2"]);
+      serverSockets[1]?.join("room1");
+      serverSockets[2]?.join("room2");
+      const sockets = await io.in("room1").fetchSockets();
+      expect(sockets.length).toBe(2);
+    });
 
-//     it.skip("works with a custom adapter", async () => {
-//       io.adapter(DummyAdapter);
-//       const sockets = await io.fetchSockets();
-//       expect(sockets.length).toBe(1);
-//       const remoteSocket = sockets[0];
-//       expect(remoteSocket.id).toBe("42");
-//       expect(remoteSocket.rooms).toStrictEqual(new Set(["42", "room1"]));
-//       expect(remoteSocket.data).toStrictEqual({ username: "john" });
-//     });
-//   });
+    it.skip("works with a custom adapter", async () => {
+      io.adapter(DummyAdapter);
+      const sockets = await io.fetchSockets();
+      expect(sockets.length).toBe(1);
+      const remoteSocket = sockets[0];
+      expect(remoteSocket.id).toBe("42");
+      expect(remoteSocket.rooms).toStrictEqual(new Set(["42", "room1"]));
+      expect(remoteSocket.data).toStrictEqual({ username: "john" });
+    });
+  });
 
-//   describe("socketsJoin", () => {
-//     it("makes all socket instances join the given room", () => {
-//       io.socketsJoin("room1");
-//       serverSockets.forEach(socket => {
-//         expect(socket.rooms).toContain("room1");
-//       });
-//     });
+  describe("socketsJoin", () => {
+    it("makes all socket instances join the given room", () => {
+      io.socketsJoin("room1");
+      serverSockets.forEach(socket => {
+        expect(socket.rooms).toContain("room1");
+      });
+    });
 
-//     it.skip("makes all socket instances in a room join the given room", () => {
-//       serverSockets[0]?.join(["room1", "room2"]);
-//       serverSockets[1]?.join("room1");
-//       serverSockets[2]?.join("room2");
-//       io.in("room1").socketsJoin("room3");
-//       expect(serverSockets[0]?.rooms).toContain("room3");
-//       expect(serverSockets[1]?.rooms).toContain("room3");
-//       expect(serverSockets[2]?.rooms).not.toContain("room3");
-//     });
-//   });
+    it.skip("makes all socket instances in a room join the given room", () => {
+      serverSockets[0]?.join(["room1", "room2"]);
+      serverSockets[1]?.join("room1");
+      serverSockets[2]?.join("room2");
+      io.in("room1").socketsJoin("room3");
+      expect(serverSockets[0]?.rooms).toContain("room3");
+      expect(serverSockets[1]?.rooms).toContain("room3");
+      expect(serverSockets[2]?.rooms).not.toContain("room3");
+    });
+  });
 
-//   describe("socketsLeave", () => {
-//     it.skip("makes all socket instances leave the given room", () => {
-//       serverSockets[0]?.join(["room1", "room2"]);
-//       serverSockets[1]?.join("room1");
-//       serverSockets[2]?.join("room2");
-//       io.socketsLeave("room1");
-//       expect(serverSockets[0]?.rooms).toContain("room2");
-//       expect(serverSockets[0]?.rooms).toContain("room1");
-//       expect(serverSockets[1]?.rooms).not.toContain("room1");
-//     });
+  describe("socketsLeave", () => {
+    it.skip("makes all socket instances leave the given room", () => {
+      serverSockets[0]?.join(["room1", "room2"]);
+      serverSockets[1]?.join("room1");
+      serverSockets[2]?.join("room2");
+      io.socketsLeave("room1");
+      expect(serverSockets[0]?.rooms).toContain("room2");
+      expect(serverSockets[0]?.rooms).toContain("room1");
+      expect(serverSockets[1]?.rooms).not.toContain("room1");
+    });
 
-//     it.skip("makes all socket instances in a room leave the given room", () => {
-//       serverSockets[0]?.join(["room1", "room2"]);
-//       serverSockets[1]?.join("room1");
-//       serverSockets[2]?.join("room2");
-//       io.in("room2").socketsLeave("room1");
-//       expect(serverSockets[0]?.rooms).toContain("room2");
-//       expect(serverSockets[0]?.rooms).not.toContain("room1");
-//       expect(serverSockets[1]?.rooms).toContain("room1");
-//     });
-//   });
+    it.skip("makes all socket instances in a room leave the given room", () => {
+      serverSockets[0]?.join(["room1", "room2"]);
+      serverSockets[1]?.join("room1");
+      serverSockets[2]?.join("room2");
+      io.in("room2").socketsLeave("room1");
+      expect(serverSockets[0]?.rooms).toContain("room2");
+      expect(serverSockets[0]?.rooms).not.toContain("room1");
+      expect(serverSockets[1]?.rooms).toContain("room1");
+    });
+  });
 
-//   describe("disconnectSockets", () => {
-//     it.skip("makes all socket instances disconnect", done => {
-//       io.disconnectSockets(true);
-//       const timeout = setTimeout(() => {
-//         done(new Error("timeout"));
-//       }, 300);
+  describe("disconnectSockets", () => {
+    it.skip("makes all socket instances disconnect", done => {
+      io.disconnectSockets(true);
+      const timeout = setTimeout(() => {
+        done(new Error("timeout"));
+      }, 300);
 
-//       const partialDone = createPartialDone(3, err => {
-//         clearTimeout(timeout);
-//         done(err);
-//       });
+      const partialDone = createPartialDone(3, err => {
+        clearTimeout(timeout);
+        done(err);
+      });
 
-//       clientSockets[0].on("disconnect", partialDone);
-//       clientSockets[1].on("disconnect", partialDone);
-//       clientSockets[2].on("disconnect", partialDone);
-//     });
+      clientSockets[0].on("disconnect", partialDone);
+      clientSockets[1].on("disconnect", partialDone);
+      clientSockets[2].on("disconnect", partialDone);
+    });
 
-//     it.skip("makes all socket instances in a room disconnect", done => {
-//       const timeout = setTimeout(() => {
-//         done(new Error("timeout"));
-//       }, 300);
+    it.skip("makes all socket instances in a room disconnect", done => {
+      const timeout = setTimeout(() => {
+        done(new Error("timeout"));
+      }, 300);
 
-//       serverSockets[0]?.join(["room1", "room2"]);
-//       serverSockets[1]?.join("room1");
-//       serverSockets[2]?.join("room2");
-//       io.in("room2").disconnectSockets(true);
+      serverSockets[0]?.join(["room1", "room2"]);
+      serverSockets[1]?.join("aroom1");
+      serverSockets[2]?.join("room2");
+      io.in("room2").disconnectSockets(true);
 
-//       const partialDone = createPartialDone(2, err => {
-//         clearTimeout(timeout);
-//         clientSockets[1].off("disconnect");
-//         done(err);
-//       });
+      const partialDone = createPartialDone(2, err => {
+        clearTimeout(timeout);
+        clientSockets[1].off("disconnect");
+        done(err);
+      });
 
-//       clientSockets[0].on("disconnect", partialDone);
-//       clientSockets[1].on("disconnect", () => {
-//         done(new Error("should not happen"));
-//       });
-//       clientSockets[2].on("disconnect", partialDone);
-//     });
-//   });
-// });
+      clientSockets[0].on("disconnect", partialDone);
+      clientSockets[1].on("disconnect", () => {
+        done(new Error("should not happen"));
+      });
+      clientSockets[2].on("disconnect", partialDone);
+    });
+  });
+});

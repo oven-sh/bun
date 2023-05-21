9d79680a1d669a3976a86cd2b413d62083914ce0
cirospaciari <ciro.spaciari@gmail.com>
2023-05-21 18:20:46 -0400

    fmt

--- test/js/bun/test/todo-test-fixture.js
+++ test/js/bun/test/todo-test-fixture.js
@@ -1,9 +1,9 @@
 import { test } from "bun:test";
 
-test.todo("todo 1")
+test.todo("todo 1");
 test.todo("todo 2", () => {
   throw new Error("this error is shown");
-})
+});
 test.todo("todo 3", () => {
   // passes
 });

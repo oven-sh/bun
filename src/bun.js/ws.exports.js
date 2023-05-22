890f008e7b2085c9cdc75ed4cfc0359059c283ea
cirospaciari <ciro.spaciari@gmail.com>
2023-05-21 22:05:06 -0300

    add opts.compress support

--- src/bun.js/ws.exports.js
+++ src/bun.js/ws.exports.js
@@ -419,8 +419,8 @@ class BunWebSocketMocked extends EventEmitter {
   #drain(ws) {
     const chunk = this.#enquedMessages[0];
     if (chunk) {
-      const [data, cb] = chunk;
-      const written = ws.send(data);
+      const [data, opts, cb] = chunk;
+      const written = ws.send(data, opts?.compress);
       if (written == -1) {
         // backpressure wait until next drain event
         return;
@@ -435,10 +435,10 @@ class BunWebSocketMocked extends EventEmitter {
 
   send(data, opts, cb) {
     if (this.#state === 1) {
-      const written = this.#ws.send(data);
+      const written = this.#ws.send(data, opts?.compress);
       if (written == -1) {
         // backpressure
-        this.#enquedMessages.push([data, cb]);
+        this.#enquedMessages.push([data, opts, cb]);
         this.#bufferedAmount += data.length;
         return;
       }
@@ -446,7 +446,7 @@ class BunWebSocketMocked extends EventEmitter {
       typeof cb === "function" && cb();
     } else if (this.#state === 0) {
       // not connected yet
-      this.#enquedMessages.push([data, cb]);
+      this.#enquedMessages.push([data, opts, cb]);
       this.#bufferedAmount += data.length;
     }
   }

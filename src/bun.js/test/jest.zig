3361814cab66c688f27a794e90571ac5aa8a6663
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-05-24 15:44:02 -0700

    Fix string check

--- src/bun.js/test/jest.zig
+++ src/bun.js/test/jest.zig
@@ -376,6 +376,7 @@ pub const TestRunner = struct {
     test_timeout_timer: ?*bun.uws.Timer = null,
     last_test_timeout_timer_duration: u32 = 0,
     active_test_for_timeout: ?TestRunner.Test.ID = null,
+    isolate: bool = false,
 
     global_callbacks: struct {
         beforeAll: std.ArrayListUnmanaged(JSC.JSValue) = .{},
@@ -1230,7 +1231,7 @@ pub const Expect = struct {
         var pass = false;
 
         var actual_length: f64 = undefined;
-        if (value.isString()) {
+        if (value.jsType() == .String) {
             actual_length = @intToFloat(f64, value.asString().length());
             if (actual_length == expected_length) pass = true;
         } else {

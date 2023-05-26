4e35ccb7d6682493eb0cc49c37382e1d4d81fd49
cirospaciari <ciro.spaciari@gmail.com>
2023-05-25 23:00:50 -0300

    fix parsing add reconnect

--- src/bun.js/event_source.exports.js
+++ src/bun.js/event_source.exports.js
@@ -9,93 +9,125 @@ class EventSource extends EventTarget {
   #data_buffer = "";
   #send_buffer = "";
   #lastEventID = "";
+  #reconnect = true;
+  #content_length = 0; // 0 means chunked
+  #received_length = 0;
 
+  static #ConnectNextTick(self) {
+    self.#connect();
+  }
   static #SendRequest(socket, url) {
-    const request = `GET ${url.pathname}${url.search} HTTP/1.1\r\nHost: bun\r\nContent-type: text/event-stream\r\nContent-length: 0\r\n\r\n`;
+    const self = socket.data;
+    const last_event_header = self.#lastEventID ? `Last-Event-ID: ${self.#lastEventID}\r\n` : "";
+    const request = `GET ${url.pathname}${url.search} HTTP/1.1\r\nHost: bun\r\nContent-type: text/event-stream\r\nContent-length: 0\r\n${last_event_header}\r\n`;
     const sended = socket.write(request);
     if (sended !== request.length) {
-      socket.data.#send_buffer = request.substring(sended);
+      self.#send_buffer = request.substring(sended);
     }
   }
 
-  static #ProcessChunk(self, chunk, offset) {
-    let start_idx = chunk.indexOf("\r\n", offset);
-    // wait for the chunk
-    if (start_idx === -1 && self.#data_buffer.length === 0) {
-      self.#data_buffer += chunk;
-      return;
-    }
-    // wait for data end
-    let event_idx = chunk.indexOf("\n\n");
-    if (event_idx == -1) {
-      // wait for more data
-      self.#data_buffer += chunk;
-      return;
-    }
-
-    // combine data
-    if (self.#data_buffer.length) {
-      self.#data_buffer += chunk;
-      chunk = self.#data_buffer;
-      self.#data_buffer = "";
-    }
-
+  static #ProcessChunk(self, chunks, offset) {
     for (;;) {
-      const event_data = chunk.substring(start_idx + 2, event_idx);
-
-      let type;
-      let data = "";
-      let id;
-      let event_line_idx = 0;
-      for (;;) {
-        let idx = event_data.indexOf("\n", event_line_idx);
-        if (idx === -1) {
-          if (event_line_idx >= event_data.length) {
-            break;
-          }
-          idx = event_data.length;
-        }
-        const line = event_data.substring(event_line_idx, idx);
-        if (line.startsWith("data:")) {
-          if (data.length) {
-            data += `\n${line.substring(5).trim()}`;
-          } else {
-            data = line.substring(5).trim();
-          }
-        } else if (line.startsWith("event:")) {
-          type = line.substring(6).trim();
-        } else if (line.startsWith("id:")) {
-          id = line.substring(3).trim();
-        }
-        event_line_idx = idx + 1;
+      if (offset >= chunks.length) {
+        return;
       }
-      self.#lastEventID = id || "";
-
-      if (data || id || type) {
-        self.dispatchEvent(
-          new MessageEvent(type || "message", {
-            data: data || "",
-            origin: self.#url.origin,
-            // @ts-ignore
-            source: self,
-            lastEventId: id,
-          }),
-        );
+      let chunk_end_idx = -1;
+      let start_idx = chunks.indexOf("\r\n", offset);
+      const chunk_start_idx = start_idx + 2;
+      if (start_idx > 0 && self.#content_length === 0) {
+        const chunk_size = parseInt(chunks.substring(offset, start_idx), 16);
+        if (chunk_size === 0) {
+          // no more chunks
+          self.#state = 2;
+          self.#socket?.end();
+          return;
+        }
+        chunk_end_idx = chunk_start_idx + chunk_size;
+      } else {
+        chunk_end_idx = chunks.length;
       }
-
-      // no more events
-      if (chunk.length === event_idx + 2) {
+      // wait for the chunk
+      if (start_idx === -1 && self.#data_buffer.length === 0) {
+        self.#data_buffer += chunks.substring(offset);
         return;
       }
 
-      const next_event_idx = chunk.indexOf("\n\n", event_idx + 1);
-      if (next_event_idx === -1) {
+      // check for chunk end
+      const chunk = chunks.substring(chunk_start_idx, chunk_end_idx);
+      offset = chunk_end_idx + 2;
+      let chunk_offset = 0;
+      // wait for data end
+      let event_idx = chunk.indexOf("\n\n");
+      if (event_idx == -1) {
         // wait for more data
-        self.#data_buffer = chunk.substring(event_idx + 1);
+        self.#data_buffer += chunks.substring(chunk_start_idx);
         return;
       }
-      start_idx = event_idx;
-      event_idx = next_event_idx;
+
+      // combine data
+      if (self.#data_buffer.length) {
+        self.#data_buffer += chunk;
+        chunk = self.#data_buffer;
+        self.#data_buffer = "";
+      }
+
+      let more_events = true;
+      while (more_events) {
+        const event_data = chunk.substring(chunk_offset, event_idx);
+
+        let type;
+        let data = "";
+        let id;
+        let event_line_idx = 0;
+        for (;;) {
+          let idx = event_data.indexOf("\n", event_line_idx);
+          if (idx === -1) {
+            if (event_line_idx >= event_data.length) {
+              break;
+            }
+            idx = event_data.length;
+          }
+          const line = event_data.substring(event_line_idx, idx);
+          if (line.startsWith("data:")) {
+            if (data.length) {
+              data += `\n${line.substring(5).trim()}`;
+            } else {
+              data = line.substring(5).trim();
+            }
+          } else if (line.startsWith("event:")) {
+            type = line.substring(6).trim();
+          } else if (line.startsWith("id:")) {
+            id = line.substring(3).trim();
+          }
+          event_line_idx = idx + 1;
+        }
+        self.#lastEventID = id || "";
+
+        if (data || id || type) {
+          self.dispatchEvent(
+            new MessageEvent(type || "message", {
+              data: data || "",
+              origin: self.#url.origin,
+              // @ts-ignore
+              source: self,
+              lastEventId: id,
+            }),
+          );
+        }
+
+        // no more events
+        if (chunk.length === event_idx + 2) {
+          more_events = false;
+          break;
+        }
+
+        const next_event_idx = chunk.indexOf("\n\n", event_idx + 1);
+        if (next_event_idx === -1) {
+          break;
+        }
+        chunk_offset = event_idx;
+        event_idx = next_event_idx;
+      }
     }
   }
   static #Handlers = {
@@ -151,31 +183,53 @@ class EventSource extends EventTarget {
           }
 
           let start_idx = status_idx + 1;
+          let mime_type_ok = false;
+          let content_length = -1;
           for (;;) {
-            const header_idx = headers.indexOf("\r\n", start_idx);
+            let header_idx = headers.indexOf("\r\n", start_idx);
             // No text/event-stream mime type
             if (header_idx === -1) {
-              self.#state = 2;
-              self.dispatchEvent(
-                new ErrorEvent("error", {
-                  error: new Error(
-                    `EventSource's response has no MIME type and "text/event-stream" is required. Aborting the connection.`,
-                  ),
-                }),
-              );
-              socket.end();
-              return;
+              if (start_idx >= headers.length) {
+                if (!mime_type_ok) {
+                  self.#state = 2;
+                  self.dispatchEvent(
+                    new ErrorEvent("error", {
+                      error: new Error(
+                        `EventSource's response has no MIME type and "text/event-stream" is required. Aborting the connection.`,
+                      ),
+                    }),
+                  );
+                  socket.end();
+                }
+
+                if (content_length === -1) {
+                  self.#state = 2;
+                  self.dispatchEvent(
+                    new ErrorEvent("error", {
+                      error: new Error(`Content-Length or Transfer-Encoding header missing. Aborting the connection.`),
+                    }),
+                  );
+                  socket.end();
+                }
+                return;
+              }
+
+              header_idx = headers.length;
             }
 
-            const header = headers.substring(start_idx, header_idx);
-            const header_name = header.substring(1, header.indexOf(":"));
+            const header = headers.substring(start_idx + 1, header_idx);
+            const header_name_idx = header.indexOf(":");
+            const header_name = header.substring(0, header_name_idx);
             const is_content_type =
               header_name.localeCompare("content-type", undefined, { sensitivity: "accent" }) === 0;
             start_idx = header_idx + 1;
+
             if (is_content_type) {
               if (header.endsWith(" text/event-stream")) {
-                // mime type check ok
-                break;
+                mime_type_ok = true;
+                if (content_length !== -1) {
+                  break;
+                }
               } else {
                 // wrong mime type
                 self.#state = 2;
@@ -189,18 +243,68 @@ class EventSource extends EventTarget {
                 socket.end();
                 return;
               }
+            } else {
+              const is_content_length =
+                header_name.localeCompare("content-length", undefined, { sensitivity: "accent" }) === 0;
+              if (is_content_length) {
+                content_length = parseInt(header.substring(header_name_idx + 1).trim(), 10);
+                if (isNaN(content_length) || content_length <= 0) {
+                  self.dispatchEvent(
+                    new ErrorEvent("error", {
+                      error: new Error(`EventSource's Content-Length is invalid. Aborting the connection.`),
+                    }),
+                  );
+                  socket.end();
+                  return;
+                }
+                if (mime_type_ok) {
+                  break;
+                }
+              } else {
+                const is_transfer_encoding =
+                  header_name.localeCompare("transfer-encoding", undefined, { sensitivity: "accent" }) === 0;
+                if (is_transfer_encoding) {
+                  if (header.substring(header_name_idx + 1).trim() !== "chunked") {
+                    self.dispatchEvent(
+                      new ErrorEvent("error", {
+                        error: new Error(`EventSource's Transfer-Encoding is invalid. Aborting the connection.`),
+                      }),
+                    );
+                    socket.end();
+                    return;
+                  }
+                  content_length = 0;
+                  if (mime_type_ok) {
+                    break;
+                  }
+                }
+              }
             }
           }
+
+          self.#content_length = content_length;
           self.#state = 1;
           self.dispatchEvent(new Event("open"));
           const chunks = text.substring(headers_idx + 4);
           EventSource.#ProcessChunk(self, chunks, 0);
-
+          if (self.#content_length > 0) {
+            self.#received_length += chunks.length;
+            if (self.#received_length >= self.#content_length) {
+              self.#state = 2;
+              socket.end();
+            }
+          }
           return;
         }
         case 1:
           EventSource.#ProcessChunk(self, buffer.toString(), 2);
-
+          if (self.#content_length > 0) {
+            self.#received_length += buffer.byteLength;
+            if (self.#received_length >= self.#content_length) {
+              self.#state = 2;
+              socket.end();
+            }
+          }
           return;
         default:
           break;
@@ -231,18 +335,32 @@ class EventSource extends EventTarget {
     },
     binaryType: "buffer",
   };
+  static #Retry(self) {
+    if (self.#state !== 2) return;
+    self.#connect();
+  }
   static #Close(socket) {
     const self = socket.data;
     self.#socket = null;
+    self.#received_length = 0;
     self.#state = 2;
+    if (self.#reconnect) {
+      setTimeout(EventSource.#Retry, 1000, self);
+    }
     return self;
   }
   constructor(url, options = undefined) {
     super();
     const uri = new URL(url);
-    const is_tls = uri.protocol === "https:";
-    this.#is_tls = is_tls;
+    this.#is_tls = uri.protocol === "https:";
     this.#url = uri;
+    this.#state = 2;
+    process.nextTick(EventSource.#ConnectNextTick, this);
+  }
+  #connect() {
+    if (this.#state !== 2) return;
+    const uri = this.#url;
+    const is_tls = this.#is_tls;
     this.#state = 0;
     //@ts-ignore
     Bun.connect({
@@ -268,6 +386,7 @@ class EventSource extends EventTarget {
   }
 
   close() {
+    this.#reconnect = false;
     this.#state = 2;
     this.#socket?.end();
   }
--- test/js/bun/eventsource/eventsource.test.js
+++ test/js/bun/eventsource/eventsource.test.js
@@ -21,6 +21,25 @@ function sse(req) {
   );
 }
 
+function sse_unstable(req) {
+  const signal = req.signal;
+  let id = parseInt(req.headers.get("last-event-id") || "0", 10);
+
+  return new Response(
+    new ReadableStream({
+      type: "direct",
+      async pull(controller) {
+        if (!signal.aborted) {
+          await controller.write(`id:${++id}\ndata: Hello, World!\n\n`);
+          await controller.flush();
+        }
+        controller.close();
+      },
+    }),
+    { status: 200, headers: { "Content-Type": "text/event-stream" } },
+  );
+}
+
 function sseServer(done, pathname, callback) {
   const server = Bun.serve({
     port: 0,
@@ -28,15 +47,22 @@ function sseServer(done, pathname, callback) {
       if (new URL(req.url).pathname === "/stream") {
         return sse(req);
       }
+      if (new URL(req.url).pathname === "/unstable") {
+        return sse_unstable(req);
+      }
       return new Response("Hello, World!");
     },
   });
+  let evtSource;
   try {
-    callback(`http://localhost:${server.port}${pathname}`, err => {
+    evtSource = new EventSource(`http://localhost:${server.port}${pathname}`);
+    callback(evtSource, err => {
+      evtSource.close();
       done(err);
       server.stop(true);
     });
   } catch (err) {
+    evtSource?.close();
     server.stop(true);
     done(err);
   }
@@ -46,8 +72,7 @@ import { describe, expect, it } from "bun:test";
 
 describe("events", () => {
   it("should call open", done => {
-    sseServer(done, "/stream", (url, done) => {
-      const evtSource = new EventSource(url);
+    sseServer(done, "/stream", (evtSource, done) => {
       evtSource.onopen = () => {
         done();
       };
@@ -55,8 +80,7 @@ describe("events", () => {
   });
 
   it("should call message", done => {
-    sseServer(done, "/stream", (url, done) => {
-      const evtSource = new EventSource(url);
+    sseServer(done, "/stream", (evtSource, done) => {
       evtSource.onmessage = e => {
         expect(e.data).toBe("Hello, World!");
         done();
@@ -65,9 +89,7 @@ describe("events", () => {
   });
 
   it("should call custom event", done => {
-    sseServer(done, "/stream", (url, done) => {
-      const evtSource = new EventSource(url);
-
+    sseServer(done, "/stream", (evtSource, done) => {
       evtSource.addEventListener("bun", e => {
         expect(e.data).toBe("Hello, World!");
         done();
@@ -76,9 +98,7 @@ describe("events", () => {
   });
 
   it("should call event with multiple lines", done => {
-    sseServer(done, "/stream", (url, done) => {
-      const evtSource = new EventSource(url);
-
+    sseServer(done, "/stream", (evtSource, done) => {
       evtSource.addEventListener("lines", e => {
         expect(e.data).toBe("Line 1!\nLine 2!");
         done();
@@ -87,9 +107,7 @@ describe("events", () => {
   });
 
   it("should receive id", done => {
-    sseServer(done, "/stream", (url, done) => {
-      const evtSource = new EventSource(url);
-
+    sseServer(done, "/stream", (evtSource, done) => {
       evtSource.addEventListener("id_test", e => {
         expect(e.lastEventId).toBe("1");
         done();
@@ -97,10 +115,23 @@ describe("events", () => {
     });
   });
 
-  it("should call error", done => {
-    sseServer(done, "/", (url, done) => {
-      const evtSource = new EventSource(url);
+  it("should reconnect with id", done => {
+    sseServer(done, "/unstable", (evtSource, done) => {
+      const ids = [];
+      evtSource.onmessage = e => {
+        ids.push(e.lastEventId);
+        if (ids.length === 2) {
+          for (let i = 0; i < 2; i++) {
+            expect(ids[i]).toBe((i + 1).toString());
+          }
+          done();
+        }
+      };
+    });
+  });
 
+  it("should call error", done => {
+    sseServer(done, "/", (evtSource, done) => {
       evtSource.onerror = e => {
         expect(e.error.message).toBe(
           `EventSource's response has a MIME type that is not "text/event-stream". Aborting the connection.`,

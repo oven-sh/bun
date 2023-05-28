9aa19b8bfe8852d64be81720bd430a0ea62adb50
Tiramify (A.K. Daniel) <94789999+TiranexDev@users.noreply.github.com>
2023-05-28 02:17:23 +0000

    Update run-process-env.test.ts

--- test/cli/run/run-process-env.test.ts
+++ test/cli/run/run-process-env.test.ts
@@ -1,25 +1,5 @@
 import { describe, expect, test } from "bun:test";
-import { tempDirWithFiles } from "./env.test";
-import { bunEnv, bunExe } from "harness";
-
-function bunRunAsScript(dir: string, script: string, env?: Record<string, string>) {
-    const result = Bun.spawnSync([bunExe(), `run`, `${script}`], {
-      cwd: dir,
-      env: {
-        ...bunEnv,
-        NODE_ENV: undefined,
-        ...env,
-      },
-    });
-
-    if (!result.success)
-        throw new Error(result.stderr.toString("utf8"));
-
-    return {
-      stdout: result.stdout.toString("utf8").trim(),
-      stderr: result.stderr.toString("utf8").trim(),
-    };
-}
+import { bunRunAsScript, tempDirWithFiles } from "harness";
 
 describe("process.env", () => {
   test("npm_lifecycle_event", () => {

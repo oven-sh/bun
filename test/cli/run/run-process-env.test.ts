4ba3ed512ac8407603c99d56d931038e0bdf0884
Tiramify (A.K. Daniel) <94789999+TiranexDev@users.noreply.github.com>
2023-05-28 03:43:08 +0000

    Implement process.env.npm_lifecycle_event (#3097)
    * Update run_command.zig
    
    * Update env.test.ts
    
    * Add files via upload
    
    * Update run-process-env.test.ts
    
    * Update env.test.ts
    
    * Update harness.ts

---
+++ test/cli/run/run-process-env.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "bun:test";
+import { bunRunAsScript, tempDirWithFiles } from "harness";
+
+describe("process.env", () => {
+  test("npm_lifecycle_event", () => {
+    const scriptName = 'start:dev';
+
+    const dir = tempDirWithFiles("processenv", {
+      "package.json": `{'scripts': {'${scriptName}': 'bun run index.ts'}}`,
+      "index.ts": "console.log(process.env.npm_lifecycle_event);",
+    });
+    
+    const { stdout } = bunRunAsScript(dir, scriptName);
+    expect(stdout).toBe(scriptName);
+  });
+});

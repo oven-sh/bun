cc533e49e347148194113c4f3a7d8c7cc4c7026a
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-05-31 03:11:31 -0700

    print

--- bench/stream-file-upload-client/stream-file-bun.js
+++ bench/stream-file-upload-client/stream-file-bun.js
@@ -1,7 +1,9 @@
 import { file } from "bun";
 console.time("stream-file-bun");
-await fetch(process.env.URL ?? "http://localhost:3000", {
+const response = await fetch(process.env.URL ?? "http://localhost:3000", {
   method: "POST",
   body: file(process.env.FILE ?? "hello.txt"),
 });
 console.timeEnd("stream-file-bun");
+
+console.log("Sent", await response.text(), "bytes");
--- bench/stream-file-upload-client/stream-file-node.mjs
+++ bench/stream-file-upload-client/stream-file-node.mjs
@@ -4,13 +4,16 @@ import http from "node:http";
 console.time("stream-file-node");
 createReadStream(process.env.FILE ?? "hello.txt")
   .pipe(
-    http.request(process.env.URL ?? "http://localhost:3000", {
-      method: "POST",
-    }),
+    http
+      .request(process.env.URL ?? "http://localhost:3000", {
+        method: "POST",
+      })
+      .on("response", response => {
+        response.on("data", data => {
+          console.log("Sent", parseInt(data.toString(), 10), "bytes");
+        });
+      }),
   )
-  .on("body", body => {
-    console.log("Sent", parseInt(body.toString(), 10), "bytes");
-  })
   .on("close", () => {
     console.timeEnd("stream-file-node");
   });

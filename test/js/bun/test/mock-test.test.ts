ee68984802e9a81349e4ce5ca3fe23dcaef5776a
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-06-08 18:05:46 -0700

    A couple more tests

--- test/js/bun/test/mock-test.test.ts
+++ test/js/bun/test/mock-test.test.ts
@@ -1,12 +1,39 @@
-import { afterAll, beforeAll, describe, expect, it, mock } from "bun:test";
+import { test, mock, expect } from "bun:test";
 
-describe("mocks", () => {
-  it("are callable", () => {
-    const fn = mock(() => 42);
-    expect(fn()).toBe(42);
-    expect(fn.mock.calls.length).toBe(1);
-    expect(fn.mock.calls[0].length).toBe(0);
-    expect(fn).toHaveBeenCalled();
-    expect(fn).toHaveBeenCalledTimes(1);
+test("are callable", () => {
+  const fn = mock(() => 42);
+  expect(fn()).toBe(42);
+  expect(fn).toHaveBeenCalled();
+  expect(fn).toHaveBeenCalledTimes(1);
+  expect(fn.mock.calls).toHaveLength(1);
+  expect(fn.mock.calls[0]).toBeEmpty();
+
+  expect(fn()).toBe(42);
+  expect(fn).toHaveBeenCalledTimes(2);
+
+  expect(fn.mock.calls).toHaveLength(2);
+  expect(fn.mock.calls[1]).toBeEmpty();
+});
+
+test("include arguments", () => {
+  const fn = mock(f => f);
+  expect(fn(43)).toBe(43);
+  expect(fn.mock.results[0]).toEqual({
+    type: "return",
+    value: 43,
   });
+  expect(fn.mock.calls[0]).toEqual([43]);
+});
+
+test("works when throwing", () => {
+  const instance = new Error("foo");
+  const fn = mock(f => {
+    throw instance;
+  });
+  expect(() => fn(43)).toThrow("foo");
+  expect(fn.mock.results[0]).toEqual({
+    type: "throw",
+    value: instance,
+  });
+  expect(fn.mock.calls[0]).toEqual([43]);
 });

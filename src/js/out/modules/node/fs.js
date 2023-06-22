1e58f1b998db60145c7d5f41e9997e54e3470d5a
cirospaciari <ciro.spaciari@gmail.com>
2023-06-22 08:36:44 -0400

    this is necessary? just testing CI/CD weird errors

--- src/js/out/modules/node/fs.js
+++ src/js/out/modules/node/fs.js
@@ -24,12 +24,6 @@ class FSWatcher extends EventEmitter {
   #listener;
   constructor(path, options, listener) {
     super();
-    if (path instanceof URL)
-      throw new TypeError("Watch URLs are not supported yet");
-    else if (Buffer.isBuffer(path))
-      path = path.toString();
-    else if (typeof path !== "string")
-      throw new TypeError("Expected path to be a string or Buffer");
     if (typeof options === "function")
       listener = options, options = {};
     else if (typeof options === "string")

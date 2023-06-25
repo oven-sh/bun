f9091900d30283f35e50bfb338d442d924fa6722
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-06-25 05:14:42 -0700

    Fixes #3334

--- test/js/workerd/html-rewriter.test.js
+++ test/js/workerd/html-rewriter.test.js
@@ -301,3 +301,24 @@ describe("HTMLRewriter", () => {
     ).toEqual("<div></div>");
   });
 });
+
+// By not segfaulting, this test passes
+it("#3334 regression", async () => {
+  for (let i = 0; i < 10; i++) {
+    const headers = new Headers({
+      "content-type": "text/html",
+    });
+    const response = new Response("<div>content</div>", { headers });
+
+    const result = await new HTMLRewriter()
+      .on("div", {
+        element(elem) {
+          elem.setInnerContent("new");
+        },
+      })
+      .transform(response)
+      .text();
+    expect(result).toEqual("<div>new</div>");
+  }
+  Bun.gc(true);
+});

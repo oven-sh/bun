f023548459387850c1b90d42d29f1c9d2ce210ea
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-06-25 10:23:24 -0700

    Support reading embedded files in compiled executables

--- src/bun.js/node/node_fs.zig
+++ src/bun.js/node/node_fs.zig
@@ -2492,6 +2492,7 @@ pub const NodeFS = struct {
     /// That means a stack-allocated buffer won't suffice. Instead, we re-use
     /// the heap allocated buffer on the NodefS struct
     sync_error_buf: [bun.MAX_PATH_BYTES]u8 = undefined,
+    vm: ?*JSC.VirtualMachine = null,
 
     pub const ReturnType = Return;
 
@@ -3442,6 +3443,34 @@ pub const NodeFS = struct {
                 const fd = switch (args.path) {
                     .path => brk: {
                         path = args.path.path.sliceZ(&this.sync_error_buf);
+                        if (this.vm) |vm| {
+                            if (vm.standalone_module_graph) |graph| {
+                                if (graph.find(path)) |file| {
+                                    return switch (args.encoding) {
+                                        .buffer => .{
+                                            .result = .{
+                                                .buffer = Buffer.fromBytes(bun.default_allocator.dupe(u8, file.contents) catch @panic("out of memory"), bun.default_allocator, .Uint8Array),
+                                            },
+                                        },
+                                        else => brk2: {
+                                            if (comptime string_type == .default) {
+                                                break :brk2 .{
+                                                    .result = .{
+                                                        .string = bun.default_allocator.dupe(u8, file.contents) catch @panic("out of memory"),
+                                                    },
+                                                };
+                                            } else {
+                                                break :brk2 .{
+                                                    .result = .{
+                                                        .null_terminated = bun.default_allocator.dupeZ(u8, file.contents) catch @panic("out of memory"),
+                                                    },
+                                                };
+                                            }
+                                        },
+                                    };
+                                }
+                            }
+                        }
                         break :brk switch (Syscall.open(
                             path,
                             os.O.RDONLY | os.O.NOCTTY,

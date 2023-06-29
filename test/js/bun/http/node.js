8a5c78d264fd0d912f3a63cc2a0cdbb234bbd11d
dave caruso <me@paperdave.net>
2023-06-28 20:17:30 -0400

    remove files

--- test/js/bun/http/node.js
+++
@@ -1,8 +0,0 @@
-// import { createServer } from "http";
-
-// const s = createServer((req, res) => {
-//   res.writeHead(200, { "Content-Type": "bruh" });
-//   res.end("bruh");
-// });
-
-// s.listen(3000, () => {});

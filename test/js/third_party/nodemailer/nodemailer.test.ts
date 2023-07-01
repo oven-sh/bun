e26a2cd34c698c0876c7ad81311b63ab65dba3ee
cirospaciari <ciro.spaciari@gmail.com>
2023-07-01 16:03:36 -0300

    show error message

--- test/js/third_party/nodemailer/nodemailer.test.ts
+++ test/js/third_party/nodemailer/nodemailer.test.ts
@@ -4,8 +4,10 @@ import path from "path";
 
 describe("nodemailer", () => {
   test("basic smtp", async () => {
-    const info = bunRun(path.join(import.meta.dir, "process-nodemailer-fixture.js"));
-    expect(info.stdout).toBe("true");
-    expect(info.stderr || "").toBe("");
+    expect(()=> {
+      const info = bunRun(path.join(import.meta.dir, "process-nodemailer-fixture.js"));
+      expect(info.stdout).toBe("true");
+      expect(info.stderr || "").toBe("");
+    }).not.toThrow();
   }, 10000);
 });

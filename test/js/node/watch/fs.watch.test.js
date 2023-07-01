3c4bc61c4267b454132bb4ddca11b92f0d2df803
Hanaasagi <ambiguous404@gmail.com>
2023-07-02 00:54:52 +0900

    format code

--- test/js/node/watch/fs.watch.test.js
+++ test/js/node/watch/fs.watch.test.js
@@ -1,4 +1,4 @@
-import fs, { link } from "fs";
+import fs from "fs";
 import path from "path";
 import { tempDirWithFiles, bunRun, bunRunAsScript } from "harness";
 import { pathToFileURL } from "bun";

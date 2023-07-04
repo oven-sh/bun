2e36f59cdf03f700cc0b83f0d87a694a659df157
alexlamsl <alexlamsl@gmail.com>
2023-07-04 03:45:07 +0300

    [install] fix run-time module loading - fix version buffer confusion - improve workaround to handle cached modules
    fixes #3507

--- test/cli/install/bun-run.test.ts
+++ test/cli/install/bun-run.test.ts
@@ -24,7 +24,37 @@ const { minify } = require("uglify-js@3.17.4");
 console.log(minify("print(6 * 7)").code);
   `,
   );
-  const { stdout, stderr, exited } = spawn({
+  const {
+    stdout: stdout1,
+    stderr: stderr1,
+    exited: exited1,
+  } = spawn({
+    cmd: [bunExe(), "run", "test.js"],
+    cwd: run_dir,
+    stdout: null,
+    stdin: "pipe",
+    stderr: "pipe",
+    env: {
+      ...env,
+      BUN_INSTALL_CACHE_DIR: join(run_dir, ".cache"),
+    },
+  });
+  expect(stderr1).toBeDefined();
+  const err1 = await new Response(stderr1).text();
+  expect(err1).toBe("");
+  expect(await readdirSorted(run_dir)).toEqual([".cache", "test.js"]);
+  expect(await readdirSorted(join(run_dir, ".cache"))).toContain("uglify-js");
+  expect(await readdirSorted(join(run_dir, ".cache", "uglify-js"))).toEqual(["3.17.4"]);
+  expect(stdout1).toBeDefined();
+  const out1 = await new Response(stdout1).text();
+  expect(out1.split(/\r?\n/)).toEqual(["print(42);", ""]);
+  expect(await exited1).toBe(0);
+  // Perform `bun test.js` with cached dependencies
+  const {
+    stdout: stdout2,
+    stderr: stderr2,
+    exited: exited2,
+  } = spawn({
     cmd: [bunExe(), "test.js"],
     cwd: run_dir,
     stdout: null,
@@ -35,14 +65,16 @@ console.log(minify("print(6 * 7)").code);
       BUN_INSTALL_CACHE_DIR: join(run_dir, ".cache"),
     },
   });
-  expect(stderr).toBeDefined();
-  const err = await new Response(stderr).text();
-  expect(err).toBe("");
-  expect(stdout).toBeDefined();
-  const out = await new Response(stdout).text();
-  expect(out.split(/\r?\n/)).toEqual(["print(42);", ""]);
-  expect(await exited).toBe(0);
+  expect(stderr2).toBeDefined();
+  const err2 = await new Response(stderr2).text();
+  expect(err2).toBe("");
   expect(await readdirSorted(run_dir)).toEqual([".cache", "test.js"]);
+  expect(await readdirSorted(join(run_dir, ".cache"))).toContain("uglify-js");
+  expect(await readdirSorted(join(run_dir, ".cache", "uglify-js"))).toEqual(["3.17.4"]);
+  expect(stdout2).toBeDefined();
+  const out2 = await new Response(stdout2).text();
+  expect(out2.split(/\r?\n/)).toEqual(["print(42);", ""]);
+  expect(await exited2).toBe(0);
 });
 
 it("should download dependencies to run local file", async () => {
@@ -58,7 +90,11 @@ for (const entry of await decompress(Buffer.from(buffer))) {
 }
   `,
   );
-  const { stdout, stderr, exited } = spawn({
+  const {
+    stdout: stdout1,
+    stderr: stderr1,
+    exited: exited1,
+  } = spawn({
     cmd: [bunExe(), "test.js"],
     cwd: run_dir,
     stdout: null,
@@ -69,9 +105,9 @@ for (const entry of await decompress(Buffer.from(buffer))) {
       BUN_INSTALL_CACHE_DIR: join(run_dir, ".cache"),
     },
   });
-  expect(stderr).toBeDefined();
-  const err = await new Response(stderr).text();
-  expect(err).toBe("");
+  expect(stderr1).toBeDefined();
+  const err1 = await new Response(stderr1).text();
+  expect(err1).toBe("");
   expect(await readdirSorted(run_dir)).toEqual([".cache", "test.js"]);
   expect(await readdirSorted(join(run_dir, ".cache"))).toContain("decompress");
   expect(await readdirSorted(join(run_dir, ".cache", "decompress"))).toEqual(["4.2.1"]);
@@ -84,13 +120,53 @@ for (const entry of await decompress(Buffer.from(buffer))) {
   expect(await file(join(run_dir, ".cache", "decompress", "4.2.1", "index.js")).text()).toContain(
     "\nmodule.exports = ",
   );
-  expect(stdout).toBeDefined();
-  const out = await new Response(stdout).text();
-  expect(out.split(/\r?\n/)).toEqual([
+  expect(stdout1).toBeDefined();
+  const out1 = await new Response(stdout1).text();
+  expect(out1.split(/\r?\n/)).toEqual([
     "directory: package/",
     "file: package/index.js",
     "file: package/package.json",
     "",
   ]);
-  expect(await exited).toBe(0);
+  expect(await exited1).toBe(0);
+  // Perform `bun run test.js` with cached dependencies
+  const {
+    stdout: stdout2,
+    stderr: stderr2,
+    exited: exited2,
+  } = spawn({
+    cmd: [bunExe(), "run", "test.js"],
+    cwd: run_dir,
+    stdout: null,
+    stdin: "pipe",
+    stderr: "pipe",
+    env: {
+      ...env,
+      BUN_INSTALL_CACHE_DIR: join(run_dir, ".cache"),
+    },
+  });
+  expect(stderr2).toBeDefined();
+  const err2 = await new Response(stderr2).text();
+  expect(err2).toBe("");
+  expect(await readdirSorted(run_dir)).toEqual([".cache", "test.js"]);
+  expect(await readdirSorted(join(run_dir, ".cache"))).toContain("decompress");
+  expect(await readdirSorted(join(run_dir, ".cache", "decompress"))).toEqual(["4.2.1"]);
+  expect(await readdirSorted(join(run_dir, ".cache", "decompress", "4.2.1"))).toEqual([
+    "index.js",
+    "license",
+    "package.json",
+    "readme.md",
+  ]);
+  expect(await file(join(run_dir, ".cache", "decompress", "4.2.1", "index.js")).text()).toContain(
+    "\nmodule.exports = ",
+  );
+  expect(stdout2).toBeDefined();
+  const out2 = await new Response(stdout2).text();
+  expect(out2.split(/\r?\n/)).toEqual([
+    "directory: package/",
+    "file: package/index.js",
+    "file: package/package.json",
+    "",
+  ]);
+  expect(await exited2).toBe(0);
 });

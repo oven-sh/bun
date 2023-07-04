de82004cd8e1eb8ca0c77aeee4ac112259fd0d30
Ashcon Partovi <ashcon@partovi.net>
2023-07-04 09:59:25 -0700

    Run prettier

--- test/js/first_party/utf-8-validate/utf-8-validate.test.ts
+++ test/js/first_party/utf-8-validate/utf-8-validate.test.ts
@@ -4,6 +4,6 @@ import isValidUTF8 from "utf-8-validate";
 
 test("utf-8-validate", () => {
   expect(isValidUTF8).toBe(isUtf8);
-  expect(isValidUTF8(Buffer.from("😀"))).toBeTrue()
+  expect(isValidUTF8(Buffer.from("😀"))).toBeTrue();
   expect(isValidUTF8(Buffer.from([0xff]))).toBeFalse();
 });

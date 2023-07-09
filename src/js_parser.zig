371316e64979a8f72f978ea63418996be82c4179
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-07-08 17:10:56 -0700

    Handle case with TS decorators and export default anonymous class

--- src/js_parser.zig
+++ src/js_parser.zig
@@ -18146,10 +18146,20 @@ fn NewParser_(
                                         data.default_name = createDefaultName(p, stmt.loc) catch unreachable;
                                     }
 
+                                    // We only inject a name into classes when there is a decorator
+                                    if (class.class.has_decorators) {
+                                        if (class.class.class_name == null or
+                                            class.class.class_name.?.ref == null)
+                                        {
+                                            class.class.class_name = data.default_name;
+                                        }
+                                    }
+
+                                    // This is to handle TS decorators, mostly.
                                     var class_stmts = p.lowerClass(.{ .stmt = s2 });
+                                    std.debug.assert(class_stmts[0].data == .s_class);
 
                                     if (class_stmts.len > 1) {
-                                        std.debug.assert(class_stmts[0].data == .s_class);
                                         data.value.stmt = class_stmts[0];
                                         stmts.append(stmt.*) catch {};
                                         stmts.appendSlice(class_stmts[1..]) catch {};
---
+++ test/transpiler/export-default-with-static-initializer.js
@@ -0,0 +1,5 @@
+export default class {
+  static {
+    this.boop = "boop";
+  }
+}

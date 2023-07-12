618d871024166c0e776cc2831d235954b1f0ee9c
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-07-11 20:57:23 -0700

    Fix another crash in Error.captureStackTrace

--- src/bun.js/bindings/ZigGlobalObject.h
+++ src/bun.js/bindings/ZigGlobalObject.h
@@ -166,7 +166,7 @@ public:
 
     void clearDOMGuardedObjects();
 
-    static void createCallSitesFromFrames(JSC::JSGlobalObject* lexicalGlobalObject, JSC::ObjectInitializationScope& objectScope, JSCStackTrace& stackTrace, JSC::JSArray* callSites);
+    static void createCallSitesFromFrames(JSC::JSGlobalObject* lexicalGlobalObject, JSCStackTrace& stackTrace, JSC::JSArray* callSites);
     JSC::JSValue formatStackTrace(JSC::VM& vm, JSC::JSGlobalObject* lexicalGlobalObject, JSC::JSObject* errorObject, JSC::JSArray* callSites);
 
     static void reportUncaughtExceptionAtEventLoop(JSGlobalObject*, JSC::Exception*);

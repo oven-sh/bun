7c5acd3ada92b8b6d68c73b428a3bc65b7bb09f6
Tiramify <tiramify@gmail.com>
2023-07-13 22:28:43 +0200

    Impl. fix

--- test/cli/run/run-process-env.test.ts
+++ test/cli/run/run-process-env.test.ts
@@ -1,16 +1,31 @@
 import { describe, expect, test } from "bun:test";
-import { bunRunAsScript, tempDirWithFiles } from "harness";
+import { bunExe, bunRunAsScript, tempDirWithFiles } from "harness";
 
 describe("process.env", () => {
   test("npm_lifecycle_event", () => {
     const scriptName = "start:dev";
 
     const dir = tempDirWithFiles("processenv", {
-      "package.json": `{'scripts': {'${scriptName}': 'bun run index.ts'}}`,
+      "package.json": `{'scripts': {'${scriptName}': '${bunExe()} run index.ts'}}`,
       "index.ts": "console.log(process.env.npm_lifecycle_event);",
     });
 
     const { stdout } = bunRunAsScript(dir, scriptName);
     expect(stdout).toBe(scriptName);
+  });
+
+  // https://github.com/oven-sh/bun/issues/3589
+  test('npm_lifecycle_event should have the value of the last call', () => {
+    const dir = tempDirWithFiles("processenv_ls_call", {
+      "package.json": `{"scripts": { "first": "${bunExe()} run --cwd lsc second" } }`,
+      "lsc": {
+        "package.json": `{"scripts": { "second": "${bunExe()} run index.ts" } }`,
+        "index.ts": "console.log(process.env.npm_lifecycle_event);",
+      }
+    });
+
+
+    const { stdout } = bunRunAsScript(dir, "first");
+    expect(stdout).toBe("second");
   });
 });

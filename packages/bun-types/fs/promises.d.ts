251183fd0214266c273265039eb04782496854f6
Colin McDonnell <colinmcd94@gmail.com>
2023-07-13 17:36:39 -0700

    Fix types

--- packages/bun-types/fs/promises.d.ts
+++ packages/bun-types/fs/promises.d.ts
@@ -27,6 +27,7 @@ declare module "fs/promises" {
     RmOptions,
     RmDirOptions,
     WatchOptions,
+    WatchEventType,
   } from "node:fs";
 
   const constants: typeof import("node:fs")["constants"];
@@ -711,62 +712,74 @@ declare module "fs/promises" {
    */
   function rmdir(path: PathLike, options?: RmDirOptions): Promise<void>;
 
+  interface FileChangeInfo<T extends string | Buffer> {
+    eventType: WatchEventType;
+    filename: T;
+  }
   /**
-     * Returns an async iterator that watches for changes on `filename`, where `filename`is either a file or a directory.
-     *
-     * ```js
-     * const { watch } = require('node:fs/promises');
-     *
-     * const ac = new AbortController();
-     * const { signal } = ac;
-     * setTimeout(() => ac.abort(), 10000);
-     *
-     * (async () => {
-     *   try {
-     *     const watcher = watch(__filename, { signal });
-     *     for await (const event of watcher)
-     *       console.log(event);
-     *   } catch (err) {
-     *     if (err.name === 'AbortError')
-     *       return;
-     *     throw err;
-     *   }
-     * })();
-     * ```
-     *
-     * On most platforms, `'rename'` is emitted whenever a filename appears or
-     * disappears in the directory.
-     *
-     * All the `caveats` for `fs.watch()` also apply to `fsPromises.watch()`.
-     * @since v0.6.8
-     * @return of objects with the properties:
-     */
-      function watch(
-        filename: PathLike,
-        options:
-            | (WatchOptions & {
-                  encoding: 'buffer';
-              })
-            | 'buffer'
-    ): AsyncIterable<FileChangeInfo<Buffer>>;
-    /**
-     * Watch for changes on `filename`, where `filename` is either a file or a directory, returning an `FSWatcher`.
-     * @param filename A path to a file or directory. If a URL is provided, it must use the `file:` protocol.
-     * @param options Either the encoding for the filename provided to the listener, or an object optionally specifying encoding, persistent, and recursive options.
-     * If `encoding` is not supplied, the default of `'utf8'` is used.
-     * If `persistent` is not supplied, the default of `true` is used.
-     * If `recursive` is not supplied, the default of `false` is used.
-     */
-    function watch(filename: PathLike, options?: WatchOptions | BufferEncoding): AsyncIterable<FileChangeInfo<string>>;
-    /**
-     * Watch for changes on `filename`, where `filename` is either a file or a directory, returning an `FSWatcher`.
-     * @param filename A path to a file or directory. If a URL is provided, it must use the `file:` protocol.
-     * @param options Either the encoding for the filename provided to the listener, or an object optionally specifying encoding, persistent, and recursive options.
-     * If `encoding` is not supplied, the default of `'utf8'` is used.
-     * If `persistent` is not supplied, the default of `true` is used.
-     * If `recursive` is not supplied, the default of `false` is used.
-     */
-    function watch(filename: PathLike, options: WatchOptions | string): AsyncIterable<FileChangeInfo<string>> | AsyncIterable<FileChangeInfo<Buffer>>;
+   * Returns an async iterator that watches for changes on `filename`, where `filename`is either a file or a directory.
+   *
+   * ```js
+   * const { watch } = require('node:fs/promises');
+   *
+   * const ac = new AbortController();
+   * const { signal } = ac;
+   * setTimeout(() => ac.abort(), 10000);
+   *
+   * (async () => {
+   *   try {
+   *     const watcher = watch(__filename, { signal });
+   *     for await (const event of watcher)
+   *       console.log(event);
+   *   } catch (err) {
+   *     if (err.name === 'AbortError')
+   *       return;
+   *     throw err;
+   *   }
+   * })();
+   * ```
+   *
+   * On most platforms, `'rename'` is emitted whenever a filename appears or
+   * disappears in the directory.
+   *
+   * All the `caveats` for `fs.watch()` also apply to `fsPromises.watch()`.
+   * @since v0.6.8
+   * @return of objects with the properties:
+   */
+  function watch(
+    filename: PathLike,
+    options:
+      | (WatchOptions & {
+          encoding: "buffer";
+        })
+      | "buffer",
+  ): AsyncIterable<FileChangeInfo<Buffer>>;
+  /**
+   * Watch for changes on `filename`, where `filename` is either a file or a directory, returning an `FSWatcher`.
+   * @param filename A path to a file or directory. If a URL is provided, it must use the `file:` protocol.
+   * @param options Either the encoding for the filename provided to the listener, or an object optionally specifying encoding, persistent, and recursive options.
+   * If `encoding` is not supplied, the default of `'utf8'` is used.
+   * If `persistent` is not supplied, the default of `true` is used.
+   * If `recursive` is not supplied, the default of `false` is used.
+   */
+  function watch(
+    filename: PathLike,
+    options?: WatchOptions | BufferEncoding,
+  ): AsyncIterable<FileChangeInfo<string>>;
+  /**
+   * Watch for changes on `filename`, where `filename` is either a file or a directory, returning an `FSWatcher`.
+   * @param filename A path to a file or directory. If a URL is provided, it must use the `file:` protocol.
+   * @param options Either the encoding for the filename provided to the listener, or an object optionally specifying encoding, persistent, and recursive options.
+   * If `encoding` is not supplied, the default of `'utf8'` is used.
+   * If `persistent` is not supplied, the default of `true` is used.
+   * If `recursive` is not supplied, the default of `false` is used.
+   */
+  function watch(
+    filename: PathLike,
+    options: WatchOptions | string,
+  ):
+    | AsyncIterable<FileChangeInfo<string>>
+    | AsyncIterable<FileChangeInfo<Buffer>>;
 }
 
 declare module "node:fs/promises" {

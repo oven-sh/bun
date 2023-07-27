86633e0af47dc9ff62e9cef23962a855d061bde8
Jarred Sumner <jarred@jarredsumner.com>
2023-07-26 21:35:49 -0700

    Start time performance improvements to build tools (#3797)
    * Make os.cpus() faster on Linux
    
    * Fix crash
    
    See https://github.com/ziglang/zig/issues/16540
    
    * Handle watcher_count == 0
    
    * Add assertion
    
    * Clean up lifetimes of fs watcher a little
    
    * :scissors:
    
    * Use `errdefer`
    
    * Make the error better
    
    * Make os.cpus() more lazy
    
    * Please don't translate-c on the entire C standard library
    
    * immediately closing works correctly is still bug
    
    * ops
    
    * fmt+fixeup
    
    * add back verbose
    
    * free instead of destroy
    
    * remove destroy option for watcher tasks
    
    * flush verbose and add debug log
    
    * fixup files
    
    * use log for debug
    
    ---------
    
    Co-authored-by: Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
    Co-authored-by: cirospaciari <ciro.spaciari@gmail.com>

--- test/js/node/watch/fixtures/relative.js
+++ test/js/node/watch/fixtures/relative.js
@@ -1,23 +1,28 @@
 import fs from "fs";
-const watcher = fs.watch("relative.txt", { signal: AbortSignal.timeout(2000) });
+try {
+  const watcher = fs.watch("relative.txt", { signal: AbortSignal.timeout(2000) });
 
-watcher.on("change", function (event, filename) {
-  if (filename !== "relative.txt" && event !== "change") {
-    console.error("fail");
+  watcher.on("change", function (event, filename) {
+    if (filename !== "relative.txt" && event !== "change") {
+      console.error("fail");
+      clearInterval(interval);
+      watcher.close();
+      process.exit(1);
+    } else {
+      clearInterval(interval);
+      watcher.close();
+    }
+  });
+  watcher.on("error", err => {
     clearInterval(interval);
-    watcher.close();
+    console.error(err.message);
     process.exit(1);
-  } else {
-    clearInterval(interval);
-    watcher.close();
-  }
-});
-watcher.on("error", err => {
-  clearInterval(interval);
+  });
+
+  const interval = setInterval(() => {
+    fs.writeFileSync("relative.txt", "world");
+  }, 10);
+} catch (err) {
   console.error(err.message);
   process.exit(1);
-});
-
-const interval = setInterval(() => {
-  fs.writeFileSync("relative.txt", "world");
-}, 10);
+}

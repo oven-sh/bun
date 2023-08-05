a4d1a19a9d16c52d6b2ca94fe24b2d84057e84b1
dave caruso <me@paperdave.net>
2023-08-04 22:23:26 -0700

    Implement -t

--- src/js_printer.zig
+++ src/js_printer.zig
@@ -340,7 +340,6 @@ pub fn writeJSONString(input: []const u8, comptime Writer: type, writer: Writer,
             } else break :brk strings.latin1ToCodepointAssumeNotASCII(char, i32);
         };
         if (canPrintWithoutEscape(i32, c, false)) {
-            const remain = text[@as(usize, width)..];
             if (encoding != .utf8 and width > 0) {
                 var codepoint_bytes: [4]u8 = undefined;
                 std.mem.writeIntNative(i32, &codepoint_bytes, c);
@@ -349,12 +348,12 @@ pub fn writeJSONString(input: []const u8, comptime Writer: type, writer: Writer,
                 );
             }
 
-            if (strings.indexOfNeedsEscape(remain)) |j| {
-                try writer.writeAll(remain[0..j]);
-                text = remain[j..];
+            if (strings.indexOfNeedsEscape(text)) |j| {
+                try writer.writeAll(text[0..j]);
+                text = text[j..];
                 continue;
             } else {
-                try writer.writeAll(remain);
+                try writer.writeAll(text);
                 break;
             }
         }

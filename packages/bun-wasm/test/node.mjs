4e8e334de4966d20b5d6218bc740d72385a0173b
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-08-07 06:25:20 -0700

    Update node.mjs

--- packages/bun-wasm/test/node.mjs
+++ packages/bun-wasm/test/node.mjs
@@ -1,5 +1,5 @@
 import { readFileSync } from "fs";
-import { init, getTests } from "../";
+import { init, getTests } from "../index.mjs";
 
 const buf = (process.argv.length > 2 ? readFileSync(process.argv.at(-1)) : "") || readFileSync(import.meta.url);
 await init(new URL("../bun.wasm", import.meta.url));

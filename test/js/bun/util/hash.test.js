c0052c153a938c14b148977e2cbabf6a4a28da9c
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2023-08-08 02:28:48 -0300

    update Bun.hash tests * properly test the returned hash * include murmur32v2

--- test/js/bun/util/hash.test.js
+++ test/js/bun/util/hash.test.js
@@ -1,47 +1,50 @@
-import fs from "fs";
 import { it, expect } from "bun:test";
-import path from "path";
 import { gcTick } from "harness";
 
 it(`Bun.hash()`, () => {
   gcTick();
-  Bun.hash("hello world");
-  Bun.hash(new TextEncoder().encode("hello world"));
+  expect(Bun.hash("hello world")).toBe(0x668D5E431C3B2573n);
+  expect(Bun.hash(new TextEncoder().encode("hello world"))).toBe(0x668D5E431C3B2573n);
 });
 it(`Bun.hash.wyhash()`, () => {
-  Bun.hash.wyhash("hello world");
+  expect(Bun.hash.wyhash("hello world")).toBe(0x668D5E431C3B2573n);
   gcTick();
-  Bun.hash.wyhash(new TextEncoder().encode("hello world"));
+  expect(Bun.hash.wyhash(new TextEncoder().encode("hello world"))).toBe(0x668D5E431C3B2573n);
 });
 it(`Bun.hash.adler32()`, () => {
-  Bun.hash.adler32("hello world");
+  expect(Bun.hash.adler32("hello world")).toBe(0x1A0B045D);
   gcTick();
-  Bun.hash.adler32(new TextEncoder().encode("hello world"));
+  expect(Bun.hash.adler32(new TextEncoder().encode("hello world"))).toBe(0x1A0B045D);
 });
 it(`Bun.hash.crc32()`, () => {
-  Bun.hash.crc32("hello world");
+  expect(Bun.hash.crc32("hello world")).toBe(0x0D4A1185);
   gcTick();
-  Bun.hash.crc32(new TextEncoder().encode("hello world"));
+  expect(Bun.hash.crc32(new TextEncoder().encode("hello world"))).toBe(0x0D4A1185);
 });
 it(`Bun.hash.cityHash32()`, () => {
-  Bun.hash.cityHash32("hello world");
+  expect(Bun.hash.cityHash32("hello world")).toBe(0x19A7581A);
   gcTick();
-  Bun.hash.cityHash32(new TextEncoder().encode("hello world"));
+  expect(Bun.hash.cityHash32(new TextEncoder().encode("hello world"))).toBe(0x19A7581A);
   gcTick();
 });
 it(`Bun.hash.cityHash64()`, () => {
-  Bun.hash.cityHash64("hello world");
+  expect(Bun.hash.cityHash64("hello world")).toBe(0xC7920BBDBECEE42Fn);
   gcTick();
-  Bun.hash.cityHash64(new TextEncoder().encode("hello world"));
+  expect(Bun.hash.cityHash64(new TextEncoder().encode("hello world"))).toBe(0xC7920BBDBECEE42Fn);
   gcTick();
 });
 it(`Bun.hash.murmur32v3()`, () => {
-  Bun.hash.murmur32v3("hello world");
+  expect(Bun.hash.murmur32v3("hello world")).toBe(0x5E928F0F);
   gcTick();
-  Bun.hash.murmur32v3(new TextEncoder().encode("hello world"));
+  expect(Bun.hash.murmur32v3(new TextEncoder().encode("hello world"))).toBe(0x5E928F0F);
+});
+it(`Bun.hash.murmur32v2()`, () => {
+  expect(Bun.hash.murmur32v2("hello world")).toBe(0x44A81419);
+  gcTick();
+  expect(Bun.hash.murmur32v2(new TextEncoder().encode("hello world"))).toBe(0x44A81419);
 });
 it(`Bun.hash.murmur64v2()`, () => {
-  Bun.hash.murmur64v2("hello world");
+  expect(Bun.hash.murmur64v2("hello world")).toBe(0xD3BA2368A832AFCEn);
   gcTick();
-  Bun.hash.murmur64v2(new TextEncoder().encode("hello world"));
+  expect(Bun.hash.murmur64v2(new TextEncoder().encode("hello world"))).toBe(0xD3BA2368A832AFCEn);
 });

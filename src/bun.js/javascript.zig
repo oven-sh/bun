987411e19ad58db5d36dbcdda07da9f6046d7182
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-08-18 14:48:55 -0700

    Delete code for `bun dev` HTTP JS

--- src/bun.js/javascript.zig
+++ src/bun.js/javascript.zig
@@ -379,7 +379,6 @@ pub const VirtualMachine = struct {
     bun_watcher: ?*JSC.Watcher = null,
     console: *ZigConsoleClient,
     log: *logger.Log,
-    event_listeners: EventListenerMixin.Map,
     main: string = "",
     main_hash: u32 = 0,
     process: js.JSObjectRef = null,
@@ -921,7 +920,6 @@ pub const VirtualMachine = struct {
             .transpiler_store = RuntimeTranspilerStore.init(allocator),
             .allocator = allocator,
             .entry_point = ServerEntryPoint{},
-            .event_listeners = EventListenerMixin.Map.init(allocator),
             .bundler = bundler,
             .console = console,
             .log = log,
@@ -1030,7 +1028,6 @@ pub const VirtualMachine = struct {
             .transpiler_store = RuntimeTranspilerStore.init(allocator),
             .allocator = allocator,
             .entry_point = ServerEntryPoint{},
-            .event_listeners = EventListenerMixin.Map.init(allocator),
             .bundler = bundler,
             .console = console,
             .log = log,
@@ -1152,7 +1149,6 @@ pub const VirtualMachine = struct {
             .allocator = allocator,
             .transpiler_store = RuntimeTranspilerStore.init(allocator),
             .entry_point = ServerEntryPoint{},
-            .event_listeners = EventListenerMixin.Map.init(allocator),
             .bundler = bundler,
             .console = console,
             .log = log,
@@ -2611,168 +2607,6 @@ pub const VirtualMachine = struct {
     }
 };
 
-const GetterFn = *const fn (
-    this: anytype,
-    ctx: js.JSContextRef,
-    thisObject: js.JSValueRef,
-    prop: js.JSStringRef,
-    exception: js.ExceptionRef,
-) js.JSValueRef;
-const SetterFn = *const fn (
-    this: anytype,
-    ctx: js.JSContextRef,
-    thisObject: js.JSValueRef,
-    prop: js.JSStringRef,
-    value: js.JSValueRef,
-    exception: js.ExceptionRef,
-) js.JSValueRef;
-
-const JSProp = struct {
-    get: ?GetterFn = null,
-    set: ?SetterFn = null,
-    ro: bool = false,
-};
-
-pub const EventListenerMixin = struct {
-    threadlocal var event_listener_names_buf: [128]u8 = undefined;
-    pub const List = std.ArrayList(js.JSObjectRef);
-    pub const Map = std.AutoHashMap(EventListenerMixin.EventType, EventListenerMixin.List);
-
-    pub const EventType = enum {
-        fetch,
-        err,
-
-        const SizeMatcher = strings.ExactSizeMatcher(8);
-
-        pub fn match(str: string) ?EventType {
-            return switch (SizeMatcher.match(str)) {
-                SizeMatcher.case("fetch") => EventType.fetch,
-                SizeMatcher.case("error") => EventType.err,
-                else => null,
-            };
-        }
-    };
-
-    pub fn emitFetchEvent(
-        vm: *VirtualMachine,
-        request_context: *http.RequestContext,
-        comptime CtxType: type,
-        ctx: *CtxType,
-        comptime onError: fn (ctx: *CtxType, err: anyerror, value: JSValue, request_ctx: *http.RequestContext) anyerror!void,
-    ) !void {
-        JSC.markBinding(@src());
-
-        var listeners = vm.event_listeners.get(EventType.fetch) orelse (return onError(ctx, error.NoListeners, JSValue.jsUndefined(), request_context) catch {});
-        if (listeners.items.len == 0) return onError(ctx, error.NoListeners, JSValue.jsUndefined(), request_context) catch {};
-        const FetchEventRejectionHandler = struct {
-            pub fn onRejection(_ctx: *anyopaque, err: anyerror, fetch_event: *FetchEvent, value: JSValue) void {
-                onError(
-                    @as(*CtxType, @ptrFromInt(@intFromPtr(_ctx))),
-                    err,
-                    value,
-                    fetch_event.request_context.?,
-                ) catch {};
-            }
-        };
-
-        // Rely on JS finalizer
-        var fetch_event = try vm.allocator.create(FetchEvent);
-
-        fetch_event.* = FetchEvent{
-            .request_context = request_context,
-            .request = try Request.fromRequestContext(request_context),
-            .onPromiseRejectionCtx = @as(*anyopaque, ctx),
-            .onPromiseRejectionHandler = FetchEventRejectionHandler.onRejection,
-        };
-
-        var fetch_args: [1]js.JSObjectRef = undefined;
-        fetch_args[0] = FetchEvent.Class.make(vm.global, fetch_event);
-        JSC.C.JSValueProtect(vm.global, fetch_args[0]);
-        defer JSC.C.JSValueUnprotect(vm.global, fetch_args[0]);
-
-        for (listeners.items) |listener_ref| {
-            vm.tick();
-            var result = js.JSObjectCallAsFunctionReturnValue(vm.global, JSValue.fromRef(listener_ref), JSValue.zero, 1, &fetch_args);
-            vm.tick();
-            var promise = JSInternalPromise.resolvedPromise(vm.global, result);
-
-            vm.event_loop.waitForPromise(JSC.AnyPromise{
-                .Internal = promise,
-            });
-
-            if (fetch_event.rejected) return;
-
-            if (promise.status(vm.global.vm()) == .Rejected) {
-                onError(ctx, error.JSError, promise.result(vm.global.vm()), request_context) catch {};
-                return;
-            }
-
-            _ = promise.result(vm.global.vm());
-
-            vm.waitForTasks();
-
-            if (request_context.has_called_done) {
-                break;
-            }
-        }
-
-        if (!request_context.has_called_done) {
-            onError(ctx, error.FetchHandlerRespondWithNeverCalled, JSValue.jsUndefined(), request_context) catch {};
-            return;
-        }
-    }
-
-    pub fn addEventListener(
-        comptime Struct: type,
-    ) type {
-        const Handler = struct {
-            pub fn addListener(
-                ctx: js.JSContextRef,
-                _: js.JSObjectRef,
-                _: js.JSObjectRef,
-                argumentCount: usize,
-                _arguments: [*c]const js.JSValueRef,
-                _: js.ExceptionRef,
-            ) callconv(.C) js.JSValueRef {
-                const arguments = _arguments[0..argumentCount];
-                if (arguments.len == 0 or arguments.len == 1 or !js.JSValueIsString(ctx, arguments[0]) or !js.JSValueIsObject(ctx, arguments[arguments.len - 1]) or !js.JSObjectIsFunction(ctx, arguments[arguments.len - 1])) {
-                    return js.JSValueMakeUndefined(ctx);
-                }
-                var name_slice = JSValue.c(arguments[0]).toSlice(ctx, ctx.allocator());
-                defer name_slice.deinit();
-                const name = name_slice.slice();
-                const event = EventType.match(name) orelse return js.JSValueMakeUndefined(ctx);
-                var entry = VirtualMachine.get().event_listeners.getOrPut(event) catch unreachable;
-
-                if (!entry.found_existing) {
-                    entry.value_ptr.* = List.initCapacity(VirtualMachine.get().allocator, 1) catch unreachable;
-                }
-
-                var callback = arguments[arguments.len - 1];
-                js.JSValueProtect(ctx, callback);
-                entry.value_ptr.append(callback) catch unreachable;
-
-                return js.JSValueMakeUndefined(ctx);
-            }
-        };
-
-        return NewClass(
-            Struct,
-            .{
-                .name = "addEventListener",
-                .read_only = true,
-            },
-            .{
-                .callAsFunction = .{
-                    .rfn = Handler.addListener,
-                },
-            },
-            .{},
-        );
-    }
-};
-
-pub const JSPrivateDataTag = JSPrivateDataPtr.Tag;
 pub const HotReloader = NewHotReloader(VirtualMachine, JSC.EventLoop, false);
 pub const Watcher = HotReloader.Watcher;
 

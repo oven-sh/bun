dacd00d7082fede526867f145dd36456143ee263
Ashcon Partovi <ashcon@partovi.net>
2023-08-24 19:30:55 -0700

    Improve preview code

--- packages/bun-debug-adapter-protocol/debugger/adapter.ts
+++ packages/bun-debug-adapter-protocol/debugger/adapter.ts
@@ -6,6 +6,7 @@ import type { ChildProcess } from "node:child_process";
 import { spawn, spawnSync } from "node:child_process";
 import capabilities from "./capabilities";
 import { Location, SourceMap } from "./sourcemap";
+import { remoteObjectToString } from "./preview";
 import { compare, parse } from "semver";
 
 type InitializeRequest = DAP.InitializeRequest & {
@@ -1519,111 +1520,6 @@ function sourceToId(source?: DAP.Source): string | number {
 function callFrameToId(callFrame: JSC.Console.CallFrame): string {
   const { url, lineNumber, columnNumber } = callFrame;
   return `${url}:${lineNumber}:${columnNumber}`;
-}
-
-function remoteObjectToString(remoteObject: JSC.Runtime.RemoteObject): string {
-  const { type, subtype, value, description, className, preview } = remoteObject;
-  switch (type) {
-    case "undefined":
-      return "undefined";
-    case "boolean":
-    case "string":
-      return JSON.stringify(value ?? description);
-    case "number":
-      return description ?? JSON.stringify(value);
-    case "symbol":
-    case "bigint":
-      return description!;
-    case "function":
-      return description!.replace("function", "ƒ") || "ƒ";
-  }
-  switch (subtype) {
-    case "null":
-      return "null";
-    case "regexp":
-    case "date":
-    case "error":
-      return description!;
-  }
-  if (preview) {
-    return objectPreviewToString(preview);
-  }
-  if (className) {
-    return className;
-  }
-  return description || "Object";
-}
-
-function objectPreviewToString(objectPreview: JSC.Runtime.ObjectPreview): string {
-  const { type, subtype, entries, properties, overflow, description, size } = objectPreview;
-  if (type !== "object") {
-    return remoteObjectToString(objectPreview);
-  }
-  let items: string[];
-  if (entries) {
-    items = entries.map(entryPreviewToString);
-  } else if (properties) {
-    if (isIndexed(subtype)) {
-      items = properties.map(indexedPropertyPreviewToString);
-    } else {
-      items = properties.map(namedPropertyPreviewToString);
-    }
-  } else {
-    items = ["…"];
-  }
-  if (overflow) {
-    items.push("…");
-  }
-  let label: string;
-  if (description === "Object") {
-    label = "";
-  } else if (size === undefined) {
-    label = description!;
-  } else {
-    label = `${description}(${size})`;
-  }
-  if (!items.length) {
-    return label || "{}";
-  }
-  if (label) {
-    label += " ";
-  }
-  if (isIndexed(subtype)) {
-    return `${label}[${items.join(", ")}]`;
-  }
-  return `${label}{${items.join(", ")}}`;
-}
-
-function propertyPreviewToString(propertyPreview: JSC.Runtime.PropertyPreview): string {
-  const { type, value, ...preview } = propertyPreview;
-  if (type === "accessor") {
-    return "ƒ";
-  }
-  return remoteObjectToString({ ...preview, type, description: value });
-}
-
-function entryPreviewToString(entryPreview: JSC.Runtime.EntryPreview): string {
-  const { key, value } = entryPreview;
-  if (key) {
-    return `${objectPreviewToString(key)} => ${objectPreviewToString(value)}`;
-  }
-  return objectPreviewToString(value);
-}
-
-function namedPropertyPreviewToString(propertyPreview: JSC.Runtime.PropertyPreview): string {
-  const { name, valuePreview } = propertyPreview;
-  if (valuePreview) {
-    return `${name}: ${objectPreviewToString(valuePreview)}`;
-  }
-  return `${name}: ${propertyPreviewToString(propertyPreview)}`;
-}
-
-function indexedPropertyPreviewToString(propertyPreview: JSC.Runtime.PropertyPreview): string {
-  const { valuePreview } = propertyPreview;
-  if (valuePreview) {
-    return objectPreviewToString(valuePreview);
-  }
-  return propertyPreviewToString(propertyPreview);
 }
 
 function sanitizeExpression(expression: string): string {

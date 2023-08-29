5dfd16678790e8aac4aabd5cfaceb7471ea00d8f
dave caruso <me@paperdave.net>
2023-08-29 12:09:22 -0700

    add a message in the builtins bundler that 0.8 is needed. it breaks on older versions lol.

--- src/js/_codegen/index.ts
+++ src/js/_codegen/index.ts
@@ -1,2 +1,12 @@
+const [major, minor] = Bun.version
+  .split("_")[0]
+  .split(".")
+  .map(x => parseInt(x));
+
+if (minor < 8) {
+  console.error("Please install bun >= 0.8.0");
+  process.exit(1);
+}
+
 import "./build-modules";
 import "./build-functions";

e2bd07f524a4519b427b98c8919556e085288f79
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-08-29 18:07:42 -0700

    Clean up some of the event loop code

--- src/bun.js/javascript.zig
+++ src/bun.js/javascript.zig
@@ -479,7 +479,6 @@ pub const VirtualMachine = struct {
     active_tasks: usize = 0,
 
     rare_data: ?*JSC.RareData = null,
-    us_loop_reference_count: usize = 0,
     is_us_loop_entered: bool = false,
     pending_internal_promise: *JSC.JSInternalPromise = undefined,
     auto_install_dependencies: bool = false,
@@ -531,6 +530,12 @@ pub const VirtualMachine = struct {
 
     pub fn mimeType(this: *VirtualMachine, str: []const u8) ?bun.HTTP.MimeType {
         return this.rareData().mimeTypeFromString(this.allocator, str);
+    }
+
+    pub fn isEventLoopAlive(vm: *const VirtualMachine) bool {
+        return vm.active_tasks > 0 or
+            vm.event_loop_handle.?.active > 0 or
+            vm.event_loop.tasks.count > 0;
     }
 
     const SourceMapHandlerGetter = struct {
@@ -732,7 +737,7 @@ pub const VirtualMachine = struct {
         this.exit_handler.dispatchOnBeforeExit();
         var dispatch = false;
         while (true) {
-            while (this.eventLoop().tasks.count > 0 or this.active_tasks > 0 or this.event_loop_handle.?.active > 0) : (dispatch = true) {
+            while (this.isEventLoopAlive()) : (dispatch = true) {
                 this.tick();
                 this.eventLoop().autoTickActive();
             }
@@ -741,7 +746,7 @@ pub const VirtualMachine = struct {
                 this.exit_handler.dispatchOnBeforeExit();
                 dispatch = false;
 
-                if (this.eventLoop().tasks.count > 0 or this.active_tasks > 0 or this.event_loop_handle.?.active > 0) continue;
+                if (this.isEventLoopAlive()) continue;
             }
 
             break;
@@ -884,7 +889,7 @@ pub const VirtualMachine = struct {
             this.eventLoop().tick();
 
             while (true) {
-                while (this.eventLoop().tasks.count > 0 or this.active_tasks > 0 or this.event_loop_handle.?.active > 0) {
+                while (this.isEventLoopAlive()) {
                     this.tick();
                     this.eventLoop().autoTickActive();
                 }

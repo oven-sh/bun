5e2dc22980b5657cc94d0334683bd046956e4d3f
dave caruso <me@paperdave.net>
2023-09-03 14:46:32 -0700

    unwatchFile but lame

--- src/js/node/fs.js
+++ src/js/node/fs.js
@@ -78,27 +78,14 @@ class FSWatcher extends EventEmitter {
 
 class StatWatcher extends EventEmitter {
   // _handle: StatWatcherHandle;
-  #listener;
 
-  constructor(path, options, listener) {
+  constructor(path, options) {
     super();
-
-    if (typeof options === "function") {
-      listener = options;
-      options = {};
-    }
-
-    if (!$isCallable(listener)) {
-      throw new TypeError("listener must be a function");
-    }
-
-    this.#listener = listener;
     this._handle = fs.watchFile(path, options, this.#onChange.bind(this));
   }
 
   #onChange(curr, prev) {
     this.emit("change", curr, prev);
-    this.#listener(curr, prev);
   }
 
   // https://github.com/nodejs/node/blob/9f51c55a47702dc6a0ca3569853dd7ba022bf7bb/lib/internal/fs/watchers.js#L259-L263
@@ -365,10 +352,55 @@ var access = function access(...args) {
   Stats = fs.Stats,
   watch = function watch(path, options, listener) {
     return new FSWatcher(path, options, listener);
-  },
-  watchFile = function watchFile(filename, options, listener) {
-    return new StatWatcher(filename, options, listener);
   };
+
+// TODO: move this entire thing into native code.
+// the reason it's not done right now is because there isnt a great way to have multiple
+// listeners per StatWatcher with the current implementation in native code. the downside
+// of this means we need to do path validation in the js side of things
+const statWatchers = new Map();
+let _pathModule;
+function getValidatedPath(p) {
+  if (p instanceof URL) return Bun.fileURLToPath(p);
+  if (typeof p !== "string") throw new TypeError("Path must be a string or URL.");
+  return (_pathModule ??= require("node:path")).resolve(p);
+}
+function watchFile(filename, options, listener) {
+  filename = getValidatedPath(filename);
+
+  if (typeof options === "function") {
+    listener = options;
+    options = {};
+  }
+
+  if (typeof listener !== "function") {
+    throw new TypeError("listener must be a function");
+  }
+
+  var stat = statWatchers.get(filename);
+  if (!stat) {
+    stat = new StatWatcher(filename, options);
+    statWatchers.set(filename, stat);
+  }
+  stat.addListener("change", listener);
+  return stat;
+}
+function unwatchFile(filename, listener) {
+  filename = getValidatedPath(filename);
+
+  var stat = statWatchers.get(filename);
+  if (!stat) return;
+  if (listener) {
+    stat.removeListener("change", listener);
+    if (stat.listenerCount("change") !== 0) {
+      return;
+    }
+  } else {
+    stat.removeAllListeners("change");
+  }
+  stat.stop();
+  statWatchers.delete(filename);
+}
 
 function callbackify(fsFunction, args) {
   try {
@@ -1254,6 +1286,7 @@ export default {
   truncateSync,
   unlink,
   unlinkSync,
+  unwatchFile,
   utimes,
   utimesSync,
   watch,

3b9318794d3316d1392152a102916bb43cb68c6b
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-09-04 03:07:45 -0700

    Fix getenvZ

--- src/bun.zig
+++ src/bun.zig
@@ -753,6 +753,21 @@ pub fn getenvZ(path_: [:0]const u8) ?[]const u8 {
         return null;
     }
 
+    if (comptime Environment.isWindows) {
+        // Windows UCRT will fill this in for us
+        for (std.os.environ) |lineZ| {
+            const line = sliceTo(lineZ, 0);
+            const key_end = strings.indexOfCharUsize('=') orelse line.len;
+            const key = line[0..key_end];
+            if (strings.eqlLong(key, path_, true)) {
+                return line[@min(key_end + 1, line.len)..];
+            }
+            continue;
+        }
+
+        return null;
+    }
+
     const ptr = std.c.getenv(path_.ptr) orelse return null;
     return sliceTo(ptr, 0);
 }

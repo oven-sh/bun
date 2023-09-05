bc2b55fdeee3001b07252d0f250671ea1876a3ed
Alex Lam S.L <alexlamsl@gmail.com>
2023-09-05 19:16:11 +0300

    fix checkout/build failure due to `src/deps/uws` (#4505)

--- src/generated_versions_list.zig
+++ src/generated_versions_list.zig
@@ -2,13 +2,11 @@
 
 pub const boringssl = "b275c5ce1c88bc06f5a967026d3c0ce1df2be815";
 pub const libarchive = "dc321febde83dd0f31158e1be61a7aedda65e7a2";
-pub const mimalloc = "3c7079967a269027e438a2aac83197076d9fe09d";
+pub const mimalloc = "7968d4285043401bb36573374710d47a4081a063";
 pub const picohttpparser = "066d2b1e9ab820703db0837a7255d92d30f0c9f5";
-pub const uws = "70b1b9fc1341e8b791b42c5447f90505c2abe156";
-pub const webkit = "60d11703a533fd694cd1d6ddda04813eecb5d69f";
+pub const webkit = "a780bdf0255ae1a7ed15e4b3f31c14af705facae";
 pub const zig = @import("std").fmt.comptimePrint("{}", .{@import("builtin").zig_version});
 pub const zlib = "885674026394870b7e7a05b7bf1ec5eb7bd8a9c0";
 pub const tinycc = "2d3ad9e0d32194ad7fd867b66ebe218dcc8cb5cd";
-pub const lolhtml = "2681dcf0b3e6907111565199df8c43cc9aab7fe8";
+pub const lolhtml = "8d4c273ded322193d017042d1f48df2766b0f88b";
 pub const c_ares = "0e7a5dee0fbb04080750cf6eabbe89d8bae87faa";
-pub const usockets = "fafc241e8664243fc0c51d69684d5d02b9805134";

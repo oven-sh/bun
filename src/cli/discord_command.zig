5fe13cdaac984575311e2a3192e739e5d7850b26
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2023-09-06 23:55:25 -0300

    fix bun discord command

--- src/cli/discord_command.zig
+++ src/cli/discord_command.zig
@@ -14,6 +14,8 @@ const open = @import("../open.zig");
 pub const DiscordCommand = struct {
     const discord_url: string = "https://bun.sh/discord";
     pub fn exec(_: std.mem.Allocator) !void {
-        try open.openURL(discord_url);
+        open.openURL(discord_url) catch {
+            Output.println(discord_url, .{});
+        };
     }
 };

8aace266e69760c87771b97dd872587628975dbc
WingLim <winglims@gmail.com>
2023-09-09 00:29:58 +0800

    chore(cli): add cache remove test

--- test/cli/install/bun-pm.test.ts
+++ test/cli/install/bun-pm.test.ts
@@ -1,7 +1,7 @@
 import { spawn } from "bun";
 import { afterAll, afterEach, beforeAll, beforeEach, expect, it } from "bun:test";
 import { bunExe, bunEnv as env } from "harness";
-import { mkdir, writeFile } from "fs/promises";
+import { mkdir, writeFile, exists } from "fs/promises";
 import { join } from "path";
 import {
   dummyAfterAll,
@@ -10,6 +10,7 @@ import {
   dummyBeforeEach,
   dummyRegistry,
   package_dir,
+  readdirSorted,
   requested,
   root_url,
   setHandler,
@@ -244,4 +245,90 @@ it("should list aliased dependencies", async () => {
   expect(await exited).toBe(0);
   expect(urls.sort()).toEqual([]);
   expect(requested).toBe(2);
+});
+
+it("should remove all cache", async () => {
+  const urls: string[] = [];
+  setHandler(dummyRegistry(urls));
+  await writeFile(
+    join(package_dir, "package.json"),
+    JSON.stringify({
+      name: "foo",
+      version: "0.0.1",
+      dependencies: {
+        "moo-1": "./moo",
+      },
+    }),
+  );
+  await mkdir(join(package_dir, "moo"));
+  await writeFile(
+    join(package_dir, "moo", "package.json"),
+    JSON.stringify({
+      name: "moo",
+      version: "0.1.0",
+      dependencies: {
+        "bar-1": "npm:bar",
+      },
+    }),
+  );
+  let cache_dir: string = join(package_dir, "node_modules", ".cache");
+  expect(
+    await spawn({
+      cmd: [bunExe(), "install"],
+      cwd: package_dir,
+      stdout: null,
+      stdin: "pipe",
+      stderr: "pipe",
+      env: {
+        ...env,
+        BUN_INSTALL_CACHE_DIR: cache_dir,
+      },
+    }).exited,
+  ).toBe(0);
+  expect(urls.sort()).toEqual([`${root_url}/bar`, `${root_url}/bar-0.0.2.tgz`]);
+  expect(requested).toBe(2);
+  expect(await readdirSorted(cache_dir)).toContain("bar");
+
+  const {
+      stdout: stdout1,
+      stderr: stderr1,
+      exited: exited1
+  } = spawn({
+      cmd: [bunExe(), "pm", "cache"],
+      cwd: package_dir,
+      stdout: "pipe",
+      stdin: "pipe",
+      stderr: "pipe",
+      env: {
+          ...env,
+          BUN_INSTALL_CACHE_DIR: cache_dir,
+      }
+  });
+  expect(stderr1).toBeDefined();
+  expect(await new Response(stderr1).text()).toBe("");
+  expect(stdout1).toBeDefined();
+  expect(await new Response(stdout1).text()).toBe(join(cache_dir));
+  expect(await exited1).toBe(0);
+
+  const {
+    stdout: stdout2,
+    stderr: stderr2,
+    exited: exited2
+  } = spawn({
+      cmd: [bunExe(), "pm", "cache", "rm"],
+      cwd: package_dir,
+      stdout: "pipe",
+      stdin: "pipe",
+      stderr: "pipe",
+      env: {
+          ...env,
+          BUN_INSTALL_CACHE_DIR: cache_dir,
+      }
+  });
+  expect(stderr2).toBeDefined();
+  expect(await new Response(stderr2).text()).toBe("");
+  expect(stdout2).toBeDefined();
+  expect(await new Response(stdout2).text()).toBe("Cache directory deleted:\n  " + cache_dir + "\n");
+  expect(await exited2).toBe(0);
+  expect(await exists(cache_dir)).toBeFalse();
 });

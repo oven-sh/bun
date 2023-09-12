a6006962f770848d714f6dea345e4cce519c3cd1
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2023-09-11 17:39:02 -0700

    fix test

--- test/cli/run/empty-file.test.ts
+++ test/cli/run/empty-file.test.ts
@@ -3,12 +3,13 @@ import { bunEnv, bunExe } from "harness";
 import { join } from "path";
 
 it("should execute empty scripts", () => {
-  const { stdout, exitCode } = Bun.spawnSync({
+  const { stdout, stderr, exitCode } = Bun.spawnSync({
     cmd: [bunExe(), "run", "--bun", join(import.meta.dir, "empty-file.js")],
     env: bunEnv,
-    stderr: "inherit",
+    stdout: "pipe",
+    stderr: "pipe",
   });
-
-  expect(stdout.toString().trim().endsWith("--pass--")).toBe(true);
+  expect(stdout.toString()).toBeEmpty();
+  expect(stderr.toString()).toBeEmpty();
   expect(exitCode).toBe(0);
 });

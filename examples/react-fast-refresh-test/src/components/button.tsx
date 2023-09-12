4ddfeab95491558653a5af2b50ccc3571c9cb2e4
Michal Lipinski <michal.lipinski@gmail.com>
2023-09-12 13:36:58 +0200

    remove old broken example. fixed also old but kinda working example

--- examples/react-fast-refresh-test/src/components/button.tsx
+++
@@ -1,9 +0,0 @@
-import { RenderCounter } from "./RenderCounter";
-
-export const Button = ({ children }) => {
-  return (
-    <RenderCounter name="Button">
-      <div className="Button">{children}</div>
-    </RenderCounter>
-  );
-};

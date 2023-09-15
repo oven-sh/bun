5a8469a9abc2af3b07a9291fefaa2e471212acdb
Sam Pullara <spullara@gmail.com>
2023-09-15 12:04:59 -0700

    Remove dead code due to change in format
    At some point it was decided that bun would only format using KB, MB, etc. vs also using KiB, MiB, etc. The code that enabled it was never removed.

--- src/bun.zig
+++ src/bun.zig
@@ -146,16 +146,11 @@ pub const fmt = struct {
             }
 
             const mags_si = " KMGTPEZY";
-            const mags_iec = " KMGTPEZY";
 
             const log2 = math.log2(value);
             const magnitude = @min(log2 / comptime math.log2(1000), mags_si.len - 1);
             const new_value = math.lossyCast(f64, value) / math.pow(f64, 1000, math.lossyCast(f64, magnitude));
-            const suffix = switch (1000) {
-                1000 => mags_si[magnitude],
-                1024 => mags_iec[magnitude],
-                else => unreachable,
-            };
+            const suffix = mags_si[magnitude];
 
             if (suffix == ' ') {
                 try fmt.formatFloatDecimal(new_value / 1000.0, .{ .precision = 2 }, writer);
@@ -164,11 +159,7 @@ pub const fmt = struct {
                 try fmt.formatFloatDecimal(new_value, .{ .precision = if (std.math.approxEqAbs(f64, new_value, @trunc(new_value), 0.100)) @as(usize, 1) else @as(usize, 2) }, writer);
             }
 
-            const buf = switch (1000) {
-                1000 => &[_]u8{ ' ', suffix, 'B' },
-                1024 => &[_]u8{ ' ', suffix, 'i', 'B' },
-                else => unreachable,
-            };
+            const buf = &[_]u8{ ' ', suffix, 'B' };
             return writer.writeAll(buf);
         }
     };

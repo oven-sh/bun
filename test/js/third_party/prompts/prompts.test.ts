20f61b236980cf12dee0b0b80b04f605bd517581
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-09-15 04:10:05 -0700

    Fix test

--- test/js/third_party/prompts/prompts.test.ts
+++ test/js/third_party/prompts/prompts.test.ts
@@ -14,13 +14,12 @@ test("works with prompts", async () => {
   child.stdin.write("999\n");
   Bun.sleepSync(100);
   child.stdin.write("hi\n");
+  expect(await child.exited).toBe(0);
 
   var out = "";
   for await (const chunk of child.stdout) {
     out += new TextDecoder().decode(chunk);
   }
-
-  expect(await child.exited).toBe(0);
 
   expect(out).toContain('twitter: "@dylan"');
   expect(out).toContain("age: 999");

12a39e19fa83b9ed815f0e80942a3b0534e003c6
pan93412 <pan93412@gmail.com>
2023-09-17 17:34:19 +0800

    chore(js/timer): Add performance.now() == NaN test case

--- test/js/web/timers/performance.test.js
+++ test/js/web/timers/performance.test.js
@@ -20,3 +20,10 @@ it("performance.now() should be monotonic", () => {
 it("performance.timeOrigin + performance.now() should be similar to Date.now()", () => {
   expect(Math.abs(performance.timeOrigin + performance.now() - Date.now()) < 1000).toBe(true);
 });
+
+it("performance.now() should never return NaN", () => {
+  for (let i = 0; i < 10e6; i++) {
+    const now = performance.now()
+    expect(Number.isNaN(now)).toBe(false);
+  }
+});

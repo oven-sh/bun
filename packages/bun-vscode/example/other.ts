d43474fdcfd2fde57777e2459dfe4dfc86281a9c
Jeremy Funk <jeremy@kombo.dev>
2023-09-20 00:50:16 +0200

    Revert "Fix bugs"
    This reverts commit 608639eb2214fdf4310477051ce47d20702b5dd0.

--- packages/bun-vscode/example/other.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "bun:test";
-
-export function externalTest() {
-  test("it works2", () => {
-    expect(() => {
-      throw new AggregateError([new TypeError("Child error 1."), new TypeError("Child error 2.")], "Parent error.");
-    }).toThrow();
-  });
-}
--- packages/bun-vscode/src/features/test-explorer/index.ts
+++
@@ -1,5 +0,0 @@
-import * as vscode from 'vscode'
-
-export function registerTestExplorer(context: vscode.ExtensionContext) {
-    const controller = vscode.tests.createTestController('bun-tests', 'Bun Tests')
-}

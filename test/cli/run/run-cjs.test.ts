6c89df254216432ca74152504cc50f8e6a6df0a5
dave caruso <me@paperdave.net>
2023-09-19 13:26:41 -0700

    r

--- test/cli/run/run-cjs.test.ts
+++ test/cli/run/run-cjs.test.ts
@@ -7,7 +7,7 @@ import { join } from "path";
 test("running a commonjs module works", async () => {
   const dir = join(realpathSync(tmpdir()), "bun-run-test1");
   mkdirSync(dir, { recursive: true });
-  await Bun.write(join(dir, "index1.js"), "module.exports = 1; console.log('hello world');", { mode: 0o755 });
+  await Bun.write(join(dir, "index1.js"), "module.exports = 1; console.log('hello world');");
   let { stdout } = Bun.spawnSync({
     cmd: [bunExe(), join(dir, "index1.js")],
     cwd: dir,

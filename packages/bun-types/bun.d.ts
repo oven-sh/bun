82d2a3886ba12dfd49e8520313808b85190300e6
Colin McDonnell <colinmcd94@gmail.com>
2023-09-28 22:41:49 -0700

    Add Bun.TOML to types

--- packages/bun-types/bun.d.ts
+++ packages/bun-types/bun.d.ts
@@ -70,6 +70,19 @@ declare module "bun" {
     options?: { PATH?: string; cwd?: string },
   ): string | null;
 
+  interface TOML {
+    /**
+     * Parse a TOML string into a JavaScript object.
+     *
+     * @param {string} command The name of the executable or script
+     * @param {string} options.PATH Overrides the PATH environment variable
+     * @param {string} options.cwd Limits the search to a particular directory in which to searc
+     *
+     */
+    parse(input: string): object;
+  }
+  export const TOML: TOML;
+
   export type Serve<WebSocketDataType = undefined> =
     | ServeOptions
     | TLSServeOptions

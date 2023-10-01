cfda38583d4fac82ac80c8c56a6ca16bcbfc2cd9
mert.altin <mert.altin@trendyol.com>
2023-10-01 23:31:15 +0300

    feat: remove Buffer.from() when readFileSync() returns Buffer

--- bench/cat/cat.mjs
+++ bench/cat/cat.mjs
@@ -4,6 +4,18 @@ const count = parseInt(process.env.ITERATIONS || "1", 10) || 1;
 
 const arg = process.argv.slice(1);
 
-// TODO: remove Buffer.from() when readFileSync() returns Buffer
-
-for (let i = 0; i < count; i++) console.log(arg.map(file => Buffer.from(readFileSync(file, "utf8"))).join(""));
+for (let i = 0; i < count; i++) {
+    console.log(
+      arg
+        .map((file) => {
+          const fileContent = readFileSync(file);
+          if (fileContent instanceof Buffer) {
+            return fileContent.toString("utf8");
+          } else {
+            return fileContent;
+          }
+        })
+        .join("")
+    );
+  }
+  

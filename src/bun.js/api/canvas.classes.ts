07edcc426e5bd496912b86c7982bb39d47c62098
Dylan Conway <dylan.conway567@gmail.com>
2023-10-09 02:00:51 -0700

    rename

---
+++ src/bun.js/api/canvas.classes.ts
@@ -0,0 +1,73 @@
+import { define } from "../scripts/class-definitions";
+
+export default [
+  define({
+    name: "Canvas",
+    construct: true,
+    finalize: true,
+    hasPendingActivity: true,
+    configurable: false,
+    klass: {},
+    JSType: "0b11101110",
+    proto: {
+      width: {
+        getter: "getWidth",
+        setter: "setWidth",
+      },
+      height: {
+        getter: "getHeight",
+        setter: "setHeight",
+      },
+      x: {
+        getter: "getX",
+        setter: "setX",
+      },
+      y: {
+        getter: "getY",
+        setter: "setY",
+      },
+      animate: {
+        fn: "animate",
+        length: 1,
+      },
+      close: {
+        fn: "close",
+        length: 0,
+      },
+      getContext: {
+        fn: "getContext",
+        length: 1,
+      },
+    },
+  }),
+  define({
+    name: "CanvasRenderingContext2D",
+    construct: true,
+    finalize: false,
+    configurable: false,
+    klass: {},
+    JSType: "0b11101110",
+    proto: {
+      strokeStyle: {
+        getter: "getStrokeStyle",
+        setter: "setStrokeStyle",
+      },
+      fillStyle: {
+        getter: "getFillStyle",
+        setter: "setFillStyle",
+      },
+      clearRect: {
+        fn: "clearRect",
+        length: 4,
+      },
+      fillRect: {
+        fn: "fillRect",
+        length: 4,
+      },
+      strokeRect: {
+        fn: "strokeRect",
+        length: 4,
+      },
+    },
+  }),
+];

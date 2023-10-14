46f978838ddb4326d9ab089fbce50d47d829a60c
Dylan Conway <35280289+dylan-conway@users.noreply.github.com>
2023-10-13 20:37:06 -0700

    fix lockfile struct padding (#6495)
    * integrity padding
    
    * error message for bytes at end of struct

--- src/install/lockfile.zig
+++ src/install/lockfile.zig
@@ -4120,6 +4120,7 @@ pub const Package = extern struct {
 
         man_dir: String = String{},
         integrity: Integrity = Integrity{},
+        _padding_integrity: [3]u8 = .{0} ** 3,
 
         /// Does the `cpu` arch and `os` match the requirements listed in the package?
         /// This is completely unrelated to "devDependencies", "peerDependencies", "optionalDependencies" etc

0dc7d74493a552b1d60549a469ed765f7b55c218
Liz3 (Yann HN) <accs@liz3.net>
2023-10-17 03:33:10 +0200

    fix: implement changes requested in review

--- src/bun.js/bindings/BunObject.cpp
+++ src/bun.js/bindings/BunObject.cpp
@@ -506,13 +506,13 @@ JSC_DEFINE_HOST_FUNCTION(functionPathToFileURL, (JSC::JSGlobalObject * lexicalGl
     auto& globalObject = *reinterpret_cast<Zig::GlobalObject*>(lexicalGlobalObject);
     auto& vm = globalObject.vm();
     auto throwScope = DECLARE_THROW_SCOPE(vm);
-    auto path = JSC::JSValue::encode(callFrame->uncheckedArgument(0));
-    // i dont know a runtime way to check this, help
-    #ifdef _WIN32
+    auto path = JSC::JSValue::encode(callFrame->argument(0));
+    // NOTE(Liz3): this is supposed to be replaced with a runtime call at some point maybe.
+#if OS(WINDOWS)
     path = Bun__Path__resolve(lexicalGlobalObject, true, reinterpret_cast<JSC__JSValue*>(&path), 1);
-    #else
+#else
     path = Bun__Path__resolve(lexicalGlobalObject, false, reinterpret_cast<JSC__JSValue*>(&path), 1);
-    #endif
+#endif
 
     JSC::JSString* pathString = JSC::JSValue::decode(path).toString(lexicalGlobalObject);
     RETURN_IF_EXCEPTION(throwScope, JSC::JSValue::encode(JSC::jsUndefined()));

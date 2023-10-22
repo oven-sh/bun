fafd9368f5361d580a5942d99288f767b8c16cf9
Jarred Sumner <jarred@jarredsumner.com>
2023-10-22 13:44:08 -0700

    Backport uWS & usockets changes (#6649)
    Co-authored-by: Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>

--- packages/bun-usockets/src/internal/networking/bsd.h
+++ packages/bun-usockets/src/internal/networking/bsd.h
@@ -90,6 +90,7 @@ LIBUS_SOCKET_DESCRIPTOR bsd_accept_socket(LIBUS_SOCKET_DESCRIPTOR fd, struct bsd
 
 int bsd_recv(LIBUS_SOCKET_DESCRIPTOR fd, void *buf, int length, int flags);
 int bsd_send(LIBUS_SOCKET_DESCRIPTOR fd, const char *buf, int length, int msg_more);
+int bsd_write2(LIBUS_SOCKET_DESCRIPTOR fd, const char *header, int header_length, const char *payload, int payload_length);
 int bsd_would_block();
 
 // return LIBUS_SOCKET_ERROR or the fd that represents listen socket

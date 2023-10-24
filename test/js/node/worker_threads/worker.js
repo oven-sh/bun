71a7f7274961cf5769b507a345c3501370fa4e5b
Jérôme Benoit <jerome.benoit@sap.com>
2023-10-24 21:31:21 +0200

    test: revert wrong refactoring
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- test/js/node/worker_threads/worker.js
+++ test/js/node/worker_threads/worker.js
@@ -2,7 +2,7 @@ const wt= require("worker_threads");
 
 wt.parentPort.on("message", e => {
   let sharedBufferView = new Int32Array(e.sharedBuffer);
-  wt.parentPort.postMessage("done!");
+  wt.workerData.postMessage("done!");
   Atomics.add(sharedBufferView, 0, 1);
   Atomics.notify(sharedBufferView, 0, Infinity);
 });

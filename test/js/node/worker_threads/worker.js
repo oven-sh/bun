15132b0d6c7261dbb0608cde13381d78fe2b0b9d
Jérôme Benoit <jerome.benoit@sap.com>
2023-10-24 22:31:01 +0200

    test: cleanup worker_threads test
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- test/js/node/worker_threads/worker.js
+++ test/js/node/worker_threads/worker.js
@@ -1,8 +1,10 @@
 import { parentPort, workerData } from "worker_threads";
 
-parentPort.on("message", e => {
-  let sharedBufferView = new Int32Array(e.sharedBuffer);
-  workerData.postMessage("done!");
+parentPort.on("message", m => {
+  let sharedBufferView = new Int32Array(m.sharedBuffer);
+  if (workerData instanceof MessagePort) {
+    workerData.postMessage("done!");
+  }
   Atomics.add(sharedBufferView, 0, 1);
   Atomics.notify(sharedBufferView, 0, Infinity);
 });

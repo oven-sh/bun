df752c8ccee34b175d01e69a0b7156bacb701534
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-10-26 03:02:34 -0700

    Deflake test

--- test/js/bun/spawn/spawn-streaming-stdout.test.ts
+++ test/js/bun/spawn/spawn-streaming-stdout.test.ts
@@ -5,14 +5,11 @@ import { closeSync, openSync } from "fs";
 
 test("spawn can read from stdout multiple chunks", async () => {
   gcTick(true);
-  const maxFD = openSync("/dev/null", "w");
-  closeSync(maxFD);
-
-  for (let i = 0; i < 10; i++)
+  var maxFD: number = -1;
+  for (let i = 0; i < 100; i++) {
     await (async function () {
-      var exited;
       const proc = spawn({
-        cmd: [bunExe(), import.meta.dir + "/spawn-streaming-stdout-repro.js"],
+        cmd: ["bun", import.meta.dir + "/spawn-streaming-stdout-repro.js"],
         stdin: "ignore",
         stdout: "pipe",
         stderr: "ignore",
@@ -35,7 +32,11 @@ test("spawn can read from stdout multiple chunks", async () => {
       proc.kill();
       expect(Buffer.concat(chunks).toString()).toBe("Wrote to stdout\n".repeat(4));
     })();
-
+    if (maxFD === -1) {
+      maxFD = openSync("/dev/null", "w");
+      closeSync(maxFD);
+    }
+  }
   const newMaxFD = openSync("/dev/null", "w");
   closeSync(newMaxFD);
   expect(newMaxFD).toBe(maxFD);

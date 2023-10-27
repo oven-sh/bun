oven-sh/bun

commit 7485c7c7cbbb47251f81cbcbcfb24431ad6075b1

feat: Windows + CMake Build System (#4410)
* Prepare for windows event loop

* More progress

* Update libuv.zig

* wip

* Make compiling each dependency a shell script

* Bump mimalloc

* Add the build scripts

* Update settings.json

* Fix a bunch of compiler warnings

* Remove more warnings

* more warnings

* cmake works

* Update JSSQLStatement.h

* put it in the zig file

* Fix usockets warnings

* Fixup

* Fix one of the compiler errors

* chunk

* draw the rest of the owl

* theres more

* Rename Process -> BunProcess

Works around a Windows issue

* Add musl polyfill for memmem on Windows

* More

* 12 mb

* Fix getenvZ

* fix variosu issues

* Add fast-ish path for bun install on Windows

* Update windows.zig

* Update windows.zig

* Fix build issue

* it works

* hmmm

* Rename file

* Fixups

* Update wtf-bindings.cpp

* Update src/bun.js/bindings/headers-handwritten.h

Co-authored-by: Dylan Conway <35280289+dylan-conway@users.noreply.github.com>

* further!

* more

* Update .gitignore

* hm

* quite a lot of fixes

* Update CMakeLists.txt

* zig fmt

* Many more things are starting to work.

* reb

* regenaret

* Update JSSink.h

* fixup

* fetch works

* Bun.serve() and much of the event loop works now

* Make require() work

* bun install progress

* more things work

* use less std.os

* Fixes

* small fixes

* Bump

* Bummp

* Undo that change

* We have to bump the version of Debian because libarchive has a higher minimum requirement

* ok

* some clenaup

* windows

* Update bun.zig

* fixup

* avoid duplicate symbols

* avoid undefined symbols

* bump

* Remove issue template for install

It's not used, and use the bug issue instead.

* Add types for cp and cpSync

* Add types for watchFile and unwatchFile

* Add bun-types to 'bun fmt' script

scripts/setup.sh
@@ -1,0 +1,11 @@
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
+bash ./update-submodules.sh
+bash ./all-dependencies.sh
+
+cd ../
+
+bun i
+
+mkdir build
+cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -G Ninja
+ninja

commit d77d78696c8343ae2f58f1e6f6cd33504589680b

fix: `ninja` invocation error in `scripts/setup.sh`
Running the script ended with this error:

```sh
$ bash ./scripts/setup.sh
...
ninja: error: loading 'build.ninja': No such file or directory
```

It happens because the `build.ninja` file is inside the `build`
directory, not the current directory, so `ninja` is not able to detect
the presence of that file and run the builds.

The error started happening in https://github.com/oven-sh/bun/pull/4410,
where this script was introduced.

Signed-off-by: Darshan Sen <raisinten@gmail.com>

scripts/setup.sh
@@ -8,4 +8,4 @@ bun i
 
 mkdir build
 cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -G Ninja
-ninja
+ninja -C build
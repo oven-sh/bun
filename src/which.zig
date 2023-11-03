5463eadd8c0aabe9c4f93f63aacc50fa980645c5
dave caruso <me@paperdave.net>
2023-11-02 17:34:45 -0700

    revert which logic, it doesnt work on windows anyways

--- src/which.zig
+++ src/which.zig
@@ -2,26 +2,13 @@ const std = @import("std");
 const bun = @import("root").bun;
 
 fn isValid(buf: *[bun.MAX_PATH_BYTES]u8, segment: []const u8, bin: []const u8) ?u16 {
-    @memcpy(buf[0..segment.len], segment);
+    bun.copy(u8, buf, segment);
     buf[segment.len] = std.fs.path.sep;
-    @memcpy(buf[segment.len + 1 .. segment.len + 1 + bin.len], bin);
-
-    if (!bun.Environment.isWindows) {
-        buf[segment.len + 1 + bin.len] = 0;
-        const filepath = buf[0 .. segment.len + 1 + bin.len :0];
-        if (!bun.sys.isExecutableFilePath(filepath)) return null;
-        return @as(u16, @intCast(filepath.len));
-    } else {
-        buf[segment.len + 1 + bin.len] = '.';
-        inline for (.{ "cmd", "exe", "bat" }) |ext| {
-            @memcpy(buf[segment.len + 1 + bin.len + 1 .. segment.len + 1 + bin.len + 1 + ext.len], ext);
-            buf[segment.len + 1 + bin.len + 1 + ext.len] = 0;
-            const filepath = buf[0 .. segment.len + 1 + bin.len + 1 + ext.len :0];
-            if (bun.sys.isExecutableFilePath(filepath))
-                return @as(u16, @intCast(filepath.len));
-        }
-        return null;
-    }
+    bun.copy(u8, buf[segment.len + 1 ..], bin);
+    buf[segment.len + 1 + bin.len ..][0] = 0;
+    const filepath = buf[0 .. segment.len + 1 + bin.len :0];
+    if (!bun.sys.isExecutableFilePath(filepath)) return null;
+    return @as(u16, @intCast(filepath.len));
 }
 
 // Like /usr/bin/which but without needing to exec a child process

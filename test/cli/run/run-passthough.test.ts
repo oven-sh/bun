3e988319e7744d498bf9ad7f63784f157399935e
gawgua <taisaodeoduoc@gmail.com>
2023-11-03 15:27:18 +0700

    reformat test

--- test/cli/run/run-passthough.test.ts
+++ test/cli/run/run-passthough.test.ts
@@ -2,15 +2,15 @@ import { expect, it } from "bun:test";
 import { bunExe, tempDirWithFiles } from "harness";
 
 it("should wrap passthrough arguments with doublequote", () => {
-    const package_json = JSON.stringify({
-      scripts: {
-        test: `${bunExe()} test.js $1`,
-      },
-    });
-    const dir = tempDirWithFiles("run-warp-arguments", { "package.json": package_json, "test.js": "console.log(process.argv[2])"});
-    const { stdout } = Bun.spawnSync({
-        cmd: [bunExe(), "run", "test", "Hello World"],
-        cwd: dir,
-      });
-    expect(stdout.toString("utf8").trim()).toBe("Hello World");
+  const package_json = JSON.stringify({
+    scripts: {
+      test: `${bunExe()} test.js $1`,
+    },
   });
+  const dir = tempDirWithFiles("run-warp-arguments", { "package.json": package_json, "test.js": "console.log(process.argv[2])"});
+  const { stdout } = Bun.spawnSync({
+    cmd: [bunExe(), "run", "test", "Hello World"],
+    cwd: dir,
+  });
+  expect(stdout.toString("utf8").trim()).toBe("Hello World");
+});

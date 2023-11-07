c82bb13dab0aa963e9842a98d72ed3c9b3c8a547
Zack <zack@theradisic.com>
2023-11-07 14:36:36 +0300

    Add stress test

---
+++ test/js/bun/glob/stress.test.ts
@@ -0,0 +1,34 @@
+import { expect, test, describe } from "bun:test";
+import { Glob } from "bun";
+
+const paths = [
+  "test/js/bun/glob/fixtures/file.md",
+  "test/js/bun/glob/fixtures/second/file.md",
+  "test/js/bun/glob/fixtures/second/nested/file.md",
+  "test/js/bun/glob/fixtures/second/nested/directory/file.md",
+  "test/js/bun/glob/fixtures/third/library/b/book.md",
+  "test/js/bun/glob/fixtures/third/library/a/book.md",
+  "test/js/bun/glob/fixtures/first/file.md",
+  "test/js/bun/glob/fixtures/first/nested/file.md",
+  "test/js/bun/glob/fixtures/first/nested/directory/file.md",
+  "test/js/bun/glob/fixtures/first/nested/directory/file.json",
+];
+
+test("Glob.scan stress test", async () => {
+  await Promise.all(
+    Array(1000)
+      .fill(null)
+      .map(() =>
+        new Glob("src/**/*.zig").scan().then(results => {
+          const set = new Set(results);
+          return set.size == paths.length && paths.every(path => set.has(path));
+        }),
+      ),
+  );
+});
+
+test("Glob.match stress test", () => {
+  for (let i = 0; i < 10000; i++) {
+    new Glob("src/**/*.zig").match("src/cli/package_manager_command.zig");
+  }
+});

16a9e9aca79a6504af2dbc4a0eea209f01ca8707
cirospaciari <ciro.spaciari@gmail.com>
2023-11-08 17:41:21 -0300

    ls-hpack build sh

---
+++ scripts/build-lshpack.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+source $(dirname -- "${BASH_SOURCE[0]}")/env.sh
+
+mkdir -p $BUN_DEPS_OUT_DIR
+
+cd $BUN_DEPS_DIR/ls-hpack
+
+
+rm -rf CMakeCache* CMakeFiles
+
+cmake "${CMAKE_FLAGS[@]}" . \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLSHPACK_XXH=ON \
+    -DSHARED=0 \
+    -GNinja
+
+ninja
+
+cp ./liblshpack.a $BUN_DEPS_OUT_DIR/liblshpack.a

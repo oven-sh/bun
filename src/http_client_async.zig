oven-sh/bun

commit 450397a277ba2d9e44eeeb9e385c878e2f6eb2fd

Fix fetch response skipping body if no content-length (#6932) (#6991)
* Fix fetch response skipping body if no content-length (#6932)

* Fix timeout on status 1xx/204/304 without content-length, tests changes

* Fix test that was wrong

* fix fetch response without content-length not disabling keep-alive

---------

Co-authored-by: Ashcon Partovi <ashcon@partovi.net>

src/http_client_async.zig
@@ -3094,7 +3094,7 @@ fn handleResponseBodyFromMultiplePackets(this: *HTTPClient, incoming_data: []con
 
     // done or streaming
     const is_done = content_length != null and this.state.total_body_received >= content_length.?;
-    if (is_done or this.signals.get(.body_streaming)) {
+    if (is_done or this.signals.get(.body_streaming) or content_length == null) {
         const processed = try this.state.processBodyBuffer(buffer.*);
 
         if (this.progress_node) |progress| {
@@ -3344,6 +3344,26 @@ pub fn handleResponseMetadata(
         response.status_code = 304;
     }
 
+    // according to RFC 7230 section 6.3:
+    //   In order to remain persistent, all messages on a connection need to
+    //   have a self-defined message length (i.e., one not defined by closure
+    //   of the connection)
+    // therefore, if response has no content-length header and is not chunked, implicitly disable
+    // the keep-alive behavior (keep-alive being the default behavior for HTTP/1.1 and not for HTTP/1.0)
+    if (this.state.content_length == null and this.state.transfer_encoding != .chunked) {
+        this.state.allow_keepalive = false;
+    }
+
+    // according to RFC 7230 section 3.3.3:
+    //   1. Any response to a HEAD request and any response with a 1xx (Informational),
+    //      204 (No Content), or 304 (Not Modified) status code
+    //      [...] cannot contain a message body or trailer section.
+    // therefore in these cases set content-length to 0, so the response body is always ignored
+    // and is not waited for (which could cause a timeout)
+    if ((response.status_code >= 100 and response.status_code < 200) or response.status_code == 204 or response.status_code == 304) {
+        this.state.content_length = 0;
+    }
+
     if (this.proxy_tunneling and this.proxy_tunnel == null) {
         if (response.status_code == 200) {
             // signal to continue the proxing
@@ -3541,7 +3561,7 @@ pub fn handleResponseMetadata(
         log("handleResponseMetadata: content_length is null and transfer_encoding {}", .{this.state.transfer_encoding});
     }
 
-    if (this.method.hasBody() and ((content_length != null and content_length.? > 0) or !this.state.allow_keepalive or this.state.transfer_encoding == .chunked or is_server_sent_events)) {
+    if (this.method.hasBody() and (content_length == null or content_length.? > 0 or !this.state.allow_keepalive or this.state.transfer_encoding == .chunked or is_server_sent_events)) {
         return ShouldContinue.continue_streaming;
     } else {
         return ShouldContinue.finished;

test/js/bun/http/serve.test.ts
@@ -40,7 +40,7 @@ afterAll(() => {
   }
 });
 
-[101, 418, 599, 200, 200n, 101n, 599n].forEach(statusCode => {
+[200, 200n, 303, 418, 599, 599n].forEach(statusCode => {
   it(`should response with HTTP status code (${statusCode})`, async () => {
     await runTest(
       {

test/js/web/fetch/fetch.test.ts
@@ -1,4 +1,4 @@
-import { AnyFunction, serve, ServeOptions, Server, sleep } from "bun";
+import { AnyFunction, serve, ServeOptions, Server, sleep, TCPSocketListener } from "bun";
 import { afterAll, afterEach, beforeAll, describe, expect, it, beforeEach } from "bun:test";
 import { chmodSync, mkdtempSync, readFileSync, realpathSync, rmSync, writeFileSync } from "fs";
 import { mkfifo } from "mkfifo";
@@ -1828,4 +1828,106 @@ it("304 not modified with 0 content-length does not cause a request timeout", as
   expect(response.status).toBe(304);
   expect(await response.arrayBuffer()).toHaveLength(0);
   server.stop(true);
+});
+
+describe("http/1.1 response body length", () => {
+  // issue #6932 (support response without Content-Length and Transfer-Encoding) + some regression tests
+
+  let server: TCPSocketListener | undefined;
+  beforeAll(async () => {
+    server = Bun.listen({
+      socket: {
+        open(socket) {
+          setTimeout(() => {
+            socket.end();
+          }, 9999).unref();
+        },
+        data(socket, data) {
+          const text = data.toString();
+          if (text.startsWith("GET /text")) {
+            socket.end("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!");
+          } else if (text.startsWith("GET /json")) {
+            socket.end('HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n{"hello":"World"}');
+          } else if (text.startsWith("GET /chunked")) {
+            socket.end(
+              "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nTransfer-Encoding: chunked\r\n\r\nd\r\nHello, World!\r\n0\r\n\r\n",
+            );
+          } else if (text.startsWith("GET /empty")) {
+            socket.end("HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n");
+          } else if (text.startsWith("GET /keepalive/bad")) {
+            const resp = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nConnection: keep-alive\r\n\r\nHello, World!";
+            socket.end(`${resp}${resp}`);
+          } else if (text.startsWith("GET /keepalive")) {
+            const resp =
+              "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nConnection: keep-alive\r\nContent-Length: 13\r\n\r\nHello, World!";
+            socket.end(`${resp}${resp}`);
+          } else {
+            socket.end(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello, World!`);
+          }
+        },
+        close() {},
+      },
+      port: 0,
+      hostname: "localhost",
+    });
+  });
+  afterAll(() => {
+    server?.stop?.();
+  });
+
+  const getHost = () => `${server!.hostname}:${server!.port}`;
+
+  describe("without content-length", () => {
+    it("should read text until socket closed", async () => {
+      const response = await fetch(`http://${getHost()}/text`);
+      expect(response.status).toBe(200);
+      expect(response.text()).resolves.toBe("Hello, World!");
+    });
+
+    it("should read json until socket closed", async () => {
+      const response = await fetch(`http://${getHost()}/json`);
+      expect(response.status).toBe(200);
+      expect(response.json<unknown>()).resolves.toEqual({ "hello": "World" });
+    });
+
+    it("should disable keep-alive", async () => {
+      // according to http/1.1 spec, the keep-alive persistence behavior should be disabled when
+      // "Content-Length" header is not set (and response is not chunked)
+      // therefore the response text for this test should contain
+      // the 1st http response body + the full 2nd http response as text
+      const response = await fetch(`http://${getHost()}/keepalive/bad`);
+      expect(response.status).toBe(200);
+      expect(response.text()).resolves.toHaveLength(95);
+    });
+  });
+
+  it("should support keep-alive", async () => {
+    const response = await fetch(`http://${getHost()}/keepalive`);
+    expect(response.status).toBe(200);
+    expect(response.text()).resolves.toBe("Hello, World!");
+  });
+
+  it("should support transfer-encoding: chunked", async () => {
+    const response = await fetch(`http://${getHost()}/chunked`);
+    expect(response.status).toBe(200);
+    expect(response.text()).resolves.toBe("Hello, World!");
+  });
+
+  it("should support non-zero content-length", async () => {
+    const response = await fetch(`http://${getHost()}/non-empty`);
+    expect(response.status).toBe(200);
+    expect(response.text()).resolves.toBe("Hello, World!");
+  });
+
+  it("should support content-length: 0", async () => {
+    const response = await fetch(`http://${getHost()}/empty`);
+    expect(response.status).toBe(200);
+    expect(response.arrayBuffer()).resolves.toHaveLength(0);
+  });
+
+  it("should ignore body on HEAD", async () => {
+    const response = await fetch(`http://${getHost()}/text`, { method: "HEAD" });
+    expect(response.status).toBe(200);
+    expect(response.arrayBuffer()).resolves.toHaveLength(0);
+  });
 });

commit 637da7843f2369e52de97a3ee5f0aa7cab947088

Fix hang introduced in #6991
This causes keep-alive to be disabled for HTTP status codes that do not support bodies, leading to many HTTP client hangs.

src/http_client_async.zig
@@ -3344,24 +3344,31 @@ pub fn handleResponseMetadata(
         response.status_code = 304;
     }
 
-    // according to RFC 7230 section 6.3:
-    //   In order to remain persistent, all messages on a connection need to
-    //   have a self-defined message length (i.e., one not defined by closure
-    //   of the connection)
-    // therefore, if response has no content-length header and is not chunked, implicitly disable
-    // the keep-alive behavior (keep-alive being the default behavior for HTTP/1.1 and not for HTTP/1.0)
-    if (this.state.content_length == null and this.state.transfer_encoding != .chunked) {
-        this.state.allow_keepalive = false;
-    }
+    // Don't do this for proxies because those connections will be open for awhile.
+    if (!this.proxy_tunneling) {
 
-    // according to RFC 7230 section 3.3.3:
-    //   1. Any response to a HEAD request and any response with a 1xx (Informational),
-    //      204 (No Content), or 304 (Not Modified) status code
-    //      [...] cannot contain a message body or trailer section.
-    // therefore in these cases set content-length to 0, so the response body is always ignored
-    // and is not waited for (which could cause a timeout)
-    if ((response.status_code >= 100 and response.status_code < 200) or response.status_code == 204 or response.status_code == 304) {
-        this.state.content_length = 0;
+        // according to RFC 7230 section 3.3.3:
+        //   1. Any response to a HEAD request and any response with a 1xx (Informational),
+        //      204 (No Content), or 304 (Not Modified) status code
+        //      [...] cannot contain a message body or trailer section.
+        // therefore in these cases set content-length to 0, so the response body is always ignored
+        // and is not waited for (which could cause a timeout)
+        if ((response.status_code >= 100 and response.status_code < 200) or response.status_code == 204 or response.status_code == 304) {
+            this.state.content_length = 0;
+        }
+
+        //
+        // according to RFC 7230 section 6.3:
+        //   In order to remain persistent, all messages on a connection need to
+        //   have a self-defined message length (i.e., one not defined by closure
+        //   of the connection)
+        // therefore, if response has no content-length header and is not chunked, implicitly disable
+        // the keep-alive behavior (keep-alive being the default behavior for HTTP/1.1 and not for HTTP/1.0)
+        //
+        // but, we must only do this IF the status code allows it to contain a body.
+        else if (this.state.content_length == null and this.state.transfer_encoding != .chunked) {
+            this.state.allow_keepalive = false;
+        }
     }
 
     if (this.proxy_tunneling and this.proxy_tunnel == null) {

src/install/install.zig
@@ -5925,7 +5925,17 @@ pub const PackageManager = struct {
             subcommand,
         );
 
-        manager.timestamp_for_manifest_cache_control = @as(u32, @truncate(@as(u64, @intCast(@max(std.time.timestamp(), 0)))));
+        manager.timestamp_for_manifest_cache_control = brk: {
+            if (comptime bun.Environment.allow_assert) {
+                if (env.map.get("BUN_CONFIG_MANIFEST_CACHE_CONTROL_TIMESTAMP")) |cache_control| {
+                    if (std.fmt.parseInt(u32, cache_control, 10)) |int| {
+                        break :brk int;
+                    } else |_| {}
+                }
+            }
+
+            break :brk @as(u32, @truncate(@as(u64, @intCast(@max(std.time.timestamp(), 0)))));
+        };
         return manager;
     }
 
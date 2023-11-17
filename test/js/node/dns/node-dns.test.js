f28cc9b07ce38043c8f21c80a1fb0a3c01610774
Jozef Steinhübl (xHyroM) <xhyrom.hyro@gmail.com>
2023-11-17 23:14:01 +0100

    space instead tabs

--- test/js/node/dns/node-dns.test.js
+++ test/js/node/dns/node-dns.test.js
@@ -59,19 +59,19 @@ test("it exists", () => {
 test("dns.resolve (bun.sh)", done => {
   dns.resolve("bun.sh", (err, domain, results) => {
     expect(err).toBeNull();
-    expect(domain).toBe("bun.sh")
-    expect(results instanceof Array).toBe(true)
+    expect(domain).toBe("bun.sh");
+    expect(results instanceof Array).toBe(true);
 
     // check if it contains only IPs
-    expect(results.every(e => typeof e === "string")).toBe(true)
+    expect(results.every(e => typeof e === "string")).toBe(true);
   });
 
   it("dns_promises.resolve should return only IPs", async () => {
     const results = await dns_promises.resolve("bun.sh");
-    expect(results instanceof Array).toBe(true)
-    expect(results.every(e => typeof e === "string")).toBe(true)
-  })
-})
+    expect(results instanceof Array).toBe(true);
+    expect(results.every(e => typeof e === "string")).toBe(true);
+  });
+});
 
 // //TODO: use a bun.sh SRV for testing
 test("dns.resolveSrv (_test._tcp.test.socketify.dev)", done => {

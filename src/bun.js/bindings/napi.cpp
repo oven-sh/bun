6e7014c91bc438c2290085a14251016553bf3dcb
Olivier Guimbal <oguimbal@users.noreply.github.com>
2023-11-17 14:27:58 +0100

    Fix `napi_get_value_string_utf8` to match node (#7175)
    * fix napi_get_value_string_utf8 to match node
    closes #6949
    
    * [autofix.ci] apply automated fixes
    
    * Update test/napi/napi.test.ts
    
    Co-authored-by: Jarred Sumner <jarred@jarredsumner.com>
    
    * Update test/napi/napi.test.ts
    
    Co-authored-by: Jarred Sumner <jarred@jarredsumner.com>
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Jarred Sumner <jarred@jarredsumner.com>

--- src/bun.js/bindings/napi.cpp
+++ src/bun.js/bindings/napi.cpp
@@ -1771,15 +1771,22 @@ extern "C" napi_status napi_get_value_string_utf8(napi_env env,
         return napi_ok;
     }
 
-    if (bufsize == NAPI_AUTO_LENGTH) {
-        bufsize = strlen(buf);
+    if (UNLIKELY(bufsize == 0)) {
+        *writtenPtr = 0;
+        return napi_ok;
+    }
+
+    if (UNLIKELY(bufsize == NAPI_AUTO_LENGTH)) {
+        *writtenPtr = 0;
+        buf[0] = '\0';
+        return napi_ok;
     }
 
     size_t written;
     if (view.is8Bit()) {
-        written = Bun__encoding__writeLatin1(view.characters8(), view.length(), reinterpret_cast<unsigned char*>(buf), bufsize, static_cast<uint8_t>(WebCore::BufferEncodingType::utf8));
+        written = Bun__encoding__writeLatin1(view.characters8(), view.length(), reinterpret_cast<unsigned char*>(buf), bufsize - 1, static_cast<uint8_t>(WebCore::BufferEncodingType::utf8));
     } else {
-        written = Bun__encoding__writeUTF16(view.characters16(), view.length(), reinterpret_cast<unsigned char*>(buf), bufsize, static_cast<uint8_t>(WebCore::BufferEncodingType::utf8));
+        written = Bun__encoding__writeUTF16(view.characters16(), view.length(), reinterpret_cast<unsigned char*>(buf), bufsize - 1, static_cast<uint8_t>(WebCore::BufferEncodingType::utf8));
     }
 
     if (writtenPtr != nullptr) {

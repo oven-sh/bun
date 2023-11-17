e59cd3bf76260d876c07186282f97dbd98ca5026
Zack <zack@theradisic.com>
2023-11-17 06:50:35 -0800

    Make leak test run in separate process

--- test/js/bun/glob/leak.test.ts
+++ test/js/bun/glob/leak.test.ts
@@ -2,33 +2,56 @@ import { expect, test, describe, beforeAll, afterAll } from "bun:test";
 import { Glob, GlobScanOptions } from "bun";
 
 describe("leaks", () => {
+  const bun = process.argv[0];
   const cwd = import.meta.dir;
   const iters = 100;
   const hundredMb = (1 << 20) * 100;
 
   test("scanSync", () => {
-    Bun.gc(true);
-    let prev = process.memoryUsage.rss();
+    const code = /* ts */ `
+      Bun.gc(true);
+      (function () {
+        const glob = new Bun.Glob("**/*.ts");
+        Array.from(glob.scanSync({ cwd: '${cwd}' }));
+      })();
+      Bun.gc(true);
+      console.error(process.memoryUsage.rss())
+    `;
+
+    let prev: number | undefined = undefined;
     for (let i = 0; i < iters; i++) {
-      Bun.gc(true);
-      const glob = new Glob("**/*.ts");
-      Array.from(glob.scanSync({ cwd }));
-      Bun.gc(true);
-      const current = process.memoryUsage.rss();
-      expect(Math.abs(prev - current)).toBeLessThanOrEqual(hundredMb);
+      const { stderr, exitCode } = Bun.spawnSync([bun, "--smol", "-e", code]);
+      expect(exitCode).toBe(0);
+      const val = parseInt(stderr.toString());
+      if (prev === undefined) {
+        prev = val;
+      } else {
+        expect(Math.abs(prev - val)).toBeLessThanOrEqual(hundredMb);
+      }
     }
   });
 
   test("scan", async () => {
-    Bun.gc(true);
-    let prev = process.memoryUsage.rss();
+    const code = /* ts */ `
+      Bun.gc(true);
+      await (async function () {
+        const glob = new Bun.Glob("**/*.ts");
+        Array.fromAsync(glob.scan({ cwd: '${cwd}' }));
+      })();
+      Bun.gc(true);
+      console.error(process.memoryUsage.rss())
+    `;
+
+    let prev: number | undefined = undefined;
     for (let i = 0; i < iters; i++) {
-      Bun.gc(true);
-      const glob = new Glob("**/*.ts");
-      await Array.fromAsync(glob.scan({ cwd }));
-      Bun.gc(true);
-      const current = process.memoryUsage.rss();
-      expect(Math.abs(prev - current)).toBeLessThanOrEqual(hundredMb);
+      const { stderr, exitCode } = Bun.spawnSync([bun, "--smol", "-e", code]);
+      expect(exitCode).toBe(0);
+      const val = parseInt(stderr.toString());
+      if (prev === undefined) {
+        prev = val;
+      } else {
+        expect(Math.abs(prev - val)).toBeLessThanOrEqual(hundredMb);
+      }
     }
   });
 });

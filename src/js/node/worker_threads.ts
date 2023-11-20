c9c60d08a9a198cb6d0c974730b7c48adaa9df6e
Jérôme Benoit <jerome.benoit@sap.com>
2023-11-20 18:21:26 +0100

    doc: add TODO comment as per review request
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/js/node/worker_threads.ts
+++ src/js/node/worker_threads.ts
@@ -110,6 +110,7 @@ function receiveMessageOnPort(port: MessagePort) {
   };
 }
 
+// TODO: parent port emulation is not complete
 function fakeParentPort() {
   const fake = Object.create(MessagePort.prototype);
   Object.defineProperty(fake, "onmessage", {

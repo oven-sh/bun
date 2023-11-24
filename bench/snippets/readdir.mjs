414f6b546005fc0535bb27f6b001b0bd61231ab9
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-11-24 03:51:37 -0800

    Update readdir.mjs

--- bench/snippets/readdir.mjs
+++ bench/snippets/readdir.mjs
@@ -1,13 +1,30 @@
-import { readdirSync } from "fs";
+import { readdirSync, readdir as readdirCb } from "fs";
+import { readdir } from "fs/promises";
 import { bench, run } from "./runner.mjs";
 import { argv } from "process";
+import { fileURLToPath } from "url";
+import { relative, resolve } from "path";
 
-const dir = argv.length > 2 ? argv[2] : "/tmp";
+let dir = resolve(argv.length > 2 ? argv[2] : fileURLToPath(new URL("../../node_modules", import.meta.url)));
+if (dir.includes(process.cwd())) {
+  dir = relative(process.cwd(), dir);
+}
 
-const count = readdirSync(dir).length;
-bench(`readdir("${dir}")`, () => {
-  readdirSync(dir, { withFileTypes: true });
+const result = await readdir(dir, { recursive: true });
+const count = result.length;
+const syncCount = readdirSync(dir, { recursive: true }).length;
+
+bench(`await readdir("${dir}", {recursive: true})`, async () => {
+  await readdir(dir, { recursive: true });
+});
+
+bench(`await readdir("${dir}", {recursive: false})`, async () => {
+  await readdir(dir, { recursive: false });
 });
 
 await run();
-console.log("\n\nFor", count, "files/dirs in", dir);
+console.log("\n", count, "files/dirs in", dir);
+
+if (count !== syncCount) {
+  throw new Error(`Mismatched file counts: ${count} async !== ${syncCount} sync`);
+}

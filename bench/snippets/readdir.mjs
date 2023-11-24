3c707357426d218400ca6cbaff665c061627f83d
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-11-24 05:01:23 -0800

    Update readdir.mjs

--- bench/snippets/readdir.mjs
+++ bench/snippets/readdir.mjs
@@ -4,6 +4,7 @@ import { bench, run } from "./runner.mjs";
 import { argv } from "process";
 import { fileURLToPath } from "url";
 import { relative, resolve } from "path";
+import { createHash } from "crypto";
 
 let dir = resolve(argv.length > 2 ? argv[2] : fileURLToPath(new URL("../../node_modules", import.meta.url)));
 if (dir.includes(process.cwd())) {
@@ -14,8 +15,27 @@ const result = await readdir(dir, { recursive: true });
 const count = result.length;
 const syncCount = readdirSync(dir, { recursive: true }).length;
 
+const hash = createHash("sha256").update(result.sort().join("\n")).digest("hex");
+
 bench(`await readdir("${dir}", {recursive: true})`, async () => {
   await readdir(dir, { recursive: true });
+});
+
+bench(`await readdir("${dir}", {recursive: true}) x 10`, async () => {
+  const promises = [
+    readdir(dir, { recursive: true }),
+    readdir(dir, { recursive: true }),
+    readdir(dir, { recursive: true }),
+    readdir(dir, { recursive: true }),
+    readdir(dir, { recursive: true }),
+    readdir(dir, { recursive: true }),
+    readdir(dir, { recursive: true }),
+    readdir(dir, { recursive: true }),
+    readdir(dir, { recursive: true }),
+    readdir(dir, { recursive: true }),
+    readdir(dir, { recursive: true }),
+  ];
+  await Promise.all(promises);
 });
 
 bench(`await readdir("${dir}", {recursive: false})`, async () => {
@@ -23,7 +43,7 @@ bench(`await readdir("${dir}", {recursive: false})`, async () => {
 });
 
 await run();
-console.log("\n", count, "files/dirs in", dir);
+console.log("\n", count, "files/dirs in", dir, "\n", "SHA256:", hash, "\n");
 
 if (count !== syncCount) {
   throw new Error(`Mismatched file counts: ${count} async !== ${syncCount} sync`);

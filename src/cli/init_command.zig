2cb914b7d5482a14eacf440048e99553c6852b55
dave caruso <me@paperdave.net>
2023-11-27 17:05:14 -0800

    fix init_command

--- src/cli/init_command.zig
+++ src/cli/init_command.zig
@@ -39,11 +39,11 @@ pub const InitCommand = struct {
 
         Output.flush();
 
-        const input = try std.io.getStdIn().reader().readUntilDelimiterAlloc(alloc, '\n', 1024);
+        var input = try std.io.getStdIn().reader().readUntilDelimiterAlloc(alloc, '\n', 1024);
+        if (strings.endsWithChar(input, '\r')) {
+            input = input[0 .. input.len - 1];
+        }
         if (input.len > 0) {
-            if (strings.endsWithChar(input, '\r')) {
-                return input[0 .. input.len - 1];
-            }
             return input;
         } else {
             return default;

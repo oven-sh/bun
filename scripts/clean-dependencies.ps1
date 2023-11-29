4d85a5f55441e33d34c39a4edce6f02a39831cea
dave caruso <me@paperdave.net>
2023-11-29 13:25:35 -0800

    fix libarchive

--- scripts/clean-dependencies.ps1
+++ scripts/clean-dependencies.ps1
@@ -1,5 +1,5 @@
-$ErrorActionPreference = 'Stop'  # Setting strict mode, similar to 'set -euo pipefail' in bash
 . (Join-Path $PSScriptRoot "env.ps1")
+$ErrorActionPreference = 'SilentlyContinue'  # Setting strict mode, similar to 'set -euo pipefail' in bash
 
 function Reset-Submodule {
   param (
@@ -16,15 +16,32 @@ function Reset-Submodule {
 }
 
 $Deps = Join-Path $PSScriptRoot "../src/deps"
+$DepsOut = Join-Path $PSScriptRoot "../src/deps"
 
-Reset-Submodule $Deps/base64
-Reset-Submodule $Deps/boringssl
-Reset-Submodule $Deps/c-ares
-Reset-Submodule $Deps/libarchive
-Reset-Submodule $Deps/lol-html
-Reset-Submodule $Deps/mimalloc
-Reset-Submodule $Deps/picohttpparser
-Reset-Submodule $Deps/tinycc
-Reset-Submodule $Deps/zlib
-Reset-Submodule $Deps/zstd
-Reset-Submodule $Deps/ls-hpack
+Reset-Submodule $Deps\base64
+Reset-Submodule $Deps\boringssl
+Reset-Submodule $Deps\c-ares
+Reset-Submodule $Deps\libarchive
+Reset-Submodule $Deps\lol-html
+Reset-Submodule $Deps\mimalloc
+Reset-Submodule $Deps\picohttpparser
+Reset-Submodule $Deps\tinycc
+Reset-Submodule $Deps\zlib
+Reset-Submodule $Deps\zstd
+Reset-Submodule $Deps\ls-hpack
+
+Remove-Item -Force $DepsOut\base64.lib
+Remove-Item -Force $DepsOut\crypto.lib
+Remove-Item -Force $DepsOut\ssl.lib
+Remove-Item -Force $DepsOut\decrepit.lib
+Remove-Item -Force $DepsOut\cares.lib
+Remove-Item -Force $DepsOut\archive.lib
+Remove-Item -Force $DepsOut\lolhtml.lib
+Remove-Item -Force $DepsOut\mimalloc.lib
+Remove-Item -Force $DepsOut\tcc.lib
+Remove-Item -Force $DepsOut\zlib.lib
+Remove-Item -Force $DepsOut\zstd.lib
+Remove-Item -Force $DepsOut\libuv.lib
+Remove-Item -Force $DepsOut\lshpack.lib
+
+$ErrorActionPreference = 'Stop'

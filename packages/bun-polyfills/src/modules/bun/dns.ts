519d53a16b7f7c4e6be51b27da92e4b6e810b582
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2023-12-01 19:26:21 -0300

    polyfills: Bun.dns improvements

--- packages/bun-polyfills/src/modules/bun/dns.ts
+++ packages/bun-polyfills/src/modules/bun/dns.ts
@@ -1,8 +1,9 @@
+import type { DNSLookup } from 'bun';
 import dns from 'node:dns';
 
 const dnsObj: typeof Bun.dns = {
     async lookup(hostname, options) {
-        const opts = { verbatim: true, all: true } as dns.LookupOptions;
+        const opts = { verbatim: true, all: true, family: 0, hints: 0 } as Required<dns.LookupOptions>;
         if (options?.family) {
             if (options.family === 'IPv4') opts.family = 4;
             else if (options.family === 'IPv6') opts.family = 6;
@@ -10,10 +11,18 @@ const dnsObj: typeof Bun.dns = {
             else opts.family = options.family;
         }
         if (options?.flags) opts.hints = options.flags;
-        const records = ((await dns.promises.resolveAny(hostname))
-            .filter(r => r.type === 'A' || r.type === 'AAAA') as (dns.AnyARecord | dns.AnyAaaaRecord)[])
-            .map(r => ({ address: r.address, family: r.type === 'A' ? 4 as const : 6 as const, ttl: r.ttl }));
-        return records;
+        opts.hints |= dns.V4MAPPED;
+        const records = await dns.promises.lookup(hostname, opts) as dns.LookupAddress[];
+        return await Promise.all(records.map(async r => {
+            const record = r as DNSLookup;
+            try {
+                record.ttl = ((await dns.promises[`resolve${record.family}`](hostname, { ttl: true })) as dns.RecordWithTtl[])
+                    .find(r => r.address === record.address)?.ttl ?? 0;
+            } catch {
+                record.ttl = 0;
+            }
+            return record;
+        }));
     },
     // This has more properties but they're not documented on bun-types yet, oh well.
 };

205c13e66e00d763ad45d9e6fd678f0c07231669
Brook Jeynes <jeynesbrook@gmail.com>
2023-12-24 01:15:55 +1000

    Upgrade argument check now only checks if all arguments contain `--` - Added more tests

--- src/cli.zig
+++ src/cli.zig
@@ -1176,19 +1176,22 @@ pub const Command = struct {
             RootCommandMatcher.case("build"), RootCommandMatcher.case("bun") => .BuildCommand,
             RootCommandMatcher.case("discord") => .DiscordCommand,
             RootCommandMatcher.case("upgrade") => brk: {
-                // Check if there are any arguments passed and whether the first contains `--` (--stable/--canary)
-                // Only error if the 1st cmd argument doesn't start with `--`
-                if (args_iter.buf.len > 2 and !strings.contains(std.mem.span(args_iter.buf[2]), "--")) {
-                    Output.prettyError(
-                        \\<r><red>error<r><d>:<r> this command updates bun itself, and does not take package names.
-                        \\Use `bun update
-                    , .{});
+                if (args_iter.buf.len > 2) {
                     for (args_iter.buf[2..]) |arg| {
                         const span = std.mem.span(arg);
-                        Output.prettyError(" {s}", .{span});
+                        if (!strings.contains(span, "--")) {
+                            Output.prettyError(
+                                \\<r><red>error<r><d>:<r> this command updates bun itself, and does not take package names.
+                                \\Use `bun update
+                            , .{});
+                            for (args_iter.buf[2..]) |arg_err| {
+                                const span_err = std.mem.span(arg_err);
+                                Output.prettyError(" {s}", .{span_err});
+                            }
+                            Output.prettyErrorln("` instead.", .{});
+                            Global.exit(1);
+                        }
                     }
-                    Output.prettyErrorln("` instead.", .{});
-                    Global.exit(1);
                 }
 
                 break :brk .UpgradeCommand;
--- test/cli/install/bun-upgrade.test.ts
+++ test/cli/install/bun-upgrade.test.ts
@@ -14,7 +14,7 @@ afterEach(async () => {
   await rm(x_dir, { force: true, recursive: true });
 });
 
-it("should display error message and suggest command", async () => {
+it("two invalid arguments, should display error message and suggest command", async () => {
   const { stderr, exitCode } = spawn({
     cmd: [bunExe(), "upgrade", "bun-types", "--dev"],
     cwd: x_dir,
@@ -30,5 +30,83 @@ it("should display error message and suggest command", async () => {
   );
   expect(err.split(/\r?\n/)).toContain(
     "Use `bun update bun-types --dev` instead.",
+  );
+});
+
+it("two invalid arguments flipped, should display error message and suggest command", async () => {
+  const { stderr, exitCode } = spawn({
+    cmd: [bunExe(), "upgrade", "--dev", "bun-types"],
+    cwd: x_dir,
+    stdout: null,
+    stdin: "pipe",
+    stderr: "pipe",
+    env,
+  });
+
+  const err = await new Response(stderr).text();
+  expect(err.split(/\r?\n/)).toContain(
+    "error: this command updates bun itself, and does not take package names.",
+  );
+  expect(err.split(/\r?\n/)).toContain(
+    "Use `bun update --dev bun-types` instead.",
+  );
+});
+
+it("one invalid argument, should display error message and suggest command", async () => {
+  const { stderr, exitCode } = spawn({
+    cmd: [bunExe(), "upgrade", "bun-types"],
+    cwd: x_dir,
+    stdout: null,
+    stdin: "pipe",
+    stderr: "pipe",
+    env,
+  });
+
+  const err = await new Response(stderr).text();
+  expect(err.split(/\r?\n/)).toContain(
+    "error: this command updates bun itself, and does not take package names.",
+  );
+  expect(err.split(/\r?\n/)).toContain(
+    "Use `bun update bun-types` instead.",
+  );
+});
+
+it("one valid argument, should succeed", async () => {
+  const { stderr, exitCode } = spawn({
+    cmd: [bunExe(), "upgrade", "--help"],
+    cwd: x_dir,
+    stdout: null,
+    stdin: "pipe",
+    stderr: "pipe",
+    env,
+  });
+
+  const err = await new Response(stderr).text();
+  // Should not contain error message
+  expect(err.split(/\r?\n/)).not.toContain(
+    "error: this command updates bun itself, and does not take package names.",
+  );
+  expect(err.split(/\r?\n/)).not.toContain(
+    "Use `bun update --help` instead.",
+  );
+});
+
+it("two valid argument, should succeed", async () => {
+  const { stderr, exitCode } = spawn({
+    cmd: [bunExe(), "upgrade", "--stable", "--profile"],
+    cwd: x_dir,
+    stdout: null,
+    stdin: "pipe",
+    stderr: "pipe",
+    env,
+  });
+
+  const err = await new Response(stderr).text();
+  // Should not contain error message
+  expect(err.split(/\r?\n/)).not.toContain(
+    "error: this command updates bun itself, and does not take package names.",
+  );
+  expect(err.split(/\r?\n/)).not.toContain(
+    "Use `bun update --stable --profile` instead.",
   );
 });

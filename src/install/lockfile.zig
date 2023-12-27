d309a1eaa469f934c2974f32bb8ed198c8067b7e
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-12-27 01:31:53 -0800

    Update lockfile.zig

--- src/install/lockfile.zig
+++ src/install/lockfile.zig
@@ -3482,32 +3482,47 @@ pub const Package = extern struct {
         if (comptime features.check_for_duplicate_dependencies and !group.behavior.isPeer()) {
             const entry = lockfile.scratch.duplicate_checker_map.getOrPutAssumeCapacity(external_alias.hash);
             if (entry.found_existing) {
-                // duplicate dependencies are allowed in optionalDependencies
-                if (comptime group.behavior.isOptional()) {
-                    for (package_dependencies[0..dependencies_count]) |*package_dep| {
-                        if (package_dep.name_hash == this_dep.name_hash) {
+                // the last one wins
+                // this means:
+                //     "devDependencies": { "lodash": "latest" },
+                //     "dependencies": { "lodash": "1.0.0" }
+                //
+                // "latest" is what will be chosen, unless they did --production.
+                var is_other_one_workspace = this_dep.behavior.isWorkspace();
+                for (package_dependencies[0..dependencies_count]) |*package_dep| {
+                    if (package_dep.name_hash == this_dep.name_hash) {
+                        // workspace always wins
+                        if (!package_dep.behavior.isWorkspace()) {
                             package_dep.* = this_dep;
-                            break;
+                            is_other_one_workspace = true;
                         }
+
+                        break;
                     }
-                    return null;
-                } else {
-                    var notes = try allocator.alloc(logger.Data, 1);
-
-                    notes[0] = .{
-                        .text = try std.fmt.allocPrint(lockfile.allocator, "\"{s}\" originally specified here", .{external_alias.slice(buf)}),
-                        .location = logger.Location.initOrNull(&source, source.rangeOfString(entry.value_ptr.*)),
-                    };
-
-                    try log.addRangeWarningFmtWithNotes(
-                        &source,
-                        source.rangeOfString(key_loc),
-                        lockfile.allocator,
-                        notes,
-                        "Duplicate dependency: \"{s}\" specified in package.json",
-                        .{external_alias.slice(buf)},
-                    );
                 }
+
+                // duplicate dependencies are allowed in optionalDependencies
+                if (comptime !group.behavior.isOptional()) {
+                    if (!is_other_one_workspace) {
+                        var notes = try allocator.alloc(logger.Data, 1);
+
+                        notes[0] = .{
+                            .text = try std.fmt.allocPrint(lockfile.allocator, "\"{s}\" originally specified here", .{external_alias.slice(buf)}),
+                            .location = logger.Location.initOrNull(&source, source.rangeOfString(entry.value_ptr.*)),
+                        };
+
+                        try log.addRangeWarningFmtWithNotes(
+                            &source,
+                            source.rangeOfString(key_loc),
+                            lockfile.allocator,
+                            notes,
+                            "Duplicate dependency: \"{s}\" specified in package.json",
+                            .{external_alias.slice(buf)},
+                        );
+                    }
+                }
+
+                return null;
             }
 
             entry.value_ptr.* = value_loc;

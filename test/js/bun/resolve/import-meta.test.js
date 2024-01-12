2ee8aca1364641c7820df02ae230d5e9ff096218
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2024-01-12 14:26:06 -0800

    Implement `import.meta.{dirname, filename}` for Node.js compatibility

--- test/js/bun/resolve/import-meta.test.js
+++ test/js/bun/resolve/import-meta.test.js
@@ -6,7 +6,7 @@ import * as Module from "node:module";
 import { join } from "node:path";
 import sync from "./require-json.json";
 
-const { path, dir } = import.meta;
+const { path, dir, dirname, filename } = import.meta;
 
 it("import.meta.main", () => {
   const { exitCode } = spawnSync({
@@ -188,6 +188,14 @@ it("import.meta.require (javascript, live bindings)", () => {
 
 it("import.meta.dir", () => {
   expect(dir.endsWith("/bun/test/js/bun/resolve")).toBe(true);
+});
+
+it("import.meta.dirname", () => {
+  expect(dirname).toBe(dir);
+});
+
+it("import.meta.filename", () => {
+  expect(filename).toBe(import.meta.path);
 });
 
 it("import.meta.path", () => {

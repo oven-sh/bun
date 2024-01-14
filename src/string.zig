6c839d4597a0a745bfcb493df0113346883e18a8
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2024-01-14 04:06:04 -0800

    Do fewer copies in readdir()

--- src/string.zig
+++ src/string.zig
@@ -280,6 +280,7 @@ pub const String = extern struct {
     extern fn BunString__fromLatin1(bytes: [*]const u8, len: usize) String;
     extern fn BunString__fromBytes(bytes: [*]const u8, len: usize) String;
     extern fn BunString__fromUTF16(bytes: [*]const u16, len: usize) String;
+    extern fn BunString__fromUTF16ToLatin1(bytes: [*]const u16, len: usize) String;
     extern fn BunString__fromLatin1Unitialized(len: usize) String;
     extern fn BunString__fromUTF16Unitialized(len: usize) String;
 
@@ -369,6 +370,9 @@ pub const String = extern struct {
 
     pub fn createUTF16(bytes: []const u16) String {
         if (bytes.len == 0) return String.empty;
+        if (bun.strings.firstNonASCII16CheckMin([]const u16, bytes, false) == null) {
+            return BunString__fromUTF16ToLatin1(bytes.ptr, bytes.len);
+        }
         return BunString__fromUTF16(bytes.ptr, bytes.len);
     }
 

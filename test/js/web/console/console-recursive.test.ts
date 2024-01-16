1820407a308bd8f4f1ff3da2621a042d8a138d64
Lukas Kastern <lukaskastern@web.de>
2024-01-16 22:15:25 +0100

    Do not rely on output of console-recursive.test.js

--- test/js/web/console/console-recursive.test.ts
+++ test/js/web/console/console-recursive.test.ts
@@ -1,10 +1,10 @@
 // @known-failing-on-windows: 1 failing
-import { file, spawn } from "bun";
+import { spawn } from "bun";
 import { expect, it } from "bun:test";
 import { bunExe, bunEnv } from "harness";
 
 it("should not hang when logging to stdout recursively", async () => {
-  const { stdout, exited } = spawn({
+  const { exited } = spawn({
     cmd: [bunExe(), import.meta.dir + "/console-recursive.js"],
     stdin: null,
     stdout: "pipe",
@@ -12,21 +12,15 @@ it("should not hang when logging to stdout recursively", async () => {
     env: bunEnv,
   });
   expect(await exited).toBe(0);
-  const outText = await new Response(stdout).text();
-  const expectedText = await new Response(file(import.meta.dir + "/console-recursive.expected.txt")).text();
-  expect(outText.replace(/^\[.+?s\] /gm, "")).toBe(expectedText.replace(/^\[.+?s\] /gm, ""));
 });
 
 it("should not hang when logging to stderr recursively", async () => {
-  const { stderr, exited } = spawn({
-    cmd: [bunExe(), import.meta.dir + "/console-recursive.js"],
+  const { exited } = spawn({
+    cmd: [bunExe(), import.meta.dir + "/console-recursive.js", "print_to_stderr_skmxctoznf"],
     stdin: null,
     stdout: "pipe",
     stderr: "pipe",
     env: bunEnv,
   });
   expect(await exited).toBe(0);
-  const outText = await new Response(stderr).text();
-  const expectedText = await new Response(file(import.meta.dir + "/console-recursive.expected.txt")).text();
-  expect(outText.replace(/^\[.+?s\] /gm, "")).toBe(expectedText.replace(/^\[.+?s\] /gm, ""));
 });

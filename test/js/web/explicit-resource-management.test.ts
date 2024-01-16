bfad4132bb47d1fbc40021367630406026efae34
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2024-01-15 23:16:12 -0800

    Update prettier

--- test/js/web/explicit-resource-management.test.ts
+++ test/js/web/explicit-resource-management.test.ts
@@ -1,53 +1,57 @@
 // tbh, we should have more tests for this
-test('Symbol.dispose exists', () => {
-  expect(Symbol.dispose).toBeDefined()
-  expect(Symbol.dispose).toBeSymbol()
-  expect(Symbol.asyncDispose).toBeDefined()
-  expect(Symbol.asyncDispose).toBeSymbol()
+test("Symbol.dispose exists", () => {
+  expect(Symbol.dispose).toBeDefined();
+  expect(Symbol.dispose).toBeSymbol();
+  expect(Symbol.asyncDispose).toBeDefined();
+  expect(Symbol.asyncDispose).toBeSymbol();
 });
 
-test('SuppressedError works', () => {
-  const e = new SuppressedError(new Error("this is error"), new Error('this was suppressed'), 'this is a message');
-  expect(e.message).toBe('this is a message');
-  expect(() => { throw e.suppressed }).toThrow('this was suppressed');
-  expect(() => { throw e.error }).toThrow('this is error');
-})
+test("SuppressedError works", () => {
+  const e = new SuppressedError(new Error("this is error"), new Error("this was suppressed"), "this is a message");
+  expect(e.message).toBe("this is a message");
+  expect(() => {
+    throw e.suppressed;
+  }).toThrow("this was suppressed");
+  expect(() => {
+    throw e.error;
+  }).toThrow("this is error");
+});
 
 let disposeOrder = 0;
 function useWithAsync() {
   return {
-    status: 'none',
+    status: "none",
     disposeOrder: -1,
     [Symbol.dispose]() {
-      this.status = 'disposed';
+      this.status = "disposed";
       this.disposeOrder = disposeOrder++;
     },
     [Symbol.asyncDispose]() {
-      this.status = 'async-disposed';
+      this.status = "async-disposed";
       this.disposeOrder = disposeOrder++;
-    }
-  }
+    },
+  };
 }
 
-test('using syntax works and doesnt collide with user symbols', () => {
+test("using syntax works and doesnt collide with user symbols", () => {
   disposeOrder = 0;
   {
-    let __using = 'break';
+    let __using = "break";
     let __callDispose = function () {
-      throw new Error('should not be called');
+      throw new Error("should not be called");
     };
     let __stack = {
       push: () => {
-        throw new Error('stack corruption');
-      }
-    }
+        throw new Error("stack corruption");
+      },
+    };
 
     const a1 = useWithAsync();
     {
       using u1 = a1;
-      expect(u1.status).toBe('none');
+      expect(u1.status).toBe("none");
     }
-    expect(a1.status).toBe('disposed');
+    expect(a1.status).toBe("disposed");
   }
 
   {
@@ -55,17 +59,18 @@ test('using syntax works and doesnt collide with user symbols', () => {
     const a2 = useWithAsync();
     const a3 = useWithAsync();
     {
-      using u1 = a1, u2 = a2;
+      using u1 = a1,
+        u2 = a2;
       {
         using u3 = a3;
-        expect(u3.status).toBe('none');
+        expect(u3.status).toBe("none");
       }
-      expect(u1.status).toBe('none');
-      expect(u2.status).toBe('none');
-      expect(a3.status).toBe('disposed');
+      expect(u1.status).toBe("none");
+      expect(u2.status).toBe("none");
+      expect(a3.status).toBe("disposed");
     }
-    expect(a1.status).toBe('disposed');
-    expect(a2.status).toBe('disposed');
+    expect(a1.status).toBe("disposed");
+    expect(a2.status).toBe("disposed");
 
     expect(a3.disposeOrder).toBe(1);
     expect(a2.disposeOrder).toBe(2);
@@ -82,27 +87,27 @@ test('using syntax works and doesnt collide with user symbols', () => {
       var _hasErr = 1;
     }
   }
-})
+});
 
-test('await using syntax works and doesnt collide with user symbols', async () => {
+test("await using syntax works and doesnt collide with user symbols", async () => {
   disposeOrder = 0;
   {
-    let __using = 'break';
+    let __using = "break";
     let __callDispose = function () {
-      throw new Error('should not be called');
+      throw new Error("should not be called");
     };
     let __stack = {
       push: () => {
-        throw new Error('stack corruption');
-      }
-    }
+        throw new Error("stack corruption");
+      },
+    };
 
     const a1 = useWithAsync();
     {
       using u1 = a1;
-      expect(u1.status).toBe('none');
+      expect(u1.status).toBe("none");
     }
-    expect(a1.status).toBe('disposed');
+    expect(a1.status).toBe("disposed");
   }
 
   {
@@ -110,18 +115,18 @@ test('await using syntax works and doesnt collide with user symbols', async () =
     const a2 = useWithAsync();
     const a3 = useWithAsync();
     {
-      using u1 = a1
+      using u1 = a1;
       await using u2 = a2;
       {
         using u3 = a3;
-        expect(u3.status).toBe('none');
+        expect(u3.status).toBe("none");
       }
-      expect(u1.status).toBe('none');
-      expect(u2.status).toBe('none');
-      expect(a3.status).toBe('disposed');
+      expect(u1.status).toBe("none");
+      expect(u2.status).toBe("none");
+      expect(a3.status).toBe("disposed");
     }
-    expect(a1.status).toBe('disposed');
-    expect(a2.status).toBe('async-disposed');
+    expect(a1.status).toBe("disposed");
+    expect(a2.status).toBe("async-disposed");
 
     expect(a3.disposeOrder).toBe(1);
     expect(a2.disposeOrder).toBe(2);
@@ -138,4 +143,4 @@ test('await using syntax works and doesnt collide with user symbols', async () =
       var _hasErr = 1;
     }
   }
-})
+});

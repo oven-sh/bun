b1b9403e1f581626ba09b7f00b59de09939c7a7d
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2024-01-23 01:05:29 +0800

    more tests

--- test/cli/init/init.test.ts
+++ test/cli/init/init.test.ts
@@ -1,8 +1,8 @@
 // @known-failing-on-windows: 1 failing
 import fs from "fs";
-import path from "path";
 import os from "os";
-import { bunExe, bunEnv } from "harness";
+import path from "path";
+import { bunEnv, bunExe } from "harness";
 
 test("bun init works", () => {
   const temp = fs.realpathSync(fs.mkdtempSync(path.join(os.tmpdir(), "bun-init-X")));
@@ -80,4 +80,53 @@ test("bun init with piped cli", () => {
   expect(fs.existsSync(path.join(temp, ".git", "config"))).toBe(true);
   expect(fs.existsSync(path.join(temp, "node_modules"))).toBe(true);
   expect(fs.existsSync(path.join(temp, "tsconfig.json"))).toBe(true);
+}, 30_000);
+
+test("bun init without ab existing .git/config", () => {
+  const temp = fs.realpathSync(fs.mkdtempSync(path.join(os.tmpdir(), "bun-init-X")));
+
+  const out = Bun.spawnSync({
+    cmd: [bunExe(), "init", "-y"],
+    cwd: temp,
+    stdio: ["ignore", "inherit", "inherit"],
+    env: bunEnv,
+  });
+
+  // @ts-expect-error
+  expect(out.signal).toBe(undefined);
+  expect(out.exitCode).toBe(0);
+
+  const gitConfig = fs.readFileSync(path.join(temp, ".git", "config"), "utf8");
+  expect(gitConfig).toEqual(`# Use \`bun\` as the textconv for \`bun.lockb\` files
+[diff "lockb"]
+textconv = bun
+binary = true
+`);
+}, 30_000);
+
+test("bun init with existing .git/config", () => {
+  const temp = fs.realpathSync(fs.mkdtempSync(path.join(os.tmpdir(), "bun-init-X")));
+
+  fs.mkdirSync(path.join(temp, ".git"));
+  fs.writeFileSync(path.join(temp, ".git", "config"), "[user]\nname = Test User\n");
+
+  const out = Bun.spawnSync({
+    cmd: [bunExe(), "init", "-y"],
+    cwd: temp,
+    stdio: ["ignore", "inherit", "inherit"],
+    env: bunEnv,
+  });
+
+  // @ts-expect-error
+  expect(out.signal).toBe(undefined);
+  expect(out.exitCode).toBe(0);
+
+  const gitConfig = fs.readFileSync(path.join(temp, ".git", "config"), "utf8");
+  expect(gitConfig).toEqual(`[user]
+name = Test User
+# Use \`bun\` as the textconv for \`bun.lockb\` files
+[diff "lockb"]
+textconv = bun
+binary = true
+`);
 }, 30_000);

b1b00c1540763b3791cde2ff255b4a04bec04c4f
Dave Caruso <me@paperdave.net>
2024-01-23 08:01:46 -0800

    Mark the rest

--- test/js/bun/sqlite/sqlite.test.js
+++ test/js/bun/sqlite/sqlite.test.js
@@ -1,4 +1,3 @@
-// @known-failing-on-windows: 1 failing
 import { expect, it, describe } from "bun:test";
 import { Database, constants, SQLiteError } from "bun:sqlite";
 import { existsSync, fstat, realpathSync, rmSync, writeFileSync } from "fs";

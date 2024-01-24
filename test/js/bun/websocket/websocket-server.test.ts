839c1a9d6b6347904f8df21b8c75aa81610a014b
Meghan Denny <meghan@bun.sh>
2024-01-23 17:48:11 -0800

    this file should work now, report any errors

--- test/js/bun/websocket/websocket-server.test.ts
+++ test/js/bun/websocket/websocket-server.test.ts
@@ -1,4 +1,3 @@
-// @known-failing-on-windows: 1 failing
 import { describe, it, expect, afterEach } from "bun:test";
 import type { Server, Subprocess, WebSocketHandler } from "bun";
 import { serve, spawn } from "bun";

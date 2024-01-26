97d01b7a39933fa8f5d1b594f2478df70b48843b
Dylan Conway <dylan.conway567@gmail.com>
2024-01-25 18:53:35 -0800

    path sep

--- test/bundler/bundler_edgecase.test.ts
+++ test/bundler/bundler_edgecase.test.ts
@@ -1,6 +1,7 @@
 // @known-failing-on-windows: panic "TODO on Windows"
 import assert from "assert";
 import dedent from "dedent";
+import { sep } from "path";
 import { itBundled, testForFile } from "./expectBundled";
 var { describe, test, expect } = testForFile(import.meta.path);
 
@@ -37,7 +38,7 @@ describe("bundler", () => {
     },
     target: "bun",
     run: {
-      stdout: "a/b",
+      stdout: `a${sep}b`,
     },
   });
   itBundled("edgecase/ImportStarFunction", {

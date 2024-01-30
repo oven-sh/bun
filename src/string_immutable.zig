3e754424dca2b034f976121fdea05760403baa4a
Dylan Conway <dylan.conway567@gmail.com>
2024-01-30 13:44:00 -0800

    fix crash

--- src/string_immutable.zig
+++ src/string_immutable.zig
@@ -5302,6 +5302,7 @@ pub fn convertUTF8toUTF16InBuffer(
     //
     // the reason i didn't implement the fallback is purely because our
     // code in this file is too chaotic. it is left as a TODO
+    if (input.len == 0) return &[_]u16{};
     const result = bun.simdutf.convert.utf8.to.utf16.le(input, buf);
     return buf[0..result];
 }
@@ -5312,6 +5313,7 @@ pub fn convertUTF16toUTF8InBuffer(
 ) ![]const u8 {
     // See above
 
+    if (input.len == 0) return &[_]u8{};
     const result = bun.simdutf.convert.utf16.to.utf8.le(input, buf);
     // switch (result.status) {
     //     .success => return buf[0..result.count],

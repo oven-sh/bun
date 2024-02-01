f4aa4f73a7bfd196ca6c0b8321a83010a2956145
Meghan Denny <meghan@bun.sh>
2024-01-31 20:14:05 -0800

    posix syscalls support path traversal, don't do the work for them

--- src/bundler/bundle_v2.zig
+++ src/bundler/bundle_v2.zig
@@ -9038,7 +9038,7 @@ const LinkerContext = struct {
                 }
                 // resolve any /./ and /../ occurrences
                 // use resolvePosix since we asserted above all seps are '/'
-                if (std.mem.indexOf(u8, rel_path, "/./") != null) {
+                if (Environment.isWindows and std.mem.indexOf(u8, rel_path, "/./") != null) {
                     var buf: [std.fs.MAX_PATH_BYTES]u8 = undefined;
                     const rel_path_fixed = c.allocator.dupe(u8, bun.path.normalizeBuf(rel_path, &buf, .posix)) catch unreachable;
                     chunk.final_rel_path = rel_path_fixed;

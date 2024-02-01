5d367664a28756bb1a047b47f374c645a69ce6d1
Meghan Denny <meghan@bun.sh>
2024-01-31 16:09:14 -0800

    don't mangle absolute paths

--- src/bun.js/node/types.zig
+++ src/bun.js/node/types.zig
@@ -657,6 +657,12 @@ pub const PathLike = union(enum) {
     pub fn sliceZWithForceCopy(this: PathLike, buf: *[bun.MAX_PATH_BYTES]u8, comptime force: bool) [:0]const u8 {
         const sliced = this.slice();
 
+        if (Environment.isWindows) {
+            if (std.fs.path.isAbsolute(sliced)) {
+                return resolve_path.PosixToWinNormalizer.resolveCWDWithExternalBufZ(buf, sliced) catch @panic("Error while resolving path.");
+            }
+        }
+
         if (sliced.len == 0) return "";
 
         if (comptime !force) {
@@ -671,13 +677,6 @@ pub const PathLike = union(enum) {
     }
 
     pub inline fn sliceZ(this: PathLike, buf: *[bun.MAX_PATH_BYTES]u8) [:0]const u8 {
-        if (Environment.isWindows) {
-            const data = this.slice();
-            if (!std.fs.path.isAbsolute(data)) {
-                return sliceZWithForceCopy(this, buf, false);
-            }
-            return resolve_path.PosixToWinNormalizer.resolveCWDWithExternalBufZ(buf, data) catch @panic("Error while resolving path.");
-        }
         return sliceZWithForceCopy(this, buf, false);
     }
 

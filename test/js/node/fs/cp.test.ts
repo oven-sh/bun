99020592af2adf40947025e02f4868745629e457
Georgijs Vilums <georgijs.vilums@gmail.com>
2024-02-01 17:15:48 -0800

    rework and simplify file opening in windows

--- test/js/node/fs/cp.test.ts
+++ test/js/node/fs/cp.test.ts
@@ -1,5 +1,6 @@
 // @known-failing-on-windows: 1 failing
 import fs from "fs";
+import { join } from "path";
 import { describe, test, expect, jest } from "bun:test";
 import { tempDirWithFiles } from "harness";
 
@@ -254,9 +255,9 @@ for (const [name, copy] of impls) {
       process.chdir(prev);
 
       expect(filter.mock.calls.sort((a, b) => a[0].localeCompare(b[0]))).toEqual([
-        [basename + "/from", basename + "/result"],
-        [basename + "/from/a.txt", basename + "/result/a.txt"],
-        [basename + "/from/b.txt", basename + "/result/b.txt"],
+        [join(basename, "from"), join(basename, "result")],
+        [join(basename, "from", "a.txt"), join(basename, "result", "a.txt")],
+        [join(basename, "from", "b.txt"), join(basename, "result", "b.txt")],
       ]);
     });
 

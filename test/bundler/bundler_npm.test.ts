8900ad8b0339a87930c834ccae7e885c846be8cb
dave caruso <me@paperdave.net>
2024-02-07 17:18:49 -0800

    we didnt win

--- test/bundler/bundler_npm.test.ts
+++ test/bundler/bundler_npm.test.ts
@@ -6,7 +6,6 @@ var { describe, test, expect } = testForFile(import.meta.path);
 
 describe("bundler", () => {
   itBundled("npm/ReactSSR", {
-    todo: process.platform === "win32" ? true : false, // https://github.com/oven-sh/bun/issues/8742
     install: ["react@next", "react-dom@next"],
     files: {
       "/entry.tsx": /* tsx */ `
--- test/cli/install/migration/complex-workspace/packages/with-postinstall/postinstall.js
+++ test/cli/install/migration/complex-workspace/packages/with-postinstall/postinstall.js
@@ -2,5 +2,4 @@ import { writeFileSync } from "fs";
 
 writeFileSync(import.meta.dir + "/postinstall.txt", `i ran!`);
 
-// TODO: postinstall doesnt run sharp's scripts yet :(
-// import "sharp";
+import "sharp";

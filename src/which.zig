e7c9c7dfe4387e8a65c2eeda769da94e00524939
cirospaciari <ciro.spaciari@gmail.com>
2024-02-08 18:34:23 -0300

    refactor

--- src/which.zig
+++ src/which.zig
@@ -71,6 +71,44 @@ pub fn endsWithExtension(str: []const u8) bool {
     return false;
 }
 
+/// Check if the WPathBuffer holds a existing file path, checking also for windows extensions variants like .exe, .cmd and .bat (internally used by whichWin)
+fn searchBin(buf: *bun.WPathBuffer, path_size: usize, check_windows_extensions: bool) ?[:0]const u16 {
+    if (bun.sys.existsOSPath(buf[0..path_size :0], true))
+        return buf[0..path_size :0];
+
+    if (check_windows_extensions) {
+        buf[path_size] = '.';
+        buf[path_size + 1 + 3] = 0;
+        inline for (win_extensionsW) |ext| {
+            @memcpy(buf[path_size + 1 .. path_size + 1 + 3], ext);
+            if (bun.sys.existsOSPath(buf[0 .. path_size + 1 + ext.len :0], true))
+                return buf[0 .. path_size + 1 + ext.len :0];
+        }
+    }
+    return null;
+}
+
+/// Check if bin file exists in this path (internally used by whichWin)
+fn searchBinInPath(buf: *bun.WPathBuffer, path_buf: *[bun.MAX_PATH_BYTES]u8, path: []const u8, bin: []const u8, check_windows_extensions: bool) ?[:0]const u16 {
+    if (path.len == 0) return null;
+    const segment = if (std.fs.path.isAbsolute(path)) (PosixToWinNormalizer.resolveCWDWithExternalBuf(path_buf, path) catch return null) else path;
+    const segment_utf16 = bun.strings.convertUTF8toUTF16InBuffer(buf, segment);
+
+    var segment_len = segment.len;
+    var segment_utf16_len = segment_utf16.len;
+    if (buf[segment.len - 1] != std.fs.path.sep) {
+        buf[segment.len] = std.fs.path.sep;
+        segment_len += 1;
+        segment_utf16_len += 1;
+    }
+
+    const bin_utf16 = bun.strings.convertUTF8toUTF16InBuffer(buf[segment_len..], bin);
+    const path_size = segment_utf16_len + bin_utf16.len;
+    buf[path_size] = 0;
+
+    return searchBin(buf, path_size, check_windows_extensions);
+}
+
 /// This is the windows version of `which`.
 /// It operates on wide strings.
 /// It is similar to Get-Command in powershell.
@@ -78,87 +116,29 @@ pub fn whichWin(buf: *bun.WPathBuffer, path: []const u8, cwd: []const u8, bin: [
     if (bin.len == 0) return null;
     var path_buf: [bun.MAX_PATH_BYTES]u8 = undefined;
 
-    const no_exec_extension = !endsWithExtension(bin);
+    const check_windows_extensions = !endsWithExtension(bin);
 
     // handle absolute paths
     if (std.fs.path.isAbsolute(bin)) {
         const normalized_bin = PosixToWinNormalizer.resolveCWDWithExternalBuf(&path_buf, bin) catch return null;
         const bin_utf16 = bun.strings.convertUTF8toUTF16InBuffer(buf, normalized_bin);
         buf[bin_utf16.len] = 0;
-        if (bun.sys.existsOSPath(buf[0..normalized_bin.len :0], true))
-            return buf[0..normalized_bin.len :0];
-        if (no_exec_extension) {
-            buf[bin_utf16.len] = '.';
-            buf[bin_utf16.len + 1 + 3] = 0;
-            inline for (win_extensionsW) |ext| {
-                @memcpy(buf[normalized_bin.len + 1 .. bin_utf16.len + 1 + ext.len], ext);
-                if (bun.sys.existsOSPath(buf[0 .. normalized_bin.len + 1 + ext.len :0], true))
-                    return buf[0 .. normalized_bin.len + 1 + ext.len :0];
-            }
-        }
-        return null;
+        return searchBin(buf, bin_utf16.len, check_windows_extensions);
     }
 
-    if (cwd.len > 0) {
-        var path_iter = std.mem.tokenizeScalar(u8, cwd, std.fs.path.delimiter);
-        while (path_iter.next()) |_segment| {
-            const segment = if (std.fs.path.isAbsolute(_segment)) (PosixToWinNormalizer.resolveCWDWithExternalBuf(&path_buf, _segment) catch continue) else _segment;
-            const segment_utf16 = bun.strings.convertUTF8toUTF16InBuffer(buf, segment);
-
-            var segment_len = segment.len;
-            var segment_utf16_len = segment_utf16.len;
-            if (buf[segment.len - 1] != std.fs.path.sep) {
-                buf[segment.len] = std.fs.path.sep;
-                segment_len += 1;
-                segment_utf16_len += 1;
-            }
-
-            const bin_utf16 = bun.strings.convertUTF8toUTF16InBuffer(buf[segment_len..], bin);
-            buf[segment_utf16_len + bin_utf16.len] = 0;
-
-            if (bun.sys.existsOSPath(buf[0 .. segment_utf16_len + bin_utf16.len :0], true))
-                return buf[0 .. segment_utf16_len + bin_utf16.len :0];
-
-            if (no_exec_extension) {
-                buf[segment_utf16_len + bin_utf16.len] = '.';
-                buf[segment_utf16_len + bin_utf16.len + 1 + 3] = 0;
-                inline for (win_extensionsW) |ext| {
-                    @memcpy(buf[segment_utf16_len + bin_utf16.len + 1 .. segment_utf16_len + bin_utf16.len + 1 + 3], ext);
-                    if (bun.sys.existsOSPath(buf[0 .. segment_utf16_len + bin_utf16.len + 1 + ext.len :0], true))
-                        return buf[0 .. segment_utf16_len + bin_utf16.len + 1 + ext.len :0];
-                }
-            }
-        }
+    // check if bin is in cwd
+    if (searchBinInPath(buf, &path_buf, cwd, bin, check_windows_extensions)) |bin_path| {
+        return bin_path;
     }
+
+    // iterate over system path delimiter
     var path_iter = std.mem.tokenizeScalar(u8, path, std.fs.path.delimiter);
     while (path_iter.next()) |_segment| {
+        // we also iterate over ':' to match linux behavior
         var segment_iter = std.mem.tokenizeScalar(u8, _segment, ':');
         while (segment_iter.next()) |segment_part| {
-            const segment = if (std.fs.path.isAbsolute(segment_part)) (PosixToWinNormalizer.resolveCWDWithExternalBuf(&path_buf, segment_part) catch continue) else segment_part;
-            const segment_utf16 = bun.strings.convertUTF8toUTF16InBuffer(buf, segment);
-
-            var segment_len = segment.len;
-            var segment_utf16_len = segment_utf16.len;
-            if (buf[segment.len - 1] != std.fs.path.sep) {
-                buf[segment.len] = std.fs.path.sep;
-                segment_len += 1;
-                segment_utf16_len += 1;
-            }
-
-            const bin_utf16 = bun.strings.convertUTF8toUTF16InBuffer(buf[segment_len..], bin);
-            buf[segment_utf16_len + bin_utf16.len] = 0;
-
-            if (bun.sys.existsOSPath(buf[0 .. segment_utf16_len + bin_utf16.len :0], true))
-                return buf[0 .. segment_utf16_len + bin_utf16.len :0];
-
-            if (no_exec_extension) {
-                buf[segment_utf16_len + bin_utf16.len] = '.';
-                buf[segment_utf16_len + bin_utf16.len + 1 + 3] = 0;
-                inline for (win_extensionsW) |ext| {
-                    @memcpy(buf[segment_utf16_len + bin_utf16.len + 1 .. segment_utf16_len + bin_utf16.len + 1 + 3], ext);
-                    if (bun.sys.existsOSPath(buf[0 .. segment_utf16_len + bin_utf16.len + 1 + ext.len :0], true))
-                        return buf[0 .. segment_utf16_len + bin_utf16.len + 1 + ext.len :0];
-                }
+            if (searchBinInPath(buf, &path_buf, segment_part, bin, check_windows_extensions)) |bin_path| {
+                return bin_path;
             }
         }
     }

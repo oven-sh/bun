cf1b89b96dcf9a9820a7d8d84d5fc2aca79cf1e4
cirospaciari <ciro.spaciari@gmail.com>
2024-02-15 18:41:04 -0300

    opsie

--- src/fd.zig
+++ src/fd.zig
@@ -303,10 +303,6 @@ pub const FDImpl = packed struct {
     }
 
     pub fn format(this: FDImpl, comptime fmt: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
-        if (fmt.len != 0) {
-            @compileError("invalid format string for FDImpl.format. must be either '' or 'd'");
-        }
-
         if (!this.isValid()) {
             try writer.writeAll("[invalid_fd]");
             return;
@@ -315,6 +311,10 @@ pub const FDImpl = packed struct {
         if (fmt.len == 1 and fmt[0] == 'd') {
             try writer.print("{d}", .{this.system()});
             return;
+        }
+
+        if (fmt.len != 0) {
+            @compileError("invalid format string for FDImpl.format. must be either '' or 'd'");
         }
 
         switch (env.os) {

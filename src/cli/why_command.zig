a7c4c3e1cb70ba2b669fc523323ea50074d4dcab
Kai Jellinghaus <contact@kaij.tech>
2024-02-23 10:19:10 +0100

    Handle multiple positionals

--- src/cli/why_command.zig
+++ src/cli/why_command.zig
@@ -56,91 +56,92 @@ pub const WhyCommand = struct {
         Output.disableBuffering();
         const lockfile = load_lockfile.ok.lockfile;
 
-        const param = pm.options.positionals[1]; // TODO: Handle no positional arg
-        const id = if (lockfile.package_index.get(String.Builder.stringHash(param))) |id| id.PackageID else {
-            Output.errGeneric("Could not find package {s}", .{param});
-            Global.exit(1);
-        };
+        for (pm.options.positionals[1..]) |param| {
+            const id = if (lockfile.package_index.get(String.Builder.stringHash(param))) |id| id.PackageID else {
+                Output.errGeneric("Could not find package {s}", .{param});
+                Global.exit(1);
+            };
 
-        const Candidate = struct {
-            name: String,
-            len: u32,
-            prev: PackageID, // 0 is a special case for "root"
-            package_idx: usize,
-        };
+            const Candidate = struct {
+                name: String,
+                len: u32,
+                prev: PackageID, // 0 is a special case for "root"
+                package_idx: usize,
+            };
 
-        var candidates = std.AutoHashMap(PackageID, Candidate).init(ctx.allocator);
-        defer candidates.deinit();
+            var candidates = std.AutoHashMap(PackageID, Candidate).init(ctx.allocator);
+            defer candidates.deinit();
 
-        // reserve root
-        try candidates.put(0, .{
-            .name = lockfile.rootPackage().?.name,
-            .prev = 0,
-            .len = 0,
-            .package_idx = 0,
-        });
+            // reserve root
+            try candidates.put(0, .{
+                .name = lockfile.rootPackage().?.name,
+                .prev = 0,
+                .len = 0,
+                .package_idx = 0,
+            });
 
-        // Output.println("Building RIndex", .{});
-        // build reverse index
-        {
-            var to_explore = std.ArrayList(PackageID).init(ctx.allocator);
-            defer to_explore.deinit();
+            // Output.println("Building RIndex", .{});
+            // build reverse index
+            {
+                var to_explore = std.ArrayList(PackageID).init(ctx.allocator);
+                defer to_explore.deinit();
 
-            (try to_explore.addOne()).* = 0;
+                (try to_explore.addOne()).* = 0;
 
-            while (to_explore.popOrNull()) |candidate_id| {
-                const candidate = candidates.get(candidate_id).?;
-                const resolutions = lockfile.packages.get(candidate.package_idx).resolutions.get(lockfile.buffers.resolutions.items);
-                // Output.println("Walking {} w/ {} res", .{ candidate_id, resolutions.len });
+                while (to_explore.popOrNull()) |candidate_id| {
+                    const candidate = candidates.get(candidate_id).?;
+                    const resolutions = lockfile.packages.get(candidate.package_idx).resolutions.get(lockfile.buffers.resolutions.items);
+                    // Output.println("Walking {} w/ {} res", .{ candidate_id, resolutions.len });
 
-                for (resolutions) |dep_id| {
-                    const val = candidates.get(dep_id);
-                    // Output.println("Walking Res {} {s}", .{ dep_id, if (val) |v| v.name.slice(lockfile.buffers.string_bytes.items) else "null" });
-                    if (val == null or (val.?.len > (candidate.len + 1))) {
-                        const idx = for (0..(lockfile.packages.len)) |i| {
-                            const p = lockfile.packages.get(i);
-                            if (p.meta.id == dep_id) {
-                                break i;
-                            }
-                        } else {
-                            unreachable;
-                        };
-                        const name = lockfile.packages.get(idx).name;
-                        // Output.println("Discovered {s} via {s} - {}", .{ name.slice(lockfile.buffers.string_bytes.items), candidate.name.slice(lockfile.buffers.string_bytes.items), candidate.len + 1 });
-                        try candidates.put(dep_id, .{
-                            .prev = candidate_id,
-                            .len = candidate.len + 1,
-                            .package_idx = idx,
-                            .name = name,
-                        });
-                        (try to_explore.addOne()).* = dep_id;
+                    for (resolutions) |dep_id| {
+                        const val = candidates.get(dep_id);
+                        // Output.println("Walking Res {} {s}", .{ dep_id, if (val) |v| v.name.slice(lockfile.buffers.string_bytes.items) else "null" });
+                        if (val == null or (val.?.len > (candidate.len + 1))) {
+                            const idx = for (0..(lockfile.packages.len)) |i| {
+                                const p = lockfile.packages.get(i);
+                                if (p.meta.id == dep_id) {
+                                    break i;
+                                }
+                            } else {
+                                unreachable;
+                            };
+                            const name = lockfile.packages.get(idx).name;
+                            // Output.println("Discovered {s} via {s} - {}", .{ name.slice(lockfile.buffers.string_bytes.items), candidate.name.slice(lockfile.buffers.string_bytes.items), candidate.len + 1 });
+                            try candidates.put(dep_id, .{
+                                .prev = candidate_id,
+                                .len = candidate.len + 1,
+                                .package_idx = idx,
+                                .name = name,
+                            });
+                            (try to_explore.addOne()).* = dep_id;
+                        }
                     }
                 }
             }
-        }
-        // Output.println("resolving tree", .{});
-        var current: ?Candidate = b: {
-            var it = candidates.valueIterator();
-            while (it.next()) |c| {
-                // const name = c.name.slice(lockfile.buffers.string_bytes.items);
-                // Output.println("searching {s}", .{name});
-                if (c.package_idx == id) {
-                    break :b c.*;
+            // Output.println("resolving tree", .{});
+            var current: ?Candidate = b: {
+                var it = candidates.valueIterator();
+                while (it.next()) |c| {
+                    // const name = c.name.slice(lockfile.buffers.string_bytes.items);
+                    // Output.println("searching {s}", .{name});
+                    if (c.package_idx == id) {
+                        break :b c.*;
+                    }
+                } else {
+                    unreachable;
                 }
-            } else {
-                unreachable;
-            }
-        };
+            };
 
-        Output.prettyln("The package {s} is installed", .{current.?.name.slice(lockfile.buffers.string_bytes.items)});
-        current = candidates.get(current.?.prev);
+            Output.prettyln("The package {s} is installed", .{current.?.name.slice(lockfile.buffers.string_bytes.items)});
+            current = candidates.get(current.?.prev);
 
-        while (current) |c| {
-            Output.prettyln("-> due to {s}", .{c.name.slice(lockfile.buffers.string_bytes.items)});
-            if (c.prev != 0) {
-                current = candidates.get(c.prev);
-            } else {
-                current = null;
+            while (current) |c| {
+                Output.prettyln("-> due to {s}", .{c.name.slice(lockfile.buffers.string_bytes.items)});
+                if (c.prev != 0) {
+                    current = candidates.get(c.prev);
+                } else {
+                    current = null;
+                }
             }
         }
 

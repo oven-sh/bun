32c50e54ad81b7829d490a0de5260de5fe25f11e
Jarred Sumner <jarred@jarredsumner.com>
2024-02-25 13:05:34 -0800

    Make `Bun.main` the resolved filesystem path (#9105)
    * Make `Bun.main` the resolved filesystem path, but only Bun.main
    
    * Fix flaky test
    
    ---------
    
    Co-authored-by: Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>

--- src/bun.js/api/BunObject.zig
+++ src/bun.js/api/BunObject.zig
@@ -980,7 +980,52 @@ pub fn getMain(
     globalThis: *JSC.JSGlobalObject,
     _: *JSC.JSObject,
 ) callconv(.C) JSC.JSValue {
-    return ZigString.init(globalThis.bunVM().main).toValueGC(globalThis);
+    const vm = globalThis.bunVM();
+
+    // Attempt to use the resolved filesystem path
+    // This makes `eval('require.main === module')` work when the main module is a symlink.
+    // This behavior differs slightly from Node. Node sets the `id` to `.` when the main module is a symlink.
+    use_resolved_path: {
+        if (vm.main_resolved_path.isEmpty()) {
+            // If it's from eval, don't try to resolve it.
+            if (strings.hasSuffixComptime(vm.main, "[eval]")) {
+                break :use_resolved_path;
+            }
+
+            const fd = bun.sys.openat(
+                // avoid going thorugh libuv for this one.
+                bun.toFD(std.fs.cwd().fd),
+
+                &(std.os.toPosixPath(vm.main) catch break :use_resolved_path),
+
+                // Open with the minimum permissions necessary for resolving the file path.
+                if (comptime Environment.isLinux) std.os.O.PATH else std.os.O.RDONLY,
+
+                0,
+            ).unwrap() catch break :use_resolved_path;
+
+            defer _ = bun.sys.close(fd);
+            if (comptime Environment.isWindows) {
+                var wpath: bun.WPathBuffer = undefined;
+                const fdpath = bun.getFdPathW(fd, &wpath) catch break :use_resolved_path;
+                vm.main_resolved_path = bun.String.createUTF16(fdpath);
+            } else {
+                var path: bun.PathBuffer = undefined;
+                const fdpath = bun.getFdPath(fd, &path) catch break :use_resolved_path;
+
+                // Bun.main === otherId will be compared many times, so let's try to create an atom string if we can.
+                if (bun.String.tryCreateAtom(fdpath)) |atom| {
+                    vm.main_resolved_path = atom;
+                } else {
+                    vm.main_resolved_path = bun.String.createUTF8(fdpath);
+                }
+            }
+        }
+
+        return vm.main_resolved_path.toJS(globalThis);
+    }
+
+    return ZigString.init(vm.main).toValueGC(globalThis);
 }
 
 pub fn getAssetPrefix(

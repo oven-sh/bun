897da8d09d46af178c91891c99c24e7142802d02
Igor Wessel <igor_wessel@hotmail.com>
2024-02-25 16:45:27 -0300

    test: add case using cjs with custom condition

--- test/js/bun/resolve/import-custom-condition.test.ts
+++ test/js/bun/resolve/import-custom-condition.test.ts
@@ -5,7 +5,7 @@ import { bunExe, bunEnv, tempDirWithFiles } from "harness";
 let dir: string;
 
 beforeAll(() => {
-  dir = tempDirWithFiles("custom", {
+  dir = tempDirWithFiles("customcondition", {
     "./node_modules/custom/index.js": "export const foo = 1;",
     "./node_modules/custom/not_allow.js": "throw new Error('should not be imported')",
     "./node_modules/custom/package.json": JSON.stringify({
@@ -17,9 +17,29 @@ beforeAll(() => {
         },
       },
     }),
+
+    "./node_modules/custom2/index.cjs": "module.exports.foo = 5;",
+    "./node_modules/custom2/index.mjs": "export const foo = 1;",
+    "./node_modules/custom2/not_allow.js": "throw new Error('should not be imported')",
+    "./node_modules/custom2/package.json": JSON.stringify({
+      name: "custom2",
+      exports: {
+        "./test": {
+          customcondition: {
+            import: "./index.mjs",
+            require: "./index.cjs",
+            default: "./index.mjs",
+          },
+          default: "./not_allow.js",
+        },
+      },
+      type: "module",
+    }),
   });
 
   writeFileSync(`${dir}/test.js`, `import {foo} from 'custom/test';\nconsole.log(foo);`);
+  writeFileSync(`${dir}/test.cjs`, `const {foo} = require("custom2/test");\nconsole.log(foo);`);
+
   writeFileSync(
     `${dir}/package.json`,
     JSON.stringify(
@@ -27,6 +47,7 @@ beforeAll(() => {
         name: "hello",
         imports: {
           custom: "custom",
+          custom2: "custom2",
         },
       },
       null,
@@ -36,16 +57,25 @@ beforeAll(() => {
 });
 
 it("custom condition in package.json resolves", async () => {
-  const { exitCode, stdout, stderr } = Bun.spawnSync({
+  const { exitCode, stdout } = Bun.spawnSync({
     cmd: [bunExe(), "--conditions=customcondition", `${dir}/test.js`],
     env: bunEnv,
     cwd: import.meta.dir,
   });
 
-  expect(stderr.toString("utf8")).toBe("");
-
   expect(exitCode).toBe(0);
   expect(stdout.toString("utf8")).toBe("1\n");
+});
+
+it("custom condition require in package.json resolves", async () => {
+  const { exitCode, stdout } = Bun.spawnSync({
+    cmd: [bunExe(), "-c=customcondition", `${dir}/test.cjs`],
+    env: bunEnv,
+    cwd: import.meta.dir,
+  });
+
+  expect(exitCode).toBe(0);
+  expect(stdout.toString("utf8")).toBe("5\n");
 });
 
 it("when not pass custom condition not resolves", async () => {

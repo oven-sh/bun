b3acc1f13c706e03877cf584c2d8d628488be927
Igor Wessel <igor_wessel@hotmail.com>
2024-02-28 12:04:25 -0300

    test: add cases for multiple --conditions specified

--- test/js/bun/resolve/import-custom-condition.test.ts
+++ test/js/bun/resolve/import-custom-condition.test.ts
@@ -12,7 +12,7 @@ beforeAll(() => {
       name: "custom",
       exports: {
         "./test": {
-          customcondition: "./index.js",
+          first: "./index.js",
           default: "./not_allow.js",
         },
       },
@@ -25,7 +25,23 @@ beforeAll(() => {
       name: "custom2",
       exports: {
         "./test": {
-          customcondition: {
+          first: {
+            import: "./index.mjs",
+            require: "./index.cjs",
+            default: "./index.mjs",
+          },
+          default: "./not_allow.js",
+        },
+        "./test2": {
+          second: {
+            import: "./index.mjs",
+            require: "./index.cjs",
+            default: "./index.mjs",
+          },
+          default: "./not_allow.js",
+        },
+        "./test3": {
+          third: {
             import: "./index.mjs",
             require: "./index.cjs",
             default: "./index.mjs",
@@ -39,6 +55,10 @@ beforeAll(() => {
 
   writeFileSync(`${dir}/test.js`, `import {foo} from 'custom/test';\nconsole.log(foo);`);
   writeFileSync(`${dir}/test.cjs`, `const {foo} = require("custom2/test");\nconsole.log(foo);`);
+  writeFileSync(
+    `${dir}/multiple-conditions.js`,
+    `const pkg1 = require("custom2/test");\nconst pkg2 = require("custom2/test2");\nconst pkg3 = require("custom2/test3");\nconsole.log(pkg1.foo, pkg2.foo, pkg3.foo);`,
+  );
 
   writeFileSync(
     `${dir}/package.json`,
@@ -56,9 +76,9 @@ beforeAll(() => {
   );
 });
 
-it("custom condition in package.json resolves", async () => {
+it("custom condition 'import' in package.json resolves", async () => {
   const { exitCode, stdout } = Bun.spawnSync({
-    cmd: [bunExe(), "--conditions=customcondition", `${dir}/test.js`],
+    cmd: [bunExe(), "--conditions=first", `${dir}/test.js`],
     env: bunEnv,
     cwd: import.meta.dir,
   });
@@ -67,9 +87,9 @@ it("custom condition in package.json resolves", async () => {
   expect(stdout.toString("utf8")).toBe("1\n");
 });
 
-it("custom condition require in package.json resolves", async () => {
+it("custom condition 'require' in package.json resolves", async () => {
   const { exitCode, stdout } = Bun.spawnSync({
-    cmd: [bunExe(), "-c=customcondition", `${dir}/test.cjs`],
+    cmd: [bunExe(), "--conditions=first", `${dir}/test.cjs`],
     env: bunEnv,
     cwd: import.meta.dir,
   });
@@ -78,9 +98,33 @@ it("custom condition require in package.json resolves", async () => {
   expect(stdout.toString("utf8")).toBe("5\n");
 });
 
-it("when not pass custom condition not resolves", async () => {
+it("multiple conditions in package.json resolves", async () => {
+  const { exitCode, stdout } = Bun.spawnSync({
+    cmd: [bunExe(), "--conditions=first", "--conditions=second", "--conditions=third", `${dir}/multiple-conditions.js`],
+    env: bunEnv,
+    cwd: import.meta.dir,
+  });
+
+  expect(exitCode).toBe(0);
+  expect(stdout.toString("utf8")).toBe("5 5 5\n");
+});
+
+it("multiple conditions when some not specified should resolves to fallback", async () => {
+  const { exitCode, stderr } = Bun.spawnSync({
+    cmd: [bunExe(), "--conditions=first", "--conditions=second", `${dir}/multiple-conditions.js`],
+    env: bunEnv,
+    cwd: import.meta.dir,
+  });
+
+  expect(exitCode).toBe(1);
+
+  // not_allow.js is the fallback for third condition, so it should be in stderr
+  expect(stderr.toString("utf8")).toMatch("new Error('should not be imported')");
+});
+
+it("custom condition when don't match condition should resolves to default", async () => {
   const { exitCode } = Bun.spawnSync({
-    cmd: [bunExe(), "--conditions=customcondition1", `${dir}/test.js`],
+    cmd: [bunExe(), "--conditions=first1", `${dir}/test.js`],
     env: bunEnv,
     cwd: import.meta.dir,
   });

ae317a711b8c22cb898489bbd07bcf60608cabd6
Meghan Denny <hello@nektro.net>
2024-02-27 22:22:52 -0800

    resolver: fix debug mode crash in test/bundler/bun-build-api.test.ts (#9140)

--- src/resolver/resolver.zig
+++ src/resolver/resolver.zig
@@ -1587,7 +1587,7 @@ pub const Resolver = struct {
     pub fn bustDirCache(r: *ThisResolver, path: string) bool {
         dev("Bust {s}", .{path});
         if (Environment.allow_assert) {
-            if (path[path.len - 1] != std.fs.path.sep) {
+            if (path[path.len - 1] != '/') {
                 std.debug.panic("Expected a trailing slash on {s}", .{path});
             }
         }

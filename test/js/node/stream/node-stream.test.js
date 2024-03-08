49bcae6196f7903e4ae283303bb9fe3bbbee8a14
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2024-03-08 04:56:06 -0800

    Deflake

--- test/js/node/stream/node-stream.test.js
+++ test/js/node/stream/node-stream.test.js
@@ -188,18 +188,8 @@ describe("createReadStream", () => {
     });
   });
 
-  it("should emit readable on end", done => {
-    const testData = "Hello world";
-    const path = join(tmpdir(), `${Date.now()}-testEmitReadableOnEnd.txt`);
-    writeFileSync(path, testData);
-    const stream = createReadStream(path);
-
-    stream.on("readable", () => {
-      const chunk = stream.read();
-      if (!chunk) {
-        done();
-      }
-    });
+  it("should emit readable on end", () => {
+    expect([join(import.meta.dir, "emit-readable-on-end.js")]).toRun();
   });
 });
 

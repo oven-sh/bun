ab274b92eb0f834e6a1d6849534dda1ff517cab5
Jarred Sumner <jarred@jarredsumner.com>
2024-03-14 08:06:28 +0100

    Test it on macOS and windows too why not

--- test/js/bun/spawn/spawn_waiter_thread.test.ts
+++ test/js/bun/spawn/spawn_waiter_thread.test.ts
@@ -3,27 +3,35 @@ import { spawn } from "bun";
 import { bunEnv, bunExe } from "harness";
 import { join } from "path";
 
-if (process.platform === "linux") {
-  test("issue #9404", async () => {
-    const proc = spawn({
-      env: {
-        ...bunEnv,
-        BUN_GARBAGE_COLLECTOR_LEVEL: "1",
-        BUN_FEATURE_FLAG_FORCE_WAITER_THREAD: "1",
-      },
-      stderr: "inherit",
-      cmd: [bunExe(), join(__dirname, "spawn_waiter_thread-fixture.js")],
-    });
-
-    setTimeout(() => {
-      proc.kill();
-    }, 1000);
-
-    await proc.exited;
-
-    const resourceUsage = proc.resourceUsage();
-    
-    // Assert we didn't use 100% of CPU time
-    expect(resourceUsage?.cpuTime.total).toBeLessThan(750_000n);
+async function run(withWaiterThread: boolean) {
+  const proc = spawn({
+    env: {
+      ...bunEnv,
+      ...(withWaiterThread
+        ? { BUN_GARBAGE_COLLECTOR_LEVEL: "1", BUN_FEATURE_FLAG_FORCE_WAITER_THREAD: "1" }
+        : { WITHOUT_WAITER_THREAD: "1" }),
+    },
+    stderr: "inherit",
+    cmd: [bunExe(), join(__dirname, "spawn_waiter_thread-fixture.js")],
   });
+
+  setTimeout(() => {
+    proc.kill();
+  }, 1000).unref();
+
+  await proc.exited;
+
+  const resourceUsage = proc.resourceUsage();
+
+  // Assert we didn't use 100% of CPU time
+  expect(resourceUsage?.cpuTime.total).toBeLessThan(750_000n);
 }
+
+test("issue #9404", async () => {
+  const promises = [run(false)];
+  if (process.platform === "linux") {
+    promises.push(run(true));
+  }
+
+  await Promise.all(promises);
+});

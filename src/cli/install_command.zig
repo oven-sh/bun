9b699ef82401c55e0e109b82b4d2db49b8d15579
Meghan Denny <meghan@bun.sh>
2024-03-15 00:52:31 -0700

    dont print zig stack trace if bun install fails

--- src/cli/install_command.zig
+++ src/cli/install_command.zig
@@ -4,6 +4,15 @@ const PackageManager = @import("../install/install.zig").PackageManager;
 
 pub const InstallCommand = struct {
     pub fn exec(ctx: Command.Context) !void {
-        try PackageManager.install(ctx);
+        PackageManager.install(ctx) catch |err| switch (err) {
+            error.InstallFailed,
+            error.InvalidPackageJSON,
+            => {
+                const log = &bun.CLI.Cli.log_;
+                log.printForLogLevel(bun.Output.errorWriter()) catch {};
+                bun.Global.exit(1);
+            },
+            else => |e| return e,
+        };
     }
 };

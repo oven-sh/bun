1479c24689bdd8a4a35b6527ba0a0addac82fd43
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2024-03-16 21:39:32 -0700

    Fixes #7148

--- test/js/node/timers/node-timers.test.ts
+++ test/js/node/timers/node-timers.test.ts
@@ -1,5 +1,6 @@
-import { describe, test } from "bun:test";
+import { describe, test, it, expect } from "bun:test";
 import { setTimeout, clearTimeout, setInterval, clearInterval, setImmediate } from "node:timers";
+import { promisify } from "util";
 
 for (const fn of [setTimeout, setInterval]) {
   describe(fn.name, () => {
@@ -16,3 +17,38 @@ for (const fn of [setTimeout, setInterval]) {
     });
   });
 }
+
+it("node.js util.promisify(setTimeout) works", async () => {
+  const setTimeout = promisify(globalThis.setTimeout);
+  await setTimeout(1);
+
+  expect(async () => {
+    await setTimeout(1).then(a => {
+      throw new Error("TestPassed");
+    });
+  }).toThrow("TestPassed");
+});
+
+it("node.js util.promisify(setInterval) works", async () => {
+  const setInterval = promisify(globalThis.setInterval);
+  var runCount = 0;
+  const start = performance.now();
+  for await (const run of setInterval(1)) {
+    if (runCount++ === 9) break;
+  }
+  const end = performance.now();
+
+  expect(runCount).toBe(10);
+  expect(end - start).toBeGreaterThan(9);
+});
+
+it("node.js util.promisify(setImmediate) works", async () => {
+  const setImmediate = promisify(globalThis.setImmediate);
+  await setImmediate();
+
+  expect(async () => {
+    await setImmediate().then(a => {
+      throw new Error("TestPassed");
+    });
+  }).toThrow("TestPassed");
+});

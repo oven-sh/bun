2418945004a8309050347e7cdcc44673485c153f
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2024-03-20 20:16:18 -0700

    bundows

--- test/js/bun/util/text-loader-fixture-dynamic-import-stress.ts
+++ test/js/bun/util/text-loader-fixture-dynamic-import-stress.ts
@@ -1,4 +1,5 @@
-for (let i = 0; i < 100000; i++) {
+const count = process.platform === "win32" ? 10_000 : 100_000;
+for (let i = 0; i < count; i++) {
   await import("./text-loader-fixture-text-file.txt?" + i++);
 }
 

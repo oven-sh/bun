240ea8a33f11a6aa2aff1a7c1dba3022de258d3e
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2024-03-22 22:55:44 -0700

    Workaround for #9041

---
+++ test/regression/issue/09041.test.ts
@@ -0,0 +1,26 @@
+import { test, expect } from "bun:test";
+import { bunEnv, bunExe, tempDirWithFiles } from "harness";
+import { join } from "path";
+import { $ } from "bun";
+import { cp, rm } from "fs/promises";
+test("09041", async () => {
+  const out = tempDirWithFiles("09041", {
+    "09041-fixture.mjs": await Bun.file(join(import.meta.dir, "09041", "09041-fixture.mjs")).text(),
+    "09041-fixture.test.js": await Bun.file(join(import.meta.dir, "09041", "09041-fixture-test.txt")).text(),
+    "package.json": `{}`,
+  });
+
+  let { exited, stderr, stdout } = Bun.spawn({
+    cmd: [bunExe(), "test"],
+    cwd: out,
+    env: bunEnv,
+    stdio: ["ignore", "pipe", "inherit"],
+  });
+
+  expect(await exited).toBe(0);
+  const err = await new Response(stderr).text();
+  expect(err).toContain("1 pass");
+  expect(err).toContain("0 fail");
+
+  await rm(out, { force: true, recursive: true });
+}, 10000);

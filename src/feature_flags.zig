2dabf1337e1e7833d0fe4c90d91d25ace93fb8f4
Jarred Sumner <jarred@jarredsumner.com>
2024-03-26 13:53:58 -0700

    what happens if nonblocking tty (#9608)
    Co-authored-by: Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>

--- src/feature_flags.zig
+++ src/feature_flags.zig
@@ -167,3 +167,8 @@ pub const runtime_transpiler_cache = true;
 pub const windows_bunx_fast_path = true;
 
 pub const breaking_changes_1_1_0 = false;
+
+// This causes strange bugs where writing via console.log (sync) has a different
+// order than via Bun.file.writer() so we turn it off until there's a unified,
+// buffered writer abstraction shared throughout Bun
+pub const nonblocking_stdout_and_stderr_on_posix = false;

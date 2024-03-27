57019dd3c8c3e731ef2ded3ca6dabf3061b72031
cirospaciari <ciro.spaciari@gmail.com>
2024-03-26 23:30:25 -0300

    is a Bun.sleep bug?

--- test/js/bun/net/keep-event-loop-alive.js
+++ test/js/bun/net/keep-event-loop-alive.js
@@ -1,47 +1,50 @@
-// (async () => {
 const port = process.argv[2] ? parseInt(process.argv[2]) : null;
 await Bun.sleep(10);
-// failed connection
-console.log("test 1: failed connection");
-try {
-  const socket = await Bun.connect({
-    hostname: "127.0.0.1",
-    port: 9999,
-    socket: { data() {} },
-  });
-  socket.end();
-} catch (error) {}
-// failed connection tls
-console.log("test 2: failed connection [tls]");
-try {
-  const socket = await Bun.connect({
-    hostname: "127.0.0.1",
-    port: 9999,
-    socket: { data() {} },
-    tls: true,
-  });
-  socket.end();
-} catch (error) {}
-if (port) {
-  // successful connection
-  console.log("test 3: successful connection");
-  const socket = await Bun.connect({
-    hostname: "127.0.0.1",
-    port,
-    socket: { data() {} },
-  });
-  socket.end();
 
-  // successful connection tls
-  console.log("test 4: successful connection [tls]");
-  const socket2 = await Bun.connect({
-    hostname: "127.0.0.1",
-    port,
-    socket: { data() {} },
-  });
-  socket2.end();
-} else {
-  console.log("run with a port as an argument to try the success situation");
-}
-console.log("success: event loop was not killed");
-// })();
+(async () => {
+  console.time("keep-event-loop-alive");
+  // failed connection
+  console.log("test 1: failed connection");
+  try {
+    const socket = await Bun.connect({
+      hostname: "127.0.0.1",
+      port: 9999,
+      socket: { data() {} },
+    });
+    socket.end();
+  } catch (error) {}
+  // failed connection tls
+  console.log("test 2: failed connection [tls]");
+  try {
+    const socket = await Bun.connect({
+      hostname: "127.0.0.1",
+      port: 9999,
+      socket: { data() {} },
+      tls: true,
+    });
+    socket.end();
+  } catch (error) {}
+  if (port) {
+    // successful connection
+    console.log("test 3: successful connection");
+    const socket = await Bun.connect({
+      hostname: "127.0.0.1",
+      port,
+      socket: { data() {} },
+    });
+    socket.end();
+
+    // successful connection tls
+    console.log("test 4: successful connection [tls]");
+    const socket2 = await Bun.connect({
+      hostname: "127.0.0.1",
+      port,
+      socket: { data() {} },
+    });
+    socket2.end();
+  } else {
+    console.log("run with a port as an argument to try the success situation");
+  }
+  console.log("success: event loop was not killed");
+  console.timeEnd("keep-event-loop-alive");
+})();

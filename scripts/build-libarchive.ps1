6f830ec8056d63a5cbe4765b7a3d91c7c824e751
dave caruso <me@paperdave.net>
2024-03-29 14:45:23 -0700

    does this fix the windows build

--- scripts/build-libarchive.ps1
+++ scripts/build-libarchive.ps1
@@ -5,7 +5,7 @@ Push-Location (Join-Path $BUN_DEPS_DIR 'libarchive')
 try {
   Set-Location (mkdir -Force build)
 
-  Run cmake @CMAKE_FLAGS -DBUILD_SHARED_LIBS=OFF -DENABLE_TEST=OFF -DENABLE_INSTALL=OFF -DENABLE_WERROR=0 ..
+  Run cmake @CMAKE_FLAGS -DBUILD_SHARED_LIBS=OFF -DENABLE_TEST=OFF -DENABLE_INSTALL=OFF -DENABLE_WERROR=0 -DENABLE_ICONV=0 ..
   Run cmake  --build . --clean-first --config Release
 
   Copy-Item libarchive\archive_static.lib $BUN_DEPS_OUT_DIR\archive.lib

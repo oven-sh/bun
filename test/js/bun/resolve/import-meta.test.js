276170ce8422c0a47996d0610d9a8fa39d102c4b
Meghan Denny <hello@nektro.net>
2024-03-30 18:45:44 -0700

    make name more accurate

--- test/js/bun/resolve/import-meta.test.js
+++ test/js/bun/resolve/import-meta.test.js
@@ -255,7 +255,7 @@ it("import.meta paths have the correct slash", () => {
   expect(import.meta.url).not.toInclude("\\");
 });
 
-it("import.meta is correct in a module that was imported with a query param: esm", async () => {
+it("import.meta is correct in a module that was imported with a query param", async () => {
   const esm = (await import("./other.js?foo=bar")).default;
 
   expect(esm.url).toBe(new URL("./other.js?foo=bar", import.meta.url).toString());
@@ -264,7 +264,7 @@ it("import.meta is correct in a module that was imported with a query param: esm
   expect(esm.file).toBe("other.js");
 });
 
-it("import.meta is correct in a module that was imported with a query param: cjs", async () => {
+it("import.meta is correct in a module that was required with a query param", async () => {
   const cjs = require("./other-cjs.js?foo=bar").meta;
   expect(cjs.url).toBe(new URL("./other-cjs.js?foo=bar", import.meta.url).toString());
   expect(cjs.path).toBe(join(import.meta.dir, "./other-cjs.js"));

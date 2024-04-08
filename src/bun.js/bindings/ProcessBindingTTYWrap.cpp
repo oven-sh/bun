e8df04a9a8b99488294866d7e02dd842d9cfc6f4
Jarred Sumner <jarred@jarredsumner.com>
2024-04-08 06:05:44 -0700

    Fixes #9952

--- src/bun.js/bindings/ProcessBindingTTYWrap.cpp
+++ src/bun.js/bindings/ProcessBindingTTYWrap.cpp
@@ -186,6 +186,8 @@ private:
     }
 };
 
+extern "C" void Bun__setCTRLHandler(BOOL add);
+
 const ClassInfo TTYWrapObject::s_info = {
     "LibuvStreamWrap"_s,
 
@@ -251,6 +253,10 @@ JSC_DEFINE_HOST_FUNCTION(TTYWrap_functionSetMode,
     }
 
 #if OS(WINDOWS)
+    if (mode.toInt32(globalObject) == 0) {
+        Bun__setCTRLHandler(1);
+    }
+
     int err = uv_tty_set_mode(ttyWrap->handle->tty(), mode.toInt32(globalObject));
 #else
     // Nodejs does not throw when ttySetMode fails. An Error event is emitted instead.

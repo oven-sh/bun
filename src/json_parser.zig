98bc5a7b18e8c103ec6df8ca626ae41af08557f4
Jarred Sumner <jarred@jarredsumner.com>
2024-04-15 21:41:05 -0700

    Always decode

--- src/json_parser.zig
+++ src/json_parser.zig
@@ -774,6 +774,42 @@ pub fn ParsePackageJSONUTF8(
     return try parser.parseExpr(false, true);
 }
 
+pub fn ParsePackageJSONUTF8AlwaysDecode(
+    source: *const logger.Source,
+    log: *logger.Log,
+    allocator: std.mem.Allocator,
+) !Expr {
+    const len = source.contents.len;
+
+    switch (len) {
+        // This is to be consisntent with how disabled JS files are handled
+        0 => {
+            return Expr{ .loc = logger.Loc{ .start = 0 }, .data = empty_object_data };
+        },
+        // This is a fast pass I guess
+        2 => {
+            if (strings.eqlComptime(source.contents[0..1], "\"\"") or strings.eqlComptime(source.contents[0..1], "''")) {
+                return Expr{ .loc = logger.Loc{ .start = 0 }, .data = empty_string_data };
+            } else if (strings.eqlComptime(source.contents[0..1], "{}")) {
+                return Expr{ .loc = logger.Loc{ .start = 0 }, .data = empty_object_data };
+            } else if (strings.eqlComptime(source.contents[0..1], "[]")) {
+                return Expr{ .loc = logger.Loc{ .start = 0 }, .data = empty_array_data };
+            }
+        },
+        else => {},
+    }
+
+    var parser = try JSONLikeParser(.{
+        .is_json = true,
+        .always_decode_escape_sequences = true,
+        .allow_comments = true,
+        .allow_trailing_commas = true,
+    }).init(allocator, source.*, log);
+    bun.assert(parser.source().contents.len > 0);
+
+    return try parser.parseExpr(false, true);
+}
+
 /// Parse Package JSON
 /// Allow trailing commas & comments.
 /// This eagerly transcodes UTF-16 strings into UTF-8 strings

468a869ad2f278a93b49d854608b08bd95cc58cb
Dylan Conway <dylan.conway567@gmail.com>
2024-04-20 05:48:18 -0700

    handle thread name exception

--- src/crash_handler.zig
+++ src/crash_handler.zig
@@ -682,6 +682,13 @@ pub fn handleSegfaultWindows(info: *windows.EXCEPTION_POINTERS) callconv(windows
             windows.EXCEPTION_ACCESS_VIOLATION => .{ .segmentation_fault = info.ExceptionRecord.ExceptionInformation[1] },
             windows.EXCEPTION_ILLEGAL_INSTRUCTION => .{ .illegal_instruction = info.ContextRecord.getRegs().ip },
             windows.EXCEPTION_STACK_OVERFLOW => .{ .stack_overflow = {} },
+
+            // exception used for thread naming
+            // https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-2017/debugger/how-to-set-a-thread-name-in-native-code?view=vs-2017#set-a-thread-name-by-throwing-an-exception
+            // related commit
+            // https://github.com/go-delve/delve/pull/1384
+            bun.windows.MS_VC_EXCEPTION => return bun.windows.EXCEPTION_CONTINUE_EXECUTION,
+
             else => return windows.EXCEPTION_CONTINUE_SEARCH,
         },
         null,
@@ -1330,11 +1337,11 @@ pub fn dumpStackTrace(trace: std.builtin.StackTrace) void {
     }
     // TODO: It would be reasonable, but hacky, to spawn LLVM-symbolizer here in
     // order to get the demangled stack traces.
-    var name_bytes: [1024]u8 = undefined;
-    for (trace.instruction_addresses[0..trace.index]) |addr| {
-        const line = StackLine.fromAddress(addr, &name_bytes);
-        stderr.print("- {}\n", .{line}) catch break;
-    }
+    // var name_bytes: [1024]u8 = undefined;
+    // for (trace.instruction_addresses[0..trace.index]) |addr| {
+    //     const line = StackLine.fromAddress(addr, &name_bytes);
+    //     stderr.print("- {}\n", .{line}) catch break;
+    // }
     const program = switch (bun.Environment.os) {
         .windows => "pdb-addr2line",
         else => "llvm-symbolizer",

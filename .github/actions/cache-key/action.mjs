668a3a292e6b8a11cbf68f2fcdb8259d590081b9
Ashcon Partovi <ashcon@partovi.net>
2024-04-23 15:04:36 -0700

    Testing workflows

---
+++ .github/actions/cache-key/action.mjs
@@ -0,0 +1,27 @@
+import { spawnSync } from "node:fs";
+import { createHash } from "node:crypto";
+import { getInput, setOutput } from "@actions/core";
+
+const paths = getInput("paths", { required: true });
+
+function getCacheKey() {
+  const { error, status, stdout, stderr } = spawnSync("git", ["ls-files", "-s", ...paths], {
+    encoding: "utf-8",
+    stdio: "pipe",
+  });
+  if (error) {
+    throw error;
+  }
+  if (status !== 0) {
+    throw new Error(`git ls-files failed: ${stderr}`);
+  }
+  return createHash("sha256").update(stdout).digest("hex");
+}
+
+function main() {
+  const cacheKey = getCacheKey();
+  console.log("Cache key:", cacheKey);
+  setOutput("cache-key", cacheKey);
+}
+
+main();

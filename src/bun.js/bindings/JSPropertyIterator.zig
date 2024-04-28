2d78a390090bdcd96d7eed3d288ccd5d669d645b
Dylan Conway <dylan.conway567@gmail.com>
2024-04-28 13:02:11 -0700

    fix creating identifiers from empty strings

--- src/bun.js/bindings/JSPropertyIterator.zig
+++ src/bun.js/bindings/JSPropertyIterator.zig
@@ -59,7 +59,7 @@ pub fn JSPropertyIterator(comptime options: JSPropertyIteratorOptions) type {
             if (comptime options.include_value) {
                 const current = Bun__JSPropertyIterator__getNameAndValue(this.impl, this.globalObject, this.object, &name, i);
                 if (current.isEmpty()) {
-                    return null;
+                    return this.next();
                 }
                 current.ensureStillAlive();
                 this.value = current;
@@ -68,12 +68,12 @@ pub fn JSPropertyIterator(comptime options: JSPropertyIteratorOptions) type {
             }
 
             if (name.tag == .Dead) {
-                return null;
+                return this.next();
             }
 
             if (comptime options.skip_empty_name) {
                 if (name.isEmpty()) {
-                    return null;
+                    return this.next();
                 }
             }
 

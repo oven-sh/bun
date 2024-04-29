a7ba2624c62b403cb26bcc18305e202c9f8be0df
Dylan Conway <35280289+dylan-conway@users.noreply.github.com>
2024-04-29 16:59:07 -0700

    fix flaky `bun-create.test.ts` with git templates (#10662)

--- src/cli.zig
+++ src/cli.zig
@@ -2206,7 +2206,7 @@ pub const Command = struct {
                         \\  <b><green>bun create<r> <blue>\<username/repo\><r> <cyan>[...flags]<r> <blue>[dest]<r>
                         \\
                         \\<b>Environment variables:<r>
-                        \\  <cyan>GITHUB_ACCESS_TOKEN<r>      <d>Supply a token to download code from GitHub with a higher rate limit<r>
+                        \\  <cyan>GITHUB_TOKEN<r>             <d>Supply a token to download code from GitHub with a higher rate limit<r>
                         \\  <cyan>GITHUB_API_DOMAIN<r>        <d>Configure custom/enterprise GitHub domain. Default "api.github.com".<r>
                         \\  <cyan>NPM_CLIENT<r>               <d>Absolute path to the npm client executable<r>
                     ;

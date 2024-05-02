5d7b3ab579a7e812a941eb8d212534c8376544b0
Dylan Conway <35280289+dylan-conway@users.noreply.github.com>
2024-05-01 19:51:45 -0700

    upgrade webkit (#10757)

--- src/bun.js/bindings/webcore/JSDOMException.h
+++ src/bun.js/bindings/webcore/JSDOMException.h
@@ -69,7 +69,7 @@ protected:
 
 class JSDOMExceptionOwner final : public JSC::WeakHandleOwner {
 public:
-    bool isReachableFromOpaqueRoots(JSC::Handle<JSC::Unknown>, void* context, JSC::AbstractSlotVisitor&, const char**) final;
+    bool isReachableFromOpaqueRoots(JSC::Handle<JSC::Unknown>, void* context, JSC::AbstractSlotVisitor&, ASCIILiteral*) final;
     void finalize(JSC::Handle<JSC::Unknown>, void* context) final;
 };
 

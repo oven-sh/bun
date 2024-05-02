75fc19ba067d058a6d18a71558e2f04aa39195a4
Dylan Conway <dylan.conway567@gmail.com>
2024-05-01 20:21:33 -0700

    undo more

--- src/cli/add_command.zig
+++ src/cli/add_command.zig
@@ -1,6 +1,5 @@
 const Command = @import("../cli.zig").Command;
 const PackageManager = @import("../install/install.zig").PackageManager;
-const bun = @import("root").bun;
 
 pub const AddCommand = struct {
     pub fn exec(ctx: Command.Context) !void {

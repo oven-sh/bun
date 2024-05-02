8e1bc6c84f9399174a8952d1836d6bcde7654c59
Dylan Conway <dylan.conway567@gmail.com>
2024-05-01 20:07:07 -0700

    catch error

--- src/cli/add_command.zig
+++ src/cli/add_command.zig
@@ -1,8 +1,18 @@
 const Command = @import("../cli.zig").Command;
 const PackageManager = @import("../install/install.zig").PackageManager;
+const bun = @import("root").bun;
 
 pub const AddCommand = struct {
     pub fn exec(ctx: Command.Context) !void {
-        try PackageManager.add(ctx);
+        PackageManager.add(ctx) catch |err| switch (err) {
+            error.InstallFailed,
+            error.InvalidPackageJSON,
+            => {
+                const log = &bun.CLI.Cli.log_;
+                log.printForLogLevel(bun.Output.errorWriter()) catch {};
+                bun.Global.exit(1);
+            },
+            else => |e| return e,
+        };
     }
 };

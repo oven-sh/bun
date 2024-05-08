1bce45528b817af8c049f38752bdcedd35e3dd9f
dave caruso <me@paperdave.net>
2024-05-08 14:57:15 -0700

    chore: fix the build scripts again (#10912)
    Co-authored-by: Jarred Sumner <jarred@jarredsumner.com>

--- scripts/build-libarchive.ps1
+++ scripts/build-libarchive.ps1
@@ -6,7 +6,7 @@ try {
   Set-Location (mkdir -Force build)
 
   Run cmake @CMAKE_FLAGS -DBUILD_SHARED_LIBS=OFF -DENABLE_TEST=OFF -DENABLE_INSTALL=OFF -DENABLE_WERROR=0 -DENABLE_ICONV=0 -DENABLE_LibGCC=0 -DENABLE_LZMA=0 -DENABLE_LZ4=0 -DENABLE_LIBXML2=0 -DENABLE_LIBB2=0 -DENABLE_OPENSSL=0 -DENABLE_CAT=0 ..
-  Run cmake  --build . --clean-first --config Release
+  Run cmake  --build . --clean-first --config Release --verbose
 
   Copy-Item libarchive\archive_static.lib $BUN_DEPS_OUT_DIR\archive.lib
   Write-Host "-> archive.lib"

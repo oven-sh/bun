ef87d0854c02bcdabcc4b563e1639c056c3b6fe7
gvilums <gvilums@users.noreply.github.com>
2024-05-08 21:07:00 +0000

    Apply formatting changes

--- test/js/node/process/process-onUncaughtException.js
+++ test/js/node/process/process-onUncaughtException.js
@@ -9,21 +9,21 @@ setTimeout(() => {
   process.exit(42);
 }, 1);
 
-process.on("uncaughtExceptionMonitor", (err) => {
+process.on("uncaughtExceptionMonitor", err => {
   monitorCalled = true;
   if (!err) {
     process.exit(1);
   }
 });
 
-process.on("uncaughtException", (err) => {
+process.on("uncaughtException", err => {
   // there should be an error
   if (!err) {
     process.exit(1);
   }
-})
+});
 
-throw new Error('error');
+throw new Error("error");
 
 // this shouldn't be hit even if the exception is caught
 process.exit(1);

d54bba08fa07239f07c1397477cf7fea2e3c7416
Jarred Sumner <jarred@jarredsumner.com>
2024-05-10 05:21:31 -0700

    Set -ffdata-sections -ffunction-sections

--- scripts/env.sh
+++ scripts/env.sh
@@ -18,21 +18,26 @@ export CMAKE_C_COMPILER=${CC}
 export CFLAGS='-O3 -fno-exceptions -fvisibility=hidden -fvisibility-inlines-hidden -mno-omit-leaf-frame-pointer -fno-omit-frame-pointer'
 export CXXFLAGS='-O3 -fno-exceptions -fno-rtti -fvisibility=hidden -fvisibility-inlines-hidden -mno-omit-leaf-frame-pointer -fno-omit-frame-pointer'
 
+if [[ $(uname -s) == 'Linux' ]]; then
+    export CFLAGS+=(' -ffunction-sections -fdata-sections -fPIC')
+    export CXXFLAGS+=(' -ffunction-sections -fdata-sections -fPIC')
+fi
+
 export CMAKE_FLAGS=(
-  -DCMAKE_C_COMPILER="${CC}"
-  -DCMAKE_CXX_COMPILER="${CXX}"
-  -DCMAKE_C_FLAGS="$CFLAGS"
-  -DCMAKE_CXX_FLAGS="$CXXFLAGS"
-  -DCMAKE_BUILD_TYPE=Release
-  -DCMAKE_CXX_STANDARD=20
-  -DCMAKE_C_STANDARD=17
-  -DCMAKE_CXX_STANDARD_REQUIRED=ON
-  -DCMAKE_C_STANDARD_REQUIRED=ON
+    -DCMAKE_C_COMPILER="${CC}"
+    -DCMAKE_CXX_COMPILER="${CXX}"
+    -DCMAKE_C_FLAGS="$CFLAGS"
+    -DCMAKE_CXX_FLAGS="$CXXFLAGS"
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_CXX_STANDARD=20
+    -DCMAKE_C_STANDARD=17
+    -DCMAKE_CXX_STANDARD_REQUIRED=ON
+    -DCMAKE_C_STANDARD_REQUIRED=ON
 )
 
 if [[ $(uname -s) == 'Linux' ]]; then
     # Ensure we always use -std=gnu++20 on Linux
-    export CMAKE_FLAGS+=( -DCMAKE_CXX_EXTENSIONS=ON )
+    export CMAKE_FLAGS+=(-DCMAKE_CXX_EXTENSIONS=ON)
 fi
 
 if [[ $(uname -s) == 'Darwin' ]]; then

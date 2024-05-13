7f80ea844aaaf22960d77c2f4181bb82ba4bad3b
Georgijs Vilums <=>
2024-05-13 11:44:29 -0700

    make test more comprehensive

--- test/js/node/stream/node-stream.test.js
+++ test/js/node/stream/node-stream.test.js
@@ -1,4 +1,4 @@
-import { expect, describe, it } from "bun:test";
+import { expect, describe, it, jest } from "bun:test";
 import { Stream, Readable, Writable, Duplex, Transform, PassThrough } from "node:stream";
 import { createReadStream } from "node:fs";
 import { join } from "path";
@@ -590,9 +590,11 @@ it("should send Readable events in the right order", async () => {
 });
 
 it("emits newListener event _before_ adding the listener", () => {
-  const stream = new Stream();
-  stream.on("newListener", event => {
+  const cb = jest.fn((event) => {
     expect(stream.listenerCount(event)).toBe(0);
   });
+  const stream = new Stream();
+  stream.on("newListener", cb);
   stream.on("foo", () => {});
+  expect(cb).toHaveBeenCalled();
 });

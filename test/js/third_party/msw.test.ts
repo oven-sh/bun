434e21e02bb1d6e7c2e0babf88d296061e8c8714
Meghan Denny <meghan@bun.sh>
2024-05-14 22:47:32 -0700

    test: msw: use expect().toRun()

--- test/js/third_party/msw.test.ts
+++ test/js/third_party/msw.test.ts
@@ -1,19 +1,7 @@
-import { bunExe } from "bun:harness";
-import { bunEnv, runBunInstall, tmpdirSync } from "harness";
+import "harness";
 import { expect, it } from "bun:test";
 import * as path from "node:path";
 
 it("works", async () => {
-  let { stdout, stderr, exited } = Bun.spawn({
-    cmd: [bunExe(), "run", path.join(import.meta.dirname, "_fixtures", "msw.ts")],
-    stdout: "pipe",
-    stdin: "ignore",
-    stderr: "pipe",
-    env: bunEnv,
-  });
-  let err = await new Response(stderr).text();
-  expect(err).toBeEmpty();
-  let out = await new Response(stdout).text();
-  expect(out).toEqual("2\n");
-  expect(await exited).toBe(0);
+  expect([path.join(import.meta.dirname, "_fixtures", "msw.ts")]).toRun("2\n");
 });

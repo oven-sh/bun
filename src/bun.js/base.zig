c4ed47d50315ad0235c001691c65379a7a511a4f
cirospaciari <ciro.spaciari@gmail.com>
2024-05-15 23:01:41 -0300

    opsie

--- src/bun.js/base.zig
+++ src/bun.js/base.zig
@@ -1527,6 +1527,7 @@ pub const Ref = struct {
 
 pub const Strong = @import("./Strong.zig").Strong;
 pub const Weak = @import("./Weak.zig").Weak;
+pub const WeakRefType = @import("./Weak.zig").WeakRefType;
 
 pub const BinaryType = enum {
     Buffer,

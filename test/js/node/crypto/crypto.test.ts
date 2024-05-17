3da0527aa0abb1c48a1177d02a5948b41d98fa37
Meghan Denny <meghan@bun.sh>
2024-05-16 21:40:05 -0700

    node:crypto: add shake128 and shake256

--- test/js/node/crypto/crypto.test.ts
+++ test/js/node/crypto/crypto.test.ts
@@ -25,6 +25,8 @@ describe("CryptoHasher", () => {
       "sha3-256",
       "sha3-384",
       "sha3-512",
+      "shake128",
+      "shake256",
     ]);
   });
 
@@ -46,6 +48,8 @@ describe("CryptoHasher", () => {
     "sha3-256": "644bcc7e564373040999aac89e7622f3ca71fba1d972fd94a31c3bfbf24e3938",
     "sha3-384": "83bff28dde1b1bf5810071c6643c08e5b05bdb836effd70b403ea8ea0a634dc4997eb1053aa3593f590f9c63630dd90b",
     "sha3-512": "840006653e9ac9e95117a15c915caab81662918e925de9e004f774ff82d7079a40d4d27b1b372657c61d46d470304c88c788b3a4527ad074d1dccbee5dbaa99a",
+    shake128: "3a9159f071e4dd1c8c4f968607c30942",
+    shake256: "369771bb2cb9d2b04c1d54cca487e372d9f187f73f7ba3f65b95c8ee7798c527",
   } as const;
 
   for (const algorithm of CryptoHasher.algorithms) {

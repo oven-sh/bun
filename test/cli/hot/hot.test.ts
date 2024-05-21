e877090858abcd8d5a4b582652b82c7e347396b4
dave caruso <me@paperdave.net>
2024-05-21 12:47:13 -0700

    a

--- test/cli/hot/hot.test.ts
+++ test/cli/hot/hot.test.ts
@@ -402,8 +402,8 @@ throw new Error('0');`,
     cmd: [bunExe(), "build", "--watch", bundleIn, "--target=bun", "--sourcemap", "--outfile", hotRunnerRoot],
     env: bunEnv,
     cwd,
-    stdout: "inherit",
-    stderr: "inherit",
+    stdout: "pipe",
+    stderr: "pipe",
     stdin: "ignore",
   });
   await using runner = spawn({

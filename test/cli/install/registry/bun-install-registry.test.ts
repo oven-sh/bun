fe4ceec1df2728ca503fe1479e3bc8963ae8080b
Meghan Denny <meghan@bun.sh>
2024-05-21 23:14:10 -0700

    ci: make bun-install-registry.test.ts less flaky on windows (#11253)

--- test/cli/install/registry/bun-install-registry.test.ts
+++ test/cli/install/registry/bun-install-registry.test.ts
@@ -5044,7 +5044,8 @@ for (const forceWaiterThread of isLinux ? [false, true] : [false]) {
         expect(proc.resourceUsage()?.cpuTime.total).toBeLessThan(750_000);
       });
 
-      test(
+      // https://github.com/oven-sh/bun/issues/11252
+      test.todoIf(isWindows)(
         "bun pm trust",
         async () => {
           const dep = isWindows ? "uses-what-bin-slow-window" : "uses-what-bin-slow";

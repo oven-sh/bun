6255dfd49c4a82ff2215f15a0b9b033f4eddfb4a
Jarred Sumner <jarred@jarredsumner.com>
2024-05-23 02:53:42 -0700

    Fix build

--- src/install/npm.zig
+++ src/install/npm.zig
@@ -708,7 +708,7 @@ pub const PackageManifest = struct {
                 try bun.sys.renameat(bun.FD.cwd(), tmp_path_abs, bun.FD.cwd(), cache_path_abs).unwrap();
             } else {
                 defer file.close();
-                try bun.sys.renameat(tmpdir, tmp_path, cache_dir, outpath).unwrap();
+                try bun.sys.renameat(bun.toFD(tmpdir), tmp_path, bun.toFD(cache_dir), outpath).unwrap();
             }
         }
 

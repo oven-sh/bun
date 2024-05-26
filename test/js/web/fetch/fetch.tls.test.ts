5efa9d922af1fa348b8ddd7564f33d0e65da74c6
Jarred Sumner <jarred@jarredsumner.com>
2024-05-26 04:09:28 -0700

    Speed up a couple tests (#11369)

--- test/js/web/fetch/fetch.tls.test.ts
+++ test/js/web/fetch/fetch.tls.test.ts
@@ -36,8 +36,8 @@ it("can handle multiple requests with non native checkServerIdentity", async () 
           return tls.checkServerIdentity(hostname, cert);
         },
       },
-    }).then((res: Response) => res.text());
-    expect(result?.length).toBeGreaterThan(0);
+    }).then((res: Response) => res.blob());
+    expect(result?.size).toBeGreaterThan(0);
     expect(called).toBe(true);
   }
   const promises = [];
@@ -48,35 +48,36 @@ it("can handle multiple requests with non native checkServerIdentity", async () 
 });
 
 it("fetch with valid tls should not throw", async () => {
-  const urls = ["https://bun.sh", "https://www.example.com"];
-  for (const url of urls) {
-    const result = await fetch(url, { keepalive: false }).then((res: Response) => res.text());
-    expect(result?.length).toBeGreaterThan(0);
-  }
+  const promises = [`https://bun.sh`, `https://www.example.com`].map(async url => {
+    const result = await fetch(url, { keepalive: false }).then((res: Response) => res.blob());
+    expect(result?.size).toBeGreaterThan(0);
+  });
+
+  await Promise.all(promises);
 });
 
 it("fetch with valid tls and non-native checkServerIdentity should work", async () => {
-  const urls = [`https://bun.sh`, `https://www.example.com`];
   let count = 0;
-  for (const url of urls) {
+  const promises = [`https://bun.sh`, `https://www.example.com`].map(async url => {
     await fetch(url, {
       keepalive: false,
       tls: {
         checkServerIdentity(hostname: string, cert: tls.PeerCertificate) {
           count++;
-          expect(["bun.sh", "www.example.com"]).toContain(hostname);
+          expect(url).toContain(hostname);
           return tls.checkServerIdentity(hostname, cert);
         },
       },
-    }).then((res: Response) => res.text());
-  }
+    }).then((res: Response) => res.blob());
+  });
+  await Promise.all(promises);
   expect(count).toBe(2);
 });
 
 it("fetch with rejectUnauthorized: false should not call checkServerIdentity", async () => {
   let count = 0;
 
-  await fetch("https://bun.sh", {
+  await fetch("https://example.com", {
     keepalive: false,
     tls: {
       rejectUnauthorized: false,
@@ -85,35 +86,38 @@ it("fetch with rejectUnauthorized: false should not call checkServerIdentity", a
         return tls.checkServerIdentity(hostname, cert);
       },
     },
-  }).then((res: Response) => res.text());
+  }).then((res: Response) => res.blob());
   expect(count).toBe(0);
 });
 
 it("fetch with self-sign tls should throw", async () => {
   await createServer(CERT_LOCALHOST_IP, async port => {
     const urls = [`https://localhost:${port}`, `https://127.0.0.1:${port}`];
-    for (const url of urls) {
-      try {
-        await fetch(url).then((res: Response) => res.text());
-        expect.unreachable();
-      } catch (e: any) {
-        expect(e.code).toBe("DEPTH_ZERO_SELF_SIGNED_CERT");
-      }
-    }
+    await Promise.all(
+      urls.map(async url => {
+        try {
+          await fetch(url).then((res: Response) => res.blob());
+          expect.unreachable();
+        } catch (e: any) {
+          expect(e.code).toBe("DEPTH_ZERO_SELF_SIGNED_CERT");
+        }
+      }),
+    );
   });
 });
 
 it("fetch with invalid tls should throw", async () => {
   await createServer(CERT_EXPIRED, async port => {
-    const urls = [`https://localhost:${port}`, `https://127.0.0.1:${port}`];
-    for (const url of urls) {
-      try {
-        await fetch(url).then((res: Response) => res.text());
-        expect.unreachable();
-      } catch (e: any) {
-        expect(e.code).toBe("CERT_HAS_EXPIRED");
-      }
-    }
+    await Promise.all(
+      [`https://localhost:${port}`, `https://127.0.0.1:${port}`].map(async url => {
+        try {
+          await fetch(url).then((res: Response) => res.blob());
+          expect.unreachable();
+        } catch (e: any) {
+          expect(e.code).toBe("CERT_HAS_EXPIRED");
+        }
+      }),
+    );
   });
 });
 
@@ -126,7 +130,7 @@ it("fetch with checkServerIdentity failing should throw", async () => {
           return new Error("CustomError");
         },
       },
-    }).then((res: Response) => res.text());
+    }).then((res: Response) => res.blob());
 
     expect.unreachable();
   } catch (e: any) {
@@ -137,28 +141,32 @@ it("fetch with checkServerIdentity failing should throw", async () => {
 it("fetch with self-sign certificate tls + rejectUnauthorized: false should not throw", async () => {
   await createServer(CERT_LOCALHOST_IP, async port => {
     const urls = [`https://localhost:${port}`, `https://127.0.0.1:${port}`];
-    for (const url of urls) {
-      try {
-        const result = await fetch(url, { tls: { rejectUnauthorized: false } }).then((res: Response) => res.text());
-        expect(result).toBe("Hello World");
-      } catch {
-        expect.unreachable();
-      }
-    }
+    await Promise.all(
+      urls.map(async url => {
+        try {
+          const result = await fetch(url, { tls: { rejectUnauthorized: false } }).then((res: Response) => res.text());
+          expect(result).toBe("Hello World");
+        } catch {
+          expect.unreachable();
+        }
+      }),
+    );
   });
 });
 
 it("fetch with invalid tls + rejectUnauthorized: false should not throw", async () => {
   await createServer(CERT_EXPIRED, async port => {
     const urls = [`https://localhost:${port}`, `https://127.0.0.1:${port}`];
-    for (const url of urls) {
-      try {
-        const result = await fetch(url, { tls: { rejectUnauthorized: false } }).then((res: Response) => res.text());
-        expect(result).toBe("Hello World");
-      } catch {
-        expect.unreachable();
-      }
-    }
+    await Promise.all(
+      urls.map(async url => {
+        try {
+          const result = await fetch(url, { tls: { rejectUnauthorized: false } }).then((res: Response) => res.text());
+          expect(result).toBe("Hello World");
+        } catch {
+          expect.unreachable();
+        }
+      }),
+    );
   });
 });
 

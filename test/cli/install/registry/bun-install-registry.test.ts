3839a0a94816a4b2fbad6b859c6f409a58cda08a
Dylan Conway <dylan.conway567@gmail.com>
2024-05-29 22:43:16 -0700

    fix

--- test/cli/install/registry/bun-install-registry.test.ts
+++ test/cli/install/registry/bun-install-registry.test.ts
@@ -30,27 +30,27 @@ var verdaccioServer: ChildProcess;
 var port: number = 4873;
 var packageDir: string;
 
-// beforeAll(async () => {
-//   setDefaultTimeout(1000 * 60 * 5);
-//   verdaccioServer = fork(
-//     require.resolve("verdaccio/bin/verdaccio"),
-//     ["-c", join(import.meta.dir, "verdaccio.yaml"), "-l", `${port}`],
-//     { silent: true, execPath: "bun" },
-//   );
+beforeAll(async () => {
+  setDefaultTimeout(1000 * 60 * 5);
+  verdaccioServer = fork(
+    require.resolve("verdaccio/bin/verdaccio"),
+    ["-c", join(import.meta.dir, "verdaccio.yaml"), "-l", `${port}`],
+    { silent: true, execPath: "bun" },
+  );
 
-//   await new Promise<void>(done => {
-//     verdaccioServer.on("message", (msg: { verdaccio_started: boolean }) => {
-//       if (msg.verdaccio_started) {
-//         console.log("Verdaccio started");
-//         done();
-//       }
-//     });
-//   });
-// });
+  await new Promise<void>(done => {
+    verdaccioServer.on("message", (msg: { verdaccio_started: boolean }) => {
+      if (msg.verdaccio_started) {
+        console.log("Verdaccio started");
+        done();
+      }
+    });
+  });
+});
 
-// afterAll(() => {
-//   verdaccioServer.kill();
-// });
+afterAll(() => {
+  verdaccioServer.kill();
+});
 
 beforeEach(async () => {
   packageDir = tmpdirSync();
@@ -2313,7 +2313,7 @@ describe("workspaces", async () => {
   }
 });
 
-describe.only("update", () => {
+describe("update", () => {
   test("dist-tags", async () => {
     await write(
       join(packageDir, "package.json"),

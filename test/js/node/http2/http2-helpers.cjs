b0b8d8d2fe469769fcdebcc98eae3de11b63d29c
Ciro Spaciari <ciro.spaciari@gmail.com>
2024-05-30 17:24:27 -0300

    fix(tests) update http2 cert in tests (#11462)

--- test/js/node/http2/http2-helpers.cjs
+++ test/js/node/http2/http2-helpers.cjs
@@ -1,14 +1,15 @@
 const path = require("path");
 
-module.exports.TLS_CERT = require("./tls-cert.cjs").TLS_CERT;
-module.exports.TLS_OPTIONS = require("./tls-cert.cjs").TLS_OPTIONS;
+const TLS_CERT = require("harness").tls;
+module.exports.TLS_CERT = TLS_CERT;
+module.exports.TLS_OPTIONS = { ca: TLS_CERT.cert };
 const nodeExecutable = typeof Bun !== "undefined" ? Bun.which("node") : "node";
 
 exports.nodeEchoServer = async function nodeEchoServer() {
   if (!nodeExecutable) throw new Error("node executable not found");
   const subprocess = require("child_process").spawn(
     nodeExecutable,
-    [path.join(__dirname, "node-echo-server.fixture.js")],
+    [path.join(__dirname, "node-echo-server.fixture.js"), JSON.stringify(TLS_CERT)],
     {
       stdout: "pipe",
       stderr: "inherit",

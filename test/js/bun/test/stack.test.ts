8f66d0eba23acbcf2cec1295254124f8c50a3470
Jarred Sumner <jarred@jarredsumner.com>
2024-05-30 17:30:42 -0700

    Add a todo

--- test/js/bun/test/stack.test.ts
+++ test/js/bun/test/stack.test.ts
@@ -37,3 +37,32 @@ test("name property is used for function calls in Bun.inspect", () => {
   expect(WRONG()).not.toContain("at WRONG");
   expect(WRONG()).toContain("at RIGHT");
 });
+
+test.todo("name property is used for function calls in Bun.inspect with bound object", () => {
+  // ** whitespace **
+  // ** whitespace **
+  // ** whitespace **
+  // ** whitespace **
+  // ** whitespace **
+  // ** whitespace **
+  let WRONG = function WRONG() {
+    try {
+      throw new Error();
+    } catch (e) {
+      return Bun.inspect(e);
+    }
+  };
+  WRONG = WRONG.bind({});
+  // ** whitespace **
+  // ** whitespace **
+  // ** whitespace **
+  // ** whitespace **
+  // ** whitespace **
+  // ** whitespace **
+  expect(WRONG()).not.toContain("at RIGHT");
+  expect(WRONG()).toContain("at WRONG");
+  Object.defineProperty(WRONG, "name", { value: "RIGHT", writable: true, configurable: true });
+  console.log(WRONG());
+  expect(WRONG()).not.toContain("at WRONG");
+  expect(WRONG()).toContain("at RIGHT");
+});

248f798031a9c6384fcdcb0384f58dd031334433
Jarred Sumner <jarred@jarredsumner.com>
2024-06-05 20:35:47 -0700

    Improve the error message when Bun.serve() is passed a non-Response object (#11562)
    Co-authored-by: dave caruso <me@paperdave.net>

--- src/bun.js/api/server.zig
+++ src/bun.js/api/server.zig
@@ -1403,16 +1403,42 @@ fn NewRequestContext(comptime ssl_enabled: bool, comptime debug_mode: bool, comp
             return JSValue.jsUndefined();
         }
 
+        fn renderMissingInvalidResponse(ctx: *RequestContext, value: JSC.JSValue) void {
+            var class_name = value.getClassInfoName() orelse bun.String.empty;
+            defer class_name.deref();
+            const globalThis: *JSC.JSGlobalObject = ctx.server.globalThis;
+
+            Output.enableBuffering();
+            var writer = Output.errorWriter();
+
+            if (class_name.eqlComptime("Response")) {
+                Output.errGeneric("Expected a native Response object, but received a polyfilled Response object. Bun.serve() only supports native Response objects.", .{});
+            } else if (!value.isEmpty() and !globalThis.hasException()) {
+                var formatter = JSC.ConsoleObject.Formatter{
+                    .globalThis = globalThis,
+                    .quote_strings = true,
+                };
+                Output.errGeneric("Expected a Response object, but received '{}'", .{value.toFmt(formatter.globalThis, &formatter)});
+            } else {
+                Output.errGeneric("Expected a Response object", .{});
+            }
+
+            Output.flush();
+            if (!globalThis.hasException()) {
+                JSC.ConsoleObject.writeTrace(@TypeOf(&writer), &writer, globalThis);
+            }
+            Output.flush();
+            ctx.renderMissing();
+        }
+
         fn handleResolve(ctx: *RequestContext, value: JSC.JSValue) void {
             if (value.isEmptyOrUndefinedOrNull() or !value.isCell()) {
-                ctx.renderMissing();
+                ctx.renderMissingInvalidResponse(value);
                 return;
             }
 
             const response = value.as(JSC.WebCore.Response) orelse {
-                Output.prettyErrorln("Expected a Response object", .{});
-                Output.flush();
-                ctx.renderMissing();
+                ctx.renderMissingInvalidResponse(value);
                 return;
             };
             ctx.response_jsvalue = value;
@@ -2558,7 +2584,7 @@ fn NewRequestContext(comptime ssl_enabled: bool, comptime debug_mode: bool, comp
             }
 
             if (response_value.isEmptyOrUndefinedOrNull()) {
-                ctx.renderMissing();
+                ctx.renderMissingInvalidResponse(response_value);
                 return;
             }
 
@@ -2611,11 +2637,11 @@ fn NewRequestContext(comptime ssl_enabled: bool, comptime debug_mode: bool, comp
                         }
 
                         if (fulfilled_value.isEmptyOrUndefinedOrNull()) {
-                            ctx.renderMissing();
+                            ctx.renderMissingInvalidResponse(fulfilled_value);
                             return;
                         }
                         var response = fulfilled_value.as(JSC.WebCore.Response) orelse {
-                            ctx.renderMissing();
+                            ctx.renderMissingInvalidResponse(fulfilled_value);
                             return;
                         };
 

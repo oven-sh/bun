4b14b2fc2d546ce30a83e9979e4ccd5aafe88da7
Ashcon Partovi <ashcon@partovi.net>
2024-06-06 13:01:59 -0700

    Better scripts

--- scripts/build-bun-cpp.ps1
+++ scripts/build-bun-cpp.ps1
@@ -1,12 +1,12 @@
 param (
-  [Parameter(Mandatory)][bool]$IsBaseline = $False,
+  [Parameter][bool]$Baseline = $False,
   [Parameter][bool]$Fast = $False
 )
 
 $ErrorActionPreference = 'Stop'  # Setting strict mode, similar to 'set -euo pipefail' in bash
 
-$Tag = If ($IsBaseline) { "-Baseline" } Else { "" }
-$Flags = If ($IsBaseline) { "-DUSE_BASELINE_BUILD=1" } Else { "" }
+$Tag = If ($Baseline) { "-Baseline" } Else { "" }
+$UseBaselineBuild = If ($Baseline) { "ON" } Else { "OFF" }
 $UseLto = If ($Fast) { "OFF" } Else { "ON" }
 
 # $CANARY_REVISION = if (Test-Path build/.canary_revision) { Get-Content build/.canary_revision } else { "0" }
@@ -19,8 +19,9 @@ cd build
 cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release `
   -DNO_CODEGEN=1 `
   -DNO_CONFIGURE_DEPENDS=1 `
-  "-DCANARY=${CANARY_REVISION}" `
+  "-DUSE_BASELINE_BUILD=${UseBaselineBuild}" `
   "-DUSE_LTO=${UseLto}" `
+  "-DCANARY=${CANARY_REVISION}" `
   -DBUN_CPP_ONLY=1 $Flags
 if ($LASTEXITCODE -ne 0) { throw "CMake configuration failed" }
 

d2b61a88c013d30ff95b2d8053324b2aaea3a064
dave caruso <me@paperdave.net>
2024-06-05 18:43:18 -0700

    Bump to v1.1.13

--- scripts/bump.ts
+++ scripts/bump.ts
@@ -2,13 +2,6 @@ import path from "path";
 
 process.chdir(path.join(import.meta.dir, "../"));
 
-const git_branch = await Bun.$`git rev-parse --abbrev-ref HEAD`.text();
-
-if (git_branch.trim() !== "main") {
-  console.error("You must be on the main branch to run this script");
-  process.exit(1);
-}
-
 const kinds = ["major", "minor", "patch"];
 const increment = kinds.findIndex(type => process.argv[2] === type);
 if (increment === -1) {
@@ -41,7 +34,7 @@ await Bun.$`git reset`;
 await Bun.write("./CMakeLists.txt", cmakelists.replace(version[1], updated_version));
 
 await Bun.$`git add CMakeLists.txt`;
-await Bun.$`git commit -m "Release Bun v${updated_version}"`;
+await Bun.$`git commit -m "Bump to v${updated_version}"`;
 
 console.log("");
 console.log("Done.");

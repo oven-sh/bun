639a731e729bf60ccf03c13c4de7db4b6eaccc4f
cirospaciari <cirospaciari@users.noreply.github.com>
2024-06-08 02:06:25 +0000

    Apply formatting changes

--- src/bun.js/node/fs_events.zig
+++ src/bun.js/node/fs_events.zig
@@ -377,15 +377,15 @@ pub const FSEventsLoop = struct {
                         if (path.len <= 1 and !is_file) {
                             continue;
                         }
-                        
-                        if(path.len == 0) {
+
+                        if (path.len == 0) {
                             // Since we're using fsevents to watch the file itself handle_path == path, and we now need to get the basename of the file back
                             const basename = bun.strings.lastIndexOfChar(handle_path, '/') orelse handle_path.len;
                             path = handle_path[basename..];
                             // Created and Removed seem to be always set, but don't make sense
                             flags &= ~kFSEventsRenamed;
-                        } 
-                        
+                        }
+
                         if (bun.strings.startsWithChar(path, '/')) {
                             // Skip forward slash
                             path = path[1..];

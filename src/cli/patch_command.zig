7f4204880f1c0a463f112cecbcde6864716bec11
Jarred Sumner <jarred@jarredsumner.com>
2024-06-08 23:07:15 -0700

    Disable `bun patch` in release builds until #11719 is fixed
    cc @zackradisic

--- src/cli/patch_command.zig
+++ src/cli/patch_command.zig
@@ -1,8 +1,14 @@
+const bun = @import("root").bun;
 const Command = @import("../cli.zig").Command;
 const PackageManager = @import("../install/install.zig").PackageManager;
 
 pub const PatchCommand = struct {
     pub fn exec(ctx: Command.Context) !void {
+        if (!bun.FeatureFlags.is_patch_cmd_enabled) {
+            bun.Output.prettyErrorln("bun patch is not available in this version of bun. Stay tuned.", .{});
+            bun.Global.exit(1);
+        }
+
         try PackageManager.patch(ctx);
     }
 };

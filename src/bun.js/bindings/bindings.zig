713999c25d8911d89986e47832667a5bc3684633
Jarred Sumner <jarred@jarredsumner.com>
2024-06-10 01:47:50 -0700

    Speculative fix for #11742
    Speculative fix for #11742

--- src/bun.js/bindings/bindings.zig
+++ src/bun.js/bindings/bindings.zig
@@ -4716,7 +4716,14 @@ pub const JSValue = enum(JSValueReprInt) {
     ///
     /// To handle exceptions, use `JSValue.toSliceOrNull`.
     pub inline fn toSlice(this: JSValue, global: *JSGlobalObject, allocator: std.mem.Allocator) ZigString.Slice {
-        return getZigString(this, global).toSlice(allocator);
+        const str = bun.String.fromJS(this, global);
+        defer str.deref();
+
+        // This keeps the WTF::StringImpl alive if it was originally a latin1
+        // ASCII-only string.
+        //
+        // Otherwise, it will be cloned using the allocator.
+        return str.toUTF8(allocator);
     }
 
     pub inline fn toSliceZ(this: JSValue, global: *JSGlobalObject, allocator: std.mem.Allocator) ZigString.Slice {

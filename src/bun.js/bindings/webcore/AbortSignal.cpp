dc144f951993710ce58e7cd4f43245504dab2b14
Jarred Sumner <jarred@jarredsumner.com>
2024-06-11 13:31:33 -0700

    In AbortSignal, use Ref explicitly to ensure its alive (#11790)

--- src/bun.js/bindings/webcore/AbortSignal.cpp
+++ src/bun.js/bindings/webcore/AbortSignal.cpp
@@ -158,9 +158,8 @@ void AbortSignal::signalAbort(JSC::JSValue reason)
     dispatchEvent(Event::create(eventNames().abortEvent, Event::CanBubble::No, Event::IsCancelable::No));
 
     // 6. For each dependent signal of signal, call signal's signalAbort method with reason.
-    auto dependentSignals = std::exchange(m_dependentSignals, {});
-    for (auto& signal : dependentSignals)
-        signal.signalAbort(reason);
+    for (Ref dependentSignal : std::exchange(m_dependentSignals, {}))
+        dependentSignal->signalAbort(reason);
 }
 
 void AbortSignal::cleanNativeBindings(void* ref)

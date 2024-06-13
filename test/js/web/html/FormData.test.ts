d993c088f36f0e5a4ffe584c5bee257ee760d8c3
cirospaciari <ciro.spaciari@gmail.com>
2024-06-13 08:32:30 -0300

    revert this test

--- test/js/web/html/FormData.test.ts
+++ test/js/web/html/FormData.test.ts
@@ -60,7 +60,7 @@ describe("FormData", () => {
     expect(b1.name).toBeUndefined();
 
     b1 = form.get("foo") as Blob;
-    expect(blob.name).toBe("foo.txt");
+    expect(blob.name).toBeUndefined();
     expect(b1.name).toBe("foo.txt");
   });
 

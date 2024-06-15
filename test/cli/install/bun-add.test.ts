cabe0f1d3ead19bbf6a41a3033294ca637517851
Dylan Conway <dylan.conway567@gmail.com>
2024-06-14 17:29:59 -0700

    windows fixup

--- test/cli/install/bun-add.test.ts
+++ test/cli/install/bun-add.test.ts
@@ -1,7 +1,7 @@
 import { file, spawn } from "bun";
 import { afterAll, afterEach, beforeAll, beforeEach, expect, it, setDefaultTimeout } from "bun:test";
 import { bunExe, bunEnv as env, toHaveBins, toBeValidBin, toBeWorkspaceLink, tmpdirSync } from "harness";
-import { access, mkdir, readlink, rm, writeFile, copyFile, appendFile } from "fs/promises";
+import { access, mkdir, readlink, rm, writeFile, copyFile, appendFile, readFile } from "fs/promises";
 import { join, relative } from "path";
 import {
   dummyAfterAll,
@@ -1622,18 +1622,18 @@ it("should add dependency without duplication (GitHub)", async () => {
 });
 
 it("should add dependencies to workspaces directly", async () => {
-  const foo_package = JSON.stringify({
+  const fooPackage = {
     name: "foo",
     version: "0.1.0",
     workspaces: ["moo"],
-  });
-  await writeFile(join(add_dir, "package.json"), foo_package);
-  const bar_package = JSON.stringify({
+  };
+  await writeFile(join(add_dir, "package.json"), JSON.stringify(fooPackage));
+  const barPackage = JSON.stringify({
     name: "bar",
     version: "0.2.0",
     workspaces: ["moo"],
   });
-  await writeFile(join(package_dir, "package.json"), bar_package);
+  await writeFile(join(package_dir, "package.json"), barPackage);
   await mkdir(join(package_dir, "moo"));
   await writeFile(
     join(package_dir, "moo", "package.json"),
@@ -1671,10 +1671,12 @@ it("should add dependencies to workspaces directly", async () => {
     "node_modules",
     "package.json",
   ]);
-  expect(await file(join(package_dir, "package.json")).text()).toEqual(bar_package);
+  expect(await file(join(package_dir, "package.json")).text()).toEqual(barPackage);
   expect(await readdirSorted(join(package_dir, "moo"))).toEqual(["bunfig.toml", "node_modules", "package.json"]);
   expect(await readdirSorted(join(package_dir, "moo", "node_modules", "foo"))).toEqual(["package.json"]);
-  expect(await file(join(package_dir, "moo", "node_modules", "foo", "package.json")).text()).toEqual(foo_package);
+  expect(await file(await readlink(join(package_dir, "moo", "node_modules", "foo", "package.json"))).json()).toEqual(
+    fooPackage,
+  );
   expect(await file(join(package_dir, "moo", "package.json")).json()).toEqual({
     name: "moo",
     version: "0.3.0",

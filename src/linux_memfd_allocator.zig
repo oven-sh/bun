60e034b610219b337c93aaaaf1cf67931add63b6
Dylan Conway <dylan.conway567@gmail.com>
2024-06-21 18:03:52 -0700

    bring comment back

--- src/linux_memfd_allocator.zig
+++ src/linux_memfd_allocator.zig
@@ -137,6 +137,7 @@ pub const LinuxMemFdAllocator = struct {
         var label_buf: [128]u8 = undefined;
         const label = std.fmt.bufPrintZ(&label_buf, "memfd-num-{d}", .{memfd_counter.fetchAdd(1, .monotonic)}) catch "";
 
+        // Using huge pages was slower.
         const fd = switch (bun.sys.memfd_create(label, std.os.linux.MFD.CLOEXEC)) {
             .err => |err| return .{ .err = bun.sys.Error.fromCode(err.getErrno(), .open) },
             .result => |fd| fd,

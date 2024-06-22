2e251e390e2970a870dcb37d4f8fbfc5f84f3bf6
Jarred Sumner <jarred@jarredsumner.com>
2024-06-22 15:13:34 -0700

    Add hack

--- src/shell/interpreter.zig
+++ src/shell/interpreter.zig
@@ -51,6 +51,7 @@ const ast = shell.AST;
 const SmolList = shell.SmolList;
 
 const GlobWalker = Glob.GlobWalker_(null, Glob.SyscallAccessor, true);
+const isWindows = bun.Environment.isWindows;
 
 const stdin_no = 0;
 const stdout_no = 1;
@@ -818,6 +819,48 @@ pub const ParsedShellScript = struct {
     }
 };
 
+// TODO: remove this hack
+// We need to force libuv to enter it's event loop
+// So we use a timer that never fires to keep the event loop running
+const WindowsKeepAlive = struct {
+    handle: ?*uv.Timer = null,
+
+    pub fn ref(this: *WindowsKeepAlive, event_loop: *uv.Loop) void {
+        var handle = this.handle orelse brk: {
+            this.handle = init(event_loop);
+            break :brk this.handle.?;
+        };
+        handle.start(std.math.maxInt(i64) - 1, 0, callback);
+        handle.ref();
+    }
+
+    pub const activate = ref;
+
+    fn init(event_loop: *uv.Loop) *uv.Timer {
+        var handle = bun.new(uv.Timer, std.mem.zeroes(uv.Timer));
+        handle.init(event_loop);
+        handle.data = null;
+        return handle;
+    }
+
+    pub fn disable(this: *WindowsKeepAlive) void {
+        if (this.handle) |handle| {
+            this.handle = null;
+            handle.stop();
+            uv.uv_close(@alignCast(@ptrCast(handle)), onClose);
+        }
+    }
+
+    fn onClose(ptr: ?*anyopaque) callconv(.C) void {
+        const timer: *uv.Timer = @alignCast(@ptrCast(ptr.?));
+        bun.destroy(timer);
+    }
+
+    fn callback(_: *uv.Timer) callconv(.C) void {
+        bun.Output.panic("WindowsKeepAlive timer should never fire", .{});
+    }
+};
+
 /// This interpreter works by basically turning the AST into a state machine so
 /// that execution can be suspended and resumed to support async.
 pub const Interpreter = struct {
@@ -843,7 +886,7 @@ pub const Interpreter = struct {
     async_commands_executing: u32 = 0,
 
     globalThis: *JSC.JSGlobalObject,
-    ref: bun.Async.KeepAlive = .{},
+    ref: if (isWindows) WindowsKeepAlive else bun.Async.KeepAlive = .{},
 
     flags: packed struct(u8) {
         done: bool = false,

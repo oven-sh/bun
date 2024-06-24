79deb34e5764f15a1beccce5286042f0926d2944
Ashcon Partovi <ashcon@partovi.net>
2024-06-24 01:45:26 -0700

    Fix scripts

--- scripts/all-dependencies.ps1
+++ scripts/all-dependencies.ps1
@@ -8,7 +8,7 @@ $DidAnything = $false;
 
 $BUN_BASE_DIR = if ($env:BUN_BASE_DIR) { $env:BUN_BASE_DIR } else { Join-Path $PSScriptRoot '..' }
 $BUN_DEPS_DIR = if ($env:BUN_DEPS_DIR) { $env:BUN_DEPS_DIR } else { Join-Path $BUN_BASE_DIR 'src\deps' }
-$BUN_DEPS_OUT_DIR = if ($env:BUN_DEPS_OUT_DIR) { $env:BUN_DEPS_OUT_DIR } else { $BUN_DEPS_DIR }
+$BUN_DEPS_OUT_DIR = if ($env:BUN_DEPS_OUT_DIR) { $env:BUN_DEPS_OUT_DIR } else { Join-Path $BUN_BASE_DIR 'build\bun-deps' }
 
 function Build-Dependency {
   param(

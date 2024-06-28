1b64e8bb2e9c539c70fa4cc27c0e4e5c58bde5fb
Jake Boone <jakeboone02@gmail.com>
2024-06-28 15:22:35 -0700

    Add tests for `test.coveragePathIgnorePatterns` option

--- test/cli/test/coverage.test.ts
+++ test/cli/test/coverage.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "bun:test";
+import { describe, test, expect } from "bun:test";
 import { tempDirWithFiles, bunExe, bunEnv } from "harness";
 import path from "path";
 import { readFileSync } from "node:fs";
@@ -53,4 +53,113 @@ export class Y {
   expect(result.exitCode).toBe(0);
   expect(result.signalCode).toBeUndefined();
   expect(readFileSync(path.join(dir, "coverage", "lcov.info"), "utf-8")).toMatchSnapshot();
+});
+
+describe("coveragePathIgnorePatterns", () => {
+  const demoFiles = {
+    "demo1.ts": `
+  // covered
+  export function covered() {
+    return 1;
+  }
+  `,
+    "demo2.ts": `
+  // tested but not covered
+  export function uncovered() {
+    return 2;
+  }
+  `,
+    "demo.test.ts": `
+  import { expect, test } from "bun:test";
+  import { covered } from "./demo1";
+  import { uncovered } from "./demo2";
+  test("demo", () => {
+    expect(covered()).toBe(1);
+    expect(uncovered()).toBe(2);
+  })
+  `,
+  };
+
+  test("coverage path ignore patterns", () => {
+    const dir = tempDirWithFiles("cov", {
+      "bunfig.toml": `[test]
+coverage = true
+coveragePathIgnorePatterns = [ "**/demo2.ts" ]
+coverageReporter = ["text", "lcov"]
+`,
+      ...demoFiles,
+    });
+
+    const result = Bun.spawnSync([bunExe(), "test"], {
+      cwd: dir,
+      env: {
+        ...bunEnv,
+      },
+      stderr: "pipe",
+      stdout: "inherit",
+      stdin: "ignore",
+    });
+
+    const stderr = result.stderr.toString();
+
+    expect(result.exitCode).toBe(0);
+    expect(stderr).toContain("demo1.ts");
+    expect(stderr).not.toContain("demo2.ts");
+    expect(readFileSync(path.join(dir, "coverage", "lcov.info"), "utf-8")).toMatchSnapshot();
+  });
+
+  test("coverage path ignore everything", () => {
+    const dir = tempDirWithFiles("cov", {
+      "bunfig.toml": `[test]
+coverage = true
+coveragePathIgnorePatterns = "**"
+coverageReporter = ["text", "lcov"]
+`,
+      ...demoFiles,
+    });
+
+    const result = Bun.spawnSync([bunExe(), "test"], {
+      cwd: dir,
+      env: {
+        ...bunEnv,
+      },
+      stderr: "pipe",
+      stdout: "inherit",
+      stdin: "ignore",
+    });
+
+    const stderr = result.stderr.toString();
+
+    expect(result.exitCode).toBe(0);
+    expect(stderr).toContain("All files     |    0.00 |    0.00 |");
+    expect(stderr).not.toContain("demo1.ts");
+    expect(stderr).not.toContain("demo2.ts");
+    expect(readFileSync(path.join(dir, "coverage", "lcov.info"), "utf-8")).toMatchSnapshot();
+  });
+
+  test("coverage path invalid config", () => {
+    const dir = tempDirWithFiles("cov", {
+      "bunfig.toml": `[test]
+coverage = true
+coveragePathIgnorePatterns = 123
+coverageReporter = ["text", "lcov"]
+`,
+      ...demoFiles,
+    });
+
+    const result = Bun.spawnSync([bunExe(), "test"], {
+      cwd: dir,
+      env: {
+        ...bunEnv,
+      },
+      stderr: "pipe",
+      stdout: "inherit",
+      stdin: "ignore",
+    });
+
+    const stderr = result.stderr.toString();
+
+    expect(result.exitCode).toBe(1);
+    expect(stderr).toContain("coveragePathIgnorePatterns");
+  });
 });

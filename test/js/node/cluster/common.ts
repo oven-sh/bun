9a1ffbb7ca459c11cff2cdf33b46ea8b08ddf62c
Meghan Denny <meghan@bun.sh>
2024-07-01 15:42:48 -0700

    one of the tests needs this

--- test/js/node/cluster/common.ts
+++ test/js/node/cluster/common.ts
@@ -5,3 +5,12 @@ import os from "node:os";
 export function tmpdirSync(pattern: string = "bun.test.") {
   return fs.mkdtempSync(path.join(fs.realpathSync(os.tmpdir()), pattern));
 }
+
+export function isAlive(pid) {
+  try {
+    process.kill(pid, "SIGCONT");
+    return true;
+  } catch {
+    return false;
+  }
+}

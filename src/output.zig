64dfc9b16e86014d8d85acc709ac9b85e376fbe8
Dylan Conway <dylan.conway567@gmail.com>
2024-07-02 19:27:34 -0700

    Revert "progress"
    This reverts commit adcaa31d41507e5bdcfe276da8013cc26ba5d7d4.

--- src/output.zig
+++ src/output.zig
@@ -412,12 +412,6 @@ pub fn flush() void {
     }
 }
 
-pub fn flushStderr() void {
-    if (Environment.isNative and source_set) {
-        source.buffered_error_stream.flush() catch {};
-    }
-}
-
 pub const ElapsedFormatter = struct {
     colors: bool,
     duration_ns: u64 = 0,

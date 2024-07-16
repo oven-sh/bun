8f4051abe61dd7cf79ea9ec9965c397b1007e5f2
Jarred Sumner <jarred@jarredsumner.com>
2024-07-16 03:28:28 -0700

    Update workaround-missing-symbols.cpp

--- src/bun.js/bindings/workaround-missing-symbols.cpp
+++ src/bun.js/bindings/workaround-missing-symbols.cpp
@@ -358,7 +358,9 @@ extern "C" int __ulock_wait2(uint32_t operation, void* addr, uint64_t value,
 
 #endif
 
+#ifndef U_SHOW_CPLUSPLUS_API
 #define U_SHOW_CPLUSPLUS_API 0
+#endif
 
 #include <unicode/uchar.h>
 

aa31f5976a007844498196bc0dcbbd3d9bb53bcb
Jarred Sumner <jarred@jarredsumner.com>
2024-07-23 14:51:10 -0700

    Update build-libdeflate.ps1

--- scripts/build-libdeflate.ps1
+++ scripts/build-libdeflate.ps1
@@ -9,7 +9,8 @@ try {
   Run cmake -S "." -B build @CMAKE_FLAGS -DLIBDEFLATE_BUILD_STATIC_LIB=ON -DLIBDEFLATE_BUILD_SHARED_LIB=OFF -DLIBDEFLATE_BUILD_GZIP=OFF
   Run cmake --build build --clean-first --config Release
 
-  Copy-Item build/libdeflate_static.lib $BUN_DEPS_OUT_DIR/deflate.lib
+  # In https://github.com/ebiggers/libdeflate/releases/tag/v1.20, it's outputting libdeflate.a even on Windows
+  Copy-Item build/libdeflate.a $BUN_DEPS_OUT_DIR/deflate.lib
   Write-Host "-> deflate.lib"
 } finally { Pop-Location }
 

c45611e059d858358fbf303f124b81570fcb5e92
Jarred Sumner <jarred@jarredsumner.com>
2024-07-22 17:22:01 -0700

    Use cmake

--- scripts/build-zlib.sh
+++ scripts/build-zlib.sh
@@ -4,6 +4,9 @@ source $(dirname -- "${BASH_SOURCE[0]}")/env.sh
 
 mkdir -p $BUN_DEPS_OUT_DIR
 cd $BUN_DEPS_DIR/zlib
-CFLAGS="${CFLAGS}" ./configure --static
-make -j${CPUS} libz.a
+rm -rf build
+mkdir build
+cd build
+cmake $CMAKE_FLAGS -G Ninja -DCMAKE_BUILD_TYPE=Release ..
+ninja
 cp ./libz.a $BUN_DEPS_OUT_DIR/libz.a

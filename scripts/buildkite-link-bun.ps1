1af943980553e41a4c90f2eb17be497bc6ef05c7
Ashcon Partovi <ashcon@partovi.net>
2024-07-24 16:23:24 -0700

    Fix Join-File

--- scripts/buildkite-link-bun.ps1
+++ scripts/buildkite-link-bun.ps1
@@ -23,7 +23,7 @@ mv -Force -ErrorAction SilentlyContinue build\build\* build
 Set-Location build
 
 # HACK: See scripts/build-bun-cpp.ps1
-Join-File -Path "bun-cpp-objects.a" -Verbose -DeletePartFiles
+Join-File -Path "$(Resolve-Path .)\bun-cpp-objects.a" -Verbose -DeletePartFiles
 
 $CANARY = if ($env:CANARY) { "$env:CANARY" } else { "1" }
 cmake .. @CMAKE_FLAGS -G Ninja -DCMAKE_BUILD_TYPE=Release `

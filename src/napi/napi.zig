a1eb6cbc459cebe261c0d127752486075e03165f
Dylan Conway <35280289+dylan-conway@users.noreply.github.com>
2024-07-24 18:57:01 -0700

    fix(napi): unref threadsafe functions on finalize (#12788)

--- src/napi/napi.zig
+++ src/napi/napi.zig
@@ -1544,6 +1544,8 @@ pub const ThreadSafeFunction = struct {
 
     pub fn finalize(opaq: *anyopaque) void {
         var this = bun.cast(*ThreadSafeFunction, opaq);
+        this.unref();
+
         if (this.finalizer.fun) |fun| {
             fun(this.event_loop.global, this.finalizer.data, this.ctx);
         }

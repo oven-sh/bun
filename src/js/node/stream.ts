a2f73d987bdf383fad9d32cf3896defb044d849e
Jarred Sumner <jarred@jarredsumner.com>
2024-07-24 22:02:27 -0700

    Update stream.ts

--- src/js/node/stream.ts
+++ src/js/node/stream.ts
@@ -5820,7 +5820,13 @@ function getNativeReadableStream(Readable, stream, options) {
   $assert(typeof ptr === "object", "Invalid native ptr");
 
   const NativeReadable = getNativeReadableStreamPrototype(type, Readable);
+  // https://github.com/oven-sh/bun/pull/12801
+  // https://github.com/oven-sh/bun/issues/9555
+  // There may be a ReadableStream.Strong handle to the ReadableStream.
+  // We can't update those handles to point to the NativeReadable from JS
+  // So we instead mark it as no longer usable, and create a new NativeReadable
   transferToNativeReadable(stream);
+
   return new NativeReadable(ptr, options);
 }
 

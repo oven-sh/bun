f151776669ec9801e4b6501b105469fa084db5ca
cirospaciari <ciro.spaciari@gmail.com>
2024-07-29 15:32:21 -0700

    do fast shutdown here

--- src/http.zig
+++ src/http.zig
@@ -899,10 +899,11 @@ pub const HTTPThread = struct {
             defer this.queued_shutdowns_lock.unlock();
             for (this.queued_shutdowns.items) |http| {
                 if (socket_async_http_abort_tracker.fetchSwapRemove(http.async_http_id)) |socket_ptr| {
-                    // close will shutdown properly on TLS sending a close_notify
+                    // close will do a fast shutdown on TLS sending a close_notify and imeadiately closing the socket
                     if (http.is_tls) {
                         const socket = uws.SocketTLS.fromAny(socket_ptr.value);
-                        socket.close(.normal);
+                        // do a fast shutdown here
+                        socket.close(.failure);
                     } else {
                         const socket = uws.SocketTCP.fromAny(socket_ptr.value);
                         socket.close(.normal);

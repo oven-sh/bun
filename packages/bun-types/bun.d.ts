c18caec40a954264aad5d4b31282fa44d1eda0c7
Meghan Denny <meghan@bun.sh>
2024-07-29 12:05:24 -0700

    moved this change to pr 12794

--- packages/bun-types/bun.d.ts
+++ packages/bun-types/bun.d.ts
@@ -4231,18 +4231,15 @@ declare module "bun" {
     hostname: string;
     port: number;
     tls?: TLSOptions;
-    exclusive?: boolean;
   }
 
   interface TCPSocketConnectOptions<Data = undefined> extends SocketOptions<Data> {
     hostname: string;
     port: number;
     tls?: boolean;
-    exclusive?: boolean;
   }
 
   interface UnixSocketOptions<Data = undefined> extends SocketOptions<Data> {
-    tls?: TLSOptions;
     unix: string;
   }
 

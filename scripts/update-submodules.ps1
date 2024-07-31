a44b7e41d2ff6fab92c2b6d0d0b7c1bc90b09ae9
Jarred Sumner <jarred@jarredsumner.com>
2024-07-30 23:03:35 -0700

    Pass `--force` to git submodule update in CI

--- scripts/update-submodules.ps1
+++ scripts/update-submodules.ps1
@@ -12,8 +12,13 @@ try {
   if (!($WebKit) -and (-not (Test-Path "src/bun.js/WebKit/.git"))) {
     $Names = $Names | Where-Object { $_ -ne 'src/bun.js/WebKit' }
   }
+  if ($env:FORCE_UPDATE_SUBMODULES -eq "1") {
+    # Set --force in CI.
+    git submodule update --init --recursive --progress --depth 1 --checkout --force @NAMES
+  } else {
+    git submodule update --init --recursive --progress --depth 1 --checkout @NAMES
+  }
 
-  git submodule update --init --recursive --progress --depth 1 --checkout @NAMES
   if ($LASTEXITCODE -ne 0) {
     throw "git submodule update failed"
   }

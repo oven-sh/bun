f7dad61ece895b7ba9592ddd7bfb1918776e7615
Ciro Spaciari <ciro.spaciari@gmail.com>
2024-07-31 14:37:43 -0700

    opsie

--- src/bun.js/api/server.zig
+++ src/bun.js/api/server.zig
@@ -2328,7 +2328,6 @@ fn NewRequestContext(comptime ssl_enabled: bool, comptime debug_mode: bool, comp
             defer this.deref();
 
             if (this.isAbortedOrEnded()) {
-                this.deref();
                 return;
             }
 

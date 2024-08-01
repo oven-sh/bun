96cac63fee29c153141420d4def74b18b05d5f3b
Dave Caruso <me@paperdave.net>
2024-07-31 19:45:41 -0700

    move tests around

--- test/cli/install/bun-upgrade.test.ts
+++ test/cli/install/bun-upgrade.test.ts
@@ -176,13 +176,3 @@ it("zero arguments, should succeed", async () => {
   // Should not contain error message
   expect(stderr.toString()).not.toContain("error:");
 });
-
-import { escapePowershell } from "bun:internal-for-testing";
-it("powershell escaping rules", () => {
-  // This formatter does not include quotes around the string intentionally
-  expect(escapePowershell("foo")).toBe("foo");
-  expect(escapePowershell("foo bar")).toBe("foo bar");
-  expect(escapePowershell("foo\" bar")).toBe("foo`\" bar");
-  expect(escapePowershell("foo\" `bar")).toBe("foo`\" ``bar");
-  expect(escapePowershell("foo\" ``\"bar")).toBe("foo`\" `````\"bar");
-});

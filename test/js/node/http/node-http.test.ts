66402d5e329b80c0ba26dcf1e6d08df5cab5b45b
cirospaciari <ciro.spaciari@gmail.com>
2024-08-01 18:13:24 -0700

    check tcp

--- test/js/node/http/node-http.test.ts
+++ test/js/node/http/node-http.test.ts
@@ -1889,53 +1889,57 @@ it("should emit events in the right order", async () => {
   ]);
 });
 
-it("destroy should end download", async () => {
+it.only("destroy should end download", async () => {
   // just simulate some file that will take forever to download
   const payload = Buffer.from("X".repeat(128 * 1024));
   let sendedByteLength = 0;
-  using server = Bun.serve({
-    port: 0,
-    async fetch(req) {
-      let running = true;
-      req.signal.onabort = () => (running = false);
-      return new Response(async function* () {
-        while (running) {
-          sendedByteLength += payload.byteLength;
-          yield payload;
-          await Bun.sleep(10);
-        }
-      });
-    },
-  });
-
-  async function run() {
-    let receivedByteLength = 0;
-    let { promise, resolve } = Promise.withResolvers();
-    const req = request(server.url, res => {
-      res.on("data", data => {
-        receivedByteLength += data.length;
-        if (resolve) {
-          resolve();
-          resolve = null;
-        }
-      });
+  {
+    using server = Bun.serve({
+      port: 0,
+      async fetch(req) {
+        let running = true;
+        req.signal.onabort = () => (running = false);
+        return new Response(async function* () {
+          while (running) {
+            sendedByteLength += payload.byteLength;
+            yield payload;
+            await Bun.sleep(10);
+          }
+        });
+      },
     });
-    req.end();
-    await promise;
-    req.destroy();
-    await Bun.sleep(10);
-    const initialByteLength = receivedByteLength;
-    // we should receive the same amount of data we sent
-    expect(initialByteLength).toBeLessThanOrEqual(sendedByteLength);
-    await Bun.sleep(10);
-    // we should not receive more data after destroy
-    expect(initialByteLength).toBe(receivedByteLength);
-    await Bun.sleep(10);
-  }
 
-  const runCount = 50;
-  const runs = Array.from({ length: runCount }, run);
-  await Promise.all(runs);
+    async function run() {
+      let receivedByteLength = 0;
+      let { promise, resolve } = Promise.withResolvers();
+      const req = request(server.url, res => {
+        res.on("data", data => {
+          receivedByteLength += data.length;
+          if (resolve) {
+            resolve();
+            resolve = null;
+          }
+        });
+      });
+      req.end();
+      await promise;
+      req.destroy();
+      await Bun.sleep(10);
+      const initialByteLength = receivedByteLength;
+      // we should receive the same amount of data we sent
+      expect(initialByteLength).toBeLessThanOrEqual(sendedByteLength);
+      await Bun.sleep(10);
+      // we should not receive more data after destroy
+      expect(initialByteLength).toBe(receivedByteLength);
+      await Bun.sleep(10);
+    }
+
+    const runCount = 50;
+    const runs = Array.from({ length: runCount }, run);
+    await Promise.all(runs);
+  }
+  Bun.gc(true);
+  await Bun.sleep(100);
 });
 
 it("can send brotli from Server and receive with fetch", async () => {

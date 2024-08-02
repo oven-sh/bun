e24f2867644d95f229cf2ae46b61a61f0a29b559
Ashcon Partovi <ashcon@partovi.net>
2024-08-01 18:29:06 -0700

    Fix LTO setting on Linux

--- .buildkite/scripts/env.sh
+++ .buildkite/scripts/env.sh
@@ -80,6 +80,9 @@ function export_environment() {
   export ZIG_LOCAL_CACHE_DIR="$HOME/.cache/zig-cache/$BUILDKITE_STEP_KEY"
   export ZIG_GLOBAL_CACHE_DIR="$HOME/.cache/zig-cache/$BUILDKITE_STEP_KEY"
   export BUN_DEPS_CACHE_DIR="$HOME/.cache/bun-deps/$BUILDKITE_STEP_KEY"
+  if [ "$(assert_os)" == "darwin" ]; then
+    export USE_LTO="ON"
+  fi
   if [ "$(assert_arch)" == "aarch64" ]; then
     export CPU_TARGET="native"
   elif [[ "$BUILDKITE_STEP_KEY" == *"baseline"* ]]; then

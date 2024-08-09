4f8bb16bd1721c95ef3fc16d4bf842be672de376
Will <will@onezerohosting.com>
2024-08-09 16:14:28 -0700

    feature: add debugger info endpoint (/json) for external tool discovery including browser DevTools similar to Node.js

--- src/js/internal/debugger.ts
+++ src/js/internal/debugger.ts
@@ -1,5 +1,4 @@
 import type { Server as WebSocketServer, WebSocketHandler, ServerWebSocket, SocketHandler, Socket } from "bun";
-
 export default function (
   executionContextId: string,
   url: string,
@@ -129,7 +128,7 @@ class Debugger {
         return Response.json(versionInfo());
       case "/json":
       case "/json/list":
-      // TODO?
+        return Response.json(debuggerInfo(this));
     }
 
     if (!this.#url.protocol.includes("unix") && this.#url.pathname !== pathname) {
@@ -213,6 +212,24 @@ function versionInfo(): unknown {
     "Bun-Version": Bun.version,
     "Bun-Revision": Bun.revision,
   };
+}
+
+function debuggerInfo(d: Debugger): unknown {
+    const ws = `${d.url}`.replace(/wss?:\/\//, '')
+
+    return [
+        {
+            "description": `Bun instance (version: ${Bun.version}, revision: ${Bun.revision})`,
+            "devtoolsFrontendUrl": `devtools://devtools/bundled/js_app.html?experiments=true&v8only=true&ws=${ws}`,
+            "devtoolsFrontendUrlCompat": `devtools://devtools/bundled/inspector.html?experiments=true&v8only=true&ws=${ws}`,
+            "faviconUrl": "https://bun.sh/logo_avatar.svg",
+            "id": `${ws}`.split('/').pop(),
+            "title": Bun.main,
+            "type": "bun",
+            "url": Bun.pathToFileURL(Bun.main),
+            "webSocketDebuggerUrl": d.url
+        }
+    ]
 }
 
 function webSocketWriter(ws: ServerWebSocket<unknown>): Writer {

a1312066b3ea05ce18399c4e057f49721ca44607
Ashcon Partovi <ashcon@partovi.net>
2024-08-14 11:13:09 -0700

    Fix secrets in CI tests (#13306)

--- test/js/third_party/msw/msw.test.ts
+++ test/js/third_party/msw/msw.test.ts
@@ -3,5 +3,5 @@ import { expect, it } from "bun:test";
 import * as path from "node:path";
 
 it("works", async () => {
-  expect([path.join(import.meta.dirname, "_fixtures", "msw.ts")]).toRun("2\n");
+  expect([path.join(import.meta.dirname, "msw.fixture.ts")]).toRun("2\n");
 });

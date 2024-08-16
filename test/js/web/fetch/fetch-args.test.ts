11bdad161c69cfd8b3363a25bcca596d1d09a465
Jarred Sumner <jarred@jarredsumner.com>
2024-08-15 18:30:42 -0700

    Fix some argument validation issues in fetch()

---
+++ test/js/web/fetch/fetch-args.test.ts
@@ -0,0 +1,184 @@
+import { beforeAll, describe, expect, it, test, afterAll } from "bun:test";
+
+let server;
+let requestCount = 0;
+beforeAll(async () => {
+  server = Bun.serve({
+    port: 0,
+    fetch(request) {
+      requestCount++;
+      return new Response(undefined, { headers: request.headers });
+    },
+  });
+});
+afterAll(() => {
+  server!.stop(true);
+});
+
+test("fetch(request subclass with headers)", async () => {
+  class MyRequest extends Request {
+    constructor(input: RequestInfo, init?: RequestInit) {
+      super(input, init);
+      this.headers.set("hello", "world");
+    }
+  }
+  const myRequest = new MyRequest(server!.url);
+  const { headers } = await fetch(myRequest);
+
+  expect(headers.get("hello")).toBe("world");
+});
+
+test("fetch(RequestInit, headers)", async () => {
+  const myRequest = {
+    headers: {
+      "hello": "world",
+    },
+    url: server!.url,
+  };
+  const { headers } = await fetch(myRequest, {
+    headers: {
+      "hello": "world2",
+    },
+  });
+
+  expect(headers.get("hello")).toBe("world2");
+});
+
+test("fetch(url, RequestSubclass)", async () => {
+  class MyRequest extends Request {
+    constructor(input: RequestInfo, init?: RequestInit) {
+      super(input, init);
+      this.headers.set("hello", "world");
+    }
+  }
+  const myRequest = new MyRequest(server!.url);
+  const { headers } = await fetch(server.url, myRequest);
+
+  expect(headers.get("hello")).toBe("world");
+});
+
+test("fetch(RequestSubclass, undefined)", async () => {
+  class MyRequest extends Request {
+    constructor(input: RequestInfo, init?: RequestInit) {
+      super(input, init);
+      this.headers.set("hello", "world");
+    }
+  }
+  const myRequest = new MyRequest(server!.url);
+  const { headers } = await fetch(myRequest, undefined);
+
+  expect(headers.get("hello")).toBe("world");
+});
+
+describe("does not send a request when", () => {
+  test("Invalid headers", async () => {
+    const prevCount = requestCount;
+    expect(
+      async () =>
+        await fetch(server!.url, {
+          headers: {
+            "😀smile ": "😀",
+          },
+        }),
+    ).toThrow();
+    // Give it a chance to possibly send the request.
+    await Bun.sleep(2);
+    expect(requestCount).toBe(prevCount);
+  });
+
+  test("Invalid url", async () => {
+    const prevCount = requestCount;
+    expect(async () => await fetch("😀")).toThrow();
+    // Give it a chance to possibly send the request.
+    await Bun.sleep(2);
+    expect(requestCount).toBe(prevCount);
+  });
+
+  test("Invalid redirect", async () => {
+    const prevCount = requestCount;
+    expect(async () => await fetch(server!.url, { redirect: "😀" })).toThrow();
+    // Give it a chance to possibly send the request.
+    await Bun.sleep(2);
+    expect(requestCount).toBe(prevCount);
+  });
+
+  test("proxy and unix", async () => {
+    const prevCount = requestCount;
+    expect(async () => await fetch(server!.url, { proxy: "http://example.com", unix: "/tmp/abc.sock" })).toThrow();
+    // Give it a chance to possibly send the request.
+    await Bun.sleep(2);
+    expect(requestCount).toBe(prevCount);
+  });
+
+  test("Invalid ca in tls", async () => {
+    const prevCount = requestCount;
+    expect(async () => await fetch("https://badssl.com", { tls: { ca: 123 } })).toThrow();
+    // Give it a chance to possibly send the request.
+    await Bun.sleep(2);
+    expect(requestCount).toBe(prevCount);
+  });
+
+  const propertyNamesToThrow = [
+    "body",
+    "decompression",
+    "headers",
+    "keepalive",
+    "method",
+    "proxy",
+    "redirect",
+    "signal",
+    "timeout",
+    "tls",
+    "unix",
+    "verbose",
+  ];
+
+  for (const propertyName of propertyNamesToThrow) {
+    test(`get "${propertyName}" throws (url, 1st arg)`, async () => {
+      const prevCount = requestCount;
+      expect(
+        async () =>
+          await fetch(server!.url, {
+            get [propertyName]() {
+              throw new Error("boom");
+            },
+          }),
+      ).toThrow();
+      // Give it a chance to possibly send the request.
+      await Bun.sleep(2);
+      expect(requestCount).toBe(prevCount);
+    });
+
+    test(`get "${propertyName}" throws (1st arg)`, async () => {
+      const prevCount = requestCount;
+      expect(
+        async () =>
+          await fetch({
+            url: "http://example.com",
+            get [propertyName]() {
+              throw new Error("boom");
+            },
+          }),
+      ).toThrow();
+      // Give it a chance to possibly send the request.
+      await Bun.sleep(2);
+      expect(requestCount).toBe(prevCount);
+    });
+
+    test(`get "${propertyName}" throws (Request object, 1st arg)`, async () => {
+      const prevCount = requestCount;
+      expect(
+        async () =>
+          await fetch(new Request("http://example.com"), {
+            get [propertyName]() {
+              throw new Error("boom");
+            },
+          }),
+      ).toThrow();
+
+      // Give it a chance to possibly send the request.
+      await Bun.sleep(2);
+      expect(requestCount).toBe(prevCount);
+    });
+  }
+});

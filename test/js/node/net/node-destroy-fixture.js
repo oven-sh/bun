5079d8aa5d0b991ada4b39d566fe6932626f3276
cirospaciari <ciro.spaciari@gmail.com>
2024-08-19 13:56:48 -0700

    cleanup

--- test/js/node/net/node-destroy-fixture.js
+++ test/js/node/net/node-destroy-fixture.js
@@ -2,7 +2,7 @@ const net = require("node:net");
 const { promise, resolve } = Promise.withResolvers();
 const client = net.createConnection(process.env.PORT, "localhost");
 client.on("connect", () => {
-  client.destroy(); // it worked normally before this PR
+  client.destroy();
   resolve(0);
 });
 

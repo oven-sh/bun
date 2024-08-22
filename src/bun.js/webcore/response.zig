84389af78f2d68bdf5bd806ad1d4a9cc4c3754d1
cirospaciari <ciro.spaciari@gmail.com>
2024-08-22 11:38:25 -0700

    move done check after mutex lock

--- src/bun.js/webcore/response.zig
+++ src/bun.js/webcore/response.zig
@@ -1115,7 +1115,6 @@ pub const Fetch = struct {
         pub fn onProgressUpdate(this: *FetchTasklet) void {
             JSC.markBinding(@src());
             log("onProgressUpdate", .{});
-            defer this.deref();
             this.mutex.lock();
             this.has_schedule_callback.store(false, .monotonic);
             const is_done = !this.result.has_more;
@@ -1773,12 +1772,11 @@ pub const Fetch = struct {
         }
 
         pub fn callback(task: *FetchTasklet, async_http: *http.AsyncHTTP, result: http.HTTPClientResult) void {
-            task.ref();
-            // we are done with the http client so we can deref our side
-            defer if (!result.has_more) task.deref();
-
             task.mutex.lock();
             defer task.mutex.unlock();
+            const is_done = !result.has_more;
+            // we are done with the http client so we can deref our side
+            defer if (is_done) task.deref();
 
             task.http.?.* = async_http.*;
             task.http.?.response_buffer = async_http.response_buffer;
@@ -1826,7 +1824,6 @@ pub const Fetch = struct {
                 }
                 if (success and result.has_more) {
                     // we are ignoring the body so we should not receive more data, so will only signal when result.has_more = true
-                    task.deref();
                     return;
                 }
             } else {
@@ -1839,7 +1836,6 @@ pub const Fetch = struct {
 
             if (task.has_schedule_callback.cmpxchgStrong(false, true, .acquire, .monotonic)) |has_schedule_callback| {
                 if (has_schedule_callback) {
-                    task.deref();
                     return;
                 }
             }

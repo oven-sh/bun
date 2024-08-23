efd8269c0a1a72db73315f7e19028015d79c247a
Jarred Sumner <jarred@jarredsumner.com>
2024-08-22 21:13:41 -0700

    //

--- test/js/bun/test/test-only.test.ts
+++ test/js/bun/test/test-only.test.ts
@@ -6,7 +6,7 @@ import { bunExe } from "harness";
 test.each(["only-fixture-1.ts", "only-fixture-2.ts", "only-fixture-3.ts"])(
   `test.only shouldn't need --only for %s`,
   async (file: string) => {
-    const result = await $`${bunExe()} test ${path.join(import.meta.dir, file)}`;
+    const result = await $`${bunExe()} test ${path.normalize(path.join(import.meta.dir, file).replaceAll("\\", "/"))}`;
 
     expect(result.stderr.toString()).toContain(" 1 pass\n");
     expect(result.stderr.toString()).toContain(" 0 fail\n");
@@ -16,7 +16,8 @@ test.each(["only-fixture-1.ts", "only-fixture-2.ts", "only-fixture-3.ts"])(
 
 test("only resets per test", async () => {
   const files = ["only-fixture-1.ts", "only-fixture-2.ts", "only-fixture-3.ts", "only-fixture-4.ts"];
-  const result = await $`${bunExe()} test ${{ raw: files.map(file => path.join(import.meta.dir, file)).join(" ") }}`;
+  const result =
+    await $`${bunExe()} test ${{ raw: files.map(file => path.normalize(path.join(import.meta.dir, file).replaceAll("\\", "/"))).join(" ") }}`;
 
   expect(result.stderr.toString()).toContain(" 6 pass\n");
   expect(result.stderr.toString()).toContain(" 0 fail\n");

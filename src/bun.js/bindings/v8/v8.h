f92dc8c2bae4e1e64a4edf10ade5c69d3b8010ef
Jarred Sumner <jarred@jarredsumner.com>
2024-08-26 19:59:21 -0700

    Update v8.h

--- src/bun.js/bindings/v8/v8.h
+++ src/bun.js/bindings/v8/v8.h
@@ -8,7 +8,12 @@
 #define BUN_EXPORT JS_EXPORT
 #endif
 
-#define V8_UNIMPLEMENTED() BUN_PANIC("You're using a module which calls a V8 function \"%s\" that Bun does not yet implement. Track progress at https://github.com/oven-sh/bun/issues/4290.", __PRETTY_FUNCTION__)
+#define V8_UNIMPLEMENTED()                                                                                                                                                                                                     \
+    {                                                                                                                                                                                                                          \
+        const str = WTF::makeString("You're using a module which calls a V8 function \""_s, __PRETTY_FUNCTION__ "" _s, "\" that Bun does not yet implement. Track progress at https://github.com/oven-sh/bun/issues/4290."_s); \
+        auto utf8 = str->utf8();                                                                                                                                                                                               \
+        Bun__panic(utf8.data(), utf8.size());                                                                                                                                                                                  \
+    }
 
 extern "C" Zig::GlobalObject* Bun__getDefaultGlobalObject();
 

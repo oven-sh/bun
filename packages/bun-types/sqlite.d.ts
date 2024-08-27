36c621b6b1a9524b7d3ed984feed41183aae9bce
Pocky <0x149527@gmail.com>
2024-08-27 09:30:22 +0800

    docs: sync Statement.all and Statement.get docs with types (#13544)

--- packages/bun-types/sqlite.d.ts
+++ packages/bun-types/sqlite.d.ts
@@ -536,7 +536,7 @@ declare module "bun:sqlite" {
      * // => [{bar: "baz"}]
      *
      * stmt.all();
-     * // => [{bar: "baz"}]
+     * // => []
      *
      * stmt.all("foo");
      * // => [{bar: "foo"}]
@@ -555,14 +555,14 @@ declare module "bun:sqlite" {
      * ```ts
      * const stmt = db.prepare("SELECT * FROM foo WHERE bar = ?");
      *
-     * stmt.all("baz");
-     * // => [{bar: "baz"}]
+     * stmt.get("baz");
+     * // => {bar: "baz"}
      *
-     * stmt.all();
-     * // => [{bar: "baz"}]
+     * stmt.get();
+     * // => null
      *
-     * stmt.all("foo");
-     * // => [{bar: "foo"}]
+     * stmt.get("foo");
+     * // => {bar: "foo"}
      * ```
      *
      * The following types can be used when binding parameters:

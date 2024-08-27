26f373b9f067a0531d6e2913e712b7fc6046fed3
Ashcon Partovi <ashcon@partovi.net>
2024-08-26 17:37:40 -0700

    Migrate libdeflate

---
+++ cmake/BuildLibDeflate.cmake
@@ -0,0 +1,24 @@
+include(cmake/BuildLibrary.cmake)
+include(cmake/GitClone.cmake)
+
+add_custom_library(
+  TARGET
+    libdeflate
+  LIBRARIES
+    deflate
+  INCLUDES
+    .
+  CMAKE_TARGETS
+    libdeflate_static
+  CMAKE_ARGS
+    -DLIBDEFLATE_BUILD_STATIC_LIB=ON
+    -DLIBDEFLATE_BUILD_SHARED_LIB=OFF
+    -DLIBDEFLATE_BUILD_GZIP=OFF
+)
+
+add_custom_clone(
+  REPOSITORY
+    ebiggers/libdeflate
+  COMMIT
+    dc76454a39e7e83b68c3704b6e3784654f8d5ac5
+)

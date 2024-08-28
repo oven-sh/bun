899053d31ed6e5084549abac3844de595f8d4119
Meghan Denny <meghan@bun.sh>
2024-08-27 22:58:44 -0700

    this was a bad test

--- test/js/node/zlib/zlib.kMaxLength.global.test.js
+++ test/js/node/zlib/zlib.kMaxLength.global.test.js
@@ -7,7 +7,6 @@ const zlib = require("node:zlib");
 const data_sync = {
   brotli: ["1b7f00f825c222b1402003", zlib.brotliDecompress, zlib.brotliDecompressSync],
   inflate: ["789c4b4c1c58000039743081", zlib.inflate, zlib.inflateSync],
-  inflate_raw: ["789c4b4c1c58000039743081", zlib.inflateRaw, zlib.inflateRawSync],
   gunzip: ["1f8b08000000000000034b4c1c5800008c362bf180000000", zlib.gunzip, zlib.gunzipSync],
   unzip: ["1f8b08000000000000034b4c1c5800008c362bf180000000", zlib.gunzip, zlib.gunzipSync],
 };

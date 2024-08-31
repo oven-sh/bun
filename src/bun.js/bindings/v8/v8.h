9a68e51045bef0a761e9dc41d4b94d2be3d7666b
190n <ben@bun.sh>
2024-08-30 18:03:32 -0700

    Fix compiler errors in src/bun.js/bindings/v8/v8.h

--- src/bun.js/bindings/v8/v8.h
+++ src/bun.js/bindings/v8/v8.h
@@ -8,12 +8,15 @@
 #define BUN_EXPORT JS_EXPORT
 #endif
 
-#define V8_UNIMPLEMENTED()                                                                                                                                                                                                     \
-    {                                                                                                                                                                                                                          \
-        const str = WTF::makeString("You're using a module which calls a V8 function \""_s, __PRETTY_FUNCTION__ "" _s, "\" that Bun does not yet implement. Track progress at https://github.com/oven-sh/bun/issues/4290."_s); \
-        auto utf8 = str->utf8();                                                                                                                                                                                               \
-        Bun__panic(utf8.data(), utf8.size());                                                                                                                                                                                  \
-    }
+#define V8_UNIMPLEMENTED()                                                                                          \
+    do {                                                                                                            \
+        const auto str = WTF::makeString(                                                                           \
+            "You're using a module which calls a V8 function \""_s,                                                 \
+            WTF::ASCIILiteral::fromLiteralUnsafe(__PRETTY_FUNCTION__),                                              \
+            "\" that Bun does not yet implement. Track progress at https://github.com/oven-sh/bun/issues/4290."_s); \
+        auto utf8 = str.utf8();                                                                                     \
+        Bun__panic(utf8.data(), utf8.length());                                                                     \
+    } while (0)
 
 extern "C" Zig::GlobalObject* Bun__getDefaultGlobalObject();
 

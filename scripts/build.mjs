d7972c47975380e7634c40d3c5cf57672261682b
Ashcon Partovi <ashcon@partovi.net>
2024-09-03 10:00:21 -0700

    Progress

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -3,71 +3,93 @@
 import { spawnSync } from "node:child_process";
 import { join } from "node:path";
 
-if (process.platform === "win32" && !process.env["VSINSTALLDIR"]) {
-  const shellPath = join(import.meta.dirname, "vs-shell.ps1");
-  const { status } = spawnSync("pwsh", ["-NoProfile", "-NoLogo", "-File", shellPath, ...process.argv], {
-    stdio: "inherit",
-  });
-  process.exit(status ?? 1);
-}
-
 // https://cmake.org/cmake/help/latest/manual/cmake.1.html#generate-a-project-buildsystem
 const generateFlags = [
-  "-S", // path to source directory
-  "-B", // path to build directory
-  "-D", // define a build option (e.g. -DCMAKE_BUILD_TYPE=Release)
-  "-G", // build generator (e.g. -GNinja)
-  "-W", // enable warnings (e.g. -Wno-dev)
-  "--fresh", // force a fresh build
-  "--log-level", // set the log level
-  "--debug-output", // print debug output
+  ["-S", "string", "path to source directory"],
+  ["-B", "string", "path to build directory"],
+  ["-D", "string", "define a build option (e.g. -DCMAKE_BUILD_TYPE=Release)"],
+  ["-G", "string", "build generator (e.g. -GNinja)"],
+  ["-W", "string", "enable warnings (e.g. -Wno-dev)"],
+  ["--fresh", "boolean", "force a fresh build"],
+  ["--log-level", "string", "set the log level"],
+  ["--debug-output", "boolean", "print debug output"],
 ];
 
 // https://cmake.org/cmake/help/latest/manual/cmake.1.html#generate-a-project-buildsystem
 const buildFlags = [
-  "--build", // path to build directory
-  "--config", // build configuration (e.g. --config Release)
-  "--target", // build target
-  "-t", // same as --target
-  "--parallel", // number of parallel jobs
-  "-j", // same as --parallel
-  "--verbose", // enable verbose output
-  "-v", // same as --verbose
+  ["--config", "string", "build configuration (e.g. --config Release)"],
+  ["--target", "string", "build target"],
+  ["-t", "string", "same as --target"],
+  ["--parallel", "number", "number of parallel jobs"],
+  ["-j", "number", "same as --parallel"],
+  ["--verbose", "boolean", "enable verbose output"],
+  ["-v", "boolean", "same as --verbose"],
 ];
 
-const args = process.argv.slice(2);
-const generateArgs = [];
-const buildArgs = [];
+function build(args) {
+  if (process.platform === "win32" && !process.env["VSINSTALLDIR"]) {
+    const shellPath = join(import.meta.dirname, "vs-shell.ps1");
+    const scriptPath = import.meta.filename;
+    spawn("pwsh", ["-NoProfile", "-NoLogo", "-File", shellPath, scriptPath, ...args], {
+      stdio: "inherit",
+    });
+    return;
+  }
 
-for (let i = 0; i < args.length; i++) {
-  const arg = args[i];
-  for (const [flags, result] of [
-    [generateFlags, generateArgs],
-    [buildFlags, buildArgs],
-  ]) {
-    for (const flag of flags) {
+  const env = {
+    ...process.env,
+    FORCE_COLOR: "1",
+    CLICOLOR_FORCE: "1",
+  };
+
+  const generateOptions = parseOptions(args, generateFlags);
+  const generateArgs = Object.entries(generateOptions).flatMap(([flag, value]) =>
+    flag.startsWith("-D") ? [`${flag}=${value}`] : [flag, value],
+  );
+  spawn("cmake", generateArgs, { stdio: "inherit", env });
+
+  const buildOptions = parseOptions(args, buildFlags);
+  const buildArgs = Object.entries(buildOptions).flatMap(([flag, value]) => [flag, value]);
+  const buildPath = generateOptions["-B"] || "build";
+  spawn("cmake", ["--build", buildPath, ...buildArgs], { stdio: "inherit", env });
+}
+
+function parseOptions(args, flags = []) {
+  const options = {};
+
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+
+    for (const [flag, type] of flags) {
       if (arg === flag) {
-        result.push(...args.slice(i, i + 2));
-        i++;
+        if (type === "boolean") {
+          options[arg] = undefined;
+        } else {
+          options[arg] = args[++i];
+        }
       } else if (arg.startsWith(flag)) {
-        result.push(...args.slice(i, i + 1));
+        const delim = arg.indexOf("=");
+        if (delim === -1) {
+          options[flag] = arg.slice(flag.length);
+        } else {
+          options[arg.slice(0, delim)] = arg.slice(delim + 1);
+        }
       }
     }
   }
+
+  return options;
 }
 
-for (const args of [generateArgs, buildArgs]) {
-  console.log("$", "cmake", ...args);
-  const timestamp = Date.now();
+function spawn(command, args, options) {
+  const description = [command, ...args]
+    .filter(Boolean)
+    .map(arg => (arg.includes(" ") ? JSON.stringify(arg) : arg))
+    .join(" ");
+  console.log("$", description);
 
-  const { status } = spawnSync("cmake", args, {
-    stdio: "inherit",
-    env: {
-      ...process.env,
-      FORCE_COLOR: "1",
-      CLICOLOR_FORCE: "1",
-    },
-  });
+  const timestamp = Date.now();
+  const { status, signal, error, stdout } = spawnSync(command, args.filter(Boolean), options);
 
   const duration = Date.now() - timestamp;
   if (duration > 60000) {
@@ -76,7 +98,19 @@ for (const args of [generateArgs, buildArgs]) {
     console.log(`Took ${(duration / 1000).toFixed(2)} seconds`);
   }
 
-  if (status !== 0) {
-    process.exit(status ?? 1);
+  if (status === 0) {
+    return stdout?.toString()?.trim();
   }
+
+  if (error) {
+    console.error(error);
+  } else if (signal) {
+    console.error(`Command killed: ${signal}`);
+  } else {
+    console.error(`Command exited: code ${status}`);
+  }
+
+  process.exit(status ?? 1);
 }
+
+build(process.argv.slice(2));

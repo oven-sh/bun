6bf542d35023e95a255d58c7814f473b72526247
Jarred Sumner <jarred@jarredsumner.com>
2024-09-04 00:04:51 -0700

    Fix poorly written test

--- test/js/bun/http/serve.test.ts
+++ test/js/bun/http/serve.test.ts
@@ -469,52 +469,48 @@ it("request.url should be based on the Host header", async () => {
 describe("streaming", () => {
   describe("error handler", () => {
     it("throw on pull renders headers, does not call error handler", async () => {
-      const onMessage = mock(async url => {
-        const response = await fetch(url);
-        expect(response.status).toBe(402);
-        expect(response.headers.get("X-Hey")).toBe("123");
-        expect(response.text()).resolves.toBe("");
-        subprocess?.kill();
-      });
-
-      await using subprocess = Bun.spawn({
+      const onMessage = Promise.withResolvers();
+      const subprocess = Bun.spawn({
         cwd: import.meta.dirname,
         cmd: [bunExe(), "readable-stream-throws.fixture.js"],
         env: bunEnv,
         stdout: "inherit",
         stderr: "pipe",
-        ipc: onMessage,
+        ipc: url => onMessage.resolve(url),
       });
+
+      const url = await onMessage.promise;
+      const response = await fetch(url);
+      expect(response.status).toBe(402);
+      expect(response.headers.get("X-Hey")).toBe("123");
+      expect(await response.text()).toBe("");
 
       let [exitCode, stderr] = await Promise.all([subprocess.exited, new Response(subprocess.stderr).text()]);
       expect(exitCode).toBeInteger();
       expect(stderr).toContain("error: Oop");
-      expect(onMessage).toHaveBeenCalled();
     });
 
     it("throw on pull after writing should not call the error handler", async () => {
-      const onMessage = mock(async href => {
-        const url = new URL("/write", href);
-        const response = await fetch(url);
-        expect(response.status).toBe(402);
-        expect(response.headers.get("X-Hey")).toBe("123");
-        expect(response.text()).resolves.toBe("");
-        subprocess.kill();
-      });
+      const onMessage = Promise.withResolvers();
 
-      await using subprocess = Bun.spawn({
+      const subprocess = Bun.spawn({
         cwd: import.meta.dirname,
         cmd: [bunExe(), "readable-stream-throws.fixture.js"],
         env: bunEnv,
         stdout: "inherit",
         stderr: "pipe",
-        ipc: onMessage,
+        ipc: url => onMessage.resolve(url),
       });
+
+      const url = await onMessage.promise;
+      const response = await fetch(url);
+      expect(response.status).toBe(402);
+      expect(response.headers.get("X-Hey")).toBe("123");
+      expect(await response.text()).toBe("");
 
       let [exitCode, stderr] = await Promise.all([subprocess.exited, new Response(subprocess.stderr).text()]);
       expect(exitCode).toBeInteger();
       expect(stderr).toContain("error: Oop");
-      expect(onMessage).toHaveBeenCalled();
     });
   });
 

12020faac800376ddd0f0955201f30372175da1f
Ashcon Partovi <ashcon@partovi.net>
2024-09-05 17:44:19 -0700

    Fix

--- cmake/tools/SetupBun.cmake
+++ cmake/tools/SetupBun.cmake
@@ -11,31 +11,13 @@ find_command(
     >=1.1.26
 )
 
-setenv(BUN_INSTALL_CACHE_DIR ${CACHE_PATH}/bun)
-
 # If this is not set, some advanced features are not checked.
 # https://github.com/oven-sh/bun/blob/cd7f6a1589db7f1e39dc4e3f4a17234afbe7826c/src/bun.js/javascript.zig#L1069-L1072
 setenv(BUN_GARBAGE_COLLECTOR_LEVEL 1)
 setenv(BUN_FEATURE_FLAG_INTERNAL_FOR_TESTING 1)
 setenv(BUN_DEBUG_QUIET_LOGS 1)
 
-if(CMAKE_HOST_WIN32)
-  setx(ESBUILD_EXECUTABLE ${CWD}/node_modules/.bin/esbuild.exe)
-else()
-  setx(ESBUILD_EXECUTABLE ${CWD}/node_modules/.bin/esbuild)
+# FIXME: https://github.com/oven-sh/bun/issues/11250
+if(NOT WIN32)
+  setenv(BUN_INSTALL_CACHE_DIR ${CACHE_PATH}/bun)
 endif()
-
-if(CMAKE_COLOR_DIAGNOSTICS)
-  set(ESBUILD_ARGS --color)
-endif()
-
-register_command(
-  COMMAND
-    ${BUN_EXECUTABLE}
-      install
-      --frozen-lockfile
-  SOURCES
-    ${CWD}/package.json
-  OUTPUTS
-    ${ESBUILD_EXECUTABLE}
-)

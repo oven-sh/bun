6eeab171ecbfdd9548e3bb07bf04c68ab2aecde3
Pavitra Golchha <hello@pavi2410.me>
2024-09-08 10:05:52 +0000

    Test & impl

--- test/js/node/util/util.test.js
+++ test/js/node/util/util.test.js
@@ -341,7 +341,7 @@ describe("util", () => {
   });
 
   it("styleText", () => {
-    [undefined, null, false, 5n, 5, Symbol(), () => {}, {}, []].forEach(invalidOption => {
+    [undefined, null, false, 5n, 5, Symbol(), () => {}, {}].forEach(invalidOption => {
       assert.throws(
         () => {
           util.styleText(invalidOption, "test");
@@ -370,6 +370,34 @@ describe("util", () => {
     );
 
     assert.strictEqual(util.styleText("red", "test"), "\u001b[31mtest\u001b[39m");
+
+    assert.strictEqual(
+      util.styleText(["bold", "red"], "test"),
+      "\u001b[1m\u001b[31mtest\u001b[39m\u001b[22m",
+    );
+
+    assert.strictEqual(
+      util.styleText(["bold", "red"], "test"),
+      util.styleText("bold", util.styleText("red", "test")),
+    );
+
+    assert.throws(
+      () => {
+        util.styleText(["invalid"], "text");
+      },
+      {
+        code: "ERR_INVALID_ARG_VALUE",
+      },
+    );
+
+    assert.throws(
+      () => {
+        util.styleText("red", "text");
+      },
+      {
+        code: "ERR_INVALID_ARG_TYPE",
+      },
+    );
   });
 
   describe("getSystemErrorName", () => {

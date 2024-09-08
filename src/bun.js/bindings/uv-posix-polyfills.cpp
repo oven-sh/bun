a299ed69bb171840b1cc825c09e155c6c8ccb24c
Jarred Sumner <jarred@jarredsumner.com>
2024-09-08 03:11:51 -0700

    Add a couple uv symbols

---
+++ src/bun.js/bindings/uv-posix-polyfills.cpp
@@ -0,0 +1,19 @@
+#include "root.h"
+
+#if OS(LINUX) || OS(DARWIN)
+
+typedef int uv_pid_t;
+
+// Returns the current process ID.
+extern "C" uv_pid_t uv_os_getpid()
+{
+    return getpid();
+}
+
+// Returns the parent process ID.
+extern "C" uv_pid_t uv_os_getppid()
+{
+    return getppid();
+}
+
+#endif

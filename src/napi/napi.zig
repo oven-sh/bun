5c05f004c3954aa9f42f419820945ba614d1f3d2
Ben Grant <ben@bun.sh>
2024-09-10 09:29:21 -0700

    Add uv_os functions to DCE suppression

--- src/napi/napi.zig
+++ src/napi/napi.zig
@@ -1838,6 +1838,8 @@ const V8API = if (!bun.Environment.isWindows) struct {
     pub extern fn _ZNK2v86String19ContainsOnlyOneByteEv() *anyopaque;
     pub extern fn _ZN2v812api_internal18GlobalizeReferenceEPNS_8internal7IsolateEm() *anyopaque;
     pub extern fn _ZN2v812api_internal13DisposeGlobalEPm() *anyopaque;
+    pub extern fn uv_os_getpid() *anyopaque;
+    pub extern fn uv_os_getppid() *anyopaque;
 } else struct {
     // MSVC name mangling is different than it is on unix.
     // To make this easier to deal with, I have provided a script to generate the list of functions.

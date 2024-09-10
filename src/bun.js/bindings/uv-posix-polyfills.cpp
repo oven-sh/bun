459dfb54a7e93cd438d50532327f20c2e69139d6
Ben Grant <ben@bun.sh>
2024-09-09 17:30:54 -0700

    Separate declarations and definitions of uv_os functions

--- src/bun.js/bindings/uv-posix-polyfills.cpp
+++ src/bun.js/bindings/uv-posix-polyfills.cpp
@@ -1,19 +1,11 @@
-#include "root.h"
+#include "uv-posix-polyfills.h"
 
-#if OS(LINUX) || OS(DARWIN)
-
-typedef int uv_pid_t;
-
-// Returns the current process ID.
-extern "C" BUN_EXPORT uv_pid_t uv_os_getpid()
+uv_pid_t uv_os_getpid()
 {
     return getpid();
 }
 
-// Returns the parent process ID.
-extern "C" BUN_EXPORT uv_pid_t uv_os_getppid()
+uv_pid_t uv_os_getppid()
 {
     return getppid();
 }
-
-#endif

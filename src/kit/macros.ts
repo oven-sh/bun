c62fa27b49b2d8bbf8ce4c95cf2c25007032e5ad
dave caruso <me@paperdave.net>
2024-09-09 17:31:07 -0700

    fix

--- src/kit/macros.ts
+++ src/kit/macros.ts
@@ -4,7 +4,8 @@ import { resolve } from 'node:path';
 export function css(file: string, is_development: boolean): string {
   const contents = readFileSync(resolve(import.meta.dir, file), 'utf-8');
   if (!is_development) {
-    throw new Error('TODO: minify');
+    // TODO: minify
+    return contents;
   }
   return contents;
 }

8988aee234088b87339bcf023374284e8a406539
cirospaciari <ciro.spaciari@gmail.com>
2024-09-11 11:48:13 -0700

    basic pipe tests

--- test/js/node/net/node-net.test.ts
+++ test/js/node/net/node-net.test.ts
@@ -1,8 +1,10 @@
-import { Socket as _BunSocket, TCPSocketListener } from "bun";
+import { Socket as _BunSocket, resolve, TCPSocketListener } from "bun";
 import { describe, expect, it } from "bun:test";
 import { bunEnv, bunExe, tmpdirSync } from "harness";
-import { connect, createConnection, isIP, isIPv4, isIPv6, Server, Socket, Stream } from "net";
-import { join } from "path";
+import { connect, createServer, createConnection, isIP, isIPv4, isIPv6, Server, Socket, Stream } from "node:net";
+import { join } from "node:path";
+import { once } from "node:events";
+import { randomUUID } from "node:crypto";
 
 const socket_domain = tmpdirSync();
 
@@ -559,4 +561,43 @@ it("should not hang after destroy", async () => {
   } finally {
     server.close();
   }
+});
+
+it("should work with named pipes", async () => {
+  async function test(pipe_name: string) {
+    const { promise: messageReceived, resolve: resolveMessageReceived } = Promise.withResolvers();
+    const { promise: clientReceived, resolve: resolveClientReceived } = Promise.withResolvers();
+    let client: ReturnType<typeof connect> | null = null;
+    let server: ReturnType<typeof createServer> | null = null;
+    try {
+      server = createServer(socket => {
+        socket.on("data", data => {
+          const message = data.toString();
+          console.log(`Message received from client: ${message}`);
+          socket.write("Goodbye World!");
+          resolveMessageReceived(message);
+        });
+      });
+
+      server.listen(pipe_name);
+      await once(server, "listening");
+
+      client = connect(pipe_name).on("data", data => {
+        const message = data.toString();
+        resolveClientReceived(message);
+      });
+
+      client?.write("Hello World!");
+      const message = await messageReceived;
+      expect(message).toBe("Hello World!");
+      const client_message = await clientReceived;
+      expect(client_message).toBe("Goodbye World!");
+    } finally {
+      client?.destroy();
+      server?.close();
+    }
+  }
+
+  await test(`\\\\.\\pipe\\test\\${randomUUID()}`);
+  await test(`\\\\?\\pipe\\test\\${randomUUID()}`);
 });
---
+++ test/js/node/tls/node-tls-namedpipes.test.ts
@@ -0,0 +1,44 @@
+import { expect, it } from "bun:test";
+import { tls } from "harness";
+import { connect, createServer } from "node:tls";
+import { once } from "node:events";
+import { randomUUID } from "node:crypto";
+
+it("should work with named pipes and tls", async () => {
+  async function test(pipe_name: string) {
+    const { promise: messageReceived, resolve: resolveMessageReceived } = Promise.withResolvers();
+    const { promise: clientReceived, resolve: resolveClientReceived } = Promise.withResolvers();
+    let client: ReturnType<typeof connect> | null = null;
+    let server: ReturnType<typeof createServer> | null = null;
+    try {
+      server = createServer(tls, socket => {
+        socket.on("data", data => {
+          const message = data.toString();
+          console.log(`Message received from client: ${message}`);
+          socket.write("Goodbye World!");
+          resolveMessageReceived(message);
+        });
+      });
+
+      server.listen(pipe_name);
+      await once(server, "listening");
+
+      client = connect({ path: pipe_name, ca: tls.cert }).on("data", data => {
+        const message = data.toString();
+        resolveClientReceived(message);
+      });
+
+      client?.write("Hello World!");
+      const message = await messageReceived;
+      expect(message).toBe("Hello World!");
+      const client_message = await clientReceived;
+      expect(client_message).toBe("Goodbye World!");
+    } finally {
+      client?.destroy();
+      server?.close();
+    }
+  }
+
+  await test(`\\\\.\\pipe\\test\\${randomUUID()}`);
+  await test(`\\\\?\\pipe\\test\\${randomUUID()}`);
+});

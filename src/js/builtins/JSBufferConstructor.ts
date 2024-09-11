7a9a99ea048aefa0704f283abf4b4d43ecc12afe
Meghan Denny <meghan@bun.sh>
2024-09-10 18:25:59 -0700

    add missing Buffer.from check

--- src/js/builtins/JSBufferConstructor.ts
+++ src/js/builtins/JSBufferConstructor.ts
@@ -29,6 +29,11 @@ export function from(items) {
       }
     }
   }
+  if (typeof items === "object") {
+    if (items.type === "Buffer" && Array.isArray(items.data)) {
+      return new $Buffer(Uint8Array.from(items.data).buffer);
+    }
+  }
 
   var arrayLike = $toObject(
     items,

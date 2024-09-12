f07b126125f517f579a896296d728732d55cce88
Meghan Denny <meghan@bun.sh>
2024-09-11 20:36:57 -0700

    these are dead imports now

--- src/js/internal/validators.ts
+++ src/js/internal/validators.ts
@@ -1,5 +1,3 @@
-const { ERR_INVALID_ARG_TYPE, ERR_OUT_OF_RANGE, ERR_BUFFER_OUT_OF_BOUNDS } = require("internal/errors");
-
 export default {
   validateIntRange: $newCppFunction("NodeValidator.cpp", "jsFunction_validateIntRange", 0),
   validateBounds: $newCppFunction("NodeValidator.cpp", "jsFunction_validateBounds", 0),

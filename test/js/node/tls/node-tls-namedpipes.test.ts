c07f159191a99c82e4454efde6e9124690e1a816
cirospaciari <ciro.spaciari@gmail.com>
2024-09-11 17:31:12 -0700

    uncomment

--- test/js/node/tls/node-tls-namedpipes.test.ts
+++ test/js/node/tls/node-tls-namedpipes.test.ts
@@ -6,54 +6,54 @@ import { once } from "node:events";
 import { randomUUID } from "node:crypto";
 import { heapStats } from "bun:jsc";
 
-// it.if(isWindows)("should work with named pipes and tls", async () => {
-//   async function test(pipe_name: string) {
-//     const { promise: messageReceived, resolve: resolveMessageReceived } = Promise.withResolvers();
-//     const { promise: clientReceived, resolve: resolveClientReceived } = Promise.withResolvers();
-//     let client: ReturnType<typeof connect> | null = null;
-//     let server: ReturnType<typeof createServer> | null = null;
-//     try {
-//       server = createServer(tls, socket => {
-//         socket.on("data", data => {
-//           const message = data.toString();
-//           socket.write("Goodbye World!");
-//           resolveMessageReceived(message);
-//         });
-//       });
+it.if(isWindows)("should work with named pipes and tls", async () => {
+  async function test(pipe_name: string) {
+    const { promise: messageReceived, resolve: resolveMessageReceived } = Promise.withResolvers();
+    const { promise: clientReceived, resolve: resolveClientReceived } = Promise.withResolvers();
+    let client: ReturnType<typeof connect> | null = null;
+    let server: ReturnType<typeof createServer> | null = null;
+    try {
+      server = createServer(tls, socket => {
+        socket.on("data", data => {
+          const message = data.toString();
+          socket.write("Goodbye World!");
+          resolveMessageReceived(message);
+        });
+      });
 
-//       server.listen(pipe_name);
-//       await once(server, "listening");
+      server.listen(pipe_name);
+      await once(server, "listening");
 
-//       client = connect({ path: pipe_name, ca: tls.cert }).on("data", data => {
-//         const message = data.toString();
-//         resolveClientReceived(message);
-//       });
+      client = connect({ path: pipe_name, ca: tls.cert }).on("data", data => {
+        const message = data.toString();
+        resolveClientReceived(message);
+      });
 
-//       client?.write("Hello World!");
-//       const message = await messageReceived;
-//       expect(message).toBe("Hello World!");
-//       const client_message = await clientReceived;
-//       expect(client_message).toBe("Goodbye World!");
-//     } finally {
-//       client?.destroy();
-//       server?.close();
-//     }
-//   }
+      client?.write("Hello World!");
+      const message = await messageReceived;
+      expect(message).toBe("Hello World!");
+      const client_message = await clientReceived;
+      expect(client_message).toBe("Goodbye World!");
+    } finally {
+      client?.destroy();
+      server?.close();
+    }
+  }
 
-//   const batch = [];
+  const batch = [];
 
-//   const before = heapStats().objectTypeCounts.TLSSocket || 0;
-//   for (let i = 0; i < 200; i++) {
-//     batch.push(test(`\\\\.\\pipe\\test\\${randomUUID()}`));
-//     batch.push(test(`\\\\?\\pipe\\test\\${randomUUID()}`));
-//     if (i % 50 === 0) {
-//       await Promise.all(batch);
-//       batch.length = 0;
-//     }
-//   }
-//   await Promise.all(batch);
-//   expectMaxObjectTypeCount(expect, "TLSSocket", before);
-// });
+  const before = heapStats().objectTypeCounts.TLSSocket || 0;
+  for (let i = 0; i < 200; i++) {
+    batch.push(test(`\\\\.\\pipe\\test\\${randomUUID()}`));
+    batch.push(test(`\\\\?\\pipe\\test\\${randomUUID()}`));
+    if (i % 50 === 0) {
+      await Promise.all(batch);
+      batch.length = 0;
+    }
+  }
+  await Promise.all(batch);
+  expectMaxObjectTypeCount(expect, "TLSSocket", before);
+});
 
 it.if(isWindows)("should be able to upgrade a named pipe connection to TLS", async () => {
   const { promise: messageReceived, resolve: resolveMessageReceived } = Promise.withResolvers();

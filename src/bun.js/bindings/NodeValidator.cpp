5e1894a9078899015af355f1ec72299c83d70c98
Meghan Denny <meghan@bun.sh>
2024-09-11 20:33:49 -0700

    condense these significantly

--- src/bun.js/bindings/NodeValidator.cpp
+++ src/bun.js/bindings/NodeValidator.cpp
@@ -19,21 +19,11 @@ JSC_DEFINE_HOST_FUNCTION(jsFunction_validateInteger, (JSC::JSGlobalObject * glob
     auto min = callFrame->argument(2);
     auto max = callFrame->argument(3);
 
-    if (!value.isNumber()) {
-        return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, name, "number"_s, value);
-    }
-    if (min.isUndefined()) {
-        min = jsNumber(-9007199254740991); // Number.MIN_SAFE_INTEGER
-    }
-    if (max.isUndefined()) {
-        max = jsNumber(9007199254740991); // Number.MAX_SAFE_INTEGER
-    }
-    if (!min.isNumber()) {
-        return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "min"_s, "number"_s, value);
-    }
-    if (!max.isNumber()) {
-        return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "max"_s, "number"_s, value);
-    }
+    if (!value.isNumber()) return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, name, "number"_s, value);
+    if (min.isUndefined()) min = jsNumber(-9007199254740991); // Number.MIN_SAFE_INTEGER
+    if (max.isUndefined()) max = jsNumber(9007199254740991); // Number.MAX_SAFE_INTEGER
+    if (!min.isNumber()) return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "min"_s, "number"_s, value);
+    if (!max.isNumber()) return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "max"_s, "number"_s, value);
 
     auto value_num = value.asNumber();
     auto min_num = min.asNumber();
@@ -41,12 +31,8 @@ JSC_DEFINE_HOST_FUNCTION(jsFunction_validateInteger, (JSC::JSGlobalObject * glob
     max_num = std::max(min_num, max_num);
 
     double intpart;
-    if (std::modf(value_num, &intpart) != 0) {
-        return Bun::ERR::OUT_OF_RANGE(scope, globalObject, name, "an integer"_s, value);
-    }
-    if (value_num < min_num || value_num > max_num) {
-        return Bun::ERR::OUT_OF_RANGE(scope, globalObject, name, min_num, max_num, value);
-    }
+    if (std::modf(value_num, &intpart) != 0) return Bun::ERR::OUT_OF_RANGE(scope, globalObject, name, "an integer"_s, value);
+    if (value_num < min_num || value_num > max_num) return Bun::ERR::OUT_OF_RANGE(scope, globalObject, name, min_num, max_num, value);
 
     return JSValue::encode(jsUndefined());
 }
@@ -64,27 +50,17 @@ JSC_DEFINE_HOST_FUNCTION(jsFunction_validateIntRange, (JSC::JSGlobalObject * glo
     auto min = callFrame->argument(2);
     auto max = callFrame->argument(3);
 
-    if (!value.isNumber()) {
-        return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, name, "number"_s, value);
-    }
-    if (!min.isNumber()) {
-        return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "min"_s, "number"_s, value);
-    }
-    if (!max.isNumber()) {
-        return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "max"_s, "number"_s, value);
-    }
+    if (!value.isNumber()) return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, name, "number"_s, value);
+    if (!min.isNumber()) return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "min"_s, "number"_s, value);
+    if (!max.isNumber()) return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "max"_s, "number"_s, value);
 
     auto value_num = value.asNumber();
     auto min_num = min.asNumber();
     auto max_num = max.asNumber();
 
-    if (value_num < min_num || value_num > max_num || std::isinf(value_num)) {
-        return Bun::ERR::OUT_OF_RANGE(scope, globalObject, name, min_num, max_num, value);
-    }
+    if (value_num < min_num || value_num > max_num || std::isinf(value_num)) return Bun::ERR::OUT_OF_RANGE(scope, globalObject, name, min_num, max_num, value);
     double intpart;
-    if (std::modf(value_num, &intpart) != 0) {
-        return Bun::ERR::OUT_OF_RANGE(scope, globalObject, name, "an integer"_s, value);
-    }
+    if (std::modf(value_num, &intpart) != 0) return Bun::ERR::OUT_OF_RANGE(scope, globalObject, name, "an integer"_s, value);
 
     return JSValue::encode(jsUndefined());
 }
@@ -100,28 +76,18 @@ JSC_DEFINE_HOST_FUNCTION(jsFunction_validateBounds, (JSC::JSGlobalObject * globa
     auto buffer = callFrame->argument(3);
     auto width = callFrame->argument(4);
 
-    if (!value.isNumber()) {
-        return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, name, "number"_s, value);
-    }
+    if (!value.isNumber()) return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, name, "number"_s, value);
 
     auto byteLength = buffer.get(globalObject, Identifier::fromString(vm, "byteLength"_s));
     RETURN_IF_EXCEPTION(scope, {});
-    if (!byteLength.isNumber()) {
-        return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "byteLength"_s, "number"_s, byteLength);
-    }
+    if (!byteLength.isNumber()) return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "byteLength"_s, "number"_s, byteLength);
 
     auto byteOffset = buffer.get(globalObject, Identifier::fromString(vm, "byteOffset"_s));
     RETURN_IF_EXCEPTION(scope, {});
-    if (!byteOffset.isNumber()) {
-        return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "byteOffset"_s, "number"_s, byteOffset);
-    }
+    if (!byteOffset.isNumber()) return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "byteOffset"_s, "number"_s, byteOffset);
 
-    if (!min.isNumber()) {
-        return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "min"_s, "number"_s, min);
-    }
-    if (!width.isNumber()) {
-        return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "width"_s, "number"_s, width);
-    }
+    if (!min.isNumber()) return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "min"_s, "number"_s, min);
+    if (!width.isNumber()) return Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "width"_s, "number"_s, width);
 
     auto value_num = value.asNumber();
     auto min_num = min.asNumber();
@@ -132,19 +98,11 @@ JSC_DEFINE_HOST_FUNCTION(jsFunction_validateBounds, (JSC::JSGlobalObject * globa
     auto max_num = byteLength_num - byteOffset_num - width_num;
     max_num = std::max(min_num, max_num);
 
-    if (std::isinf(value_num)) {
-        return Bun::ERR::OUT_OF_RANGE(scope, globalObject, name, min_num, max_num, value);
-    }
+    if (std::isinf(value_num)) return Bun::ERR::OUT_OF_RANGE(scope, globalObject, name, min_num, max_num, value);
     double intpart;
-    if (std::modf(value_num, &intpart) != 0) {
-        return Bun::ERR::OUT_OF_RANGE(scope, globalObject, name, "an integer"_s, value);
-    }
-    if (byteOffset_num + value_num > byteLength_num || byteOffset_num + value_num + width_num - 1 > byteLength_num) {
-        return Bun::ERR::BUFFER_OUT_OF_BOUNDS(scope, globalObject);
-    }
-    if (value_num < min_num || value_num > max_num) {
-        return Bun::ERR::OUT_OF_RANGE(scope, globalObject, name, min_num, max_num, value);
-    }
+    if (std::modf(value_num, &intpart) != 0) return Bun::ERR::OUT_OF_RANGE(scope, globalObject, name, "an integer"_s, value);
+    if (byteOffset_num + value_num > byteLength_num || byteOffset_num + value_num + width_num - 1 > byteLength_num) return Bun::ERR::BUFFER_OUT_OF_BOUNDS(scope, globalObject);
+    if (value_num < min_num || value_num > max_num) return Bun::ERR::OUT_OF_RANGE(scope, globalObject, name, min_num, max_num, value);
 
     return JSValue::encode(jsUndefined());
 }

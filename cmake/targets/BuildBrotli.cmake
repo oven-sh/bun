4cccc485861cd53f9da0da773ab7a2768060d8fe
Ashcon Partovi <ashcon@partovi.net>
2024-09-12 17:31:30 -0700

    Big opps

--- cmake/targets/BuildBrotli.cmake
+++ cmake/targets/BuildBrotli.cmake
@@ -9,7 +9,7 @@ register_repository(
 
 # Tests fail with "BrotliDecompressionError" when LTO is enabled
 # only on Linux x64 (non-baseline). It's a mystery.
-if(LINUX AND CMAKE_SYSTEM_PROCESSOR MATCHES "arm64|ARM64|aarch64|AARCH64" AND NOT ENABLE_BASELINE)
+if(LINUX AND CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64|X86_64|x64|X64|amd64|AMD64" AND NOT ENABLE_BASELINE)
   set(BROTLI_CMAKE_ARGS -DCMAKE_C_FLAGS="-fno-lto")
 endif()
 

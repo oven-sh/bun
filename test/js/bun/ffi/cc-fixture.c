d412e60b69e55121b1b8251bfdd32be1d8057389
Jarred Sumner <jarred@jarredsumner.com>
2024-09-13 00:28:03 -0700

    Dumb it down further for CI

--- test/js/bun/ffi/cc-fixture.c
+++ test/js/bun/ffi/cc-fixture.c
@@ -3,11 +3,8 @@
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stddef.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdnoreturn.h>
-#include <string.h>
 
 int main() {
   // Check fprint stdout and stderr.
@@ -16,20 +13,7 @@ int main() {
 
   // Verify printf doesn't crash.
   printf("Hello, World!\n");
-
-  // Verify stdint.h works.
-  uint8_t a = 1;
-  uint16_t b = 2;
-  uint32_t c = 3;
-  uint64_t d = 4;
-  printf("uint8_t 1 = %hhu, uint16_t 2 = %hu, uint32_t 3 = %u, uint64_t 4 = "
-         "%llu\n",
-         a, b, c, d);
-
-  // Verify stddef.h works.
-  size_t e = 5;
-  ptrdiff_t f = 6;
-  printf("size_t 5 = %zu, ptrdiff_t 6 = %td\n", e, f);
+  printf("Hi!, 123 == %d\n", 123);
 
   // Verify stdbool.h works.
   bool g = true;

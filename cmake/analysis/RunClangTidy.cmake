800e378b6e3403abdae8304ce35a473a8625620e
Ashcon Partovi <ashcon@partovi.net>
2024-09-14 14:29:45 -0700

    Run scripts

--- cmake/analysis/RunClangTidy.cmake
+++ cmake/analysis/RunClangTidy.cmake
@@ -11,25 +11,61 @@ find_command(
     OFF
 )
 
-set(CLANG_TIDY_COMMAND ${CLANG_TIDY_PROGRAM}
-  -p ${BUILD_PATH}  
-  --config-file=${CWD}/.clang-tidy
-  --fix
-  --fix-errors
-  --fix-notes
-  ${BUN_C_SOURCES}
-  ${BUN_CXX_SOURCES}
+set(CLANG_TIDY_SOURCES ${BUN_C_SOURCES} ${BUN_CXX_SOURCES})
+
+find_command(
+  VARIABLE
+    GIT_PROGRAM
+  COMMAND
+    git
+  REQUIRED
+    OFF
 )
 
-if(CMAKE_COLOR_DIAGNOSTICS)
-  list(APPEND CLANG_TIDY_COMMAND --use-color)
+if(GIT_PROGRAM)
+  execute_process(
+    COMMAND
+      ${GIT_PROGRAM}
+        diff
+        --name-only
+        --diff-filter=AM
+        main
+    WORKING_DIRECTORY
+      ${CWD}
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    OUTPUT_VARIABLE
+      GIT_CHANGED_FILES
+    ERROR_QUIET
+  )
+  string(REPLACE "\n" ";" GIT_CHANGED_FILES ${GIT_CHANGED_FILES})
+  list(TRANSFORM GIT_CHANGED_FILES PREPEND ${CWD}/)
+
+  set(CLANG_TIDY_CHANGED_SOURCES)
+  foreach(source ${CLANG_TIDY_SOURCES})
+    list(FIND GIT_CHANGED_FILES ${source} index)
+    if(NOT ${index} EQUAL -1)
+      list(APPEND CLANG_TIDY_CHANGED_SOURCES ${source})
+    endif()
+  endforeach()
+
+  if(CLANG_TIDY_CHANGED_SOURCES)
+    set(CLANG_TIDY_SOURCES ${CLANG_TIDY_CHANGED_SOURCES})
+  else()
+    set(CLANG_TIDY_COMMAND ${CMAKE_COMMAND} -E echo "No files changed for clang-tidy")
+  endif()
 endif()
 
-# Extra clang-tidy checks that are normally disabled due to noise.
-# e.g. JavaScriptCore/Lookup.h
-set(CLANG_TIDY_EXTRA_COMMAND ${CLANG_TIDY_PROGRAM}
-  --checks=performance-*
-)
+if(NOT CLANG_TIDY_COMMAND)
+  set(CLANG_TIDY_COMMAND ${CLANG_TIDY_PROGRAM}
+    -p ${BUILD_PATH}  
+    --config-file=${CWD}/.clang-tidy
+    --fix
+    --fix-errors
+    --fix-notes
+    --use-color
+    ${CLANG_TIDY_SOURCES}
+  )
+endif()
 
 register_command(
   TARGET
@@ -38,18 +74,6 @@ register_command(
     "Running clang-tidy"
   COMMAND
     ${CLANG_TIDY_COMMAND} 
-  CWD
-    ${BUILD_PATH}
-  ALWAYS_RUN
-)
-
-register_command(
-  TARGET
-    clang-tidy-extra
-  COMMENT
-    "Running clang-tidy with extra checks"
-  COMMAND
-    ${CLANG_TIDY_EXTRA_COMMAND} 
   CWD
     ${BUILD_PATH}
   ALWAYS_RUN

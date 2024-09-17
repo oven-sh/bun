fba1d65cc7be211c5f4e2a49b085946efdb0f668
Ragavendaran Puliyadi <ragavendaran.puli@gmail.com>
2024-09-17 22:10:15 +0530

    fix: use Bun's Fetch interface for fetch Allows TypeScript to use Bun's FetchRequestInit instead of Node's RequestInit in fetch request options.

--- packages/bun-types/bun.d.ts
+++ packages/bun-types/bun.d.ts
@@ -3170,7 +3170,7 @@ declare module "bun" {
    */
   function openInEditor(path: string, options?: EditorOptions): void;
 
-  const fetch: typeof globalThis.fetch & {
+  const fetch: Fetch & {
     preconnect(url: string): void;
   };
 

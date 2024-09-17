685b0f871c63cad252b041187084f892d0933790
Ciro Spaciari <ciro.spaciari@gmail.com>
2024-09-17 16:05:34 -0700

    fix socket

--- src/bun.js/api/bun/socket.zig
+++ src/bun.js/api/bun/socket.zig
@@ -1328,13 +1328,12 @@ fn NewSocket(comptime ssl: bool) type {
         protos: ?[]const u8,
         server_name: ?[]const u8 = null,
 
-
         // TODO: switch to something that uses `visitAggregate` and have the
         // `Listener` keep a list of all the sockets JSValue in there
         // This is wasteful because it means we are keeping a JSC::Weak for every single open socket
         has_pending_activity: std.atomic.Value(bool) = std.atomic.Value(bool).init(true),
-        pub usingnamespace bun.NewRefCounted(@This(), @This().deinit);
         native_callbacks: NativeCallbacks = .{},
+        pub usingnamespace bun.NewRefCounted(@This(), @This().deinit);
 
         // We use this direct callbacks on HTTP2 when available
         pub const NativeCallbacks = struct {
@@ -1427,7 +1426,7 @@ fn NewSocket(comptime ssl: bool) type {
             JSC.markBinding(@src());
             log("onWritable", .{});
             if (this.socket.isDetached()) return;
-            if(this.native_callbacks.onWritable) |nativeCallback| {
+            if (this.native_callbacks.onWritable) |nativeCallback| {
                 nativeCallback(this.native_callbacks.ctx);
                 return;
             }
@@ -1796,8 +1795,8 @@ fn NewSocket(comptime ssl: bool) type {
             this.socket.detach();
             defer this.deref();
             defer this.markInactive();
-            
-            if(this.native_callbacks.onClose) |nativeCallback| {
+
+            if (this.native_callbacks.onClose) |nativeCallback| {
                 nativeCallback(this.native_callbacks.ctx);
             }
 
@@ -1838,20 +1837,16 @@ fn NewSocket(comptime ssl: bool) type {
             log("onData({d})", .{data.len});
             if (this.socket.isDetached()) return;
 
+            if (this.native_callbacks.onData) |nativeCallback| {
+                nativeCallback(this.native_callbacks.ctx, data);
+            }
+
             const handlers = this.handlers;
             const callback = handlers.onData;
             if (callback == .zero or this.flags.finalizing) return;
             if (handlers.vm.isShuttingDown()) {
                 return;
             }
-            
-            if(this.native_callbacks.onData) |nativeCallback| {
-                nativeCallback(this.native_callbacks.ctx, data);
-            }
-
-            const handlers = this.handlers;
-            const callback = handlers.onData;
-            if (callback == .zero) return;
 
             const globalObject = handlers.globalObject;
             const this_value = this.getThisValue(globalObject);
@@ -2040,7 +2035,7 @@ fn NewSocket(comptime ssl: bool) type {
             return ZigString.init(text).toJS(globalThis);
         }
 
-        fn writeMaybeCorked(this: *This, buffer: []const u8, is_end: bool) i32 {
+        pub fn writeMaybeCorked(this: *This, buffer: []const u8, is_end: bool) i32 {
             if (this.socket.isShutdown() or this.socket.isClosed()) {
                 return -1;
             }

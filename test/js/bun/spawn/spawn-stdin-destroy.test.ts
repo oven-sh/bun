oven-sh/bun

commit eb8ed27a4a47a9759413cbb6d9b3b4ebf34a2a60

fix(ipc/subprocess)   (#13414)
Co-authored-by: Jarred Sumner <jarred@jarredsumner.com>

src/bun.js/api/bun/subprocess.zig
@@ -202,6 +202,9 @@ pub const Subprocess = struct {
     flags: Flags = .{},
 
     weak_file_sink_stdin_ptr: ?*JSC.WebCore.FileSink = null,
+    ref_count: u32 = 1,
+
+    usingnamespace bun.NewRefCounted(@This(), Subprocess.deinit);
 
     pub const Flags = packed struct {
         is_sync: bool = false,
@@ -345,7 +348,7 @@ pub const Subprocess = struct {
         return this.has_pending_activity.load(.acquire);
     }
 
-    pub fn ref(this: *Subprocess) void {
+    pub fn jsRef(this: *Subprocess) void {
         this.process.enableKeepingEventLoopAlive();
 
         if (!this.hasCalledGetter(.stdin)) {
@@ -364,7 +367,7 @@ pub const Subprocess = struct {
     }
 
     /// This disables the keeping process alive flag on the poll and also in the stdin, stdout, and stderr
-    pub fn unref(this: *Subprocess) void {
+    pub fn jsUnref(this: *Subprocess) void {
         this.process.disableKeepingEventLoopAlive();
 
         if (!this.hasCalledGetter(.stdin)) {
@@ -675,7 +678,6 @@ pub const Subprocess = struct {
         if (this.hasExited()) {
             return .{ .result = {} };
         }
-
         return this.process.kill(@intCast(sig));
     }
 
@@ -691,23 +693,20 @@ pub const Subprocess = struct {
     }
 
     pub fn doRef(this: *Subprocess, _: *JSC.JSGlobalObject, _: *JSC.CallFrame) JSValue {
-        this.ref();
+        this.jsRef();
         return .undefined;
     }
 
     pub fn doUnref(this: *Subprocess, _: *JSC.JSGlobalObject, _: *JSC.CallFrame) JSValue {
-        this.unref();
+        this.jsUnref();
         return .undefined;
     }
 
     pub fn onStdinDestroyed(this: *Subprocess) void {
         this.flags.has_stdin_destructor_called = true;
         this.weak_file_sink_stdin_ptr = null;
-
-        if (this.flags.finalized) {
-            // if the process has already been garbage collected, we can free the memory now
-            bun.default_allocator.destroy(this);
-        } else {
+        defer this.deref();
+        if (!this.flags.finalized) {
             // otherwise update the pending activity flag
             this.updateHasPendingActivity();
         }
@@ -736,19 +735,21 @@ pub const Subprocess = struct {
 
         return .undefined;
     }
-
+    pub fn disconnectIPC(this: *Subprocess, nextTick: bool) void {
+        const ipc_data = this.ipc() orelse return;
+        ipc_data.close(nextTick);
+    }
     pub fn disconnect(this: *Subprocess, globalThis: *JSGlobalObject, callframe: *JSC.CallFrame) JSValue {
         _ = globalThis;
         _ = callframe;
-        const ipc_data = this.ipc_maybe() orelse return .undefined;
-        ipc_data.close();
-        this.ipc_data = null;
+        this.disconnectIPC(true);
         return .undefined;
     }
 
     pub fn getConnected(this: *Subprocess, globalThis: *JSGlobalObject) JSValue {
         _ = globalThis;
-        return JSValue.jsBoolean(this.ipc_maybe() != null);
+        const ipc_data = this.ipc();
+        return JSValue.jsBoolean(ipc_data != null and ipc_data.?.disconnected == false);
     }
 
     pub fn pid(this: *const Subprocess) i32 {
@@ -837,7 +838,7 @@ pub const Subprocess = struct {
             ref_count: u32 = 1,
             buffer: []const u8 = "",
 
-            pub usingnamespace bun.NewRefCounted(@This(), deinit);
+            pub usingnamespace bun.NewRefCounted(@This(), @This().deinit);
             const This = @This();
             const print = bun.Output.scoped(.StaticPipeWriter, false);
 
@@ -961,7 +962,7 @@ pub const Subprocess = struct {
         pub const IOReader = bun.io.BufferedReader;
         pub const Poll = IOReader;
 
-        pub usingnamespace bun.NewRefCounted(PipeReader, deinit);
+        pub usingnamespace bun.NewRefCounted(PipeReader, PipeReader.deinit);
 
         pub fn hasPendingActivity(this: *const PipeReader) bool {
             if (this.state == .pending)
@@ -1238,6 +1239,7 @@ pub const Subprocess = struct {
                             }
                             pipe.writer.setParent(pipe);
                             subprocess.weak_file_sink_stdin_ptr = pipe;
+                            subprocess.ref();
                             subprocess.flags.has_stdin_destructor_called = false;
 
                             return Writable{
@@ -1296,6 +1298,7 @@ pub const Subprocess = struct {
                     }
 
                     subprocess.weak_file_sink_stdin_ptr = pipe;
+                    subprocess.ref();
                     subprocess.flags.has_stdin_destructor_called = false;
 
                     pipe.writer.handle.poll.flags.insert(.socket);
@@ -1347,6 +1350,7 @@ pub const Subprocess = struct {
                     } else {
                         subprocess.flags.has_stdin_destructor_called = false;
                         subprocess.weak_file_sink_stdin_ptr = pipe;
+                        subprocess.ref();
                         if (@intFromPtr(pipe.signal.ptr) == @intFromPtr(subprocess)) {
                             pipe.signal.clear();
                         }
@@ -1415,6 +1419,9 @@ pub const Subprocess = struct {
         this_jsvalue.ensureStillAlive();
         this.pid_rusage = rusage.*;
         const is_sync = this.flags.is_sync;
+        defer this.deref();
+        defer this.disconnectIPC(true);
+
 
         var stdin: ?*JSC.WebCore.FileSink = this.weak_file_sink_stdin_ptr;
         var existing_stdin_value = JSC.JSValue.zero;
@@ -1557,6 +1564,11 @@ pub const Subprocess = struct {
         this.on_disconnect_callback.deinit();
     }
 
+    pub fn deinit(this: *Subprocess) void {
+        log("deinit", .{});
+        this.destroy();
+    }
+
     pub fn finalize(this: *Subprocess) callconv(.C) void {
         log("finalize", .{});
         // Ensure any code which references the "this" value doesn't attempt to
@@ -1571,10 +1583,7 @@ pub const Subprocess = struct {
         this.process.deref();
 
         this.flags.finalized = true;
-        if (this.weak_file_sink_stdin_ptr == null) {
-            // if no file sink exists we can free immediately
-            bun.default_allocator.destroy(this);
-        }
+        this.deref();
     }
 
     pub fn getExited(
@@ -2057,7 +2066,7 @@ pub const Subprocess = struct {
             } else {},
         };
 
-        const process_allocator = globalThis.allocator();
+        const process_allocator = bun.default_allocator;
         var subprocess = process_allocator.create(Subprocess) catch {
             globalThis.throwOutOfMemory();
             return .zero;
@@ -2156,23 +2165,28 @@ pub const Subprocess = struct {
                 .is_sync = is_sync,
             },
         };
+        subprocess.ref(); // + one ref for the process
         subprocess.process.setExitHandler(subprocess);
 
         if (subprocess.ipc_data) |*ipc_data| {
             if (Environment.isPosix) {
                 if (posix_ipc_info.ext(*Subprocess)) |ctx| {
                     ctx.* = subprocess;
+                    subprocess.ref(); // + one ref for the IPC
                 }
             } else {
+                subprocess.ref(); // + one ref for the IPC
+
                 if (ipc_data.configureServer(
                     Subprocess,
                     subprocess,
                     subprocess.stdio_pipes.items[@intCast(ipc_channel)].buffer,
                 ).asErr()) |err| {
-                    process_allocator.destroy(subprocess);
+                    subprocess.deref();
                     globalThis.throwValue(err.toJSC(globalThis));
                     return .zero;
                 }
+                subprocess.stdio_pipes.items[@intCast(ipc_channel)] = .unavailable;
             }
             ipc_data.writeVersionPacket();
         }
@@ -2319,8 +2333,12 @@ pub const Subprocess = struct {
     pub fn handleIPCClose(this: *Subprocess) void {
         IPClog("Subprocess#handleIPCClose", .{});
         this.updateHasPendingActivity();
-        const ok = this.ipc_data != null;
-        if (ok) this.ipc().internal_msg_queue.deinit();
+        defer this.deref();
+        var ok = false;
+        if (this.ipc()) |ipc_data| {
+            ok = true;
+            ipc_data.internal_msg_queue.deinit();
+        }
         this.ipc_data = null;
 
         const this_jsvalue = this.this_jsvalue;
@@ -2330,11 +2348,7 @@ pub const Subprocess = struct {
         }
     }
 
-    pub fn ipc(this: *Subprocess) *IPC.IPCData {
-        return &this.ipc_data.?;
-    }
-
-    pub fn ipc_maybe(this: *Subprocess) ?*IPC.IPCData {
+    pub fn ipc(this: *Subprocess) ?*IPC.IPCData {
         return &(this.ipc_data orelse return null);
     }
 

src/bun.js/api/server.zig
@@ -5776,7 +5776,7 @@ pub fn NewServer(comptime NamespaceType: type, comptime ssl_enabled_: bool, comp
                     break :brk false;
                 };
 
-                JSC.C.JSValueUnprotect(this.globalThis, this.thisObject.asObjectRef());
+                this.thisObject.unprotect();
                 this.thisObject = .undefined;
                 this.stop(abrupt);
             }
@@ -5786,7 +5786,7 @@ pub fn NewServer(comptime NamespaceType: type, comptime ssl_enabled_: bool, comp
 
         pub fn disposeFromJS(this: *ThisServer) JSC.JSValue {
             if (this.listener != null) {
-                JSC.C.JSValueUnprotect(this.globalThis, this.thisObject.asObjectRef());
+                this.thisObject.unprotect();
                 this.thisObject = .undefined;
                 this.stop(true);
             }

src/bun.js/ipc.zig
@@ -333,7 +333,8 @@ const SocketIPCData = struct {
     outgoing: bun.io.StreamBuffer = .{},
     has_written_version: if (Environment.allow_assert) u1 else u0 = 0,
     internal_msg_queue: node_cluster_binding.InternalMsgHolder = .{},
-
+    disconnected: bool = false,
+    is_server: bool = false,
     pub fn writeVersionPacket(this: *SocketIPCData) void {
         if (Environment.allow_assert) {
             bun.assert(this.has_written_version == 0);
@@ -400,8 +401,22 @@ const SocketIPCData = struct {
         return true;
     }
 
-    pub fn close(this: *SocketIPCData) void {
-        this.socket.close(.normal);
+    pub fn close(this: *SocketIPCData, nextTick: bool) void {
+        log("SocketIPCData#close", .{});
+        if (this.disconnected) return;
+        this.disconnected = true;
+        if (nextTick) {
+            JSC.VirtualMachine.get().enqueueTask(JSC.ManagedTask.New(SocketIPCData, closeTask).init(this));
+        } else {
+            this.closeTask();
+        }
+    }
+
+    pub fn closeTask(this: *SocketIPCData) void {
+        log("SocketIPCData#closeTask", .{});
+        if (this.disconnected) {
+            this.socket.close(.normal);
+        }
     }
 };
 
@@ -412,13 +427,12 @@ const NamedPipeIPCData = struct {
     mode: Mode,
 
     // we will use writer pipe as Duplex
-    writer: bun.io.StreamingWriter(NamedPipeIPCData, onWrite, onError, null, onClientClose) = .{},
+    writer: bun.io.StreamingWriter(NamedPipeIPCData, onWrite, onError, null, onPipeClose) = .{},
 
     incoming: bun.ByteList = .{}, // Maybe we should use IPCBuffer here as well
-    connected: bool = false,
     disconnected: bool = false,
+    is_server: bool = false,
     connect_req: uv.uv_connect_t = std.mem.zeroes(uv.uv_connect_t),
-    server: ?*uv.Pipe = null,
     onClose: ?CloseHandler = null,
     has_written_version: if (Environment.allow_assert) u1 else u0 = 0,
     internal_msg_queue: node_cluster_binding.InternalMsgHolder = .{},
@@ -428,39 +442,52 @@ const NamedPipeIPCData = struct {
         context: *anyopaque,
     };
 
-    fn onWrite(_: *NamedPipeIPCData, amount: usize, status: bun.io.WriteStatus) void {
-        log("onWrite {d} {}", .{ amount, status });
+    fn onServerPipeClose(this: *uv.Pipe) callconv(.C) void {
+        // safely free the pipes
+        bun.default_allocator.destroy(this);
     }
 
-    fn onError(_: *NamedPipeIPCData, err: bun.sys.Error) void {
-        log("Failed to write outgoing data {}", .{err});
-    }
+    fn detach(this: *NamedPipeIPCData) void {
+        log("NamedPipeIPCData#detach: is_server {}", .{this.is_server});
+        const source = this.writer.source.?;
+        // unref because we are closing the pipe
+        source.pipe.unref();
+        this.writer.source = null;
 
-    fn onClientClose(this: *NamedPipeIPCData) void {
-        log("onClientClose", .{});
-        this.connected = false;
-        if (this.server) |server| {
-            // we must close the server too
-            server.close(onServerClose);
-        } else {
-            if (this.onClose) |handler| {
-                this.onClose = null;
-                handler.callback(handler.context);
-                // deinit dont free the instance of IPCData we should call it before the onClose callback actually frees it
-                this.deinit();
-            }
-        }
-    }
-
-    fn onServerClose(pipe: *uv.Pipe) callconv(.C) void {
-        log("onServerClose", .{});
-        const this = bun.cast(*NamedPipeIPCData, pipe.data);
-        this.server = null;
-        if (this.connected) {
-            // close and deinit client if connected
-            this.writer.close();
+        if(this.is_server) {
+            source.pipe.data = source.pipe;
+            source.pipe.close(onServerPipeClose);
+            this.onPipeClose();
             return;
         }
+        // server will be destroyed by the process that created it
+        defer bun.default_allocator.destroy(source.pipe);
+        this.writer.source = null;
+        this.onPipeClose();
+    }
+
+    fn onWrite(this: *NamedPipeIPCData, amount: usize, status: bun.io.WriteStatus) void {
+        log("onWrite {d} {}", .{ amount, status });
+
+        switch (status) {
+            .pending => {},
+            .drained => {
+                // unref after sending all data
+                this.writer.source.?.pipe.unref();
+            },
+            .end_of_file => {
+                this.detach();
+            },
+        }
+    }
+
+    fn onError(this: *NamedPipeIPCData, err: bun.sys.Error) void {
+        log("Failed to write outgoing data {}", .{err});
+        this.detach();
+    }
+
+    fn onPipeClose(this: *NamedPipeIPCData) void {
+        log("onPipeClose", .{});
         if (this.onClose) |handler| {
             this.onClose = null;
             handler.callback(handler.context);
@@ -475,11 +502,11 @@ const NamedPipeIPCData = struct {
         }
         const bytes = getVersionPacket(this.mode);
         if (bytes.len > 0) {
-            if (this.connected) {
-                _ = this.writer.write(bytes);
-            } else {
+            if (this.disconnected) {
                 // enqueue to be sent after connecting
                 this.writer.outgoing.write(bytes) catch bun.outOfMemory();
+            } else {
+                _ = this.writer.write(bytes);
             }
         }
         if (Environment.allow_assert) {
@@ -494,7 +521,8 @@ const NamedPipeIPCData = struct {
         if (this.disconnected) {
             return false;
         }
-
+        // ref because we have pending data
+        this.writer.source.?.pipe.ref();
         const start_offset = this.writer.outgoing.list.items.len;
 
         const payload_length: usize = serialize(this, &this.writer.outgoing, global, value) catch return false;
@@ -503,9 +531,7 @@ const NamedPipeIPCData = struct {
 
         if (start_offset == 0) {
             bun.assert(this.writer.outgoing.cursor == 0);
-            if (this.connected) {
-                _ = this.writer.flush();
-            }
+            _ = this.writer.flush();
         }
 
         return true;
@@ -518,7 +544,8 @@ const NamedPipeIPCData = struct {
         if (this.disconnected) {
             return false;
         }
-
+        // ref because we have pending data
+        this.writer.source.?.pipe.ref();
         const start_offset = this.writer.outgoing.list.items.len;
 
         const payload_length: usize = serializeInternal(this, &this.writer.outgoing, global, value) catch return false;
@@ -527,57 +554,62 @@ const NamedPipeIPCData = struct {
 
         if (start_offset == 0) {
             bun.assert(this.writer.outgoing.cursor == 0);
-            if (this.connected) {
-                _ = this.writer.flush();
-            }
+            _ = this.writer.flush();
         }
 
         return true;
     }
 
-    pub fn close(this: *NamedPipeIPCData) void {
+    pub fn close(this: *NamedPipeIPCData, nextTick: bool) void {
         log("NamedPipeIPCData#close", .{});
-        if (this.server) |server| {
-            server.close(onServerClose);
-        } else {
-            this.disconnected = true;
+        if (this.disconnected) return;
+        this.disconnected = true;
+        if (nextTick) {
             JSC.VirtualMachine.get().enqueueTask(JSC.ManagedTask.New(NamedPipeIPCData, closeTask).init(this));
+        } else {
+            this.closeTask();
         }
     }
 
     pub fn closeTask(this: *NamedPipeIPCData) void {
-        log("NamedPipeIPCData#closeTask", .{});
+        log("NamedPipeIPCData#closeTask is_server {}", .{this.is_server});
         if (this.disconnected) {
-            this.writer.close();
-            this.onClientClose();
+            _ = this.writer.flush();
+            this.writer.end();
+            if(this.writer.getStream()) |stream| {
+                stream.readStop();
+            }
+            if (!this.writer.hasPendingData()) {
+                this.detach();
+            }
         }
     }
 
     pub fn configureServer(this: *NamedPipeIPCData, comptime Context: type, instance: *Context, ipc_pipe: *uv.Pipe) JSC.Maybe(void) {
         log("configureServer", .{});
-        this.server = ipc_pipe;
         ipc_pipe.data = @ptrCast(instance);
         this.onClose = .{
             .callback = @ptrCast(&NewNamedPipeIPCHandler(Context).onClose),
             .context = @ptrCast(instance),
         };
-        this.connected = true;
         ipc_pipe.unref();
-
+        this.is_server = true;
+        this.writer.setParent(this);
+        this.writer.owns_fd = false;
         const startPipeResult = this.writer.startWithPipe(ipc_pipe);
         if (startPipeResult == .err) {
-            this.close();
+            this.close(false);
             return startPipeResult;
         }
 
         const stream = this.writer.getStream() orelse {
-            this.close();
+            this.close(false);
             return JSC.Maybe(void).errno(bun.C.E.PIPE, .pipe);
         };
 
         const readStartResult = stream.readStart(instance, NewNamedPipeIPCHandler(Context).onReadAlloc, NewNamedPipeIPCHandler(Context).onReadError, NewNamedPipeIPCHandler(Context).onRead);
         if (readStartResult == .err) {
-            this.close();
+            this.close(false);
             return readStartResult;
         }
         return .{ .result = {} };
@@ -595,10 +627,10 @@ const NamedPipeIPCData = struct {
             return err;
         };
         ipc_pipe.unref();
-        this.connected = true;
-
+        this.writer.owns_fd = false;
+        this.writer.setParent(this);
         this.writer.startWithPipe(ipc_pipe).unwrap() catch |err| {
-            this.close();
+            this.close(false);
             return err;
         };
         this.connect_req.data = @ptrCast(instance);
@@ -608,12 +640,12 @@ const NamedPipeIPCData = struct {
         };
 
         const stream = this.writer.getStream() orelse {
-            this.close();
+            this.close(false);
             return error.FailedToConnectIPC;
         };
 
         stream.readStart(instance, NewNamedPipeIPCHandler(Context).onReadAlloc, NewNamedPipeIPCHandler(Context).onReadError, NewNamedPipeIPCHandler(Context).onRead).unwrap() catch |err| {
-            this.close();
+            this.close(false);
             return err;
         };
     }
@@ -621,10 +653,6 @@ const NamedPipeIPCData = struct {
     fn deinit(this: *NamedPipeIPCData) void {
         log("deinit", .{});
         this.writer.deinit();
-        if (this.server) |server| {
-            this.server = null;
-            bun.default_allocator.destroy(server);
-        }
         this.incoming.deinitWithAllocator(bun.default_allocator);
     }
 };
@@ -664,7 +692,7 @@ fn NewSocketIPCHandler(comptime Context: type) type {
             all_data: []const u8,
         ) void {
             var data = all_data;
-            const ipc = this.ipc();
+            const ipc = this.ipc() orelse return;
             log("onData {}", .{std.fmt.fmtSliceHexLower(data)});
 
             // In the VirtualMachine case, `globalThis` is an optional, in case
@@ -746,18 +774,17 @@ fn NewSocketIPCHandler(comptime Context: type) type {
             context: *Context,
             socket: Socket,
         ) void {
-            const to_write = context.ipc().outgoing.slice();
+            const ipc = context.ipc() orelse return;
+            const to_write = ipc.outgoing.slice();
             if (to_write.len == 0) {
-                context.ipc().outgoing.reset();
-                context.ipc().outgoing.reset();
+                ipc.outgoing.reset();
                 return;
             }
             const n = socket.write(to_write, false);
             if (n == to_write.len) {
-                context.ipc().outgoing.reset();
-                context.ipc().outgoing.reset();
+                ipc.outgoing.reset();
             } else if (n > 0) {
-                context.ipc().outgoing.cursor += @intCast(n);
+                ipc.outgoing.cursor += @intCast(n);
             }
         }
 
@@ -790,7 +817,7 @@ fn NewSocketIPCHandler(comptime Context: type) type {
 fn NewNamedPipeIPCHandler(comptime Context: type) type {
     return struct {
         fn onReadAlloc(this: *Context, suggested_size: usize) []u8 {
-            const ipc = this.ipc();
+            const ipc = this.ipc() orelse return "";
             var available = ipc.incoming.available();
             if (available.len < suggested_size) {
                 ipc.incoming.ensureUnusedCapacity(bun.default_allocator, suggested_size) catch bun.outOfMemory();
@@ -802,12 +829,13 @@ fn NewNamedPipeIPCHandler(comptime Context: type) type {
 
         fn onReadError(this: *Context, err: bun.C.E) void {
             log("NewNamedPipeIPCHandler#onReadError {}", .{err});
-            this.ipc().close();
-            onClose(this);
+            if (this.ipc()) |ipc_data| {
+                ipc_data.close(true);
+            }
         }
 
         fn onRead(this: *Context, buffer: []const u8) void {
-            const ipc = this.ipc();
+            const ipc = this.ipc() orelse return;
 
             log("NewNamedPipeIPCHandler#onRead {d}", .{buffer.len});
             ipc.incoming.len += @as(u32, @truncate(buffer.len));
@@ -822,7 +850,7 @@ fn NewNamedPipeIPCHandler(comptime Context: type) type {
                     if (this.globalThis) |global| {
                         break :brk global;
                     }
-                    ipc.close();
+                    ipc.close(true);
                     return;
                 },
                 else => @panic("Unexpected globalThis type: " ++ @typeName(@TypeOf(this.globalThis))),
@@ -838,7 +866,7 @@ fn NewNamedPipeIPCHandler(comptime Context: type) type {
                     },
                     error.InvalidFormat => {
                         Output.printErrorln("InvalidFormatError during IPC message handling", .{});
-                        ipc.close();
+                        ipc.close(false);
                         return;
                     },
                 };
@@ -868,7 +896,7 @@ fn NewNamedPipeIPCHandler(comptime Context: type) type {
 /// struct {
 ///     globalThis: ?*JSGlobalObject,
 ///
-///     fn ipc(*Context) *IPCData,
+///     fn ipc(*Context) ?*IPCData,
 ///     fn handleIPCMessage(*Context, DecodedIPCMessage) void
 ///     fn handleIPCClose(*Context) void
 /// }

commit a8fa716d409410bd3a1743a1b2b79945e7068c35

Fix crash introduced in https://github.com/oven-sh/bun/pull/13414

src/bun.js/api/bun/subprocess.zig
@@ -211,6 +211,7 @@ pub const Subprocess = struct {
         killed: bool = false,
         has_stdin_destructor_called: bool = false,
         finalized: bool = false,
+        deref_on_stdin_destroyed: bool = false,
     };
 
     pub const SignalCode = bun.SignalCode;
@@ -703,9 +704,13 @@ pub const Subprocess = struct {
     }
 
     pub fn onStdinDestroyed(this: *Subprocess) void {
+        const must_deref = this.flags.deref_on_stdin_destroyed;
+        this.flags.deref_on_stdin_destroyed = false;
+        defer if (must_deref) this.deref();
+
         this.flags.has_stdin_destructor_called = true;
         this.weak_file_sink_stdin_ptr = null;
-        defer this.deref();
+
         if (!this.flags.finalized) {
             // otherwise update the pending activity flag
             this.updateHasPendingActivity();
@@ -1240,6 +1245,7 @@ pub const Subprocess = struct {
                             pipe.writer.setParent(pipe);
                             subprocess.weak_file_sink_stdin_ptr = pipe;
                             subprocess.ref();
+                            subprocess.flags.deref_on_stdin_destroyed = true;
                             subprocess.flags.has_stdin_destructor_called = false;
 
                             return Writable{
@@ -1300,6 +1306,7 @@ pub const Subprocess = struct {
                     subprocess.weak_file_sink_stdin_ptr = pipe;
                     subprocess.ref();
                     subprocess.flags.has_stdin_destructor_called = false;
+                    subprocess.flags.deref_on_stdin_destroyed = true;
 
                     pipe.writer.handle.poll.flags.insert(.socket);
 
@@ -1345,12 +1352,16 @@ pub const Subprocess = struct {
                 .pipe => |pipe| {
                     this.* = .{ .ignore = {} };
                     if (subprocess.process.hasExited() and !subprocess.flags.has_stdin_destructor_called) {
+                        // onAttachedProcessExit() can call deref on the subprocess, but we don't want that.
+                        // since we never called ref() on it here.
+                        bun.debugAssert(!subprocess.flags.deref_on_stdin_destroyed);
                         pipe.onAttachedProcessExit();
                         return pipe.toJS(globalThis);
                     } else {
                         subprocess.flags.has_stdin_destructor_called = false;
                         subprocess.weak_file_sink_stdin_ptr = pipe;
                         subprocess.ref();
+                        subprocess.flags.deref_on_stdin_destroyed = true;
                         if (@intFromPtr(pipe.signal.ptr) == @intFromPtr(subprocess)) {
                             pipe.signal.clear();
                         }
@@ -2157,6 +2168,9 @@ pub const Subprocess = struct {
                 default_max_buffer_size,
                 is_sync,
             ),
+            // 1. JavaScript.
+            // 2. Process.
+            .ref_count = if (is_sync) 1 else 2,
             .stdio_pipes = spawned.extra_pipes.moveToUnmanaged(),
             .on_exit_callback = if (on_exit_callback != .zero) JSC.Strong.create(on_exit_callback, globalThis) else .{},
             .on_disconnect_callback = if (on_disconnect_callback != .zero) JSC.Strong.create(on_disconnect_callback, globalThis) else .{},
@@ -2177,7 +2191,7 @@ pub const Subprocess = struct {
                 .is_sync = is_sync,
             },
         };
-        subprocess.ref(); // + one ref for the process
+
         subprocess.process.setExitHandler(subprocess);
 
         if (subprocess.ipc_data) |*ipc_data| {

test/js/bun/spawn/bad-fixture.js
@@ -1,0 +1,3 @@
+process.stdin.read();
+
+throw new Error("test");

test/js/bun/spawn/spawn-stdin-destroy.test.ts
@@ -1,0 +1,42 @@
+import { bunEnv, bunExe } from "harness";
+import path from "path";
+
+test("stdin destroy after exit crash", async () => {
+  let before;
+  await (async () => {
+    const child = Bun.spawn({
+      cmd: [bunExe(), path.join(import.meta.dir, "bad-fixture.js")],
+      env: bunEnv,
+      stdout: "pipe",
+      stdin: "pipe",
+    });
+
+    await Bun.sleep(80);
+    await child.stdin.write("dylan\n");
+    await child.stdin.write("999\n");
+    await child.stdin.flush();
+    await child.stdin.end();
+
+    async function read() {
+      var out = "";
+      for await (const chunk of child.stdout) {
+        out += new TextDecoder().decode(chunk);
+      }
+      return out;
+    }
+
+    // This bug manifested as child.exited rejecting with an error code of "TODO"
+    const [out, exited] = await Promise.all([read(), child.exited]);
+
+    expect(out).toBe("");
+    expect(exited).toBe(1);
+    before = require("bun:jsc").heapStats().objectTypeCounts;
+    Bun.gc(true);
+    await Bun.sleep(50);
+  })();
+  Bun.gc(true);
+
+  const { FileSink = 0, Subprocess = 0 } = require("bun:jsc").heapStats().objectTypeCounts;
+  expect(FileSink).toBeLessThan(before.FileSink || 0);
+  expect(Subprocess).toBeLessThan(before.Subprocess || 0);
+});
53a9ad5e071928f5bf8b13099485debe633aa593
Zack Radisic <56137411+zackradisic@users.noreply.github.com>
2024-09-23 16:22:29 -0700

    stuff

---
+++ src/css/context.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const bun = @import("root").bun;
+const logger = bun.logger;
+const Log = logger.Log;
+
+pub const css = @import("./css_parser.zig");
+
+const ArrayList = std.ArrayListUnmanaged;
+
+pub const SupportsEntry = struct {
+    condition: css.SupportsCondition,
+    declarations: ArrayList(css.Property),
+    important_declarations: ArrayList(css.Property),
+};
+
+pub const DeclarationContext = enum {
+    none,
+    style_rule,
+    keyframes,
+    style_attribute,
+};
+
+pub const PropertyHandlerContext = struct {
+    allocator: Allocator,
+    targets: css.targets.Targets,
+    is_important: bool,
+    supports: ArrayList(SupportsEntry),
+    ltr: ArrayList(css.Property),
+    rtl: ArrayList(css.Property),
+    dark: ArrayList(css.Property),
+    context: DeclarationContext,
+    unused_symbols: *const std.StringArrayHashMapUnmanaged(void),
+
+    pub fn new(
+        allocator: Allocator,
+        targets: css.targets.Targets,
+        unused_symbols: *const std.StringArrayHashMapUnmanaged(void),
+    ) PropertyHandlerContext {
+        return PropertyHandlerContext{
+            .allocator = allocator,
+            .targets = targets,
+            .is_important = false,
+            .supports = ArrayList(SupportsEntry){},
+            .ltr = ArrayList(css.Property){},
+            .rtl = ArrayList(css.Property){},
+            .dark = ArrayList(css.Property){},
+            .context = DeclarationContext.none,
+            .unused_symbols = unused_symbols,
+        };
+    }
+};

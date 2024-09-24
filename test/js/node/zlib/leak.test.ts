8d8c2425b78a1ecbf2b6937644a0e375b08c6d57
Meghan Denny <meghan@bun.sh>
2024-09-24 01:23:28 -0700

    dont use .repeat to create input in leak test

--- test/js/node/zlib/leak.test.ts
+++ test/js/node/zlib/leak.test.ts
@@ -2,29 +2,21 @@ import { describe, test, expect } from "bun:test";
 import { promisify } from "node:util";
 import zlib from "node:zlib";
 
-describe("zlib compression does not leak memory", () => {
-  const inputString =
-    "ΩΩLorem ipsum dolor sit amet, consectetur adipiscing eli" +
-    "t. Morbi faucibus, purus at gravida dictum, libero arcu " +
-    "convallis lacus, in commodo libero metus eu nisi. Nullam" +
-    " commodo, neque nec porta placerat, nisi est fermentum a" +
-    "ugue, vitae gravida tellus sapien sit amet tellus. Aenea" +
-    "n non diam orci. Proin quis elit turpis. Suspendisse non" +
-    " diam ipsum. Suspendisse nec ullamcorper odio. Vestibulu" +
-    "m arcu mi, sodales non suscipit id, ultrices ut massa. S" +
-    "ed ac sem sit amet arcu malesuada fermentum. Nunc sed. ".repeat(50);
+const input = Buffer.alloc(50000);
+for (let i = 0; i < input.length; i++) input[i] = Math.random();
 
+describe("zlib compression does not leak memory", () => {
   for (const compress of ["deflate", "gzip"] as const) {
     test(
       compress,
       async () => {
         for (let index = 0; index < 1_000; index++) {
-          await promisify(zlib[compress])(inputString);
+          await promisify(zlib[compress])(input);
         }
         const baseline = process.memoryUsage.rss();
         console.log(baseline);
         for (let index = 0; index < 1_000; index++) {
-          await promisify(zlib[compress])(inputString);
+          await promisify(zlib[compress])(input);
         }
         Bun.gc(true);
         const after = process.memoryUsage.rss();
@@ -40,12 +32,12 @@ describe("zlib compression does not leak memory", () => {
       compress,
       async () => {
         for (let index = 0; index < 1_000; index++) {
-          zlib[compress](inputString);
+          zlib[compress](input);
         }
         const baseline = process.memoryUsage.rss();
         console.log(baseline);
         for (let index = 0; index < 1_000; index++) {
-          zlib[compress](inputString);
+          zlib[compress](input);
         }
         Bun.gc(true);
         const after = process.memoryUsage.rss();

b93f05c6ffa3c660ffa96e8903de4468b32b0e0a
Meghan Denny <meghan@bun.sh>
2024-09-24 01:24:33 -0700

    add more logging to leak test to observe values even when pass

--- test/js/node/zlib/leak.test.ts
+++ test/js/node/zlib/leak.test.ts
@@ -29,6 +29,8 @@ describe("zlib compression does not leak memory", () => {
         Bun.gc(true);
         const after = process.memoryUsage.rss();
         console.log(after);
+        console.log("-", after - baseline);
+        console.log("-", 1024 * 1024 * 20);
         expect(after - baseline).toBeLessThan(1024 * 1024 * 20);
       },
       0,
@@ -50,6 +52,8 @@ describe("zlib compression does not leak memory", () => {
         Bun.gc(true);
         const after = process.memoryUsage.rss();
         console.log(after);
+        console.log("-", after - baseline);
+        console.log("-", 1024 * 1024 * 20);
         expect(after - baseline).toBeLessThan(1024 * 1024 * 20);
       },
       0,

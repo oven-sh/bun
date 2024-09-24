57681631bc885ab5ceded2cf5f024561c1421317
Meghan Denny <meghan@bun.sh>
2024-09-24 01:24:09 -0700

    prime leak test better and run for more iterations

--- test/js/node/zlib/leak.test.ts
+++ test/js/node/zlib/leak.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from "bun:test";
+import { describe, test, expect, beforeAll } from "bun:test";
 import { promisify } from "node:util";
 import zlib from "node:zlib";
 
@@ -6,16 +6,24 @@ const input = Buffer.alloc(50000);
 for (let i = 0; i < input.length; i++) input[i] = Math.random();
 
 describe("zlib compression does not leak memory", () => {
+  beforeAll(() => {
+    for (let index = 0; index < 10_000; index++) {
+      zlib.deflateSync(input);
+    }
+    Bun.gc(true);
+    console.log("beforeAll done");
+  });
+
   for (const compress of ["deflate", "gzip"] as const) {
     test(
       compress,
       async () => {
-        for (let index = 0; index < 1_000; index++) {
+        for (let index = 0; index < 10_000; index++) {
           await promisify(zlib[compress])(input);
         }
         const baseline = process.memoryUsage.rss();
         console.log(baseline);
-        for (let index = 0; index < 1_000; index++) {
+        for (let index = 0; index < 10_000; index++) {
           await promisify(zlib[compress])(input);
         }
         Bun.gc(true);
@@ -31,12 +39,12 @@ describe("zlib compression does not leak memory", () => {
     test(
       compress,
       async () => {
-        for (let index = 0; index < 1_000; index++) {
+        for (let index = 0; index < 10_000; index++) {
           zlib[compress](input);
         }
         const baseline = process.memoryUsage.rss();
         console.log(baseline);
-        for (let index = 0; index < 1_000; index++) {
+        for (let index = 0; index < 10_000; index++) {
           zlib[compress](input);
         }
         Bun.gc(true);

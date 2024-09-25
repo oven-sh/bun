9bd076146004960a96fa1c4b9d4517c9f1f22b4e
Ashcon Partovi <ashcon@partovi.net>
2024-09-24 19:02:28 -0700

    Revert "Run `bun run clang-format` using LLVM 18.1.8"
    This reverts commit 7b5d0e029c43de5b2a5bfdd58b4157ef5a49200e.

--- src/bun.js/modules/BunObjectModule.cpp
+++ src/bun.js/modules/BunObjectModule.cpp
@@ -11,8 +11,7 @@ void generateNativeModule_BunObject(JSC::JSGlobalObject *lexicalGlobalObject,
                                     JSC::MarkedArgumentBuffer &exportValues) {
   // FIXME: this does not add each property as a top level export
   JSC::VM &vm = lexicalGlobalObject->vm();
-  Zig::GlobalObject *globalObject =
-      jsCast<Zig::GlobalObject *>(lexicalGlobalObject);
+  Zig::GlobalObject *globalObject = jsCast<Zig::GlobalObject *>(lexicalGlobalObject);
 
   JSObject *object = globalObject->bunObject();
 

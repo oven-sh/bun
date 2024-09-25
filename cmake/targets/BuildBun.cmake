b9a56a60876ec22c812d082bc3a86abe381f1f56
Ashcon Partovi <ashcon@partovi.net>
2024-09-25 09:39:06 -0700

    cmake: Add target to download Node.js headers

--- cmake/targets/BuildBun.cmake
+++ cmake/targets/BuildBun.cmake
@@ -592,6 +592,22 @@ register_repository(
     picohttpparser.c
 )
 
+set(NODEJS_HEADERS_PATH ${VENDOR_PATH}/nodejs)
+
+register_command(
+  TARGET
+    bun-node-headers
+  COMMENT
+    "Download node ${NODEJS_VERSION} headers"
+  COMMAND
+    ${CMAKE_COMMAND}
+      -DDOWNLOAD_PATH=${NODEJS_HEADERS_PATH}
+      -DDOWNLOAD_URL=https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-headers.tar.gz
+      -P ${CWD}/cmake/scripts/DownloadUrl.cmake
+  OUTPUTS
+    ${NODEJS_HEADERS_PATH}/include/node/node_version.h
+)
+
 list(APPEND BUN_CPP_SOURCES
   ${BUN_C_SOURCES}
   ${BUN_CXX_SOURCES}

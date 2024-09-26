fa1e6710bdb8705d41c6dd7675655cb78b2c7f50
Ben Grant <ben@bun.sh>
2024-09-26 10:38:37 -0700

    Try to please Microsoft

--- src/bun.js/bindings/v8/real_v8.h
+++ src/bun.js/bindings/v8/real_v8.h
@@ -5,6 +5,27 @@
 // files from Bun's V8 implementation, and it should only be included in source files (not
 // header files).
 
+#include <array>
+#include <atomic>
+#include <bit>
+#include <cassert>
+#include <climits>
+#include <cstddef>
+#include <cstdint>
+#include <fstream>
+#include <functional>
+#include <iosfwd>
+#include <iterator>
+#include <limits>
+#include <memory>
+#include <new>
+#include <optional>
+#include <ostream>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
 #define v8 real_v8
 #define private public
 #include "node/v8.h"

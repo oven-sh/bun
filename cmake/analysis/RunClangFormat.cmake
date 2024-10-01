e2f20d794f30378344a00d7ac13545eb765d73bf
Meghan Denny <meghan@bun.sh>
2024-10-01 03:49:10 -0700

    clang-format: add InsertNewlineAtEOF (#14267)
    Co-authored-by: nektro <nektro@users.noreply.github.com>

--- cmake/analysis/RunClangFormat.cmake
+++ cmake/analysis/RunClangFormat.cmake
@@ -13,6 +13,17 @@ set(CLANG_FORMAT_SOURCES ${BUN_C_SOURCES} ${BUN_CXX_SOURCES})
 
 register_command(
   TARGET
+    clang-format-version
+  COMMENT
+    "Print clang-format version"
+  COMMAND
+    ${CLANG_FORMAT_PROGRAM}
+      --version
+  ALWAYS_RUN
+)
+
+register_command(
+  TARGET
     clang-format-check
   COMMENT
     "Running clang-format"

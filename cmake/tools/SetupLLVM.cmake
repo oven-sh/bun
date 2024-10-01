c956bd55774c8f0d06638e24324512938ce0253f
Ashcon Partovi <ashcon@partovi.net>
2024-10-01 14:42:08 -0700

    Fix path?

--- cmake/tools/SetupLLVM.cmake
+++ cmake/tools/SetupLLVM.cmake
@@ -1,9 +1,3 @@
-optionx(SKIP_LLVM BOOL "If LLVM setup should be skipped" DEFAULT OFF)
-
-if(SKIP_LLVM)
-  return()
-endif()
-
 if(CMAKE_HOST_WIN32 OR CMAKE_HOST_APPLE)
   set(DEFAULT_LLVM_VERSION "18.1.8")
 else()
@@ -12,45 +6,70 @@ endif()
 
 optionx(LLVM_VERSION STRING "The version of LLVM to use" DEFAULT ${DEFAULT_LLVM_VERSION})
 
+string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)" match ${LLVM_VERSION})
+if(match)
+  set(LLVM_VERSION_MAJOR ${CMAKE_MATCH_1})
+  set(LLVM_VERSION_MINOR ${CMAKE_MATCH_2})
+  set(LLVM_VERSION_PATCH ${CMAKE_MATCH_3})
+else()
+  set(LLVM_VERSION_MAJOR 0)
+  set(LLVM_VERSION_MINOR 0)
+  set(LLVM_VERSION_PATCH 0)
+endif()
+
+set(LLVM_PATHS)
+
 if(APPLE)
   execute_process(
-    COMMAND brew --prefix llvm@${LLVM_VERSION_MAJOR}
-    OUTPUT_VARIABLE DEFAULT_LLVM_PREFIX
+    COMMAND brew --prefix
+    OUTPUT_VARIABLE HOMEBREW_PREFIX
     OUTPUT_STRIP_TRAILING_WHITESPACE
     ERROR_QUIET
   )
-  if(NOT DEFAULT_LLVM_PREFIX)
-    set(DEFAULT_LLVM_PREFIX /opt/homebrew/opt/llvm)
+
+  if(NOT HOMEBREW_PREFIX)
+    if(CMAKE_SYSTEM_PROCESSOR MATCHES "arm64|ARM64|aarch64|AARCH64")
+      set(HOMEBREW_PREFIX /opt/homebrew)
+    else()
+      set(HOMEBREW_PREFIX /usr/local)
+    endif()
   endif()
-elseif(NOT WIN32)
-  set(DEFAULT_LLVM_PREFIX /usr/lib/llvm-${LLVM_VERSION_MAJOR})
-else()
-  set(DEFAULT_LLVM_PREFIX /usr/lib)
+
+  list(APPEND LLVM_PATHS
+    ${HOMEBREW_PREFIX}/opt/llvm@${LLVM_VERSION_MAJOR}/bin
+    ${HOMEBREW_PREFIX}/opt/llvm/bin
+  )
 endif()
 
-optionx(LLVM_PREFIX FILEPATH "The path to the LLVM installation" DEFAULT ${DEFAULT_LLVM_PREFIX})
-set(LLVM_PATH ${LLVM_PREFIX}/bin)
+if(UNIX)
+  list(APPEND LLVM_PATHS
+    /usr/lib/llvm-${LLVM_VERSION_MAJOR}.${LLVM_VERSION_MINOR}.${LLVM_VERSION_PATCH}/bin
+    /usr/lib/llvm-${LLVM_VERSION_MAJOR}.${LLVM_VERSION_MINOR}/bin
+    /usr/lib/llvm-${LLVM_VERSION_MAJOR}/bin
+    /usr/lib/llvm/bin
+  )
+endif()
 
-macro(find_llvm_command VARIABLE COMMAND)
+macro(find_llvm_command variable command)
   find_command(
-    VARIABLE ${VARIABLE}
+    VARIABLE ${variable}
     VERSION_VARIABLE LLVM_VERSION
-    COMMAND ${COMMAND} ${COMMAND}-${LLVM_VERSION_MAJOR}
-    PATHS ${LLVM_PATH}
+    COMMAND ${command} ${command}-${LLVM_VERSION_MAJOR}
+    PATHS ${LLVM_PATHS}
     VERSION ${LLVM_VERSION}
   )
-  list(APPEND CMAKE_ARGS -D${VARIABLE}=${${VARIABLE}})
+  list(APPEND CMAKE_ARGS -D${variable}=${${variable}})
 endmacro()
 
-macro(find_llvm_command_no_version VARIABLE COMMAND)
+macro(find_llvm_command_no_version variable command)
   find_command(
-    VARIABLE ${VARIABLE}
+    VARIABLE ${variable}
     VERSION_VARIABLE LLVM_VERSION
-    COMMAND ${COMMAND} ${COMMAND}-${LLVM_VERSION_MAJOR}
-    PATHS ${LLVM_PATH}
+    COMMAND ${command} ${command}-${LLVM_VERSION_MAJOR}
+    PATHS ${LLVM_PATHS}
     REQUIRED ON
   )
-  list(APPEND CMAKE_ARGS -D${VARIABLE}=${${VARIABLE}})
+  list(APPEND CMAKE_ARGS -D${variable}=${${variable}})
 endmacro()
 
 if(WIN32)

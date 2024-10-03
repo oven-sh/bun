11274ce7d227d412d7a505f2ff665c507215832b
Ciro Spaciari <ciro.spaciari@gmail.com>
2024-10-03 11:58:02 -0700

    more

--- src/js/node/net.ts
+++ src/js/node/net.ts
@@ -1034,7 +1034,7 @@ class Server extends EventEmitter {
       const options = this[bunSocketServerOptions];
       let contexts: Map<string, any> | null = null;
       if (typeof bunTLS === "function") {
-        [tls, TLSSocketClass] = bunTLS.$call(this, port, isIP(hostname) ? "" : hostname, false);
+        [tls, TLSSocketClass] = bunTLS.$call(this, port, hostname, false);
         options.servername = tls.serverName;
         options.InternalSocketClass = TLSSocketClass;
         contexts = tls.contexts;

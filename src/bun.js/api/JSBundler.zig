535ad4cb9161c5995a29777efb1c0f55a0de6ba9
Zack Radisic <zackradisic@Mac.attlocal.net>
2024-10-03 22:34:39 -0700

    Add option to JSBundler

--- src/bun.js/api/JSBundler.zig
+++ src/bun.js/api/JSBundler.zig
@@ -95,6 +95,10 @@ pub const JSBundler = struct {
             errdefer this.deinit(allocator);
             errdefer if (plugins.*) |plugin| plugin.deinit();
 
+            if (config.getTruthy(globalThis, "experimentalCss")) |enable_css| {
+                this.experimental_css = if (enable_css.isBoolean()) enable_css.toBoolean() else false;
+            }
+
             // Plugins must be resolved first as they are allowed to mutate the config JSValue
             if (try config.getArray(globalThis, "plugins")) |array| {
                 var iter = array.arrayIterator(globalThis);

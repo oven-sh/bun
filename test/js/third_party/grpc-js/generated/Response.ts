0ef692d37dd27d1f1e6842f1761e8d6c0ed81781
Ciro Spaciari <ciro.spaciari@gmail.com>
2024-10-08 11:45:28 -0700

    more tests

---
+++ test/js/third_party/grpc-js/generated/Response.ts
@@ -0,0 +1,12 @@
+// Original file: test/fixtures/test_service.proto
+
+
+export interface Response {
+  'count'?: (number);
+  'message'?: (string);
+}
+
+export interface Response__Output {
+  'count': (number);
+  'message': (string);
+}

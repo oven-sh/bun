6b62afa201a020a6d9bfba0499c96a77268207fd
Meghan Denny <meghan@bun.sh>
2024-10-10 03:14:56 -0700

    accidentally committed skipping the regular tests

--- scripts/runner.node.mjs
+++ scripts/runner.node.mjs
@@ -908,7 +908,7 @@ async function getTests(cwd) {
   }
   // prettier-ignore
   return [
-    // ...Array.from(getFiles(cwd, "")),
+    ...Array.from(getFiles(cwd, "")),
     ...(await import("./citgm-items.mjs")).default
       .filter(v => !(v[2] && isWindows))
       .map(v => v[1]),

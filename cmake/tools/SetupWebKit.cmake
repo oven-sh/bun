b0a2e2c637a2ea5d2552078866c608be2c4ee79c
Dylan Conway <dylan.conway567@gmail.com>
2024-10-09 17:22:36 -0700

    update webkit

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -2,7 +2,7 @@ option(WEBKIT_VERSION "The version of WebKit to use")
 option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of downloading")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION 0a0a3838e5fab36b579df26620237bb62ed6d950)
+  set(WEBKIT_VERSION 019ff6e1e879ff4533f2a857cab5028b6b95ab53)
 endif()
 
 if(WEBKIT_LOCAL)

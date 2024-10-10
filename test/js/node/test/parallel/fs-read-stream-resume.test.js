62fcf8a1e06a3d4558da4e33c4bdc729e0367328
Jarred Sumner <jarred@jarredsumner.com>
2024-10-10 16:22:52 -0700

    Add a couple node tests that are now passing

---
+++ test/js/node/test/parallel/fs-read-stream-resume.test.js
@@ -0,0 +1,62 @@
+//#FILE: test-fs-read-stream-resume.js
+//#SHA1: ce30da9de55a395441a4670145bc3a57a2e1763e
+//-----------------
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+'use strict';
+const fs = require('fs');
+const path = require('path');
+
+const file = path.join(__dirname, '..', 'fixtures', 'x.txt');
+
+test('fs.createReadStream resume behavior', (done) => {
+  let data = '';
+  let first = true;
+
+  const stream = fs.createReadStream(file);
+  stream.setEncoding('utf8');
+  
+  const dataHandler = jest.fn((chunk) => {
+    data += chunk;
+    if (first) {
+      first = false;
+      stream.resume();
+    }
+  });
+
+  stream.on('data', dataHandler);
+
+  process.nextTick(() => {
+    stream.pause();
+    setTimeout(() => {
+      stream.resume();
+    }, 100);
+  });
+
+  stream.on('end', () => {
+    expect(data).toBe('xyz\n');
+    expect(dataHandler).toHaveBeenCalled();
+    done();
+  });
+});
+
+//<#END_FILE: test-fs-read-stream-resume.js

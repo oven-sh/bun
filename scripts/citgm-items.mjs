b39e00748a1a8e0e131fdf4292c3a24a67c9a9f9
Meghan Denny <meghan@bun.sh>
2024-10-11 03:11:42 -0700

    remove .git from clone urls

--- scripts/citgm-items.mjs
+++ scripts/citgm-items.mjs
@@ -3,12 +3,12 @@ export default [
   //    mode, clone_url, skipWindows
   // 0
   [0, "https://github.com/maxogden/binary-split.git", 1], //binary-split
-  [0, "https://github.com/mafintosh/blake2b-wasm.git"], //blake2b-wasm
-  [0, "https://github.com/mafintosh/flush-write-stream.git"], //flush-write-stream
-  [0, "https://github.com/hughsk/from2.git"], //from2
-  [0, "https://github.com/unicode-org/full-icu-test.git"], //full-icu-test
+  [0, "https://github.com/mafintosh/blake2b-wasm"], //blake2b-wasm
+  [0, "https://github.com/mafintosh/flush-write-stream"], //flush-write-stream
+  [0, "https://github.com/hughsk/from2"], //from2
+  [0, "https://github.com/unicode-org/full-icu-test"], //full-icu-test
   // 5
-  [0, "https://github.com/mafintosh/pumpify.git"], //pumpify
-  [0, "https://github.com/ForbesLindesay/thread-sleep.git"], //thread-sleep
-  [0, "https://github.com/juliangruber/isarray.git"], //isarray
+  [0, "https://github.com/mafintosh/pumpify"], //pumpify
+  [0, "https://github.com/ForbesLindesay/thread-sleep"], //thread-sleep
+  [0, "https://github.com/juliangruber/isarray"], //isarray
 ];

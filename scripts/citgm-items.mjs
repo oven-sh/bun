dc9c8a4d8f766cdf45eff3cda6ff426ff30571ea
Meghan Denny <meghan@bun.sh>
2024-10-11 03:33:13 -0700

    add more passing packages to the corpus

--- scripts/citgm-items.mjs
+++ scripts/citgm-items.mjs
@@ -11,4 +11,35 @@ export default [
   [0, "https://github.com/mafintosh/pumpify"], //pumpify
   [0, "https://github.com/ForbesLindesay/thread-sleep"], //thread-sleep
   [0, "https://github.com/juliangruber/isarray"], //isarray
+  [0, "https://github.com/justmoon/node-extend"],
+  [0, "https://github.com/Raynos/xtend"],
+  // 10
+  [0, "https://github.com/dominictarr/through"],
+  [0, "https://github.com/chrisdickinson/raf"],
+  [0, "https://github.com/standard/eslint-plugin-standard"],
+  [0, "https://github.com/cryptocoinjs/bs58"],
+  [0, "https://github.com/broccolijs/broccoli-merge-trees"],
+  // 15
+  [0, "https://github.com/hughsk/vinyl-source-stream"],
+  [0, "https://github.com/mariocasciaro/object-path"],
+  [0, "https://github.com/babel/eslint-plugin-babel"],
+  [0, "https://github.com/ciaranj/node-oauth"],
+  [0, "https://github.com/mathiasbynens/he"],
+  // 20
+  [0, "https://github.com/hughsk/vinyl-buffer"],
+  [0, "https://github.com/mathiasbynens/base64"],
+  [0, "https://github.com/dreamerslab/node.extend"],
+  [0, "https://github.com/mathiasbynens/utf8.js"],
+  [0, "https://github.com/jxson/front-matter"],
+  // 25
+  [0, "https://github.com/jonschlinkert/extend-shallow"],
+  [0, "https://github.com/enricomarino/is"],
+  [0, "https://github.com/stevemao/left-pad"],
+  [0, "https://github.com/hughsk/envify"],
+  [0, "https://github.com/ljharb/object.assign"],
+  // 30
+  [0, "https://github.com/jonschlinkert/word-wrap"],
+  [0, "https://github.com/goto100/xpath"],
+
+  // [0, "https://github.com/moment/moment-timezone"],//popular and passing but suite takes >8m to complete
 ];

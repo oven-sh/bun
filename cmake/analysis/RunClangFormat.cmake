6948e9549230b0245a3a7e0968331e9e13c161fc
Meghan Denny <meghan@bun.sh>
2024-10-15 15:56:26 -0700

    maybe its not recursive?

--- cmake/analysis/RunClangFormat.cmake
+++ cmake/analysis/RunClangFormat.cmake
@@ -1,7 +1,8 @@
 # https://clang.llvm.org/docs/ClangFormat.html
 
-file(GLOB BUN_H_SOURCES ${CONFIGURE_DEPENDS}
-  ${CWD}/src/*.h
+file(GLOB BUN_H_SOURCES LIST_DIRECTORIES false ${CONFIGURE_DEPENDS}
+  ${CWD}/src/bun.js/bindings/*.h
+  ${CWD}/src/bun.js/modules/*.h
 )
 
 set(CLANG_FORMAT_SOURCES ${BUN_C_SOURCES} ${BUN_CXX_SOURCES} ${BUN_H_SOURCES})

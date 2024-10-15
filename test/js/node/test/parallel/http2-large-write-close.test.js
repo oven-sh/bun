fb4e4a3581fc1eda4fc25805b6dacdf71985a3ca
Ciro Spaciari <ciro.spaciari@gmail.com>
2024-10-14 18:03:48 -0700

    close

--- test/js/node/test/parallel/http2-large-write-close.test.js
+++ test/js/node/test/parallel/http2-large-write-close.test.js
@@ -1,30 +1,30 @@
 //#FILE: test-http2-large-write-close.js
 //#SHA1: 66ad4345c0888700887c23af455fdd9ff49721d9
 //-----------------
-'use strict';
-const fixtures = require('../common/fixtures');
-const http2 = require('http2');
+"use strict";
+const fixtures = require("../common/fixtures");
+const http2 = require("http2");
 
 const content = Buffer.alloc(1e5, 0x44);
 
 let server;
 let port;
 
-beforeAll((done) => {
+beforeAll(done => {
   if (!process.versions.openssl) {
-    return test.skip('missing crypto');
+    return test.skip("missing crypto");
   }
 
   server = http2.createSecureServer({
-    key: fixtures.readKey('agent1-key.pem'),
-    cert: fixtures.readKey('agent1-cert.pem')
+    key: fixtures.readKey("agent1-key.pem"),
+    cert: fixtures.readKey("agent1-cert.pem"),
   });
 
-  server.on('stream', (stream) => {
+  server.on("stream", stream => {
     stream.respond({
-      'Content-Type': 'application/octet-stream',
-      'Content-Length': (content.length.toString() * 2),
-      'Vary': 'Accept-Encoding'
+      "Content-Type": "application/octet-stream",
+      "Content-Length": content.length.toString() * 2,
+      "Vary": "Accept-Encoding",
     });
 
     stream.write(content);
@@ -39,23 +39,22 @@ beforeAll((done) => {
   });
 });
 
-afterAll((done) => {
-  server.close(done);
+afterAll(() => {
+  server.close();
 });
 
-test('HTTP/2 large write and close', (done) => {
-  const client = http2.connect(`https://localhost:${port}`,
-                               { rejectUnauthorized: false });
+test("HTTP/2 large write and close", done => {
+  const client = http2.connect(`https://localhost:${port}`, { rejectUnauthorized: false });
 
-  const req = client.request({ ':path': '/' });
+  const req = client.request({ ":path": "/" });
   req.end();
 
   let receivedBufferLength = 0;
-  req.on('data', (buf) => {
+  req.on("data", buf => {
     receivedBufferLength += buf.length;
   });
 
-  req.on('close', () => {
+  req.on("close", () => {
     expect(receivedBufferLength).toBe(content.length * 2);
     client.close();
     done();

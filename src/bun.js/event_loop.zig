029e5bdb7c1b3f511617c962475a7162c8bb19e3
Jarred Sumner <jarred@jarredsumner.com>
2024-10-15 00:03:34 -0700

    Update event_loop.zig

--- src/bun.js/event_loop.zig
+++ src/bun.js/event_loop.zig
@@ -915,6 +915,7 @@ pub const EventLoop = struct {
         }
 
         while (@field(this, queue_name).readItem()) |task| {
+            log("run {s}", .{@tagName(task.tag())});
             defer counter += 1;
             switch (task.tag()) {
                 @field(Task.Tag, typeBaseName(@typeName(ShellAsync))) => {

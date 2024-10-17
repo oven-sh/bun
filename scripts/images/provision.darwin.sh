89a93902121f54fb513243e4d7afaa9e00ecd501
Ashcon Partovi <ashcon@partovi.net>
2024-10-17 10:28:00 -0700

    Fix

--- scripts/images/provision.darwin.sh
+++ scripts/images/provision.darwin.sh
@@ -9,7 +9,7 @@ enable_passwordless_sudo() {
 enable_auto_login() {
   # https://github.com/xfreebird/kcpassword
   echo "00000000: 1ced 3f4a bcbc ba2c caca 4e82" | sudo xxd -r - /etc/kcpassword
-  sudo defaults write /Library/Preferences/com.apple.loginwindow autoLoginUser {username}
+  sudo defaults write /Library/Preferences/com.apple.loginwindow autoLoginUser ${username}
 }
 
 disable_screen_saver() {
@@ -18,7 +18,7 @@ disable_screen_saver() {
 }
 
 disable_screen_lock() {
-  sysadminctl -screenLock off -password {password}
+  sysadminctl -screenLock off -password ${password}
 }
 
 disable_sleep() {

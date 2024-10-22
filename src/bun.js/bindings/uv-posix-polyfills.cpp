e6af5d6d164ad059e8ec70e2676ad5a1e5ed64c8
Jarred Sumner <jarred@jarredsumner.com>
2024-10-22 16:57:26 -0700

    Add uv_hrtime()

--- src/bun.js/bindings/uv-posix-polyfills.cpp
+++ src/bun.js/bindings/uv-posix-polyfills.cpp
@@ -12,4 +12,22 @@ uv_pid_t uv_os_getppid()
     return getppid();
 }
 
+#if OS(LINUX)
+#include <time.h>
+
+uint64_t uv_hrtime()
+{
+    timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec * 1000000000ULL + ts.tv_nsec;
+}
+
+#elif OS(DARWIN)
+#include <mach/mach_time.h>
+uint64_t uv_hrtime()
+{
+    return mach_absolute_time();
+}
+#endif
+
 #endif
--- src/bun.js/bindings/uv-posix-polyfills.h
+++ src/bun.js/bindings/uv-posix-polyfills.h
@@ -12,4 +12,7 @@ extern "C" BUN_EXPORT uv_pid_t uv_os_getpid();
 // Returns the parent process ID.
 extern "C" BUN_EXPORT uv_pid_t uv_os_getppid();
 
+// Returns the current high-resolution time in nanoseconds.
+extern "C" BUN_EXPORT uint64_t uv_hrtime();
+
 #endif

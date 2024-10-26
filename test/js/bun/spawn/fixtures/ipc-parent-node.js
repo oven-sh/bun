bdffd5155d4b479739019886cc3d302074d15875
Meghan Denny <meghan@bun.sh>
2024-10-26 01:44:08 -0700

    Update ipc-parent-node.js

--- test/js/bun/spawn/fixtures/ipc-parent-node.js
+++ test/js/bun/spawn/fixtures/ipc-parent-node.js
@@ -3,7 +3,7 @@ const child_process = require("node:child_process");
 
 console.log("p start");
 
-const child = child_process.spawn(process.argv[2], [path.resolve(import.meta.dirname, "ipc-child-bun.js")], {
+const child = child_process.spawn(process.argv[2], [path.resolve(__dirname, "ipc-child-bun.js")], {
   stdio: ["ignore", "inherit", "inherit", "ipc"],
 });
 child.on("message", message => {

076d1d3d36ab158a8188a99fdcae64eb25a674d7
Jarred Sumner <jarred@jarredsumner.com>
2024-11-06 03:42:24 -0800

    Never try using epoll_pwait2 on old linux kernels (#14980)

--- src/analytics/analytics_thread.zig
+++ src/analytics/analytics_thread.zig
@@ -343,6 +343,25 @@ pub const GenerateHeader = struct {
             return linux_kernel_version;
         }
 
+        export fn Bun__isEpollPwait2SupportedOnLinuxKernel() i32 {
+            if (comptime !Environment.isLinux) {
+                return 0;
+            }
+
+            // https://man.archlinux.org/man/epoll_pwait2.2.en#HISTORY
+            const min_epoll_pwait2 = Semver.Version{
+                .major = 5,
+                .minor = 11,
+                .patch = 0,
+            };
+
+            return switch (kernelVersion().order(min_epoll_pwait2, "", "")) {
+                .gt => 1,
+                .eq => 1,
+                .lt => 0,
+            };
+        }
+
         fn forLinux() Analytics.Platform {
             linux_os_name = std.mem.zeroes(@TypeOf(linux_os_name));
 

66ba6ba061aa32be15d4c77e45e12fcfe14bf2b4
Dylan Conway <35280289+dylan-conway@users.noreply.github.com>
2024-11-06 19:41:39 -0800

    fix 14957 (#15025)

--- src/bun.js/bindings/bindings.zig
+++ src/bun.js/bindings/bindings.zig
@@ -3759,7 +3759,6 @@ pub const JSValue = enum(JSValueReprInt) {
         MaxJS = 0b11111111,
         Event = 0b11101111,
         DOMWrapper = 0b11101110,
-        Blob = 0b11111100,
 
         /// This means that we don't have Zig bindings for the type yet, but it
         /// implements .toJSON()
@@ -4320,8 +4319,6 @@ pub const JSValue = enum(JSValueReprInt) {
 
             return ZigType.fromJS(value);
         }
-
-        return JSC.GetJSPrivateData(ZigType, value.asObjectRef());
     }
 
     extern fn JSC__JSValue__dateInstanceFromNullTerminatedString(*JSGlobalObject, [*:0]const u8) JSValue;
@@ -4342,13 +4339,6 @@ pub const JSValue = enum(JSValueReprInt) {
 
     pub fn isDate(this: JSValue) bool {
         return this.jsType() == .JSDate;
-    }
-
-    pub fn asCheckLoaded(value: JSValue, comptime ZigType: type) ?*ZigType {
-        if (!ZigType.Class.isLoaded() or value.isUndefinedOrNull())
-            return null;
-
-        return JSC.GetJSPrivateData(ZigType, value.asObjectRef());
     }
 
     pub fn protect(this: JSValue) void {

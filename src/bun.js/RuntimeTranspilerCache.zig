4cf98517476f2f138eaebe9a7dbce17469d62c88
pfg <pfg@pfg.pw>
2024-11-11 14:38:17 -0800

    Bump runtime transpiler cache version for #15009 (#15094)

--- src/bun.js/RuntimeTranspilerCache.zig
+++ src/bun.js/RuntimeTranspilerCache.zig
@@ -5,7 +5,8 @@
 /// Version 6: `use strict` is preserved in CommonJS modules when at the top of the file
 /// Version 7: Several bundler changes that are likely to impact the runtime as well.
 /// Version 8: Fix for generated symbols
-const expected_version = 8;
+/// Version 9: String printing changes
+const expected_version = 9;
 
 const bun = @import("root").bun;
 const std = @import("std");

0447bcd6a9f94bf82409ac59c8dc714fe6ec4c44
ippsav <mehdiboujid@gmail.com>
2024-11-12 16:33:28 +0100

    handle different str encoding

--- src/string_case.zig
+++ src/string_case.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const bun = @import("root").bun;
+const strings = bun.strings;
 
 const BunString = bun.String;
 const JSC = bun.JSC;
@@ -71,49 +72,129 @@ pub fn StringCaseConverter(comptime OutputType: type) type {
             comptime capitalize_word: bool,
             comptime uppercase_word: bool,
         ) !OutputType {
-            const words = try splitWords(input, allocator);
-            defer allocator.free(words);
+            if (strings.isAllASCII(input)) {
+                const words = try splitWords(input, allocator);
+                defer allocator.free(words);
 
-            var total_len: usize = 0;
-            for (words) |word| {
-                total_len += word.len;
-            }
-            total_len += seperator.len * @max(0, words.len -| 1);
+                var total_len: usize = 0;
+                for (words) |word| {
+                    total_len += word.len;
+                }
+                total_len += seperator.len * (words.len -| 1);
 
-            var result = try allocator.alloc(u8, total_len);
-            errdefer allocator.free(result);
+                var result = try allocator.alloc(u8, total_len);
+                errdefer allocator.free(result);
 
-            var pos: usize = 0;
-            for (words, 0..) |word, i| {
-                if (word.len == 0) continue;
+                var pos: usize = 0;
+                for (words, 0..) |word, i| {
+                    if (word.len == 0) continue;
 
-                const should_capitalize = (i == 0 and capitalize_first) or
-                    (i > 0 and capitalize_word);
+                    const should_capitalize = (i == 0 and capitalize_first) or
+                        (i > 0 and capitalize_word);
 
-                if (uppercase_word) {
-                    for (word, 0..) |c, j| {
-                        result[pos + j] = std.ascii.toUpper(c);
+                    if (uppercase_word) {
+                        for (word, 0..) |c, j| {
+                            result[pos + j] = std.ascii.toUpper(c);
+                        }
+                    } else if (should_capitalize) {
+                        result[pos] = std.ascii.toUpper(word[0]);
+                        @memcpy(result[pos + 1 ..][0 .. word.len - 1], word[1..]);
+                    } else {
+                        if (i == 0) {
+                            result[pos] = std.ascii.toLower(word[0]);
+                            @memcpy(result[pos + 1 ..][0 .. word.len - 1], word[1..]);
+                        } else {
+                            @memcpy(result[pos..][0..word.len], word);
+                        }
                     }
-                } else if (should_capitalize) {
-                    result[pos] = std.ascii.toUpper(word[0]);
-                    @memcpy(result[pos + 1 ..][0 .. word.len - 1], word[1..]);
-                } else {
-                    @memcpy(result[pos..][0..word.len], word);
+
+                    pos += word.len;
+
+                    if (i < word.len - 1 and seperator.len > 0) {
+                        @memcpy(result[pos..][0..seperator.len], seperator);
+                        pos += seperator.len;
+                    }
                 }
 
-                pos += word.len;
+                return switch (OutputType) {
+                    []const u8 => result,
+                    BunString => BunString.createUTF8(result),
+                    else => @compileError("Unsupported output type"),
+                };
+            } else {
+                var iter = strings.CodepointIterator.init(input);
+                var cursor = strings.CodepointIterator.Cursor{};
 
-                if (i < word.len - 1 and seperator.len > 0) {
-                    @memcpy(result[pos..][0..seperator.len], seperator);
-                    pos += seperator.len;
+                // First pass calculating length
+                var total_len: usize = 0;
+                var word_count: usize = 0;
+                var prev_was_seperator = true;
+
+                while (iter.next(&cursor)) {
+                    const is_seperator = cursor.c <= 127 and isSeperator(@intCast(cursor.c));
+
+                    if (!is_seperator) {
+                        total_len += cursor.width;
+                        if (prev_was_seperator) word_count += 1;
+                    }
+
+                    prev_was_seperator = is_seperator;
                 }
+
+                total_len += seperator.len * (word_count -| 1);
+                var result = try allocator.alloc(u8, total_len);
+                errdefer allocator.free(result);
+
+                // Second pass build str
+                iter = strings.CodepointIterator.init(input);
+                cursor = strings.CodepointIterator.Cursor{};
+
+                var pos: usize = 0;
+                var word_index: usize = 0;
+                prev_was_seperator = true;
+
+                while (iter.next(&cursor)) {
+                    const is_seperator = cursor.c <= 127 and isSeperator(@intCast(cursor.c));
+                    if (is_seperator) {
+                        prev_was_seperator = true;
+                        continue;
+                    }
+
+                    if (!prev_was_seperator and word_index > 0 and seperator.len > 0) {
+                        @memcpy(result[pos..][0..seperator.len], seperator);
+                        pos += seperator.len;
+                    }
+
+                    if (cursor.c <= 127) {
+                        const c: u8 = @intCast(cursor.c);
+                        const should_capitalize = uppercase_word or
+                            (word_index > 0 and capitalize_word and prev_was_seperator) or
+                            (word_index == 0 and capitalize_first);
+
+                        result[pos] = brk: {
+                            if (should_capitalize)
+                                break :brk std.ascii.toUpper(c)
+                            else if (word_index == 0)
+                                break :brk std.ascii.toLower(c)
+                            else
+                                break :brk c;
+                        };
+                        pos += 1;
+                    } else {
+                        @memcpy(result[pos..][0..cursor.width], input[cursor.i..][0..cursor.width]);
+                        pos += cursor.width;
+                    }
+
+                    if (prev_was_seperator) word_index += 1;
+                    prev_was_seperator = false;
+                }
+
+                return switch (OutputType) {
+                    []const u8 => result,
+                    BunString => BunString.createUTF8(result),
+                    else => @compileError("Unsupported output type"),
+                };
             }
-
-            return switch (OutputType) {
-                []const u8 => result,
-                BunString => BunString.createUTF8(result),
-                else => @compileError("Unsupported output type"),
-            };
         }
     };
 }
@@ -130,7 +211,11 @@ pub fn camelCase(
     const input = arguments.ptr[0].toBunString(globalThis);
     defer input.deref();
 
-    const result: BunString = JSCaseConverter.toCamelCase(input.byteSlice(), bun.default_allocator) catch |err| {
+    // TODO: better input handling
+    const utf8 = input.toUTF8(bun.default_allocator);
+    defer utf8.deinit();
+
+    const result: BunString = JSCaseConverter.toCamelCase(utf8.byteSlice(), bun.default_allocator) catch |err| {
         globalThis.throw("toCamelCase() internal error: {s}", .{@errorName(err)});
         return .undefined;
     };

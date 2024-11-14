85b448c47df832a54e26dbd442dca9d9c2d383e8
ippsav <mehdiboujid@gmail.com>
2024-11-14 19:55:12 +0100

    handle utf8 codepoints

--- src/string_case.zig
+++ src/string_case.zig
@@ -75,34 +75,28 @@ const Encoding = enum {
     }
 };
 
-fn isSeperator(comptime kind: Encoding, c: kind.Byte()) bool {
-    return switch (kind) {
-        .utf16 => switch (c) {
-            // ASCII separators
-            0x0020, // space
-            0x002D, // hyphen-minus
-            0x005F, // underscore
-            0x002E, // dot
-            0x0009, // tab
-            0x000A, // line feed
-            0x000D, // carriage return
+fn isSeperator(c: u32) bool {
+    return switch (c) {
+        // ASCII separators
+        0x0020, // space
+        0x002D, // hyphen-minus
+        0x005F, // underscore
+        0x002E, // dot
+        0x0009, // tab
+        0x000A, // line feed
+        0x000D, // carriage return
 
-            // Additional Unicode separators
-            0x00A0, // no-break space
-            0x00B6, // pilcrow sign
-            0x2000...0x200B, // various Unicode spaces and zero-width spaces
-            0x2010...0x2015, // various Unicode hyphens and dashes
-            0x2018...0x201F, // quotation marks
-            0x2026, // ellipsis
-            0x3000, // ideographic space
-            0xFEFF, // zero width no-break space
-            => true,
-            else => false,
-        },
-        else => switch (c) {
-            ' ', '-', '_', '.', '\t', '\n', '\r' => true,
-            else => false,
-        },
+        // Additional Unicode separators
+        0x00A0, // no-break space
+        0x00B6, // pilcrow sign
+        0x2000...0x200B, // various Unicode spaces and zero-width spaces
+        0x2010...0x2015, // various Unicode hyphens and dashes
+        0x2018...0x201F, // quotation marks
+        0x2026, // ellipsis
+        0x3000, // ideographic space
+        0xFEFF, // zero width no-break space
+        => true,
+        else => false,
     };
 }
 
@@ -120,12 +114,169 @@ fn isUpper(comptime kind: Encoding, c: kind.Byte()) bool {
     };
 }
 
+fn processUtf8(bytes: []const u8, idx: usize) u32 {
+    const sequence_len = strings.wtf8ByteSequenceLength(bytes[idx]);
+    bun.assert(sequence_len > 1);
+
+    var buffer: [4]u8 = undefined;
+    @memcpy(buffer[0..sequence_len], bytes[idx..][0..sequence_len]);
+
+    const c = strings.decodeWTF8RuneTMultibyte(&buffer, sequence_len, u32, strings.unicode_replacement);
+    std.debug.print("codepoint {d}\n", .{c});
+    return c;
+}
+
+fn peekNextUtf8(bytes: []const u8, offset: usize) ?struct { usize, usize } {
+    var i = offset;
+    var start = offset;
+
+    while (i < bytes.len) {
+        const c = if (bytes[i] > 127)
+            processUtf8(bytes, i)
+        else
+            bytes[i];
+
+        if (!isSeperator(c)) break;
+
+        i += if (bytes[i] > 127)
+            strings.wtf8ByteSequenceLength(bytes[i])
+        else
+            1;
+        start = i;
+    }
+
+    if (i >= bytes.len) return null;
+
+    while (i < bytes.len) {
+        const c = if (bytes[i] > 127)
+            processUtf8(bytes, i)
+        else
+            bytes[i];
+
+        const is_separator = isSeperator(c);
+
+        const is_case_boundary = if (i > 0 and i + 1 < bytes.len and
+            bytes[i] <= 127 and bytes[i - 1] <= 127)
+            std.ascii.isLower(bytes[i - 1]) and std.ascii.isUpper(bytes[i])
+        else
+            false;
+
+        if (is_separator or is_case_boundary) {
+            if (i > start) return .{ start, i };
+        }
+
+        i += if (bytes[i] > 127)
+            strings.wtf8ByteSequenceLength(bytes[i])
+        else
+            1;
+    }
+
+    if (start < bytes.len) {
+        return .{ start, bytes.len };
+    }
+
+    return null;
+}
+
+fn peekNext(comptime kind: Encoding, bytes: []const kind.Byte(), offset: usize) ?struct { usize, usize } {
+    // Utf8 should be handled differently
+    bun.assert(kind != .utf8);
+
+    var i = offset;
+    var start = offset;
+
+    while (i < bytes.len and isSeperator(bytes[i])) {
+        i += 1;
+        start = i;
+    }
+
+    if (i >= bytes.len) return null;
+
+    while (i < bytes.len) {
+        const curr = bytes[i];
+
+        const is_separator = isSeperator(curr);
+
+        const is_case_boundary = if (i > 0 and i + 1 < bytes.len)
+            isLower(kind, bytes[i - 1]) and isUpper(kind, curr)
+        else
+            false;
+
+        if (is_separator or is_case_boundary) {
+            if (i > start) return .{ start, i };
+        }
+
+        i += 1;
+    }
+
+    if (start < bytes.len) {
+        return .{ start, bytes.len };
+    }
+
+    return null;
+}
+
+// TODO: handle casing of utf8 codepoints
+pub fn convertUtf8(
+    from: []const u8,
+    to: []u8,
+    case: CaseRules,
+) void {
+    var pos: usize = 0;
+    var is_first = true;
+
+    var iter = NewWordIterator(.utf8).init(from);
+
+    while (iter.next()) |word| {
+        if (!is_first and case.sep != null) {
+            to[pos] = case.sep.?;
+            pos += 1;
+        }
+
+        const should_capitalize = (is_first and case.capitalize_first) or
+            (!is_first and case.capitalize_word);
+
+        if (case.uppercase_word) {
+            for (word, 0..) |c, i| {
+                to[pos + i] = if (c <= 127)
+                    std.ascii.toUpper(c)
+                else
+                    c;
+            }
+        } else if (should_capitalize) {
+            to[pos] = if (word[0] <= 127)
+                std.ascii.toUpper(word[0])
+            else
+                word[0];
+
+            for (word[1..], 1..) |c, i| {
+                to[pos + i] = if (c <= 127)
+                    std.ascii.toLower(c)
+                else
+                    c;
+            }
+        } else {
+            for (word, 0..) |c, i| {
+                to[pos + i] = if (c <= 127)
+                    std.ascii.toLower(c)
+                else
+                    c;
+            }
+        }
+
+        pos += word.len;
+        is_first = false;
+    }
+}
+
 pub fn convert(
     comptime kind: Encoding,
     from: []const kind.Byte(),
     to: []kind.Byte(),
     case: CaseRules,
 ) void {
+    if (kind == .utf8) return convertUtf8(from, to, case);
+
     var pos: usize = 0;
     var is_first = true;
 
@@ -209,97 +360,63 @@ pub fn convertLen(
 }
 
 pub fn NewWordIterator(comptime kind: Encoding) type {
-    return struct {
-        const Self = @This();
-        bytes: []const kind.Byte(),
-        i: usize,
-        start: usize,
+    return switch (kind) {
+        .utf8 => struct {
+            const Self = @This();
+            bytes: []const kind.Byte(),
+            i: usize,
 
-        pub fn init(bytes: []const kind.Byte()) Self {
-            return .{
-                .bytes = bytes,
-                .i = 0,
-                .start = 0,
-            };
-        }
-
-        pub fn next(self: *Self) ?[]const kind.Byte() {
-            while (self.i < self.bytes.len and isSeperator(kind, self.bytes[self.i])) {
-                self.i += 1;
-                self.start = self.i;
+            pub fn init(bytes: []const kind.Byte()) Self {
+                return .{
+                    .bytes = bytes,
+                    .i = 0,
+                };
             }
 
-            if (self.i >= self.bytes.len) return null;
-
-            while (self.i < self.bytes.len) {
-                const curr = self.bytes[self.i];
-
-                const is_seperator = isSeperator(kind, curr);
-
-                const is_case_boundary = if (self.i > 0 and self.i + 1 < self.bytes.len)
-                    isLower(kind, self.bytes[self.i - 1]) and isUpper(kind, curr)
-                else
-                    false;
-
-                if (is_seperator or is_case_boundary) {
-                    const word = self.bytes[self.start..self.i];
-                    if (is_case_boundary) {
-                        self.start = self.i;
-                    } else {
-                        self.i += 1;
-                        self.start = self.i;
-                    }
-
-                    if (word.len > 0) return word;
+            pub fn next(self: *Self) ?[]const kind.Byte() {
+                if (peekNextUtf8(self.bytes, self.i)) |res| {
+                    const start, const end = res;
+                    self.i = end;
+                    return self.bytes[start..end];
                 }
-
-                self.i += 1;
+                return null;
             }
 
-            if (self.start < self.bytes.len) {
-                const word = self.bytes[self.start..];
-                self.start = self.bytes.len;
-                return word;
+            pub fn peek(self: *const Self) ?[]const kind.Byte() {
+                if (peekNextUtf8(self.bytes, self.start)) |res| {
+                    const start, const end = res;
+                    return self.bytes[start..end];
+                } else return null;
+            }
+        },
+        else => struct {
+            const Self = @This();
+            bytes: []const kind.Byte(),
+            i: usize,
+
+            pub fn init(bytes: []const kind.Byte()) Self {
+                return .{
+                    .bytes = bytes,
+                    .i = 0,
+                };
             }
 
-            return null;
-        }
-
-        pub fn peek(self: *const Self) ?[]const kind.Byte() {
-            var i = self.i;
-            var start = self.start;
-
-            while (i < self.bytes.len and isSeperator(kind, self.bytes[i])) {
-                i += 1;
-                start = i;
-            }
-
-            if (i >= self.bytes.len) return null;
-
-            while (i < self.bytes.len) {
-                const curr = self.bytes[i];
-
-                const is_separator = isSeperator(kind, curr);
-
-                const is_case_boundary = if (i > 0 and i + 1 < self.bytes.len)
-                    isLower(kind, self.bytes[i - 1]) and isUpper(kind, curr)
-                else
-                    false;
-
-                if (is_separator or is_case_boundary) {
-                    const word = self.bytes[start..i];
-                    if (word.len > 0) return word;
+            pub fn next(self: *Self) ?[]const kind.Byte() {
+                if (peekNext(kind, self.bytes, self.i)) |res| {
+                    const start, const end = res;
+                    self.i = end;
+                    return self.bytes[start..end];
                 }
-
-                i += 1;
+                return null;
             }
 
-            if (start < self.bytes.len) {
-                return self.bytes[start..];
+            pub fn peek(self: *const Self) ?[]const kind.Byte() {
+                if (peekNext(kind, self.bytes, self.start)) |res| {
+                    const start, const end = res;
+                    return self.bytes[start..end];
+                } else return null;
             }
-
-            return null;
-        }
+        },
     };
 }
 

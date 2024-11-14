73be53e5b600d155795199b5d68547ddd55da43b
Dennis Dudek <admin@kapsonfire.de>
2024-11-14 09:33:09 +0100

    change pr scope to remove CONNECT

--- src/http/method.zig
+++ src/http/method.zig
@@ -52,7 +52,6 @@ pub const Method = enum {
 
     const with_body: std.enums.EnumSet(Method) = brk: {
         var values = std.enums.EnumSet(Method).initFull();
-        values.remove(.CONNECT);
         values.remove(.HEAD);
         values.remove(.TRACE);
         break :brk values;
@@ -60,7 +59,6 @@ pub const Method = enum {
 
     const with_request_body: std.enums.EnumSet(Method) = brk: {
         var values = std.enums.EnumSet(Method).initFull();
-        values.remove(.CONNECT);
         values.remove(.GET);
         values.remove(.HEAD);
         values.remove(.OPTIONS);

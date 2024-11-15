ac23c89c8965faefe77bab01327d7a147df6e2c8
Kapsonfire-DE <admin@kapsonfire.de>
2024-11-15 15:05:55 +0100

    typo

--- src/bun.js/api/filesystem_router.zig
+++ src/bun.js/api/filesystem_router.zig
@@ -223,7 +223,7 @@ pub const FileSystemRouter = struct {
                         }
 
                         var abs_parts_con = [_]string{ entry.dir, entry.base() };
-                        const full_path = vm.bundler.fs.abs(&abs_parts_con);;
+                        const full_path = vm.bundler.fs.abs(&abs_parts_con);
                         bustDirCacheRecursive(this, globalThis, full_path);
                         _ = vm.bundler.resolver.bustDirCache(strings.withoutTrailingSlashWindowsPath(full_path));
 

c258685b87f9cfffb06a6fb4703230906c59c010
Grigory <techuila.sunset@gmail.com>
2024-11-20 02:19:53 +0500

    feat(resolver): add support for self-referencing imports

---
+++ test/cli/run/self-import.test.ts
@@ -0,0 +1,67 @@
+import { describe, expect, test } from "bun:test";
+import { spawn } from "bun";
+import { bunExe, tmpdirSync } from "harness";
+import { join } from "path";
+import { writeFile } from "fs/promises";
+
+const testWord = "bunny";
+
+describe("bun", () => {
+  test("should resolve self-imports by name", async () => {
+    const tempDir = tmpdirSync();
+
+    for (const packageName of ["pkg", "@scope/pkg"]) {
+      // general check without exports
+      await writeFile(
+        join(tempDir, "package.json"),
+        JSON.stringify({
+          name: packageName,
+        }),
+      );
+      await writeFile(join(tempDir, "index.js"), `module.exports.testWord = "${testWord}";`);
+      await writeFile(
+        join(tempDir, "other.js"),
+        `const pkg = require("${packageName}");\nimport pkg2 from "${packageName}"\nconsole.log(pkg.testWord,pkg2.testWord);`,
+      );
+
+      let subprocess = spawn({
+        cmd: [bunExe(), "run", "other.js"],
+        cwd: tempDir,
+        stdout: "pipe",
+      });
+      let out = await new Response(subprocess.stdout).text();
+      expect(out).toContain(`${testWord} ${testWord}`);
+
+      // should not resolve not exported files
+      await writeFile(
+        join(tempDir, "package.json"),
+        JSON.stringify({
+          name: packageName,
+          exports: { "./index.js": "./index.js" },
+        }),
+      );
+
+      subprocess = spawn({
+        cmd: [bunExe(), "run", "other.js"],
+        cwd: tempDir,
+        stdout: "pipe",
+      });
+      out = await new Response(subprocess.stdout).text();
+      expect(out).not.toContain(`${testWord} ${testWord}`);
+
+      // should resolve exported files
+      await writeFile(
+        join(tempDir, "other.js"),
+        `const pkg = require("${packageName}/index.js");\nimport pkg2 from "${packageName}/index.js"\nconsole.log(pkg.testWord,pkg2.testWord);`,
+      );
+
+      subprocess = spawn({
+        cmd: [bunExe(), "run", "other.js"],
+        cwd: tempDir,
+        stdout: "pipe",
+      });
+      out = await new Response(subprocess.stdout).text();
+      expect(out).toContain(`${testWord} ${testWord}`);
+    }
+  });
+});

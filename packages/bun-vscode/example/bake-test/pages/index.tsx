4110c2dce79e138619a0e27679f12b1dc8b1cce4
Alistair Smith <hi@alistair.sh>
2024-11-20 11:17:43 -0800

    break the index.tsx page

--- packages/bun-vscode/example/bake-test/pages/index.tsx
+++ packages/bun-vscode/example/bake-test/pages/index.tsx
@@ -1,3 +1,17 @@
-export default function Route() {
-  return <div>Hello world</div>;
+"use client";
+
+
+import { useState } from "react";
+
+function App() {
+	const [count, setCount] = useState(null);
+
+	return (
+		<>
+      {/* @ts-expect-error */}
+		  <button onClick={() => setCount(count => count.charAt(0))}>count is {count}</button>
+		</>
+	);
 }
+
+export default App;

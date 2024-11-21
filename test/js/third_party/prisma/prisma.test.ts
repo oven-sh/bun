c7ca6d702886b32527052be10eae53292b160ad5
Jarred Sumner <jarred@jarredsumner.com>
2024-11-21 00:18:01 -0800

    Copy test

--- test/js/third_party/prisma/prisma.test.ts
+++ test/js/third_party/prisma/prisma.test.ts
@@ -2,6 +2,9 @@ import { createCanvas } from "@napi-rs/canvas";
 import { it as bunIt, test as bunTest, describe, expect } from "bun:test";
 import { generate, generateClient } from "./helper.ts";
 import type { PrismaClient } from "./prisma/types.d.ts";
+import { appendFile } from "fs/promises";
+import { heapStats } from "bun:jsc";
+import { isCI } from "harness";
 
 function* TestIDGenerator(): Generator<number> {
   while (true) {
@@ -73,6 +76,54 @@ async function cleanTestId(prisma: PrismaClient, testId: number) {
         expect().pass();
       });
     }
+
+    test(
+      "does not leak",
+      async (prisma: PrismaClient, _: number) => {
+        // prisma leak was 8 bytes per query, so a million requests would manifest as an 8MB leak
+        const batchSize = 1000;
+        const warmupIters = 8_000_000 / batchSize;
+        const testIters = 4_000_000 / batchSize;
+        const gcPeriod = 100_000 / batchSize;
+        let totalIters = 0;
+        const queries = new Array(batchSize);
+
+        async function runQuery() {
+          totalIters++;
+          // GC occasionally to make memory usage more deterministic
+          if (totalIters % gcPeriod == gcPeriod - 1) {
+            Bun.gc(true);
+            const line = `${totalIters * batchSize},${(process.memoryUsage.rss() / 1024 / 1024) | 0}`;
+            console.log(line);
+            if (!isCI) await appendFile("rss.csv", line + "\n");
+          }
+
+          for (let i = 0; i < batchSize; i++) {
+            queries[i] = prisma.$queryRaw`SELECT 1`;
+          }
+          await Promise.all(queries);
+        }
+
+        console.time("Warmup x " + warmupIters + " x " + batchSize);
+        for (let i = 0; i < warmupIters; i++) {
+          await runQuery();
+        }
+        console.timeEnd("Warmup x " + warmupIters + " x " + batchSize);
+
+        console.time("Test x " + testIters + " x " + batchSize);
+        // measure memory now
+        const before = process.memoryUsage.rss();
+        // run a bunch more iterations to see if memory usage increases
+        for (let i = 0; i < testIters; i++) {
+          await runQuery();
+        }
+        console.timeEnd("Test x " + testIters + " x " + batchSize);
+        const after = process.memoryUsage.rss();
+        const deltaMB = (after - before) / 1024 / 1024;
+        expect(deltaMB).toBeLessThan(10);
+      },
+      120_000,
+    );
 
     test(
       "CRUD basics",

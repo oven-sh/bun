a5e4680a2a0a5af521225006261b33c8754b0eb6
Alistair Smith <hi@alistair.sh>
2024-11-21 11:56:04 -0800

    test

--- packages/bun-vscode/example/axios.ts
+++ packages/bun-vscode/example/axios.ts
@@ -1,10 +1,8 @@
-import * as axios from "axios";
+// import axios from "axios";
 
-console.log(axios);
+// const res = await axios.get("https://httpbin.org/status/500");
+const res = await fetch("https://httpbin.org/status/500");
 
-// const res = await axios("https://httpbin.org/status/500");
-// const res = await fetch("https://httpbin.org/status/500");
-
-// if ("res.ok") {
-//   throw new Error("Fucked it");
-// }
+if (!res.ok) {
+  throw new Error("Didn't work!");
+}

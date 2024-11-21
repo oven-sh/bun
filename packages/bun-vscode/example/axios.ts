c59e73b31ef188e0052be31d4d3cfa05c1f8b39b
Alistair Smith <hi@alistair.sh>
2024-11-21 12:21:20 -0800

    delete axios file

--- packages/bun-vscode/example/axios.ts
+++
@@ -1,8 +0,0 @@
-// import axios from "axios";
-
-// const res = await axios.get("https://httpbin.org/status/500");
-const res = await fetch("https://httpbin.org/status/500");
-
-if (!res.ok) {
-  throw new Error("Didn't work!");
-}

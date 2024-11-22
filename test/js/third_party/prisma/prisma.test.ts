f3ed4439bdf32195573e9263cabde313ad12b1c0
Ciro Spaciari <ciro.spaciari@gmail.com>
2024-11-22 11:49:20 -0800

    make prisma avoid env url because of CI and relly on getSecret

--- test/js/third_party/prisma/prisma.test.ts
+++ test/js/third_party/prisma/prisma.test.ts
@@ -22,23 +22,25 @@ async function cleanTestId(prisma: PrismaClient, testId: number) {
 ["sqlite", "postgres" /*"mssql", "mongodb"*/].forEach(async type => {
   let Client: typeof PrismaClient;
 
-  if (!isCI) {
-    if (type !== "sqlite" && !process.env[`TLS_${type.toUpperCase()}_DATABASE_URL`]) {
-      throw new Error(`$TLS_${type.toUpperCase()}_DATABASE_URL is not set`);
-    }
-  } else if (type !== "sqlite") {
-    process.env[`TLS_${type.toUpperCase()}_DATABASE_URL`] ||= getSecret(`TLS_${type.toUpperCase()}_DATABASE_URL`);
-  }
+  const database_url = type !== "sqlite" ? getSecret(`TLS_${type.toUpperCase()}_DATABASE_URL`) : null;
 
   Client = await generateClient(type);
 
   async function test(label: string, callback: Function, timeout: number = 5000) {
-    const it = Client ? bunTest : bunTest.skip;
+    const it = Client && (database_url || type === "sqlite") ? bunTest : bunTest.skip;
 
     it(
       label,
       async () => {
-        const prisma = new Client();
+        const prisma = database_url
+          ? new Client({
+              datasources: {
+                db: {
+                  url: database_url as string,
+                },
+              },
+            })
+          : new Client();
         const currentTestId = test_id.next().value;
         await cleanTestId(prisma, currentTestId);
         try {

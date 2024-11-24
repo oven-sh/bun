f8e87e34d64ee1ee6c54dda5745ab0f5bd2755eb
Jarred Sumner <jarred@jarredsumner.com>
2024-11-24 02:51:04 -0800

    Update http-hello.js

--- bench/snippets/http-hello.js
+++ bench/snippets/http-hello.js
@@ -1,7 +1,8 @@
 var i = 0;
 export default {
+  port: 3002,
   fetch(req) {
-    if (i++ === 200_000 - 1) queueMicrotask(() => process.exit(0));
+    if (i++ === 200_000 - 1) setTimeout(() => process.exit(0), 0);
     return new Response("Hello, World!" + i);
   },
 };

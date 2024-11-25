aa2a73ccdac7ffc41605099e9b9b6fcd4b4c0739
Dylan Conway <dylan.conway567@gmail.com>
2024-11-25 13:50:05 -0800

    todo

--- test/js/bun/http/bun-serve-args.test.ts
+++ test/js/bun/http/bun-serve-args.test.ts
@@ -379,7 +379,7 @@ describe("Bun.serve hostname and port validation", () => {
 });
 
 describe("Bun.serve hostname coercion", () => {
-  test("number hostnames coerce to string", () => {
+  test.todo("number hostnames coerce to string", () => {
     using server = serve({
       // @ts-expect-error - Testing runtime coercion
       hostname: 0, // Should coerce to "0"

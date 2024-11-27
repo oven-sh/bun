215fdb4697da0228ee6654d18ab04d82855e9701
Meghan Denny <meghan@bun.sh>
2024-11-26 17:17:12 -0800

    zig: make throwInvalidArgumentTypeValue use JSError (#15302)
    Co-authored-by: Ciro Spaciari <ciro.spaciari@gmail.com>

--- src/bun.js/bindings/bindings.zig
+++ src/bun.js/bindings/bindings.zig
@@ -3023,10 +3023,10 @@ pub const JSGlobalObject = opaque {
         argname: []const u8,
         typename: []const u8,
         value: JSValue,
-    ) JSValue {
+    ) bun.JSError {
         var formatter = JSC.ConsoleObject.Formatter{ .globalThis = this };
         this.ERR_INVALID_ARG_TYPE("The \"{s}\" argument must be of type {s}. Received {}", .{ argname, typename, value.toFmt(&formatter) }).throw();
-        return .zero;
+        return error.JSError;
     }
 
     pub fn throwInvalidArgumentRangeValue(
@@ -3034,9 +3034,9 @@ pub const JSGlobalObject = opaque {
         argname: []const u8,
         typename: []const u8,
         value: i64,
-    ) JSValue {
+    ) bun.JSError {
         this.ERR_OUT_OF_RANGE("The \"{s}\" is out of range. {s}. Received {}", .{ argname, typename, value }).throw();
-        return .zero;
+        return error.JSError;
     }
 
     pub fn throwInvalidPropertyTypeValue(
@@ -3044,11 +3044,11 @@ pub const JSGlobalObject = opaque {
         field: []const u8,
         typename: []const u8,
         value: JSValue,
-    ) JSValue {
+    ) bun.JSError {
         const ty_str = value.jsTypeString(this).toSlice(this, bun.default_allocator);
         defer ty_str.deinit();
         this.ERR_INVALID_ARG_TYPE("The \"{s}\" property must be of type {s}. Received {s}", .{ field, typename, ty_str.slice() }).throw();
-        return .zero;
+        return error.JSError;
     }
 
     pub fn createNotEnoughArguments(
@@ -3423,15 +3423,13 @@ pub const JSGlobalObject = opaque {
             allowFunction: bool = false,
             nullable: bool = false,
         },
-    ) bool {
+    ) bun.JSError!void {
         if ((!opts.nullable and value.isNull()) or
             (!opts.allowArray and value.isArray()) or
             (!value.isObject() and (!opts.allowFunction or !value.isFunction())))
         {
-            _ = this.throwInvalidArgumentTypeValue(arg_name, "object", value);
-            return false;
+            return this.throwInvalidArgumentTypeValue(arg_name, "object", value);
         }
-        return true;
     }
 
     pub fn throwRangeError(this: *JSGlobalObject, value: anytype, options: bun.fmt.OutOfRangeOptions) void {
@@ -3447,7 +3445,7 @@ pub const JSGlobalObject = opaque {
         always_allow_zero: bool = false,
     };
 
-    pub fn validateIntegerRange(this: *JSGlobalObject, value: JSValue, comptime T: type, default: T, comptime range: IntegerRange) ?T {
+    pub fn validateIntegerRange(this: *JSGlobalObject, value: JSValue, comptime T: type, default: T, comptime range: IntegerRange) bun.JSError!T {
         if (value == .undefined or value == .zero) {
             return default;
         }
@@ -3476,14 +3474,13 @@ pub const JSGlobalObject = opaque {
             }
             if (int < min_t or int > max_t) {
                 this.throwRangeError(int, .{ .field_name = field_name, .min = min, .max = max });
-                return null;
+                return error.JSError;
             }
             return @intCast(int);
         }
 
         if (!value.isNumber()) {
-            _ = this.throwInvalidPropertyTypeValue(field_name, "number", value);
-            return null;
+            return this.throwInvalidPropertyTypeValue(field_name, "number", value);
         }
         const f64_val = value.asNumber();
         if (always_allow_zero and f64_val == 0) {
@@ -3495,12 +3492,11 @@ pub const JSGlobalObject = opaque {
             return default;
         }
         if (@floor(f64_val) != f64_val) {
-            _ = this.throwInvalidPropertyTypeValue(field_name, "integer", value);
-            return null;
+            return this.throwInvalidPropertyTypeValue(field_name, "integer", value);
         }
         if (f64_val < min_t or f64_val > max_t) {
             this.throwRangeError(f64_val, .{ .field_name = comptime field_name, .min = min, .max = max });
-            return null;
+            return error.JSError;
         }
 
         return @intFromFloat(f64_val);
@@ -5406,10 +5402,8 @@ pub const JSValue = enum(i64) {
         if (prop.isNull() or prop == .false) {
             return null;
         }
-
         if (prop.isSymbol()) {
-            _ = global.throwInvalidPropertyTypeValue(property, "string", prop);
-            return error.JSError;
+            return global.throwInvalidPropertyTypeValue(property, "string", prop);
         }
 
         const str = prop.toBunString(global);
@@ -5417,11 +5411,9 @@ pub const JSValue = enum(i64) {
             str.deref();
             return error.JSError;
         }
-
         if (str.isEmpty()) {
             return null;
         }
-
         return str;
     }
 

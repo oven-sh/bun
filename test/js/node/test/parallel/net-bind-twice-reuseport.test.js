6cb1f66d4356fdac3b9deb3045f6905e976319d9
Ciro Spaciari <ciro.spaciari@gmail.com>
2024-11-28 02:30:30 -0300

    fix(net/tls) fix reusePort, allowHalfOpen, FIN before reconnect (#15452)
    Co-authored-by: Jarred Sumner <jarred@jarredsumner.com>

--- test/js/node/test/parallel/net-bind-twice-reuseport.test.js
+++ test/js/node/test/parallel/net-bind-twice-reuseport.test.js
@@ -2,27 +2,38 @@
 //#SHA1: 432eb9529d0affc39c8af9ebc1147528d96305c9
 //-----------------
 "use strict";
-const net = require("net");
 
-test("net.Server should not allow binding to the same port twice", done => {
+import { test } from "bun:test";
+import net from "node:net";
+import { isWindows } from "harness";
+
+test.skipIf(isWindows)("net.Server should not allow binding to the same port twice", done => {
   const server1 = net.createServer(() => {
     throw new Error("Server1 should not receive connections");
   });
 
-  server1.listen(0, "127.0.0.1", () => {
+  const options = {
+    reusePort: true,
+    port: 0,
+    host: "127.0.0.1",
+  };
+  server1.listen(options, () => {
     const server2 = net.createServer(() => {
       throw new Error("Server2 should not receive connections");
     });
 
     const port = server1.address().port;
-    server2.listen(port, "127.0.0.1", () => {
-      throw new Error("Server2 should not be able to listen");
+    server2.listen({ ...options, port }, () => {
+      server2.close(() => {
+        server1.close(() => {
+          done();
+        });
+      });
     });
 
     server2.on("error", e => {
-      expect(e.code).toBe("EADDRINUSE");
       server1.close(() => {
-        done();
+        done(e);
       });
     });
   });

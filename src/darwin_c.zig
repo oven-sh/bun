5c420d972f3d3314631c3c7fb5a43a33553ec9ac
Jarred Sumner <jarred@jarredsumner.com>
2024-12-02 16:36:34 +0100

    Fix

--- src/darwin_c.zig
+++ src/darwin_c.zig
@@ -74,16 +74,16 @@ pub extern "c" fn fclonefileat(c_int, c_int, [*:0]const u8, uint32_t: c_int) c_i
 pub extern "c" fn clonefile(src: [*:0]const u8, dest: [*:0]const u8, flags: c_int) c_int;
 
 pub const lstat = blk: {
-    const T = *const fn ([*c]const u8, [*c]std.c.stat) callconv(.C) c_int;
+    const T = *const fn ([*c]const u8, [*c]std.c.Stat) callconv(.C) c_int;
     break :blk @extern(T, .{ .name = "lstat64" });
 };
 
 pub const fstat = blk: {
-    const T = *const fn ([*c]const u8, [*c]std.c.stat) callconv(.C) c_int;
+    const T = *const fn ([*c]const u8, [*c]std.c.Stat) callconv(.C) c_int;
     break :blk @extern(T, .{ .name = "fstat64" });
 };
 pub const stat = blk: {
-    const T = *const fn ([*c]const u8, [*c]std.c.stat) callconv(.C) c_int;
+    const T = *const fn ([*c]const u8, [*c]std.c.Stat) callconv(.C) c_int;
     break :blk @extern(T, .{ .name = "stat64" });
 };
 

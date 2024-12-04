351eda5c4e76fc1eff3c9c2fdfe2224c39ad5e73
Ben Grant <ben@bun.sh>
2024-12-04 13:12:52 -0800

    Release heap access while waiting for events (the change from #14996)

--- src/bun.js/bindings/BunJSCEventLoop.cpp
+++ src/bun.js/bindings/BunJSCEventLoop.cpp
@@ -5,19 +5,17 @@
 
 extern "C" int Bun__JSC_onBeforeWait(JSC::VM* vm)
 {
-    UNUSED_PARAM(vm);
     // TODO: use JSC timers, run the incremental sweeper.
     // That will fix this.
     // In the meantime, we're disabling this due to https://github.com/oven-sh/bun/issues/14982
-    // if (vm->heap.hasAccess()) {
-    //     vm->heap.releaseAccess();
-    //     return 1;
-    // }
+    if (vm->heap.hasAccess()) {
+        vm->heap.releaseAccess();
+        return 1;
+    }
     return 0;
 }
 
 extern "C" void Bun__JSC_onAfterWait(JSC::VM* vm)
 {
-    UNUSED_PARAM(vm);
-    // vm->heap.acquireAccess();
+    vm->heap.acquireAccess();
 }

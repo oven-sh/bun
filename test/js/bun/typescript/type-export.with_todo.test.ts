a0586da1a36d91f051b24ddf47c0d0dd20bfa5f8
pfg <pfg@pfg.pw>
2024-12-06 18:13:36 -0800

    add a test demonstrating the re-export problem

---
+++ test/js/bun/typescript/type-export.with_todo.test.ts
@@ -0,0 +1,88 @@
+import { describe, test, expect } from "bun:test";
+import { bunEnv, tempDirWithFiles } from "harness";
+
+/*
+Potential solutions:
+- Option 1: Make a fake export `export const my_string = undefined;` and make sure it is not enumerable
+- Option 2: In b.ts, make javascriptcore skip re-exporting something if it is not found rather than SyntaxErroring
+  - this won't work because in the import {} export {} case, the error will be on the import
+*/
+
+const a_file = `
+  export type my_string = "1";
+
+  export type my_value = "2";
+  export const my_value = "2";
+
+  export const my_only = "3";
+`;
+const b_files = [
+  {
+    name: "export from",
+    value: `export { my_string, my_value, my_only } from "./a.ts";`,
+  },
+  {
+    name: "import then export",
+    value: `
+      import { my_string, my_value, my_only } from "./a.ts";
+      export { my_string, my_value, my_only };
+    `,
+  },
+  {
+    name: "export star",
+    value: `export * from "./a.ts";`,
+  },
+];
+const c_files = [
+  { name: "require", value: `console.log(JSON.stringify(require("./b")));` },
+  { name: "import star", value: `import * as b from "./b"; console.log(JSON.stringify(b));` },
+  { name: "await import", value: `console.log(JSON.stringify(await import("./b")));` },
+  {
+    name: "import individual",
+    value: `
+      import { my_string, my_value, my_only } from "./b";
+      console.log(JSON.stringify({ my_only, my_value }));
+    `,
+  },
+];
+for (const b_file of b_files) {
+  describe(`re-export with ${b_file.name}`, () => {
+    for (const c_file of c_files) {
+      describe(`import with ${c_file.name}`, () => {
+        const dir = tempDirWithFiles("type-export", {
+          "a.ts": a_file,
+          "b.ts": b_file.value,
+          "c.ts": c_file.value,
+        });
+
+        const runAndVerify = (filename: string) => {
+          const result = Bun.spawnSync({
+            cmd: ["bun", "run", filename],
+            cwd: dir,
+            env: bunEnv,
+            stdio: ["inherit", "pipe", "inherit"],
+          });
+
+          expect(result.exitCode).toBe(0);
+          expect(JSON.parse(result.stdout.toString().trim())).toEqual({ my_value: "2", my_only: "3" });
+        };
+
+        test.todoIf(b_file.name !== "export star")("run", () => {
+          runAndVerify("c.ts");
+        });
+
+        test("build", async () => {
+          const result = Bun.spawnSync({
+            cmd: ["bun", "build", "--target=bun", "--outfile", "bundle.js", "c.ts"],
+            cwd: dir,
+            env: bunEnv,
+            stdio: ["inherit", "inherit", "inherit"],
+          });
+
+          expect(result.exitCode).toBe(0);
+          runAndVerify("bundle.js");
+        });
+      });
+    }
+  });
+}

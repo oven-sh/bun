04f696449660932af0b49f9a3d4361bfa0390971
Jarred Sumner <jarred@jarredsumner.com>
2024-12-07 15:30:08 +0100

    [sudo] build images

--- scripts/create-nix-amis.mjs
+++ scripts/create-nix-amis.mjs
@@ -69,7 +69,7 @@ echo "buildkite-agent ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/buildkite-agent
 echo "Installing BuildKite agent..."
 
 # Install BuildKite agent
-sh -c 'echo deb https://apt.buildkite.com/buildkite-agent stable main > /etc/apt/sources.list.d/buildkite-agent.list'
+sudo sh -c 'echo deb https://apt.buildkite.com/buildkite-agent stable main > /etc/apt/sources.list.d/buildkite-agent.list'
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 32A37959C2FA5C3C99EFBC32A79206696452D198
 sudo apt-get update
 sudo apt-get install -y buildkite-agent

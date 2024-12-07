257b2e77ff6672932a928c9913f05654e5f88b87
Kai Tamkun <kai@tamkun.io>
2024-12-06 19:41:06 -0800

    Don't use timeouts in stream tests

--- test/js/node/stream/node-stream.test.js
+++ test/js/node/stream/node-stream.test.js
@@ -550,19 +550,29 @@ for (const size of [0x10, 0xffff, 0x10000, 0x1f000, 0x20000, 0x20010, 0x7ffff, 0
     const path = `${tmpdir()}/${Date.now()}.readable_and_close.txt`;
     writeFileSync(path, new Uint8Array(size));
     const stream = createReadStream(path);
-    const close_cb = jest.fn();
-    const null_read = jest.fn();
-    stream.on("close", close_cb);
+
+    let closes = 0;
+    let null_reads = 0;
+
+    stream.on("close", () => {
+      ++closes;
+      expect(closes).toBe(1);
+      expect(null_reads).toBeLessThanOrEqual(1);
+      if (null_reads >= 1) {
+        done();
+      }
+    });
+
     stream.on("readable", () => {
       const data = stream.read();
       if (data === null) {
-        null_read();
+        ++null_reads;
+        expect(null_reads).toBe(1);
+        expect(closes).toBeLessThanOrEqual(1);
+        if (closes >= 1) {
+          done();
+        }
       }
     });
-    setTimeout(() => {
-      expect(close_cb).toHaveBeenCalledTimes(1);
-      expect(null_read).toHaveBeenCalledTimes(1);
-      done();
-    }, 20);
   });
 }

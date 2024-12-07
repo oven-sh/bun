c57cf76f1745f3ca6e395ab9ae87d89d364ca0c1
pfg <pfg@pfg.pw>
2024-12-06 19:48:17 -0800

    {todo: true}

--- test/js/bun/typescript/type-export.test.ts
+++ test/js/bun/typescript/type-export.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from "bun:test";
+import { describe, test, expect } from "bun:test" with { todo: "true" };
 import { bunEnv, tempDirWithFiles } from "harness";
 
 /*

9ed3c07a79c51d9a185dfdc1da2e6723ed74b6db
Ben Grant <ben@bun.sh>
2024-12-10 12:08:17 -0800

    Add test for @napi-rs/canvas

---
+++ test/js/third_party/@napi-rs/canvas/napi-rs-canvas.test.ts
@@ -0,0 +1,24 @@
+// Create an image, then print it as binary to stdout
+import { createCanvas, loadImage } from "@napi-rs/canvas";
+import { Jimp } from "jimp";
+
+describe("@napi-rs/canvas", () => {
+  it("produces correct output", async () => {
+    const canvas = createCanvas(200, 200);
+    const ctx = canvas.getContext("2d");
+
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "red";
+    ctx.fillStyle = "blue";
+
+    ctx.fillRect(0, 0, 200, 200);
+    ctx.strokeRect(50, 50, 100, 100);
+
+    const image = await loadImage("icon-small.png");
+    ctx.drawImage(image, 0, 0);
+
+    const expected = await Jimp.read("expected.png");
+    const actual = await Jimp.read(await canvas.encode("png"));
+    expect(Array.from(actual.bitmap.data)).toEqual(Array.from(expected.bitmap.data));
+  });
+});

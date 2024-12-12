3f9f08d4bfb8fca6bef2caae9f54206c4c3328aa
pfg <pfg@pfg.pw>
2024-12-11 19:04:53 -0800

    pass test-console-tty-colors.js in ci

--- test/js/node/test/parallel/test-console-tty-colors.js
+++ test/js/node/test/parallel/test-console-tty-colors.js
@@ -1,4 +1,7 @@
 'use strict';
+// ci sets process.env["FORCE_COLOR"], which makes the test fail in both node and bun
+delete process.env["FORCE_COLOR"];
+
 const common = require('../common');
 const assert = require('assert');
 const util = require('util');

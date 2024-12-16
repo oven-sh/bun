01f162d7ed6bed6f4b3cc53eb7c1a32534391617
Ben Grant <ben@bun.sh>
2024-12-16 10:44:32 -0800

    Load promisified timer functions immediately

--- src/js/internal/promisify.ts
+++ src/js/internal/promisify.ts
@@ -77,24 +77,15 @@ promisify.custom = kCustomPromisifiedSymbol;
   const { setTimeout: timeout, setImmediate: immediate, setInterval: interval } = globalThis;
 
   if (timeout && $isCallable(timeout)) {
-    defineCustomPromisify(timeout, function setTimeout(arg1) {
-      const fn = defineCustomPromisify(timeout, require("node:timers/promises").setTimeout);
-      return fn.$apply(this, arguments);
-    });
+    defineCustomPromisify(timeout, require("node:timers/promises").setTimeout);
   }
 
   if (immediate && $isCallable(immediate)) {
-    defineCustomPromisify(immediate, function setImmediate(arg1) {
-      const fn = defineCustomPromisify(immediate, require("node:timers/promises").setImmediate);
-      return fn.$apply(this, arguments);
-    });
+    defineCustomPromisify(immediate, require("node:timers/promises").setImmediate);
   }
 
   if (interval && $isCallable(interval)) {
-    defineCustomPromisify(interval, function setInterval(arg1) {
-      const fn = defineCustomPromisify(interval, require("node:timers/promises").setInterval);
-      return fn.$apply(this, arguments);
-    });
+    defineCustomPromisify(interval, require("node:timers/promises").setInterval);
   }
 }
 

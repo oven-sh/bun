0f6343f0a012108e1ce4db4d4643620a38fea0aa
dave caruso <me@paperdave.net>
2024-12-18 14:48:50 -0800

    leak test

--- test/internal/bindgen.test.ts
+++ test/internal/bindgen.test.ts
@@ -1,5 +1,6 @@
 import { it, expect } from "bun:test";
 import { bindgen } from "bun:internal-for-testing";
+import { simpleMemoryLeakChecker } from "harness";
 
 it("bindgen add example", () => {
   // Simple cases
@@ -93,37 +94,6 @@ it("custom deserializers / zigEnum / StringOrBuffer / ArrayBuffer", () => {
     message: 'The "e" argument must be an instance of Buffer, TypedArray, or DataView. Received "hi"',
   });
 });
-
-/**
- * Given `run`, which allocates and frees memory, run it many times and observe
- * if memory increases.
- *
- * Done by sampling the memory usage increase of a couple of warmup runs, and
- * then runs the sampling runs, with a GC call in between.
- */
-function simpleMemoryLeakChecker(options: { samples: number; preSamples?: number; run: () => void }) {
-  Bun.gc(true);
-  const memory = process.memoryUsage().rss;
-  Bun.gc(true);
-
-  const { preSamples = 2 } = options;
-  let threshold = 0;
-  for (let i = 0; i < preSamples; i++) {
-    options.run();
-    const firstRun = process.memoryUsage().rss;
-    threshold = Math.max(threshold, firstRun);
-  }
-  Bun.gc(true);
-
-  for (let i = 0; i < options.samples; i++) {
-    options.run();
-    Bun.gc(true);
-  }
-  Bun.gc(true);
-
-  const memoryEnd = process.memoryUsage().rss;
-  expect(memoryEnd, `Memory leak of ${((memoryEnd - memory) / 1024 / 1024).toFixed(2)}mb`).toBeLessThan(threshold);
-}
 
 it("returned bun string gets dereferenced", () => {
   const referenceString = bindgen.returnBunString(32 * 1000 * 1000);

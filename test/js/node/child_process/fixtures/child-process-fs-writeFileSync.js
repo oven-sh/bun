8d55733e8b8c3e5907e14ffb65477ab0f43c2cfd
Don Isaac <don@bun.sh>
2024-12-19 14:25:20 -0800

    fix filename

--- test/js/node/child_process/fixtures/child-process-fs-writeFileSync.js
+++ test/js/node/child_process/fixtures/child-process-fs-writeFileSync.js
@@ -3,6 +3,9 @@
  * @see fs-writeFile-child-process.test.ts
  */
 const { writeFileSync } = require("node:fs");
+const assert = require("assert");
 
+const filename = __filename.split("/").pop();
+assert(filename && filename.endsWith("writeFileSync.js"));
 writeFileSync(__filename, "please don't override this source file");
 process.stdout.write(process.cwd());

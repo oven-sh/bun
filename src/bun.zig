826501c3a3a5f5cb0d9f5891e63db18ac360fe4f
Jarred Sumner <jarred@jarredsumner.com>
2024-12-25 15:20:21 -0800

    Update bun.zig

--- src/bun.zig
+++ src/bun.zig
@@ -4153,13 +4153,7 @@ pub const StackCheck = struct {
         const remaining_stack = switch (builtin.os.tag) {
             .macos, .ios => stack_ptr -| this.cached_stack_end,
             .linux => stack_ptr -| this.cached_stack_end,
-
-            // Reverse the operands for Windows
-            .windows => if (stack_ptr <= this.cached_stack_end)
-                return false
-            else
-                stack_ptr - this.cached_stack_end,
-
+            .windows => stack_ptr -| this.cached_stack_end,
             else => @compileError("Unsupported platform"),
         };
         return remaining_stack > 1024 * 128;

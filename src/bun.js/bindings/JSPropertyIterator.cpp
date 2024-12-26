1cf8c1621e4fbb11af2176efe951ef767393cacd
Dave Caruso <me@paperdave.net>
2024-12-25 21:16:19 -0800

    Allow iterating through process.env specifically

--- src/bun.js/bindings/JSPropertyIterator.cpp
+++ src/bun.js/bindings/JSPropertyIterator.cpp
@@ -1,7 +1,8 @@
-
-
 #include "root.h"
 
+#include "BunClientData.h"
+#include "ZigGlobalObject.h"
+#include "JavaScriptCore/JSType.h"
 #include "JavaScriptCore/EnumerationMode.h"
 #include "JavaScriptCore/ExceptionScope.h"
 #include "JavaScriptCore/ThrowScope.h"
@@ -26,6 +27,7 @@ public:
 
     RefPtr<JSC::PropertyNameArrayData> properties;
     Ref<JSC::VM> vm;
+    bool isSpecialProxy = false;
     static JSPropertyIterator* create(JSC::VM& vm, RefPtr<JSC::PropertyNameArrayData> data)
     {
         return new JSPropertyIterator(vm, data);
@@ -42,6 +44,32 @@ extern "C" JSPropertyIterator* Bun__JSPropertyIterator__create(JSC::JSGlobalObje
 
     auto scope = DECLARE_THROW_SCOPE(vm);
     JSC::PropertyNameArray array(vm, PropertyNameMode::StringsAndSymbols, PrivateSymbolMode::Exclude);
+
+#if OS(WINDOWS)
+    if (UNLIKELY(object->type() == JSC::ProxyObjectType)) {
+        // Check if we're actually iterating through the JSEnvironmentVariableMap's proxy.
+        auto* zigGlobal = defaultGlobalObject(globalObject);
+        if (zigGlobal->m_processEnvObject.isInitialized()) {
+            if (object == zigGlobal->m_processEnvObject.get(zigGlobal)) {
+                object->methodTable()->getOwnPropertyNames(
+                    object,
+                    globalObject,
+                    array,
+                    DontEnumPropertiesMode::Exclude);
+                RETURN_IF_EXCEPTION(scope, nullptr);
+
+                *count = array.size();
+                if (array.size() == 0) {
+                    return nullptr;
+                }
+
+                auto* iter = JSPropertyIterator::create(vm, array.releaseData());
+                iter->isSpecialProxy = true;
+                return iter;
+            }
+        }
+    }
+#endif
 
     if (own_properties_only) {
         if (only_non_index_properties) {
@@ -74,9 +102,32 @@ extern "C" size_t Bun__JSPropertyIterator__getLongestPropertyName(JSPropertyIter
     return longest;
 }
 
+static EncodedJSValue getOwnProxyObject(JSPropertyIterator* iter, JSObject* object, const JSC::Identifier& prop, BunString* propertyName)
+{
+    auto& vm = iter->vm;
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    PropertySlot slot(object, PropertySlot::InternalMethodType::GetOwnProperty, nullptr);
+    auto* globalObject = object->globalObject();
+    if (!object->methodTable()->getOwnPropertySlot(object, globalObject, prop, slot)) {
+        return {};
+    }
+    RETURN_IF_EXCEPTION(scope, {});
+
+    JSValue result = slot.getValue(globalObject, prop);
+    RETURN_IF_EXCEPTION(scope, {});
+
+    *propertyName = Bun::toString(prop.impl());
+    return JSValue::encode(result);
+}
+
 extern "C" EncodedJSValue Bun__JSPropertyIterator__getNameAndValue(JSPropertyIterator* iter, JSC::JSGlobalObject* globalObject, JSC::JSObject* object, BunString* propertyName, size_t i)
 {
     const auto& prop = iter->properties->propertyNameVector()[i];
+    if (UNLIKELY(iter->isSpecialProxy)) {
+        return getOwnProxyObject(iter, object, prop, propertyName);
+    }
+
     auto& vm = iter->vm;
     auto scope = DECLARE_THROW_SCOPE(vm);
     PropertySlot slot(object, PropertySlot::InternalMethodType::GetOwnProperty);
@@ -95,8 +146,12 @@ extern "C" EncodedJSValue Bun__JSPropertyIterator__getNameAndValue(JSPropertyIte
 extern "C" EncodedJSValue Bun__JSPropertyIterator__getNameAndValueNonObservable(JSPropertyIterator* iter, JSC::JSGlobalObject* globalObject, JSC::JSObject* object, BunString* propertyName, size_t i)
 {
     const auto& prop = iter->properties->propertyNameVector()[i];
+    if (UNLIKELY(iter->isSpecialProxy)) {
+        return getOwnProxyObject(iter, object, prop, propertyName);
+    }
     auto& vm = iter->vm;
     auto scope = DECLARE_THROW_SCOPE(vm);
+
     PropertySlot slot(object, PropertySlot::InternalMethodType::VMInquiry, vm.ptr());
     if (!object->getNonIndexPropertySlot(globalObject, prop, slot)) {
         return {};

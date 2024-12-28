a601bd8dea937bfdc43bd11355c69ab0d6bfa327
Ciro Spaciari <ciro.spaciari@gmail.com>
2024-12-27 19:52:04 -0800

    more tests

--- test/js/bun/s3/s3.test.ts
+++ test/js/bun/s3/s3.test.ts
@@ -12,9 +12,16 @@ const s3Options: S3FileOptions = {
 
 const S3Bucket = getSecret("S3_R2_BUCKET");
 
+function makePayLoadFrom(text: string, size: number): string {
+  while (Buffer.byteLength(text) < size) {
+    text += text;
+  }
+  return text.slice(0, size);
+}
+
 // 15 MiB big enough to Multipart upload in more than one part
-const bigPayload = Buffer.alloc(15 * 1024 * 1024, "a");
-const bigishPayload = Buffer.alloc(1 * 1024 * 1024, "a");
+const bigPayload = makePayLoadFrom("Bun is the best runtime ever", 15 * 1024 * 1024);
+const bigishPayload = makePayLoadFrom("Bun is the best runtime ever", 1 * 1024 * 1024);
 
 describe.skipIf(!s3Options.accessKeyId)("s3", () => {
   for (let bucketInName of [true, false]) {
@@ -207,14 +214,15 @@ describe.skipIf(!s3Options.accessKeyId)("s3", () => {
               }),
               options,
             );
-            expect(await S3.size(tmp_filename, options)).toBe(bigPayload.byteLength);
+            expect(await S3.size(tmp_filename, options)).toBe(Buffer.byteLength(bigPayload));
           }
         }, 10_000);
 
         it("should be able to upload large files in one go using S3.upload", async () => {
           {
             await S3.upload(tmp_filename, bigPayload, options);
-            expect(await S3.size(tmp_filename, options)).toBe(bigPayload.byteLength);
+            expect(await S3.size(tmp_filename, options)).toBe(Buffer.byteLength(bigPayload));
+            expect(await new S3(tmp_filename, options).text()).toBe(bigPayload);
           }
         }, 10_000);
 
@@ -222,7 +230,8 @@ describe.skipIf(!s3Options.accessKeyId)("s3", () => {
           {
             const s3File = new S3(tmp_filename, options);
             await s3File.write(bigPayload);
-            expect(await s3File.size).toBe(bigPayload.byteLength);
+            expect(await s3File.size).toBe(Buffer.byteLength(bigPayload));
+            expect(await s3File.text()).toBe(bigPayload);
           }
         }, 10_000);
       });
@@ -295,7 +304,8 @@ describe.skipIf(!s3Options.accessKeyId)("s3", () => {
         it("should be able to upload large files in one go using Bun.write", async () => {
           {
             await Bun.write(file(tmp_filename, options), bigPayload);
-            expect(await S3.size(tmp_filename, options)).toBe(bigPayload.byteLength);
+            expect(await S3.size(tmp_filename, options)).toBe(Buffer.byteLength(bigPayload));
+            expect(await file(tmp_filename, options).text()).toEqual(bigPayload);
           }
         }, 15_000);
 
@@ -303,7 +313,8 @@ describe.skipIf(!s3Options.accessKeyId)("s3", () => {
           {
             const s3File = file(tmp_filename, options);
             await s3File.write(bigPayload);
-            expect(await s3File.size).toBe(bigPayload.byteLength);
+            expect(await s3File.size).toBe(Buffer.byteLength(bigPayload));
+            expect(await s3File.text()).toBe(bigPayload);
           }
         }, 10_000);
       });
@@ -383,14 +394,15 @@ describe.skipIf(!s3Options.accessKeyId)("s3", () => {
         it("should be able to upload large files in one go using S3.upload", async () => {
           {
             await S3.upload(s3(tmp_filename, options), bigPayload);
-            expect(await S3.size(tmp_filename, options)).toBe(bigPayload.byteLength);
+            expect(await S3.size(tmp_filename, options)).toBe(Buffer.byteLength(bigPayload));
           }
         }, 10_000);
 
         it("should be able to upload large files in one go using Bun.write", async () => {
           {
             await Bun.write(s3(tmp_filename, options), bigPayload);
-            expect(await S3.size(tmp_filename, options)).toBe(bigPayload.byteLength);
+            expect(await S3.size(tmp_filename, options)).toBe(Buffer.byteLength(bigPayload));
+            expect(await s3(tmp_filename, options).text()).toBe(bigPayload);
           }
         }, 10_000);
 
@@ -398,7 +410,8 @@ describe.skipIf(!s3Options.accessKeyId)("s3", () => {
           {
             const s3File = s3(tmp_filename, options);
             await s3File.write(bigPayload);
-            expect(await s3File.size).toBe(bigPayload.byteLength);
+            expect(await s3File.size).toBe(Buffer.byteLength(bigPayload));
+            expect(await s3File.text()).toBe(bigPayload);
           }
         }, 10_000);
 
@@ -415,12 +428,17 @@ describe.skipIf(!s3Options.accessKeyId)("s3", () => {
             const stream = s3file.stream();
             const reader = stream.getReader();
             let bytes = 0;
+            let chunks: Array<Buffer> = [];
+
             while (true) {
               const { done, value } = await reader.read();
               if (done) break;
               bytes += value?.length ?? 0;
+
+              if (value) chunks.push(value as Buffer);
             }
             expect(bytes).toBe(10);
+            expect(Buffer.concat(chunks)).toEqual(Buffer.from("Hello Bun!"));
           });
 
           it("should work with large files", async () => {
@@ -429,15 +447,156 @@ describe.skipIf(!s3Options.accessKeyId)("s3", () => {
             const stream = s3file.stream();
             const reader = stream.getReader();
             let bytes = 0;
+            let chunks: Array<Buffer> = [];
             while (true) {
               const { done, value } = await reader.read();
               if (done) break;
               bytes += value?.length ?? 0;
+              if (value) chunks.push(value as Buffer);
             }
-            expect(bytes).toBe(bigishPayload.byteLength);
+            expect(bytes).toBe(Buffer.byteLength(bigishPayload));
+            expect(Buffer.concat(chunks).toString()).toBe(bigishPayload);
           }, 20_000);
         });
       });
     });
   }
+
+  describe("credentials", () => {
+    it("should error with invalid access key id", async () => {
+      [s3, (...args) => new S3(...args), file].forEach(fn => {
+        const s3file = fn("s3://bucket/credentials-test", {
+          ...s3Options,
+          accessKeyId: "invalid",
+        });
+        expect(s3file.write("Hello Bun!")).rejects.toThrow(/Access Key Id| AccessKeyId/gim);
+      });
+    });
+    it("should error with invalid secret key id", async () => {
+      [s3, (...args) => new S3(...args), file].forEach(fn => {
+        const s3file = fn("s3://bucket/credentials-test", {
+          ...s3Options,
+          secretAccessKey: "invalid",
+        });
+        expect(s3file.write("Hello Bun!")).rejects.toThrow(/key/gim);
+      });
+    });
+
+    it("should error with invalid endpoint", async () => {
+      [s3, (...args) => new S3(...args), file].forEach(fn => {
+        const s3file = fn("s3://bucket/credentials-test", {
+          ...s3Options,
+          endpoint: "🙂.🥯",
+        });
+        expect(s3file.write("Hello Bun!")).rejects.toThrow();
+      });
+    });
+
+    it("should error with invalid endpoint", async () => {
+      [s3, (...args) => new S3(...args), file].forEach(fn => {
+        const s3file = fn("s3://bucket/credentials-test", {
+          ...s3Options,
+          endpoint: "..asd.@%&&&%%",
+        });
+        expect(s3file.write("Hello Bun!")).rejects.toThrow();
+      });
+    });
+
+    it("should error with invalid bucket", async () => {
+      [s3, (...args) => new S3(...args), file].forEach(fn => {
+        const s3file = fn("s3://credentials-test", {
+          ...s3Options,
+          bucket: "invalid",
+        });
+        expect(s3file.write("Hello Bun!")).rejects.toThrow(/bucket/);
+      });
+    });
+  });
+
+  describe("S3 static methods", () => {
+    describe("presign", () => {
+      it("should work", async () => {
+        const s3file = s3("s3://bucket/credentials-test", s3Options);
+        const url = s3file.presign();
+        expect(url).toBeDefined();
+        expect(url.includes("X-Amz-Expires=86400")).toBe(true);
+        expect(url.includes("X-Amz-Date")).toBe(true);
+        expect(url.includes("X-Amz-Signature")).toBe(true);
+        expect(url.includes("X-Amz-Credential")).toBe(true);
+        expect(url.includes("X-Amz-Algorithm")).toBe(true);
+        expect(url.includes("X-Amz-SignedHeaders")).toBe(true);
+      });
+      it("should work with expires", async () => {
+        const s3file = s3("s3://bucket/credentials-test", s3Options);
+        const url = s3file.presign({
+          expiresIn: 10,
+        });
+        expect(url).toBeDefined();
+        expect(url.includes("X-Amz-Expires=10")).toBe(true);
+        expect(url.includes("X-Amz-Date")).toBe(true);
+        expect(url.includes("X-Amz-Signature")).toBe(true);
+        expect(url.includes("X-Amz-Credential")).toBe(true);
+        expect(url.includes("X-Amz-Algorithm")).toBe(true);
+        expect(url.includes("X-Amz-SignedHeaders")).toBe(true);
+      });
+
+      it("S3.presign should work", async () => {
+        const url = S3.presign("s3://bucket/credentials-test", {
+          ...s3Options,
+          expiresIn: 10,
+        });
+        expect(url).toBeDefined();
+        expect(url.includes("X-Amz-Expires=10")).toBe(true);
+        expect(url.includes("X-Amz-Date")).toBe(true);
+        expect(url.includes("X-Amz-Signature")).toBe(true);
+        expect(url.includes("X-Amz-Credential")).toBe(true);
+        expect(url.includes("X-Amz-Algorithm")).toBe(true);
+        expect(url.includes("X-Amz-SignedHeaders")).toBe(true);
+      });
+
+      it("S3.presign endpoint should work", async () => {
+        const url = S3.presign("s3://bucket/credentials-test", {
+          ...s3Options,
+          expiresIn: 10,
+          endpoint: "https://s3.bun.sh",
+        });
+        expect(url).toBeDefined();
+        expect(url.includes("https://s3.bun.sh")).toBe(true);
+        expect(url.includes("X-Amz-Expires=10")).toBe(true);
+        expect(url.includes("X-Amz-Date")).toBe(true);
+        expect(url.includes("X-Amz-Signature")).toBe(true);
+        expect(url.includes("X-Amz-Credential")).toBe(true);
+        expect(url.includes("X-Amz-Algorithm")).toBe(true);
+        expect(url.includes("X-Amz-SignedHeaders")).toBe(true);
+      });
+
+      it("S3.presign endpoint should work", async () => {
+        const url = S3.presign("s3://folder/credentials-test", {
+          ...s3Options,
+          expiresIn: 10,
+          bucket: "my-bucket",
+        });
+        expect(url).toBeDefined();
+        expect(url.includes("my-bucket")).toBe(true);
+        expect(url.includes("X-Amz-Expires=10")).toBe(true);
+        expect(url.includes("X-Amz-Date")).toBe(true);
+        expect(url.includes("X-Amz-Signature")).toBe(true);
+        expect(url.includes("X-Amz-Credential")).toBe(true);
+        expect(url.includes("X-Amz-Algorithm")).toBe(true);
+        expect(url.includes("X-Amz-SignedHeaders")).toBe(true);
+      });
+    });
+
+    it("exists, upload, size, unlink should work", async () => {
+      const filename = randomUUID();
+      const fullPath = `s3://${S3Bucket}/${filename}`;
+      expect(await S3.exists(fullPath, s3Options)).toBe(false);
+
+      await S3.upload(fullPath, "bun", s3Options);
+      expect(await S3.exists(fullPath, s3Options)).toBe(true);
+      expect(await S3.size(fullPath, s3Options)).toBe(3);
+      await S3.unlink(fullPath, s3Options);
+      expect(await S3.exists(fullPath, s3Options)).toBe(false);
+    });
+  });
 });

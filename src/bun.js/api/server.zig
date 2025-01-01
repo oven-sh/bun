19191659cfa9403ce186c2616024cff9fe0a76ba
Jarred Sumner <jarred@jarredsumner.com>
2024-12-31 19:48:33 -0800

    Avoid resolving substrings in bun:sqlite and Buffer.byteLength (#16092)

--- src/bun.js/api/server.zig
+++ src/bun.js/api/server.zig
@@ -5239,10 +5239,17 @@ pub const ServerWebSocket = struct {
             return globalThis.throw("sendText expects a string", .{});
         }
 
-        var string_slice = message_value.toSlice(globalThis, bun.default_allocator);
-        defer string_slice.deinit();
+        var js_string = message_value.toString(globalThis);
+        if (globalThis.hasException()) {
+            return .zero;
+        }
+        const view = js_string.view(globalThis);
+        const slice = view.toSlice(bun.default_allocator);
+        defer slice.deinit();
 
-        const buffer = string_slice.slice();
+        defer js_string.ensureStillAlive();
+
+        const buffer = slice.slice();
         switch (this.websocket().send(buffer, .text, compress, true)) {
             .backpressure => {
                 log("sendText() backpressure ({d} bytes string)", .{buffer.len});

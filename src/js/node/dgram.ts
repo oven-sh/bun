f03ec3d6af17c3e6baefb332e19303398d3e3ce8
Kai Tamkun <kai@tamkun.io>
2025-01-02 13:35:52 -0800

    Fix dgram socket error emission

--- src/js/node/dgram.ts
+++ src/js/node/dgram.ts
@@ -377,7 +377,7 @@ Socket.prototype.bind = function (port_, address_ /* , callback */) {
               family,
             });
           },
-          error: (_socket, error) => {
+          error: error => {
             this.emit("error", error);
           },
         },

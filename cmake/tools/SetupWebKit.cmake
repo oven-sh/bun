f5af2ccd412ba52c6eb0399d53800bd6ace0079e
Jarred Sumner <jarred@jarredsumner.com>
2025-01-03 02:46:53 -0800

    Remove spinlock in libpas on Linux

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -2,7 +2,7 @@ option(WEBKIT_VERSION "The version of WebKit to use")
 option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of downloading")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION 05798ff248070ff86118ae40583f759dbd193c6f)
+  set(WEBKIT_VERSION c30d63fe69913f17dce5fcbe17d06e670e0eaeff)
 endif()
 
 if(WEBKIT_LOCAL)

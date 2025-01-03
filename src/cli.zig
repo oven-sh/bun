1fb6030503203f3076134742f6f6771a026c027a
Mariot Chauvin <mariot.chauvin@gmail.con>
2025-01-03 16:50:27 +0000

    Add a 'ci' command alias equivalent to 'install' command with the '--frozen-lockfile' parameter

--- src/cli.zig
+++ src/cli.zig
@@ -1664,6 +1664,8 @@ pub const Command = struct {
 
                 break :brk .InstallCommand;
             },
+            RootCommandMatcher.case("ci") => .InstallCommand,
+
             RootCommandMatcher.case("c"), RootCommandMatcher.case("create") => .CreateCommand,
 
             RootCommandMatcher.case("test") => .TestCommand,

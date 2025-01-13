22436ede1278a46010c5b0953753111eb5657224
Jarred Sumner <jarred@jarredsumner.com>
2025-01-12 20:03:24 -0800

    Faster debug builds (#16354)

--- src/js/node/stream.ts
+++ src/js/node/stream.ts
@@ -5258,18 +5258,10 @@ function createNativeStreamReadable(Readable) {
   var handleNumberResult = function (nativeReadable, result, view, isClosed) {
     if (result > 0) {
       const slice = view.subarray(0, result);
-      const remainder = view.subarray(result);
+      view = slice.byteLength < view.byteLength ? view.subarray(result) : undefined;
       if (slice.byteLength > 0) {
         nativeReadable.push(slice);
       }
-
-      if (isClosed) {
-        ProcessNextTick(() => {
-          nativeReadable.push(null);
-        });
-      }
-
-      return remainder.byteLength > 0 ? remainder : undefined;
     }
 
     if (isClosed) {

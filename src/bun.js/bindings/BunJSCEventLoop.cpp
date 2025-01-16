e35e05fd9760415fa823b9d8b1e96a19e8319f61
Ben Grant <ben@bun.sh>
2025-01-16 14:43:29 -0800

    Remove TODO

--- src/bun.js/bindings/BunJSCEventLoop.cpp
+++ src/bun.js/bindings/BunJSCEventLoop.cpp
@@ -5,9 +5,6 @@
 
 extern "C" int Bun__JSC_onBeforeWait(JSC::VM* vm)
 {
-    // TODO: use JSC timers, run the incremental sweeper.
-    // That will fix this.
-    // In the meantime, we're disabling this due to https://github.com/oven-sh/bun/issues/14982
     if (vm->heap.hasAccess()) {
         vm->heap.releaseAccess();
         return 1;

9524dc9e6c1e8968ca67a369d54de4abedaecd63
Ben Grant <ben@bun.sh>
2025-01-17 14:55:20 -0800

    Bump WebKit to include USE_BUN_EVENT_LOOP

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -2,7 +2,7 @@ option(WEBKIT_VERSION "The version of WebKit to use")
 option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of downloading")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION 9e3b60e4a6438d20ee6f8aa5bec6b71d2b7d213f)
+  set(WEBKIT_VERSION fca5c31290625e0dc75991d6604daa923981c66a)
 endif()
 
 string(SUBSTRING ${WEBKIT_VERSION} 0 16 WEBKIT_VERSION_PREFIX)

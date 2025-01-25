efdf07f1f0d5608a114baeac94c2b5ed2471fe3b
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-01-24 21:20:20 -0800

    fix path

--- src/bun.js/node/path.zig
+++ src/bun.js/node/path.zig
@@ -970,25 +970,44 @@ pub fn format(globalObject: *JSC.JSGlobalObject, isWindows: bool, args_ptr: [*]J
     const allocator = stack_fallback.get();
 
     var root: []const u8 = "";
+    var root_slice: ?JSC.ZigString.Slice = null;
+    if (root_slice) |slice| slice.deinit();
+
     if (try pathObject_ptr.getTruthy(globalObject, "root")) |jsValue| {
-        root = (try jsValue.toSlice(globalObject, allocator)).slice();
+        root_slice = try jsValue.toSlice(globalObject, allocator);
+        root = root_slice.?.slice();
     }
     var dir: []const u8 = "";
+    var dir_slice: ?JSC.ZigString.Slice = null;
+    if (dir_slice) |slice| slice.deinit();
+
     if (try pathObject_ptr.getTruthy(globalObject, "dir")) |jsValue| {
-        dir = (try jsValue.toSlice(globalObject, allocator)).slice();
+        dir_slice = try jsValue.toSlice(globalObject, allocator);
+        dir = dir_slice.?.slice();
     }
     var base: []const u8 = "";
+    var base_slice: ?JSC.ZigString.Slice = null;
+    if (base_slice) |slice| slice.deinit();
+
     if (try pathObject_ptr.getTruthy(globalObject, "base")) |jsValue| {
-        base = (try jsValue.toSlice(globalObject, allocator)).slice();
+        base_slice = try jsValue.toSlice(globalObject, allocator);
+        base = base_slice.?.slice();
     }
-    // Prefix with _ to avoid shadowing the identifier in the outer scope.
     var _name: []const u8 = "";
+    var _name_slice: ?JSC.ZigString.Slice = null;
+    if (_name_slice) |slice| slice.deinit();
+
     if (try pathObject_ptr.getTruthy(globalObject, "name")) |jsValue| {
-        _name = (try jsValue.toSlice(globalObject, allocator)).slice();
+        _name_slice = try jsValue.toSlice(globalObject, allocator);
+        _name = _name_slice.?.slice();
     }
     var ext: []const u8 = "";
+    var ext_slice: ?JSC.ZigString.Slice = null;
+    if (ext_slice) |slice| slice.deinit();
+
     if (try pathObject_ptr.getTruthy(globalObject, "ext")) |jsValue| {
-        ext = (try jsValue.toSlice(globalObject, allocator)).slice();
+        ext_slice = try jsValue.toSlice(globalObject, allocator);
+        ext = ext_slice.?.slice();
     }
     return formatJS_T(u8, globalObject, allocator, isWindows, .{ .root = root, .dir = dir, .base = base, .ext = ext, .name = _name });
 }

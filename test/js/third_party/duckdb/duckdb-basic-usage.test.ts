22bd61a8694c2c35ed21b29205b7060212e36e4a
Ben Grant <ben@bun.sh>
2025-01-27 15:47:43 -0800

    Skip DuckDB test on musl

--- test/js/third_party/duckdb/duckdb-basic-usage.test.ts
+++ test/js/third_party/duckdb/duckdb-basic-usage.test.ts
@@ -1,5 +1,12 @@
+import { libcFamily } from "harness";
+if (libcFamily == "musl") {
+  // duckdb does not distribute musl binaries, so we skip this test on musl to avoid CI noise
+  process.exit(0);
+}
+
 import { describe, test, expect } from "bun:test";
-import { Database } from "duckdb";
+// Must be CJS require so that the above code can exit before we attempt to import DuckDB
+const { Database } = require("duckdb");
 
 describe("duckdb", () => {
   test("basic usage", () => {

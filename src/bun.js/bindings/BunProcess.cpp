ad0b1853759612963523eb559be7cb592ff040a7
Ben Grant <ben@bun.sh>
2025-01-27 19:11:35 -0800

    Flip assert/return_if_exception

--- src/bun.js/bindings/BunProcess.cpp
+++ src/bun.js/bindings/BunProcess.cpp
@@ -460,8 +460,8 @@ JSC_DEFINE_HOST_FUNCTION(Process_functionDlopen, (JSC::JSGlobalObject * globalOb
             auto* meta = new Bun::NapiModuleMeta(globalObject->m_pendingNapiModuleDlopenHandle);
             Bun::NapiExternal* napi_external = Bun::NapiExternal::create(vm, globalObject->NapiExternalStructure(), meta, nullptr, nullptr);
             bool success = resultObject->putDirect(vm, WebCore::builtinNames(vm).napiDlopenHandlePrivateName(), napi_external, JSC::PropertyAttribute::DontDelete | JSC::PropertyAttribute::ReadOnly);
-            RETURN_IF_EXCEPTION(scope, {});
             ASSERT(success);
+            RETURN_IF_EXCEPTION(scope, {});
         }
     }
 

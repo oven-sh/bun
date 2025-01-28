cece0361f4b213a3a0ddf66c209ba1ff7bccf349
Ben Grant <ben@bun.sh>
2025-01-28 12:11:08 -0800

    Revert releasing heap access in event loop

--- src/bun.js/bindings/BunJSCEventLoop.cpp
+++ src/bun.js/bindings/BunJSCEventLoop.cpp
@@ -5,14 +5,16 @@
 
 extern "C" int Bun__JSC_onBeforeWait(JSC::VM* vm)
 {
-    if (vm->heap.hasAccess()) {
-        vm->heap.releaseAccess();
-        return 1;
-    }
+    (void) vm;
+    // if (vm->heap.hasAccess()) {
+    //     vm->heap.releaseAccess();
+    //     return 1;
+    // }
     return 0;
 }
 
 extern "C" void Bun__JSC_onAfterWait(JSC::VM* vm)
 {
-    vm->heap.acquireAccess();
+    (void) vm;
+    // vm->heap.acquireAccess();
 }

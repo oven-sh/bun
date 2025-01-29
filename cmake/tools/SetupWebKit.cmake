450a2b4e5ead33b25b37adc6352c01f89b99e361
chloe caruso <git@paperclover.net>
2025-01-28 20:24:45 -0800

    wea

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -2,7 +2,7 @@ option(WEBKIT_VERSION "The version of WebKit to use")
 option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of downloading")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION 035bbcd99ecdda871264fbadc8fe8298f7d79054)
+  set(WEBKIT_VERSION 9b33aa6097e14d88e6bb839829cac565ae145466)
 endif()
 
 string(SUBSTRING ${WEBKIT_VERSION} 0 16 WEBKIT_VERSION_PREFIX)

57ec29d70b551c90193d3c46ada46fb6ff36ff0e
Meghan Denny <meghan@bun.sh>
2025-01-29 01:25:17 -0800

    add macos fixes to zig fork

--- cmake/tools/SetupZig.cmake
+++ cmake/tools/SetupZig.cmake
@@ -21,7 +21,7 @@ else()
 endif()
 
 optionx(ZIG_VERSION STRING "The zig version of the compiler to download" DEFAULT "0.14.0-dev.2851+b074fb7dd")
-optionx(ZIG_COMMIT STRING "The zig commit to use in oven-sh/zig" DEFAULT "b074fb7ddaefaa6ed9c5235d22b2f7378e3b80e8")
+optionx(ZIG_COMMIT STRING "The zig commit to use in oven-sh/zig" DEFAULT "561483c58cbfaff2a484ff5fd25b780f5e6e4233")
 optionx(ZIG_TARGET STRING "The zig target to use" DEFAULT ${DEFAULT_ZIG_TARGET})
 
 if(CMAKE_BUILD_TYPE STREQUAL "Release")

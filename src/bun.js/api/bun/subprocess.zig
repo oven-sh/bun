ec917d6e97de4873d1585b7d56301993f7a9d0da
pfg <pfg@pfg.pw>
2025-02-05 18:11:28 -0800

    Fix UAF in throwCommandNotFound

--- src/bun.js/api/bun/subprocess.zig
+++ src/bun.js/api/bun/subprocess.zig
@@ -2440,7 +2440,6 @@ pub const Subprocess = struct {
 
     fn throwCommandNotFound(globalThis: *JSC.JSGlobalObject, command: []const u8) bun.JSError {
         const message = bun.String.createFormat("Executable not found in $PATH: \"{s}\"", .{command}) catch bun.outOfMemory();
-        defer message.deref();
         const err = message.toZigString().toErrorInstance(globalThis);
         err.putZigString(globalThis, JSC.ZigString.static("code"), JSC.ZigString.init("ENOENT").toJS(globalThis));
         err.putZigString(globalThis, JSC.ZigString.static("path"), JSC.ZigString.init(command).toJS(globalThis));

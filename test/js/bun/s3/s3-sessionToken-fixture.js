4b22fff944aca6e73685704074155971379bf93f
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-02-06 16:38:26 -0800

    example

---
+++ test/js/bun/s3/s3-sessionToken-fixture.js
@@ -0,0 +1,47 @@
+import { S3Client, s3, randomUUIDv7 } from "bun";
+import { expect } from "bun:test";
+{
+  // custom client
+  const s3Client = new S3Client({
+    sessionToken: null,
+  });
+  const filename = randomUUIDv7().replaceAll("-", "");
+
+  const s3file = s3Client.file(filename);
+  await s3file.write("content");
+  expect(s3file.text()).toBe("content");
+  await s3file.unlink();
+}
+
+{
+  // default client
+  const filename = randomUUIDv7().replaceAll("-", "");
+
+  const s3file = await s3.file(filename, {
+    sessionToken: null,
+  });
+
+  await s3file.write("content");
+  expect(s3file.text()).toBe("content");
+  await s3file.unlink();
+}
+
+{
+  // default client but in the methods
+  const filename = randomUUIDv7().replaceAll("-", "");
+
+  const s3file = await s3.file(filename);
+
+  await s3file.write("content", {
+    sessionToken: null,
+  });
+
+  expect(
+    s3file.text({
+      sessionToken: null,
+    }),
+  ).toBe("content");
+  await s3file.unlink({
+    sessionToken: null,
+  });
+}

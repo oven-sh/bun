5dc56f5423ac4fb1276c565169665988c34faf31
RiskyMH <git@riskymh.dev>
2025-02-08 08:24:56 +0000

    more efficient

--- src/cli/pack_command.zig
+++ src/cli/pack_command.zig
@@ -1356,10 +1356,10 @@ pub const PackCommand = struct {
                         var includes: std.ArrayListUnmanaged(Pattern) = .{};
                         defer includes.deinit(ctx.allocator);
 
+                        var path_buf: PathBuffer = undefined;
                         var files_array = _files_array;
                         while (files_array.next()) |files_entry| {
                             if (files_entry.asString(ctx.allocator)) |file_entry_str| {
-                                var path_buf: PathBuffer = undefined;
                                 const normalized = bun.path.normalizeBuf(file_entry_str, &path_buf, .posix);
                                 const parsed = try Pattern.fromUTF8(ctx.allocator, try ctx.allocator.dupe(u8, normalized)) orelse continue;
                                 try includes.append(ctx.allocator, parsed);

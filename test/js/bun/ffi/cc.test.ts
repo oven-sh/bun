2e087fa2279c1ee78f95770a5ee26ff1068020d8
Don Isaac <donald.isaac@gmail.com>
2025-02-16 21:16:43 -0800

    add test cases

--- test/js/bun/ffi/cc.test.ts
+++ test/js/bun/ffi/cc.test.ts
@@ -1,6 +1,8 @@
-import { expect, it } from "bun:test";
-import { bunEnv, bunExe, isWindows } from "harness";
+import { describe, beforeAll, afterAll, expect, it } from "bun:test";
+import { bunEnv, bunExe, isWindows, tempDirWithFiles } from "harness";
 import path from "path";
+import { promises as fs } from "fs";
+import { cc, type Library } from "bun:ffi";
 
 // TODO: we need to install build-essential and Apple SDK in CI.
 // It can't find includes. It can on machines with that enabled.
@@ -13,4 +15,108 @@ it.todoIf(isWindows)("can run a .c file", () => {
   });
 
   expect(result.exitCode).toBe(0);
+});
+
+describe("given an add(a, b) function", () => {
+  const source = /* c */ `
+      int add(int a, int b) {
+        return a + b;
+      }
+    `;
+  let dir;
+
+  beforeAll(() => {
+    dir = tempDirWithFiles("bun-ffi-cc-test", {
+      "add.c": source,
+    });
+  });
+
+  afterAll(async () => {
+    await fs.rm(dir, { recursive: true, force: true });
+  });
+
+  describe("when compiled", () => {
+    let res: Library<{ add: { args: ["int", "int"]; returns: "int" } }>;
+
+    beforeAll(() => {
+      res = cc({
+        source: path.join(dir, "add.c"),
+        symbols: {
+          add: {
+            returns: "int",
+            args: ["int", "int"],
+          },
+        },
+      });
+    });
+
+    afterAll(() => {
+      // FIXME: causes a segfault
+      // res.close();
+    });
+
+    it("provides an add symbol", () => {
+      expect(res.symbols.add(1, 2)).toBe(3);
+    });
+
+    it("Only contains 1 symbol", () => {
+      expect(Object.keys(res.symbols)).toHaveLength(1);
+    });
+  }); // </when compiled>
+
+  it("when compiled with a symbol that doesn't exist, throws an error", () => {
+    expect(() => {
+      cc({
+        source: path.join(dir, "add.c"),
+        symbols: { subtract: { args: ["int", "int"], returns: "int" } },
+      });
+    }).toThrow(/"subtract" is missing/);
+  });
+
+  it("when compiled with a `void` return type instead of `int`, returns undefined", () => {
+    const ret = cc({
+      source: path.join(dir, "add.c"),
+      symbols: {
+        add: {
+          returns: "void",
+          args: ["int", "int"],
+        },
+      },
+    });
+    expect(ret.symbols.add(1, 2)).toBeUndefined();
+  });
+}); // </given add(a, b) function>
+
+describe("given a source file with syntax errors", () => {
+  const source = /* c */ `
+    int add(int a, int b) {
+      return a  b;
+    }
+  `;
+  let dir: string;
+
+  beforeAll(() => {
+    dir = tempDirWithFiles("bun-ffi-cc-test", {
+      "add.c": source,
+    });
+  });
+
+  afterAll(async () => {
+    await fs.rm(dir, { recursive: true, force: true });
+  });
+
+  // FIXME: fails asan poisoning check
+  it.skip("when compiled, throws an error", () => {
+    expect(() => {
+      cc({
+        source: path.join(dir, "add.c"),
+        symbols: {
+          add: {
+            returns: "int",
+            args: ["int", "int"],
+          },
+        },
+      });
+    }).toThrow();
+  });
 });

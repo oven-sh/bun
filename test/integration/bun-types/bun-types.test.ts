fc803cdd37de23d53717290c8292c45efab46130
Alistair Smith <hi@alistair.sh>
2025-02-17 22:55:10 +0000

    fix test script

--- test/integration/bun-types/bun-types.test.ts
+++ test/integration/bun-types/bun-types.test.ts
@@ -5,38 +5,35 @@ import { join } from "node:path";
 const BUN_REPO_ROOT = fileURLToPath(import.meta.resolve("../../../"));
 const BUN_TYPES_REPO_ROOT = join(BUN_REPO_ROOT, "packages", "bun-types");
 const FIXTURE_DIR = fileURLToPath(import.meta.resolve("./fixture"));
-
-let dir: string;
+const TSCONFIG_SOURCE_PATH = join(BUN_REPO_ROOT, "src/cli/init/tsconfig.default.json");
 
 beforeAll(async () => {
-  dir = fileURLToPath(import.meta.resolve("./fixture"));
-
   try {
     await $`
-      cd ${dir}
-      bun install
       cd ${BUN_TYPES_REPO_ROOT}
       bun install
       bun pm pack --destination ${FIXTURE_DIR}
+
       cd ${FIXTURE_DIR}
+      cp ${TSCONFIG_SOURCE_PATH} tsconfig.json
+      bun install
       bun add bun-types@bun-types-no-release.tgz
       rm bun-types-no-release.tgz
     `;
   } catch (e) {
     if (e instanceof ShellError) {
       console.log(e.stderr.toString());
-    } else {
-      console.error(e);
     }
 
-    process.exit(1);
+    throw e;
   }
 });
 
 afterAll(async () => {
   await $`
-    cd ${dir}
+    cd ${FIXTURE_DIR}
     bun uninstall bun-types
+    rm tsconfig.json
   `;
 });
 
@@ -47,7 +44,7 @@ beforeAll(() => {
 describe("@types/bun integration test", () => {
   test("it typechecks successfully", async () => {
     const p = await $`
-      cd ${dir}
+      cd ${FIXTURE_DIR}
       bun run check
     `;
 

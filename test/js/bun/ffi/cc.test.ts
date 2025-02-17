b2d4adbf2d9dda4aa738ba4df32b200f57284855
Don Isaac <donald.isaac@gmail.com>
2025-02-17 12:57:54 -0800

    more tests

--- test/js/bun/ffi/cc.test.ts
+++ test/js/bun/ffi/cc.test.ts
@@ -2,7 +2,7 @@ import { describe, beforeAll, afterAll, expect, it } from "bun:test";
 import { bunEnv, bunExe, isWindows, tempDirWithFiles } from "harness";
 import path from "path";
 import { promises as fs } from "fs";
-import { cc, type Library, CString, Pointer, ptr } from "bun:ffi";
+import { cc, type Library, type FFIFunction, CString, ptr } from "bun:ffi";
 
 // TODO: we need to install build-essential and Apple SDK in CI.
 // It can't find includes. It can on machines with that enabled.
@@ -143,54 +143,95 @@ describe("given a source file with syntax errors", () => {
   });
 });
 
-describe("given a strlen(cstring) function", () => {
-  let dir: string;
-  let strlenPath: string;
+describe.skip("given a ping(cstr) function", () => {
+  const library = makeValidCase(
+    "ping",
+    /* c */ `
+    char* ping(char* str) {
+      return str;
+    }
+  `,
+    {
+      ping: {
+        args: ["cstring"],
+        returns: "cstring",
+      },
+    },
+  );
+
+  it("given a valid CString, returns the same pointer", () => {
+    const buf = Buffer.from("hello\0");
+    const arr = new Uint8Array(buf);
+    const cstr = new CString(ptr(arr));
+
+    expect(library.symbols.ping(cstr)).toBe(cstr);
+  });
+}); // </given a ping(cstr) function>
+
+// FIXME: bus error
+describe.skip("given a strlen(cstring) function", () => {
+  const library = makeValidCase(
+    "strlen",
+    /* c */ `
+      size_t strlen(char* str) {
+        char* s = str;
+        while (*s) s++;
+        return s - str;
+      }
+    `,
+    {
+      strlen: {
+        args: ["cstring"],
+        returns: "usize",
+      },
+    },
+  );
+
+  it("given a valid CString containing 'hello', returns the correct length", () => {
+    const buf = Buffer.from("hello\0");
+    const arr = new Uint8Array(buf);
+    const cstr = new CString(ptr(arr));
+
+    expect(library.symbols.strlen(cstr)).toBe(5);
+  });
+
+  it("given a JSString, throws", () => {
+    // @ts-expect-error
+    expect(() => library.symbols.strlen("hello")).toThrow(TypeError);
+  });
+}); // </given a strlen(cstring) function>
+
+// =============================================================================
+
+function makeValidCase<Fns extends Record<string, FFIFunction>>(
+  name: string,
+  source: string,
+  symbols: Fns,
+): Library<Fns> {
+  const filename = `${name}.c`;
+
+  var library: Library<Fns>;
 
   beforeAll(() => {
-    dir = tempDirWithFiles("bun-ffi-cc-test", {
-      "strlen.c": /* c */ `
-        unsigned int strlen(char* str) {
-          char* s = str;
-          while (*s != '\0') s++;
-          return s - str;
-        }
-      `,
-    });
-    strlenPath = path.join(dir, "strlen.c");
-  });
-
-  afterAll(async () => {
-    await fs.rm(dir, { recursive: true, force: true });
-  });
-
-  describe.skip("when compiled", () => {
-    let lib: Library<{ strlen: { args: ["cstring"]; returns: "int" } }>;
-
-    beforeAll(() => {
-      lib = cc({
-        source: strlenPath,
-        symbols: {
-          strlen: {
-            args: ["cstring"],
-            // FIXME: u32, u64 fails
-            returns: "int",
-          },
-        },
+    try {
+      var dir = tempDirWithFiles(`bun-ffi-cc-${name}`, {
+        [filename]: source,
       });
-    });
 
-    afterAll(() => {
-      lib.close();
-    });
+      library = cc({
+        source: path.join(dir, filename),
+        symbols,
+      });
+    } finally {
+      // @ts-ignore -- `var` gets hoisted
+      if (dir) fs.rm(dir, { recursive: true, force: true });
+    }
+  });
 
-    it("returns the correct length", () => {
-      const buf = Buffer.from("hello\0");
-      const arr = new Uint8Array(buf);
-      const cstr = new CString(ptr(arr));
+  afterAll(() => {
+    library.close();
+  });
 
-      // @ts-ignore
-      expect(lib.symbols.strlen(cstr)).toBe(5);
-    });
-  }); // </when compiled>
-}); // </given a strlen(cstring) function>
+  // @ts-ignore
+  return library;
+}

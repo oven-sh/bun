b42e1d1ffa4d9ef9bf6679b78d33e350037ee691
Dylan Conway <dylan.conway567@gmail.com>
2025-02-17 19:31:35 -0800

    add projects

---
+++ src/create/projects/react-tailwind/src/index.tsx
@@ -0,0 +1,33 @@
+import { serve } from "bun";
+import index from "./index.html";
+
+const server = serve({
+  routes: {
+    // Serve index.html for all unmatched routes.
+    "/*": index,
+
+    "/api/hello": {
+      async GET(req) {
+        return Response.json({
+          message: "Hello, world!",
+          method: "GET",
+        });
+      },
+      async PUT(req) {
+        return Response.json({
+          message: "Hello, world!",
+          method: "PUT",
+        });
+      },
+    },
+
+    "/api/hello/:name": async (req) => {
+      const name = req.params.name;
+      return Response.json({
+        message: `Hello, ${name}!`,
+      });
+    },
+  },
+});
+
+console.log(`🚀 Server running at ${server.url}`);

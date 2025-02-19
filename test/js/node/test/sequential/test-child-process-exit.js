cff08b498492ec96d965e53ce741e6382872b9a1
pfg <pfg@pfg.pw>
2025-02-18 18:00:55 -0800

    test-child-process-execsync.js

--- test/js/node/test/sequential/test-child-process-exit.js
+++ test/js/node/test/sequential/test-child-process-exit.js
@@ -21,23 +21,39 @@
 
 'use strict';
 require('../common');
+
+// Open a chain of five Node processes each a child of the next. The final
+// process exits immediately. Each process in the chain is instructed to exit
+// when its child exits.
+// https://github.com/joyent/node/issues/1726
+
 const assert = require('assert');
-const fixtures = require('../common/fixtures');
+const ch = require('child_process');
 
-const spawn = require('child_process').spawn;
-const childPath = fixtures.path('parent-process-nonpersistent.js');
-let persistentPid = -1;
+const gen = +(process.argv[2] || 0);
+const maxGen = 5;
 
-const child = spawn(process.execPath, [ childPath ]);
 
-child.stdout.on('data', function(data) {
-  persistentPid = parseInt(data, 10);
+if (gen === maxGen) {
+  console.error('hit maxGen, exiting', maxGen);
+  return;
+}
+
+const child = ch.spawn(process.execPath, [__filename, gen + 1], {
+  stdio: [ 'ignore', 'pipe', 'ignore' ]
+});
+assert.ok(!child.stdin);
+assert.ok(child.stdout);
+assert.ok(!child.stderr);
+
+console.error('gen=%d, pid=%d', gen, process.pid);
+
+child.on('exit', function(code) {
+  console.error('exit %d from gen %d', code, gen + 1);
 });
 
-process.on('exit', function() {
-  assert.notStrictEqual(persistentPid, -1);
-  assert.throws(function() {
-    process.kill(child.pid);
-  }, /^Error: kill ESRCH$|ESRCH/);
-  process.kill(persistentPid);
+child.stdout.pipe(process.stdout);
+
+child.stdout.on('close', function() {
+  console.error('child.stdout close  gen=%d', gen);
 });

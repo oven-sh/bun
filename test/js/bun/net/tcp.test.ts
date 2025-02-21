04ac8ae9e4aa4f0b760a52521ac1b309f4f2dab2
Don Isaac <donald.isaac@gmail.com>
2025-02-21 00:45:02 -0800

    more test cases

--- test/js/bun/net/tcp.test.ts
+++ test/js/bun/net/tcp.test.ts
@@ -1,26 +1,33 @@
-import { SocketHandler } from "bun";
+import { SocketHandler, TCPSocketListener } from "bun";
 import type { Mock } from "bun:test";
-import { jest, describe, test, expect } from "bun:test";
+import { jest, afterEach, test, expect } from "bun:test";
+import jsc from "bun:jsc";
+
+const handlerNames: Set<keyof SocketHandler<any>> = new Set([
+  "open",
+  "handshake",
+  "close",
+  "error",
+  "end",
+  "data",
+  "drain",
+]);
 
 class MockSocket<Data = void> implements SocketHandler<Data> {
-  open = jest.fn();
-  handshake = jest.fn();
-  close = jest.fn();
-  error = jest.fn();
-  end = jest.fn();
-  data = jest.fn();
-  drain = jest.fn();
+  open = undefined as any;
+  handshake = undefined as any;
+  close = undefined as any;
+  error = undefined as any;
+  end = undefined as any;
+  data = undefined as any;
 
   events: string[];
 
-  constructor() {
+  constructor(impls: Partial<SocketHandler<Data>> = {}) {
     this.events = [];
-    for (const method of Object.keys(this)) {
-      if (this[method]._isMockFunction) {
-        (this[method] as Mock<any>).mockImplementation(() => {
-          this.events.push(method);
-        });
-      }
+    for (const method of handlerNames) {
+      const impl = impls[method] ? impls[method].bind(this) : () => this.events.push(method);
+      this[method] = jest.fn(impl as any);
     }
   }
 
@@ -30,11 +37,52 @@ class MockSocket<Data = void> implements SocketHandler<Data> {
         (this[method] as Mock<any>).mockClear();
       }
     }
+    this.events.length = 0;
   }
 }
 
 const nextEventLoopCycle = () => new Promise(resolve => setTimeout(resolve, 0));
 const nextTick = () => new Promise(resolve => process.nextTick(resolve));
+
+const makeClient = async <Data = unknown>(handlerOverrides?: Partial<SocketHandler<Data>>, options?) => {
+  const handler = new MockSocket<Data>(handlerOverrides);
+  const socket = await Bun.connect({
+    hostname: "localhost",
+    port: 3000,
+    ...options,
+    socket: handler,
+  });
+
+  return {
+    socket,
+    handler,
+    [Symbol.dispose]() {
+      socket[Symbol.dispose]();
+    },
+  };
+};
+
+const makeServer = <Data = unknown>(handlerOverrides?: Partial<SocketHandler<Data>>, options?) => {
+  const handler = new MockSocket<Data>(handlerOverrides);
+  const socket: TCPSocketListener<Data> = Bun.listen({
+    hostname: "localhost",
+    port: 3000,
+    ...options,
+    socket: handler,
+  });
+  return {
+    socket,
+    handler,
+    [Symbol.dispose]() {
+      socket[Symbol.dispose]();
+    },
+  };
+};
+
+afterEach(async () => {
+  await nextEventLoopCycle();
+  jsc.drainMicrotasks();
+});
 
 test("open() event timing", async () => {
   const socket = { server: new MockSocket(), client: new MockSocket() };
@@ -74,4 +122,38 @@ test("open() event timing", async () => {
   await nextEventLoopCycle();
   expect(socket.server.open).toHaveBeenCalled();
   expect(socket.client.open).toHaveBeenCalled();
+});
+
+/**
+ * Client sends FIN, so server emits `end` event. Client still reads data before
+ * closing.
+ */
+test("client writes then closes the socket", async () => {
+  using server = makeServer({
+    data(socket, data) {
+      expect(data.toString("utf8")).toBe("hello");
+      this.events.push("data");
+    },
+  });
+  using client = await makeClient();
+
+  server.socket;
+  client.socket.end("hello");
+  await nextEventLoopCycle();
+  expect(server.handler.events).toEqual(["open", "data", "end", "close"]);
+  expect(client.handler.events).toEqual(["open", "close"]);
+});
+
+test.skip("client writes while server closes in the same tick", async () => {
+  using server = makeServer({
+    open(socket) {
+      socket.write("hello");
+      socket.end();
+      this.events.push("open");
+    },
+  });
+  using client = await makeClient();
+  await nextEventLoopCycle();
+  expect(server.handler.events).toEqual(["open", "close"]);
+  expect(client.handler.events).toEqual(["open", "data", "end", "close"]);
 });

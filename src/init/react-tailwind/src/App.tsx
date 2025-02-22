cabf6bf14930e7fc5bc6b674e47a5a6073d199b0
Pham Minh Triet <92496972+Nanome203@users.noreply.github.com>
2025-02-22 16:37:38 +0700

    Update react-tailwind/src/App.tsx

--- src/init/react-tailwind/src/App.tsx
+++ src/init/react-tailwind/src/App.tsx
@@ -8,16 +8,20 @@ export function App() {
   return (
     <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
       <div className="flex justify-center items-center gap-8 mb-8">
-        <img
-          src={logo}
-          alt="Bun Logo"
-          className="h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
-        />
-        <img
-          src={reactLogo}
-          alt="React Logo"
-          className="h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] animate-[spin_20s_linear_infinite]"
-        />
+        <a href="https://bun.sh/" target="_blank">
+          <img
+            src={logo}
+            alt="Bun Logo"
+            className="h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
+          />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img
+            src={reactLogo}
+            alt="React Logo"
+            className="h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] animate-[spin_20s_linear_infinite]"
+          />
+        </a>
       </div>
 
       <h1 className="text-5xl font-bold my-4 leading-tight">Bun + React</h1>

bbce36e5de26bac60d03ec141bd26ab70d9a098f
pfg <pfg@pfg.pw>
2025-02-21 17:52:35 -0800

    toml log fix

--- src/bundler/bundle_v2.zig
+++ src/bundler/bundle_v2.zig
@@ -4116,8 +4116,13 @@ pub const ParseTask = struct {
             .toml => {
                 const trace = tracer(@src(), "ParseTOML");
                 defer trace.end();
-                const root = try TOML.parse(&source, log, allocator, false);
-                return JSAst.init((try js_parser.newLazyExportAST(allocator, transpiler.options.define, opts, log, root, &source, "")).?);
+                var temp_log = bun.logger.Log.init(allocator);
+                defer {
+                    temp_log.cloneToWithRecycled(log, true) catch bun.outOfMemory();
+                    temp_log.msgs.clearAndFree();
+                }
+                const root = try TOML.parse(&source, &temp_log, allocator, false);
+                return JSAst.init((try js_parser.newLazyExportAST(allocator, transpiler.options.define, opts, &temp_log, root, &source, "")).?);
             },
             .text => {
                 const root = Expr.init(E.String, E.String{

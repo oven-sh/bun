3ec7a2ae6384d3906ef027f76d5c8dd494276222
Jarred Sumner <jarred@jarredsumner.com>
2025-02-23 19:51:14 -0800

    Try CPU pinning on Linux

--- src/bundler/bundle_v2.zig
+++ src/bundler/bundle_v2.zig
@@ -307,6 +307,37 @@ pub const ThreadPool = struct {
 
             this.data.transpiler.resolver.caches = CacheSet.Set.init(this.allocator);
             debug("Worker.create()", .{});
+
+            if (Environment.isLinux) {
+                if (!bun.getRuntimeFeatureFlag("BUN_FEATURE_FLAG_NO_CPU_AFFINITY")) {
+                    const Affinity = struct {
+                        threadlocal var has_set_affinity_on_thread: bool = false;
+
+                        pub fn pin() void {
+                            if (!has_set_affinity_on_thread) {
+                                has_set_affinity_on_thread = true;
+                                var cpu: usize = 0;
+                                if (std.os.linux.getcpu(&cpu, null) == 0) {
+                                    var set = std.mem.zeroes(std.os.linux.cpu_set_t);
+                                    const word_index = cpu / @bitSizeOf(usize);
+                                    const bit_index = cpu % @bitSizeOf(usize);
+                                    set[word_index] = @as(usize, 1) << @as(u6, @intCast(bit_index));
+
+                                    std.os.linux.sched_setaffinity(0, &set) catch |err| {
+                                        Output.debugWarn("Failed to set affinity for worker thread: {s}", .{@errorName(err)});
+                                        return;
+                                    };
+                                    debug("Pinned worker thread to CPU {d}", .{cpu});
+                                } else {
+                                    Output.debugWarn("Failed to get CPU for worker thread", .{});
+                                }
+                            }
+                        }
+                    };
+
+                    Affinity.pin();
+                }
+            }
         }
 
         pub fn run(this: *Worker, ctx: *BundleV2) void {

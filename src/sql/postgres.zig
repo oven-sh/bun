2ddc0813ab5ddf1cb94cb31362440f9b69bb973c
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-02-24 16:56:05 -0800

    opsie

--- src/sql/postgres.zig
+++ src/sql/postgres.zig
@@ -2840,7 +2840,7 @@ pub const PostgresSQLConnection = struct {
         }
 
         pub fn fromBytes(binary: bool, bigint: bool, oid: int4, bytes: []const u8, globalObject: *JSC.JSGlobalObject) !DataCell {
-            const oid_tag = if (std.math.maxInt(short) < oid) @as(types.Tag, @enumFromInt(@as(short, @intCast(oid)))) else .text;
+            const oid_tag = if (std.math.maxInt(short) < oid) .text else @as(types.Tag, @enumFromInt(@as(short, @intCast(oid))));
             switch (oid_tag) {
                 // TODO: .int2_array, .float8_array
                 inline .int4_array, .float4_array => |tag| {

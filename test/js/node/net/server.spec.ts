d0a57800fcfbedb91c03fe0ed1f5d0ee7cbe0f2e
Don Isaac <donald.isaac@gmail.com>
2025-02-24 16:36:37 -0800

    windows

--- test/js/node/net/server.spec.ts
+++ test/js/node/net/server.spec.ts
@@ -1,6 +1,7 @@
 import net from "node:net";
 import type { SocketConnectOpts } from "node:net";
 import EventEmitter from "node:events";
+import { isWindows } from "harness";
 
 describe("net.createServer(connectionListener)", () => {
   const onListen = jest.fn((socket: net.Socket) => {
@@ -113,7 +114,8 @@ describe("new net.Server()", () => {
       expect(server.keepAlive).toBe(false);
       expect(server.keepAliveInitialDelay).toBe(0);
     });
-    it("has a high water mark of 65,536", () => expect(server.highWaterMark).toBe(65_536));
+    it.skipIf(isWindows)("has a high water mark of 65,536 on posix", () =>    expect(server.highWaterMark).toBe(65_536));
+    it.skipIf(!isWindows)("has a high water mark of 16,384 on Windows", () => expect(server.highWaterMark).toBe(16_384));
     it("has no event listeners", () => {
       expect(server._eventsCount).toBe(0);
       expect(server._events).toEqual({});

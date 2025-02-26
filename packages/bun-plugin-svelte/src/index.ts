c79e2ea0e705106e3bec357a3479fb7c31813348
Don Isaac <donald.isaac@gmail.com>
2025-02-26 15:08:03 -0800

    cleanup

--- packages/bun-plugin-svelte/src/index.ts
+++ packages/bun-plugin-svelte/src/index.ts
@@ -21,52 +21,22 @@ function SveltePlugin(options: SvelteOptions = kEmptyObject as SvelteOptions): B
       const { config = kEmptyObject as Partial<BuildConfig> } = builder;
       const baseCompileOptions = getBaseCompileOptions(options ?? (kEmptyObject as Partial<SvelteOptions>), config);
 
-      // TODO: pre-process source with Bun.build and/or use require-cache
-      // hacking to shim ESTree imports to bun's parser (which is not yet ESTree-compliant)
-      // const isProd = !baseCompileOptions.dev;
-      // const ts = new Bun.Transpiler({
-      //   loader: "ts",
-      //   define: config.define,
-      //   treeShaking: isProd,
-      //   trimUnusedImports: isProd,
-      //   deadCodeElimination: isProd,
-      //   inline: isProd,
-      //   target: config.target,
-      // });
-
       builder
         .onLoad({ filter: /\.svelte(?:\.[tj]s)?$/ }, async args => {
           assert(args && typeof args === "object");
           const { path } = args;
           assert(typeof path === "string");
 
-          // FIXME: "side" missing when passed to `Bun.plugin`
-          // assert("side" in args);
-          // const side = args.side;
-          // assert(side === "client" || side === "server");
-
-          var needsPreprocess = true;
           var isModule = false;
 
           switch (path.substring(path.length - 2)) {
-            // @ts-expect-error
             case "js":
-              needsPreprocess = false;
-            // fallthrough
             case "ts":
               isModule = true;
               break;
           }
 
           const sourceText = await Bun.file(path).text();
-
-          // TODO: forward processed.dependencies to watchList
-          // var preprocessed: Processed | undefined;
-          // var source = sourceText;
-          // if (needsPreprocess) {
-          //   preprocessed = await preprocess(source, preprocessors, { filename: path });
-          //   source = preprocessed.code;
-          // }
 
           const side =
             args && "side" in args // "side" only passed when run from dev server
@@ -76,7 +46,6 @@ function SveltePlugin(options: SvelteOptions = kEmptyObject as SvelteOptions): B
           const compileFn = isModule ? compileModule : compile;
           const result = compileFn(sourceText, {
             ...baseCompileOptions,
-            // sourcemap: builder.config?.sourcemap ? preprocessed?.map : undefined,
             generate: baseCompileOptions.generate ?? side,
             filename: args.path,
           });
@@ -92,24 +61,10 @@ function SveltePlugin(options: SvelteOptions = kEmptyObject as SvelteOptions): B
             contents: result.js.code,
             loader: "js",
           } satisfies OnLoadResult;
-          // TODO: multiple results. maybe something like this:
-          // const { js, css } = result;
-          // return [
-          //   {
-          //     contents: result.js.code,
-          //     loader: "ts",
-          //     // watchList: [args.path],
-          //   },
-          //   css && {
-          //     contents: css.code,
-          //     sourceMap: css.map
-          //     loader: "css",
-          //     // watchList: [args.path],
-          //   },
-          // ].filter(Boolean);
+          // TODO: allow plugins to return multiple results.
+          // TODO: support layered sourcemaps
         })
         .onResolve({ filter: /^bun-svelte:/ }, args => {
-          console.log("onResolve(virtual css)", args);
           const [ns, name] = args.path.split(":");
           assert(ns === "bun-svelte" && !!name);
           return {
@@ -117,13 +72,11 @@ function SveltePlugin(options: SvelteOptions = kEmptyObject as SvelteOptions): B
             namespace: "bun-svelte",
           };
         })
-        // .onLoad({ filter: /^bun-svelte:/, namespace: "bun-svelte" }, args => {
         .onLoad({ filter: /\.css$/, namespace: virtualNamespace }, args => {
-          console.log("onLoad(virtual css)", args);
           const { path } = args;
           const code = virtualCssModules.get(path);
           assert(code != null, `bun-svelte-plugin: CSS not found for virtual css module "${path}"`);
-          virtualCssModules.delete(path); //
+          virtualCssModules.delete(path);
           return {
             contents: code,
             loader: "css",

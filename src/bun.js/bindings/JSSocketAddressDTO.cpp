d767c15d0ab5dfc1f0a836aee5e0010f4d4041db
Kai Tamkun <kai@tamkun.io>
2025-02-26 17:13:06 -0800

    Fix SocketAddress-related bugs

--- src/bun.js/bindings/JSSocketAddressDTO.cpp
+++ src/bun.js/bindings/JSSocketAddressDTO.cpp
@@ -20,7 +20,7 @@ JSObject* create(Zig::GlobalObject* globalObject, JSString* value, int32_t port,
 
     VM& vm = globalObject->vm();
 
-    JSObject* thisObject = constructEmptyObject(vm, globalObject->JSSocketAddressStructure());
+    JSObject* thisObject = constructEmptyObject(vm, globalObject->JSSocketAddressDTOStructure());
     thisObject->putDirectOffset(vm, 0, value);
     thisObject->putDirectOffset(vm, 1, isIPv6 ? jsString(vm, IPv6) : jsString(vm, IPv4));
     thisObject->putDirectOffset(vm, 2, jsNumber(port));
@@ -67,7 +67,7 @@ Structure* createStructure(VM& vm, JSGlobalObject* globalObject)
 } // namespace JSSocketAddress
 } // namespace Bun
 
-extern "C" JSC__JSValue JSSocketAddressDTO__create(JSGlobalObject* globalObject, JSString* address, int32_t port, bool isIPv6)
+extern "C" JSC__JSValue JSSocketAddressDTO__create(JSGlobalObject* globalObject, EncodedJSValue address, int32_t port, bool isIPv6)
 {
     ASSERT(port < std::numeric_limits<uint16_t>::max());
 
@@ -77,7 +77,7 @@ extern "C" JSC__JSValue JSSocketAddressDTO__create(JSGlobalObject* globalObject,
     auto* af = isIPv6 ? global->commonStrings().IPv6String(global) : global->commonStrings().IPv4String(global);
 
     JSObject* thisObject = constructEmptyObject(vm, global->JSSocketAddressDTOStructure());
-    thisObject->putDirectOffset(vm, Bun::JSSocketAddressDTO::addressOffset, address);
+    thisObject->putDirectOffset(vm, Bun::JSSocketAddressDTO::addressOffset, JSValue::decode(address));
     thisObject->putDirectOffset(vm, Bun::JSSocketAddressDTO::familyOffset, af);
     thisObject->putDirectOffset(vm, Bun::JSSocketAddressDTO::portOffset, jsNumber(port));
 

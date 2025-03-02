28d0fddd48150f5835c2333a1fe1d5cb3d7d55d0
Jarred Sumner <jarred@jarredsumner.com>
2025-03-02 07:23:05 -0800

    move 1 thing

--- src/sql/postgres.zig
+++ src/sql/postgres.zig
@@ -258,65 +258,8 @@ pub const PostgresSQLQueryResultMode = enum(u8) {
     values = 1,
     raw = 2,
 };
+const JSRef = JSC.JSRef;
 
-const JSRef = union(enum) {
-    weak: JSC.JSValue,
-    strong: JSC.Strong,
-
-    pub fn initWeak(value: JSC.JSValue) @This() {
-        return .{ .weak = value };
-    }
-
-    pub fn initStrong(value: JSC.JSValue, globalThis: *JSC.JSGlobalObject) @This() {
-        return .{ .strong = JSC.Strong.create(value, globalThis) };
-    }
-
-    pub fn empty() @This() {
-        return .{ .weak = .zero };
-    }
-
-    pub fn get(this: *@This()) JSC.JSValue {
-        return switch (this.*) {
-            .weak => this.weak,
-            .strong => this.strong.get() orelse .zero,
-        };
-    }
-    pub fn setWeak(this: *@This(), value: JSC.JSValue) void {
-        if (this == .strong) {
-            this.strong.deinit();
-        }
-        this.* = .{ .weak = value };
-    }
-
-    pub fn setStrong(this: *@This(), value: JSC.JSValue, globalThis: *JSC.JSGlobalObject) void {
-        if (this == .strong) {
-            this.strong.set(globalThis, value);
-            return;
-        }
-        this.* = .{ .strong = JSC.Strong.create(value, globalThis) };
-    }
-
-    pub fn upgrade(this: *@This(), globalThis: *JSC.JSGlobalObject) void {
-        switch (this.*) {
-            .weak => {
-                bun.assert(this.weak != .zero);
-                this.* = .{ .strong = JSC.Strong.create(this.weak, globalThis) };
-            },
-            .strong => {},
-        }
-    }
-
-    pub fn deinit(this: *@This()) void {
-        switch (this.*) {
-            .weak => {
-                this.weak = .zero;
-            },
-            .strong => {
-                this.strong.deinit();
-            },
-        }
-    }
-};
 pub const PostgresSQLQuery = struct {
     statement: ?*PostgresSQLStatement = null,
     query: bun.String = bun.String.empty,

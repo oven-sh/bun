e007066822ef154cfa6d5b2d4be09397a9c961fc
pfg <pfg@pfg.pw>
2025-03-03 18:45:37 -0800

    upgrade webkit

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -2,7 +2,7 @@ option(WEBKIT_VERSION "The version of WebKit to use")
 option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of downloading")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION f14adff4c39ba08174b84a942f9584028fc9a7ae)
+  set(WEBKIT_VERSION 02b922852cf99ea7ca25fc22710e57a404421eff)
 endif()
 
 string(SUBSTRING ${WEBKIT_VERSION} 0 16 WEBKIT_VERSION_PREFIX)

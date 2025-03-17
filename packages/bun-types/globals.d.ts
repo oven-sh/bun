11d4a885ed887a6a5e7afbcfc1808ea11fdf9533
Alistair Smith <hi@alistair.sh>
2025-03-17 23:57:32 +0000

    move fetch declarations to globals.d.ts for doc gen purposes

--- packages/bun-types/globals.d.ts
+++ packages/bun-types/globals.d.ts
@@ -1469,3 +1469,169 @@ declare var MessagePort: Bun.__internal.UseLibDomIfAvailable<
 interface ErrorConstructor {
   isError(value: unknown): value is Error;
 }
+
+//#region Fetch
+interface RequestInit extends Bun.__internal.LibOrFallbackRequestInit {}
+interface ResponseInit extends Bun.__internal.LibOrFallbackResponseInit {}
+
+interface Headers extends Bun.__internal.BunHeadersOverride {}
+declare var Headers: Bun.__internal.UseLibDomIfAvailable<
+  "Headers",
+  {
+    prototype: Headers;
+    new (init?: Bun.HeadersInit): Headers;
+  }
+>;
+
+interface Request extends Bun.__internal.BunRequestOverride {}
+declare var Request: Bun.__internal.UseLibDomIfAvailable<
+  "Request",
+  {
+    prototype: Request;
+    new (requestInfo: string, init?: RequestInit): Request;
+    new (requestInfo: RequestInit & { url: string }): Request;
+    new (requestInfo: Request, init?: RequestInit): Request;
+  }
+>;
+
+interface Response extends Bun.__internal.BunResponseOverride {}
+declare var Response: Bun.__internal.UseLibDomIfAvailable<
+  "Response",
+  {
+    new (body?: Bun.BodyInit | null | undefined, init?: ResponseInit | undefined): Response;
+    /**
+     * Create a new {@link Response} with a JSON body
+     *
+     * @param body - The body of the response
+     * @param options - options to pass to the response
+     *
+     * @example
+     *
+     * ```ts
+     * const response = Response.json({hi: "there"});
+     * console.assert(
+     *   await response.text(),
+     *   `{"hi":"there"}`
+     * );
+     * ```
+     * -------
+     *
+     * This is syntactic sugar for:
+     * ```js
+     *  new Response(JSON.stringify(body), {headers: { "Content-Type": "application/json" }})
+     * ```
+     * @link https://github.com/whatwg/fetch/issues/1389
+     */
+    json(body?: any, init?: ResponseInit | number): Response;
+
+    /**
+     * Create a new {@link Response} that redirects to url
+     *
+     * @param url - the URL to redirect to
+     * @param status - the HTTP status code to use for the redirect
+     */
+    redirect(url: string, status?: number): Response;
+
+    /**
+     * Create a new {@link Response} that redirects to url
+     *
+     * @param url - the URL to redirect to
+     * @param options - options to pass to the response
+     */
+    redirect(url: string, init?: ResponseInit): Response;
+
+    /**
+     * Create a new {@link Response} that has a network error
+     */
+    error(): Response;
+  }
+>;
+
+/**
+ * Extends Bun.TLSOptions with extra properties that are only supported in `fetch(url, {tls: ...})`
+ */
+interface BunFetchRequestInitTLS extends Bun.TLSOptions {
+  /**
+   * Custom function to check the server identity
+   * @param hostname - The hostname of the server
+   * @param cert - The certificate of the server
+   * @returns An error if the server is unauthorized, otherwise undefined
+   */
+  checkServerIdentity?: NonNullable<import("node:tls").ConnectionOptions["checkServerIdentity"]>;
+}
+
+/**
+ * BunFetchRequestInit represents additional options that Bun supports in `fetch()` only.
+ *
+ * Bun extends the `fetch` API with some additional options, except
+ * this interface is not quite a `RequestInit`, because they won't work
+ * if passed to `new Request()`. This is why it's a separate type.
+ */
+interface BunFetchRequestInit extends RequestInit {
+  /**
+   * Override the default TLS options
+   */
+  tls?: BunFetchRequestInitTLS;
+
+  /**
+   * Log the raw HTTP request & response to stdout. This API may be
+   * removed in a future version of Bun without notice.
+   * This is a custom property that is not part of the Fetch API specification.
+   * It exists mostly as a debugging tool
+   */
+  verbose?: boolean;
+
+  /**
+   * Override http_proxy or HTTPS_PROXY
+   * This is a custom property that is not part of the Fetch API specification.
+   */
+  proxy?: string;
+
+  /**
+   * Override the default S3 options
+   */
+  s3?: Bun.S3Options;
+}
+
+/**
+ * Send a HTTP(s) request
+ *
+ * @param request Request object
+ * @param init A structured value that contains settings for the fetch() request.
+ *
+ * @returns A promise that resolves to {@link Response} object.
+ */
+declare function fetch(request: Request, init?: BunFetchRequestInit): Promise<Response>;
+
+/**
+ * Send a HTTP(s) request
+ *
+ * @param url URL string
+ * @param init A structured value that contains settings for the fetch() request.
+ *
+ * @returns A promise that resolves to {@link Response} object.
+ */
+declare function fetch(url: string | URL | Request, init?: BunFetchRequestInit): Promise<Response>;
+
+/**
+ * Send a HTTP(s) request
+ *
+ * @param input URL string or Request object
+ * @param init A structured value that contains settings for the fetch() request.
+ *
+ * @returns A promise that resolves to {@link Response} object.
+ */
+declare function fetch(input: string | URL | Request, init?: BunFetchRequestInit): Promise<Response>;
+
+declare namespace fetch {
+  export function preconnect(
+    url: string | URL,
+    options?: {
+      dns?: boolean;
+      tcp?: boolean;
+      http?: boolean;
+      https?: boolean;
+    },
+  ): void;
+}
+//#endregion

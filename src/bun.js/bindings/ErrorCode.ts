dd80b662505d5cf45563e260ed01224aac63c2f4
Kai Tamkun <kai@tamkun.io>
2025-03-20 15:39:51 -0700

    Add missing ERR_HTTP_SOCKET_ASSIGNED

--- src/bun.js/bindings/ErrorCode.ts
+++ src/bun.js/bindings/ErrorCode.ts
@@ -80,6 +80,7 @@ const errors: ErrorCodeMapping = [
   ["ERR_HTTP_HEADERS_SENT", Error],
   ["ERR_HTTP_INVALID_HEADER_VALUE", TypeError],
   ["ERR_HTTP_INVALID_STATUS_CODE", RangeError],
+  ["ERR_HTTP_SOCKET_ASSIGNED", Error],
   ["ERR_HTTP2_ALTSVC_INVALID_ORIGIN", TypeError],
   ["ERR_HTTP2_ALTSVC_LENGTH", TypeError],
   ["ERR_HTTP2_ERROR", Error],

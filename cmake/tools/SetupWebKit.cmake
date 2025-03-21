7f42afb0359b74b3d27303cd39520f2790d938db
Meghan Denny <meghan@bun.sh>
2025-03-20 23:02:03 -0700

    deps: bump WebKit

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -2,7 +2,7 @@ option(WEBKIT_VERSION "The version of WebKit to use")
 option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of downloading")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION 1b232942b0654e54e93dcf86bf6f5b581b37b1ae)
+  set(WEBKIT_VERSION 91bf2baced1b1309c7e05f19177c97fefec20976)
 endif()
 
 string(SUBSTRING ${WEBKIT_VERSION} 0 16 WEBKIT_VERSION_PREFIX)

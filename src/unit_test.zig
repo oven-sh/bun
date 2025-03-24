017be4ca28dc46eda9273576783c67d371dc4814
Don Isaac <donald.isaac@gmail.com>
2025-03-24 13:43:34 -0700

    add tests to shell/braces.zig

--- src/unit_test.zig
+++ src/unit_test.zig
@@ -2,13 +2,14 @@ const std = @import("std");
 const bun = @import("root").bun;
 const t = std.testing;
 
-fn add(a: i32, b: i32) i32 {
-    return a + b;
+test {
+    _ = @import("shell/braces.zig");
 }
 
-test "idk strings or something" {
+test "basic string usage" {
     var s = bun.String.createUTF8("hi"); // it can create `WTF::String`s too
     defer s.deref();
+    try t.expect(s.tag != .Dead and s.tag != .Empty);
     try t.expectEqual(s.length(), 2);
     try t.expectEqualStrings(s.asUTF8().?, "hi");
 }

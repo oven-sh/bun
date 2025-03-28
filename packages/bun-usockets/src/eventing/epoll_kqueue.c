06f07d40c0ee1df996d1323460b135b81170232d
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-03-28 16:02:55 -0700

    fix unix

--- packages/bun-usockets/src/eventing/epoll_kqueue.c
+++ packages/bun-usockets/src/eventing/epoll_kqueue.c
@@ -399,6 +399,10 @@ int us_poll_start_rc(struct us_poll_t *p, struct us_loop_t *loop, int events) {
 
 #ifdef LIBUS_USE_EPOLL
     struct epoll_event event;
+    if(!(events & LIBUS_SOCKET_READABLE)) {
+        // if we are disabling readable, we need to add the other events to detect EOF/HUP/ERR
+        events |= EPOLLRDHUP | EPOLLHUP | EPOLLERR;
+    }
     event.events = events;
     event.data.ptr = p;
     int ret;
@@ -427,6 +431,7 @@ void us_poll_change(struct us_poll_t *p, struct us_loop_t *loop, int events) {
             // if we are disabling readable, we need to add the other events to detect EOF/HUP/ERR
             events |= EPOLLRDHUP | EPOLLHUP | EPOLLERR;
         }
+        event.events = events;
         event.data.ptr = p;
         int rc;
         do {

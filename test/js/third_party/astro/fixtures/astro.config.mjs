a92632943cdf121679a7c1ca235bc552a01eeee4
cirospaciari <6379399+cirospaciari@users.noreply.github.com>
2025-03-28 20:31:19 +0000

    `bun run prettier:extra`

--- test/js/third_party/astro/fixtures/astro.config.mjs
+++ test/js/third_party/astro/fixtures/astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import node from "@astrojs/node";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   adapter: node({
-    mode: 'standalone'
+    mode: "standalone",
   }),
 });

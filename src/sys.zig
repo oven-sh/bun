9d5f4345004c100791f45eebf13aa6035c4d2bdf
Jarred Sumner <jarred@jarredsumner.com>
2025-03-29 03:31:30 -0700

    Fix EPERM issue on Windows

--- src/sys.zig
+++ src/sys.zig
@@ -2316,8 +2316,21 @@ pub fn readlinkat(fd: bun.FileDescriptor, in: [:0]const u8, buf: []u8) Maybe([:0
 
 pub fn ftruncate(fd: bun.FileDescriptor, size: isize) Maybe(void) {
     if (comptime Environment.isWindows) {
-        if (kernel32.SetFileValidData(fd.cast(), size) == 0) {
-            return Maybe(void).errnoSysFd(0, .ftruncate, fd) orelse Maybe(void).success;
+        var io_status_block: std.posix.windows.IO_STATUS_BLOCK = undefined;
+        var eof_info = std.posix.windows.FILE_END_OF_FILE_INFORMATION{
+            .EndOfFile = @bitCast(size),
+        };
+
+        const rc = windows.ntdll.NtSetInformationFile(
+            fd,
+            &io_status_block,
+            &eof_info,
+            @sizeOf(windows.FILE_END_OF_FILE_INFORMATION),
+            .FileEndOfFileInformation,
+        );
+
+        if (rc != 0) {
+            return Maybe(void).errnoSysFd(rc, .ftruncate, fd) orelse Maybe(void).success;
         }
 
         return Maybe(void).success;

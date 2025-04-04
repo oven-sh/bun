2e29c7d40ce9b868c7b2c7a57c303c49c2959bdf
Meghan Denny <meghan@bun.sh>
2025-04-03 23:06:02 -0700

    node:net: fix Server.prototype.address()

--- src/bun.js/api/sockets.classes.ts
+++ src/bun.js/api/sockets.classes.ts
@@ -281,6 +281,11 @@ export default [
         getter: "getData",
         setter: "setData",
       },
+
+      getsockname: {
+        fn: "getsockname",
+        length: 1,
+      },
     },
     finalize: true,
     construct: true,

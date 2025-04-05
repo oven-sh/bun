oven-sh/bun

commit 7021c42cf25ae9de47f430b2f3b88d8b1501a617

fix(node:http) fix post regression (#18599)
Co-authored-by: cirospaciari <6379399+cirospaciari@users.noreply.github.com>
Co-authored-by: Jarred Sumner <jarred@jarredsumner.com>
Co-authored-by: Jarred-Sumner <709451+Jarred-Sumner@users.noreply.github.com>

src/js/node/http.ts
@@ -952,6 +952,8 @@ const ServerPrototype = {
           });
           isNextIncomingMessageHTTPS = prevIsNextIncomingMessageHTTPS;
           handle.onabort = onServerRequestEvent.bind(socket);
+          // start buffering data if any, the user will need to resume() or .on("data") to read it
+          handle.pause();
           drainMicrotasks();
 
           let capturedError;
@@ -2037,6 +2039,12 @@ const ServerResponsePrototype = {
     const headerState = this[headerStateSymbol];
     callWriteHeadIfObservable(this, headerState);
 
+    const flags = handle.flags;
+    if (!!(flags & NodeHTTPResponseFlags.closed_or_completed)) {
+      // node.js will return true if the handle is closed but the internal state is not
+      // and will not throw or emit an error
+      return true;
+    }
     if (headerState !== NodeHTTPHeaderState.sent) {
       handle.cork(() => {
         handle.writeHead(this.statusCode, this.statusMessage, this[headersSymbol]);

packages/bun-usockets/src/eventing/epoll_kqueue.c
@@ -353,15 +353,21 @@ int kqueue_change(int kqfd, int fd, int old_events, int new_events, void *user_d
     int change_length = 0;
 
     /* Do they differ in readable? */
+    int is_readable =  (new_events & LIBUS_SOCKET_READABLE);
+    int is_writable =  (new_events & LIBUS_SOCKET_WRITABLE);
     if ((new_events & LIBUS_SOCKET_READABLE) != (old_events & LIBUS_SOCKET_READABLE)) {
-        EV_SET64(&change_list[change_length++], fd, EVFILT_READ, (new_events & LIBUS_SOCKET_READABLE) ? EV_ADD : EV_DELETE, 0, 0, (uint64_t)(void*)user_data, 0, 0);
+        EV_SET64(&change_list[change_length++], fd, EVFILT_READ, is_readable ? EV_ADD : EV_DELETE, 0, 0, (uint64_t)(void*)user_data, 0, 0);
     }
-
-    /* Do they differ in writable? */
-    if ((new_events & LIBUS_SOCKET_WRITABLE) != (old_events & LIBUS_SOCKET_WRITABLE)) {
+    
+    if(!is_readable && !is_writable) {
+        if(!(old_events & LIBUS_SOCKET_WRITABLE)) {
+            // if we are not reading or writing, we need to add writable to receive FIN
+            EV_SET64(&change_list[change_length++], fd, EVFILT_WRITE, EV_ADD, 0, 0, (uint64_t)(void*)user_data, 0, 0);
+        }
+    } else if ((new_events & LIBUS_SOCKET_WRITABLE) != (old_events & LIBUS_SOCKET_WRITABLE)) {
+        /* Do they differ in writable? */    
         EV_SET64(&change_list[change_length++], fd, EVFILT_WRITE, (new_events & LIBUS_SOCKET_WRITABLE) ? EV_ADD : EV_DELETE, 0, 0, (uint64_t)(void*)user_data, 0, 0);
-    }
-
+    } 
     int ret;
     do {
         ret = kevent64(kqfd, change_list, change_length, change_list, change_length, KEVENT_FLAG_ERROR_EVENTS, NULL);
@@ -399,6 +405,10 @@ int us_poll_start_rc(struct us_poll_t *p, struct us_loop_t *loop, int events) {
 
 #ifdef LIBUS_USE_EPOLL
     struct epoll_event event;
+    if(!(events & LIBUS_SOCKET_READABLE) && !(events & LIBUS_SOCKET_WRITABLE)) {
+        // if we are disabling readable, we need to add the other events to detect EOF/HUP/ERR
+        events |= EPOLLRDHUP | EPOLLHUP | EPOLLERR;
+    }
     event.events = events;
     event.data.ptr = p;
     int ret;
@@ -423,6 +433,10 @@ void us_poll_change(struct us_poll_t *p, struct us_loop_t *loop, int events) {
 
 #ifdef LIBUS_USE_EPOLL
         struct epoll_event event;
+        if(!(events & LIBUS_SOCKET_READABLE) && !(events & LIBUS_SOCKET_WRITABLE)) {
+             // if we are disabling readable, we need to add the other events to detect EOF/HUP/ERR
+            events |= EPOLLRDHUP | EPOLLHUP | EPOLLERR;
+        }
         event.events = events;
         event.data.ptr = p;
         int rc;

packages/bun-usockets/src/eventing/libuv.c
@@ -104,7 +104,6 @@ void us_poll_change(struct us_poll_t *p, struct us_loop_t *loop, int events) {
         us_internal_poll_type(p) |
         ((events & LIBUS_SOCKET_READABLE) ? POLL_TYPE_POLLING_IN : 0) |
         ((events & LIBUS_SOCKET_WRITABLE) ? POLL_TYPE_POLLING_OUT : 0);
-
     uv_poll_start(p->uv_p, events, poll_cb);
   }
 }

packages/bun-usockets/src/socket.c
@@ -558,11 +558,6 @@ struct us_loop_t *us_connecting_socket_get_loop(struct us_connecting_socket_t *c
 void us_socket_pause(int ssl, struct us_socket_t *s) {
     // closed cannot be paused because it is already closed
     if(us_socket_is_closed(ssl, s)) return;
-    if(us_socket_is_shut_down(ssl, s)) {
-        // we already sent FIN so we pause all events because we are read-only
-        us_poll_change(&s->p, s->context->loop, 0);
-        return;
-    }
     // we are readable and writable so we can just pause readable side
     us_poll_change(&s->p, s->context->loop, LIBUS_SOCKET_WRITABLE);
 }

src/bun.js/api/server/NodeHTTPResponse.zig
@@ -104,6 +104,7 @@ pub fn upgrade(this: *NodeHTTPResponse, data_value: JSValue, sec_websocket_proto
     if (socketValue == .zero) {
         return false;
     }
+    this.raw_response.@"resume"();
 
     defer {
         this.setOnAbortedHandler();
@@ -534,6 +535,7 @@ pub fn onTimeout(this: *NodeHTTPResponse, _: uws.AnyResponse) void {
 pub fn doPause(this: *NodeHTTPResponse, globalObject: *JSC.JSGlobalObject, callframe: *JSC.CallFrame, thisValue: JSC.JSValue) bun.JSError!JSC.JSValue {
     _ = globalObject; // autofix
     _ = callframe; // autofix
+
     if (this.flags.request_has_completed or this.flags.socket_closed) {
         return .false;
     }
@@ -542,12 +544,14 @@ pub fn doPause(this: *NodeHTTPResponse, globalObject: *JSC.JSGlobalObject, callf
         this.raw_response.onData(*NodeHTTPResponse, onBufferRequestBodyWhilePaused, this);
     }
 
-    this.raw_response.pause();
+    if (!Environment.isWindows) {
+        // TODO: figure out why windows is not emitting EOF with UV_DISCONNECT
+        this.raw_response.pause();
+    }
     return .true;
 }
 
-pub fn drainRequestBody(this: *NodeHTTPResponse, globalObject: *JSC.JSGlobalObject, callframe: *JSC.CallFrame) bun.JSError!JSC.JSValue {
-    _ = callframe; // autofix
+pub fn drainRequestBody(this: *NodeHTTPResponse, globalObject: *JSC.JSGlobalObject, _: *JSC.CallFrame) bun.JSError!JSC.JSValue {
     return this.drainBufferedRequestBodyFromPause(globalObject) orelse .undefined;
 }
 
@@ -656,7 +660,7 @@ pub fn abort(this: *NodeHTTPResponse, globalObject: *JSC.JSGlobalObject, callfra
     if (state.isHttpEndCalled()) {
         return .undefined;
     }
-
+    this.raw_response.@"resume"();
     this.raw_response.clearOnData();
     this.raw_response.clearOnWritable();
     this.raw_response.clearTimeout();
@@ -666,6 +670,7 @@ pub fn abort(this: *NodeHTTPResponse, globalObject: *JSC.JSGlobalObject, callfra
 }
 
 fn onBufferRequestBodyWhilePaused(this: *NodeHTTPResponse, chunk: []const u8, last: bool) void {
+    log("onBufferRequestBodyWhilePaused({d}, {})", .{ chunk.len, last });
     this.buffered_request_body_data_during_pause.append(bun.default_allocator, chunk) catch bun.outOfMemory();
     if (last) {
         this.flags.is_data_buffered_during_pause_last = true;
@@ -977,6 +982,8 @@ pub fn write(this: *NodeHTTPResponse, globalObject: *JSC.JSGlobalObject, callfra
 
 pub fn end(this: *NodeHTTPResponse, globalObject: *JSC.JSGlobalObject, callframe: *JSC.CallFrame) bun.JSError!JSC.JSValue {
     const arguments = callframe.arguments_old(3).slice();
+    //We dont wanna a paused socket when we call end, so is important to resume the socket
+    this.raw_response.@"resume"();
     return writeOrEnd(this, globalObject, arguments, callframe.this(), true);
 }
 

test/js/node/http/node-http.test.ts
@@ -2285,3 +2285,35 @@ it("should not emit/throw error when writing after socket.end", async () => {
     server.close();
   }
 });
+
+it("should handle data if not immediately handled", async () => {
+  // Create a local server to receive data from
+  const server = http.createServer();
+
+  // Listen to the request event
+  server.on("request", (request, res) => {
+    setTimeout(() => {
+      const body: Uint8Array[] = [];
+      request.on("data", chunk => {
+        body.push(chunk);
+      });
+      request.on("end", () => {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(Buffer.concat(body));
+      });
+    }, 100);
+  });
+  try {
+    await once(server.listen(0), "listening");
+    const url = `http://localhost:${server.address().port}`;
+    const payload = "Hello, world!".repeat(10).toString();
+    const res = await fetch(url, {
+      method: "POST",
+      body: payload,
+    });
+    expect(res.status).toBe(200);
+    expect(await res.text()).toBe(payload);
+  } finally {
+    server.close();
+  }
+});

test/js/third_party/astro/astro-post.test.js
@@ -1,0 +1,41 @@
+import { preview, build } from "astro";
+import { expect, test } from "bun:test";
+import { join } from "path";
+test("is able todo a POST request to an astro action", async () => {
+  await build({
+    root: join(import.meta.dirname, "fixtures"),
+    devOutput: false,
+    logLevel: "error",
+  });
+  const previewServer = await preview({
+    root: join(import.meta.dirname, "fixtures"),
+    port: 0,
+    logLevel: "error",
+  });
+
+  try {
+    const r = await fetch(`http://localhost:${previewServer.port}/_actions/getGreeting/`, {
+      headers: {
+        accept: "application/json",
+        "accept-language": "en-US,en;q=0.9,es;q=0.8",
+        "content-type": "application/json",
+        "sec-ch-ua": '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        Referer: "http://localhost:4321/",
+        "Referrer-Policy": "strict-origin-when-cross-origin",
+      },
+      body: '{"name":"World"}',
+      method: "POST",
+    });
+    expect(r.status).toBe(200);
+    const text = await r.text();
+    expect(text).toBe('["Hello, World!"]');
+  } finally {
+    // Stop the server if needed
+    await previewServer.stop();
+  }
+});

test/js/third_party/astro/fixtures/astro.config.mjs
@@ -1,0 +1,11 @@
+// @ts-check
+import node from "@astrojs/node";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+});

commit b066ca5f37aef1d4299f3ce718149b0589a3232f

Fix performance regression from #18599

src/js/node/http.ts
@@ -952,8 +952,13 @@ const ServerPrototype = {
           });
           isNextIncomingMessageHTTPS = prevIsNextIncomingMessageHTTPS;
           handle.onabort = onServerRequestEvent.bind(socket);
-          // start buffering data if any, the user will need to resume() or .on("data") to read it
-          handle.pause();
+
+          if (hasBody) {
+            // Don't automatically read the body, the user will need to resume() or .on("data") to read it.
+            // But, avoid the system call overhead unless we are going to receive body data.
+            handle.pause();
+          }
+
           drainMicrotasks();
 
           let capturedError;
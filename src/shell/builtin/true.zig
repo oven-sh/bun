e2da7ba1a72854ac88b4b8d8174ed00c5ca41542
Jarred Sumner <jarred@jarredsumner.com>
2025-04-04 21:18:29 -0700

    Split builtin shell commands into individual files

---
+++ src/shell/builtin/true.zig
@@ -0,0 +1,23 @@
+bltn: *Builtin,
+
+pub fn start(this: *@This()) Maybe(void) {
+    this.bltn.done(0);
+    return Maybe(void).success;
+}
+
+pub fn onIOWriterChunk(_: *@This(), _: usize, _: ?JSC.SystemError) void {
+    // no IO is done
+}
+
+pub fn deinit(this: *@This()) void {
+    _ = this;
+}
+
+// --
+const bun = @import("root").bun;
+const interpreter = @import("../interpreter.zig");
+const Interpreter = interpreter.Interpreter;
+const Builtin = Interpreter.Builtin;
+
+const JSC = bun.JSC;
+const Maybe = bun.sys.Maybe;

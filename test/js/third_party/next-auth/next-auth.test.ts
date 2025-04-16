45af4c27b7009f0e9f797ff46b8e8505cf52983b
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-04-15 21:29:10 -0700

    use local packages instead of general

--- test/js/third_party/next-auth/next-auth.test.ts
+++ test/js/third_party/next-auth/next-auth.test.ts
@@ -1,8 +1,13 @@
 import { describe, it, expect } from "bun:test";
-import { bunRun } from "harness";
+import { bunRun, runBunInstall, bunEnv } from "harness";
 import { join } from "path";
 describe("next-auth", () => {
   it("should be able to call server action multiple times using auth middleware #18977", async () => {
+    await runBunInstall(bunEnv, join(import.meta.dir, "fixture"), {
+      allowWarnings: true,
+      allowErrors: true,
+      savesLockfile: false,
+    });
     const result = bunRun(join(import.meta.dir, "fixture", "server.js"));
     expect(result.stderr).toBe("");
     expect(result.stdout).toBeDefined();

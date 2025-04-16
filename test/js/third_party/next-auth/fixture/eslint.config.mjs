37ac81041f9119223edea3f8c2bfcd02155d5152
cirospaciari <6379399+cirospaciari@users.noreply.github.com>
2025-04-16 03:30:09 +0000

    `bun run prettier:extra`

--- test/js/third_party/next-auth/fixture/eslint.config.mjs
+++ test/js/third_party/next-auth/fixture/eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +9,6 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
 
 export default eslintConfig;

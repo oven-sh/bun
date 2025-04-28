868f1756a6b7326ce7a35933f46c2868cb2725d6
Ben Grant <ben@bun.sh>
2025-04-28 16:51:03 -0700

    Mark vm.main_resolved_path as stale when we change vm.main

--- src/bun.js/VirtualMachine.zig
+++ src/bun.js/VirtualMachine.zig
@@ -1950,6 +1950,7 @@ extern fn Bun__loadHTMLEntryPoint(global: *JSGlobalObject) *JSInternalPromise;
 pub fn reloadEntryPoint(this: *VirtualMachine, entry_path: []const u8) !*JSInternalPromise {
     this.has_loaded = false;
     this.main = entry_path;
+    this.main_resolved_path = .empty;
     this.main_hash = Watcher.getHash(entry_path);
 
     try this.ensureDebugger(true);
@@ -2018,6 +2019,7 @@ export fn Bun__VirtualMachine__setOverrideModuleRunMainPromise(vm: *VirtualMachi
 pub fn reloadEntryPointForTestRunner(this: *VirtualMachine, entry_path: []const u8) !*JSInternalPromise {
     this.has_loaded = false;
     this.main = entry_path;
+    this.main_resolved_path = .empty;
     this.main_hash = Watcher.getHash(entry_path);
 
     this.eventLoop().ensureWaker();

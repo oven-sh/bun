595fde2c887a8a173bae0554603f220bf7d5691a
Dylan Conway <dylan.conway567@gmail.com>
2025-05-01 14:00:30 -0700

    more test

---
+++ test/js/node/timers/timers-immediate-exception-fixture.js
@@ -0,0 +1,72 @@
+import { mustCall } from "../test/common/index.mjs";
+
+process.on(
+  "uncaughtException",
+  mustCall(err => {
+    if (err.message !== "oops") {
+      throw err;
+    }
+  }, 2),
+);
+
+function checkNextTick(expected) {
+  process.nextTick(
+    mustCall(() => {
+      if (counter !== expected) {
+        throw new Error("oops: " + expected + " != " + counter);
+      }
+    }),
+  );
+}
+
+var counter = 0;
+setImmediate(
+  mustCall(() => {
+    counter++;
+    checkNextTick(1);
+  }),
+);
+
+setImmediate(
+  mustCall(() => {
+    counter++;
+    checkNextTick(2);
+  }),
+);
+
+setImmediate(
+  mustCall(() => {
+    counter++;
+    checkNextTick(4);
+    throw new Error("oops");
+  }),
+);
+
+setImmediate(
+  mustCall(() => {
+    counter++;
+    checkNextTick(4);
+  }),
+);
+
+setImmediate(
+  mustCall(() => {
+    counter++;
+    checkNextTick(6);
+    throw new Error("oops");
+  }),
+);
+
+setImmediate(
+  mustCall(() => {
+    counter++;
+    checkNextTick(6);
+  }),
+);
+
+setImmediate(
+  mustCall(() => {
+    counter++;
+    checkNextTick(7);
+  }),
+);

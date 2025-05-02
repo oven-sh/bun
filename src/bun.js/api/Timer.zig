f24430d5c0a556f8dd66a6eee99f0f61befa4528
Dylan Conway <dylan.conway567@gmail.com>
2025-05-02 15:45:04 -0700

    test

--- src/bun.js/api/Timer.zig
+++ src/bun.js/api/Timer.zig
@@ -126,7 +126,7 @@ pub const All = struct {
             else
                 timespec{ .nsec = 0, .sec = 0 };
 
-            this.uv_timer.start(wait.msUnsigned(), 0, &onUVTimer);
+            this.uv_timer.start(@max(1, wait.msUnsigned()), 0, &onUVTimer);
 
             if (this.active_timer_count > 0) {
                 this.uv_timer.ref();

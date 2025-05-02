af756ce4b745c20b6911a5927d102b4543b3984b
chloe caruso <git@paperclover.net>
2025-05-01 20:58:54 -0700

    waaa

--- src/bundler/bundle_v2.zig
+++ src/bundler/bundle_v2.zig
@@ -3466,7 +3466,7 @@ pub const BundleV2 = struct {
                         graph.input_files.items(.loader)[source.index.get()],
                         parse_result.watcher_data.dir_fd,
                         null,
-                        false,
+                        bun.Environment.isWindows,
                     );
                 }
             }

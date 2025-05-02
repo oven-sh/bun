7521e45b17830e39cfcb271f98c8e686b43ebb7f
Jarred Sumner <jarred@jarredsumner.com>
2025-05-02 12:55:57 -0700

    --console & console: true  (#19427)
    Co-authored-by: Zack Radisic <56137411+zackradisic@users.noreply.github.com>

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -2,7 +2,7 @@ option(WEBKIT_VERSION "The version of WebKit to use")
 option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of downloading")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION 53d4176ddc98ba721e50355826f58ec758766fa8)
+  set(WEBKIT_VERSION c244f567ab804c2558067d00733013c01725d824)
 endif()
 
 string(SUBSTRING ${WEBKIT_VERSION} 0 16 WEBKIT_VERSION_PREFIX)

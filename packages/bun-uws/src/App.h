14ea951eec5092fd7097c29fe2ec273f5701d994
Alistair Smith <hi@alistair.sh>
2025-05-07 17:49:48 -0700

    assign tls versions

--- packages/bun-uws/src/App.h
+++ packages/bun-uws/src/App.h
@@ -78,6 +78,8 @@ namespace uWS {
         int request_cert = 0;
         unsigned int client_renegotiation_limit = 3;
         unsigned int client_renegotiation_window = 600;
+        double min_tls_version = 0.0;
+        double max_tls_version = 0.0;
 
         /* Conversion operator used internally */
         operator struct us_bun_socket_context_options_t() const {

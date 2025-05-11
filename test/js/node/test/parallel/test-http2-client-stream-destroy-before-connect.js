6149f629ff5a9cc2fc830c817a3328099d363f0a
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-05-11 16:17:33 -0700

    dup

--- test/js/node/test/parallel/test-http2-client-stream-destroy-before-connect.js
+++ test/js/node/test/parallel/test-http2-client-stream-destroy-before-connect.js
@@ -44,7 +44,6 @@ server.listen(0, common.mustCall(() => {
 
   req.on('close', common.mustCall(() => {
     assert.strictEqual(req.rstCode, NGHTTP2_INTERNAL_ERROR);
-    assert.strictEqual(req.rstCode, NGHTTP2_INTERNAL_ERROR);
     countdown.dec();
   }));
 

1e0972a34bebcdf5d00a138d62680e5e15b8c1f3
190n <7763597+190n@users.noreply.github.com>
2025-05-12 23:09:56 +0000

    `bun run prettier`

--- test/js/web/workers/message-event.test.ts
+++ test/js/web/workers/message-event.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from "bun:test";
+import { describe, expect, test } from "bun:test";
 
 describe("MessageEvent constructor", () => {
   test("returns an Event instance", () => {

5b3fefbc30ba61ec503b93c1935f239ede444f1e
Ben Grant <ben@bun.sh>
2025-05-13 13:03:36 -0700

    describe node.js behavior

--- test/js/web/workers/worker.test.ts
+++ test/js/web/workers/worker.test.ts
@@ -318,8 +318,9 @@ describe("worker_threads", () => {
   test("worker terminate while setting up thread", async () => {
     // this test is inherently somewhat flaky: if we call terminate() before the worker starts
     // running any JavaScript the code will be 0 like we expect, but if we terminate while it is
-    // running code the exit code is 1 instead. this means we can randomly see an exit code of 1 if
-    // the main thread happens to run slower than usual and allows the worker to run some code.
+    // running code the exit code is 1 instead (this happens in Node.js too). this means we can
+    // randomly see an exit code of 1 if the main thread happens to run slower than usual and allows
+    // the worker to run some code.
     //
     // to prevent it from polluting the flaky test list, we try 10 times and expect:
     // - at least 1 time the exit code was 0

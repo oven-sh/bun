e983a3e4625a9e399e0f8cdafeccc5af68a5a0b3
pfg <pfg@pfg.pw>
2025-05-15 14:55:33 -0700

    note

--- test/js/node/test/parallel/test-whatwg-readablestream.js
+++ test/js/node/test/parallel/test-whatwg-readablestream.js
@@ -281,7 +281,10 @@ class Source {
   reader.read(view);
 }
 
-// TODO: RangeError: Invalid value for view.byteLength in respondWithNewView
+// TODO: bug in webkit's implementation
+// with autoAllocateChunkSize: 10, it emits "Invalid value for view.byteLength"
+// it peeks, sees the next item is 10 long, and errors for respondWithNewView(3)
+// in respondWithNewView
 // {
 //   const stream = new ReadableStream({
 //     type: 'bytes',

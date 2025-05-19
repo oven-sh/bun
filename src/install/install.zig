f65b36cd309803f16d8b2a767a94a620d77e7107
Jarred Sumner <jarred@jarredsumner.com>
2025-05-19 01:46:11 -0700

    [dns] Add fallback for ADDRCONFIG like Chrome's, avoid glibc UDP port 0 hangs

--- src/install/install.zig
+++ src/install/install.zig
@@ -14537,7 +14537,7 @@ pub const PackageManager = struct {
             const allocator = hostname_stack.get();
             const hostname = try allocator.dupeZ(u8, manager.options.scope.url.hostname);
             defer allocator.free(hostname);
-            bun.dns.internal.prefetch(manager.event_loop.loop(), hostname);
+            bun.dns.internal.prefetch(manager.event_loop.loop(), hostname, manager.options.scope.url.getPortAuto());
         }
 
         var load_result: Lockfile.LoadResult = if (manager.options.do.load_lockfile)

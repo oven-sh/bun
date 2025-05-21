a71b08063951f04b90e4f90baddacc237fad1046
Kai Tamkun <kai@tamkun.io>
2025-05-21 14:32:44 -0700

    Update WebKit version again

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -2,7 +2,7 @@ option(WEBKIT_VERSION "The version of WebKit to use")
 option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of downloading")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION 3db06352c224f419f1e2e52d39fd5e26141063e1)
+  set(WEBKIT_VERSION 6ac94f7524dd6bfc1f4a565c68f799c731eb2d1e)
 endif()
 
 string(SUBSTRING ${WEBKIT_VERSION} 0 16 WEBKIT_VERSION_PREFIX)

e7084e36b9c2ee2b6149b4822e666bd6165718d3
Alistair Smith <hi@alistair.sh>
2025-05-26 15:46:49 -0700

    move prototype

--- src/bun.js/bindings/JSNodePerformanceHooksHistogramPrototype.cpp
+++ src/bun.js/bindings/JSNodePerformanceHooksHistogramPrototype.cpp
@@ -1,3 +1,5 @@
+#include "ErrorCode.h"
+#include "JSDOMExceptionHandling.h"
 #include "root.h"
 
 #include "JSNodePerformanceHooksHistogramPrototype.h"
@@ -6,6 +8,8 @@
 #include <JavaScriptCore/JSCInlines.h>
 #include <JavaScriptCore/JSGlobalObject.h>
 #include <JavaScriptCore/ObjectConstructor.h>
+#include <JavaScriptCore/JSMap.h>
+#include <JavaScriptCore/JSMapInlines.h>
 
 namespace Bun {
 
@@ -40,6 +44,383 @@ void JSNodePerformanceHooksHistogramPrototype::finishCreation(VM& vm)
     Base::finishCreation(vm);
     reifyStaticProperties(vm, JSNodePerformanceHooksHistogram::info(), JSNodePerformanceHooksHistogramPrototypeTableValues, *this);
     JSC_TO_STRING_TAG_WITHOUT_TRANSITION();
+}
+
+JSC_DEFINE_HOST_FUNCTION(jsNodePerformanceHooksHistogramProtoFuncRecord, (JSGlobalObject * globalObject, CallFrame* callFrame))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(callFrame->thisValue());
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "record"_s);
+        return {};
+    }
+
+    if (callFrame->argumentCount() < 1) {
+        Bun::ERR::MISSING_ARGS(scope, globalObject, "record requires at least one argument"_s);
+        return {};
+    }
+
+    JSValue arg = callFrame->uncheckedArgument(0);
+    int64_t value;
+    if (arg.isNumber()) {
+        value = static_cast<int64_t>(arg.asNumber());
+    } else if (arg.isBigInt()) {
+        auto* bigInt = jsCast<JSBigInt*>(arg);
+        value = JSBigInt::toBigInt64(bigInt);
+    } else {
+        Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "value"_s, "number or BigInt"_s, arg);
+        return {};
+    }
+
+    if (value < 1) {
+        Bun::ERR::OUT_OF_RANGE(scope, globalObject, "value is out of range (must be >= 1)"_s);
+        return {};
+    }
+
+    thisObject->record(value);
+    return JSValue::encode(jsUndefined());
+}
+
+JSC_DEFINE_HOST_FUNCTION(jsNodePerformanceHooksHistogramProtoFuncRecordDelta, (JSGlobalObject * globalObject, CallFrame* callFrame))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(callFrame->thisValue());
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "recordDelta"_s);
+        return {};
+    }
+
+    uint64_t delta = thisObject->recordDelta(globalObject);
+    return JSValue::encode(jsNumber(static_cast<double>(delta)));
+}
+
+JSC_DEFINE_HOST_FUNCTION(jsNodePerformanceHooksHistogramProtoFuncAdd, (JSGlobalObject * globalObject, CallFrame* callFrame))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(callFrame->thisValue());
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "add"_s);
+        return {};
+    }
+
+    if (callFrame->argumentCount() < 1) {
+        Bun::ERR::MISSING_ARGS(scope, globalObject, "add requires at least one argument"_s);
+        return {};
+    }
+
+    JSValue otherArg = callFrame->uncheckedArgument(0);
+    JSNodePerformanceHooksHistogram* otherHistogram = jsDynamicCast<JSNodePerformanceHooksHistogram*>(otherArg);
+    if (!otherHistogram) [[unlikely]] {
+        Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "argument"_s, "Histogram"_s, otherArg);
+        return {};
+    }
+
+    double dropped = thisObject->add(otherHistogram);
+    return JSValue::encode(jsNumber(dropped));
+}
+
+JSC_DEFINE_HOST_FUNCTION(jsNodePerformanceHooksHistogramProtoFuncReset, (JSGlobalObject * globalObject, CallFrame* callFrame))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(callFrame->thisValue());
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "reset"_s);
+        return {};
+    }
+
+    thisObject->reset();
+    return JSValue::encode(jsUndefined());
+}
+
+JSC_DEFINE_HOST_FUNCTION(jsNodePerformanceHooksHistogramProtoFuncPercentile, (JSGlobalObject * globalObject, CallFrame* callFrame))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(callFrame->thisValue());
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "percentile"_s);
+        return {};
+    }
+
+    if (callFrame->argumentCount() < 1) {
+        Bun::ERR::MISSING_ARGS(scope, globalObject, "percentile requires an argument"_s);
+        return {};
+    }
+
+    double percentile = callFrame->uncheckedArgument(0).toNumber(globalObject);
+    RETURN_IF_EXCEPTION(scope, {});
+    if (percentile <= 0 || percentile > 100 || std::isnan(percentile)) {
+        Bun::ERR::OUT_OF_RANGE(scope, globalObject, "percentile"_s, "> 0 && <= 100"_s, jsNumber(percentile));
+        return {};
+    }
+
+    return JSValue::encode(jsNumber(static_cast<double>(thisObject->getPercentile(percentile))));
+}
+
+JSC_DEFINE_HOST_FUNCTION(jsNodePerformanceHooksHistogramProtoFuncPercentileBigInt, (JSGlobalObject * globalObject, CallFrame* callFrame))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(callFrame->thisValue());
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "percentileBigInt"_s);
+        return {};
+    }
+
+    if (callFrame->argumentCount() < 1) {
+        Bun::ERR::MISSING_ARGS(scope, globalObject, "percentileBigInt requires an argument"_s);
+        return {};
+    }
+
+    double percentile = callFrame->uncheckedArgument(0).toNumber(globalObject);
+    RETURN_IF_EXCEPTION(scope, {});
+    if (percentile <= 0 || percentile > 100 || std::isnan(percentile)) {
+        Bun::ERR::OUT_OF_RANGE(scope, globalObject, "percentile"_s, "> 0 && <= 100"_s, jsNumber(percentile));
+        return {};
+    }
+
+    return JSValue::encode(JSBigInt::createFrom(globalObject, thisObject->getPercentile(percentile)));
+}
+
+JSC_DEFINE_HOST_FUNCTION(jsNodePerformanceHooksHistogramProtoFuncGetPercentiles, (JSGlobalObject * globalObject, CallFrame* callFrame))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(callFrame->thisValue());
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "percentiles"_s);
+        return {};
+    }
+
+    if (callFrame->argumentCount() < 1 || !callFrame->uncheckedArgument(0).isObject()) {
+        Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "argument"_s, "Map"_s, callFrame->uncheckedArgument(0));
+        return {};
+    }
+    JSMap* map = jsDynamicCast<JSMap*>(callFrame->uncheckedArgument(0));
+    if (!map) [[unlikely]] {
+        Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "argument"_s, "Map"_s, callFrame->uncheckedArgument(0));
+        return {};
+    }
+
+    thisObject->getPercentiles(globalObject, map);
+    return JSValue::encode(map);
+}
+
+JSC_DEFINE_HOST_FUNCTION(jsNodePerformanceHooksHistogramProtoFuncGetPercentilesBigInt, (JSGlobalObject * globalObject, CallFrame* callFrame))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(callFrame->thisValue());
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "percentilesBigInt"_s);
+        return {};
+    }
+
+    if (callFrame->argumentCount() < 1 || !callFrame->uncheckedArgument(0).isObject()) {
+        Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "argument"_s, "Map"_s, callFrame->uncheckedArgument(0));
+        return {};
+    }
+    JSMap* map = jsDynamicCast<JSMap*>(callFrame->uncheckedArgument(0));
+    if (!map) [[unlikely]] {
+        Bun::ERR::INVALID_ARG_TYPE(scope, globalObject, "argument"_s, "Map"_s, callFrame->uncheckedArgument(0));
+        return {};
+    }
+
+    thisObject->getPercentilesBigInt(globalObject, map);
+    return JSValue::encode(map);
+}
+
+// MARK: Property getters
+
+JSC_DEFINE_CUSTOM_GETTER(jsNodePerformanceHooksHistogramGetter_count, (JSGlobalObject * globalObject, EncodedJSValue thisValue, PropertyName))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(JSValue::decode(thisValue));
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "count"_s);
+        return {};
+    }
+    return JSValue::encode(jsNumber(static_cast<double>(thisObject->getCount())));
+}
+
+JSC_DEFINE_CUSTOM_GETTER(jsNodePerformanceHooksHistogramGetter_countBigInt, (JSGlobalObject * globalObject, EncodedJSValue thisValue, PropertyName))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(JSValue::decode(thisValue));
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "countBigInt"_s);
+        return {};
+    }
+    return JSValue::encode(JSBigInt::createFrom(globalObject, thisObject->getCount()));
+}
+
+JSC_DEFINE_CUSTOM_GETTER(jsNodePerformanceHooksHistogramGetter_min, (JSGlobalObject * globalObject, EncodedJSValue thisValue, PropertyName))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(JSValue::decode(thisValue));
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "min"_s);
+        return {};
+    }
+    return JSValue::encode(jsNumber(static_cast<double>(thisObject->getMin())));
+}
+
+JSC_DEFINE_CUSTOM_GETTER(jsNodePerformanceHooksHistogramGetter_minBigInt, (JSGlobalObject * globalObject, EncodedJSValue thisValue, PropertyName))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(JSValue::decode(thisValue));
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "minBigInt"_s);
+        return {};
+    }
+    return JSValue::encode(JSBigInt::createFrom(globalObject, thisObject->getMin()));
+}
+
+JSC_DEFINE_CUSTOM_GETTER(jsNodePerformanceHooksHistogramGetter_max, (JSGlobalObject * globalObject, EncodedJSValue thisValue, PropertyName))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(JSValue::decode(thisValue));
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "max"_s);
+        return {};
+    }
+    return JSValue::encode(jsNumber(static_cast<double>(thisObject->getMax())));
+}
+
+JSC_DEFINE_CUSTOM_GETTER(jsNodePerformanceHooksHistogramGetter_maxBigInt, (JSGlobalObject * globalObject, EncodedJSValue thisValue, PropertyName))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(JSValue::decode(thisValue));
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "maxBigInt"_s);
+        return {};
+    }
+    return JSValue::encode(JSBigInt::createFrom(globalObject, thisObject->getMax()));
+}
+
+JSC_DEFINE_CUSTOM_GETTER(jsNodePerformanceHooksHistogramGetter_mean, (JSGlobalObject * globalObject, EncodedJSValue thisValue, PropertyName))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(JSValue::decode(thisValue));
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "mean"_s);
+        return {};
+    }
+    return JSValue::encode(jsNumber(thisObject->getMean()));
+}
+
+JSC_DEFINE_CUSTOM_GETTER(jsNodePerformanceHooksHistogramGetter_stddev, (JSGlobalObject * globalObject, EncodedJSValue thisValue, PropertyName))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(JSValue::decode(thisValue));
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "stddev"_s);
+        return {};
+    }
+    return JSValue::encode(jsNumber(thisObject->getStddev()));
+}
+
+JSC_DEFINE_CUSTOM_GETTER(jsNodePerformanceHooksHistogramGetter_exceeds, (JSGlobalObject * globalObject, EncodedJSValue thisValue, PropertyName))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(JSValue::decode(thisValue));
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "exceeds"_s);
+        return {};
+    }
+    return JSValue::encode(jsNumber(static_cast<double>(thisObject->getExceeds())));
+}
+
+JSC_DEFINE_CUSTOM_GETTER(jsNodePerformanceHooksHistogramGetter_exceedsBigInt, (JSGlobalObject * globalObject, EncodedJSValue thisValue, PropertyName))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSNodePerformanceHooksHistogram* thisObject = jsDynamicCast<JSNodePerformanceHooksHistogram*>(JSValue::decode(thisValue));
+    if (!thisObject) [[unlikely]] {
+        WebCore::throwThisTypeError(*globalObject, scope, "Histogram"_s, "exceedsBigInt"_s);
+        return {};
+    }
+    return JSValue::encode(JSBigInt::createFrom(globalObject, static_cast<uint64_t>(thisObject->getExceeds())));
+}
+
+// JSC Host function wrapper for creating histogram from JavaScript
+JSC_DEFINE_HOST_FUNCTION(jsFunction_createHistogram, (JSGlobalObject * globalObject, CallFrame* callFrame))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    // Default values
+    int64_t lowest = 1;
+    int64_t highest = std::numeric_limits<int64_t>::max();
+    int figures = 3;
+
+    // Parse arguments
+    if (callFrame->argumentCount() >= 1) {
+        JSValue lowestArg = callFrame->uncheckedArgument(0);
+        if (lowestArg.isNumber()) {
+            lowest = static_cast<int64_t>(lowestArg.asNumber());
+        } else if (lowestArg.isBigInt()) {
+            auto* bigInt = jsCast<JSBigInt*>(lowestArg);
+            lowest = JSBigInt::toBigInt64(bigInt);
+        }
+    }
+
+    if (callFrame->argumentCount() >= 2) {
+        JSValue highestArg = callFrame->uncheckedArgument(1);
+        if (highestArg.isNumber()) {
+            highest = static_cast<int64_t>(highestArg.asNumber());
+        } else if (highestArg.isBigInt()) {
+            auto* bigInt = jsCast<JSBigInt*>(highestArg);
+            highest = JSBigInt::toBigInt64(bigInt);
+        }
+    }
+
+    if (callFrame->argumentCount() >= 3) {
+        JSValue figuresArg = callFrame->uncheckedArgument(2);
+        if (figuresArg.isNumber()) {
+            figures = static_cast<int>(figuresArg.asNumber());
+        }
+    }
+
+    auto* zigGlobalObject = defaultGlobalObject(globalObject);
+    Structure* structure = zigGlobalObject->m_JSNodePerformanceHooksHistogramClassStructure.get(zigGlobalObject);
+    RETURN_IF_EXCEPTION(scope, {});
+
+    JSNodePerformanceHooksHistogram* histogram = JSNodePerformanceHooksHistogram::create(vm, structure, globalObject, lowest, highest, figures);
+    RETURN_IF_EXCEPTION(scope, {});
+
+    return JSValue::encode(histogram);
 }
 
 } // namespace Bun

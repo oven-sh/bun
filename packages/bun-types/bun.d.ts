fd3d8a103efff2034460c54be4d54b0fe52a855c
Alistair Smith <hi@alistair.sh>
2025-05-26 21:20:58 -0700

    fix my last review comment

--- packages/bun-types/bun.d.ts
+++ packages/bun-types/bun.d.ts
@@ -5900,11 +5900,10 @@ declare module "bun" {
      * @returns The number of bytes written for the final chunk. Returns `-1` if the socket was already closed or shutting down.
      * @example
      * ```ts
+     * // send some data and close the write site
      * socket.end("Goodbye!");
-     * ```
-     * @example
-     * ```ts
-     * socket.end(); // Close write side without sending final data
+     * // or close write side without sending final data
+     * socket.end();
      * ```
      */
     end(data?: string | BufferSource, byteOffset?: number, byteLength?: number): number;

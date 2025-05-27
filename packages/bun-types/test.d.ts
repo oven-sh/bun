325d0b1ed607b7a1d1e0acfaa0683ac805540bb7
Yechao LI <liyechao@yechao.xyz>
2025-05-27 12:58:10 +0800

    fix: correct function type for spyon an optional function (#19240)
    Co-authored-by: Alistair Smith <hi@alistair.sh>

--- packages/bun-types/test.d.ts
+++ packages/bun-types/test.d.ts
@@ -146,7 +146,7 @@ declare module "bun:test" {
   export function spyOn<T extends object, K extends keyof T>(
     obj: T,
     methodOrPropertyValue: K,
-  ): Mock<T[K] extends (...args: any[]) => any ? T[K] : never>;
+  ): Mock<Extract<T[K], (...args: any[]) => any>>;
 
   interface FunctionLike {
     readonly name: string;

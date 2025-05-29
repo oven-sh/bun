d3e80acdeab2861a605210acf0a8c6f86a39579e
Jarred Sumner <jarred@jarredsumner.com>
2025-05-29 16:12:03 -0700

    implement process.setSourceMapsEnabled

---
+++ test/js/node/test/parallel/test-process-setsourcemapsenabled.js
@@ -0,0 +1,15 @@
+require('../common');
+const assert = require('assert');
+
+const unexpectedValues = [
+  undefined,
+  null,
+  1,
+  {},
+  () => {},
+];
+for (const it of unexpectedValues) {
+  assert.throws(() => {
+    process.setSourceMapsEnabled(it);
+  }, /ERR_INVALID_ARG_TYPE/);
+}

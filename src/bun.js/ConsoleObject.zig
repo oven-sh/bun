82a91eea4d8f5d97b624c3c1218e01719e75c7c4
Mats Willemsen <mats.willemsen@ah.nl>
2025-05-29 18:10:37 +0200

    fix: console.trace should go to stderr

--- src/bun.js/ConsoleObject.zig
+++ src/bun.js/ConsoleObject.zig
@@ -160,7 +160,7 @@ fn messageWithTypeAndLevel_(
     else
         Output.enable_ansi_colors_stdout;
 
-    var buffered_writer = if (level == .Warning or level == .Error)
+    var buffered_writer = if (level == .Warning or level == .Error or message_type == .Trace)
         &console.error_writer
     else
         &console.writer;
@@ -218,7 +218,10 @@ fn messageWithTypeAndLevel_(
         }
     }
 
-    if (print_length > 0)
+    if (print_length > 0) {
+        if (message_type == .Trace) {
+            writer.writeAll("Trace: ") catch {};
+        }
         try format2(
             level,
             global,
@@ -228,8 +231,8 @@ fn messageWithTypeAndLevel_(
             Writer,
             writer,
             print_options,
-        )
-    else if (message_type == .Log) {
+        );
+    } else if (message_type == .Log) {
         _ = console.writer.write("\n") catch 0;
         console.writer.flush() catch {};
     } else if (message_type != .Trace)

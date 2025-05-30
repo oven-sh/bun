541a5b069b841f50188c06512130fec15eb6474f
Jarred-Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2025-05-30 00:27:07 +0000

    Sync Node.js tests with upstream

--- test/js/node/test/parallel/test-http-outgoing-renderHeaders.js
+++ test/js/node/test/parallel/test-http-outgoing-renderHeaders.js
@@ -1,3 +1,4 @@
+'use strict';
 // Flags: --expose-internals
 
 require('../common');
@@ -8,42 +9,42 @@ const http = require('http');
 const OutgoingMessage = http.OutgoingMessage;
 
 {
-const outgoingMessage = new OutgoingMessage();
-outgoingMessage._header = {};
-assert.throws(
-() => outgoingMessage._renderHeaders(),
-{
-code: 'ERR_HTTP_HEADERS_SENT',
-name: 'Error',
-message: 'Cannot render headers after they are sent to the client'
-}
-);
-}
-
-{
-const outgoingMessage = new OutgoingMessage();
-outgoingMessage[kOutHeaders] = null;
-const result = outgoingMessage._renderHeaders();
-assert.deepStrictEqual(result, {});
-}
-
-
-{
-const outgoingMessage = new OutgoingMessage();
-outgoingMessage[kOutHeaders] = {};
-const result = outgoingMessage._renderHeaders();
-assert.deepStrictEqual(result, {});
+  const outgoingMessage = new OutgoingMessage();
+  outgoingMessage._header = {};
+  assert.throws(
+    () => outgoingMessage._renderHeaders(),
+    {
+      code: 'ERR_HTTP_HEADERS_SENT',
+      name: 'Error',
+      message: 'Cannot render headers after they are sent to the client'
+    }
+  );
 }
 
 {
-const outgoingMessage = new OutgoingMessage();
-outgoingMessage[kOutHeaders] = {
-host: ['host', 'nodejs.org'],
-origin: ['Origin', 'localhost']
-};
-const result = outgoingMessage._renderHeaders();
-assert.deepStrictEqual(result, {
-host: 'nodejs.org',
-Origin: 'localhost'
-});
+  const outgoingMessage = new OutgoingMessage();
+  outgoingMessage[kOutHeaders] = null;
+  const result = outgoingMessage._renderHeaders();
+  assert.deepStrictEqual(result, {});
+}
+
+
+{
+  const outgoingMessage = new OutgoingMessage();
+  outgoingMessage[kOutHeaders] = {};
+  const result = outgoingMessage._renderHeaders();
+  assert.deepStrictEqual(result, {});
+}
+
+{
+  const outgoingMessage = new OutgoingMessage();
+  outgoingMessage[kOutHeaders] = {
+    host: ['host', 'nodejs.org'],
+    origin: ['Origin', 'localhost']
+  };
+  const result = outgoingMessage._renderHeaders();
+  assert.deepStrictEqual(result, {
+    host: 'nodejs.org',
+    Origin: 'localhost'
+  });
 }

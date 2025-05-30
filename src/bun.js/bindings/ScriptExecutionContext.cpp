c663ccd83bd2dad8c54151d64831ebbb368f77cb
Alistair Smith <hi@alistair.sh>
2025-05-30 15:09:56 -0700

    1

--- src/bun.js/bindings/ScriptExecutionContext.cpp
+++ src/bun.js/bindings/ScriptExecutionContext.cpp
@@ -122,6 +122,25 @@ void ScriptExecutionContext::unrefEventLoop()
     Bun__eventLoop__incrementRefConcurrently(WebCore::clientData(vm())->bunVM, -1);
 }
 
+bool ScriptExecutionContext::canSendMessage()
+{
+    us_loop_t* loop = (us_loop_t*)uws_get_loop();
+    uint32_t currentTickNr = static_cast<uint32_t>(us_loop_iteration_number(loop));
+    
+    if (lastSendTickNr != currentTickNr) {
+        messagesSentThisTick = 0;
+        lastSendTickNr = currentTickNr;
+    }
+
+    constexpr uint32_t MAX_MESSAGES_PER_TICK = 1000;
+    if (messagesSentThisTick >= MAX_MESSAGES_PER_TICK) {
+        return false;
+    }
+
+    messagesSentThisTick++;
+    return true;
+}
+
 ScriptExecutionContext::~ScriptExecutionContext()
 {
     checkConsistency();

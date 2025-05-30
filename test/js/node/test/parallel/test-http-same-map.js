dcda307ecdaccf6857ab7ab1f81e2aba7f5a8480
Jarred-Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2025-05-30 00:28:30 +0000

    Sync Node.js tests with upstream

--- test/js/node/test/parallel/test-http-same-map.js
+++ test/js/node/test/parallel/test-http-same-map.js
@@ -1,12 +1,12 @@
+// Flags: --allow_natives_syntax
 'use strict';
-// Flags: --allow-natives-syntax
 
 const common = require('../common');
 const assert = require('assert');
 const http = require('http');
 
 const server =
-  http.createServer(onrequest).listen(0, common.localhostIPv4, () => next(0));
+    http.createServer(onrequest).listen(0, common.localhostIPv4, () => next(0));
 
 function onrequest(req, res) {
   res.end('ok');

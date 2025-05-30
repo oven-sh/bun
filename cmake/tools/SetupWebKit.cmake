d0639a47404d016b5fe4c84a6d8f936982e05dea
Jarred Sumner <jarred@jarredsumner.com>
2025-05-30 01:10:28 -0700

    Update SetupWebKit.cmake

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -2,7 +2,7 @@ option(WEBKIT_VERSION "The version of WebKit to use")
 option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of downloading")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION b98e20b11e6ab044f73218bdd05ab064587b9ead)
+  set(WEBKIT_VERSION 55d34298be71160492754c7ffb427dfe757bf4e3)
 endif()
 
 string(SUBSTRING ${WEBKIT_VERSION} 0 16 WEBKIT_VERSION_PREFIX)

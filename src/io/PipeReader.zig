de5354fb9c20f9e4cce425a2381606a9042055ab
Meghan Denny <meghan@bun.sh>
2025-05-31 03:36:15 -0700

    fix memory leak when pipe Bun.spawn stdio is never read repeatedly

--- src/io/PipeReader.zig
+++ src/io/PipeReader.zig
@@ -226,6 +226,7 @@ const PosixBufferedReader = struct {
 
         bun.assert(!this.flags.is_done);
         this.flags.is_done = true;
+        this._buffer.clearAndFree();
     }
 
     fn closeHandle(this: *PosixBufferedReader) void {
@@ -801,6 +802,7 @@ pub const WindowsBufferedReader = struct {
     fn finish(this: *WindowsBufferedReader) void {
         this.flags.has_inflight_read = false;
         this.flags.is_done = true;
+        this._buffer.clearAndFree();
     }
 
     pub fn done(this: *WindowsBufferedReader) void {

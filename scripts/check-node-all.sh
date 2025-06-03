aef0087097fd7195bafa22d11b6b6468f33348df
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-06-03 15:47:53 -0700

    revert this

--- scripts/check-node-all.sh
+++ scripts/check-node-all.sh
@@ -2,8 +2,6 @@
 
 i=0
 j=0
-r=0
-f=0
 
 if [ -z "$1" ]
 then
@@ -20,28 +18,17 @@ case $1 in
 esac
 
 export BUN_DEBUG_QUIET_LOGS=1
-#clean regressions.txt and fails.txt
-rm ./regressions.txt
-rm  ./fails.txt
 
 for x in $(find test/js/node/test/parallel -type f -name "test-$1*.js" | sort)
 do
   i=$((i+1))
   echo ./$x
-  if timeout 5 $PWD/build/debug/bun-debug ./$x
+  if timeout 2 $PWD/build/debug/bun-debug ./$x
   then
     j=$((j+1))
     git add $x
-  elif git ls-files --error-unmatch $x > /dev/null 2>&1; then
-    echo ./$x >> ./regressions.txt
-    r=$((r+1))  # Increment regression count
-  else
-    echo ./$x >> ./fails.txt
-    f=$((f+1))  # Increment failure count
   fi
 done
 
 echo $i tests tested
 echo $j tests passed
-echo $r tests regressions
-echo $f tests failed

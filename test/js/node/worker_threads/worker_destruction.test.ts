9df2b097680adca66f18a4d9defb2bea8d8e2cae
Alistair Smith <hi@alistair.sh>
2025-06-03 18:50:57 -0700

    push

--- test/js/node/worker_threads/worker_destruction.test.ts
+++ test/js/node/worker_threads/worker_destruction.test.ts
@@ -7,8 +7,8 @@ describe("Worker destruction", () => {
   const method = ["Bun.connect", "Bun.listen", "fetch"];
   describe.each(method)("bun when %s is used in a Worker that is terminating", method => {
     // fetch: ASAN failure
-    test.skipIf(isBroken && method == "fetch")("exits cleanly", () => {
-      expect([join(import.meta.dir, "worker_thread_check.ts"), method]).toRun();
+    test.skipIf(isBroken && method == "fetch")("exits cleanly", async () => {
+      await expect([join(import.meta.dir, "worker_thread_check.ts"), method]).toRunAsync();
     });
   });
 });

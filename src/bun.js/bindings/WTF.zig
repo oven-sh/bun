8408c5bfcb84b84bf55e5236e881e6d37573e3bf
Jarred Sumner <jarred@jarredsumner.com>
2025-06-05 20:23:31 -0700

    The fix

--- src/bun.js/bindings/WTF.zig
+++ src/bun.js/bindings/WTF.zig
@@ -18,14 +18,14 @@ pub const WTF = struct {
         return res;
     }
 
-    extern fn Bun__writeHTTPDate(buffer: *[32]u8, timestampMs: u64) c_int;
+    extern fn Bun__writeHTTPDate(buffer: *[32]u8, length: usize, timestampMs: u64) c_int;
 
     pub fn writeHTTPDate(buffer: *[32]u8, timestampMs: u64) []u8 {
         if (timestampMs == 0) {
             return buffer[0..0];
         }
 
-        const res = Bun__writeHTTPDate(buffer, timestampMs);
+        const res = Bun__writeHTTPDate(buffer, 32, timestampMs);
         if (res < 1) {
             return buffer[0..0];
         }

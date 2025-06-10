63349bcc04393e9bf100a4df65a4e90c0e52f3e9
Meghan Denny <meghan@bun.sh>
2025-06-09 19:48:04 -0700

    a bit more

--- test/js/node/tls/node-tls-server.test.ts
+++ test/js/node/tls/node-tls-server.test.ts
@@ -689,13 +689,19 @@ it("connectionListener should emit the right amount of times, and with alpnProto
 
 describe("should fuzz the native handle and not crash", () => {
   const handle = require("tls").Server().listen().unref()._handle;
-  for (const k in Object.getOwnPropertyDescriptors(Object.getPrototypeOf(handle))) {
+  const proto = Object.getPrototypeOf(handle);
+  const descs = Object.getOwnPropertyDescriptors(proto);
+  for (const k in descs) {
     for (const v of [undefined, null, "", "a", 24, -3.4, {}, [], Symbol("b")]) {
       it(`${k}(${String(v)})`, () => {
         try {
           handle[k];
+          descs[k].get?.call(handle);
           if (typeof handle[k] != "function") return;
           handle[k].call(handle, v);
+          handle[k].call(undefined, v);
+          handle[k].call(null, v);
+          descs[k].set?.call(handle, v);
         } catch (e) {
           //
         }

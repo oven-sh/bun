80684fe9c6399942cf4004b7d7222b81a829390c
Alistair Smith <hi@alistair.sh>
2025-06-11 16:41:39 -0700

    comments

--- test/js/web/workers/worker-exit-event-is-emitted-before-parent-exits.test.ts
+++ test/js/web/workers/worker-exit-event-is-emitted-before-parent-exits.test.ts
@@ -7,6 +7,10 @@ import stripAnsi from "strip-ansi";
 
 const IS_CHILD = process.env.IS_CHILD === "true";
 
+// At the time of writing, this test file passes in Node.js and fails in Bun.
+// Node.js seems to wait for the exit event to happen before the parent process
+// exits, which means that the Worker's exit code is printed to stdout
+
 if (IS_CHILD) {
   const worker = new Worker("process.exit(1)", { eval: true });
   worker.on("exit", code => console.log(code));

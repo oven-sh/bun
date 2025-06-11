e63712ac915ce9264fdfbc7da0d49b9c842a0978
Alistair Smith <hi@alistair.sh>
2025-06-11 14:37:59 -0700

    rm

--- test/js/node/worker_threads/worker-with-process-exit-does-terminate.test.ts
+++ test/js/node/worker_threads/worker-with-process-exit-does-terminate.test.ts
@@ -1,22 +1,3 @@
-// import assert from "node:assert";
-// import { once } from "node:events";
-// import { fileURLToPath } from "node:url";
-// import { Worker, isMainThread, parentPort } from "node:worker_threads";
-
-// if (isMainThread) {
-//   const worker = new Worker(fileURLToPath(import.meta.url));
-//   worker.postMessage("process.exit(2); parentPort.postMessage('done')");
-//   worker.on("message", () => {
-//     assert.fail("worker should not send a message");
-//   });
-//   const [exitCode] = await once(worker, "exit");
-//   assert.strictEqual(exitCode, 2);
-// } else {
-//   parentPort.on("message", code => {
-//     console.log(`EVAL(${code})`);
-//     eval(code);
-//   });
-// }
 import { once } from "node:events";
 import { fileURLToPath } from "node:url";
 import { Worker, isMainThread, parentPort } from "node:worker_threads";
@@ -25,12 +6,9 @@ if (isMainThread) {
   const { test, expect } = await import("bun:test");
 
   test("process.exit() works", async () => {
-    console.log("Testing process.exit() in worker thread...");
-
     const worker = new Worker(fileURLToPath(import.meta.url));
 
     worker.on("message", () => expect().fail("worker should not keep executing after process.exit()"));
-
     worker.postMessage("boom");
 
     const [exitCode] = await once(worker, "exit");

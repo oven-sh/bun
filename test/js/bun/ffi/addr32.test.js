770461f9bdecb5248a57582f1c64f1019bdc2de3
Ben Grant <ben@bun.sh>
2025-06-12 14:00:11 -0700

    Add test

---
+++ test/js/bun/ffi/addr32.test.js
@@ -0,0 +1,21 @@
+import { test, expect } from "bun:test";
+import { isLinux } from "../../../harness";
+import { dlopen, CString, FFIType } from "bun:ffi";
+import { describe } from "bun:jsc";
+
+// Only runs on Linux because that is where we can most reliably allocate a 32-bit pointer.
+test.skipIf(!isLinux)("can use addresses encoded as int32s", async () => {
+  const compiler = Bun.spawn(["cc", "-shared", "-o", "libaddr32.so", "addr32.c"], {
+    cwd: __dirname,
+  });
+  await compiler.exited;
+  expect(compiler.exitCode).toBe(0);
+
+  const { symbols } = dlopen("./libaddr32.so", { addr32: { args: [], returns: FFIType.pointer } });
+  const addr = symbols.addr32();
+  expect(addr).toBeGreaterThan(0);
+  expect(addr).toBeLessThan(2 ** 31);
+  const addrIntEncoded = addr | 0;
+  expect(describe(addrIntEncoded)).toContain("Int32");
+  expect(new CString(addrIntEncoded).toString()).toBe("hello world");
+});

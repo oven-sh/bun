19bfdd7951bb144aa0109cd9c548b81ab075c6b7
RiskyMH <git@riskymh.dev>
2025-06-12 23:55:12 +1000

    [bun build] in cjs node, dont convert `module` to `require.module`

--- test/bundler/bundler_edgecase.test.ts
+++ test/bundler/bundler_edgecase.test.ts
@@ -1799,6 +1799,28 @@ describe("bundler", () => {
       api.expectFile("/out.js").not.toMatch(/[^\.:]module/); // `.module` and `node:module` are ok.
     },
   });
+  itBundled("edgecase/build-cjs-module#20308", {
+    files: {
+      "/entry.ts": /* js */ `
+        import {other} from './other';
+        console.log(capture(import.meta.main), capture(require.main === module), ...other);
+      `,
+      "/other.ts": /* js */`
+        globalThis['ca' + 'pture'] = x => x;
+
+        export const other = [capture(require.main === module), capture(import.meta.main)];
+      `,
+    },
+    target: "node",
+    format: "cjs",
+    capture: ["false", "false", "require.main == module", "require.main == module"],
+    onAfterBundle(api) {
+      console.log(api.readFile("/out.js"));
+      // This should be marked as a CommonJS module
+      api.expectFile("/out.js").toMatch(/\brequire\b/); // __require is not ok
+      api.expectFile("/out.js").toMatch(/[^\.:]module/); // `.module` and `node:module` are not ok.
+    },
+  });
   itBundled("edgecase/IdentifierInEnum#13081", {
     files: {
       "/entry.ts": `

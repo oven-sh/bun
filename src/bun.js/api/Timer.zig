0a64da7a51613597987dd6ce0c7d72176a670b49
Jarred Sumner <jarred@jarredsumner.com>
2025-06-18 00:11:01 -0700

    Update Timer.zig

--- src/bun.js/api/Timer.zig
+++ src/bun.js/api/Timer.zig
@@ -8,7 +8,10 @@ const JSGlobalObject = JSC.JSGlobalObject;
 const Debugger = JSC.Debugger;
 const Environment = bun.Environment;
 const uv = bun.windows.libuv;
+const api = bun.api;
+const StatWatcherScheduler = @import("../node/node_fs_stat_watcher.zig").StatWatcherScheduler;
 const Timer = @This();
+const DNSResolver = @import("./bun/dns_resolver.zig").DNSResolver;
 
 /// TimeoutMap is map of i32 to nullable Timeout structs
 /// i32 is exposed to JavaScript and can be used with clearTimeout, clearInterval, etc.
@@ -545,133 +548,11 @@ pub const All = struct {
     }
 };
 
+const uws = bun.uws;
+
 pub const EventLoopTimer = @import("./Timer/EventLoopTimer.zig");
 
-pub const TimeoutObject = struct {
-    const RefCount = bun.ptr.RefCount(@This(), "ref_count", deinit, .{});
-    pub const ref = RefCount.ref;
-    pub const deref = RefCount.deref;
-
-    pub const js = JSC.Codegen.JSTimeout;
-    pub const toJS = js.toJS;
-    pub const fromJS = js.fromJS;
-    pub const fromJSDirect = js.fromJSDirect;
-
-    ref_count: RefCount,
-    event_loop_timer: EventLoopTimer = .{
-        .next = .{},
-        .tag = .TimeoutObject,
-    },
-    internals: TimerObjectInternals,
-
-    pub fn init(
-        globalThis: *JSGlobalObject,
-        id: i32,
-        kind: Kind,
-        interval: u31,
-        callback: JSValue,
-        arguments: JSValue,
-    ) JSValue {
-        // internals are initialized by init()
-        const timeout = bun.new(TimeoutObject, .{ .ref_count = .init(), .internals = undefined });
-        const js_value = timeout.toJS(globalThis);
-        defer js_value.ensureStillAlive();
-        timeout.internals.init(
-            js_value,
-            globalThis,
-            id,
-            kind,
-            interval,
-            callback,
-            arguments,
-        );
-
-        if (globalThis.bunVM().isInspectorEnabled()) {
-            Debugger.didScheduleAsyncCall(
-                globalThis,
-                .DOMTimer,
-                ID.asyncID(.{ .id = id, .kind = kind.big() }),
-                kind != .setInterval,
-            );
-        }
-
-        return js_value;
-    }
-
-    fn deinit(this: *TimeoutObject) void {
-        this.internals.deinit();
-        bun.destroy(this);
-    }
-
-    pub fn constructor(globalObject: *JSC.JSGlobalObject, callFrame: *JSC.CallFrame) !*TimeoutObject {
-        _ = callFrame;
-        return globalObject.throw("Timeout is not constructible", .{});
-    }
-
-    pub fn toPrimitive(this: *TimeoutObject, _: *JSGlobalObject, _: *JSC.CallFrame) bun.JSError!JSValue {
-        return this.internals.toPrimitive();
-    }
-
-    pub fn doRef(this: *TimeoutObject, globalThis: *JSGlobalObject, callFrame: *JSC.CallFrame) bun.JSError!JSValue {
-        return this.internals.doRef(globalThis, callFrame.this());
-    }
-
-    pub fn doUnref(this: *TimeoutObject, globalThis: *JSGlobalObject, callFrame: *JSC.CallFrame) bun.JSError!JSValue {
-        return this.internals.doUnref(globalThis, callFrame.this());
-    }
-
-    pub fn doRefresh(this: *TimeoutObject, globalThis: *JSGlobalObject, callFrame: *JSC.CallFrame) bun.JSError!JSValue {
-        return this.internals.doRefresh(globalThis, callFrame.this());
-    }
-
-    pub fn hasRef(this: *TimeoutObject, _: *JSGlobalObject, _: *JSC.CallFrame) bun.JSError!JSValue {
-        return this.internals.hasRef();
-    }
-
-    pub fn finalize(this: *TimeoutObject) void {
-        this.internals.finalize();
-    }
-
-    pub fn getDestroyed(this: *TimeoutObject, globalThis: *JSGlobalObject) JSValue {
-        _ = globalThis;
-        return .jsBoolean(this.internals.getDestroyed());
-    }
-
-    pub fn close(this: *TimeoutObject, globalThis: *JSGlobalObject, callFrame: *JSC.CallFrame) JSValue {
-        this.internals.cancel(globalThis.bunVM());
-        return callFrame.this();
-    }
-
-    pub fn get_onTimeout(_: *TimeoutObject, thisValue: JSValue, _: *JSGlobalObject) JSValue {
-        return TimeoutObject.js.callbackGetCached(thisValue).?;
-    }
-
-    pub fn set_onTimeout(_: *TimeoutObject, thisValue: JSValue, globalThis: *JSGlobalObject, value: JSValue) void {
-        TimeoutObject.js.callbackSetCached(thisValue, globalThis, value);
-    }
-
-    pub fn get_idleTimeout(_: *TimeoutObject, thisValue: JSValue, _: *JSGlobalObject) JSValue {
-        return TimeoutObject.js.idleTimeoutGetCached(thisValue).?;
-    }
-
-    pub fn set_idleTimeout(_: *TimeoutObject, thisValue: JSValue, globalThis: *JSGlobalObject, value: JSValue) void {
-        TimeoutObject.js.idleTimeoutSetCached(thisValue, globalThis, value);
-    }
-
-    pub fn get_repeat(_: *TimeoutObject, thisValue: JSValue, _: *JSGlobalObject) JSValue {
-        return TimeoutObject.js.repeatGetCached(thisValue).?;
-    }
-
-    pub fn set_repeat(_: *TimeoutObject, thisValue: JSValue, globalThis: *JSGlobalObject, value: JSValue) void {
-        TimeoutObject.js.repeatSetCached(thisValue, globalThis, value);
-    }
-
-    pub fn dispose(this: *TimeoutObject, globalThis: *JSGlobalObject, _: *JSC.CallFrame) bun.JSError!JSValue {
-        this.internals.cancel(globalThis.bunVM());
-        return .js_undefined;
-    }
-};
-
+pub const TimeoutObject = @import("./Timer/TimeoutObject.zig");
 pub const ImmediateObject = @import("./Timer/ImmediateObject.zig");
 pub const TimerObjectInternals = @import("./Timer/TimerObjectInternals.zig");
 

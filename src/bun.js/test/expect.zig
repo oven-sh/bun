8e2fb5a80ee8881b5b75b05a9a580380a54c2018
Ben Grant <ben@bun.sh>
2025-06-18 14:47:37 -0700

    This has to be anyerror

--- src/bun.js/test/expect.zig
+++ src/bun.js/test/expect.zig
@@ -5465,7 +5465,7 @@ pub const ExpectCustomAsymmetricMatcher = struct {
     }
 
     /// Calls a custom implementation (if provided) to stringify this asymmetric matcher, and returns true if it was provided and it succeed
-    pub fn customPrint(_: *ExpectCustomAsymmetricMatcher, thisValue: JSValue, globalThis: *JSGlobalObject, writer: anytype, comptime dontThrow: bool) bun.JSError!bool {
+    pub fn customPrint(_: *ExpectCustomAsymmetricMatcher, thisValue: JSValue, globalThis: *JSGlobalObject, writer: anytype, comptime dontThrow: bool) !bool {
         const matcher_fn: JSValue = js.matcherFnGetCached(thisValue) orelse return false;
         if (matcher_fn.get(globalThis, "toAsymmetricMatcher") catch |e| return maybeClear(dontThrow, globalThis, e)) |fn_value| {
             if (fn_value.jsType().isFunction()) {

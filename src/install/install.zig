a241d8724ae6449814ec2231a84859b09267997b
pfg <pfg@pfg.pw>
2025-06-24 15:12:40 -0700

    extract ExternalSlice, ExternalStringMap to its own file

--- src/install/install.zig
+++ src/install/install.zig
@@ -129,61 +129,9 @@ pub const Dependency = @import("./dependency.zig");
 pub const Behavior = @import("./dependency.zig").Behavior;
 pub const FolderResolution = @import("./resolvers/folder_resolver.zig").FolderResolution;
 
-pub fn ExternalSlice(comptime Type: type) type {
-    return extern struct {
-        pub const Slice = @This();
-
-        pub const Child: type = Type;
-
-        off: u32 = 0,
-        len: u32 = 0,
-
-        pub const invalid: @This() = .{ .off = std.math.maxInt(u32), .len = std.math.maxInt(u32) };
-
-        pub inline fn isInvalid(this: Slice) bool {
-            return this.off == std.math.maxInt(u32) and this.len == std.math.maxInt(u32);
-        }
-
-        pub inline fn contains(this: Slice, id: u32) bool {
-            return id >= this.off and id < (this.len + this.off);
-        }
-
-        pub inline fn get(this: Slice, in: []const Type) []const Type {
-            if (comptime Environment.allow_assert) {
-                bun.assert(this.off + this.len <= in.len);
-            }
-            // it should be impossible to address this out of bounds due to the minimum here
-            return in.ptr[this.off..@min(in.len, this.off + this.len)];
-        }
-
-        pub inline fn mut(this: Slice, in: []Type) []Type {
-            if (comptime Environment.allow_assert) {
-                bun.assert(this.off + this.len <= in.len);
-            }
-            return in.ptr[this.off..@min(in.len, this.off + this.len)];
-        }
-
-        pub inline fn begin(this: Slice) u32 {
-            return this.off;
-        }
-
-        pub inline fn end(this: Slice) u32 {
-            return this.off + this.len;
-        }
-
-        pub fn init(buf: []const Type, in: []const Type) Slice {
-            // if (comptime Environment.allow_assert) {
-            //     bun.assert(@intFromPtr(buf.ptr) <= @intFromPtr(in.ptr));
-            //     bun.assert((@intFromPtr(in.ptr) + in.len) <= (@intFromPtr(buf.ptr) + buf.len));
-            // }
-
-            return Slice{
-                .off = @as(u32, @truncate((@intFromPtr(in.ptr) - @intFromPtr(buf.ptr)) / @sizeOf(Type))),
-                .len = @as(u32, @truncate(in.len)),
-            };
-        }
-    };
-}
+pub const external = @import("./external.zig");
+pub const ExternalSlice = external.ExternalSlice;
+pub const ExternalStringMap = external.ExternalStringMap;
 
 pub const PackageID = u32;
 pub const DependencyID = u32;
@@ -199,14 +147,9 @@ pub const DependencyID = u32;
 pub const invalid_package_id = std.math.maxInt(PackageID);
 pub const invalid_dependency_id = std.math.maxInt(DependencyID);
 
-pub const ExternalStringList = ExternalSlice(ExternalString);
-pub const ExternalPackageNameHashList = ExternalSlice(PackageNameHash);
-pub const VersionSlice = ExternalSlice(Semver.Version);
-
-pub const ExternalStringMap = extern struct {
-    name: ExternalStringList = .{},
-    value: ExternalStringList = .{},
-};
+pub const ExternalStringList = external.ExternalStringList;
+pub const ExternalPackageNameHashList = external.ExternalPackageNameHashList;
+pub const VersionSlice = external.VersionSlice;
 
 pub const PackageNameAndVersionHash = u64;
 pub const PackageNameHash = u64; // Use String.Builder.stringHash to compute this

4d4ade5001fe87143dce21fc88103a58c43726bb
pfg <pfg@pfg.pw>
2025-06-24 18:27:32 -0700

    install.zig: sort imports

--- src/install/install.zig
+++ src/install/install.zig
@@ -1,50 +1,4 @@
-const bun = @import("bun");
-const string = bun.string;
-const Output = bun.Output;
-const Global = bun.Global;
-const Environment = bun.Environment;
-const strings = bun.strings;
-const MutableString = bun.MutableString;
-const stringZ = bun.stringZ;
-const default_allocator = bun.default_allocator;
-const std = @import("std");
-const JSC = bun.JSC;
-const DirInfo = @import("../resolver/dir_info.zig");
-const File = bun.sys.File;
-const logger = bun.logger;
-const OOM = bun.OOM;
-const FD = bun.FD;
-
-const JSON = bun.JSON;
-const JSPrinter = bun.js_printer;
-
-const Api = @import("../api/schema.zig").Api;
-const Path = bun.path;
-const Command = @import("../cli.zig").Command;
-const BunArguments = @import("../cli.zig").Arguments;
-const transpiler = bun.transpiler;
-
-const DotEnv = @import("../env_loader.zig");
-const which = @import("../which.zig").which;
-const Run = @import("../bun_js.zig").Run;
-const Fs = @import("../fs.zig");
-const FileSystem = Fs.FileSystem;
-const URL = @import("../url.zig").URL;
-const HTTP = bun.http;
-const AsyncHTTP = HTTP.AsyncHTTP;
-
-const HeaderBuilder = HTTP.HeaderBuilder;
-
-pub const ExtractTarball = @import("./extract_tarball.zig");
-pub const Npm = @import("./npm.zig");
-const Syscall = bun.sys;
-const RunCommand = @import("../cli/run_command.zig").RunCommand;
 threadlocal var initialized_store = false;
-
-pub const Lockfile = @import("./lockfile.zig");
-pub const TextLockfile = @import("./lockfile/bun.lock.zig");
-pub const PatchedDep = Lockfile.PatchedDep;
-const Walker = @import("../walker_skippable.zig");
 
 pub const bun_hash_tag = ".bun-tag-";
 pub const max_hex_hash_len: comptime_int = brk: {
@@ -62,15 +16,10 @@ pub fn buntaghashbuf_make(buf: *BuntagHashBuf, patch_hash: u64) [:0]u8 {
     return bunhashtag;
 }
 
-pub const patch = @import("./patch_install.zig");
-pub const PatchTask = patch.PatchTask;
-
 // these bytes are skipped
 // so we just make it repeat bun bun bun bun bun bun bun bun bun
 // because why not
 pub const alignment_bytes_to_repeat_buffer = [_]u8{0} ** 144;
-
-const JSAst = bun.JSAst;
 
 pub fn initializeStore() void {
     if (initialized_store) {
@@ -116,23 +65,6 @@ pub fn initializeMiniStore() void {
     }
 }
 
-pub const IdentityContext = @import("../identity_context.zig").IdentityContext;
-pub const ArrayIdentityContext = @import("../identity_context.zig").ArrayIdentityContext;
-const Semver = bun.Semver;
-const ExternalString = Semver.ExternalString;
-const String = Semver.String;
-const GlobalStringBuilder = bun.StringBuilder;
-const SlicedString = Semver.SlicedString;
-pub const Repository = @import("./repository.zig").Repository;
-pub const Bin = @import("./bin.zig").Bin;
-pub const Dependency = @import("./dependency.zig");
-pub const Behavior = @import("./dependency.zig").Behavior;
-pub const FolderResolution = @import("./resolvers/folder_resolver.zig").FolderResolution;
-
-pub const external = @import("./external.zig");
-pub const ExternalSlice = external.ExternalSlice;
-pub const ExternalStringMap = external.ExternalStringMap;
-
 pub const PackageID = u32;
 pub const DependencyID = u32;
 
@@ -146,10 +78,6 @@ pub const DependencyID = u32;
 
 pub const invalid_package_id = std.math.maxInt(PackageID);
 pub const invalid_dependency_id = std.math.maxInt(DependencyID);
-
-pub const ExternalStringList = external.ExternalStringList;
-pub const ExternalPackageNameHashList = external.ExternalPackageNameHashList;
-pub const VersionSlice = external.VersionSlice;
 
 pub const PackageNameAndVersionHash = u64;
 pub const PackageNameHash = u64; // Use String.Builder.stringHash to compute this
@@ -169,8 +97,6 @@ pub const Aligner = struct {
         return std.mem.alignForward(usize, pos, @alignOf(Type)) - pos;
     }
 };
-
-pub const NetworkTask = @import("NetworkTask.zig");
 
 pub const Origin = enum(u8) {
     local = 0,
@@ -248,8 +174,6 @@ pub const PreinstallState = enum(u4) {
     applying_patch,
 };
 
-pub const Task = @import("Task.zig");
-
 pub const ExtractData = struct {
     url: string = "",
     resolved: string = "",
@@ -258,11 +182,6 @@ pub const ExtractData = struct {
         buf: []u8 = "",
     } = null,
 };
-
-pub const PackageInstall = @import("./PackageInstall.zig").PackageInstall;
-
-pub const Resolution = @import("./resolution.zig").Resolution;
-const Progress = bun.Progress;
 
 pub const DependencyInstallContext = struct {
     tree_id: Lockfile.Tree.Id = 0,
@@ -277,15 +196,10 @@ pub const TaskCallbackContext = union(enum) {
     root_request_id: PackageID,
 };
 
-pub const PackageManifestMap = @import("PackageManifestMap.zig");
-
 // We can't know all the packages we need until we've downloaded all the packages
 // The easy way would be:
 // 1. Download all packages, parsing their dependencies and enqueuing all dependencies for resolution
 // 2.
-pub const PackageManager = @import("PackageManager.zig");
-
-const Package = Lockfile.Package;
 
 pub const PackageManifestError = error{
     PackageManifestHTTP400,
@@ -297,4 +211,47 @@ pub const PackageManifestError = error{
     PackageManifestHTTP5xx,
 };
 
+pub const ExtractTarball = @import("./extract_tarball.zig");
+pub const NetworkTask = @import("NetworkTask.zig");
+pub const Npm = @import("./npm.zig");
+pub const PackageManager = @import("PackageManager.zig");
+pub const PackageManifestMap = @import("PackageManifestMap.zig");
+pub const Task = @import("Task.zig");
+pub const TextLockfile = @import("./lockfile/bun.lock.zig");
+const std = @import("std");
+pub const Bin = @import("./bin.zig").Bin;
+pub const FolderResolution = @import("./resolvers/folder_resolver.zig").FolderResolution;
 pub const LifecycleScriptSubprocess = @import("./lifecycle_script_runner.zig").LifecycleScriptSubprocess;
+pub const PackageInstall = @import("./PackageInstall.zig").PackageInstall;
+pub const Repository = @import("./repository.zig").Repository;
+pub const Resolution = @import("./resolution.zig").Resolution;
+
+pub const ArrayIdentityContext = @import("../identity_context.zig").ArrayIdentityContext;
+pub const IdentityContext = @import("../identity_context.zig").IdentityContext;
+
+pub const Dependency = @import("./dependency.zig");
+pub const Behavior = @import("./dependency.zig").Behavior;
+
+pub const external = @import("./external.zig");
+pub const ExternalPackageNameHashList = external.ExternalPackageNameHashList;
+pub const ExternalSlice = external.ExternalSlice;
+pub const ExternalStringList = external.ExternalStringList;
+pub const ExternalStringMap = external.ExternalStringMap;
+pub const VersionSlice = external.VersionSlice;
+
+pub const Lockfile = @import("./lockfile.zig");
+const Package = Lockfile.Package;
+pub const PatchedDep = Lockfile.PatchedDep;
+
+pub const patch = @import("./patch_install.zig");
+pub const PatchTask = patch.PatchTask;
+
+const bun = @import("bun");
+const HTTP = bun.http;
+const JSAst = bun.JSAst;
+const default_allocator = bun.default_allocator;
+const string = bun.string;
+
+const Semver = bun.Semver;
+const ExternalString = Semver.ExternalString;
+const String = Semver.String;

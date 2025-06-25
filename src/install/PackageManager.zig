e70cd49b48ff6468787faca8e9e3fe97ac750b32
pfg <pfg@pfg.pw>
2025-06-25 15:04:15 -0700

    extract preinstall fns

--- src/install/PackageManager.zig
+++ src/install/PackageManager.zig
@@ -642,116 +642,6 @@ pub fn formatLaterVersionInCache(
     }
 }
 
-pub fn ensurePreinstallStateListCapacity(this: *PackageManager, count: usize) void {
-    if (this.preinstall_state.items.len >= count) {
-        return;
-    }
-
-    const offset = this.preinstall_state.items.len;
-    this.preinstall_state.ensureTotalCapacity(this.allocator, count) catch bun.outOfMemory();
-    this.preinstall_state.expandToCapacity();
-    @memset(this.preinstall_state.items[offset..], PreinstallState.unknown);
-}
-
-pub fn setPreinstallState(this: *PackageManager, package_id: PackageID, lockfile: *Lockfile, value: PreinstallState) void {
-    this.ensurePreinstallStateListCapacity(lockfile.packages.len);
-    this.preinstall_state.items[package_id] = value;
-}
-
-pub fn getPreinstallState(this: *PackageManager, package_id: PackageID) PreinstallState {
-    if (package_id >= this.preinstall_state.items.len) {
-        return PreinstallState.unknown;
-    }
-    return this.preinstall_state.items[package_id];
-}
-
-pub fn determinePreinstallState(
-    manager: *PackageManager,
-    pkg: Package,
-    lockfile: *Lockfile,
-    out_name_and_version_hash: *?u64,
-    out_patchfile_hash: *?u64,
-) PreinstallState {
-    switch (manager.getPreinstallState(pkg.meta.id)) {
-        .unknown => {
-
-            // Do not automatically start downloading packages which are disabled
-            // i.e. don't download all of esbuild's versions or SWCs
-            if (pkg.isDisabled()) {
-                manager.setPreinstallState(pkg.meta.id, lockfile, .done);
-                return .done;
-            }
-
-            const patch_hash: ?u64 = brk: {
-                if (manager.lockfile.patched_dependencies.entries.len == 0) break :brk null;
-                var sfb = std.heap.stackFallback(1024, manager.lockfile.allocator);
-                const name_and_version = std.fmt.allocPrint(
-                    sfb.get(),
-                    "{s}@{}",
-                    .{
-                        pkg.name.slice(manager.lockfile.buffers.string_bytes.items),
-                        pkg.resolution.fmt(manager.lockfile.buffers.string_bytes.items, .posix),
-                    },
-                ) catch unreachable;
-                const name_and_version_hash = String.Builder.stringHash(name_and_version);
-                const patched_dep = manager.lockfile.patched_dependencies.get(name_and_version_hash) orelse break :brk null;
-                defer out_name_and_version_hash.* = name_and_version_hash;
-                if (patched_dep.patchfile_hash_is_null) {
-                    manager.setPreinstallState(pkg.meta.id, manager.lockfile, .calc_patch_hash);
-                    return .calc_patch_hash;
-                }
-                out_patchfile_hash.* = patched_dep.patchfileHash().?;
-                break :brk patched_dep.patchfileHash().?;
-            };
-
-            const folder_path = switch (pkg.resolution.tag) {
-                .git => manager.cachedGitFolderNamePrintAuto(&pkg.resolution.value.git, patch_hash),
-                .github => manager.cachedGitHubFolderNamePrintAuto(&pkg.resolution.value.github, patch_hash),
-                .npm => manager.cachedNPMPackageFolderName(lockfile.str(&pkg.name), pkg.resolution.value.npm.version, patch_hash),
-                .local_tarball => manager.cachedTarballFolderName(pkg.resolution.value.local_tarball, patch_hash),
-                .remote_tarball => manager.cachedTarballFolderName(pkg.resolution.value.remote_tarball, patch_hash),
-                else => "",
-            };
-
-            if (folder_path.len == 0) {
-                manager.setPreinstallState(pkg.meta.id, lockfile, .extract);
-                return .extract;
-            }
-
-            if (manager.isFolderInCache(folder_path)) {
-                manager.setPreinstallState(pkg.meta.id, lockfile, .done);
-                return .done;
-            }
-
-            // If the package is patched, then `folder_path` looks like:
-            // is-even@1.0.0_patch_hash=abc8s6dedhsddfkahaldfjhlj
-            //
-            // If that's not in the cache, we need to put it there:
-            // 1. extract the non-patched pkg in the cache
-            // 2. copy non-patched pkg into temp dir
-            // 3. apply patch to temp dir
-            // 4. rename temp dir to `folder_path`
-            if (patch_hash != null) {
-                const non_patched_path_ = folder_path[0 .. std.mem.indexOf(u8, folder_path, "_patch_hash=") orelse @panic("Expected folder path to contain `patch_hash=`, this is a bug in Bun. Please file a GitHub issue.")];
-                const non_patched_path = manager.lockfile.allocator.dupeZ(u8, non_patched_path_) catch bun.outOfMemory();
-                defer manager.lockfile.allocator.free(non_patched_path);
-                if (manager.isFolderInCache(non_patched_path)) {
-                    manager.setPreinstallState(pkg.meta.id, manager.lockfile, .apply_patch);
-                    // yay step 1 is already done for us
-                    return .apply_patch;
-                }
-                // we need to extract non-patched pkg into the cache
-                manager.setPreinstallState(pkg.meta.id, lockfile, .extract);
-                return .extract;
-            }
-
-            manager.setPreinstallState(pkg.meta.id, lockfile, .extract);
-            return .extract;
-        },
-        else => |val| return val,
-    }
-}
-
 pub fn scopeForPackageName(this: *const PackageManager, name: string) *const Npm.Registry.Scope {
     if (name.len == 0 or name[0] != '@') return &this.options.scope;
     return this.options.registries.getPtr(
@@ -1886,6 +1776,12 @@ pub const enqueuePatchTask = enqueue.enqueuePatchTask;
 pub const enqueuePatchTaskPre = enqueue.enqueuePatchTaskPre;
 pub const enqueueTarballForDownload = enqueue.enqueueTarballForDownload;
 pub const enqueueTarballForReading = enqueue.enqueueTarballForReading;
+
+const preinstall = @import("PackageManager/PackageManagerPreinstall.zig");
+pub const determinePreinstallState = preinstall.determinePreinstallState;
+pub const ensurePreinstallStateListCapacity = preinstall.ensurePreinstallStateListCapacity;
+pub const getPreinstallState = preinstall.getPreinstallState;
+pub const setPreinstallState = preinstall.setPreinstallState;
 
 pub const progress_zig = @import("PackageManager/ProgressStrings.zig");
 pub const ProgressStrings = progress_zig.ProgressStrings;

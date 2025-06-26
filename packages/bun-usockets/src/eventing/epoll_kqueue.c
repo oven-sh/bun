59af63cf4e8af2f9c29be3b4d75240d16f5e7f06
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-06-26 13:23:46 -0700

    tsy

--- packages/bun-usockets/src/eventing/epoll_kqueue.c
+++ packages/bun-usockets/src/eventing/epoll_kqueue.c
@@ -384,21 +384,15 @@ struct us_poll_t *us_poll_resize(struct us_poll_t *p, struct us_loop_t *loop, un
 
     struct us_poll_t *new_p = us_realloc(p, sizeof(struct us_poll_t) + ext_size);
     if (p != new_p) {
-        if (events) {
 #ifdef LIBUS_USE_EPOLL
             /* Hack: forcefully update poll by stripping away already set events */
             new_p->state.poll_type = us_internal_poll_type(new_p);
             us_poll_change(new_p, loop, events);
 #else
             /* Forcefully update poll by resetting them with new_p as user data */
-            kqueue_change(loop->fd, new_p->state.fd, 0, events, new_p);
+            kqueue_change(loop->fd, new_p->state.fd, 0, LIBUS_SOCKET_WRITABLE | LIBUS_SOCKET_READABLE, new_p);
 #endif      /* This is needed for epoll also (us_change_poll doesn't update the old poll) */
             us_internal_loop_update_pending_ready_polls(loop, p, new_p, events, events);
-        } else {
-            /* If we dont have events just make sure that the poll is removed from pending ready polls */
-            us_internal_loop_update_pending_ready_polls(loop, p, 0, events, events);
-        }
-      
     }
 
     return new_p;

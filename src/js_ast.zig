f58667d9ee65ce6cb6a06f40a5a6fdf6618cb3c9
pfg <pfg@pfg.pw>
2025-06-25 17:08:01 -0700

    extract B and TS from js_ast

--- src/js_ast.zig
+++ src/js_ast.zig
@@ -115,95 +115,6 @@ pub const Flags = struct {
     };
 };
 
-/// B is for Binding! Bindings are on the left side of variable
-/// declarations (s_local), which is how destructuring assignments
-/// are represented in memory. Consider a basic example.
-///
-///     let hello = world;
-///         ^       ^
-///         |       E.Identifier
-///         B.Identifier
-///
-/// Bindings can be nested
-///
-///                B.Array
-///                | B.Identifier
-///                | |
-///     let { foo: [ bar ] } = ...
-///         ----------------
-///         B.Object
-pub const B = union(Binding.Tag) {
-    // let x = ...
-    b_identifier: *B.Identifier,
-    // let [a, b] = ...
-    b_array: *B.Array,
-    // let { a, b: c } = ...
-    b_object: *B.Object,
-    // this is used to represent array holes
-    b_missing: B.Missing,
-
-    pub const Identifier = struct {
-        ref: Ref,
-    };
-
-    pub const Property = struct {
-        flags: Flags.Property.Set = Flags.Property.None,
-        key: ExprNodeIndex,
-        value: Binding,
-        default_value: ?Expr = null,
-    };
-
-    pub const Object = struct {
-        properties: []B.Property,
-        is_single_line: bool = false,
-
-        pub const Property = B.Property;
-    };
-
-    pub const Array = struct {
-        items: []ArrayBinding,
-        has_spread: bool = false,
-        is_single_line: bool = false,
-
-        pub const Item = ArrayBinding;
-    };
-
-    pub const Missing = struct {};
-
-    /// This hash function is currently only used for React Fast Refresh transform.
-    /// This doesn't include the `is_single_line` properties, as they only affect whitespace.
-    pub fn writeToHasher(b: B, hasher: anytype, symbol_table: anytype) void {
-        switch (b) {
-            .b_identifier => |id| {
-                const original_name = id.ref.getSymbol(symbol_table).original_name;
-                writeAnyToHasher(hasher, .{ std.meta.activeTag(b), original_name.len });
-            },
-            .b_array => |array| {
-                writeAnyToHasher(hasher, .{ std.meta.activeTag(b), array.has_spread, array.items.len });
-                for (array.items) |item| {
-                    writeAnyToHasher(hasher, .{item.default_value != null});
-                    if (item.default_value) |default| {
-                        default.data.writeToHasher(hasher, symbol_table);
-                    }
-                    item.binding.data.writeToHasher(hasher, symbol_table);
-                }
-            },
-            .b_object => |object| {
-                writeAnyToHasher(hasher, .{ std.meta.activeTag(b), object.properties.len });
-                for (object.properties) |property| {
-                    writeAnyToHasher(hasher, .{ property.default_value != null, property.flags });
-                    if (property.default_value) |default| {
-                        default.data.writeToHasher(hasher, symbol_table);
-                    }
-                    property.key.data.writeToHasher(hasher, symbol_table);
-                    property.value.data.writeToHasher(hasher, symbol_table);
-                }
-            },
-            .b_missing => {},
-        }
-    }
-};
-
 pub const ClauseItem = struct {
     alias: string,
     alias_loc: logger.Loc = logger.Loc.Empty,
@@ -332,134 +243,6 @@ pub const TlaCheck = struct {
 pub const Span = struct {
     text: string = "",
     range: logger.Range = .{},
-};
-
-/// This is for TypeScript "enum" and "namespace" blocks. Each block can
-/// potentially be instantiated multiple times. The exported members of each
-/// block are merged into a single namespace while the non-exported code is
-/// still scoped to just within that block:
-///
-///    let x = 1;
-///    namespace Foo {
-///      let x = 2;
-///      export let y = 3;
-///    }
-///    namespace Foo {
-///      console.log(x); // 1
-///      console.log(y); // 3
-///    }
-///
-/// Doing this also works inside an enum:
-///
-///    enum Foo {
-///      A = 3,
-///      B = A + 1,
-///    }
-///    enum Foo {
-///      C = A + 2,
-///    }
-///    console.log(Foo.B) // 4
-///    console.log(Foo.C) // 5
-///
-/// This is a form of identifier lookup that works differently than the
-/// hierarchical scope-based identifier lookup in JavaScript. Lookup now needs
-/// to search sibling scopes in addition to parent scopes. This is accomplished
-/// by sharing the map of exported members between all matching sibling scopes.
-pub const TSNamespaceScope = struct {
-    /// This is specific to this namespace block. It's the argument of the
-    /// immediately-invoked function expression that the namespace block is
-    /// compiled into:
-    ///
-    ///   var ns;
-    ///   (function (ns2) {
-    ///     ns2.x = 123;
-    ///   })(ns || (ns = {}));
-    ///
-    /// This variable is "ns2" in the above example. It's the symbol to use when
-    /// generating property accesses off of this namespace when it's in scope.
-    arg_ref: Ref,
-
-    /// This is shared between all sibling namespace blocks
-    exported_members: *TSNamespaceMemberMap,
-
-    /// This is a lazily-generated map of identifiers that actually represent
-    /// property accesses to this namespace's properties. For example:
-    ///
-    ///   namespace x {
-    ///     export let y = 123
-    ///   }
-    ///   namespace x {
-    ///     export let z = y
-    ///   }
-    ///
-    /// This should be compiled into the following code:
-    ///
-    ///   var x;
-    ///   (function(x2) {
-    ///     x2.y = 123;
-    ///   })(x || (x = {}));
-    ///   (function(x3) {
-    ///     x3.z = x3.y;
-    ///   })(x || (x = {}));
-    ///
-    /// When we try to find the symbol "y", we instead return one of these lazily
-    /// generated proxy symbols that represent the property access "x3.y". This
-    /// map is unique per namespace block because "x3" is the argument symbol that
-    /// is specific to that particular namespace block.
-    property_accesses: bun.StringArrayHashMapUnmanaged(Ref) = .{},
-
-    /// Even though enums are like namespaces and both enums and namespaces allow
-    /// implicit references to properties of sibling scopes, they behave like
-    /// separate, er, namespaces. Implicit references only work namespace-to-
-    /// namespace and enum-to-enum. They do not work enum-to-namespace. And I'm
-    /// not sure what's supposed to happen for the namespace-to-enum case because
-    /// the compiler crashes: https://github.com/microsoft/TypeScript/issues/46891.
-    /// So basically these both work:
-    ///
-    ///   enum a { b = 1 }
-    ///   enum a { c = b }
-    ///
-    ///   namespace x { export let y = 1 }
-    ///   namespace x { export let z = y }
-    ///
-    /// This doesn't work:
-    ///
-    ///   enum a { b = 1 }
-    ///   namespace a { export let c = b }
-    ///
-    /// And this crashes the TypeScript compiler:
-    ///
-    ///   namespace a { export let b = 1 }
-    ///   enum a { c = b }
-    ///
-    /// Therefore we only allow enum/enum and namespace/namespace interactions.
-    is_enum_scope: bool,
-};
-
-pub const TSNamespaceMemberMap = bun.StringArrayHashMapUnmanaged(TSNamespaceMember);
-
-pub const TSNamespaceMember = struct {
-    loc: logger.Loc,
-    data: Data,
-
-    pub const Data = union(enum) {
-        /// "namespace ns { export let it }"
-        property,
-        /// "namespace ns { export namespace it {} }"
-        namespace: *TSNamespaceMemberMap,
-        /// "enum ns { it }"
-        enum_number: f64,
-        /// "enum ns { it = 'it' }"
-        enum_string: *E.String,
-        /// "enum ns { it = something() }"
-        enum_property: void,
-
-        pub fn isEnum(data: Data) bool {
-            return switch (data) {
-                inline else => |_, tag| comptime std.mem.startsWith(u8, @tagName(tag), "enum_"),
-            };
-        }
-    };
 };
 
 /// Inlined enum values can only be numbers and strings
@@ -872,12 +655,18 @@ pub const Stmt = @import("ast/Stmt.zig");
 pub const StmtNodeIndex = Stmt;
 pub const Symbol = @import("ast/Symbol.zig");
 const std = @import("std");
+pub const B = @import("ast/B.zig").B;
 pub const NewStore = @import("ast/NewStore.zig").NewStore;
 const TypeScript = @import("./js_parser.zig").TypeScript;
 pub const UseDirective = @import("ast/UseDirective.zig").UseDirective;
 
 pub const CharFreq = @import("ast/CharFreq.zig");
 const char_freq_count = CharFreq.char_freq_count;
+
+pub const TS = @import("ast/TS.zig");
+pub const TSNamespaceMember = TS.TSNamespaceMember;
+pub const TSNamespaceMemberMap = TS.TSNamespaceMemberMap;
+pub const TSNamespaceScope = TS.TSNamespaceScope;
 
 pub const Index = @import("ast/base.zig").Index;
 pub const Ref = @import("ast/base.zig").Ref;
@@ -890,4 +679,3 @@ const Output = bun.Output;
 const logger = bun.logger;
 const string = bun.string;
 const strings = bun.strings;
-const writeAnyToHasher = bun.writeAnyToHasher;

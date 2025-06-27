ac5ef4071c567db1957f4f22df28bd757738e33d
pfg <pfg@pfg.pw>
2025-06-27 14:58:29 -0700

    remove unused imports in visit.zig

--- src/ast/visit.zig
+++ src/ast/visit.zig
@@ -1318,12 +1318,13 @@ pub fn fnBodyContainsUseStrict(body: []Stmt) ?logger.Loc {
     return null;
 }
 
-// @sortImports @noRemoveUnused
+// @sortImports
 
 const bun = @import("bun");
 const Environment = bun.Environment;
 const Output = bun.Output;
 const assert = bun.assert;
+const js_lexer = bun.js_lexer;
 const logger = bun.logger;
 const string = bun.string;
 const strings = bun.strings;
@@ -1332,19 +1333,15 @@ const js_ast = bun.JSAst;
 const B = js_ast.B;
 const Binding = js_ast.Binding;
 const BindingNodeIndex = js_ast.BindingNodeIndex;
-const BindingNodeList = js_ast.BindingNodeList;
 const DeclaredSymbol = js_ast.DeclaredSymbol;
 const E = js_ast.E;
 const Expr = js_ast.Expr;
-const ExprNodeIndex = js_ast.ExprNodeIndex;
 const ExprNodeList = js_ast.ExprNodeList;
-const Flags = js_ast.Flags;
 const LocRef = js_ast.LocRef;
 const S = js_ast.S;
 const Scope = js_ast.Scope;
 const Stmt = js_ast.Stmt;
 const StmtNodeIndex = js_ast.StmtNodeIndex;
-const StmtNodeList = js_ast.StmtNodeList;
 const Symbol = js_ast.Symbol;
 
 const G = js_ast.G;
@@ -1352,58 +1349,25 @@ const Arg = G.Arg;
 const Decl = G.Decl;
 const Property = G.Property;
 
-const Op = js_ast.Op;
-const Level = js_ast.Op.Level;
-
-const SymbolPropertyUseMap = js_ast.Part.SymbolPropertyUseMap;
-const SymbolUseMap = js_ast.Part.SymbolUseMap;
-
-const js_lexer = bun.js_lexer;
-const T = js_lexer.T;
-
 const js_parser = bun.js_parser;
-const AsyncPrefixExpression = js_parser.AsyncPrefixExpression;
-const DeferredErrors = js_parser.DeferredErrors;
-const DeferredTsDecorators = js_parser.DeferredTsDecorators;
 const ExprIn = js_parser.ExprIn;
 const FnOnlyDataVisit = js_parser.FnOnlyDataVisit;
-const FnOrArrowDataParse = js_parser.FnOrArrowDataParse;
 const FnOrArrowDataVisit = js_parser.FnOrArrowDataVisit;
-const IdentifierOpts = js_parser.IdentifierOpts;
 const ImportItemForNamespaceMap = js_parser.ImportItemForNamespaceMap;
-const ImportKind = js_parser.ImportKind;
 const JSXTransformType = js_parser.JSXTransformType;
-const KnownGlobal = js_parser.KnownGlobal;
-const ParenExprOpts = js_parser.ParenExprOpts;
-const ParseClassOptions = js_parser.ParseClassOptions;
-const ParseStatementOptions = js_parser.ParseStatementOptions;
-const ParsedPath = js_parser.ParsedPath;
-const Prefill = js_parser.Prefill;
 const PrependTempRefsOpts = js_parser.PrependTempRefsOpts;
-const PropertyOpts = js_parser.PropertyOpts;
-const ReactRefresh = js_parser.ReactRefresh;
 const Ref = js_parser.Ref;
-const RelocateVars = js_parser.RelocateVars;
 const RuntimeFeatures = js_parser.RuntimeFeatures;
 const SideEffects = js_parser.SideEffects;
-const StmtList = js_parser.StmtList;
 const StmtsKind = js_parser.StmtsKind;
 const StringVoidMap = js_parser.StringVoidMap;
 const TempRef = js_parser.TempRef;
-const ThenCatchChain = js_parser.ThenCatchChain;
-const TransposeState = js_parser.TransposeState;
-const TypeParameterFlag = js_parser.TypeParameterFlag;
 const TypeScript = js_parser.TypeScript;
 const VisitArgsOpts = js_parser.VisitArgsOpts;
-const floatToInt32 = js_parser.floatToInt32;
-const fs = js_parser.fs;
 const isEvalOrArguments = js_parser.isEvalOrArguments;
 const options = js_parser.options;
-const statementCaresAboutScope = js_parser.statementCaresAboutScope;
 
 const std = @import("std");
 const AutoHashMap = std.AutoHashMap;
 const List = std.ArrayListUnmanaged;
 const ListManaged = std.ArrayList;
-const Map = std.AutoHashMapUnmanaged;
-const Allocator = std.mem.Allocator;

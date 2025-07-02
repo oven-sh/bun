b8ca277ff81bd9b56a16ae9978083abed13a5550
RiskyMH <git@riskymh.dev>
2025-07-03 00:04:15 +1000

    add `module-sync` to default export conditions
    for bun it doesn't mean as much because we already support require(esm), but for compatibility we should still read it
    
    see https://github.com/nodejs/node/blob/main/doc/api/packages.md#conditional-exports

--- src/resolver/package_json.zig
+++ src/resolver/package_json.zig
@@ -1758,10 +1758,10 @@ pub const ESModule = struct {
 
                         if (strings.eqlComptime(key, "import")) {
                             r.module_type.* = .esm;
-                        }
-
-                        if (strings.eqlComptime(key, "require")) {
+                        } else if (strings.eqlComptime(key, "require")) {
                             r.module_type.* = .cjs;
+                        } else if (strings.eqlComptime(key, "module-sync")) {
+                            r.module_type.* = .esm;
                         }
 
                         return result;
@@ -1998,6 +1998,8 @@ pub const ESModule = struct {
                                 r.module_type.* = .esm;
                             } else if (strings.eqlComptime(map_key, "require")) {
                                 r.module_type.* = .cjs;
+                            } else if (strings.eqlComptime(map_key, "module-sync")) {
+                                r.module_type.* = .esm;
                             }
 
                             return result;

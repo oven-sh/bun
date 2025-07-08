180afe5f549112d64ebeaf44c52ca3a5ae98b307
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-07-07 18:41:01 -0700

    refactor(postgres) improve postgres code base (#20808)
    Co-authored-by: cirospaciari <6379399+cirospaciari@users.noreply.github.com>

---
+++ src/sql/postgres/types/bool.zig
@@ -0,0 +1,20 @@
+pub const to = 16;
+pub const from = [_]short{16};
+
+pub fn toJS(
+    _: *JSC.JSGlobalObject,
+    value: bool,
+) AnyPostgresError!JSValue {
+    return JSValue.jsBoolean(value);
+}
+
+// @sortImports
+
+const bun = @import("bun");
+const AnyPostgresError = @import("../AnyPostgresError.zig").AnyPostgresError;
+
+const int_types = @import("./int_types.zig");
+const short = int_types.short;
+
+const JSC = bun.JSC;
+const JSValue = JSC.JSValue;

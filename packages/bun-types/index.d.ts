f2b5b579c78fe12f1fa3cd1126c7721e1fc0b1c4
Alistair Smith <hi@alistair.sh>
2025-07-10 15:12:09 -0700

    remove old comment

--- packages/bun-types/index.d.ts
+++ packages/bun-types/index.d.ts
@@ -25,9 +25,6 @@
 
 /// <reference path="./bun.ns.d.ts" />
 
-// Must disable this so it doesn't conflict with the DOM onmessage type, but still
-// allows us to declare our own globals that Node's types can "see" and not conflict with
-
 // We must declare that `onmessage` exists globally since many of Node's declarations
 // fallback to the globally available versions when, which Bun overrides. It detects
 // if the symbols are available by looking for a declaration of `onmessage`.

38c192a09d3acf8ea769750489a14ff0d1ae10f1
Meghan Denny <meghan@bun.sh>
2025-07-11 17:58:23 -0700

    3 hops this time

--- scripts/runner.node.mjs
+++ scripts/runner.node.mjs
@@ -134,7 +134,7 @@ const { values: options, positionals: filters } = parseArgs({
     },
     ["retries"]: {
       type: "string",
-      default: isCI ? "0" : "0", // N retries = N+1 attempts
+      default: isCI ? "3" : "0", // N retries = N+1 attempts
     },
     ["junit"]: {
       type: "boolean",

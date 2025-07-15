af15c690cc166342502de02708106872cc7392b9
Kai Tamkun <kai@tamkun.io>
2025-07-14 20:47:02 -0700

    Possible 09748 fix

--- src/string.zig
+++ src/string.zig
@@ -778,7 +778,7 @@ pub const String = extern struct {
     pub fn toThreadSafeSlice(this: *const String, allocator: std.mem.Allocator) SliceWithUnderlyingString {
         if (this.tag == .WTFStringImpl) {
             if (!this.value.WTFStringImpl.isThreadSafe()) {
-                const slice = this.value.WTFStringImpl.toUTF8WithoutRef(allocator);
+                const slice = this.value.WTFStringImpl.toUTF8(allocator);
 
                 if (slice.allocator.isNull()) {
                     // this was a WTF-allocated string
@@ -790,8 +790,8 @@ pub const String = extern struct {
                 }
 
                 if (comptime bun.Environment.allow_assert) {
-                    bun.assert(!isWTFAllocator(slice.allocator.get().?)); // toUTF8WithoutRef() should never return a WTF allocator
-                    bun.assert(slice.allocator.get().?.vtable == allocator.vtable); // assert that the allocator is the same
+                    // bun.assert(!isWTFAllocator(slice.allocator.get().?)); // toUTF8WithoutRef() should never return a WTF allocator
+                    // bun.assert(slice.allocator.get().?.vtable == allocator.vtable); // assert that the allocator is the same
                 }
 
                 // We've already cloned the string, so let's just return the slice.

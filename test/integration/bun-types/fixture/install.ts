0ec8e9d469ab8b9365e1df86d9852f8ae7984dab
Alistair Smith <hi@alistair.sh>
2025-07-16 15:16:24 -0700

    sketch types for Bun.Install.Security

---
+++ test/integration/bun-types/fixture/install.ts
@@ -0,0 +1,43 @@
+// This is (for now) very loose implementation reference, mostly type testing
+
+import { expectType } from "./utilities";
+
+const mySecurityScanner: Bun.Install.Security.Provider = {
+  version: "1",
+  onInstall: async ({ packages }: Bun.Install.Security.OnInstallInfo) => {
+    const response = await fetch("https://threat-feed.example.com");
+
+    if (!response.ok) {
+      throw new Error("Unable to fetch threat feed");
+    }
+
+    // Would recommend using Zod or something to validate here.
+    // This code needs to be defensive...
+    const myThreatFeed = (await response.json()) as Array<{
+      package: string;
+      version: string;
+      url: string;
+      description: string;
+      category: "unhealthy" | "spam" | "malware"; // Imagine some other categories...
+    }>;
+
+    return myThreatFeed.flatMap((threat): Bun.Install.Security.Advisory[] => {
+      const match = packages.some(p => p.name === threat.package && p.version === threat.version);
+
+      if (!match) {
+        return [];
+      }
+
+      return [
+        {
+          level: threat.category === "malware" ? "fatal" : "warn",
+          package: threat.package,
+          url: threat.url,
+          description: threat.description,
+        },
+      ];
+    });
+  },
+};
+
+expectType(mySecurityScanner).toBeDefined();

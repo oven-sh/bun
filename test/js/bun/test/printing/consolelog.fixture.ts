e520accfd29bf060cc946d888d49cec0d3d48f96
pfg <pfg@pfg.pw>
2025-07-15 20:26:38 -0700

    ,,

--- test/js/bun/test/printing/consolelog.fixture.ts
+++ test/js/bun/test/printing/consolelog.fixture.ts
@@ -2,5 +2,6 @@ console.log("--- begin ---");
 console.log({
   a: "a",
   multiline: 'pub fn main() !void {\n    std.log.info("Hello, {s}", .{name});\n}',
+  error: new Error("Hello, world!"),
 });
 console.log("--- end ---");
--- test/js/bun/test/printing/consolelogexample.test.ts
+++ test/js/bun/test/printing/consolelogexample.test.ts
@@ -18,6 +18,19 @@ test("console.log output", async () => {
       multiline: "pub fn main() !void {
            std.log.info(\\"Hello, {s}\\", .{name});
        }",
+      error: 1 | console.log("--- begin ---");
+    2 | console.log({
+    3 |   a: "a",
+    4 |   multiline: 'pub fn main() !void {\\n    std.log.info("Hello, {s}", .{name});\\n}',
+    5 |   error: new Error("Hello, world!"),
+                 ^
+    error: Hello, world!
+          at /Users/pfg/Dev/Node/bun/test/js/bun/test/printing/consolelog.fixture.ts:5:10
+          at loadAndEvaluateModule (7:44)
+          at asyncFunctionResume (9:85)
+          at promiseReactionJobWithoutPromiseUnwrapAsyncContext (14:20)
+          at promiseReactionJob (31:60)
+    ,
     }
     --- end ---
     "

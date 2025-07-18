f88301cda184c85cca20786f45ad5b5637f135b6
pfg <pfg@pfg.pw>
2025-07-18 16:05:08 -0700

    printDiff update

--- src/bun.js/test/diff/printDiff.zig
+++ src/bun.js/test/diff/printDiff.zig
@@ -1,14 +1,20 @@
 const DMP = diff_match_patch.DMP(u8);
 const DMPUsize = diff_match_patch.DMP(usize);
 
+pub const DiffConfig = struct {
+    min_bytes_before_chunking: usize = 2 * 1024, // 2kB
+    chunk_context_lines: usize = 3,
+    enable_ansi_colors: bool,
+};
+
 fn removeTrailingNewline(text: []const u8) []const u8 {
     if (!std.mem.endsWith(u8, text, "\n")) return text;
     return text[0 .. text.len - 1];
 }
 
-pub fn printDiffMain(arena: std.mem.Allocator, not: bool, received_slice: []const u8, expected_slice: []const u8, writer: anytype, enable_ansi_colors: bool) !void {
+pub fn printDiffMain(arena: std.mem.Allocator, not: bool, received_slice: []const u8, expected_slice: []const u8, writer: anytype, config: DiffConfig) !void {
     if (not) {
-        switch (enable_ansi_colors) {
+        switch (config.enable_ansi_colors) {
             true => try writer.print("Expected: not " ++ colors.green ++ "{s}" ++ colors.reset, .{expected_slice}),
             false => try writer.print("Expected: not {s}", .{expected_slice}),
         }
@@ -65,65 +71,67 @@ pub fn printDiffMain(arena: std.mem.Allocator, not: bool, received_slice: []cons
         diff_segment.inserted = removeTrailingNewline(diff_segment.inserted);
     };
 
-    // Split 'equal' segments into lines
-    var new_diff_segments = std.ArrayList(DiffSegment).init(arena);
-    for (diff_segments.items) |diff_segment| {
-        if (diff_segment.mode == .equal) {
-            var split = std.mem.splitScalar(u8, diff_segment.removed, '\n');
-            while (split.next()) |line| {
-                try new_diff_segments.append(DiffSegment{
-                    .removed = line,
-                    .inserted = line,
-                    .mode = .equal,
-                    .skip = true,
-                });
-            }
-        } else {
-            try new_diff_segments.append(diff_segment);
-        }
-    }
+    // Determine if the diff needs to be chunked
+    if (expected_slice.len > config.min_bytes_before_chunking or received_slice.len > config.min_bytes_before_chunking) {
+        // Split 'equal' segments into lines
+        var new_diff_segments = std.ArrayList(DiffSegment).init(arena);
 
-    const CONTEXT_LINES = 3;
-    // unskip segments within CONTEXT_LINES of a non-equal segment
-
-    // Forward pass: unskip segments after non-equal segments
-    for (new_diff_segments.items, 0..) |segment, i| {
-        if (segment.mode != .equal) {
-            const end = @min(i + CONTEXT_LINES + 1, new_diff_segments.items.len);
-            for (new_diff_segments.items[i..end]) |*seg| {
-                seg.skip = false;
+        for (diff_segments.items) |diff_segment| {
+            if (diff_segment.mode == .equal) {
+                var split = std.mem.splitScalar(u8, diff_segment.removed, '\n');
+                while (split.next()) |line| {
+                    try new_diff_segments.append(DiffSegment{
+                        .removed = line,
+                        .inserted = line,
+                        .mode = .equal,
+                        .skip = true,
+                    });
+                }
+            } else {
+                try new_diff_segments.append(diff_segment);
             }
         }
-    }
 
-    {
-        // Reverse pass: unskip segments before non-equal segments
-        var i = new_diff_segments.items.len;
-        while (i > 0) {
-            i -= 1;
-            const segment = new_diff_segments.items[i];
+        diff_segments = new_diff_segments;
+
+        // Forward pass: unskip segments after non-equal segments
+        for (diff_segments.items, 0..) |segment, i| {
             if (segment.mode != .equal) {
-                const start = if (i >= CONTEXT_LINES) i - CONTEXT_LINES else 0;
-                for (new_diff_segments.items[start .. i + 1]) |*seg| {
+                const end = @min(i +| config.chunk_context_lines +| 1, diff_segments.items.len);
+                for (diff_segments.items[i..end]) |*seg| {
                     seg.skip = false;
+                }
+            }
+        }
+
+        {
+            // Reverse pass: unskip segments before non-equal segments
+            var i = diff_segments.items.len;
+            while (i > 0) {
+                i -= 1;
+                const segment = diff_segments.items[i];
+                if (segment.mode != .equal) {
+                    const start = i -| config.chunk_context_lines;
+                    for (diff_segments.items[start .. i + 1]) |*seg| {
+                        seg.skip = false;
+                    }
                 }
             }
         }
     }
 
     // fill removed_line_count and inserted_line_count
-    for (new_diff_segments.items, 0..) |*segment, i| {
+    for (diff_segments.items, 0..) |*segment, i| {
         for (segment.removed) |char| if (char == '\n') {
             segment.removed_line_count += 1;
         };
-        if (i != new_diff_segments.items.len - 1) segment.removed_line_count += 1;
+        if (i != diff_segments.items.len - 1) segment.removed_line_count += 1;
         for (segment.inserted) |char| if (char == '\n') {
             segment.inserted_line_count += 1;
         };
-        if (i != new_diff_segments.items.len - 1) segment.inserted_line_count += 1;
+        if (i != diff_segments.items.len - 1) segment.inserted_line_count += 1;
     }
-
-    try printDiff(arena, writer, new_diff_segments.items, enable_ansi_colors);
+    try printDiff(arena, writer, diff_segments.items, config.enable_ansi_colors);
 }
 
 pub const Diff = struct {
@@ -143,8 +151,37 @@ const colors = struct {
     const cyan = "\x1b[36m";
     const red_background = "\x1b[41m";
     const green_background = "\x1b[42m";
+    const underline = "\x1b[4m";
     const dim = "\x1b[2m";
     const reset = "\x1b[0m";
+};
+
+const styles = struct {
+    const removed = Style{
+        .prefix = "+ ",
+        .prefix_color = colors.red,
+        .text_color = colors.red_background,
+    };
+    const inserted = Style{
+        .prefix = "- ",
+        .prefix_color = colors.green,
+        .text_color = colors.green_background,
+    };
+    const equal = Style{
+        .prefix = "  ",
+        .prefix_color = "",
+        .text_color = colors.dim,
+    };
+    const removed_equal = Style{
+        .prefix = "+ ",
+        .prefix_color = colors.red,
+        .text_color = colors.red,
+    };
+    const inserted_equal = Style{
+        .prefix = "- ",
+        .prefix_color = colors.green,
+        .text_color = colors.green,
+    };
 };
 
 pub const DiffSegment = struct {
@@ -161,22 +198,34 @@ pub const DiffSegment = struct {
     skip: bool = false,
 };
 
-fn printDiffHeader(writer: anytype, enable_colors: bool) !void {
-    if (enable_colors) {
-        try writer.print("{s}- Received{s}\n{s}+ Expected{s}\n\n", .{ colors.red, colors.reset, colors.green, colors.reset });
-    } else {
-        try writer.print("- Received\n+ Expected\n\n", .{});
-    }
+fn printDiffFooter(writer: anytype, enable_colors: bool, received_diff_lines: usize, expected_diff_lines: usize) !void {
+    if (enable_colors) try writer.writeAll(styles.inserted.prefix_color);
+    try writer.writeAll(styles.inserted.prefix);
+    try writer.writeAll("Expected");
+    try writer.print("  {s}{d}", .{ styles.inserted.prefix, expected_diff_lines });
+    if (enable_colors) try writer.writeAll(colors.reset);
+    try writer.writeAll("\n");
+    if (enable_colors) try writer.writeAll(styles.removed.prefix_color);
+    try writer.writeAll(styles.removed.prefix);
+    try writer.writeAll("Received");
+    try writer.print("  {s}{d}", .{ styles.removed.prefix, received_diff_lines });
+    if (enable_colors) try writer.writeAll(colors.reset);
+    try writer.writeAll("\n");
 }
+
+const Style = struct {
+    prefix: []const u8,
+    prefix_color: []const u8,
+    text_color: []const u8,
+};
 
 fn printLinePrefix(
     writer: anytype,
     enable_colors: bool,
-    prefix: []const u8,
-    prefix_color: []const u8,
+    style: Style,
 ) !void {
-    if (enable_colors) try writer.writeAll(prefix_color);
-    try writer.writeAll(prefix);
+    if (enable_colors) try writer.writeAll(style.prefix_color);
+    try writer.writeAll(style.prefix);
     if (enable_colors) try writer.writeAll(colors.reset);
 }
 
@@ -184,20 +233,18 @@ fn printSegment(
     text: []const u8,
     writer: anytype,
     enable_colors: bool,
-    prefix: []const u8,
-    prefix_color: []const u8,
-    text_color: []const u8,
+    style: Style,
 ) !void {
     var lines = std.mem.splitScalar(u8, text, '\n');
 
-    if (enable_colors) try writer.writeAll(text_color);
+    if (enable_colors) try writer.writeAll(style.text_color);
     try writer.writeAll(lines.next().?);
     if (enable_colors) try writer.writeAll(colors.reset);
 
     while (lines.next()) |line| {
         try writer.writeAll("\n");
-        try printLinePrefix(writer, enable_colors, prefix, prefix_color);
-        if (enable_colors) try writer.writeAll(text_color);
+        try printLinePrefix(writer, enable_colors, style);
+        if (enable_colors) try writer.writeAll(style.text_color);
         try writer.writeAll(line);
         if (enable_colors) try writer.writeAll(colors.reset);
     }
@@ -211,22 +258,22 @@ fn printModifiedSegment(
 ) !void {
     const char_diff = try DMP.default.diff(arena, segment.removed, segment.inserted, true);
 
-    try printLinePrefix(writer, enable_colors, "- ", colors.red);
+    try printLinePrefix(writer, enable_colors, styles.inserted);
     for (char_diff.items) |item| {
         switch (item.operation) {
-            .delete => try printSegment(item.text, writer, enable_colors, "- ", colors.red, colors.red_background),
-            .insert => {},
-            .equal => try printSegment(item.text, writer, enable_colors, "- ", colors.red, colors.red),
+            .delete => {},
+            .insert => try printSegment(item.text, writer, enable_colors, styles.inserted),
+            .equal => try printSegment(item.text, writer, enable_colors, styles.inserted_equal),
         }
     }
     try writer.writeAll("\n");
 
-    try printLinePrefix(writer, enable_colors, "+ ", colors.green);
+    try printLinePrefix(writer, enable_colors, styles.removed);
     for (char_diff.items) |item| {
         switch (item.operation) {
-            .delete => {},
-            .insert => try printSegment(item.text, writer, enable_colors, "+ ", colors.green, colors.green_background),
-            .equal => try printSegment(item.text, writer, enable_colors, "+ ", colors.green, colors.green),
+            .delete => try printSegment(item.text, writer, enable_colors, styles.removed),
+            .insert => {},
+            .equal => try printSegment(item.text, writer, enable_colors, styles.removed_equal),
         }
     }
     try writer.writeAll("\n");
@@ -246,10 +293,12 @@ pub fn printDiff(
     diff_segments: []const DiffSegment,
     enable_ansi_colors: bool,
 ) !void {
-    try printDiffHeader(writer, enable_ansi_colors);
+    try writer.writeAll("\n");
 
     var original_line_number: usize = 1;
     var changed_line_number: usize = 1;
+    var original_diff_lines: usize = 0;
+    var changed_diff_lines: usize = 0;
 
     const has_skipped_segments = for (diff_segments) |seg| {
         if (seg.skip) break true;
@@ -281,27 +330,33 @@ pub fn printDiff(
                     was_skipped = true;
                     continue;
                 }
-                try printLinePrefix(writer, enable_ansi_colors, "  ", "");
-                try printSegment(segment.removed, writer, enable_ansi_colors, "  ", "", colors.dim);
+                try printLinePrefix(writer, enable_ansi_colors, styles.equal);
+                try printSegment(segment.removed, writer, enable_ansi_colors, styles.equal);
                 try writer.writeAll("\n");
             },
             .removed => {
-                try printLinePrefix(writer, enable_ansi_colors, "- ", colors.red);
-                try printSegment(segment.removed, writer, enable_ansi_colors, "- ", colors.red, colors.red_background);
+                try printLinePrefix(writer, enable_ansi_colors, styles.removed);
+                try printSegment(segment.removed, writer, enable_ansi_colors, styles.removed);
                 try writer.writeAll("\n");
+                original_diff_lines += segment.removed_line_count;
             },
             .inserted => {
-                try printLinePrefix(writer, enable_ansi_colors, "+ ", colors.green);
-                try printSegment(segment.inserted, writer, enable_ansi_colors, "+ ", colors.green, colors.green_background);
+                try printLinePrefix(writer, enable_ansi_colors, styles.inserted);
+                try printSegment(segment.inserted, writer, enable_ansi_colors, styles.inserted);
                 try writer.writeAll("\n");
+                changed_diff_lines += segment.inserted_line_count;
             },
             .modified => {
                 try printModifiedSegment(segment, arena, writer, enable_ansi_colors);
+                original_diff_lines += segment.removed_line_count;
+                changed_diff_lines += segment.inserted_line_count;
             },
         }
     }
 
     try writer.writeAll("\n");
+
+    try printDiffFooter(writer, enable_ansi_colors, original_diff_lines, changed_diff_lines);
 }
 
 // @sortImports

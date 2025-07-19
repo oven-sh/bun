aaa34c76d89f8c26743663183703603295e919a6
Ben Grant <ben@bun.sh>
2025-07-18 17:22:47 -0700

    fix cwd

--- test/js/bun/util/bun-main.test.ts
+++ test/js/bun/util/bun-main.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, test } from "bun:test";
+import { join } from "node:path";
 import "../../../harness"; // for expect().toRun()
 
 describe("Bun.main", () => {
@@ -12,6 +13,10 @@ describe("Bun.main", () => {
   });
 
   test("override is reset when switching to a new test file", () => {
-    expect(["test", "./bun-main-test-fixture-1.ts", "./bun-main-test-fixture-2.ts"]).toRun();
+    expect([
+      "test",
+      join(import.meta.dir, "bun-main-test-fixture-1.ts"),
+      join(import.meta.dir, "bun-main-test-fixture-2.ts"),
+    ]).toRun();
   });
 });

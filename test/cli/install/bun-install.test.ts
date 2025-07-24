4be5a57c84d7bdbdcf0efb2a34a26a005b135907
RiskyMH <git@riskymh.dev>
2025-07-24 20:24:34 +1000

    .

--- test/cli/install/bun-install.test.ts
+++ test/cli/install/bun-install.test.ts
@@ -489,7 +489,7 @@ it("should work when moving workspace packages", async () => {
 
   await Bun.$`${bunExe()} i`.env(bunEnv).cwd(package_dir);
 
-  await Bun.$ /* sh */ `
+  await Bun.$/* sh */ `
   mkdir config
 
   # change workspaces from "packages/*" to "config/*"
@@ -561,7 +561,7 @@ it("should work when renaming a single workspace package", async () => {
 
   await Bun.$`${bunExe()} i`.env(bunEnv).cwd(package_dir);
 
-  await Bun.$ /* sh */ `
+  await Bun.$/* sh */ `
   echo ${JSON.stringify({
     "name": "my-workspace",
     version: "0.0.1",
@@ -8837,6 +8837,8 @@ describe("platform-specific dependencies", () => {
     const packages = [`${process.platform}-${process.arch}`];
     if (isLinux) {
       packages.push(`linux-${process.arch}-${libcFamily}`);
+    } else if (isWindows) {
+      packages[0].replace("win32", "windows");
     }
 
     expect(installedPackages).toContainValues(packages);

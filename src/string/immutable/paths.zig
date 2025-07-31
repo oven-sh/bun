19f7ad044aec0e10d3cb454b8e19b06e1aa3de8a
Arnold Filip <arnold.filip@gmail.com>
2025-07-31 15:15:12 +0200

    Trim current dir prefix.

--- src/string/immutable/paths.zig
+++ src/string/immutable/paths.zig
@@ -173,6 +173,11 @@ pub fn toWPathNormalized(wbuf: []u16, utf8: []const u8) [:0]u16 {
 
     var path_to_use = normalizeSlashesOnly(renormalized, utf8, '\\');
 
+    // is there a current-dir prefix ".\\"? Let's remove it before converting to UTF-16
+    if (path_to_use.len > 2 and std.mem.startsWith(u8, path_to_use, ".\\")) {
+        path_to_use = path_to_use[2..];
+    }
+
     // is there a trailing slash? Let's remove it before converting to UTF-16
     if (path_to_use.len > 3 and bun.path.isSepAny(path_to_use[path_to_use.len - 1])) {
         path_to_use = path_to_use[0 .. path_to_use.len - 1];

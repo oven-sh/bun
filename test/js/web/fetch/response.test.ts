ef468587aa57300b9ac2b3a29cef507828a45730
Jarred Sumner <jarred@jarredsumner.com>
2025-07-30 23:59:23 -0700

    Update response.test.ts

--- test/js/web/fetch/response.test.ts
+++ test/js/web/fetch/response.test.ts
@@ -49,7 +49,7 @@ describe("2-arg form", () => {
 test("print size", () => {
   expect(normalizeBunSnapshot(Bun.inspect(new Response(Bun.file(import.meta.filename)))), import.meta.dir)
     .toMatchInlineSnapshot(`
-    "Response (3.48 KB) {
+    "Response (3.82 KB) {
       ok: true,
       url: "",
       status: 200,

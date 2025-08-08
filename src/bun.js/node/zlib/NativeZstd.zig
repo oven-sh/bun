5c206b95a7c40c5870bebdf60ac5a94ae4c2e06b
Meghan Denny <meghan@bun.sh>
2025-08-08 02:13:11 -0700

    zig: fix crash in NativeZstd estimatedSize

--- src/bun.js/node/zlib/NativeZstd.zig
+++ src/bun.js/node/zlib/NativeZstd.zig
@@ -54,8 +54,8 @@ pub fn constructor(globalThis: *jsc.JSGlobalObject, callframe: *jsc.CallFrame) b
 
 pub fn estimatedSize(this: *const @This()) usize {
     return @sizeOf(@This()) + switch (this.stream.mode) {
-        .ZSTD_COMPRESS => bun.c.ZSTD_sizeof_CCtx(@ptrCast(this.stream.state)),
-        .ZSTD_DECOMPRESS => bun.c.ZSTD_sizeof_DCtx(@ptrCast(this.stream.state)),
+        .ZSTD_COMPRESS => 5272, // estimate of bun.c.ZSTD_sizeof_CCtx(@ptrCast(this.stream.state)),
+        .ZSTD_DECOMPRESS => 95968, // estimate of bun.c.ZSTD_sizeof_DCtx(@ptrCast(this.stream.state)),
         else => 0,
     };
 }

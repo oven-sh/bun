21258a944d97c736785ff967f5be1836f582340a
Meghan Denny <meghan@bun.sh>
2025-08-08 17:47:24 -0700

    ci: lower build-cpp agent from 16xlarge to 4xlarge

--- .buildkite/ci.mjs
+++ .buildkite/ci.mjs
@@ -303,7 +303,7 @@ function getCppAgent(platform, options) {
   }
 
   return getEc2Agent(platform, options, {
-    instanceType: arch === "aarch64" ? "c8g.16xlarge" : "c7i.16xlarge",
+    instanceType: arch === "aarch64" ? "c8g.4xlarge" : "c7i.4xlarge",
     cpuCount: 32,
     threadsPerCore: 1,
   });

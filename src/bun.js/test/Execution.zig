e902f0a6057e73761d039a5f9cb7191f85d08c46
pfg <pfg@pfg.pw>
2025-08-12 21:19:52 -0700

    wip,,

--- src/bun.js/test/Execution.zig
+++ src/bun.js/test/Execution.zig
@@ -2,7 +2,27 @@ pub fn init(_: std.mem.Allocator) Execution {
     return .{};
 }
 
+fn bunTest(this: *Execution) *BunTest {
+    group.begin(@src());
+    defer group.end();
+
+    return @fieldParentPtr("execution", this);
+}
+
+pub fn runLoop() bun.JSError!void {
+    while (true) {
+        // run the next test or hook
+    }
+}
+
 const std = @import("std");
 
 const describe2 = @import("./describe2.zig");
+const BunTest = describe2.BunTest;
+const Collection = describe2.Collection;
 const Execution = describe2.Execution;
+const DescribeScope = describe2.DescribeScope;
+const group = describe2.group;
+
+const bun = @import("bun");
+const jsc = bun.jsc;

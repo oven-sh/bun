b0bcb2e50cac7ea6cc2cb422d7bd8ae20c553059
pfg <pfg@pfg.pw>
2025-08-14 17:30:54 -0700

    describe2

--- describe2.ts
+++ describe2.ts
@@ -1,67 +1,69 @@
 import { describe, test } from "bun:test";
 
-console.log("HIT 1");
-describe("abc", () => {
-  console.log("HIT 2");
+// .split("\n").map((line, lineno) => line.replaceAll(/"LINE d+"/g, '"LINE '+(lineno+1)+'"')).join("\n"))
 
-  test("1", () => {});
+console.log("LINE 5");
+describe("LINE 6", () => {
+  console.log("LINE 7");
+
+  test("LINE 9", () => console.log("LINE 9"));
 });
-console.log("HIT 3");
-describe("abc", () => {
-  console.log("HIT 4");
-  test("2", () => {});
-  describe("abc", () => {
-    console.log("HIT 7");
-    test("3", () => {});
+console.log("LINE 11");
+describe("LINE 12", () => {
+  console.log("LINE 13");
+  test("LINE 14", () => console.log("LINE 14"));
+  describe("LINE 15", () => {
+    console.log("LINE 16");
+    test("LINE 17", () => console.log("LINE 17"));
   });
-  console.log("HIT 5");
-  test("4", () => {});
-  describe("abc", () => {
-    console.log("HIT 8");
-    test("5", () => {});
+  console.log("LINE 19");
+  test("LINE 20", () => console.log("LINE 20"));
+  describe("LINE 21", () => {
+    console.log("LINE 22");
+    test("LINE 23", () => console.log("LINE 23"));
   });
-  console.log("HIT 6");
-  test("6", () => {});
+  console.log("LINE 25");
+  test("LINE 26", () => console.log("LINE 26"));
 });
-console.log("HIT 9");
-test("7", () => {});
+console.log("LINE 28");
+test("LINE 29", () => console.log("LINE 29"));
 await Promise.resolve(undefined);
 
-console.log("HIT 10");
-test("8", () => {});
-describe("abc", async () => {
-  console.log("HIT 11");
-  test("9", () => {});
-  describe("abc", async () => {
-    test("10", () => {});
-    console.log("HIT 14");
+console.log("LINE 32");
+test("LINE 33", () => console.log("LINE 33"));
+describe("LINE 34", async () => {
+  console.log("LINE 35");
+  test("LINE 36", () => console.log("LINE 36"));
+  describe("LINE 37", async () => {
+    test("LINE 38", () => console.log("LINE 38"));
+    console.log("LINE 39");
   });
-  test("11", () => {});
+  test("LINE 41", () => console.log("LINE 41"));
 });
-test("12", () => {});
-console.log("HIT 12");
-describe("def", async () => {
-  test("13", () => {});
-  console.log("HIT 15");
-  describe("def", async () => {
-    test("14", () => {});
-    console.log("HIT 16");
+test("LINE 43", () => console.log("LINE 43"));
+console.log("LINE 44");
+describe("LINE 45", async () => {
+  test("LINE 46", () => console.log("LINE 46"));
+  console.log("LINE 47");
+  describe("LINE 48", async () => {
+    test("LINE 49", () => console.log("LINE 49"));
+    console.log("LINE 50");
   });
-  test("15", () => {});
-  describe("def", () => {
-    test("16", () => {});
-    console.log("HIT 17");
+  test("LINE 52", () => console.log("LINE 52"));
+  describe("LINE 53", () => {
+    test("LINE 54", () => console.log("LINE 54"));
+    console.log("LINE 55");
   });
-  test("17", () => {});
-  describe("def", async () => {
-    test("18", () => {});
-    console.log("HIT 18");
-    test("19", () => {});
+  test("LINE 57", () => console.log("LINE 57"));
+  describe("LINE 58", async () => {
+    test("LINE 59", () => console.log("LINE 59"));
+    console.log("LINE 60");
+    test("LINE 61", () => console.log("LINE 61"));
   });
-  test("20", () => {});
+  test("LINE 63", () => console.log("LINE 63"));
 });
-console.log("HIT 13");
-test("21", () => {});
+console.log("LINE 65");
+test("LINE 66", () => console.log("LINE 66"));
 
 await describe.forDebuggingExecuteTestsNow();
 

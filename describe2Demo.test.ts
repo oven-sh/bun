c36258fd3cca126f6f5f0c98d582853e134f175c
pfg <pfg@pfg.pw>
2025-08-15 18:25:40 -0700

    waitForPromise fix

---
+++ describe2Demo.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, test } from "bun:test";
+
+test("test one", async () => {
+  console.log("<1>");
+  expect(Bun.sleep(50)).resolves.toBeUndefined();
+  console.log("</1>");
+});
+
+test.concurrent("test two", async () => {
+  console.log("<2>");
+  expect(Bun.sleep(50)).resolves.toBeUndefined();
+  console.log("</2>");
+});
+
+test.concurrent("test three", async () => {
+  console.log("<3>");
+  expect(Bun.sleep(50)).resolves.toBeUndefined();
+  console.log("</3>");
+});
+
+test("test four", async () => {
+  console.log("<4>");
+  expect(Bun.sleep(50)).resolves.toBeUndefined();
+  console.log("</4>");
+});
+
+console.log("before exec");
+await describe.forDebuggingExecuteTestsNow();
+console.log("after exec");
+describe.forDebuggingDeinitNow();

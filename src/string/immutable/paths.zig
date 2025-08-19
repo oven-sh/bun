64d4cf2c35e32d3d3c8792a56b105e274b0a451b
Arnold Filip <arnold.filip@gmail.com>
2025-08-19 18:42:21 +0200

    Trim current dir prefix in similar normalization functions
    (Not called in shell rm but maybe elsewhere, though i did not find a lot of references.)

--- src/string/immutable/paths.zig
+++ src/string/immutable/paths.zig
@@ -189,6 +189,12 @@ pub fn toWPathNormalized(wbuf: []u16, utf8: []const u8) [:0]u16 {
 pub fn toWPathNormalized16(wbuf: []u16, path: []const u16) [:0]u16 {
     var path_to_use = normalizeSlashesOnlyT(u16, wbuf, path, '\\', true);
 
+    // is there a current-dir prefix ".\\"? Let's remove it before converting to UTF-16
+    const curdirPrefix: [2]u16 = .{ '.', '\\' };
+    if (path_to_use.len > 2 and std.mem.startsWith(u16, path_to_use, &curdirPrefix)) {
+        path_to_use = path_to_use[2..];
+    }
+
     // is there a trailing slash? Let's remove it before converting to UTF-16
     if (path_to_use.len > 3 and bun.path.isSepAnyT(u16, path_to_use[path_to_use.len - 1])) {
         path_to_use = path_to_use[0 .. path_to_use.len - 1];
@@ -204,6 +210,11 @@ pub fn toPathNormalized(buf: []u8, utf8: []const u8) [:0]const u8 {
     defer bun.path_buffer_pool.put(renormalized);
 
     var path_to_use = normalizeSlashesOnly(renormalized, utf8, '\\');
+
+    // is there a current-dir prefix ".\\"? Let's remove it before converting to UTF-16
+    if (path_to_use.len > 2 and std.mem.startsWith(u8, path_to_use, ".\\")) {
+        path_to_use = path_to_use[2..];
+    }
 
     // is there a trailing slash? Let's remove it before converting to UTF-16
     if (path_to_use.len > 3 and bun.path.isSepAny(path_to_use[path_to_use.len - 1])) {

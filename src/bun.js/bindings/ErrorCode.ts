16b3288f0557204def67ce080df379a86f9e860a
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-08-21 08:26:40 -0700

    🅼🆈🆂🆀🅻 error refactor WIP

--- src/bun.js/bindings/ErrorCode.ts
+++ src/bun.js/bindings/ErrorCode.ts
@@ -208,6 +208,7 @@ const errors: ErrorCodeMapping = [
   ["ERR_MYSQL_CONNECTION_TIMEOUT", Error, "MySQLError"],
   ["ERR_MYSQL_IDLE_TIMEOUT", Error, "MySQLError"],
   ["ERR_MYSQL_LIFETIME_TIMEOUT", Error, "MySQLError"],
+  ["ERR_UNHANDLED_REJECTION", Error, "UnhandledPromiseRejection"],
   ["ERR_REQUIRE_ASYNC_MODULE", Error],
   ["ERR_S3_INVALID_ENDPOINT", Error],
   ["ERR_S3_INVALID_METHOD", Error],

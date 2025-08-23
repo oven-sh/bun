579646a0f5244f7898eb90de62a41faf57c5eb77
Alistair Smith <hi@alistair.sh>
2025-08-22 21:20:03 -0700

    revert http.zig

--- src/http.zig
+++ src/http.zig
@@ -4,8 +4,6 @@ const HTTPClient = @This();
 pub var default_allocator: std.mem.Allocator = undefined;
 pub var default_arena: Arena = undefined;
 pub var http_thread: HTTPThread = undefined;
-const DeadSocket = opaque {};
-var dead_socket = @as(*DeadSocket, @ptrFromInt(1));
 //TODO: this needs to be freed when Worker Threads are implemented
 pub var socket_async_http_abort_tracker = std.AutoArrayHashMap(u32, uws.InternalSocket).init(bun.default_allocator);
 pub var async_http_id_monotonic: std.atomic.Value(u32) = std.atomic.Value(u32).init(0);

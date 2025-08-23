7ae3a4d54dcd5193ca5862484a0bd65066071af6
Meghan Denny <meghan@bun.sh>
2025-08-22 22:44:05 -0700

    tidy

--- src/js/node/util.ts
+++ src/js/node/util.ts
@@ -197,8 +197,9 @@ var toUSVString = input => {
 
 function styleText(format, text) {
   validateString(text, "text");
+  if (!$isJSArray(format)) format = [format];
 
-  if ($isJSArray(format)) {
+  {
     let left = "";
     let right = "";
     for (const key of format) {
@@ -212,10 +213,6 @@ function styleText(format, text) {
     }
 
     return `${left}${text}${right}`;
-  }
-
-  if (format === "none") {
-    return text;
   }
 
   let formatCodes = inspect.colors[format];

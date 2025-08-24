40a76cbb8ce9e570845312e892d556f9693d0c13
Alistair Smith <hi@alistair.sh>
2025-08-24 16:06:45 -0700

    fix compilation

--- src/bun.js/api/JSBundler.zig
+++ src/bun.js/api/JSBundler.zig
@@ -1086,7 +1086,7 @@ pub const JSBundler = struct {
         extern fn JSBundlerPlugin__tombstone(*Plugin) void;
         extern fn JSBundlerPlugin__runOnEndCallbacks(*Plugin, jsc.JSValue, jsc.JSValue) jsc.JSValue;
 
-        pub fn runOnEndCallbacks(this: *Plugin, build_result: jsc.JSValue, build_promise: *jsc.JSPromise) jsc.JSValue {
+        pub fn runOnEndCallbacks(this: *Plugin, globalThis: *jsc.JSGlobalObject, build_result: jsc.JSValue, build_promise: *jsc.JSPromise) jsc.JSValue {
             jsc.markBinding(@src());
             return JSBundlerPlugin__runOnEndCallbacks(this, build_result, build_promise.asValue(globalThis));
         }

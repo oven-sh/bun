a1b9dabf101caff897a28ab9cc11e27d62931ded
Alistair Smith <hi@alistair.sh>
2025-08-27 14:31:40 -0700

    fix: run on update

--- test/js/bun/test/jest.d.ts
+++ test/js/bun/test/jest.d.ts
@@ -1,1 +1,9 @@
 /// <reference path="../../../../packages/bun-types/test-globals.d.ts"" />
+
+// Eventually move these to @types/bun somehow
+interface ReadableStream {
+  text(): Promise<string>;
+  json(): Promise<any>;
+  arrayBuffer(): Promise<ArrayBuffer>;
+  blob(): Promise<Blob>;
+}

10859083862350fdac890adf6c02969ea4a94c18
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-08-29 01:03:17 -0700

    fix(Bun.SQL) MYSQL fix old auth and auth switch + add lastInsertRowid and affectedRows (#22132)
    ### What does this PR do?
    
    add `lastInsertRowid` (matching SQLite)
    add `affectedRows`
    fix `mysql_native_password` deprecated authentication
    fix AuthSwitch
    Fixes:
    https://github.com/oven-sh/bun/issues/22178#issuecomment-3228716080
    ### How did you verify your code works?
    tests
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- test/js/sql/sql-mysql.test.ts
+++ test/js/sql/sql-mysql.test.ts
@@ -24,6 +24,18 @@ describeWithContainer(
       max: 1,
     };
     const sql = new SQL(options);
+    test("should return lastInsertRowid and affectedRows", async () => {
+      await using db = new SQL({ ...options, max: 1, idleTimeout: 5 });
+      using sql = await db.reserve();
+      const random_name = "test_" + randomUUIDv7("hex").replaceAll("-", "");
+
+      await sql`CREATE TEMPORARY TABLE ${sql(random_name)} (id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, name text)`;
+
+      const { lastInsertRowid } = await sql`INSERT INTO ${sql(random_name)} (name) VALUES (${"test"})`;
+      expect(lastInsertRowid).toBe(1);
+      const { affectedRows } = await sql`UPDATE ${sql(random_name)} SET name = "test2" WHERE id = ${lastInsertRowid}`;
+      expect(affectedRows).toBe(1);
+    });
     describe("should work with more than the max inline capacity", () => {
       for (let size of [50, 60, 62, 64, 70, 100]) {
         for (let duplicated of [true, false]) {

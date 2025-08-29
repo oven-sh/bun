cd92d9903fd5cba37cd83de74afd2c6bbe64cbdd
pfg <pfg@pfg.pw>
2025-08-28 20:46:03 -0700

    migrate SafeStrong to use private fields

--- src/bun.js/SafeStrong.zig
+++ src/bun.js/SafeStrong.zig
@@ -1,31 +1,31 @@
-_raw: jsc.JSValue,
-_safety: Safety,
+#raw: jsc.JSValue,
+#safety: Safety,
 const Safety = if (enable_safety) ?struct { ptr: *Strong, gpa: std.mem.Allocator, ref_count: u32 } else void;
 pub fn initNonCell(non_cell: jsc.JSValue) Strong {
     bun.assert(!non_cell.isCell());
     const safety: Safety = if (enable_safety) null;
-    return .{ ._raw = non_cell, ._safety = safety };
+    return .{ .#raw = non_cell, .#safety = safety };
 }
 pub fn init(safety_gpa: std.mem.Allocator, value: jsc.JSValue) Strong {
     // TODO: consider using withAsyncContextIfNeeded
     value.protect();
-    const safety: Safety = if (enable_safety) .{ .ptr = bun.create(safety_gpa, Strong, .{ ._raw = @enumFromInt(0xAEBCFA), ._safety = null }), .gpa = safety_gpa, .ref_count = 1 };
-    return .{ ._raw = value, ._safety = safety };
+    const safety: Safety = if (enable_safety) .{ .ptr = bun.create(safety_gpa, Strong, .{ .#raw = @enumFromInt(0xAEBCFA), .#safety = null }), .gpa = safety_gpa, .ref_count = 1 };
+    return .{ .#raw = value, .#safety = safety };
 }
 pub fn deinit(this: *Strong) void {
-    this._raw.unprotect();
-    if (enable_safety) if (this._safety) |safety| {
-        bun.assert(@intFromEnum(safety.ptr.*._raw) == 0xAEBCFA);
-        safety.ptr.*._raw = @enumFromInt(0xFFFFFF);
+    this.#raw.unprotect();
+    if (enable_safety) if (this.#safety) |safety| {
+        bun.assert(@intFromEnum(safety.ptr.*.#raw) == 0xAEBCFA);
+        safety.ptr.*.#raw = @enumFromInt(0xFFFFFF);
         bun.assert(safety.ref_count == 1);
         safety.gpa.destroy(safety.ptr);
     };
 }
 pub fn get(this: Strong) jsc.JSValue {
-    return this._raw;
+    return this.#raw;
 }
 pub fn swap(this: *Strong, safety_gpa: std.mem.Allocator, next: jsc.JSValue) jsc.JSValue {
-    const prev = this._raw;
+    const prev = this.#raw;
     this.deinit();
     this.* = .init(safety_gpa, next);
     return prev;
@@ -34,17 +34,17 @@ pub fn dupe(this: Strong, gpa: std.mem.Allocator) Strong {
     return .init(gpa, this.get());
 }
 pub fn ref(this: *Strong) void {
-    this._raw.protect();
-    if (enable_safety) if (this._safety) |safety| {
+    this.#raw.protect();
+    if (enable_safety) if (this.#safety) |safety| {
         safety.ref_count += 1;
     };
 }
 pub fn unref(this: *Strong) void {
-    this._raw.unprotect();
-    if (enable_safety) if (this._safety) |safety| {
+    this.#raw.unprotect();
+    if (enable_safety) if (this.#safety) |safety| {
         if (safety.ref_count == 1) {
-            bun.assert(@intFromEnum(safety.ptr.*._raw) == 0xAEBCFA);
-            safety.ptr.*._raw = @enumFromInt(0xFFFFFF);
+            bun.assert(@intFromEnum(safety.ptr.*.#raw) == 0xAEBCFA);
+            safety.ptr.*.#raw = @enumFromInt(0xFFFFFF);
             safety.gpa.destroy(safety.ptr);
             return;
         }
@@ -53,63 +53,63 @@ pub fn unref(this: *Strong) void {
 }
 
 pub const Optional = struct {
-    _backing: Strong,
+    #backing: Strong,
     pub const empty: Optional = .initNonCell(null);
     pub fn initNonCell(non_cell: ?jsc.JSValue) Optional {
-        return .{ ._backing = .initNonCell(non_cell orelse .zero) };
+        return .{ .#backing = .initNonCell(non_cell orelse .zero) };
     }
     pub fn init(safety_gpa: std.mem.Allocator, value: ?jsc.JSValue) Optional {
-        return .{ ._backing = .init(safety_gpa, value orelse .zero) };
+        return .{ .#backing = .init(safety_gpa, value orelse .zero) };
     }
     pub fn deinit(this: *Optional) void {
-        this._backing.deinit();
+        this.#backing.deinit();
     }
     pub fn get(this: Optional) ?jsc.JSValue {
-        const result = this._backing.get();
+        const result = this.#backing.get();
         if (result == .zero) return null;
         return result;
     }
     pub fn swap(this: *Optional, safety_gpa: std.mem.Allocator, next: ?jsc.JSValue) ?jsc.JSValue {
-        const result = this._backing.swap(safety_gpa, next orelse .zero);
+        const result = this.#backing.swap(safety_gpa, next orelse .zero);
         if (result == .zero) return null;
         return result;
     }
     pub fn dupe(this: Optional, gpa: std.mem.Allocator) Optional {
-        return .{ ._backing = this._backing.dupe(gpa) };
+        return .{ .#backing = this.#backing.dupe(gpa) };
     }
     pub fn has(this: Optional) bool {
-        return this._backing.get() != .zero;
+        return this.#backing.get() != .zero;
     }
     pub fn ref(this: *Optional) void {
-        this._backing.ref();
+        this.#backing.ref();
     }
     pub fn unref(this: *Optional) void {
-        this._backing.unref();
+        this.#backing.unref();
     }
 };
 
 pub const List = struct {
-    _backing: std.ArrayListUnmanaged(jsc.JSValue),
-    pub const empty: List = .{ ._backing = .{ .items = &.{} } };
+    #backing: std.ArrayListUnmanaged(jsc.JSValue),
+    pub const empty: List = .{ .#backing = .{ .items = &.{} } };
     pub fn init(gpa: std.mem.Allocator, items: []const jsc.JSValue) List {
         var result: std.ArrayListUnmanaged(jsc.JSValue) = .empty;
         result.appendSlice(gpa, items) catch bun.outOfMemory();
         for (result.items) |*item| item.protect();
-        return .{ ._backing = result };
+        return .{ .#backing = result };
     }
     pub fn deinit(this: *List, gpa: std.mem.Allocator) void {
-        for (this._backing.items) |*item| item.unprotect();
-        this._backing.deinit(gpa);
+        for (this.#backing.items) |*item| item.unprotect();
+        this.#backing.deinit(gpa);
     }
     pub fn get(this: List) []const jsc.JSValue {
-        return this._backing.items;
+        return this.#backing.items;
     }
     pub fn append(this: *List, gpa: std.mem.Allocator, item: jsc.JSValue) void {
         item.protect();
-        this._backing.append(gpa, item) catch bun.outOfMemory();
+        this.#backing.append(gpa, item) catch bun.outOfMemory();
     }
     pub fn ensureUnusedCapacity(this: *List, gpa: std.mem.Allocator, additional: usize) void {
-        this._backing.ensureUnusedCapacity(gpa, additional) catch bun.outOfMemory();
+        this.#backing.ensureUnusedCapacity(gpa, additional) catch bun.outOfMemory();
     }
 
     pub fn swap(this: *List, gpa: std.mem.Allocator, items: []jsc.JSValue) void {

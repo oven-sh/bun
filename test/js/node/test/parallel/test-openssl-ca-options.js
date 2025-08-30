babae37bf48c49e94f78de63c1c4d9ff6362d0f5
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-08-29 17:39:33 -0700

    fix tests

--- test/js/node/test/parallel/test-openssl-ca-options.js
+++ test/js/node/test/parallel/test-openssl-ca-options.js
@@ -14,8 +14,7 @@ const result = childProcess.spawnSync(
   { encoding: 'utf8' }
 );
 
-assert.strictEqual(result.stderr, `${process.execPath
-}: either --use-openssl-ca or --use-bundled-ca can be used, not both${os.EOL}`
+assert.strictEqual(result.stderr, `error: either --use-openssl-ca or --use-bundled-ca can be used, not both${os.EOL}`
 );
 assert.strictEqual(result.status, 9);
 

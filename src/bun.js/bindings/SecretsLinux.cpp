b353792a583356f544a186351fab9b442a89152b
Dylan Conway <dylan.conway567@gmail.com>
2025-08-30 03:18:58 +0000

    update

--- src/bun.js/bindings/SecretsLinux.cpp
+++ src/bun.js/bindings/SecretsLinux.cpp
@@ -39,7 +39,8 @@ typedef enum {
 
 typedef enum {
     SECRET_SCHEMA_ATTRIBUTE_STRING = 0,
-    SECRET_SCHEMA_ATTRIBUTE_INTEGER = 1
+    SECRET_SCHEMA_ATTRIBUTE_INTEGER = 1,
+    SECRET_SCHEMA_ATTRIBUTE_BOOLEAN = 2
 } SecretSchemaAttributeType;
 
 typedef struct {

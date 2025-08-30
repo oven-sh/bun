76908a5a9bf23cd8f33bbf467c64b3a39890b00c
Jarred Sumner <jarred@jarredsumner.com>
2025-08-30 02:32:54 -0700

    Update ReadableStreamInternals.ts

--- src/js/builtins/ReadableStreamInternals.ts
+++ src/js/builtins/ReadableStreamInternals.ts
@@ -1053,7 +1053,7 @@ export function onPullDirectStream(controller: ReadableStreamDirectController) {
         controller._handleError = $handleDirectStreamErrorReject.bind(controller);
       }
 
-      result.$catch(controller._handleError);
+      result.catch(controller._handleError);
     }
   } catch (e) {
     return $handleDirectStreamErrorReject.$call(controller, e);

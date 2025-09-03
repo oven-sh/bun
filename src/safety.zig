3d361c8b49d05c9847a1957cbfd4594d336ca531
taylor.fish <contact@taylor.fish>
2025-09-03 15:40:44 -0700

    Static allocator polymorphism (#22227)
    * Define a generic allocator interface to enable static polymorphism for
    allocators (see `GenericAllocator` in `src/allocators.zig`). Note that
    `std.mem.Allocator` itself is considered a generic allocator.
    * Add utilities to `bun.allocators` for working with generic allocators.
    * Add a new namespace, `bun.memory`, with basic utilities for working
    with memory and objects (`create`, `destroy`, `initDefault`, `deinit`).
    * Add `bun.DefaultAllocator`, a zero-sized generic allocator type whose
    `allocator` method simply returns `bun.default_allocator`.
    * Implement the generic allocator interface in `AllocationScope` and
    `MimallocArena`.
    * Improve `bun.threading.GuardedValue` (now `bun.threading.Guarded`).
    * Improve `bun.safety.AllocPtr` (now `bun.safety.CheckedAllocator`).
    
    (For internal tracking: fixes STAB-1085, STAB-1086, STAB-1087,
    STAB-1088, STAB-1089, STAB-1090, STAB-1091)

--- src/safety.zig
+++ src/safety.zig
@@ -1,4 +1,4 @@
 pub const alloc = @import("./safety/alloc.zig");
-pub const AllocPtr = alloc.AllocPtr;
+pub const CheckedAllocator = alloc.CheckedAllocator;
 pub const CriticalSection = @import("./safety/CriticalSection.zig");
 pub const ThreadLock = @import("./safety/ThreadLock.zig");

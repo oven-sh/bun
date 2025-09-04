19491b9db3081c0e4fdfe9650bdf7cb958d43b4c
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-09-04 15:38:49 -0700

    minimal test

--- test/js/web/fetch/fetch.upgrade.test.ts
+++ test/js/web/fetch/fetch.upgrade.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "bun:test";
 import { decodeFrames, encodeCloseFrame, encodeTextFrame, upgradeHeaders } from "./websocket.helpers";
-
+import http from "http";
 describe("fetch upgrade", () => {
   test("should upgrade to websocket", async () => {
     const serverMessages: string[] = [];
@@ -57,6 +57,77 @@ describe("fetch upgrade", () => {
       }
     }
     await promise;
+    expect(serverMessages).toEqual(["hello", "world", "bye", "close"]);
+    expect(clientMessages).toEqual(["Hello World", "close"]);
+  });
+
+  test("should upgrade to websocket using http.request", async () => {
+    const serverMessages: string[] = [];
+    using server = Bun.serve({
+      port: 0,
+      fetch(req) {
+        if (server.upgrade(req)) return;
+        return new Response("Hello World");
+      },
+      websocket: {
+        open(ws) {
+          ws.send("Hello World");
+        },
+        message(ws, message) {
+          serverMessages.push(message as string);
+        },
+        close(ws) {
+          serverMessages.push("close");
+        },
+      },
+    });
+    const req = http.request(
+      {
+        port: server.url.port,
+        hostname: server.url.hostname,
+        headers: upgradeHeaders(),
+      },
+      res => {
+        expect.unreachable("should not call response callback");
+      },
+    );
+    const clientMessages: string[] = [];
+
+    const { promise, resolve, reject } = Promise.withResolvers<void>();
+    req.on("upgrade", (req, socket, head) => {
+      try {
+        expect(req.statusCode).toBe(101);
+        expect(req.headers.upgrade).toBe("websocket");
+        expect(req.headers["sec-websocket-accept"]).toBeDefined();
+        expect(req.headers.connection).toBe("Upgrade");
+        expect(head).toBeDefined();
+        expect(Buffer.isBuffer(head)).toBe(true);
+        socket.on("data", data => {
+          for (const msg of decodeFrames(data)) {
+            if (typeof msg === "string") {
+              clientMessages.push(msg);
+            } else {
+              clientMessages.push(msg.type);
+              if (msg.type === "close") {
+                socket.end();
+                resolve();
+              }
+            }
+          }
+        });
+
+        socket.write(encodeTextFrame("hello"));
+        socket.write(encodeTextFrame("world"));
+        socket.write(encodeTextFrame("bye"));
+        socket.write(encodeCloseFrame());
+      } catch (err) {
+        reject(err);
+      }
+    });
+
+    req.end();
+    await promise;
+
     expect(serverMessages).toEqual(["hello", "world", "bye", "close"]);
     expect(clientMessages).toEqual(["Hello World", "close"]);
   });

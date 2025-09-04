28b195e8cf6da71aefefb2543fcbacbe2181959d
Meghan Denny <meghan@bun.com>
2025-09-03 22:05:36 -0700

    node: resync fixtures folder for 24.3.0

--- test/js/node/test/fixtures/require-resolve.js
+++ test/js/node/test/fixtures/require-resolve.js
@@ -16,7 +16,7 @@ if (require.resolve.paths) {
   // Verify that existing paths are removed.
   assert.throws(() => {
     require.resolve('bar', { paths: [] })
-  }, /Cannot find package 'bar'/);
+  }, /^Error: Cannot find module 'bar'/);
 }
 
 // Verify that resolution path can be overwritten.
@@ -24,14 +24,14 @@ if (require.resolve.paths) {
   // three.js cannot be loaded from this file by default.
   assert.throws(() => {
     require.resolve('three')
-  }, /Cannot find package 'three'/);
+  }, /^Error: Cannot find module 'three'/);
 
   // If the nested-index directory is provided as a resolve path, 'three'
   // cannot be found because nested-index is used as a starting point and not
   // a searched directory.
   assert.throws(() => {
     require.resolve('three', { paths: [nestedIndex] })
-  }, /Cannot find package 'three'/);
+  }, /^Error: Cannot find module 'three'/);
 
   // Resolution from nested index directory also checks node_modules.
   assert.strictEqual(

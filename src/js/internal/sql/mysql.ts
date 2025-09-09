b809cd297e69e55efea5a6b7b7469187ed8be646
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-09-08 20:50:40 -0700

    test

--- src/js/internal/sql/mysql.ts
+++ src/js/internal/sql/mysql.ts
@@ -396,19 +396,13 @@ class PooledMySQLConnection {
     this.queryCount = 0;
     this.flags &= ~PooledConnectionFlags.reserved;
 
-    try {
-      // notify all queries that the connection is closed
-      for (const onClose of queries) {
-        onClose(err);
-      }
-      const onFinish = this.onFinish;
-      if (onFinish) {
-        onFinish(err);
-      }
-    } catch (e) {
-      console.log("onClose error", e, this instanceof PooledMySQLConnection, this, this.queries);
-      console.log("error", err);
-      throw e;
+    // notify all queries that the connection is closed
+    for (const onClose of queries) {
+      onClose(err);
+    }
+    const onFinish = this.onFinish;
+    if (onFinish) {
+      onFinish(err);
     }
 
     this.adapter.release(this, true);
@@ -425,7 +419,7 @@ class PooledMySQLConnection {
     this.connection = await PooledMySQLConnection.createConnection(
       this.connectionInfo,
       this.#onConnected.bind(this),
-      this.#onClose.bind(this),
+      (...args) => this.#onClose(...args),
     );
   }
 

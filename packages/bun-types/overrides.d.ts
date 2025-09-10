b0fc59a8361a6f4a85ed944bdf46e0395c8c58b5
Meghan Denny <meghan@bun.com>
2025-09-09 20:29:31 -0700

    bun-types: fill out HTTPParser

--- packages/bun-types/overrides.d.ts
+++ packages/bun-types/overrides.d.ts
@@ -261,7 +261,7 @@ declare global {
           "FLUSH",
           "QUERY",
         ];
-        HTTPParser: unknown;
+        HTTPParser: HTTPParserConstructor;
         ConnectionsList: unknown;
       };
       binding(m: string): object;
@@ -269,6 +269,60 @@ declare global {
 
     interface ProcessVersions extends Dict<string> {
       bun: string;
+    }
+
+    interface HTTPParserConstructor {
+      new (): HTTPParser;
+
+      REQUEST: 1;
+      RESPONSE: 2;
+
+      kOnMessageBegin: 0;
+      kOnHeaders: 1;
+      kOnHeadersComplete: 2;
+      kOnBody: 3;
+      kOnMessageComplete: 4;
+      kOnExecute: 5;
+      kOnTimeout: 6;
+
+      kLenientNone: 0;
+      kLenientHeaders: 1;
+      kLenientChunkedLength: 2;
+      kLenientKeepAlive: 4;
+      kLenientTransferEncoding: 8;
+      kLenientVersion: 16;
+      kLenientDataAfterClose: 32;
+      kLenientOptionalLFAfterCR: 64;
+      kLenientOptionalCRLFAfterChunk: 128;
+      kLenientOptionalCRBeforeLF: 256;
+      kLenientSpacesAfterChunkSize: 512;
+      kLenientAll: 1023;
+    }
+
+    interface HTTPParser {
+      close();
+      free();
+      remove();
+      execute();
+      finish();
+      initialize();
+      pause();
+      resume();
+      consume();
+      unconsume();
+      getCurrentBuffer();
+      duration();
+      headersCompleted();
+
+      _headers;
+      _url;
+      _consumed;
+      socket;
+      incoming;
+      outgoing;
+      maxHeaderPairs;
+      onIncoming;
+      joinDuplicateHeaders;
     }
   }
 }

541d09a50ad92eb83fdf2b5504e6fc2208ce9888
Jarred Sumner <jarred@jarredsumner.com>
2025-09-09 22:56:44 -0700

    Fix crypto.randomInt

--- src/bun.js/bindings/BunProcess.cpp
+++ src/bun.js/bindings/BunProcess.cpp
@@ -3412,12 +3412,8 @@ void Process::queueNextTick(JSC::JSGlobalObject* globalObject, const ArgList& ar
     ASSERT(!args.isEmpty());
     JSObject* nextTickFn = this->m_nextTickFunction.get();
     ASSERT(nextTickFn);
-    auto* frame = jsDynamicCast<AsyncContextFrame*>(args.at(0));
-    if (frame) {
-        frame->run(globalObject, nextTickFn, jsUndefined(), args);
-    } else {
-        AsyncContextFrame::call(globalObject, nextTickFn, jsUndefined(), args);
-    }
+    ASSERT_WITH_MESSAGE(!args.at(0).inherits<AsyncContextFrame>(), "queueNextTick must not pass an AsyncContextFrame. This will cause a crash.");
+    JSC::call(globalObject, nextTickFn, args, "Failed to call nextTick"_s);
     RELEASE_AND_RETURN(scope, void());
 }
 

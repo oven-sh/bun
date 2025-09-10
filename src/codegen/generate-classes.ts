c646f7dad5e83f321c5fb28f72705e17348465a5
pfg <pfg@pfg.pw>
2025-09-09 20:00:37 -0700

    fix done missing call/apply

--- src/codegen/generate-classes.ts
+++ src/codegen/generate-classes.ts
@@ -1785,7 +1785,7 @@ ${
 
 JSObject* ${name}::createPrototype(VM& vm, JSDOMGlobalObject* globalObject)
 {
-    auto *structure = ${prototypeName(typeName)}::createStructure(vm, globalObject, globalObject->objectPrototype());
+    auto *structure = ${prototypeName(typeName)}::createStructure(vm, globalObject, ${obj.instanceCallable ? "globalObject->functionPrototype()" : "globalObject->objectPrototype()"});
     structure->setMayBePrototype(true);
     return ${prototypeName(typeName)}::create(vm, globalObject, structure);
 }

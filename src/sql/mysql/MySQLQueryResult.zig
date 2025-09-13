b47c545f531ec56b9a8035e8621543ef1f9537d4
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-09-12 18:43:01 -0700

    WIP

---
+++ src/sql/mysql/MySQLQueryResult.zig
@@ -0,0 +1,6 @@
+const MySQLQueryResult = @This();
+
+result_count: u64,
+last_insert_id: u64,
+affected_rows: u64,
+is_last_result: bool,
---
+++ src/sql/mysql/MySQLRequestQueue.zig
@@ -0,0 +1,212 @@
+pub const MySQLRequestQueue = @This();
+
+#requests: Queue,
+
+#pipelined_requests: u32 = 0,
+#nonpipelinable_requests: u32 = 0,
+#waiting_to_prepare: bool = false,
+#is_ready_for_query: bool = false,
+
+pub inline fn canExecuteQuery(this: *@This(), connection: *MySQLConnection) bool {
+    return connection.status == .connected and
+        connection.isAbleToWrite() and
+        this.#is_ready_for_query and
+        this.current() == null;
+}
+pub inline fn canPrepareQuery(this: *@This(), connection: *MySQLConnection) bool {
+    return connection.status == .connected and
+        connection.isAbleToWrite() and
+        this.#is_ready_for_query and
+        !this.#waiting_to_prepare and
+        this.#pipelined_requests == 0;
+}
+
+pub inline fn markAsReadyForQuery(this: *@This()) void {
+    this.#is_ready_for_query = true;
+    this.#waiting_to_prepare = false;
+}
+pub inline fn canPipeline(this: *@This(), connection: *MySQLConnection) bool {
+    if (bun.getRuntimeFeatureFlag(.BUN_FEATURE_FLAG_DISABLE_SQL_AUTO_PIPELINING)) {
+        @branchHint(.unlikely);
+        return false;
+    }
+
+    return connection.status == .connected and
+        this.#is_ready_for_query and
+        this.#nonpipelinable_requests == 0 and // need to wait for non pipelinable requests to finish
+        !this.#waiting_to_prepare and
+        connection.isAbleToWrite();
+}
+
+pub fn markCurrentRequestAsFinished(this: *@This(), item: *JSMySQLQuery) void {
+    if (item.isRunning()) {
+        if (item.wasPipelined()) {
+            this.#pipelined_requests -= 1;
+        } else {
+            this.#nonpipelinable_requests -= 1;
+        }
+    }
+    if (item.isBeingPrepared()) {
+        this.#waiting_to_prepare = false;
+    }
+}
+
+pub fn advance(this: *@This(), connection: *MySQLConnection) void {
+    var offset: usize = 0;
+    defer {
+        while (this.#requests.readableLength() > 0) {
+            const request = this.#requests.peekItem(0);
+            // An item may be in the success or failed state and still be inside the queue (see deinit later comments)
+            // so we do the cleanup her
+            if (request.isCompleted()) {
+                this.#requests.discard(1);
+                request.deref();
+                continue;
+            }
+            break;
+        }
+    }
+
+    while (this.#requests.readableLength() > offset and !connection.flags.has_backpressure) {
+        if (connection.vm.isShuttingDown()) return connection.close();
+        var request: *JSMySQLQuery = this.#requests.peekItem(offset);
+
+        if (request.isCompleted()) {
+            if (offset > 0) {
+                // discard later
+                offset += 1;
+                continue;
+            }
+            this.#requests.discard(1);
+            request.deref();
+            continue;
+        }
+
+        if (request.isBeingPrepared()) {
+            // cannot continue the queue until the current request is marked as prepared
+            return;
+        }
+        if (request.isRunning()) {
+            const total_requests_running = this.#pipelined_requests + this.#nonpipelinable_requests;
+            if (offset < total_requests_running) {
+                offset += total_requests_running;
+            } else {
+                offset += 1;
+            }
+            continue;
+        }
+
+        request.run(connection) catch |err| {
+            if (connection.globalObject.tryTakeException()) |err_| {
+                request.rejectWithJSValue(connection.getQueriesArray(), err_);
+            } else {
+                request.reject(connection.getQueriesArray(), err);
+            }
+            if (offset == 0) {
+                this.#requests.discard(1);
+                request.deref();
+            }
+        };
+        if (request.isRunning()) {
+            this.#is_ready_for_query = false;
+            if (request.isBeingPrepared()) {
+                this.#waiting_to_prepare = true;
+                return;
+            }
+            if (request.wasPipelined()) {
+                this.#pipelined_requests += 1;
+                if (this.canPipeline(connection)) {
+                    offset += 1;
+                    continue;
+                }
+                return;
+            }
+
+            this.#nonpipelinable_requests += 1;
+        }
+        return;
+    }
+}
+
+pub fn init() @This() {
+    return .{ .#requests = Queue.init(bun.default_allocator) };
+}
+
+pub fn isEmpty(this: *@This()) bool {
+    return this.#requests.readableLength() == 0;
+}
+
+pub fn add(this: *@This(), request: *JSMySQLQuery, connection: *MySQLConnection) void {
+    if (request.isRunning()) {
+        this.#is_ready_for_query = false;
+        if (request.isBeingPrepared()) {
+            this.#waiting_to_prepare = true;
+        } else {
+            if (request.wasPipelined()) {
+                this.#pipelined_requests += 1;
+            } else {
+                this.#nonpipelinable_requests += 1;
+            }
+        }
+    }
+    request.ref();
+    bun.handleOom(this.#requests.writeItem(request));
+    connection.flushDataAndResetTimeout();
+}
+
+// pub fn cleanupWithJSValueReason(this: *@This(), reason: ?jsc.JSValue) void {
+//     while (this.#requests.pop()) |request| {
+//         request.failWithJSValue(reason);
+//     }
+// }
+
+pub fn current(this: *@This()) ?*JSMySQLQuery {
+    if (this.#requests.readableLength() == 0) {
+        return null;
+    }
+
+    return this.#requests.peekItem(0);
+}
+
+pub fn clean(this: *@This(), reason: ?JSValue, queries_array: JSValue) void {
+    while (this.current()) |request| {
+        if (request.isCompleted()) {
+            request.deref();
+            this.#requests.discard(1);
+            continue;
+        }
+        if (reason) |r| {
+            request.rejectWithJSValue(queries_array, r);
+        } else {
+            request.reject(queries_array, error.ConnectionClosed);
+        }
+        this.#requests.discard(1);
+        request.deref();
+        continue;
+    }
+    this.#pipelined_requests = 0;
+    this.#nonpipelinable_requests = 0;
+    this.#waiting_to_prepare = false;
+}
+
+pub fn deinit(this: *@This()) void {
+    for (this.#requests.readableSlice(0)) |request| {
+        this.#requests.discard(1);
+        // We cannot touch JS here
+        request.markAsFailed();
+        request.deref();
+    }
+    this.#pipelined_requests = 0;
+    this.#nonpipelinable_requests = 0;
+    this.#waiting_to_prepare = false;
+    this.#requests.deinit();
+}
+
+const std = @import("std");
+const bun = @import("bun");
+const jsc = bun.jsc;
+const JSValue = jsc.JSValue;
+const JSMySQLQuery = @import("./js/JSMySQLQuery.zig");
+const Queue = std.fifo.LinearFifo(*JSMySQLQuery, .Dynamic);
+const MySQLConnection = @import("./MySQLConnection.zig");
+const MAX_PIPELINE_SIZE = std.math.maxInt(u16); // about 64KB per connection

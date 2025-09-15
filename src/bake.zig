1217e8737983ca3d6396c48670c6ffd64b7b5a11
Alistair Smith <hi@alistair.sh>
2025-09-15 15:07:32 -0700

    remove builtInModules in zig

--- src/bake.zig
+++ src/bake.zig
@@ -627,37 +627,37 @@ pub const Framework = struct {
                     "registerClientReference",
             };
         };
-        const built_in_modules: bun.StringArrayHashMapUnmanaged(BuiltInModule) = built_in_modules: {
-            const array = try opts.getArray(global, "builtInModules") orelse
-                break :built_in_modules .{};
+        // const built_in_modules: bun.StringArrayHashMapUnmanaged(BuiltInModule) = built_in_modules: {
+        //     const array = try opts.getArray(global, "builtInModules") orelse
+        //         break :built_in_modules .{};
 
-            const len = try array.getLength(global);
-            var files: bun.StringArrayHashMapUnmanaged(BuiltInModule) = .{};
-            try files.ensureTotalCapacity(arena, len);
+        //     const len = try array.getLength(global);
+        //     var files: bun.StringArrayHashMapUnmanaged(BuiltInModule) = .{};
+        //     try files.ensureTotalCapacity(arena, len);
 
-            var it = try array.arrayIterator(global);
-            var i: usize = 0;
-            while (try it.next()) |file| : (i += 1) {
-                if (!file.isObject()) {
-                    return global.throwInvalidArguments("'builtInModules[{d}]' is not an object", .{i});
-                }
+        //     var it = try array.arrayIterator(global);
+        //     var i: usize = 0;
+        //     while (try it.next()) |file| : (i += 1) {
+        //         if (!file.isObject()) {
+        //             return global.throwInvalidArguments("'builtInModules[{d}]' is not an object", .{i});
+        //         }
 
-                const path = try getOptionalString(file, global, "import", refs, arena) orelse {
-                    return global.throwInvalidArguments("'builtInModules[{d}]' is missing 'import'", .{i});
-                };
+        //         const path = try getOptionalString(file, global, "import", refs, arena) orelse {
+        //             return global.throwInvalidArguments("'builtInModules[{d}]' is missing 'import'", .{i});
+        //         };
 
-                const value: BuiltInModule = if (try getOptionalString(file, global, "path", refs, arena)) |str|
-                    .{ .import = str }
-                else if (try getOptionalString(file, global, "code", refs, arena)) |str|
-                    .{ .code = str }
-                else
-                    return global.throwInvalidArguments("'builtInModules[{d}]' needs either 'path' or 'code'", .{i});
+        //         const value: BuiltInModule = if (try getOptionalString(file, global, "path", refs, arena)) |str|
+        //             .{ .import = str }
+        //         else if (try getOptionalString(file, global, "code", refs, arena)) |str|
+        //             .{ .code = str }
+        //         else
+        //             return global.throwInvalidArguments("'builtInModules[{d}]' needs either 'path' or 'code'", .{i});
 
-                files.putAssumeCapacity(path, value);
-            }
+        //         files.putAssumeCapacity(path, value);
+        //     }
 
-            break :built_in_modules files;
-        };
+        //     break :built_in_modules files;
+        // };
         const file_system_router_types: []FileSystemRouterType = brk: {
             const array: JSValue = try opts.getArray(global, "fileSystemRouterTypes") orelse {
                 return global.throwInvalidArguments("Missing 'framework.fileSystemRouterTypes'", .{});
@@ -755,7 +755,7 @@ pub const Framework = struct {
             .file_system_router_types = file_system_router_types,
             .react_fast_refresh = react_fast_refresh,
             .server_components = server_components,
-            .built_in_modules = built_in_modules,
+            // .built_in_modules = built_in_modules,
         };
 
         if (try opts.getOptional(global, "plugins", JSValue)) |plugin_array| {

a64f073ad39b61fa694436a0be4cb2cfc0a01657
Alistair Smith <hi@alistair.sh>
2025-09-15 21:13:45 -0700

    minimize

--- packages/bun-framework-react/index.ts
+++ packages/bun-framework-react/index.ts
@@ -1,13 +1,12 @@
-import type * as Bake from "bun:app";
-import { fileURLToPath } from "node:url";
+import type { Framework } from "bun:app";
 
-const framework: Bake.Framework = {
+const framework: Framework = {
   fileSystemRouterTypes: [
     {
       root: "pages",
       prefix: "/",
-      clientEntryPoint: fileURLToPath(import.meta.resolve("./client.tsx")),
-      serverEntryPoint: fileURLToPath(import.meta.resolve("./server.tsx")),
+      clientEntryPoint: Bun.fileURLToPath(import.meta.resolve("./client.tsx")),
+      serverEntryPoint: Bun.fileURLToPath(import.meta.resolve("./server.tsx")),
       extensions: [".tsx", ".jsx"],
       style: "nextjs-pages",
       layouts: true,

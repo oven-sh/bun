9b755176b454160c881613d811008f895a4ef43d
pfg <pfg@pfg.pw>
2025-09-18 21:26:31 -0700

    r

--- test/js/bun/test/concurrent.fixture.ts
+++ test/js/bun/test/concurrent.fixture.ts
@@ -1,7 +1,7 @@
 import { test, describe, beforeEach } from "bun:test";
 
 let activeGroup: (() => void)[] = [];
-function deterministicWait() {
+function tick() {
   const { resolve, reject, promise } = Promise.withResolvers();
   activeGroup.push(() => resolve());
   setTimeout(() => {
@@ -12,18 +12,18 @@ function deterministicWait() {
 
 test("test 1", async () => {
   console.log("[0] start test 1");
-  await deterministicWait();
+  await tick();
   console.log("[1] end test 1");
   console.log("--- concurrent boundary ---");
 });
 test.concurrent("test 2", async () => {
   console.log("[0] start test 2");
-  await deterministicWait();
+  await tick();
   console.log("[1] end test 2");
 });
 test.concurrent("test 3", async () => {
   console.log("[0] start test 3");
-  await deterministicWait();
+  await tick();
   console.log("[2] end test 3");
 });
 test("test 4", () => {
@@ -31,35 +31,35 @@ test("test 4", () => {
 });
 test.concurrent("test 5", async () => {
   console.log("[0] start test 5");
-  await deterministicWait();
+  await tick();
   console.log("[1] end test 5");
 });
 test.concurrent("test 6", async () => {
   console.log("[0] start test 6");
-  await deterministicWait();
+  await tick();
   console.log("[2] end test 6");
 });
 
 describe.concurrent("describe group 7", () => {
   beforeEach(async () => {
     console.log("[0] start before test 7");
-    await deterministicWait();
+    await tick();
     console.log("[3] end before test 7");
   });
 
   test("test 7", async () => {
     console.log("[3] start test 7");
-    await deterministicWait();
+    await tick();
     console.log("[4] end test 7");
   });
 });
 describe("describe group 8", () => {
   test.concurrent("test 8", async () => {
     console.log("[0] start test 8");
-    await deterministicWait();
-    await deterministicWait();
-    await deterministicWait();
-    await deterministicWait();
+    await tick();
+    await tick();
+    await tick();
+    await tick();
     console.log("[5] end test 8");
   });
 });

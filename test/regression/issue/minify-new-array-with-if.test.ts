becc86b1c1c81285942d85318c1faf60b78a410a
Dylan Conway <dylan.conway567@gmail.com>
2025-09-19 13:44:51 -0700

    fix

---
+++ test/regression/issue/minify-new-array-with-if.test.ts
@@ -0,0 +1,21 @@
+import { expect, test } from "bun:test";
+import { build, file } from "bun";
+import { tempDir } from "harness";
+import { join } from "path";
+
+test("minifying new Array(if (0) 1 else 2) works", async () => {
+  const testDir = tempDir("minify-new-array-with-if", {
+    "entry.js": "console.log(new Array(Math.random() > -1 ? 1 : 2));",
+  });
+
+  await build({
+    entrypoints: [join(testDir, "entry.js")],
+    minify: true,
+    outdir: join(testDir, "outdir"),
+  });
+
+  expect(await file(join(testDir, "outdir/entry.js")).text()).toMatchInlineSnapshot(`
+    "console.log(Array(Math.random()>-1?1:2));
+    "
+  `);
+});

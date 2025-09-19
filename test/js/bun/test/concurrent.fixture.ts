fd63a6b854a908a290ad8eab5ebe59294db2f88a
pfg <pfg@pfg.pw>
2025-09-18 21:25:10 -0700

    switch concurrent fixture to no longer use sleep

--- test/js/bun/test/concurrent.fixture.ts
+++ test/js/bun/test/concurrent.fixture.ts
@@ -1,24 +1,33 @@
 import { test, describe, beforeEach } from "bun:test";
 
-function sleep(n: number) {
-  // if this test is flaky, increase the number 10
-  return new Promise(r => setTimeout(r, 10 * n));
+let activeGroup: (() => void)[] = [];
+function deterministicWait() {
+  const { resolve, reject, promise } = Promise.withResolvers();
+  activeGroup.push(() => resolve());
+  setTimeout(() => {
+    process.nextTick(() => {
+      Promise.resolve().then(() => {
+        activeGroup.shift()?.();
+      });
+    });
+  }, 0);
+  return promise;
 }
 
 test("test 1", async () => {
   console.log("[0] start test 1");
-  await sleep(1);
+  await deterministicWait();
   console.log("[1] end test 1");
   console.log("--- concurrent boundary ---");
 });
 test.concurrent("test 2", async () => {
   console.log("[0] start test 2");
-  await sleep(1);
+  await deterministicWait();
   console.log("[1] end test 2");
 });
 test.concurrent("test 3", async () => {
   console.log("[0] start test 3");
-  await sleep(2);
+  await deterministicWait();
   console.log("[2] end test 3");
 });
 test("test 4", () => {
@@ -26,32 +35,35 @@ test("test 4", () => {
 });
 test.concurrent("test 5", async () => {
   console.log("[0] start test 5");
-  await sleep(1);
+  await deterministicWait();
   console.log("[1] end test 5");
 });
 test.concurrent("test 6", async () => {
   console.log("[0] start test 6");
-  await sleep(2);
+  await deterministicWait();
   console.log("[2] end test 6");
 });
 
 describe.concurrent("describe group 7", () => {
   beforeEach(async () => {
     console.log("[0] start before test 7");
-    await sleep(3);
+    await deterministicWait();
     console.log("[3] end before test 7");
   });
 
   test("test 7", async () => {
     console.log("[3] start test 7");
-    await sleep(1);
+    await deterministicWait();
     console.log("[4] end test 7");
   });
 });
 describe("describe group 8", () => {
   test.concurrent("test 8", async () => {
     console.log("[0] start test 8");
-    await sleep(5);
+    await deterministicWait();
+    await deterministicWait();
+    await deterministicWait();
+    await deterministicWait();
     console.log("[5] end test 8");
   });
 });

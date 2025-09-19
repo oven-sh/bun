bcea163fd22567212a44d86b49172f9a1a2be6a5
Alistair Smith <hi@alistair.sh>
2025-09-18 17:55:28 -0700

    update framework def

--- packages/bun-framework-react/index.ts
+++ packages/bun-framework-react/index.ts
@@ -4,21 +4,21 @@ const framework: Framework = {
   fileSystemRouterTypes: [
     {
       root: "pages",
-      prefix: "/",
       clientEntryPoint: Bun.fileURLToPath(import.meta.resolve("./client.tsx")),
       serverEntryPoint: Bun.fileURLToPath(import.meta.resolve("./server.tsx")),
       extensions: [".tsx", ".jsx"],
       style: "nextjs-pages",
       layouts: true,
       ignoreUnderscores: true,
-      ignoreDirs: ["node_modules", ".git"],
     },
   ],
 
+  staticRouters: ["public"],
+
   serverComponents: {
     separateSSRGraph: true,
-    serverRuntimeImportSource: "react-server-dom-bun/server",
     serverRegisterClientReferenceExport: "registerClientReference",
+    serverRuntimeImportSource: "react-server-dom-webpack/server",
   },
 
   reactFastRefresh: {

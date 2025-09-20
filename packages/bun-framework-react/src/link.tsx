d8fa01ed412ca667743a8cb62d621077bb22a901
Alistair Smith <hi@alistair.sh>
2025-09-19 18:16:09 -0700

    basic impl

--- packages/bun-framework-react/src/link.tsx
+++ packages/bun-framework-react/src/link.tsx
@@ -1,3 +1,20 @@
-export function Link(): React.JSX.Element {
-  return <p>boon cpu</p>;
+import { router } from "../client/constants.ts";
+
+export interface LinkProps extends React.ComponentProps<"a"> {
+  /**
+   * The URL to navigate to
+   */
+  href: string;
+}
+
+export function Link(props: LinkProps): React.JSX.Element {
+  return (
+    <a
+      {...props}
+      onClick={async e => {
+        e.preventDefault();
+        await router.navigate(props.href, undefined);
+      }}
+    />
+  );
 }

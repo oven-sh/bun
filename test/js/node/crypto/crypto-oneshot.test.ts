32c5cb599343b29e183d21a61a8467bde593a639
Filip <filipstevanovic@gmail.com>
2025-09-23 10:16:19 +0200

    revert test updates

--- test/js/node/crypto/crypto-oneshot.test.ts
+++ test/js/node/crypto/crypto-oneshot.test.ts
@@ -62,6 +62,7 @@ describe("crypto.hash", () => {
     "sha-512_256",
     "sha-512256",
     "sha512-256",
+    "sha384",
     "sha3-224",
     "sha3-256",
     "sha3-384",
@@ -72,7 +73,7 @@ describe("crypto.hash", () => {
     "shake-256",
   ].forEach(method => {
     test(`output matches crypto.createHash(${method})`, () => {
-      for (const outputEncoding of ["buffer", "hex", "base64", "base64url", undefined]) {
+      for (const outputEncoding of ["buffer", "hex", "base64", undefined]) {
         const oldDigest = crypto
           .createHash(method)
           .update(input)

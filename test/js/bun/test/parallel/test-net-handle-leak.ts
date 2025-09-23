69063812bef33e931473a740f996fe5dbbe7c7bd
Meghan Denny <meghan@bun.com>
2025-09-23 11:46:16 -0700

    cross platform test

--- test/js/bun/test/parallel/test-net-handle-leak.ts
+++ test/js/bun/test/parallel/test-net-handle-leak.ts
@@ -1,11 +1,15 @@
 import * as net from "node:net";
 import { setTimeout } from "node:timers/promises";
 import { expect } from "bun:test";
+import { tmpdir } from "node:os";
+import { join } from "node:path";
+
+const listen_path = join(tmpdir(), "test-net-successful-connection-handle-leak.sock");
 
 const { promise, resolve } = Promise.withResolvers();
 const server = net
   .createServer()
-  .listen("/tmp/test-net-successful-connection-handle-leak.sock")
+  .listen(listen_path)
   .on("listening", () => resolve());
 await promise;
 const address = server.address();
@@ -15,16 +19,13 @@ let started;
 
 started = 0;
 while (started < 50_000) {
-  const promises = [];
+  const promises: Promise<void>[] = [];
   for (let i = 0; i < 100; i++) {
-    const { promise, resolve, reject } = Promise.withResolvers();
+    const { promise, resolve, reject } = Promise.withResolvers<void>();
     const socket = net
-      .connect({
-        path: String.fromCharCode("/".charCodeAt(0)) + "tmp/test-net-successful-connection-handle-leak.sock".slice(),
-      })
+      .connect({ path: listen_path })
       .on("connect", () => {
         socket.on("close", () => resolve());
-
         socket.end();
       })
       .on("error", e => {
@@ -44,16 +45,13 @@ const warmup_rss = process.memoryUsage.rss();
 
 started = 0;
 while (started < 100_000) {
-  const promises = [];
+  const promises: Promise<void>[] = [];
   for (let i = 0; i < 100; i++) {
-    const { promise, resolve, reject } = Promise.withResolvers();
+    const { promise, resolve, reject } = Promise.withResolvers<void>();
     const socket = net
-      .connect({
-        path: String.fromCharCode("/".charCodeAt(0)) + "tmp/test-net-successful-connection-handle-leak.sock".slice(),
-      })
+      .connect({ path: listen_path })
       .on("connect", () => {
         socket.on("close", () => resolve());
-
         socket.end();
       })
       .on("error", e => {

b2c6f98efc260b5283cba0cc5f19a7bae3163916
Meghan Denny <meghan@bun.com>
2025-09-25 15:11:56 -0700

    [build images]

--- src/codegen/cppbind.ts
+++ src/codegen/cppbind.ts
@@ -719,7 +719,7 @@ function generateZigFn(
     `    } else {`,
     `        const value = raw.${formatZigName(fn.name)}(${fn.parameters.map(p => formatZigName(p.name)).join(", ")});`,
     `        if (value == ${equalsValue}) return error.JSError;`,
-    ...(fn.tag === "false_is_throw" ? [] : [`        return value;`]),
+    ...(fn.tag === "false_is_throw" ? [] : [`        return value${fn.tag === "null_is_throw" ? ".?" : ""};`]),
     `    }`,
     `}`,
   );

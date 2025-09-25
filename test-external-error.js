81e50395e0f8443ce602df4f5992919cdd240f42
RiskyMH <git@riskymh.dev>
2025-09-26 05:27:51 +1000

    lol

--- test-external-error.js
+++
@@ -1,8 +0,0 @@
-// Test importing from outside CWD
-import { externalModuleFunction } from '/tmp/external-module.js';
-
-function localFunction() {
-  externalModuleFunction();
-}
-
-localFunction();

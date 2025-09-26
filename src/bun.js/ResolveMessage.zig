bbd1c6054deffdc79c29743111c0a961e26cee1f
Meghan Denny <meghan@bun.com>
2025-09-25 19:56:10 -0700

    fix test/bundler/esbuild/tsconfig.test.ts

--- src/bun.js/ResolveMessage.zig
+++ src/bun.js/ResolveMessage.zig
@@ -226,6 +226,7 @@ pub const ResolveMessage = struct {
 
     pub fn finalize(this: *ResolveMessage) callconv(.C) void {
         this.msg.deinit(this.allocator);
+        bun.default_allocator.destroy(this);
     }
 };
 

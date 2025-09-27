24a075e57b9ceff5dd11a8ba95ddca6b2556a67f
Sosuke Suzuki <aosukeke@gmail.com>
2025-09-27 18:32:20 +0900

    Clear extra frames with zero

--- src/bun.js/VirtualMachine.zig
+++ src/bun.js/VirtualMachine.zig
@@ -2689,6 +2689,8 @@ pub fn remapZigException(
                 frames[j] = frame;
                 j += 1;
             }
+
+            @memset(frames[j..], jsc.ZigStackFrame.Zero);
             exception.stack.frames_len = @as(u8, @truncate(j));
             frames.len = j;
         }

1c6165e68a83bd2053d9216afe0007897df20cc6
Zack Radisic <56137411+zackradisic@users.noreply.github.com>
2025-09-27 15:39:40 -0700

    fix leak

--- src/bun.js/event_loop/Task.zig
+++ src/bun.js/event_loop/Task.zig
@@ -63,7 +63,6 @@ pub const Task = TaggedPointerUnion(.{
     RuntimeTranspilerStore,
     S3HttpDownloadStreamingTask,
     S3HttpSimpleTask,
-    ServerAllConnectionsClosedTask,
     ShellAsync,
     ShellAsyncSubprocessDone,
     ShellCondExprStatTask,
@@ -468,10 +467,6 @@ pub fn tickQueueWithCount(this: *EventLoop, virtual_machine: *VirtualMachine) u3
                 var any: *RuntimeTranspilerStore = task.get(RuntimeTranspilerStore).?;
                 any.runFromJSThread(this, global, virtual_machine);
             },
-            @field(Task.Tag, @typeName(ServerAllConnectionsClosedTask)) => {
-                var any: *ServerAllConnectionsClosedTask = task.get(ServerAllConnectionsClosedTask).?;
-                any.runFromJSThread(virtual_machine);
-            },
             @field(Task.Tag, @typeName(bun.bundle_v2.DeferredBatchTask)) => {
                 var any: *bun.bundle_v2.DeferredBatchTask = task.get(bun.bundle_v2.DeferredBatchTask).?;
                 any.runOnJSThread();
@@ -530,7 +525,6 @@ const Environment = bun.Environment;
 const TaggedPointerUnion = bun.TaggedPointerUnion;
 const shell = bun.shell;
 const FlushPendingFileSinkTask = bun.webcore.FileSink.FlushPendingTask;
-const ServerAllConnectionsClosedTask = bun.api.server.ServerAllConnectionsClosedTask;
 const CopyFilePromiseTask = bun.webcore.Blob.copy_file.CopyFilePromiseTask;
 const GetAddrInfoRequestTask = bun.api.dns.GetAddrInfoRequest.Task;
 const ReadFileTask = bun.webcore.Blob.read_file.ReadFileTask;

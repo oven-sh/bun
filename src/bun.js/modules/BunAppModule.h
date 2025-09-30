9ce909f63429818ad160b911faaecb118781723a
Zack Radisic <zack@theradisic.com>
2025-09-29 18:45:05 -0700

    Fix crash

--- src/bun.js/modules/BunAppModule.h
+++ src/bun.js/modules/BunAppModule.h
@@ -3,21 +3,23 @@
 #include "root.h"
 #include "_NativeModule.h"
 #include "BakeAdditionsToGlobalObject.h"
+#include <stdio.h>
 
 namespace Zig {
 using namespace WebCore;
 using namespace JSC;
 
-// External function to get SSRResponse constructor
-extern "C" JSC::EncodedJSValue Bake__getSSRResponseConstructor(JSC::JSGlobalObject* globalObject);
-
 DEFINE_NATIVE_MODULE(BunApp)
 {
     INIT_NATIVE_MODULE(1);
 
-    JSValue ssrResponseConstructor = JSValue::decode(Bake__getSSRResponseConstructor(globalObject));
+    printf("INSIDE HERE MOFO!\n");
+    auto* zig = reinterpret_cast<Zig::GlobalObject*>(globalObject);
+    JSValue ssrResponseConstructor = zig->bakeAdditions().JSBakeResponseConstructor(zig);
 
     put(JSC::Identifier::fromString(vm, "Response"_s), ssrResponseConstructor);
+
+    printf("RETURN NATIVE MODULE HERE MOFO!\n");
 
     RETURN_NATIVE_MODULE();
 }

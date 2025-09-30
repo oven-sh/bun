5e5d8765e369e741d8b0f39df8ca0d7a9576a5c8
Zack Radisic <56137411+zackradisic@users.noreply.github.com>
2025-09-29 19:18:10 -0700

    lol

--- src/bun.js/modules/BunAppModule.h
+++ src/bun.js/modules/BunAppModule.h
@@ -3,7 +3,6 @@
 #include "root.h"
 #include "_NativeModule.h"
 #include "BakeAdditionsToGlobalObject.h"
-#include <stdio.h>
 
 namespace Zig {
 using namespace WebCore;
@@ -13,13 +12,10 @@ DEFINE_NATIVE_MODULE(BunApp)
 {
     INIT_NATIVE_MODULE(1);
 
-    printf("INSIDE HERE MOFO!\n");
     auto* zig = reinterpret_cast<Zig::GlobalObject*>(globalObject);
     JSValue ssrResponseConstructor = zig->bakeAdditions().JSBakeResponseConstructor(zig);
 
     put(JSC::Identifier::fromString(vm, "Response"_s), ssrResponseConstructor);
-
-    printf("RETURN NATIVE MODULE HERE MOFO!\n");
 
     RETURN_NATIVE_MODULE();
 }

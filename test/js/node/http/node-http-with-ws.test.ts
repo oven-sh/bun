6f40741420b1119bfbd5ad8c7217c9c565d42ec6
Ciro Spaciari <ciro.spaciari@gmail.com>
2025-09-30 20:06:15 -0700

    add test

---
+++ test/js/node/http/node-http-with-ws.test.ts
@@ -0,0 +1,49 @@
+import https from "https";
+import tls from "tls";
+import type { AddressInfo } from "node:net";
+import { WebSocketServer } from "ws";
+import { tls as options } from "harness";
+import { expect, test } from "bun:test";
+test("should not crash when closing sockets after upgrade", async () => {
+  const { promise, resolve } = Promise.withResolvers();
+  let http_sockets: tls.TLSSocket[] = [];
+
+  const server = https.createServer(options, (req, res) => {
+    http_sockets.push(res.socket as tls.TLSSocket);
+    res.writeHead(200, { "Content-Type": "text/plain", "Connection": "Keep-Alive" });
+    res.end("okay");
+    res.detachSocket(res.socket!);
+  });
+
+  server.listen(0, "127.0.0.1", () => {
+    const wsServer = new WebSocketServer({ server });
+    wsServer.on("connection", socket => {});
+
+    const socket = tls.connect({ port: (server.address() as AddressInfo).port, ca: options.cert }, () => {
+      // normal request keep the socket alive
+      socket.write("GET / HTTP/1.1\r\nHost: localhost:3000\r\nConnection: Keep-Alive\r\nContent-Length: 0\r\n\r\n");
+      socket.write("GET / HTTP/1.1\r\nHost: localhost:3000\r\nConnection: Keep-Alive\r\nContent-Length: 0\r\n\r\n");
+      socket.write("GET / HTTP/1.1\r\nHost: localhost:3000\r\nConnection: Keep-Alive\r\nContent-Length: 0\r\n\r\n");
+      // upgrade to websocket
+      socket.write(
+        "GET / HTTP/1.1\r\nHost: localhost:3000\r\nConnection: Upgrade\r\nUpgrade: websocket\r\nSec-WebSocket-Version: 13\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n\r\n",
+      );
+    });
+    socket.on("data", data => {
+      const isWebSocket = data?.toString().includes("Upgrade: websocket");
+      if (isWebSocket) {
+        socket.destroy();
+        setTimeout(() => {
+          http_sockets.forEach(http_socket => {
+            http_socket?.destroy();
+          });
+          server.closeAllConnections();
+          resolve();
+        }, 10);
+      }
+    });
+  });
+
+  await promise;
+  expect().pass();
+});

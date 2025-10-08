93bef3b0d4a669990f9452a50e03cd5e3cbec01d
Dylan Conway <dylan.conway567@gmail.com>
2025-10-07 17:52:41 -0700

    Revert "[autofix.ci] apply automated fixes"
    This reverts commit 3e253e032195db3fa16899ca4fb64b5378c48e03.

--- test/cli/install/bun-add.test.ts
+++ test/cli/install/bun-add.test.ts
@@ -1,6 +1,7 @@
 import { file, spawn } from "bun";
 import { afterAll, afterEach, beforeAll, beforeEach, expect, it, setDefaultTimeout } from "bun:test";
 import { access, appendFile, copyFile, mkdir, readlink, rm, writeFile } from "fs/promises";
+import { readlinkSync } from "fs";
 import { bunExe, bunEnv as env, readdirSorted, tmpdirSync, toBeValidBin, toBeWorkspaceLink, toHaveBins } from "harness";
 import { join, relative, resolve } from "path";
 import {

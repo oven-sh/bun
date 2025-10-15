35551f888c8c4572c6a6333da61abf1466c890cd
Jarred Sumner <jarred@jarredsumner.com>
2025-10-14 19:54:13 -0700

    Revert "ok"
    This reverts commit 17088e24d8d83f488a71bbea0feffe2d5e626860.

--- src/bun.js/webcore/fetch.zig
+++ src/bun.js/webcore/fetch.zig
@@ -431,9 +431,9 @@ pub const FetchTasklet = struct {
                     this.memory_reporter.discard(scheduled_response_buffer.allocatedSlice());
                     readable.ptr.Bytes.onData(
                         .{
-                            // TODO: We are still investigating the memory issues with owned_and_done
-                            // .owned_and_done = bun.ByteList.moveFromList(scheduled_response_buffer),
-                            .temporary_and_done = bun.ByteList.fromBorrowedSliceDangerous(chunk),
+                            .owned_and_done = bun.ByteList.moveFromList(scheduled_response_buffer),
+                            // Investigate: owned_and_done is the correct behavior, is having bigger memory usage than temporary_and_done
+                            // .temporary_and_done = bun.ByteList.fromBorrowedSliceDangerous(chunk),
                         },
                         bun.default_allocator,
                     );

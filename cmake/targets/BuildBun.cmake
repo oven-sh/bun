a4179a7c2dabf6890dbc5fa72fb02cc0f2cbcfd7
pfg <pfg@pfg.pw>
2025-10-29 16:15:48 -0700

    remove freference-trace in buildbun.cmake

--- cmake/targets/BuildBun.cmake
+++ cmake/targets/BuildBun.cmake
@@ -694,7 +694,6 @@ register_command(
       -Dcodegen_embed=$<IF:$<BOOL:${CODEGEN_EMBED}>,true,false>
       --prominent-compile-errors
       --summary all
-      -freference-trace
       ${ZIG_FLAGS_BUN}
   ARTIFACTS
     ${BUN_ZIG_OUTPUT}

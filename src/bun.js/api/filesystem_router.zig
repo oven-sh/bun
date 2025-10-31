153b7ab0148305ee99effb85266608cc1fa7f285
pfg <pfg@pfg.pw>
2025-10-30 21:24:56 -0700

    fix filesystem_router.test.ts on windows

--- src/bun.js/api/filesystem_router.zig
+++ src/bun.js/api/filesystem_router.zig
@@ -251,8 +251,8 @@ pub const FileSystemRouter = struct {
             return globalThis.throwValue(try log.toJS(globalThis, globalThis.allocator(), "loading routes"));
         };
 
-        this.arena.deinit();
         this.router.deinit();
+        this.arena.deinit();
         globalThis.allocator().destroy(this.arena);
 
         this.arena = arena;

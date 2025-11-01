ccfcc084fd39394eb411071a30fbf4b50873518e
pfg <pfg@pfg.pw>
2025-10-31 18:23:07 -0700

    linux fix?

--- build.zig
+++ build.zig
@@ -612,7 +612,7 @@ fn configureObj(b: *Build, opts: *BunBuildOptions, obj: *Compile) void {
 
     // Object options
     obj.use_llvm = !opts.no_llvm;
-    obj.use_lld = if (opts.os == .mac) false else !opts.no_llvm;
+    obj.use_lld = if (opts.os == .mac or opts.os == .linux) false else !opts.no_llvm;
 
     if (opts.optimize == .Debug) {
         if (@hasField(std.meta.Child(@TypeOf(obj)), "llvm_codegen_threads"))

fca1ec88c31d2218d67c1e953e99e858388f6be2
Jarred Sumner <jarred@jarredsumner.com>
2025-11-03 22:06:57 -0800

    Update do.test.ts

--- test/napi/node-napi-tests/test/node-api/test_general/do.test.ts
+++ test/napi/node-napi-tests/test/node-api/test_general/do.test.ts
@@ -1,12 +1,13 @@
 import { basename, dirname, sep } from "node:path";
 import { build, run } from "../../../harness";
+import { test } from "bun:test";
 
 test("build", async () => {
   await build(import.meta.dir);
 });
 
 for (const file of Array.from(new Bun.Glob("*.js").scanSync(import.meta.dir))) {
-  test.todoIf(["test.js"].includes(file))(file, () => {
+  test(file, () => {
     run(dirname(import.meta.dir), basename(import.meta.dir) + sep + file);
   });
 }

ad6a08e77aef0a4c0ebff73bff5b99874fe2ba11
Marko Vejnovic <marko@bun.com>
2025-11-05 18:59:15 -0800

    ENG-21468: RELEASE=1 disables sccache

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -3,10 +3,12 @@ import { existsSync, readFileSync } from "node:fs";
 import { basename, join, relative, resolve } from "node:path";
 import {
   formatAnnotationToHtml,
+  getEnv,
   getSecret,
   isCI,
   isWindows,
   parseAnnotations,
+  parseBoolean,
   printEnvironment,
   reportAnnotationToBuildKite,
   startGroup,
@@ -71,7 +73,8 @@ async function build(args) {
   }
 
   if (!generateOptions["-DCACHE_STRATEGY"]) {
-    generateOptions["-DCACHE_STRATEGY"] = "read-write";
+    generateOptions["-DCACHE_STRATEGY"] =
+      parseBoolean(getEnv("RELEASE", false) || "false") ? "none" : "read-write";
   }
 
   const toolchain = generateOptions["--toolchain"];

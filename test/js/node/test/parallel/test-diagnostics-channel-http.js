912fced1e8313ee17db5f4d27dae009c0b0b5066
Alistair Smith <hi@alistair.sh>
2025-11-06 12:28:53 -0800

    use the correct parallel test

--- test/js/node/test/parallel/test-diagnostics-channel-http.js
+++ test/js/node/test/parallel/test-diagnostics-channel-http.js
@@ -3,15 +3,14 @@ const common = require('../common');
 const { addresses } = require('../common/internet');
 const assert = require('assert');
 const http = require('http');
+const net = require('net');
 const dc = require('diagnostics_channel');
 
+const isHTTPServer = (server) => server instanceof http.Server;
 const isIncomingMessage = (object) => object instanceof http.IncomingMessage;
 const isOutgoingMessage = (object) => object instanceof http.OutgoingMessage;
+const isNetSocket = (socket) => socket instanceof net.Socket;
 const isError = (error) => error instanceof Error;
-
-dc.subscribe('http.client.request.created', common.mustCall(({ request }) => {
-  assert.strictEqual(isOutgoingMessage(request), true);
-}, 2));
 
 dc.subscribe('http.client.request.start', common.mustCall(({ request }) => {
   assert.strictEqual(isOutgoingMessage(request), true);
@@ -30,22 +29,55 @@ dc.subscribe('http.client.response.finish', common.mustCall(({
   assert.strictEqual(isIncomingMessage(response), true);
 }));
 
+dc.subscribe('http.server.request.start', common.mustCall(({
+  request,
+  response,
+  socket,
+  server,
+}) => {
+  assert.strictEqual(isIncomingMessage(request), true);
+  assert.strictEqual(isOutgoingMessage(response), true);
+  assert.strictEqual(isNetSocket(socket), true);
+  assert.strictEqual(isHTTPServer(server), true);
+}));
+
+dc.subscribe('http.server.response.finish', common.mustCall(({
+  request,
+  response,
+  socket,
+  server,
+}) => {
+  assert.strictEqual(isIncomingMessage(request), true);
+  assert.strictEqual(isOutgoingMessage(response), true);
+  assert.strictEqual(isNetSocket(socket), true);
+  assert.strictEqual(isHTTPServer(server), true);
+}));
+
+dc.subscribe('http.server.response.created', common.mustCall(({
+  request,
+  response,
+}) => {
+  assert.strictEqual(isIncomingMessage(request), true);
+  assert.strictEqual(isOutgoingMessage(response), true);
+}));
+
+dc.subscribe('http.client.request.created', common.mustCall(({ request }) => {
+  assert.strictEqual(isOutgoingMessage(request), true);
+  assert.strictEqual(isHTTPServer(server), true);
+}, 2));
+
 const server = http.createServer(common.mustCall((req, res) => {
   res.end('done');
 }));
 
 server.listen(async () => {
   const { port } = server.address();
-
-  // Test error event with invalid host
   const invalidRequest = http.get({
     host: addresses.INVALID_HOST,
   });
   await new Promise((resolve) => {
     invalidRequest.on('error', resolve);
   });
-
-  // Test successful request
   http.get(`http://localhost:${port}`, (res) => {
     res.resume();
     res.on('end', () => {

f403ceefeaeebe7940d6dad9130c3419a05cbed7
Alistair Smith <hi@alistair.sh>
2025-11-05 19:22:45 -0800

    revert spawn.ipc.test.ts

--- test/js/bun/spawn/spawn.ipc.test.ts
+++ test/js/bun/spawn/spawn.ipc.test.ts
@@ -4,34 +4,30 @@ import { bunExe, gcTick } from "harness";
 import path from "path";
 
 describe.each(["advanced", "json"])("ipc mode %s", mode => {
-  it("the subprocess should be defined and the child should send", async () => {
+  it("the subprocess should be defined and the child should send", done => {
     gcTick();
-    const done = Promise.withResolvers<void>();
-
-    await using returned_subprocess = spawn([bunExe(), path.join(__dirname, "bun-ipc-child.js")], {
+    const returned_subprocess = spawn([bunExe(), path.join(__dirname, "bun-ipc-child.js")], {
       ipc: (message, subProcess) => {
         expect(subProcess).toBe(returned_subprocess);
         expect(message).toBe("hello");
-        done.resolve();
+        subProcess.kill();
+        done();
         gcTick();
       },
       stdio: ["inherit", "inherit", "inherit"],
       serialization: mode,
     });
-
-    await done.promise;
   });
 
-  it("the subprocess should receive the parent message and respond back", async () => {
+  it("the subprocess should receive the parent message and respond back", done => {
     gcTick();
 
     const parentMessage = "I am your father";
-    const done = Promise.withResolvers<void>();
-
-    await using childProc = spawn([bunExe(), path.join(__dirname, "bun-ipc-child-respond.js")], {
-      ipc: (message, _subProcess) => {
+    const childProc = spawn([bunExe(), path.join(__dirname, "bun-ipc-child-respond.js")], {
+      ipc: (message, subProcess) => {
         expect(message).toBe(`pong:${parentMessage}`);
-        done.resolve();
+        subProcess.kill();
+        done();
         gcTick();
       },
       stdio: ["inherit", "inherit", "inherit"],
@@ -40,6 +36,5 @@ describe.each(["advanced", "json"])("ipc mode %s", mode => {
 
     childProc.send(parentMessage);
     gcTick();
-    await done.promise;
   });
 });

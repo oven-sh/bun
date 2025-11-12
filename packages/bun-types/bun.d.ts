c48fe1815ad6959001d1a1478542e7c8da11260b
RiskyMH <git@riskymh.dev>
2025-11-13 01:37:24 +1100

    it can be undefined

--- packages/bun-types/bun.d.ts
+++ packages/bun-types/bun.d.ts
@@ -6431,14 +6431,14 @@ declare module "bun" {
     catalogs?: Record<string, Record<string, string>>;
 
     /**
-     * `0` for projects created before v1.3.2, `1` for projects created after
+     * `0` / `undefined` for projects created before v1.3.2, `1` for projects created after.
      *
      * ---
      * Right now this only changes the default [install linker strategy](https://bun.com/docs/pm/cli/install#isolated-installs):
      * - With `0`, the linker is hoisted.
      * - With `1`, the linker is isolated for workspaces and hoisted for single-package projects.
      */
-    configVersion: 0 | 1;
+    configVersion?: 0 | 1;
 
     /**
      * ```

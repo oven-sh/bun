c2851bf51f11327c84ce090f4d14f7e1afbdbb02
Dylan Conway <dylan.conway567@gmail.com>
2025-11-11 17:49:31 -0800

    add test for cycles

--- test/cli/install/nohoist.test.ts
+++ test/cli/install/nohoist.test.ts
@@ -1,6 +1,7 @@
 import { afterAll, beforeAll, describe, expect, test } from "bun:test";
 import { VerdaccioRegistry, bunEnv, readdirSorted, runBunInstall } from "harness";
 import { join } from "path";
+import { exists } from "fs/promises";
 
 var registry = new VerdaccioRegistry();
 
@@ -59,5 +60,31 @@ describe("workspaces.nohoist", () => {
 
     expect(await readdirSorted(join(packageDir, "node_modules"))).toEqual(["a-dep", "no-deps", "pkg1"]);
     expect(await readdirSorted(join(packageDir, "packages/pkg1/node_modules"))).toEqual(["one-dep"]);
+  });
+
+  test("handles cycles", async () => {
+    const { packageDir } = await registry.createTestDir({
+      files: {
+        "package.json": JSON.stringify({
+          name: "cycles",
+          workspaces: {
+            nohoist: ["**"],
+          },
+          dependencies: {
+            "a-dep-b": "1.0.0",
+          },
+        }),
+      },
+    });
+
+    await runBunInstall(bunEnv, packageDir, { linker: "hoisted" });
+
+    expect(
+      await Promise.all([
+        readdirSorted(join(packageDir, "node_modules")),
+        readdirSorted(join(packageDir, "node_modules/a-dep-b/node_modules")),
+        exists(join(packageDir, "node_modules/a-dep-b/node_modules/b-dep-a/node_modules")),
+      ]),
+    ).toEqual([["a-dep-b"], ["b-dep-a"], false]);
   });
 });

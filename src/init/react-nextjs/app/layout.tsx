1b940426bca0224b2638b8ce73d5955ddd272402
Lydia Hallie <lydiajuliettehallie@gmail.com>
2025-11-14 13:42:05 -0800

    format

--- src/init/react-nextjs/app/layout.tsx
+++ src/init/react-nextjs/app/layout.tsx
@@ -13,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/init/react-nextjs/app/page.tsx
+++ src/init/react-nextjs/app/page.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 async function getBunInfo() {
   return {

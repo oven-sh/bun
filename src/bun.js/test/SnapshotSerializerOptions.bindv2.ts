efdb2e8769305f5428df341a9a4bf0f7a67b09b3
pfg <pfg@pfg.pw>
2025-11-19 12:51:06 -0800

    fixes bindv2 usage

--- src/bun.js/test/SnapshotSerializerOptions.bindv2.ts
+++ src/bun.js/test/SnapshotSerializerOptions.bindv2.ts
@@ -3,23 +3,19 @@ import * as b from "bindgenv2";
 export const SnapshotSerializerOptions = b.dictionary(
   {
     name: "SnapshotSerializerOptions",
-    userFacingName: "SnapshotSerializerOptions",
     generateConversionFunction: true,
   },
   {
     test: {
       type: b.RawAny,
-      required: true,
       internalName: "test_fn",
     },
     serialize: {
       type: b.RawAny,
-      optional: true,
       internalName: "serialize_fn",
     },
     print: {
       type: b.RawAny,
-      optional: true,
       internalName: "print_fn",
     },
   },

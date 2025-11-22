7c73bd543bc6eeb9875e83a5d59d3acaca5e291f
Chongyi Zheng <git@zcy.dev>
2025-11-22 16:56:04 -0600

    Avoid editing shell config if requested

--- src/cli/install.sh
+++ src/cli/install.sh
@@ -188,129 +188,135 @@ fi
 
 echo
 
-case $(basename "$SHELL") in
-fish)
-    # Install completions, but we don't care if it fails
-    IS_BUN_AUTO_UPDATE=true SHELL=fish $exe completions &>/dev/null || :
+no_shell_config="${NO_SHELL_CONFIG:-false}"
 
-    commands=(
-        "set --export $install_env $quoted_install_dir"
-        "set --export PATH $bin_env \$PATH"
-    )
+if [[ "$no_shell_config" == "true" ]]; then
+    info "Skipping shell configuration as requested."
+elif
+    case $(basename "$SHELL") in
+    fish)
+        # Install completions, but we don't care if it fails
+        IS_BUN_AUTO_UPDATE=true SHELL=fish $exe completions &>/dev/null || :
 
-    fish_config=$HOME/.config/fish/config.fish
-    tilde_fish_config=$(tildify "$fish_config")
-
-    if [[ -w $fish_config ]]; then
-        {
-            echo -e '\n# bun'
-
-            for command in "${commands[@]}"; do
-                echo "$command"
-            done
-        } >>"$fish_config"
-
-        info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_fish_config\""
-
-        refresh_command="source $tilde_fish_config"
-    else
-        echo "Manually add the directory to $tilde_fish_config (or similar):"
-
-        for command in "${commands[@]}"; do
-            info_bold "  $command"
-        done
-    fi
-    ;;
-zsh)
-    # Install completions, but we don't care if it fails
-    IS_BUN_AUTO_UPDATE=true SHELL=zsh $exe completions &>/dev/null || :
-
-    commands=(
-        "export $install_env=$quoted_install_dir"
-        "export PATH=\"$bin_env:\$PATH\""
-    )
-
-    zsh_config=$HOME/.zshrc
-    tilde_zsh_config=$(tildify "$zsh_config")
-
-    if [[ -w $zsh_config ]]; then
-        {
-            echo -e '\n# bun'
-
-            for command in "${commands[@]}"; do
-                echo "$command"
-            done
-        } >>"$zsh_config"
-
-        info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_zsh_config\""
-
-        refresh_command="exec $SHELL"
-    else
-        echo "Manually add the directory to $tilde_zsh_config (or similar):"
-
-        for command in "${commands[@]}"; do
-            info_bold "  $command"
-        done
-    fi
-    ;;
-bash)
-    # Install completions, but we don't care if it fails
-    IS_BUN_AUTO_UPDATE=true SHELL=bash $exe completions &>/dev/null || :
-
-    commands=(
-        "export $install_env=$quoted_install_dir"
-        "export PATH=\"$bin_env:\$PATH\""
-    )
-
-    bash_configs=(
-        "$HOME/.bash_profile"
-        "$HOME/.bashrc"
-    )
-
-    if [[ ${XDG_CONFIG_HOME:-} ]]; then
-        bash_configs+=(
-            "$XDG_CONFIG_HOME/.bash_profile"
-            "$XDG_CONFIG_HOME/.bashrc"
-            "$XDG_CONFIG_HOME/bash_profile"
-            "$XDG_CONFIG_HOME/bashrc"
+        commands=(
+            "set --export $install_env $quoted_install_dir"
+            "set --export PATH $bin_env \$PATH"
         )
-    fi
 
-    set_manually=true
-    for bash_config in "${bash_configs[@]}"; do
-        tilde_bash_config=$(tildify "$bash_config")
+        fish_config=$HOME/.config/fish/config.fish
+        tilde_fish_config=$(tildify "$fish_config")
 
-        if [[ -w $bash_config ]]; then
+        if [[ -w $fish_config ]]; then
             {
                 echo -e '\n# bun'
 
                 for command in "${commands[@]}"; do
                     echo "$command"
                 done
-            } >>"$bash_config"
+            } >>"$fish_config"
 
-            info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_bash_config\""
+            info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_fish_config\""
 
-            refresh_command="source $bash_config"
-            set_manually=false
-            break
+            refresh_command="source $tilde_fish_config"
+        else
+            echo "Manually add the directory to $tilde_fish_config (or similar):"
+
+            for command in "${commands[@]}"; do
+                info_bold "  $command"
+            done
         fi
-    done
+        ;;
+    zsh)
+        # Install completions, but we don't care if it fails
+        IS_BUN_AUTO_UPDATE=true SHELL=zsh $exe completions &>/dev/null || :
 
-    if [[ $set_manually = true ]]; then
-        echo "Manually add the directory to $tilde_bash_config (or similar):"
+        commands=(
+            "export $install_env=$quoted_install_dir"
+            "export PATH=\"$bin_env:\$PATH\""
+        )
 
-        for command in "${commands[@]}"; do
-            info_bold "  $command"
+        zsh_config=$HOME/.zshrc
+        tilde_zsh_config=$(tildify "$zsh_config")
+
+        if [[ -w $zsh_config ]]; then
+            {
+                echo -e '\n# bun'
+
+                for command in "${commands[@]}"; do
+                    echo "$command"
+                done
+            } >>"$zsh_config"
+
+            info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_zsh_config\""
+
+            refresh_command="exec $SHELL"
+        else
+            echo "Manually add the directory to $tilde_zsh_config (or similar):"
+
+            for command in "${commands[@]}"; do
+                info_bold "  $command"
+            done
+        fi
+        ;;
+    bash)
+        # Install completions, but we don't care if it fails
+        IS_BUN_AUTO_UPDATE=true SHELL=bash $exe completions &>/dev/null || :
+
+        commands=(
+            "export $install_env=$quoted_install_dir"
+            "export PATH=\"$bin_env:\$PATH\""
+        )
+
+        bash_configs=(
+            "$HOME/.bash_profile"
+            "$HOME/.bashrc"
+        )
+
+        if [[ ${XDG_CONFIG_HOME:-} ]]; then
+            bash_configs+=(
+                "$XDG_CONFIG_HOME/.bash_profile"
+                "$XDG_CONFIG_HOME/.bashrc"
+                "$XDG_CONFIG_HOME/bash_profile"
+                "$XDG_CONFIG_HOME/bashrc"
+            )
+        fi
+
+        set_manually=true
+        for bash_config in "${bash_configs[@]}"; do
+            tilde_bash_config=$(tildify "$bash_config")
+
+            if [[ -w $bash_config ]]; then
+                {
+                    echo -e '\n# bun'
+
+                    for command in "${commands[@]}"; do
+                        echo "$command"
+                    done
+                } >>"$bash_config"
+
+                info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_bash_config\""
+
+                refresh_command="source $bash_config"
+                set_manually=false
+                break
+            fi
         done
-    fi
-    ;;
-*)
-    echo 'Manually add the directory to ~/.bashrc (or similar):'
-    info_bold "  export $install_env=$quoted_install_dir"
-    info_bold "  export PATH=\"$bin_env:\$PATH\""
-    ;;
-esac
+
+        if [[ $set_manually = true ]]; then
+            echo "Manually add the directory to $tilde_bash_config (or similar):"
+
+            for command in "${commands[@]}"; do
+                info_bold "  $command"
+            done
+        fi
+        ;;
+    *)
+        echo 'Manually add the directory to ~/.bashrc (or similar):'
+        info_bold "  export $install_env=$quoted_install_dir"
+        info_bold "  export PATH=\"$bin_env:\$PATH\""
+        ;;
+    esac
+fi
 
 echo
 info "To get started, run:"

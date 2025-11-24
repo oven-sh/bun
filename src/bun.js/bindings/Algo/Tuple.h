9a38d9de22afd2f3e53e4e22c92180073b0fd73b
Marko Vejnovic <marko@bun.com>
2025-11-24 14:44:11 -0800

    add some concepts constraints

--- src/bun.js/bindings/Algo/Tuple.h
+++ src/bun.js/bindings/Algo/Tuple.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "wtf/StdLibExtras.h"
+#include <concepts>
+#include <tuple>
 #include <type_traits>
 #include <utility>
 
@@ -8,18 +11,27 @@ namespace Bun::Algo::Tuple {
 namespace detail {
 
 template <typename Tuple, typename Func, std::size_t... Is>
-void forEachIndexedImpl(Tuple&& t, Func&& f, std::index_sequence<Is...>) {
+    requires(
+        std::invocable<Func, std::integral_constant<std::size_t, Is>,
+        decltype(std::get<Is>(std::declval<Tuple>()))> && ...
+    )
+void forEachIndexedImpl(Tuple&& t, Func&& f, std::index_sequence<Is...>)
+{
     (f(std::integral_constant<std::size_t, Is>{}, std::get<Is>(std::forward<Tuple>(t))), ...);
 }
 
 } // namespace detail
 
-
 /// @brief Iterates over tuple elements, invoking a callback with each element and its index.
+///
+/// @see WTF::apply if you do not need the index.
+///
 /// @tparam Tuple A tuple-like type (std::tuple, std::pair, std::array)
-/// @tparam Func Callable with signature void(auto&& element, std::size_t index)
+/// @tparam Func Callable with signature `void(std::size_t index, ElementType element)`
 template <typename Tuple, typename Func>
-void forEachIndexed(Tuple&& t, Func&& f) {
+    requires(WTF::TupleLike<Tuple>)
+void forEachIndexed(Tuple&& t, Func&& f)
+{
     detail::forEachIndexedImpl(
         std::forward<Tuple>(t),
         std::forward<Func>(f),

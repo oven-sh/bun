410282cf9cb0d632e8dc4519f5ceef9e6dccc387
pfg <pfg@pfg.pw>
2025-12-01 12:39:47 -0800

    mistake

--- src/bun.js/bindings/JSMockFunction.cpp
+++ src/bun.js/bindings/JSMockFunction.cpp
@@ -1451,7 +1451,7 @@ BUN_DEFINE_HOST_FUNCTION(JSMock__jsSetSystemTime, (JSC::JSGlobalObject * globalO
         }
         return JSValue::encode(callframe->thisValue());
     }
-    // number > 0 is a valid date otherwise it's invalid and we should reset the time (set to NaN)
+    // number > 0 is a valid date otherwise it's invalid and we should reset the time (set to -1)
     globalObject->overridenDateNow = (argument0.isNumber() && argument0.asNumber() >= 0) ? argument0.asNumber() : -1;
 
     return JSValue::encode(callframe->thisValue());

29d817dcb4bcea578f855d847a5bd5615ca8b88f
Ciro Spaciari MacBook <ciro@anthropic.com>
2025-12-05 16:44:38 -0800

    test

--- src/s3/credentials.zig
+++ src/s3/credentials.zig
@@ -1095,12 +1095,12 @@ pub const S3Credentials = struct {
             result._headers_len += 1;
         }
 
-        if (content_disposition) |cd| {
-            const content_disposition_value = bun.handleOom(bun.default_allocator.dupe(u8, cd));
-            result.content_disposition = content_disposition_value;
-            result._headers[result._headers_len] = .{ .name = "Content-Disposition", .value = content_disposition_value };
-            result._headers_len += 1;
-        }
+        // if (content_disposition) |cd| {
+        //     const content_disposition_value = bun.handleOom(bun.default_allocator.dupe(u8, cd));
+        //     result.content_disposition = content_disposition_value;
+        //     result._headers[result._headers_len] = .{ .name = "Content-Disposition", .value = content_disposition_value };
+        //     result._headers_len += 1;
+        // }
 
         if (storage_class) |storage_class_value| {
             result._headers[result._headers_len] = .{ .name = "x-amz-storage-class", .value = storage_class_value };

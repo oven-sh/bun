606bf2e232571332aa7fdc4c4d30c0819f93c0ba
Sosuke Suzuki <sosuke@bun.com>
2025-12-19 15:05:52 -0800

    Remove temporaly include path fix

--- .buildkite/ci.mjs
+++ .buildkite/ci.mjs
@@ -572,8 +572,7 @@ function getTestBunStep(platform, options, testOptions = {}) {
   if (buildId) {
     args.push(`--build-id=${buildId}`);
   }
-  // TEMPORARY: Only run test/js/web/fetch tests for debugging Windows timeout issue
-  args.push(`--include=js/web/fetch`);
+
   if (testFiles) {
     args.push(...testFiles.map(testFile => `--include=${testFile}`));
   }

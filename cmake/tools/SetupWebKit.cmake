cd6e46896b64c4eef5581d6952318883afdff1b7
Sosuke Suzuki <sosuke@bun.com>
2025-12-27 00:46:37 +0900

    Update preview build

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -2,7 +2,7 @@ option(WEBKIT_VERSION "The version of WebKit to use")
 option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of downloading")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION 2317e1413969bff5e86d240f9f50aa2120776414)
+  set(WEBKIT_VERSION preview-pr-121-f24072bb)
 endif()
 
 string(SUBSTRING ${WEBKIT_VERSION} 0 16 WEBKIT_VERSION_PREFIX)

5ef60b98879e65c4d4b257d2847128c30aa6a1e2
Sosuke Suzuki <sosuke@bun.com>
2026-01-13 16:17:04 +0900

    Update WEBKIT_VERSION

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -6,7 +6,7 @@ option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of down
 option(WEBKIT_BUILD_TYPE "The build type for local WebKit (defaults to CMAKE_BUILD_TYPE)")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION preview-pr-135-aec04e25)
+  set(WEBKIT_VERSION preview-pr-135-6ba5c161)
 endif()
 
 

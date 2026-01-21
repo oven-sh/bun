e4d7631cc34dd3bea5903b13664f84defbe929a9
Jarred Sumner <jarred@jarredsumner.com>
2026-01-21 13:18:40 -0800

    Revert "Add env var"
    This reverts commit 59c3cd8eaefca6526505a2726e48e7d1a12c5924.

--- src/bun.js.zig
+++ src/bun.js.zig
@@ -277,13 +277,13 @@ pub const Run = struct {
         vm.onUnhandledRejection = &onUnhandledRejectionBeforeClose;
 
         // Start CPU profiler if enabled
-        if (this.ctx.runtime_options.cpu_prof.enabled or bun.env_var.BUN_CPU_PROFILE.get() or bun.env_var.BUN_CPU_PROFILE_MD.get()) {
+        if (this.ctx.runtime_options.cpu_prof.enabled or bun.env_var.BUN_CPU_PROFILE.get()) {
             const cpu_prof_opts = this.ctx.runtime_options.cpu_prof;
 
             vm.cpu_profiler_config = CPUProfiler.CPUProfilerConfig{
                 .name = cpu_prof_opts.name orelse bun.env_var.BUN_CPU_PROFILE_NAME.get() orelse "",
                 .dir = cpu_prof_opts.dir orelse bun.env_var.BUN_CPU_PROFILE_DIR.get() orelse "",
-                .md_format = cpu_prof_opts.md_format or bun.env_var.BUN_CPU_PROFILE_MD.get(),
+                .md_format = cpu_prof_opts.md_format,
                 .json_format = cpu_prof_opts.json_format,
             };
             CPUProfiler.startCPUProfiler(vm.jsc_vm);

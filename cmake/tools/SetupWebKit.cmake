27cdb4ed0c3d3c06eff0c2ff6a26ea9668226f5a
Dylan Conway <dylan.conway567@gmail.com>
2026-01-21 11:46:51 -0800

    update

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -2,7 +2,7 @@ option(WEBKIT_VERSION "The version of WebKit to use")
 option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of downloading")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION autobuild-preview-pr-141-481ba51a)
+  set(WEBKIT_VERSION autobuild-preview-pr-141-9899e383)
 endif()
 
 string(SUBSTRING ${WEBKIT_VERSION} 0 16 WEBKIT_VERSION_PREFIX)

46a60fdc3aea26ddf29d264e8d3824cfb1450523
Dylan Conway <dylan.conway567@gmail.com>
2026-01-29 19:20:14 -0800

    update

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -2,7 +2,7 @@ option(WEBKIT_VERSION "The version of WebKit to use")
 option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of downloading")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION 515344bc5d65aa2d4f9ff277b5fb944f0e051dcd)
+  set(WEBKIT_VERSION autobuild-preview-pr-151-8fb3f004)
 endif()
 
 # Use preview build URL for Windows ARM64 until the fix is merged to main

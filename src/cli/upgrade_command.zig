f6f4dc18d8135a5114b0bedda766f3f7575bfacf
Echo <github@3kh0.net>
2026-02-10 16:39:45 -0500

    Update installation URLs for Bun

--- src/cli/upgrade_command.zig
+++ src/cli/upgrade_command.zig
@@ -310,7 +310,7 @@ pub const UpgradeCommand = struct {
     const profile_exe_subpath = Version.profile_folder_name ++ std.fs.path.sep_str ++ "bun-profile" ++ exe_suffix;
 
     const manual_upgrade_command = switch (Environment.os) {
-        .linux, .mac => "curl -fsSL https://bun.com/install | bash",
+        .linux, .mac => "curl -fsSL https://bun.sh/install | bash",
         .windows => "powershell -c 'irm bun.sh/install.ps1|iex'",
         else => "(TODO: Install script for " ++ Environment.os.displayString() ++ ")",
     };
@@ -862,7 +862,7 @@ pub const UpgradeCommand = struct {
                     \\
                     \\What's new in Bun v{s}:
                     \\
-                    \\    <cyan>https://bun.com/blog/release-notes/{s}<r>
+                    \\    <cyan>https://bun.sh/blog/release-notes/{s}<r>
                     \\
                     \\Report any bugs:
                     \\

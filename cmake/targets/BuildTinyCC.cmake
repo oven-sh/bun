4272a59f8912e2a84b5b99144f12509b9669a9bc
Dylan Conway <dylan.conway567@gmail.com>
2026-02-21 00:20:17 -0800

    suppress vendored library warnings on Windows (clang-cl)
    - lshpack, mimalloc, tinycc: pass -w to suppress all C/C++ warnings
    - mimalloc: skip -moutline-atomics on Windows (unsupported by clang-cl)
    - real_v8.h: suppress -Wmicrosoft-include diagnostic

--- cmake/targets/BuildTinyCC.cmake
+++ cmake/targets/BuildTinyCC.cmake
@@ -7,9 +7,16 @@ register_repository(
     12882eee073cfe5c7621bcfadf679e1372d4537b
 )
 
+# Suppress all warnings from vendored tinycc on Windows (clang-cl)
+if(WIN32)
+  set(TINYCC_CMAKE_ARGS "-DCMAKE_C_FLAGS=-w")
+endif()
+
 register_cmake_command(
   TARGET
     tinycc
+  ARGS
+    ${TINYCC_CMAKE_ARGS}
   LIBRARIES
     tcc
 )

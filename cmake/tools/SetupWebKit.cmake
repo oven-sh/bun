90e7814cdcff190e2704447c4c2c49fb4962ae2c
Dylan Conway <dylan.conway567@gmail.com>
2026-03-04 20:06:44 -0800

    Revert "TEST: WebKit pr-171 preview (do not merge)"
    This reverts commit cb5f783789bb0891c1238405f4a0ff1937fee8dc.

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -6,7 +6,7 @@ option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of down
 option(WEBKIT_BUILD_TYPE "The build type for local WebKit (defaults to CMAKE_BUILD_TYPE)")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION autobuild-preview-pr-171-164c1fb9)
+  set(WEBKIT_VERSION 4a6a32c32c11ffb9f5a94c310b10f50130bfe6de)
 endif()
 
 

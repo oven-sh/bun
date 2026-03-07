bb149a3adc806061969ecafe7342fd83eaa0c618
Dylan Conway <dylan.conway567@gmail.com>
2026-03-06 21:51:11 -0800

    update webkit

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -6,7 +6,7 @@ option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of down
 option(WEBKIT_BUILD_TYPE "The build type for local WebKit (defaults to CMAKE_BUILD_TYPE)")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION 7a6bba5ae4a2b04d845a6fbafba9805658467ba2)
+  set(WEBKIT_VERSION 84648875d78fd31931ba71f7df4d943c90769ffd)
 endif()
 
 

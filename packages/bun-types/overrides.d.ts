ba7956cbc76457c1f2ad126b141740bf5f4fa24e
ssing2 <ssing2011@126.com>
2026-03-10 05:59:10 +0800

    feat(types): add NodeJS signals types

--- packages/bun-types/overrides.d.ts
+++ packages/bun-types/overrides.d.ts
@@ -13026,3 +13026,10 @@ declare global {
         const arch: string;
     }
 }
+
+// Signals types
+declare global {
+    namespace NodeJS {
+        type Signals = "SIGABRT" | "SIGALRM" | "SIGBUS" | "SIGCHLD" | "SIGCONT" | "SIGFPE" | "SIGHUP" | "SIGILL" | "SIGINT" | "SIGIO" | "SIGIOT" | "SIGKILL" | "SIGPIPE" | "SIGPOLL" | "SIGPROF" | "SIGPWR" | "SIGQUIT" | "SIGSEGV" | "SIGSTKFLT" | "SIGSTOP" | "SIGSYS" | "SIGTERM" | "SIGTRAP" | "SIGTSTP" | "SIGTTIN" | "SIGTTOU" | "SIGURG" | "SIGUSR1" | "SIGUSR2" | "SIGVTALRM" | "SIGWINCH" | "SIGXCPU" | "SIGXFSZ" | "SIGBREAK" | "SIGLOST" | "SIGINFO";
+    }
+}

889a6e4613de44629940028a5856ba316347a1ec
ssing2 <ssing2011@126.com>
2026-03-10 19:50:53 +0800

    feat(types): add Date additional types

--- packages/bun-types/overrides.d.ts
+++ packages/bun-types/overrides.d.ts
@@ -15355,3 +15355,52 @@ declare global {
     values(o: object): any[];
   }
 }
+
+// Date additional types
+declare global {
+  interface Date {
+    toString(): string;
+    toDateString(): string;
+    toTimeString(): string;
+    toLocaleString(locales?: string | string[], options?: Intl.DateTimeFormatOptions): string;
+    toLocaleDateString(locales?: string | string[], options?: Intl.DateTimeFormatOptions): string;
+    toLocaleTimeString(locales?: string | string[], options?: Intl.DateTimeFormatOptions): string;
+    valueOf(): number;
+    getTime(): number;
+    getFullYear(): number;
+    getUTCFullYear(): number;
+    getMonth(): number;
+    getUTCMonth(): number;
+    getDate(): number;
+    getUTCDate(): number;
+    getDay(): number;
+    getUTCDay(): number;
+    getHours(): number;
+    getUTCHours(): number;
+    getMinutes(): number;
+    getUTCMinutes(): number;
+    getSeconds(): number;
+    getUTCSeconds(): number;
+    getMilliseconds(): number;
+    getUTCMilliseconds(): number;
+    getTimezoneOffset(): number;
+    setTime(time: number): number;
+    setMilliseconds(ms: number): number;
+    setUTCMilliseconds(ms: number): number;
+    setSeconds(sec: number, ms?: number): number;
+    setUTCSeconds(sec: number, ms?: number): number;
+    setMinutes(min: number, sec?: number, ms?: number): number;
+    setUTCMinutes(min: number, sec?: number, ms?: number): number;
+    setHours(hours: number, min?: number, sec?: number, ms?: number): number;
+    setUTCHours(hours: number, min?: number, sec?: number, ms?: number): number;
+    setDate(date: number): number;
+    setUTCDate(date: number): number;
+    setMonth(month: number, date?: number): number;
+    setUTCMonth(month: number, date?: number): number;
+    setFullYear(year: number, month?: number, date?: number): number;
+    setUTCFullYear(year: number, month?: number, date?: number): number;
+    toUTCString(): string;
+    toISOString(): string;
+    toJSON(key?: string): string;
+  }
+}

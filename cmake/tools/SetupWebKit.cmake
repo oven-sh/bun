14276e77e53f11b70cb7708559951a4d1e405daa
Sosuke Suzuki <sosuke@bun.com>
2026-03-21 19:13:45 +0900

    Bump WebKit to fc9f2fa7272fec64905df6a9c78e15d7912f14ca

--- cmake/tools/SetupWebKit.cmake
+++ cmake/tools/SetupWebKit.cmake
@@ -6,7 +6,7 @@ option(WEBKIT_LOCAL "If a local version of WebKit should be used instead of down
 option(WEBKIT_BUILD_TYPE "The build type for local WebKit (defaults to CMAKE_BUILD_TYPE)")
 
 if(NOT WEBKIT_VERSION)
-  set(WEBKIT_VERSION de176b300e6545e3dbb2049e38ef1e22d943b3e0)
+  set(WEBKIT_VERSION fc9f2fa7272fec64905df6a9c78e15d7912f14ca)
 endif()
 
 

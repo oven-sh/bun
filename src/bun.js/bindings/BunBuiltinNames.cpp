c88a773bd5fbcc69a1f43230c33dbdb5df6e9ec0
Jarred Sumner <jarred@jarredsumner.com>
2026-04-21 01:54:07 -0700

    Add root.h include + optnone to BunBuiltinNames ctor
    With the ctor as the only function in its TU, ModuleInlinerWrapperPass /
    DevirtSCCRepeatedPass spent ~90s repeatedly inlining and re-devirtualizing
    the ~400 Identifier::fromString / appendExternalName calls (vs ~5s when the
    same body was one of many functions in BunClientData.cpp). The ctor runs
    once at VM init, so optnone has no meaningful runtime cost. Also include
    root.h first so the file participates in the PCH.

--- src/bun.js/bindings/BunBuiltinNames.cpp
+++ src/bun.js/bindings/BunBuiltinNames.cpp
@@ -1,10 +1,14 @@
+#include "root.h"
 #include "BunBuiltinNames.h"
 
 namespace WebCore {
 
 // FIXME: Remove the __attribute__((nodebug)) when <rdar://68246686> is fixed.
+// optnone: with the ctor as the only function in this TU, ModuleInlinerWrapperPass
+// otherwise spends ~90s repeatedly devirtualizing/inlining the ~400 Identifier
+// calls. The ctor runs once at VM init.
 #if COMPILER(CLANG)
-__attribute__((nodebug))
+__attribute__((nodebug, optnone))
 #endif
 BunBuiltinNames::BunBuiltinNames(JSC::VM& vm)
     : m_vm(vm)

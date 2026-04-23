3f695f435b0aeea340ad57fbef51e423b04401e9
Jarred Sumner <jarred@jarredsumner.com>
2026-04-23 14:39:56 -0700

    Deflake test/js/bun/resolve/load-file-loader-a-lot.test.ts

--- test/js/bun/resolve/load-file-loader-a-lot.test.ts
+++ test/js/bun/resolve/load-file-loader-a-lot.test.ts
@@ -1,4 +1,5 @@
 import { test } from "bun:test";
+import { isASAN } from "harness";
 
 test("a file: loader file can be imported 10,000 times", async () => {
   const prev = Bun.unsafe.gcAggressionLevel();
@@ -11,7 +12,7 @@ test("a file: loader file can be imported 10,000 times", async () => {
   }
   baseline = process.memoryUsage.rss();
   Bun.gc(true);
-  for (let i = 0; i < 100_000; i++) {
+  for (let i = 0; i < (isASAN ? 25_000 : 100_000); i++) {
     await import("./an-empty-file-with-a-strange-extension.weird?i" + i);
     Loader.registry.clear();
   }

e0716f1a5a1eba8dbfa4c3357f3fba90f471f5ff
Jarred Sumner <jarred@jarredsumner.com>
2026-05-07 23:42:34 +0000

    cli: fallback tag_params returns base+runtime+transpiler set

--- src/runtime/cli/mod.rs
+++ src/runtime/cli/mod.rs
@@ -1307,7 +1307,7 @@ To create a project with the official Next.js scaffolding tool, run\n\
             Tag::BuildCommand => arguments::BUILD_PARAMS.as_slice(),
             Tag::TestCommand => arguments::TEST_PARAMS.as_slice(),
             Tag::BunxCommand => arguments::RUN_PARAMS.as_slice(),
-            _ => arguments::BASE_PARAMS_.as_slice(),
+            _ => arguments::BASE_RUNTIME_TRANSPILER_PARAMS.as_slice(),
         }
     }
 

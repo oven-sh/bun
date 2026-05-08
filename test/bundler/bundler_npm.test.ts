cdcc1ad9508a379401ca7457ed42161903ff8251
Jarred Sumner <jarred@jarredsumner.com>
2026-05-08 05:39:56 +0000

    test/bundler_npm: update minified-name snapshot for accumulating CharFreq

--- test/bundler/bundler_npm.test.ts
+++ test/bundler/bundler_npm.test.ts
@@ -57,8 +57,8 @@ describe("bundler", () => {
           "../entry.tsx",
         ],
         mappings: [
-          ["react.development.js:524:'getContextName'", "1:5567:Y1"],
-          ["react.development.js:2495:'actScopeDepth'", "23:4082:GJ++"],
+          ["react.development.js:524:'getContextName'", "1:5567:nt"],
+          ["react.development.js:2495:'actScopeDepth'", "23:4082:ar++"],
           ["react.development.js:696:''Component'", '1:7629:\'Component "%s"'],
           ["entry.tsx:6:'\"Content-Type\"'", '100:18808:"Content-Type"'],
           ["entry.tsx:11:'<html>'", "100:19062:void"],

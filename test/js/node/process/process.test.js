b1746f0bc04802b98cc452e9361e5c9ba2d4cc2c
Jarred Sumner <jarred@jarredsumner.com>
2026-07-13 06:32:37 -0700

    test: match the mimalloc sha in process.versions to the bumped pin

--- test/js/node/process/process.test.js
+++ test/js/node/process/process.test.js
@@ -333,7 +333,7 @@ it("process.versions", () => {
   const expectedVersions = {
     boringssl: "1a41b9025c2c0a37edd07ff10f6944f03e028522",
     libarchive: "ded82291ab41d5e355831b96b0e1ff49e24d8939",
-    mimalloc: "c41edb7dc8c01d69316258827235dc594193a4e5",
+    mimalloc: "3e70ad5cdbd82c8cab3e3c39a9af842da16d0ffc",
     picohttpparser: "066d2b1e9ab820703db0837a7255d92d30f0c9f5",
     zlib: "12731092979c6d07f42da27da673a9f6c7b13586",
     tinycc: "12882eee073cfe5c7621bcfadf679e1372d4537b",

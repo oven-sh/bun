a81c157b8e6c8dd4a6ad11b0b5e5559711eca9ae
robobun <117481402+robobun@users.noreply.github.com>
2026-07-18 10:07:38 +0000

    Drop self-as-nativeConstructor in putNativeFn and utf-8-validate

--- src/jsc/modules/UTF8ValidateModule.h
+++ src/jsc/modules/UTF8ValidateModule.h
@@ -14,8 +14,7 @@ generateNativeModule_UTF8Validate(JSC::JSGlobalObject* globalObject,
     exportNames.append(vm.propertyNames->defaultKeyword);
     exportValues.append(JSC::JSFunction::create(
         vm, globalObject, 1, "utf8Validate"_s, jsBufferConstructorFunction_isUtf8,
-        ImplementationVisibility::Public, NoIntrinsic,
-        jsBufferConstructorFunction_isUtf8));
+        ImplementationVisibility::Public, NoIntrinsic));
 }
 
 } // namespace Zig

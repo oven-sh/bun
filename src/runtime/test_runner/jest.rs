12a67b7f94d1a12497860c44c6f248c71bf8c23c
Unmesh100 <bubunghosh1214@gmail.com>
2026-07-19 20:13:58 +0530

    fix(test): preserve distinct colliding file paths

--- src/runtime/test_runner/jest.rs
+++ src/runtime/test_runner/jest.rs
@@ -241,10 +241,9 @@ impl<'a> TestRunner<'a> {
     }
 
     pub fn get_or_put_file(&mut self, file_path: &'static [u8]) -> GetOrPutFileResult {
-        // TODO: this is wrong. you can't put a hash as the key in a hashmap.
         let entry = self
             .index
-            .get_or_put(bun_wyhash::hash(file_path) as u32)
+            .get_or_put(file_path)
             .expect("unreachable");
         if entry.found_existing {
             return GetOrPutFileResult {
@@ -312,8 +311,7 @@ bun_collections::multi_array_columns! {
         log: bun_ast::Log,
     }
 }
-// u32 keys hash as identity in bun_collections.
-pub(crate) type FileMap = ArrayHashMap<u32, u32>;
+pub(crate) type FileMap = bun_collections::StringArrayHashMap<FileId>;
 
 #[allow(non_snake_case)]
 pub mod Jest {

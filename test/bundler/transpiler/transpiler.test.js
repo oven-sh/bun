c9e59569c6fc862bc48494c5acc135967999b2db
robobun <117481402+robobun@users.noreply.github.com>
2026-07-25 09:37:59 +0000

    Clarify autoImportJSX JSDoc; make no-arg test NODE_ENV-agnostic

--- test/bundler/transpiler/transpiler.test.js
+++ test/bundler/transpiler/transpiler.test.js
@@ -2198,7 +2198,7 @@ console.log(<div {...obj} key="after" />);`),
   describe("autoImportJSX defaults to true for the automatic runtime", () => {
     it("no-arg constructor", () => {
       expect(new Bun.Transpiler().transformSync("export default <div/>;")).toMatch(
-        /^import { jsxDEV as (\w+) } from "react\/jsx-dev-runtime";\nexport default \1\("div",/,
+        /^import \{ jsx(?:DEV)? as (\w+) \} from "react\/jsx-(?:dev-)?runtime";\nexport default \1\("div",/,
       );
     });
 

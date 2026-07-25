e085e6b398876c8759bb1b9041454b34e98c6175
robobun <117481402+robobun@users.noreply.github.com>
2026-07-25 21:02:22 +0000

    process.getgroups: check the second getgroups(2) return value
    _FORTIFY_SOURCE gives getgroups() __attribute__((warn_unused_result)),
    which under -Werror breaks the build on the fill call. Checking it is
    also the correct behaviour: if the supplementary group set changes
    between the size probe and the fill, the old code would read
    uninitialized or stale gid_t entries. Use the actual count returned
    by the second call and size the result array from it.

--- src/jsc/bindings/BunProcess.cpp
+++ src/jsc/bindings/BunProcess.cpp
@@ -3057,16 +3057,20 @@ JSC_DEFINE_HOST_FUNCTION(Process_functiongetgid, (JSGlobalObject * globalObject,
 JSC_DEFINE_HOST_FUNCTION(Process_functiongetgroups, (JSGlobalObject * globalObject, CallFrame* callFrame))
 {
     auto& vm = JSC::getVM(globalObject);
-    int ngroups = getgroups(0, nullptr);
     auto throwScope = DECLARE_THROW_SCOPE(vm);
+    int ngroups = getgroups(0, nullptr);
+    if (ngroups == -1) {
+        throwSystemError(throwScope, globalObject, "getgroups"_s, errno);
+        return {};
+    }
+    Vector<gid_t> groupVector(ngroups);
+    ngroups = getgroups(ngroups, groupVector.begin());
     if (ngroups == -1) {
         throwSystemError(throwScope, globalObject, "getgroups"_s, errno);
         return {};
     }
     JSArray* groups = constructEmptyArray(globalObject, nullptr, ngroups);
     RETURN_IF_EXCEPTION(throwScope, {});
-    Vector<gid_t> groupVector(ngroups);
-    getgroups(ngroups, groupVector.begin());
     for (unsigned i = 0; i < ngroups; i++) {
         groups->putDirectIndex(globalObject, i, jsNumber(groupVector[i]));
     }

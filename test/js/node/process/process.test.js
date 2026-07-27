92895f5e95ef3a8a771e8d26cd11301e388aa17f
robobun <117481402+robobun@users.noreply.github.com>
2026-07-27 21:08:45 +0000

    test: update expected lolhtml commit for v3.0.0

--- test/js/node/process/process.test.js
+++ test/js/node/process/process.test.js
@@ -337,7 +337,7 @@ it("process.versions", () => {
     picohttpparser: "066d2b1e9ab820703db0837a7255d92d30f0c9f5",
     zlib: "12731092979c6d07f42da27da673a9f6c7b13586",
     tinycc: "05f0fafaa3be31e31d7b4b5c17dc60f62c991171",
-    lolhtml: "77127cd2b8545998756e8d64e36ee2313c4bb312",
+    lolhtml: "02f139c4437b2da666a50d32e11d9158cbe0a393",
     ares: "3ac47ee46edd8ea40370222f91613fc16c434853",
     libdeflate: "c8c56a20f8f621e6a966b716b31f1dedab6a41e3",
     zstd: "f8745da6ff1ad1e7bab384bd1f9d742439278e99",

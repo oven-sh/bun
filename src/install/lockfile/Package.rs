46339b05e5605830a05f80658a92deaffb953d97
robobun <117481402+robobun@users.noreply.github.com>
2026-07-29 01:05:37 +0000

    drop explanatory comment flagged by comment-cop

--- src/install/lockfile/Package.rs
+++ src/install/lockfile/Package.rs
@@ -1296,8 +1296,6 @@ impl Diff {
                 for (key, to_version) in to_lockfile.workspace_versions.iter() {
                     match from_lockfile.workspace_versions.get(key) {
                         Some(from_version) => {
-                            // `Version::eql` is precedence equality (ignores build metadata);
-                            // compare build.hash too so `1.0.0+a` -> `1.0.0+b` is a diff.
                             if !from_version.eql(*to_version)
                                 || from_version.tag.build.hash != to_version.tag.build.hash
                             {

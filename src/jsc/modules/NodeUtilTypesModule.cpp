12caa5af12d3e1fbcf7bab3ddd681e2a5cf4ac01
robobun <117481402+robobun@users.noreply.github.com>
2026-08-11 20:29:53 +0000

    Shorten the inlines include comment

--- src/jsc/modules/NodeUtilTypesModule.cpp
+++ src/jsc/modules/NodeUtilTypesModule.cpp
@@ -12,9 +12,7 @@
 #include <JavaScriptCore/RegExpObject.h>
 #include <JavaScriptCore/JSSetIterator.h>
 #include <JavaScriptCore/ObjectPrototype.h>
-// objectPrototypeToString is ALWAYS_INLINE in ObjectPrototypeInlines.h; release
-// WebKit archives no longer carry an out-of-line copy to link against.
-#include <JavaScriptCore/ObjectPrototypeInlines.h>
+#include <JavaScriptCore/ObjectPrototypeInlines.h> // objectPrototypeToString is ALWAYS_INLINE, no out-of-line copy to link
 #include <cmath>
 #include "JSEventTarget.h"
 #include "JavaScriptCore/TopExceptionScope.h"

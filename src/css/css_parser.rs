ffe2806463656750db8c63861e1b576478c44b1d
robobun <117481402+robobun@users.noreply.github.com>
2026-08-14 22:17:22 +0000

    css: leave the expect_no_error_token doc comment as it was

--- src/css/css_parser.rs
+++ src/css/css_parser.rs
@@ -3298,8 +3298,7 @@ impl<'a> Parser<'a> {
     }
 
     /// Parse the input until exhaustion and check that it contains no "error"
-    /// token. See `Token::is_parse_error`. This also checks nested blocks and
-    /// functions recursively.
+    /// token. See `Token::is_parse_error`.
     pub(crate) fn expect_no_error_token(&mut self) -> CssResult<()> {
         loop {
             let tok = match self.next_including_whitespace_and_comments() {

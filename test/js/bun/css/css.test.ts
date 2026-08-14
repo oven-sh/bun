4452e169f226d73423aed46acbe0260b60600dc4
robobun <117481402+robobun@users.noreply.github.com>
2026-08-14 19:59:18 +0000

    test(css): put the supports block after the media block

--- test/js/bun/css/css.test.ts
+++ test/js/bun/css/css.test.ts
@@ -6451,6 +6451,61 @@ describe("css tests", () => {
     error_test("@media (prefers-color-scheme = dark) { .foo { color: chartreuse }}", "ParserError::InvalidMediaQuery");
   });
 
+  describe("supports", () => {
+    // Whatever precedes a `not` condition (`@supports `, a grouping `(`, or
+    // `supports(` in @import) already writes its own separator, so `not` must
+    // not be printed with a leading space of its own.
+    minify_test("@supports not (display:grid) { a { color: red } }", "@supports not (display:grid){a{color:red}}");
+    minify_test(
+      "@supports not selector(:focus-visible) { a { color: red } }",
+      "@supports not selector(:focus-visible){a{color:red}}",
+    );
+    minify_test(
+      "@supports not ((display:grid) and (display:flex)) { a { color: red } }",
+      "@supports not ((display:grid) and (display:flex)){a{color:red}}",
+    );
+    minify_test(
+      "@supports not ((display:grid) or (display:flex)) { a { color: red } }",
+      "@supports not ((display:grid) or (display:flex)){a{color:red}}",
+    );
+    minify_test(
+      "@supports not (not (display:grid)) { a { color: red } }",
+      "@supports not (not (display:grid)){a{color:red}}",
+    );
+    minify_test(
+      "@supports (display:grid) and (not (display:inline-grid)) { a { color: red } }",
+      "@supports (display:grid) and (not (display:inline-grid)){a{color:red}}",
+    );
+    minify_test(
+      "@supports (not (display:grid)) and (display:flex) { a { color: red } }",
+      "@supports (not (display:grid)) and (display:flex){a{color:red}}",
+    );
+    minify_test(
+      "@supports (display:grid) or (not (display:inline-grid)) { a { color: red } }",
+      "@supports (display:grid) or (not (display:inline-grid)){a{color:red}}",
+    );
+    minify_test(
+      "@import url(foo.css) supports(not (display:grid));",
+      '@import "foo.css" supports(not (display:grid));',
+    );
+    minify_test(
+      "@import url(foo.css) supports((display:grid) and (not (display:flex)));",
+      '@import "foo.css" supports((display:grid) and (not (display:flex)));',
+    );
+    minify_test(
+      "@import url(foo.css) supports(not (display:grid)) screen;",
+      '@import "foo.css" supports(not (display:grid)) screen;',
+    );
+
+    // The minified forms parse back to themselves.
+    minify_test("@supports not (display:grid){a{color:red}}", "@supports not (display:grid){a{color:red}}");
+    minify_test(
+      "@supports (display:grid) and (not (display:inline-grid)){a{color:red}}",
+      "@supports (display:grid) and (not (display:inline-grid)){a{color:red}}",
+    );
+    minify_test('@import "foo.css" supports(not (display:grid));', '@import "foo.css" supports(not (display:grid));');
+  });
+
   describe("transition", () => {
     minify_test(".foo { transition-duration: 500ms }", ".foo{transition-duration:.5s}");
     minify_test(".foo { transition-duration: .5s }", ".foo{transition-duration:.5s}");
@@ -7557,61 +7612,6 @@ describe("css tests", () => {
     minify_test("@page toc,index{margin:1em}", "@page toc,index{margin:1em}");
     minify_test("@page CompanyLetterHead:first{margin:1em}", "@page CompanyLetterHead:first{margin:1em}");
     minify_test("@page \\31 st{margin:1em}", "@page \\31 st{margin:1em}");
-  });
-
-  describe("supports", () => {
-    // Whatever precedes a `not` condition (`@supports `, a grouping `(`, or
-    // `supports(` in @import) already writes its own separator, so `not` must
-    // not be printed with a leading space of its own.
-    minify_test("@supports not (display:grid) { a { color: red } }", "@supports not (display:grid){a{color:red}}");
-    minify_test(
-      "@supports not selector(:focus-visible) { a { color: red } }",
-      "@supports not selector(:focus-visible){a{color:red}}",
-    );
-    minify_test(
-      "@supports not ((display:grid) and (display:flex)) { a { color: red } }",
-      "@supports not ((display:grid) and (display:flex)){a{color:red}}",
-    );
-    minify_test(
-      "@supports not ((display:grid) or (display:flex)) { a { color: red } }",
-      "@supports not ((display:grid) or (display:flex)){a{color:red}}",
-    );
-    minify_test(
-      "@supports not (not (display:grid)) { a { color: red } }",
-      "@supports not (not (display:grid)){a{color:red}}",
-    );
-    minify_test(
-      "@supports (display:grid) and (not (display:inline-grid)) { a { color: red } }",
-      "@supports (display:grid) and (not (display:inline-grid)){a{color:red}}",
-    );
-    minify_test(
-      "@supports (not (display:grid)) and (display:flex) { a { color: red } }",
-      "@supports (not (display:grid)) and (display:flex){a{color:red}}",
-    );
-    minify_test(
-      "@supports (display:grid) or (not (display:inline-grid)) { a { color: red } }",
-      "@supports (display:grid) or (not (display:inline-grid)){a{color:red}}",
-    );
-    minify_test(
-      "@import url(foo.css) supports(not (display:grid));",
-      '@import "foo.css" supports(not (display:grid));',
-    );
-    minify_test(
-      "@import url(foo.css) supports((display:grid) and (not (display:flex)));",
-      '@import "foo.css" supports((display:grid) and (not (display:flex)));',
-    );
-    minify_test(
-      "@import url(foo.css) supports(not (display:grid)) screen;",
-      '@import "foo.css" supports(not (display:grid)) screen;',
-    );
-
-    // The minified forms parse back to themselves.
-    minify_test("@supports not (display:grid){a{color:red}}", "@supports not (display:grid){a{color:red}}");
-    minify_test(
-      "@supports (display:grid) and (not (display:inline-grid)){a{color:red}}",
-      "@supports (display:grid) and (not (display:inline-grid)){a{color:red}}",
-    );
-    minify_test('@import "foo.css" supports(not (display:grid));', '@import "foo.css" supports(not (display:grid));');
   });
 
   describe("container", () => {

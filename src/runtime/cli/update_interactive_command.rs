74237574d31fe18a3ca04d477a705e4892742c9d
Alistair Smith <hi@alistair.sh>
2026-08-14 19:56:20 -0700

    update: the interactive checkbox colour is an enum, not a string compared by eye

--- src/runtime/cli/update_interactive_command.rs
+++ src/runtime/cli/update_interactive_command.rs
@@ -120,6 +120,14 @@ struct CatalogUpdateRequest {
     catalog_name: Option<Box<[u8]>>,
 }
 
+/// How risky an update looks from its semver distance.
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum CheckboxColor {
+    Green,
+    Yellow,
+    Red,
+}
+
 struct ColumnWidths {
     name: usize,
     current: usize,
@@ -1559,7 +1567,7 @@ impl UpdateInteractiveCommand {
                         ))
                     };
 
-                    let mut checkbox_color: &str = "green"; // default
+                    let mut checkbox_color = CheckboxColor::Green;
                     if current_ver_parsed.valid && update_ver_parsed.valid {
                         let current_full = semver::Version {
                             major: current_ver_parsed.version.major.unwrap_or(0),
@@ -1589,22 +1597,24 @@ impl UpdateInteractiveCommand {
                         );
                         if let Some(d) = diff {
                             match d {
-                                semver::version::ChangedVersion::Major => checkbox_color = "red",
+                                semver::version::ChangedVersion::Major => {
+                                    checkbox_color = CheckboxColor::Red
+                                }
                                 semver::version::ChangedVersion::Minor => {
                                     if current_full.major == 0 {
-                                        checkbox_color = "red"; // 0.x.y minor changes are breaking
+                                        checkbox_color = CheckboxColor::Red; // 0.x.y minor changes are breaking
                                     } else {
-                                        checkbox_color = "yellow";
+                                        checkbox_color = CheckboxColor::Yellow;
                                     }
                                 }
                                 semver::version::ChangedVersion::Patch => {
                                     if current_full.major == 0 && current_full.minor == 0 {
-                                        checkbox_color = "red"; // 0.0.x patch changes are breaking
+                                        checkbox_color = CheckboxColor::Red; // 0.0.x patch changes are breaking
                                     } else {
-                                        checkbox_color = "green";
+                                        checkbox_color = CheckboxColor::Green;
                                     }
                                 }
-                                _ => checkbox_color = "green",
+                                _ => checkbox_color = CheckboxColor::Green,
                             }
                         }
                     }
@@ -1618,9 +1628,9 @@ impl UpdateInteractiveCommand {
 
                     // Checkbox with appropriate color
                     if selected {
-                        if checkbox_color == "red" {
+                        if checkbox_color == CheckboxColor::Red {
                             bun_core::pretty!("<r><red>{}<r> ", checkbox);
-                        } else if checkbox_color == "yellow" {
+                        } else if checkbox_color == CheckboxColor::Yellow {
                             bun_core::pretty!("<r><yellow>{}<r> ", checkbox);
                         } else {
                             bun_core::pretty!("<r><green>{}<r> ", checkbox);
@@ -1672,9 +1682,9 @@ impl UpdateInteractiveCommand {
                     );
 
                     if selected {
-                        if checkbox_color == "red" {
+                        if checkbox_color == CheckboxColor::Red {
                             bun_core::pretty!("<r><red>{}<r>", hyperlink);
-                        } else if checkbox_color == "yellow" {
+                        } else if checkbox_color == CheckboxColor::Yellow {
                             bun_core::pretty!("<r><yellow>{}<r>", hyperlink);
                         } else {
                             bun_core::pretty!("<r><green>{}<r>", hyperlink);

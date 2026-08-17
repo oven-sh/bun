luci-digital/luci-bun

napi: propagate Proxy trap exceptions from napi_get_all_property_names descriptor filter
When napi_get_all_property_names is called with napi_key_enumerable,
napi_key_writable, or napi_key_configurable, it iterates the collected
keys and calls getOwnPropertyDescriptor/getPrototype on the target to
filter by descriptor attributes. If the target is a Proxy whose
getOwnPropertyDescriptor or getPrototypeOf trap throws, the loop kept
iterating with a pending exception and then NAPI_RETURN_SUCCESS would
assert (debug) or return napi_ok with the exception still pending
(release). In release builds the next iteration would dereference an
empty JSValue from exportKeys->get and segfault.

Add NAPI_RETURN_IF_EXCEPTION after every call in the filter loop that
can run user JS (get, toPropertyKey, getOwnPropertyDescriptor,
getPrototype, push) and restructure the prototype walk so the
exception check runs regardless of getOwnPropertyDescriptor's return
value.

src/jsc/bindings/napi.cpp
@@ -1837,20 +1837,31 @@ extern "C" napi_status napi_get_all_property_names(
         JSArray* filteredKeys = JSArray::create(JSC::getVM(globalObject), globalObject->originalArrayStructureForIndexingType(ArrayWithContiguous), 0);
         for (unsigned i = 0; i < exportKeys->getArrayLength(); i++) {
             JSValue key = exportKeys->get(globalObject, i);
+            NAPI_RETURN_IF_EXCEPTION(env);
+            auto propertyKey = key.toPropertyKey(globalObject);
+            NAPI_RETURN_IF_EXCEPTION(env);
             PropertyDescriptor desc;
 
             if (key_mode == napi_key_include_prototypes) {
                 // Climb up the prototype chain to find inherited properties
                 JSObject* current_object = object;
-                while (!current_object->getOwnPropertyDescriptor(globalObject, key.toPropertyKey(globalObject), desc)) {
-                    JSObject* proto = current_object->getPrototype(globalObject).getObject();
+                while (true) {
+                    bool found = current_object->getOwnPropertyDescriptor(globalObject, propertyKey, desc);
+                    NAPI_RETURN_IF_EXCEPTION(env);
+                    if (found) {
+                        break;
+                    }
+                    JSValue protoValue = current_object->getPrototype(globalObject);
+                    NAPI_RETURN_IF_EXCEPTION(env);
+                    JSObject* proto = protoValue.getObject();
                     if (!proto) {
                         break;
                     }
                     current_object = proto;
                 }
             } else {
-                object->getOwnPropertyDescriptor(globalObject, key.toPropertyKey(globalObject), desc);
+                object->getOwnPropertyDescriptor(globalObject, propertyKey, desc);
+                NAPI_RETURN_IF_EXCEPTION(env);
             }
 
             bool include = true;
@@ -1866,6 +1877,7 @@ extern "C" napi_status napi_get_all_property_names(
 
             if (include) {
                 filteredKeys->push(globalObject, key);
+                NAPI_RETURN_IF_EXCEPTION(env);
             }
         }
         exportKeys = filteredKeys;

oven-sh/bun

src/jsc/bindings/napi.cpp
@@ -2070,14 +2070,23 @@ extern "C" napi_status napi_get_all_property_names(
         JSArray* filteredKeys = JSArray::create(JSC::getVM(globalObject), globalObject->originalArrayStructureForIndexingType(ArrayWithContiguous), 0);
         for (unsigned i = 0; i < exportKeys->getArrayLength(); i++) {
             JSValue key = exportKeys->get(globalObject, i);
+            NAPI_RETURN_IF_EXCEPTION(env);
             auto propKey = key.toPropertyKey(globalObject);
+            NAPI_RETURN_IF_EXCEPTION(env);
             PropertyDescriptor desc;
 
             JSObject* owner = object;
             if (key_mode == napi_key_include_prototypes) {
                 // Climb up the prototype chain to find inherited properties
-                while (!owner->getOwnPropertyDescriptor(globalObject, propKey, desc)) {
-                    JSObject* proto = owner->getPrototype(globalObject).getObject();
+                while (true) {
+                    bool found = owner->getOwnPropertyDescriptor(globalObject, propKey, desc);
+                    NAPI_RETURN_IF_EXCEPTION(env);
+                    if (found) {
+                        break;
+                    }
+                    JSValue protoValue = owner->getPrototype(globalObject);
+                    NAPI_RETURN_IF_EXCEPTION(env);
+                    JSObject* proto = protoValue.getObject();
                     if (!proto) {
                         break;
                     }
@@ -2085,6 +2094,7 @@ extern "C" napi_status napi_get_all_property_names(
                 }
             } else {
                 owner->getOwnPropertyDescriptor(globalObject, propKey, desc);
+                NAPI_RETURN_IF_EXCEPTION(env);
             }
 
             // V8 never applies ONLY_WRITABLE/ONLY_CONFIGURABLE to Proxy keys
@@ -2115,6 +2125,7 @@ extern "C" napi_status napi_get_all_property_names(
 
             if (include) {
                 filteredKeys->push(globalObject, key);
+                NAPI_RETURN_IF_EXCEPTION(env);
             }
         }
         exportKeys = filteredKeys;
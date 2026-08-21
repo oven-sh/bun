6d7f261be0f8231a64c00f7b2a099242b32d36ec
robobun <117481402+robobun@users.noreply.github.com>
2026-08-21 01:13:31 +0000

    process: propagate from the worker stdio builder as well
    constructNodeWorkerStdioStream (from #39536) builds process.stdout,
    stderr and stdin of a node:worker_threads worker and had the same
    clear, report and store undefined shape as the builders converted here.

--- src/jsc/bindings/BunProcess.cpp
+++ src/jsc/bindings/BunProcess.cpp
@@ -2914,12 +2914,7 @@ static JSValue constructNodeWorkerStdioStream(JSC::JSGlobalObject* globalObject,
     args.append(JSC::jsNumber(fd));
     args.append(ports);
     auto result = JSC::profiledCall(globalObject, ProfilingReason::API, getStream, JSC::getCallData(getStream), globalObject->globalThis(), args);
-    if (auto* exception = scope.exception()) {
-        (void)scope.tryClearException();
-        Zig::GlobalObject::reportUncaughtExceptionAtEventLoop(globalObject, exception);
-        RETURN_IF_EXCEPTION(scope, jsUndefined());
-        return jsUndefined();
-    }
+    RETURN_IF_EXCEPTION(scope, {});
     return result;
 }
 

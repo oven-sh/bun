d02997c616af2b2a3b2c18abf0a88c11f45e48da
Jarred Sumner <jarred@jarredsumner.com>
2026-08-25 06:54:05 +0000

    BundleV2: drop the bust_dir_cache forwarder the hot reloader no longer calls

--- src/bundler/bundle_v2.rs
+++ src/bundler/bundle_v2.rs
@@ -7583,10 +7583,6 @@ pub mod bv2_impl {
             }
         }
 
-        /// To satisfy the interface from NewHotReloader()
-        pub fn bust_dir_cache(&mut self, path: &[u8]) -> bool {
-            self.transpiler.resolver.bust_dir_cache(path)
-        }
     }
 
     pub use bun_core::cheap_prefix_normalizer;

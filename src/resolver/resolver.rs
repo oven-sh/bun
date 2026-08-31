16655447868138772a5a6f3f369c9b977068f7f4
Jarred Sumner <jarred@jarredsumner.com>
2026-08-31 10:24:10 +0000

    cargo fmt

--- src/resolver/resolver.rs
+++ src/resolver/resolver.rs
@@ -1831,7 +1831,12 @@ impl<'a> Resolver<'a> {
                 let mut found = ResultUnion::NotFound;
                 for custom_path in &*custom_paths {
                     let custom_utf8 = custom_path.to_utf8();
-                    found = self.check_relative_path(custom_utf8.slice(), import_path, kind, global_cache);
+                    found = self.check_relative_path(
+                        custom_utf8.slice(),
+                        import_path,
+                        kind,
+                        global_cache,
+                    );
                     if !matches!(found, ResultUnion::NotFound) {
                         break;
                     }
@@ -1970,7 +1975,12 @@ impl<'a> Resolver<'a> {
                 let mut found = ResultUnion::NotFound;
                 for custom_path in &*custom_paths {
                     let custom_utf8 = custom_path.to_utf8();
-                    found = self.check_package_path(custom_utf8.slice(), import_path, kind, global_cache);
+                    found = self.check_package_path(
+                        custom_utf8.slice(),
+                        import_path,
+                        kind,
+                        global_cache,
+                    );
                     if !matches!(found, ResultUnion::NotFound) {
                         break;
                     }

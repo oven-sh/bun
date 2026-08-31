8281a15f46a21cf5338a1061a0cc32d29f0de760
Jarred Sumner <jarred@jarredsumner.com>
2026-08-31 07:39:57 +0000

    update bundler_browser / html-import-manifest expectations for setter-less __export

--- test/bundler/html-import-manifest.test.ts
+++ test/bundler/html-import-manifest.test.ts
@@ -107,11 +107,11 @@ console.log(favicon);
             "files": [
               {
                 "input": "client.html",
-                "path": "./client-tevdxjba.js",
+                "path": "./client-3f3jndb8.js",
                 "loader": "js",
                 "isEntry": true,
                 "headers": {
-                  "etag": "2s77zd0SC2o",
+                  "etag": "g8-j8jXNKyg",
                   "content-type": "text/javascript;charset=utf-8"
                 }
               },
@@ -121,7 +121,7 @@ console.log(favicon);
                 "loader": "html",
                 "isEntry": true,
                 "headers": {
-                  "etag": "GDn0RdEyL2w",
+                  "etag": "mPg2FlqrIhI",
                   "content-type": "text/html;charset=utf-8"
                 }
               },

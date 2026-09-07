f27aa4172132cb2327141b00b364bbb274890dfd
robobun <117481402+robobun@users.noreply.github.com>
2026-09-07 05:35:34 +0000

    IdentifierEventListenerMap: word the remove() contract in listener order

--- src/jsc/bindings/webcore/IdentifierEventListenerMap.h
+++ src/jsc/bindings/webcore/IdentifierEventListenerMap.h
@@ -54,7 +54,7 @@ public:
     // node:events semantics, not EventTarget's: the same function may be registered any number of times.
     void add(const JSC::Identifier& eventType, Ref<EventListener>&&, bool once);
     void prepend(const JSC::Identifier& eventType, Ref<EventListener>&&, bool once);
-    // Removes the most recently added registration of this callback.
+    // Removes the last registration of this callback in listener order, like node's removeListener.
     bool remove(const JSC::Identifier& eventType, EventListener&);
     // Removes exactly this registration.
     bool remove(const JSC::Identifier& eventType, SimpleRegisteredEventListener&);

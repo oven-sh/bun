769c998ebd8baa626e7e283d5a624337310415bf
robobun <117481402+robobun@users.noreply.github.com>
2026-09-16 08:08:03 +0000

    install: replace an existing file before linking a file: dependency on Windows

--- src/install/PackageInstall.rs
+++ src/install/PackageInstall.rs
@@ -1850,6 +1850,12 @@ impl<'a> PackageInstall<'a> {
                         }
                         EntryKind::File => match sys::symlink_w(dest, src, Default::default()) {
                             Err(err) => {
+                                if err.get_errno() == sys::E::EEXIST {
+                                    let _ = sys::unlink_w(dest);
+                                    sys::symlink_w(dest, src, Default::default())?;
+                                    continue;
+                                }
+
                                 if let Some(entry_dirname) =
                                     bun_paths::Dirname::dirname_u16(entry.path.as_slice())
                                 {
